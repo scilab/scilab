07410dd7addd70446d40c7512c06a379f8e401b0
Francois Delebecque <Francois.Delebecque@inria.fr>
2001-05-18 09:48:36 +0000

    zeros

--- scilab/routines/interf/matelm.f
+++ scilab/routines/interf/matelm.f
@@ -26,14 +26,14 @@ c     eye  rand ones maxi mini sort kron matr sin  cos  atan exp
 c      13   14   15   16   17   18  19-21 22   23   24   25   26
 c     sqrt log   ^  sign clean floor ceil expm cumsum  cumprod testmatrix
 c      27   28   29  30   31     32   33   34    35      36      37
-c     isreal frexp
-c       38     39
+c     isreal frexp zeros
+c       38     39    40
 c!
 c
       goto (10 ,15 ,20 ,25 ,30 ,35 ,40 ,45 ,50 ,60,
      1      61 ,62 ,70 ,72 ,71 ,90 ,91 ,105,110,110,
      2      110,130,140,150,160,170,180,190,200,210,
-     3      220,37 ,39 ,173,46 ,47, 230,240, 250),fin
+     3      220,37 ,39 ,173,46 ,47, 230,240, 250, 260),fin
 
  10   continue
       call intabs(id)
@@ -232,6 +232,9 @@ c
       call intfrexp(id)
       goto 900
 c
+ 260  continue
+      call intzeros(id)
+      goto 900
  900  return
       end
 
@@ -2625,6 +2628,113 @@ c     to avoid integer overflow
       lstk(top+1)=l+mn
       if(mn.eq.0) return
       call dset(mn,1.0d+0,stk(l),1)
+      return
+      end
+
+      subroutine intzeros(id)
+      INCLUDE '../stack.h'
+      integer id(nsiz),name(nlgh)
+
+      integer tops
+      double precision s
+      integer iadr,sadr
+c
+      iadr(l)=l+l-1
+      sadr(l)=(l/2)+1
+c
+      if (lhs .ne. 1) then
+         call error(41)
+         return
+      endif
+      if(rhs.gt.2) then
+         call setfunnam(ids(1,pt+1),'%hm_zeros',8)
+         fun=-1
+c         call error(42)
+         return
+      endif
+
+      tops=top
+c
+
+      if(rhs.le.0) then
+c     ones sans argument
+         top=top+1
+         m=1
+         n=1
+      elseif(rhs.eq.1) then
+         il=iadr(lstk(top))
+         if(abs(istk(il)).gt.10) then
+            call funnam(ids(1,pt+1),'zeros',il)
+            fun=-1
+            return
+         endif
+         if(istk(il).lt.0) il=iadr(istk(il+1))
+         m=istk(il+1)
+         n=istk(il+2)
+c     ones(matrice)
+      elseif(rhs.eq.2) then
+c     ones(m,n)
+         il=iadr(lstk(top))
+         if(istk(il).lt.0) il=iadr(istk(il+1))
+         if(istk(il).ne.1) then
+            err=1
+            call  error(53)
+            return
+         endif
+         if(istk(il+3).ne.0) then
+            err=1
+            call  error(52)
+            return
+         endif
+         if(istk(il+1)*istk(il+2).ne.1) then
+            err=1
+            call error(89)
+            return
+         endif
+         n=max(int(stk(sadr(il+4))),0)
+c
+         top=top-1
+         il=iadr(lstk(top))
+         if(istk(il).lt.0) il=iadr(istk(il+1))
+         if(istk(il).ne.1) then
+            err=1
+            call  error(53)
+            return
+         endif
+         if(istk(il+3).ne.0) then
+            err=1
+            call  error(52)
+            return
+         endif
+         if(istk(il+1)*istk(il+2).ne.1) then
+            err=1
+            call error(89)
+            return
+         endif
+         m=max(int(stk(sadr(il+4))),0)
+      endif
+c
+      mn=m*n
+      if(m.eq.0) n=0
+      if(n.eq.0) m=0
+
+      il=iadr(lstk(top))
+      l=sadr(il+4)
+
+c     to avoid integer overflow
+      s=l+dble(m)*dble(n)- lstk(bot)
+      if(s.gt.0.0d0) then
+         err=s
+         call error(17)
+         return
+      endif
+      istk(il)=1
+      istk(il+1)=m
+      istk(il+2)=n
+      istk(il+3)=0
+      lstk(top+1)=l+mn
+      if(mn.eq.0) return
+      call dset(mn,0.0d+0,stk(l),1)
       return
       end
 
