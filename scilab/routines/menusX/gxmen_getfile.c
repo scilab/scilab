d8ac3ad1e47196e85e30bcc6426fa4482f32d153
Jean-philippe Chancelier <jpc@cermics.enpc.fr>
2001-06-18 12:39:23 +0000

    removing

--- scilab/routines/menusX/gxmen_getfile.c
+++ scilab/routines/menusX/gxmen_getfile.c
@@ -44,10 +44,19 @@ static void file_selection_cancel (GtkWidget *w,  state *rep)
 
 /* XXXX reste a rajouter un bouton home et un bouton SCI 
  * Il faut aussi expanser les dirname 
+ * on utilise ici flag pour rajouter des actions 
+ * returns  a filename in file or an operator + filename  if 
+ * action == 1 
  */
 
-int GetFileWindow(char *filemask,char **file,char *dirname,int flag,int *ierr,char *title)
+int GetFileWindow(char *filemask,char **file,char *dirname,
+		  int action,int *ierr,char *title)
 {
+  static int last_choice = 0;
+  GList *cbitems = NULL;
+  GtkWidget *combo;
+  static int n_actions = 4 ;
+  static char *actions[]={ "getf","exec","load","chdir",NULL };
   guint signals[3];
   static state rep;
   GtkWidget *window;
@@ -59,6 +68,23 @@ int GetFileWindow(char *filemask,char **file,char *dirname,int flag,int *ierr,ch
   else 
     gtk_file_selection_set_filename (GTK_FILE_SELECTION (window),dirname);
   gtk_window_set_position (GTK_WINDOW (window), GTK_WIN_POS_MOUSE);
+
+  if ( action == 1 ) 
+    {
+      int j;
+      for (j = 0; j < n_actions  ; ++j) 
+	cbitems = g_list_append(cbitems, actions[j]);
+      combo =  gtk_combo_new ();
+      gtk_combo_set_popdown_strings (GTK_COMBO (combo), cbitems);
+      gtk_entry_set_text (GTK_ENTRY (GTK_COMBO(combo)->entry),
+			  actions[last_choice]);
+      gtk_entry_set_editable(GTK_ENTRY (GTK_COMBO(combo)->entry),FALSE);
+      
+      gtk_box_pack_start (GTK_BOX (GTK_FILE_SELECTION (window)->action_area), 
+			  combo, FALSE, FALSE, 0);
+      gtk_widget_show (combo);
+    }
+
   signals[0]=gtk_signal_connect (GTK_OBJECT (window), "destroy",
 		      GTK_SIGNAL_FUNC(file_selection_destroy),
 		      &rep);
@@ -80,17 +106,38 @@ int GetFileWindow(char *filemask,char **file,char *dirname,int flag,int *ierr,ch
     }
   if ( rep == OK ) 
     {
+      int action_length=0;
       char *loc = gtk_file_selection_get_filename(GTK_FILE_SELECTION(window));
-      if (( *file = (char *) MALLOC((strlen(loc)+1)*sizeof(char))) == NULL) 
+      if ( action == 1 ) 
+	{
+	  int j;
+	  gchar *entry_text;
+	  entry_text = gtk_entry_get_text(GTK_ENTRY(GTK_COMBO(combo)->entry));
+	  for (j = 0; j < n_actions ; ++j) 
+	    { 
+	      if ( strcmp(entry_text, actions[j]) == 0) 
+		{
+		  last_choice = j ;
+		  action_length = strlen(actions[j])+3;
+		  break;
+		}
+	    }
+	}
+      
+      if (( *file = (char *) MALLOC((strlen(loc)+1+action_length)*sizeof(char))) == NULL) 
 	{
 	  Scistring("Malloc : running out of memory");
 	  *ierr = 1;
 	}
       else 
 	{ 
-	  strcpy(*file,loc);
+	  if ( action == 1) 
+	    sprintf(*file,"%s %s;",actions[last_choice],loc);
+	  else 
+	    strcpy(*file,loc);
 	  *ierr=0;
 	}
+
     }
   /* since here we are no more in a gtk_main we must disconnect signals 
    * before destroying widget window 
