2c19b35a7ff0333c8e12a6c3538a66eb573b251d
Scilab <scilab@scilab.org>
2001-06-22 11:37:55 +0000

    ajout primitive rankqr

--- scilab/routines/interf/intslicot.c
+++ scilab/routines/interf/intslicot.c
@@ -5,10 +5,27 @@ extern Gatefunc C2F(sident);
 extern Gatefunc C2F(sorder);
 extern Gatefunc C2F(findbd);
 
+int intrankqr(fname)
+     char* fname;
+{
+  int *header1;int *header2;
+  int Cmplx;int ret;
+
+  header1 = (int *) GetData(1);
+  Cmplx=header1[3];
+  if (Cmplx==0) {
+    ret = C2F(intmb03od)("rankqr",6L);
+    return; } else
+      {
+	/*	ret = C2F(intzb03od)("rankqr",6L); */
+	return; } 
+}
+
 static GenericTable Tab[]={
-{(Myinterfun) fortran_mex_gateway, C2F(sident),"sident msg"},
-{(Myinterfun) fortran_mex_gateway, C2F(sorder),"sorder msg"},
-{(Myinterfun) fortran_mex_gateway, C2F(findbd),"findbd msg"},
+{(Myinterfun) fortran_mex_gateway, C2F(sident),"sident"},
+{(Myinterfun) fortran_mex_gateway, C2F(sorder),"sorder"},
+{(Myinterfun) fortran_mex_gateway, C2F(findbd),"findbd"},
+{(Myinterfun) sci_gateway, intrankqr,"rankqr"},
 	 };
  
 int C2F(intslicot)()
