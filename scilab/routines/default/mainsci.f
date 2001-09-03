d9cb8bee9c0582f6148e3c513eba753d92e701c7
Jean-philippe Chancelier <jpc@cermics.enpc.fr>
2001-09-03 07:09:28 +0000

    pour forcer un link

--- scilab/routines/default/mainsci.f
+++ scilab/routines/default/mainsci.f
@@ -1,5 +1,11 @@
       program scilab 
+      character*1 atom
 c     Copyright INRIA
       call realmain() 
+c     unused code but we want lge and lle to be loaded 
+c     in scilex
+      IF (LGE(ATOM,'0') .AND. LLE(ATOM,'9')) THEN
+         atom='0' 
+      endif
       end 
 
