e0f366bea7abd617cd9c354ad32bc43e972035ce
Jean-philippe Chancelier <jpc@cermics.enpc.fr>
2001-09-19 13:51:01 +0000

    color 7 was lost

--- scilab/routines/graphics/periX11.c
+++ scilab/routines/graphics/periX11.c
@@ -81,7 +81,7 @@ unsigned short default_colors[] = {
   0, 255, 255, /* Cyan */
   255,   0,   0, /* Red */
   255,   0, 255, /* Magenta */
-  255,   0,   0, /* Yellow */
+  255,   255,   0, /* Yellow */
   255, 255, 255, /* White: DEFAULTWHITE */
   0,   0, 144, /* Blue4 */
   0,   0, 176, /* Blue3 */
