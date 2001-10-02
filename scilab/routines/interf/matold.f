d789503b034024a0e89b5d07a6d37bd59223ac38
Serge Steer <serge.steer@scilab.org>
2001-10-02 15:56:30 +0000

    for backward compatibility (lapack)

---
+++ scilab/routines/interf/matold.f
@@ -0,0 +1,14 @@
+      subroutine matold
+c ====================================================================
+c
+c     evaluate functions involving gaussian elimination
+c
+c ====================================================================
+c
+c     Copyright INRIA
+      include '../stack.h'
+      buf='some primitive functions has moved,'//
+     &     'recompile your scilab functions'
+      call error(1111)
+      return
+      end
