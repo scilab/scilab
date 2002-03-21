a49c6011cd8edc72a8f6cf3db08f542b1736c73a
Jean-philippe Chancelier <jpc@cermics.enpc.fr>
2002-03-21 09:20:28 +0000

    ....

--- scilab/routines/interf/strelm.f
+++ scilab/routines/interf/strelm.f
@@ -29,6 +29,12 @@ c     9         10       11        12    13
 c     strcat    strindex strsubst ascii  grep
 
 c     
+c      if ( fin.eq.8)  then 
+c         call intssort(id)
+c      else 
+c         call cstrelm()
+c      endif
+c      return 
 
       goto (10,20,25,50,60,70,80,90,100,110,120,125,130) fin
 c     
@@ -36,83 +42,70 @@ c     length
 c     
  10   continue
       call intlength(id)
-c      call cstrelm()
       goto 900
 
 
 c     part
  20   continue
       call intpart(id)
-c      call cstrelm()
       goto 900
 c     
 c     string
 c     
  25   continue
       call intstring(id)
-c      call cstrelm()
       goto 900
 
 c     
 c     convstr
  50   continue
       call intconvstr(id)
-c      call cstrelm()
       goto 900
 
 c     emptystr
  60   continue
       call intemptystr(id)
-c      call cstrelm()
       goto 900
 c     
 c     str2code
  70   continue
       call intstr2code(id)
-c      call cstrelm()
       goto 900
 c     
 c     code2str
  80   continue
       call intcode2str(id)
-c      call cstrelm()
       goto 900
 c     
 c     sort
  90   continue
       call intssort(id)
-c      call cstrelm()
       goto 900
 c     
 c     strcat(str [,ins])
  100  continue
       call instrcat(id)
-c      call cstrelm()
       goto 900
 c     
 c     strindex(str1,str2)
  110  continue
       call instrindex(id)
-c      call cstrelm()
       goto 900
 c     
 c     strsubst(str1,str2,str3)
  120  continue 
       call intstrsubst(id)
-c      call cstrelm()
       goto 900
 c     
 c     ascii
 c     
  125  continue
       call intascii(id)
-c      call cstrelm()
       goto 900
 c     
 c     grep(str1,str2) 
  130  continue
       call intgrep()
-c     call cstrelm()
       goto 900
 
 c     
