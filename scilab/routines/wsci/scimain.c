3420d3dd5eeb64ae57d460c12983e7c05f360a5c
Jean-philippe Chancelier <jpc@cermics.enpc.fr>
2002-07-05 14:08:58 +0000

    added for cygwin

---
+++ scilab/routines/wsci/scimain.c
@@ -0,0 +1,22 @@
+#ifndef STRICT
+#define STRICT
+#endif
+#include <windows.h>
+#include <windowsx.h>
+#include <commctrl.h>
+
+#ifdef __STDC__
+#include <stdlib.h>
+#else
+#include <malloc.h>
+#endif
+
+extern int MyWinMain();
+
+int main()
+{
+  HMODULE x = GetModuleHandle (0);
+  MyWinMain (x, 0, GetCommandLine (), 1);
+  return (0);
+}
+
