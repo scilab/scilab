ece4d7abeb4e0fd6d4a550edb035508d571a1f36
Serge Steer <serge.steer@scilab.org>
2003-01-30 13:50:23 +0000

    modif de mat

--- scilab/tcl/browsehelpexe.c
+++ scilab/tcl/browsehelpexe.c
@@ -28,17 +28,26 @@ int main(int argc, char *argv[])
     }
   strcpy(TkScriptpath, getenv("SCI"));
   strcat(TkScriptpath, "/tcl/browsehelp.tcl");
-  
+
+#ifdef TCL_MAJOR_VERSION
+#ifdef TCL_MINOR_VERSION
+#if TCL_MAJOR_VERSION >= 8
+#if TCL_MINOR_VERSION > 0  
   Tcl_FindExecutable(NULL);
+#endif
+#endif
+#endif
+#endif 
+
   interp = Tcl_CreateInterp();
-  // TK_AppInit
+  /* TK_AppInit*/
   if (Tcl_Init(interp) == TCL_ERROR) 
     {
       return TCL_ERROR;
     }
   if (Tk_Init(interp) == TCL_ERROR) 
     {
-     //return TCL_ERROR;
+      /*return TCL_ERROR;*/
     }
   if (TKmainWindow == NULL)
     {
@@ -50,7 +59,7 @@ int main(int argc, char *argv[])
       Tcl_SetVar(interp, "SciPath",getenv("SCI") ,TCL_GLOBAL_ONLY);
       Tcl_EvalFile(interp, TkScriptpath);
       Tcl_Eval(interp, "wm withdraw .");
-      //Tcl_Eval(interp, "catch {destroy $sciw}");
+      /*Tcl_Eval(interp, "catch {destroy $sciw}");*/
       Tcl_Eval(interp, "wm protocol $sciw WM_DELETE_WINDOW exit;");
       Tcl_Eval(interp, "proc quitapp {widget} {exit;}");
       flushTKEvents();
