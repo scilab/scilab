f2a5971eed1f899d330cdd2154480e9de0b8ce4b
Ramine Nikoukhah <ramine.nikoukhah@inria.fr>
2003-03-20 10:25:51 +0000

    clipping

--- scilab/routines/scicos/scicosclip.c
+++ scilab/routines/scicos/scicosclip.c
@@ -1,12 +1,16 @@
 #include "../machine.h"
-
+#include <math.h>
+#include "../graphics/Math.h" 
+#include "../graphics/Graphics.h"
+#include "../graphics/PloEch.h"
 
 void C2F(scicosclip)(int *n)
 {
   if(*n==1){
-frame_clip_on();
+C2F(dr1)("xset","clipgrf",PI0,PI0,PI0,PI0, PI0,PI0,PD0,PD0,PD0,PD0,0L,0L);
   }
   else{
-frame_clip_off();
+C2F(dr1)("xset","clipoff",PI0,PI0,PI0,PI0, PI0,PI0,PD0,PD0,PD0,PD0,0L,0L);
+/*frame_clip_off();*/
   }
 }
