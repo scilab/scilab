522618528b3b284b49727efc195e0a6e1afc93c4
Scilab <scilab@scilab.org>
2003-11-03 15:36:48 +0000

    periWin.c

--- scilab/routines/graphics/periWin.c
+++ scilab/routines/graphics/periWin.c
@@ -455,7 +455,7 @@ void C2F(show)(v1, v2, v3, v4)
   if ( ScilabXgc->hdcCompat)
     {
       HDC hdc1=GetDC(ScilabXgc->CWindow);
-      sciprint("ici ");
+
       BitBlt (hdc1,0,0,ScilabXgc->CWindowWidth,ScilabXgc->CWindowHeight,
 	      ScilabXgc->hdcCompat,ScilabXgc->horzsi.nPos,ScilabXgc->vertsi.nPos,SRCCOPY);
       ReleaseDC(ScilabXgc->CWindow,hdc1);
@@ -2087,7 +2087,7 @@ void C2F(setpixmapOn)(num, v2, v3, v4)
 	  /* ajout */
 	  if (!hbmTemp)
 	    {
-	      sciprint("Seeting pixmap on is impossible \r\n");
+	      sciprint("Setting pixmap on is impossible \r\n");
 	      return;
 	    }
 	  else
