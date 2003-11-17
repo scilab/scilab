2c837b2de71212e60b1c61d5f3813e69d588ed26
Scilab <scilab@scilab.org>
2003-11-17 12:21:03 +0000

    indentation

--- scilab/routines/scicos/scicosclip.c
+++ scilab/routines/scicos/scicosclip.c
@@ -7,10 +7,9 @@
 void C2F(scicosclip)(int *n)
 {
   if(*n==1){
-C2F(dr1)("xset","clipgrf",PI0,PI0,PI0,PI0, PI0,PI0,PD0,PD0,PD0,PD0,0L,0L);
+    C2F(dr1)("xset","clipgrf",PI0,PI0,PI0,PI0, PI0,PI0,PD0,PD0,PD0,PD0,0L,0L);
   }
   else{
-C2F(dr1)("xset","clipoff",PI0,PI0,PI0,PI0, PI0,PI0,PD0,PD0,PD0,PD0,0L,0L);
-/*frame_clip_off();*/
+    C2F(dr1)("xset","clipoff",PI0,PI0,PI0,PI0, PI0,PI0,PD0,PD0,PD0,PD0,0L,0L);
   }
 }
