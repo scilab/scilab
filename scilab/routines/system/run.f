47deac8d31717b4ef7405416798c667c470fb7f7
Serge Steer <serge.steer@scilab.org>
2003-11-19 10:11:00 +0000

    bug errcatch

--- scilab/routines/system/run.f
+++ scilab/routines/system/run.f
@@ -123,8 +123,10 @@ c
          call putid(ids(1,pt+1),istk(lc+1))
          if(fun.eq.0) then
             call error(4)
-            top=top-1
-            return
+            if(err.gt.0) then
+               top=top-1
+               return
+            endif
          else
 c     .     referenced name was function at compile time it is now a
 c     .     primitive. Modify the code for further use
