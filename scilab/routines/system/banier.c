0c465ba6cb75dec6b5240034dfd47764993e7209
Allan Cornet <allan.cornet@scilab.org>
2003-12-02 15:39:30 +0000

    retrait ref. ENPC

--- scilab/routines/system/banier.c
+++ scilab/routines/system/banier.c
@@ -9,8 +9,8 @@ int C2F(banier)(integer *flag)
 {
   if (*flag != 999 && sci_show_banner == 1) {
     sciprint("        __________________________________________\r\n");
-    sciprint("                    %s\r\n\n",VERSION);
-    sciprint("            Copyright (C) 1989-2003 INRIA/ENPC  \r\n");
+    sciprint("                       %s\r\n\n",VERSION);
+    sciprint("              Copyright (C) 1989-2004 INRIA  \r\n");
     sciprint("        __________________________________________\r\n");
   }
   return 0;
