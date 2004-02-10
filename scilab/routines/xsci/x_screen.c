43873da7656e3dd0e33b7a0ef1e0c3983bc3c224
Serge Steer <serge.steer@scilab.org>
2004-02-10 09:35:17 +0000

    HistoryFunctions removed out of this file

--- scilab/routines/xsci/x_screen.c
+++ scilab/routines/xsci/x_screen.c
@@ -52,25 +52,6 @@ void ClearScreenConsole _PARAMS((char *fname))
 	sciprint("\nNot yet implemented. \n");
 }
 
-void HistoryFunction _PARAMS((char *fname))     
-{
-	sciprint("Not yet implemented. \r\n");
-}
-
-void LoadHistoryFunction(void)
-{
-  sciprint("Not yet implemented. \r\n");
-}
-
-void SaveHistoryFunction(void)
-{
-  sciprint("Not yet implemented. \r\n");
-}
-
-void ResetHistoryFunction(void)
-{
-  sciprint("Not yet implemented. \r\n");
-}
 /*
    allocates memory for a 2-dimensional array of chars and returns a pointer
    thereto
