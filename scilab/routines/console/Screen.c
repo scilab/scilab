1163a555af8d60e02215dcbaf16433e70680decb
Jean-philippe Chancelier <jpc@cermics.enpc.fr>
2004-02-11 21:30:31 +0000

    .h added for sciprint

--- scilab/routines/console/Screen.c
+++ scilab/routines/console/Screen.c
@@ -1,13 +1,17 @@
 #ifndef WIN32 /** The win32 version is defined in the wsci directory **/
+
+#include "../graphics/Graphics.h" /* for sciprint !! */
 /* Modification Allan CORNET */
 /* cls et tohome */
+
 void HomeFunction(void)
 {
-	sciprint("\nNot yet implemented. \n");
+  sciprint("\nNot yet implemented. \n");
 }
 
 void ClearScreenConsole(char *fname)
 {
-	sciprint("\nNot yet implemented. \n");
+  sciprint("\nNot yet implemented. \n");
 }
+
 #endif
