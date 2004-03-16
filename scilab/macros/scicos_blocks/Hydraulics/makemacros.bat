0742f5e1e0b84819652803a6ef8b0577ff573b85
Allan Cornet <allan.cornet@scilab.org>
2004-03-16 18:09:52 +0000

    ajout pour projet visual

---
+++ scilab/macros/scicos_blocks/Hydraulics/makemacros.bat
@@ -0,0 +1,9 @@
+if EXIST names del names > nul
+echo mode(0);>tmp_Macros
+for %%f in (*.sci) do echo %%~nf>>names
+for %%f in (*.sci) do echo getf('%%f'),save('%%~nf.bin'),clear ;>>tmp_Macros
+echo exit;>>tmp_Macros
+..\..\..\bin\scilex.exe -ns -nwni -f tmp_Macros
+..\..\..\bin\scilex.exe -ns -nwni -e scshydraulicslib=lib('SCI/macros/scicos_blocks/hydraulics/');save('SCI/macros/scicos_blocks/hydraulics/lib',scshydraulicslib);exit
+echo scshydraulicslib=lib('SCI/macros/scicos_blocks/hydraulics');save('SCI/macros/scicos_blocks/hydraulics/lib',scshydraulicslib);exit>genlib
+del tmp_Macros > nul
