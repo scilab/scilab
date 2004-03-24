18107e986d3808b39743a4ad4e9ea162345ec12d
Francois Delebecque <Francois.Delebecque@inria.fr>
2004-03-24 16:23:00 +0000

    hypermat dims is int32

--- scilab/examples/interface-tour-so/ex11intc.c
+++ scilab/examples/interface-tour-so/ex11intc.c
@@ -6,19 +6,19 @@
  */
 
 typedef struct hypermat {
-  int *dims,it,dimsize,size;
+  SciIntMat sc; /* coding informations */
+  int it,size;
   double *R, *I;
 } HyperMat;
 
 
 #define GetRhsHMat(pos,H) if (!get_rhs_hmat(pos,H)) { return 0;}
 
-int get_rhs_hmat(pos,H) 
-     int pos;
-     HyperMat *H;
+int get_rhs_hmat(int pos,HyperMat *H) 
 {
   char **Str2;
-  int m,n,l,m1,n1,m2,n2,l2,m3,n3,l3,it3,lc3;
+  int m,n,l,m1,n1,m2,n2,m3,n3,l3,it3,lc3;
+  SciIntMat l2;
   GetRhsVar(pos,"m",&m,&n,&l);
   CheckLength(pos,m,3);
   GetListRhsVar(pos,1,"S",&m1,&n1,&Str2);
@@ -27,10 +27,8 @@ int get_rhs_hmat(pos,H)
       Scierror(999,"Argument %d is not an hypermatrix\r\n",pos);
       return 0;
     }
-  GetListRhsVar(pos,2,"i",&m2,&n2,&l2);
+  GetListRhsVar(pos,2,"I",&m2,&n2,&H->sc);
   GetListRhsCVar(pos,3,"d",&it3,&m3,&n3,&l3,&lc3);
-  H->dims = istk(l2);
-  H->dimsize = m2*n2;
   H->size = m3*n3;
   H->R = stk(l3);
   H->I = (it3==1) ? stk(lc3): NULL;
@@ -43,15 +41,13 @@ int get_rhs_hmat(pos,H)
 
 #define CreateHMat(pos,H) if (! cre_hmat(pos,H)) { return 0;}
 
-int cre_hmat(pos,H)
-     int pos;
-     HyperMat *H;
+int cre_hmat(int pos,  HyperMat *H)
 {
   static char *Str[]= { "hm","dims","entries"}; int m1=1,n1=3;
   int mL=3,nL=1,lL, un=1;
   CreateVar(pos,"m", &mL, &nL, &lL);
   CreateListVarFromPtr(pos,1,"S", &m1, &n1, Str);
-  CreateListVarFromPtr(pos,2,"i",&un, &H->dimsize, &H->dims);
+  CreateListVarFromPtr(pos,2,"I",&(H->sc.m), &(H->sc.n), &(H->sc));
   CreateListCVarFromPtr(pos,3,"d", &H->it,&H->size,&un ,&H->R,&H->I);
   return 1;
 }
