61e4271b8fe9a409c0c015a79f921afd35f7daf3
Allan Cornet <allan.cornet@scilab.org>
2004-03-25 19:24:40 +0000

    Mise a jour pour Ramine

---
+++ scilab/routines/scicos/modulo_count.c
@@ -0,0 +1,15 @@
+#include "scicos_block.h"
+#include <math.h>
+#include <stdlib.h>
+#include "../machine.h"
+
+void 
+modulo_count(scicos_block *block,int flag)
+{
+  if(flag == 1) {
+    *block->outptr[0]=block->z[0];
+  }else if (flag == 2){
+    block->z[0]=(1+(int)block->z[0])%(block->ipar[0]);
+   }
+}
+
