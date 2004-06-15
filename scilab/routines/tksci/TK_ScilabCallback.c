8659577d502ad4988e6c2ad6b27fc2fb68599f3d
Allan Cornet <allan.cornet@scilab.org>
2004-06-15 09:13:56 +0000

    Correction Comportement tcl/tk bug avec ged()

--- scilab/routines/tksci/TK_ScilabCallback.c
+++ scilab/routines/tksci/TK_ScilabCallback.c
@@ -5,10 +5,15 @@
 #include "tksci.h"
 
 extern int StoreCommand ( char *command);
+extern int StoreCommand1 (char *command,int flag);
 
 int TK_EvalScilabCmd(ClientData clientData,Tcl_Interp * theinterp,int  objc,char ** argv)
 {
-  StoreCommand(argv[1]);  
+  #if WIN32
+		StoreCommand1 (argv[1],2);
+  #else
+		StoreCommand(argv[1]);  
+  #endif
   Tcl_SetResult(theinterp,NULL,NULL);
   return TCL_OK;
 }
