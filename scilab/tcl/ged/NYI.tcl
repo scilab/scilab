08481162d8d8eeefc3fd2d7b8b0aac561d10bf79
Fabrice Leray <fabriceyarel@hotmail.com>
2004-07-06 15:54:25 +0000

    Unimplemented case

---
+++ scilab/tcl/ged/NYI.tcl
@@ -0,0 +1,102 @@
+
+set sourcedir [file join "$env(SCIPATH)" "tcl" "utils"]
+
+source [file join $sourcedir Notebook.tcl]
+source [file join $sourcedir Combobox.tcl]
+package require combobox 2.3
+catch {namespace import combobox::*}
+
+global SELOBJECT
+global ged_handle_list_size
+global lalist
+global curgedindex
+global curgedobject
+
+global curvis
+
+set ww .axes
+catch {destroy $ww}
+toplevel $ww
+wm title $ww "Unimplemented Object"
+wm iconname $ww "UO"
+wm geometry $ww 650x700
+
+set w $ww
+frame $w.frame -borderwidth 0
+pack $w.frame -anchor w -fill both
+
+#Hierarchical selection
+set lalist ""
+for {set i 1} {$i<=$ged_handle_list_size} {incr i} { 
+append lalist "\""
+append lalist "$SELOBJECT($i)" 
+append lalist "\" "
+}
+
+set curgedobject $SELOBJECT($curgedindex)
+
+
+#Hiereachical viewer
+frame $w.frame.view  -borderwidth 0
+pack $w.frame.view  -in $w.frame  -side top  -fill x
+
+label $w.frame.selgedobjectlabel  -height 0 -text "Edit properties for:    " -width 0 
+combobox $w.frame.selgedobject \
+    -borderwidth 2 \
+    -highlightthickness 3 \
+    -maxheight 0 \
+    -width 3 \
+    -textvariable curgedobject \
+    -editable false \
+    -background white \
+    -command [list SelectObject ]
+eval $w.frame.selgedobject list insert end $lalist
+pack $w.frame.selgedobjectlabel -in $w.frame.view   -side left
+pack $w.frame.selgedobject   -in $w.frame.view   -fill x
+
+
+
+#temp label : unimplemented message
+frame $w.frame.lbl -borderwidth 0
+pack $w.frame.lbl -in $w.frame  -side top -fill x
+message  $w.frame.meslbl  -justify left -text " Unfortunatly, this type of entity is not yet implemented in the Graphic Editor. \ A complete graphic editor will be available soon."
+$w.frame.meslbl config -foreground red
+pack $w.frame.meslbl -in $w.frame.lbl  -anchor c -fill x
+
+
+
+Notebook:create .axes.n -pages {Style} -pad 20 
+pack .axes.n -fill both -expand 1
+
+########### Style onglet ##########################################
+###################################################################
+set w [Notebook:frame .axes.n Style]
+
+frame $w.frame -borderwidth 0
+pack $w.frame -anchor w -fill both
+
+#visibility
+frame $w.frame.vis -borderwidth 0
+pack $w.frame.vis  -in $w.frame  -side top -fill x
+label $w.frame.vislabel  -text "       Visibility: "
+checkbutton $w.frame.visib  -textvariable curvis  \
+    -variable curvis  -onvalue "on" -offvalue "off" \
+    -command "toggleVis" 
+pack $w.frame.vislabel -in $w.frame.vis  -side left
+pack $w.frame.visib  -in $w.frame.vis    -side left -fill x
+
+
+#exit button
+frame $w.buttons
+pack $w.buttons -side bottom -fill x -pady 2m
+button $w.buttons.dismiss -text Quit -command "destroy $ww" 
+pack $w.buttons.dismiss  -side bottom -expand 1
+
+
+
+#proc associes
+proc toggleVis {} {
+global curvis
+ScilabEval "global ged_handle;ged_handle.visible='$curvis'"
+}
+
