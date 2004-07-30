6b4a2f6b86e6822abc719817e204c2435d02334f
Serge Steer <serge.steer@scilab.org>
2004-07-30 15:39:04 +0000

    optional argument seq added

--- scilab/routines/tksci/TK_ScilabCallback.c
+++ scilab/routines/tksci/TK_ScilabCallback.c
@@ -6,10 +6,13 @@
 
 extern int StoreCommand ( char *command);
 extern int StoreCommand1 (char *command,int flag);
-
+extern void SetCommandflag(int flag) ;
 int TK_EvalScilabCmd(ClientData clientData,Tcl_Interp * theinterp,int  objc,char ** argv)
 {
-  StoreCommand(argv[1]);  
+  StoreCommand(argv[1]); 
+  if (argv[2] != (char *)0 && strncmp(argv[2],"seq",3)==0) {
+    SetCommandflag(1);
+  }
   Tcl_SetResult(theinterp,NULL,NULL);
   return TCL_OK;
 }
