86cb333abab6403fd059a7c1dba9c9509a374b87
Allan Cornet <allan.cornet@scilab.org>
2004-09-18 09:21:19 +0000

    correction detection ocaml build (getversion)

--- scilab/routines/system/inisci-c.c
+++ scilab/routines/system/inisci-c.c
@@ -2,13 +2,21 @@
 #include "../machine.h" 
 #include "../sun/Sun.h" 
 
+#ifdef WIN32
+#include <windows.h>
+BOOL FileExist(char *filename);
+BOOL ExistModelicac(void);
+#endif
+/*************************************************************************************************/
 /*-------------------------------------------
  *  get configure options used for compilation 
  *  used in inisci.f
  *  Copyright ENPC/ Jean-Philipe Chancelier 
  *-------------------------------------------*/
+/*************************************************************************************************/
 
-int C2F(withtk)(int *rep) { 
+int C2F(withtk)(int *rep)
+{ 
 #ifdef WITH_TK
   *rep =1; 
 #else 
@@ -16,8 +24,9 @@ int C2F(withtk)(int *rep) {
 #endif 
   return 0;
 }
-
-int C2F(withpvm)(int *rep) { 
+/*************************************************************************************************/
+int C2F(withpvm)(int *rep)
+{ 
 #ifdef WITH_PVM
   *rep =1; 
 #else 
@@ -25,8 +34,9 @@ int C2F(withpvm)(int *rep) {
 #endif 
   return 0;
 }
-
-int C2F(withgtk)(int *rep) { 
+/*************************************************************************************************/
+int C2F(withgtk)(int *rep)
+{ 
 #ifdef WITH_GTK
   *rep =1; 
 #else 
@@ -34,16 +44,28 @@ int C2F(withgtk)(int *rep) {
 #endif 
   return 0;
 }
-
-int C2F(withocaml)(int *rep) { 
-#ifdef WITH_OCAML
-  *rep =1; 
-#else 
-  *rep =0; 
-#endif 
+/*************************************************************************************************/
+int C2F(withocaml)(int *rep)
+{
+#ifdef WIN32
+	if (ExistModelicac())
+	{
+		*rep =1;
+	}
+	else
+	{
+		*rep =0; 
+	}
+#else
+	#ifdef WITH_OCAML
+		*rep =1; 
+	#else 
+		*rep =0; 
+	#endif 
+#endif
   return 0;
 }
-
+/*************************************************************************************************/
 int C2F(getcomp)(char *buf,int *nbuf,long int lbuf)
 {
   int ierr,iflag=0,l1buf=lbuf;
@@ -52,4 +74,37 @@ int C2F(getcomp)(char *buf,int *nbuf,long int lbuf)
   *nbuf = strlen(buf);
   return 0;
 }
+/*************************************************************************************************/
+#ifdef WIN32
+BOOL FileExist(char *filename)
+{
+     BOOL retour=FALSE;	
+	
+     WIN32_FIND_DATA FindFileData;
+     HANDLE handle = FindFirstFile (filename, &FindFileData);
+     if (handle != INVALID_HANDLE_VALUE)
+     {
+         FindClose (handle);
+         retour=TRUE;
+     }
+     else retour=FALSE;
 
+     return retour;
+}
+/*************************************************************************************************/
+BOOL ExistModelicac(void)
+{
+	#define ModelicacName "/bin/modelicac.exe"
+
+	BOOL bOK=FALSE;
+	char *SCIPATH = (char*)getenv ("SCI");
+	char *fullpathModelicac=NULL;
+	
+	fullpathModelicac=(char*)malloc((strlen(SCIPATH)+strlen(ModelicacName)+1)*sizeof(char));
+	wsprintf(fullpathModelicac,"%s%s",SCIPATH,ModelicacName);
+	bOK=FileExist(fullpathModelicac);
+	free(fullpathModelicac);
+	return bOK;
+}
+#endif
+/*************************************************************************************************/
