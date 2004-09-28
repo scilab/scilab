b68dd3027938804f6f975c75b312aff8dcbfd1c4
Claude Gomez <claude.gomez@scilab.org>
2004-09-28 08:38:52 +0000

    For HPUX 11, "finite" does not exist and we have to use "isfinite" instead.

--- scilab/routines/calelm/finite.c
+++ scilab/routines/calelm/finite.c
@@ -1,5 +1,16 @@
 #include "../machine.h"
 
+#if !(defined HAVE_FINITE) && (defined hppa)
+
+#include <math.h>
+
+int finite(double x)
+{
+  if (isfinite(x)==0) return 0;
+  return 1;
+}
+
+#else
 #if !(defined HAVE_FINITE) || (defined __MINGW32__) || (defined __ABSC__) 
 
 typedef unsigned int __uint32_t;
@@ -27,5 +38,5 @@ double x;
   hx = gh_u.parts.msw;
   return  (int)((__uint32_t)((hx&0x7fffffff)-0x7ff00000)>>31);
 }
-
+#endif
 #endif
