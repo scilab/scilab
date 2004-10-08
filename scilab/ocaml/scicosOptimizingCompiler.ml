41b4da286e6677c32c4178635d13a499550da1a8
Serge Steer <serge.steer@scilab.org>
2004-10-08 15:01:40 +0000

    version 1.4.0 (Sebastien Furic)

---
+++ scilab/ocaml/scicosOptimizingCompiler.ml
@@ -0,0 +1,15 @@
+(** Modelicac for Scicos. *)
+
+module Compiler = OptimizingCompiler.Make (ScicosCodeGeneration)
+
+let sccs_id =
+  "@(#)Modelicac - Copyright (C) 2003-2004 TNI-Valiosys"
+
+let name = Sys.argv.(0)
+
+let _ =
+  Printf.printf "This is Modelicac v.%s for Scicos.\n" Compiler.version;
+  Printf.printf "%s\n" (String.sub sccs_id 4 (String.length sccs_id - 4));
+  flush stdout
+
+let () = Compiler.run ()
