6c93c5ac2a6272efdda8fe89289f00e92bfa587e
Francois Vogel <fvogelnew1@free.fr>
2004-10-29 20:36:02 +0000

    Scipad 3.8

--- scilab/tcl/scipadsources/defaults.tcl
+++ scilab/tcl/scipadsources/defaults.tcl
@@ -1,5 +1,5 @@
 set winTitle "SciPad"
-set version "Version 3.7"
+set version "Version 3.8"
 set wordWrap none
 ##ES: default options which can be overridden
 if { ![info exists BGCOLOR] } {set BGCOLOR "snow1"}
