5a88ad61b64e6e100f0155f63feb357be91fbb52
Pierre Marechal <pierre.marechal@scilab.org>
2004-11-19 09:47:22 +0000

    bug // => /** **/ fixed

--- scilab/routines/interf/matdes.c
+++ scilab/routines/interf/matdes.c
@@ -4798,7 +4798,7 @@ int sciunzoom(fname,fname_len)
   else {
     int m,n,l,i;
     GetRhsVar(1,"h",&m,&n,&l); 
-    for (i=0;i<m*n;i++) unzoom_one_axes((sciPointObj*)hstk(l+i)); // Correction Bug 1083 Compilation avec VC++ 6.0
+    for (i=0;i<m*n;i++) unzoom_one_axes((sciPointObj*)hstk(l+i)); /** Correction Bug 1083 Compilation avec VC++ 6.0 **/
   }
   LhsVar(1)=0; 
   return 0;
