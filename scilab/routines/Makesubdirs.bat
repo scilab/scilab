2fcaaf5ecba4f3cd38dfdd15b70ef94b70ca9f11
Allan Cornet <allan.cornet@scilab.org>
2004-12-08 11:38:43 +0000

    Correction pour Compilation Win2K + VC 6

--- scilab/routines/Makesubdirs.bat
+++ scilab/routines/Makesubdirs.bat
@@ -127,14 +127,14 @@ cd wsci
 echo Making %1 in directory  wsci
  nmake /C /f Makefile.mak %1
 cd ..
-cd wsci/Wscilex
-echo Making %1 in directory  wsci/WScilex
+cd wsci\Wscilex
+echo Making %1 in directory  wsci\WScilex
  nmake /C /f Makefile.mak %1
-cd ../..
-cd wsci/CScilex
-echo Making %1 in directory  wsci/CScilex
+cd ..\..
+cd wsci\CScilex
+echo Making %1 in directory  wsci\CScilex
  nmake /C /f Makefile.mak %1
-cd ../..
+cd ..\..
 cd gd
 echo Making %1 in directory  gd
  nmake /C /f Makefile.mak %1
