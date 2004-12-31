4b50ea32fa7fb5748512376e606d1d94b895c320
Allan Cornet <allan.cornet@scilab.org>
2004-12-31 21:29:32 +0000

    Happy new year 2005

--- scilab/routines/system/banier.c
+++ scilab/routines/system/banier.c
@@ -16,7 +16,7 @@ int C2F(banier)(integer *flag)
 	#endif
 
     sciprint("                         %s\r\n\n",VERSION);
-    sciprint("                  Copyright (c) 1989-2004          \r\n");
+    sciprint("                  Copyright (c) 1989-2005          \r\n");
     sciprint("              Consortium Scilab (INRIA, ENPC)      \r\n");
 
     #ifdef WIN32
