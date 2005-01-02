d365b817a7bb938d44f847309bb4e86bf6c88937
Francois Vogel <fvogelnew1@free.fr>
2005-01-02 21:23:52 +0000

    added handling of sciprompt

--- scilab/routines/tksci/TK_ScilabCallback.c
+++ scilab/routines/tksci/TK_ScilabCallback.c
@@ -10,8 +10,10 @@ extern void SetCommandflag(int flag) ;
 extern void C2F(syncexec)(char * str, int *ns, int *ierr, int *seq);
 extern int GetCommand(char *str); 
 extern integer C2F (ismenu)(void); 
+extern int C2F(tksynchro)();
+static int c_n1 = -1;
 
-int TK_EvalScilabCmd(ClientData clientData,Tcl_Interp * theinterp,int objc,char ** argv)
+int TK_EvalScilabCmd(ClientData clientData,Tcl_Interp * theinterp,int objc,CONST char ** argv)
 {
   int ns,ierr,seq;
   char *command;
@@ -51,52 +53,48 @@ int TK_EvalScilabCmd(ClientData clientData,Tcl_Interp * theinterp,int objc,char 
 
     if (argv[2] != (char *)0 && strncmp(argv[2],"sync",4)==0) {
       /* sync or sync seq */
+      C2F(tksynchro)(&c_n1);  /* set sciprompt to -1 (scilab busy) */
       seq=(argv[3] != (char *)0) && (strncmp(argv[3],"seq",3)==0);
       ns=strlen(command); 
-      if (C2F(iop).ddt==-1) {
-        sciprint(" Execution starts for %s\r\n",command);
-      }
+      if (C2F(iop).ddt==-1) sciprint(" Execution starts for %s\r\n",command);
       C2F(syncexec)(command,&ns,&ierr,&seq);
-      if (C2F(iop).ddt==-1) {
-        sciprint(" Execution ends for %s\r\n",command);
-      }
+      if (C2F(iop).ddt==-1) sciprint(" Execution ends for %s\r\n",command);
+      C2F(tksynchro)(&C2F(recu).paus);
       if (ierr != 0) return TCL_ERROR;
     }
 
     else if (strncmp(command,"flush",5)==0) {
       /* flush */
-      if (C2F(iop).ddt==-1) {
-        sciprint(" Flushing starts for queued commands\r\n");
-      }
+      if (C2F(iop).ddt==-1) sciprint(" Flushing starts for queued commands\r\n");
       while (C2F(ismenu)() && ncomm<arbitrary_max_queued_callbacks-1) {
         ncomm++;
         comm[ncomm] = (char *) malloc (bsiz+1);
-			  if (comm[ncomm] == (char *) 0)
-				{
-					sciprint ("TK_EvalScilabCmd: No more memory\r\n");
-					return TCL_ERROR;
-				}
+        if (comm[ncomm] == (char *) 0)
+        {
+          sciprint ("TK_EvalScilabCmd: No more memory\r\n");
+          return TCL_ERROR;
+        }
         seqf[ncomm]=GetCommand (comm[ncomm]);
       }
-      if (C2F(ismenu)()) {
-          sciprint("Warning: Too many callbacks in queue!\r\n");
-      }
+      if (C2F(ismenu)()) sciprint("Warning: Too many callbacks in queue!\r\n");
       for (nc = 0 ; nc <= ncomm ; nc++ ) {
-        if (C2F(iop).ddt==-1) {
+        C2F(tksynchro)(&c_n1);  /* set sciprompt to -1 (scilab busy) */
+        if (C2F(iop).ddt==-1)
+        {
 		      if (seqf[nc]==0) { sciprint(" Flushed execution starts for %s - No option\r\n",comm[nc]); }
 		      else             { sciprint(" Flushed execution starts for %s - seq\r\n",comm[nc]); }
         }
         ns=strlen(comm[nc]);
         C2F(syncexec)(comm[nc],&ns,&ierr,&(seqf[nc]));
-        if (C2F(iop).ddt==-1) {
+        if (C2F(iop).ddt==-1)
+        {
           sciprint(" Flushed execution ends for %s\r\n",comm[nc]);
           free(comm[nc]);
         }
+        C2F(tksynchro)(&C2F(recu).paus);
         if (ierr != 0) return TCL_ERROR;
       }
-      if (C2F(iop).ddt==-1) {
-        sciprint(" Flushing ends\r\n");
-      }
+      if (C2F(iop).ddt==-1) sciprint(" Flushing ends\r\n");
     }
 
     else {
@@ -107,7 +105,7 @@ int TK_EvalScilabCmd(ClientData clientData,Tcl_Interp * theinterp,int objc,char 
       }
       else
       /* unknown option */
-      Tcl_SetResult(theinterp,NULL,NULL);
+        Tcl_SetResult(theinterp,NULL,NULL);
     }
     free(command);
 
