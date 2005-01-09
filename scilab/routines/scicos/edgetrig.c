b0c49e420fccf86d1c7148e6fad1b2bac939e14d
Allan Cornet <allan.cornet@scilab.org>
2005-01-09 08:25:34 +0000

    Correction Warning

--- scilab/routines/scicos/edgetrig.c
+++ scilab/routines/scicos/edgetrig.c
@@ -14,13 +14,13 @@ void edgetrig(scicos_block *block,int flag)
     if(block->ipar[0]!=0){
       z=z*block->ipar[0];
       u=u*block->ipar[0];
-      if((z<=0&u>0)||(z<0&u>=0)) {
+      if(((z<=0)&(u>0))||((z<0)&(u>=0))) {
 	block->outptr[0][0]=1.;
       }else{
 	block->outptr[0][0]=0.;
       }
     } else{ /* rising and falling edge */
-      if((z<=0&u>0)||(z<0&u>=0)||(z>0&u<=0)||(z>=0&u<0)){
+      if(((z<=0)&(u>0))||((z<0)&(u>=0))||((z>0)&(u<=0))||((z>=0)&(u<0))){
 	block->outptr[0][0]=1.;
       }else{
 	block->outptr[0][0]=0.;
