50dac54ac84a1f735408982ec0258a05c38c7492
Serge Steer <serge.steer@scilab.org>
2005-01-13 09:28:43 +0000

    d1mach definition removed

--- scilab/routines/integ/daux.f
+++ scilab/routines/integ/daux.f
@@ -1,20 +1,3 @@
-      DOUBLE PRECISION FUNCTION D1MACH (IDUM)
-      INTEGER IDUM
-C-----------------------------------------------------------------------
-C THIS ROUTINE COMPUTES THE UNIT ROUNDOFF OF THE MACHINE IN DOUBLE
-C PRECISION.  THIS IS DEFINED AS THE SMALLEST POSITIVE MACHINE NUMBER
-C U SUCH THAT  1.0D0 + U .NE. 1.0D0 (IN DOUBLE PRECISION).
-C-----------------------------------------------------------------------
-      DOUBLE PRECISION U, COMP
-      U = 1.0D0
- 10   U = U*0.5D0
-      COMP = 1.0D0 + U
-      IF (COMP .NE. 1.0D0) GO TO 10
-      D1MACH = U*2.0D0
-      RETURN
-C----------------------- END OF FUNCTION D1MACH ------------------------
-      END
-
 *DECK XERRWD
       SUBROUTINE XERRWD (MSG, NMES, NERR, LEVEL, NI, I1, I2, NR, R1, R2)
 C***BEGIN PROLOGUE  XERRWD
