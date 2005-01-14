104bd82b4a3ce584011da5241dbf02efb88caaaa
Allan Cornet <allan.cornet@scilab.org>
2005-01-14 10:32:56 +0000

    Compilation macros sparse directory

--- scilab/macros/sparse/makemacros.bat
+++ scilab/macros/sparse/makemacros.bat
@@ -4,6 +4,6 @@ for %%f in (*.sci) do echo %%~nf>>names
 for %%f in (*.sci) do echo getf('%%f'),save('%%~nf.bin'),clear ;>>tmp_Macros
 echo exit;>>tmp_Macros
 ..\..\bin\scilex.exe -ns -nwni -f tmp_Macros
-..\..\bin\scilex.exe -ns -nwni -e soundlib=lib('SCI/macros/sound/');save('SCI/macros/sparse/lib',sparselib);exit
+..\..\bin\scilex.exe -ns -nwni -e sparselib=lib('SCI/macros/sparse/');save('SCI/macros/sparse/lib',sparselib);exit
 echo sparselib=lib('SCI/macros/sparse/');save('SCI/macros/sparse/lib',sparselib);exit>genlib
 del tmp_Macros > nul
