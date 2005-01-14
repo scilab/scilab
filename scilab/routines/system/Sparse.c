04e467f4aeb9f0bc74915741d40969141711995d
Jean-philippe Chancelier <jpc@cermics.enpc.fr>
2005-01-14 14:16:17 +0000

    force link

---
+++ scilab/routines/system/Sparse.c
@@ -0,0 +1,19 @@
+/* Copyright INRIA/ENPC */
+#include "../machine.h"
+
+/** only used to force the linker to load  functions **/
+
+extern int C2F(dspe2)();
+extern int C2F(wspe2)();
+
+int Sparse_contents( int x)
+{
+  if ( x== 1) 
+    {
+      C2F(dspe2)();
+      C2F(wspe2)();
+    }
+  return 0;
+}
+
+
