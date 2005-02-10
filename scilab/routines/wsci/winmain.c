a67a1ea4142553d867060d2eb8f29fef324caec5
Allan Cornet <allan.cornet@scilab.org>
2005-02-10 15:47:29 +0000

    Correction definition SCI pour Windows suivant la version 9x, 2k ou XP ...

--- scilab/routines/wsci/winmain.c
+++ scilab/routines/wsci/winmain.c
@@ -1182,25 +1182,37 @@ void set_sci_env(char *p)
 	else
 	{
 		/* Error */
-		exit(0);
+		exit(1);
 	}
-
-	/* to be sure that it's unix format */
-	ConvertPathWindowsToUnixFormat(p,CopyOfp);
 
 	/* SCI variable Environment */
 	if ((environmentTemp = getenv ("SCI")) == (char *) 0)
 	{
-		if ( GetVersion() < 0x80000000 )
+		if ( GetVersion() < 0x80000000 ) /* Windows NT */
 		{
-			/* Windows NT */
 			char ShortPath[MAX_PATH+1];
-			GetShortPathName(CopyOfp ,ShortPath,MAX_PATH);
+			if ( ( GetOSVersion() == OS_WIN32_WINDOWS_XP ) || ( GetOSVersion() == OS_WIN32_WINDOWS_SERVER_2003_FAMILY ) )
+			{
+				/* to be sure that it's unix format */
+				/* c:/progra~1/scilab-3.1 */
+				ConvertPathWindowsToUnixFormat(p,CopyOfp);
+				GetShortPathName(CopyOfp ,ShortPath,MAX_PATH);
+			}
+			else /* Windows 2000 */
+			{
+				/* to be sure that it's windows format */
+				/* c:\progra~1\scilab-3.1 */
+				ConvertPathUnixToWindowsFormat(p,CopyOfp);
+				GetShortPathName(CopyOfp ,ShortPath,MAX_PATH);
+			}
 			sprintf (env, "SCI=%s",ShortPath);
 		}
 		else
 		{
 			/* Win32s, Win95,Win98,WinME */
+			ConvertPathUnixToWindowsFormat(p,CopyOfp);
+			/* to be sure that it's windows format 8.3 */
+			/* c:\progra~1\scilab-3.1 */
 			sprintf (env, "SCI=%s",CopyOfp);
 		}
 
