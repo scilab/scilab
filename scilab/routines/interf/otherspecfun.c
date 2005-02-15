49ca06267b126ec96c287938272cad116e1203d9
Serge Steer <serge.steer@scilab.org>
2005-02-15 17:07:53 +0000

    besselh function added

--- scilab/routines/interf/otherspecfun.c
+++ scilab/routines/interf/otherspecfun.c
@@ -42,6 +42,8 @@ extern void  C2F(dbesyv) (double *x,int* nx, double *alpha, int *na, int *kode,
 			  double *r, double *w, int *ierr);
 extern void  C2F(zbesyv) (double *xr,double *xi,int* nx, double *alpha, int *na, 
 			  int *kode, double *rr,double *ri, double *wr, double *wi, int *ierr);
+extern void  C2F(zbeshv) (double *xr,double *xi,int* nx, double *alpha, int *na, 
+			  int *kode, int *K, double *rr,double *ri, double *wr, double *wi, int *ierr);
 
 extern void  C2F(dxlegf)(double *dnu1, int *nudiff, int *mu1, int *mu2, double *x, 
 			int *id, double *pqa, int *ipqa, int *ierror);
@@ -935,6 +937,129 @@ int intbessely(fname,fname_len)
   return 0;
 }  
 
+int intbesselh(fname,fname_len)
+     /* Author Serge Steer, Copyright INRIA 2005 */
+     char *fname;
+     unsigned long fname_len;
+{
+  int m1,n1,l1,m2,n2,it2,l2,l2r,l2i,mr,nr,itr,lr,li,lwi,lwr,rhs1;
+  int r1,r2,na,nx,kode,lpos;
+  int un=1,ierr;
+  int K;
+
+  static int *Lstk    = C2F(vstk).lstk-1;
+
+ 
+  CheckRhs(2,4);
+  kode = 1;
+  rhs1 = Rhs;
+  if (Rhs==4) { /* normalized bessel required */
+    GetRhsVar(4,"d",&m1,&n1,&l1);CheckScalar(1,m1,n1);
+    kode = *stk(l1)+1;
+    rhs1--;
+  }
+  K = 1; 
+  if (Rhs > 2) { /* normalized bessel required */
+    GetRhsVar(2,"d",&m1,&n1,&l1);CheckScalar(1,m1,n1);
+    K = *stk(l1);
+  }
+
+  GetRhsVar(1,"d",&m1,&n1,&l1); /* get alpha */
+  r1=l1>Lstk[Bot]; /* true if the variable has been passed by reference */
+
+  GetRhsCVar(rhs1,"d",&it2,&m2,&n2,&l2,&l2i); /* get x */
+  r2=l2>Lstk[Bot];/* true if the variable has been passed by reference */
+  
+  if (m1*n1 == 0) { /*besselh([],x) */
+    LhsVar(1)=1;
+    return 0;
+  }
+  if (m2*n2 == 0) { /*besselh(alpha,[]) */
+    LhsVar(1)=rhs1;
+    return 0;
+  }
+
+  itr=1;
+  lpos=Rhs;
+  if (m1*n1 == 1) { /*bessely(scalar,matrix) */
+    double wr[3],wi[3];
+    mr=m2;
+    nr=n2;    
+    CreateCVar(lpos+1,"d",&itr,&mr,&nr,&lr,&li);
+    LhsVar(1)=lpos+1;
+ 
+    nx=m2*n2;
+    na=1;
+    C2F(zbeshv) (stk(l2),stk(l2i),&nx,stk(l1),&na,&kode,&K,stk(lr),stk(li),wr,wi,&ierr);
+  }
+
+  else if (m2*n2 == 1) { /* bessely(matrix,scalar) */
+    int lwr,lwi,nw;
+    mr=m1;
+    nr=n1;   
+    CreateCVar(lpos+1,"d",&itr,&mr,&nr,&lr,&li);
+    nx=1;
+    na=m1*n1;
+    nw=3*na;
+    CreateCVar(lpos+2,"d",&itr,&nx,&nw,&lwr,&lwi);
+    C2F(zbeshv) (stk(l2),stk(l2i),&nx,stk(l1),&na,&kode,&K,stk(lr),stk(li),stk(lwr),stk(lwi),&ierr);
+
+    LhsVar(1)=lpos+1;
+  }
+  else if ((m1==1 && n2==1)|| (n1==1 && m2==1)) { /* bessely(row,col) or bessely(col,row) */
+    int un=1,nw;
+    mr=m2*n2;
+    nr=m1*n1;
+    CreateCVar(lpos+1,"d",&itr,&mr,&nr,&lr,&li);
+    nx=m2*n2;
+    na=m1*n1;
+    nw=3*na;
+    CreateCVar(lpos+2,"d",&itr,&un,&nw,&lwr,&lwi);
+    C2F(zbeshv) (stk(l2),stk(l2i),&nx,stk(l1),&na,&kode,&K, stk(lr),stk(li),stk(lwr),stk(lwi),&ierr);
+
+    LhsVar(1)=lpos+1;
+  }
+  else { /* element wise case */
+    double wr[2],wi[2];
+    CheckDimProp(1,2,m1*n1!=m2*n2) 
+    mr=m2;
+    nr=n2;  
+    CreateCVar(lpos+1,"d",&itr,&mr,&nr,&lr,&li);
+    LhsVar(1)=lpos+1;
+    nx=mr*nr;
+    na=-1;
+    C2F(zbeshv) (stk(l2),stk(l2i),&nx,stk(l1),&na,&kode,&K,stk(lr),stk(li),wr,wi,&ierr);
+  }
+  if (ierr==2) {
+    if ( C2F(errgst).ieee==0) { 
+      ierr=69;
+      C2F(error)(&ierr);
+    }
+    else if ( C2F(errgst).ieee==1) {
+     ierr=63;
+     C2F(msgs)(&ierr,&un);
+ 
+    }
+  }
+  else if (ierr==3) {/* inacurate result */
+    ierr=4;
+    C2F(msgs)(&ierr,&un);
+  }
+  else if (ierr==4||ierr==5) {
+    if ( C2F(errgst).ieee==0) { 
+      ierr=69;
+      C2F(error)(&ierr);
+    }
+    else if ( C2F(errgst).ieee==1) {
+     ierr=107;
+     C2F(msgs)(&ierr,&un);
+    }
+ }
+ 
+
+  return 0;
+}  
+
 
 static TabF Tab[]={ 
   {intlegendre, "legendre"},
@@ -942,7 +1067,8 @@ static TabF Tab[]={
   {intbesseli,"besseli"},
   {intbesselj,"besselj"},
   {intbesselk,"besselk"},
-  {intbessely,"bessely"}
+  {intbessely,"bessely"},
+  {intbesselh,"besselh"}
 };
 
 int C2F(otherspfunlib)(void)
