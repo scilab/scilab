fc38e6bcb43d67d222466eaafe975c9d5a8432f8
Allan Cornet <allan.cornet@scilab.org>
2005-03-15 19:59:55 +0000

    Add support matrix to TCL_SetVar a=[1 2 3 4] TCL_SetVar('a',a)
    b=1
    TCL_SetVar('b',b)

--- scilab/routines/tclsci/intTclSetVar.c
+++ scilab/routines/tclsci/intTclSetVar.c
@@ -23,7 +23,6 @@ int C2F(intTclSetVar) _PARAMS((char *fname))
 			
 		GetRhsVar(2,"c",&m1,&n1,&l1);
 		VarValue=cstk(l1);
-		
 
 		if ( !Tcl_SetVar(TCLinterp, VarName, VarValue, 0) )
 		{
@@ -37,15 +36,74 @@ int C2F(intTclSetVar) _PARAMS((char *fname))
 		CreateVarFromPtr(1, "b", &n1, &n1, &paramoutINT);
 		LhsVar(1)=1;
 		C2F(putlhsvar)();
+		if (paramoutINT) {free(paramoutINT);paramoutINT=NULL;}
 
 	}
 	else
+	if ( (TypeVar1 == sci_strings) && (TypeVar2 == sci_matrix) )
 	{
-		Scierror(999,"TCL_SetVar : Argument type must be character string \r\n");
+		char *VarName=NULL,*VarValue=NULL;
+		char *VarNameWithIndice=NULL;
+		int *paramoutINT=(int*)malloc(sizeof(int));
+		int i=0;
+		int j=0;
+		int bTestTCL_SetVar=(int)(TRUE);
+		
+		GetRhsVar(1,"c",&m1,&n1,&l1);
+		VarName=cstk(l1);
+		
+		GetRhsVar(2,"d",&m1,&n1,&l1);
+
+		if ( (m1==1) && (n1==1) )
+		{
+			/* just a scalar */
+			char buffer[2048];
+
+			sprintf(buffer,"%.10lf",*stk(l1));
+			if (!Tcl_SetVar(TCLinterp,VarName,buffer,0))
+			{
+				bTestTCL_SetVar=(int)(FALSE);
+			}
+		}
+		else for (j=0;j<n1;j++)
+		{
+			char buffer[2048];
+			int lenJ=0;
+			int lenI=0;
+
+			itoa(j,buffer,10);
+			lenJ=strlen(buffer);
+
+			for (i=0;i<m1;i++)
+			{	
+				int len=0;
+				itoa(i,buffer,10);
+				lenI=strlen(buffer);
+							
+				len=strlen(VarName)+strlen("(,)")+lenI+lenJ+1;
+				VarNameWithIndice=(char*)malloc(len*sizeof(char));
+				sprintf(VarNameWithIndice,"%s(%d,%d)",VarName,i,j);
+				sprintf(buffer,"%.10lf",*stk(l1++));
+				if (!Tcl_SetVar(TCLinterp,VarNameWithIndice,buffer,0))
+				{
+					bTestTCL_SetVar=(int)(FALSE);
+				}
+				if (VarNameWithIndice){free(VarNameWithIndice);VarNameWithIndice=NULL;}
+			}
+		}
+		
+		*paramoutINT=bTestTCL_SetVar;
+		n1=1;
+		CreateVarFromPtr(1, "b", &n1, &n1, &paramoutINT);
+		LhsVar(1)=1;
+		C2F(putlhsvar)();
+		if (paramoutINT) {free(paramoutINT);paramoutINT=NULL;}
+	}
+	else
+	{
+		Scierror(999,"TCL_SetVar : Invalid Argument type.\r\n");
 		return 0;
 	}
-
-		
 	
 	return 0;
 }
