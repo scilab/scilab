67d55ea31aa1534288501a8d55ce258408dbe334
Francois Vogel <fvogelnew1@free.fr>
2005-03-25 12:21:30 +0000

    Scipad 4.0: Just a version number change for inclusion in Scilab 3.1RC

--- scilab/tcl/scipadsources/defaults.tcl
+++ scilab/tcl/scipadsources/defaults.tcl
@@ -1,5 +1,5 @@
 set winTitle "SciPad"
-set version "Version 3.56"
+set version "Version 4.0"
 
 # all one needs in order to add a new retrievable preference is:
 #  - add the variable name to $listofpref below, if it is not a list
