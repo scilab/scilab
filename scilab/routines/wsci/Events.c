f6e2afaba4b8d260ff8948c112578f6fb9de64c1
Allan Cornet <allan.cornet@scilab.org>
2005-03-30 14:24:29 +0000

    desactivation double click

--- scilab/routines/wsci/Events.c
+++ scilab/routines/wsci/Events.c
@@ -16,63 +16,9 @@ int GetEventKeyboardAndMouse(  UINT message, WPARAM wParam, LPARAM lParam,struct
 
 	switch(message)
 	{
-		case WM_TIMER:
-		{
-			
-		    KillTimer(ScilabGC->CWindow, wParam );
-
-			if ( wParam == WM_LBUTTONDOWN ) 
-			{
-					//sciprint("bouton pressed\n");
-
-					PushClickQueue (ScilabGC->CurWindow, ((int) LOWORD (lParam)) + 
-					ScilabGC->horzsi.nPos,HIWORD (lParam) + ScilabGC->vertsi.nPos, 0, 0, 0);
-					DoubleClick=FALSE;
-					
-			}
-
-			if ( wParam == WM_LBUTTONUP )
-			{
-				if (!DoubleClick)
-				{
-					if (Precedent==WM_LBUTTONDOWN)
-						{
-							//sciprint("bouton simple\n");
-						}
-					else
-						{
-							//sciprint("bouton released\n");
-							Precedent=wParam;
-						}
-
-					PushClickQueue (ScilabGC->CurWindow, ((int) LOWORD (lParam)) + 
-					ScilabGC->horzsi.nPos,HIWORD (lParam) + ScilabGC->vertsi.nPos, -5, 0, 1);
-			
-				}
-				else 
-				{
-					DoubleClick=FALSE;
-					Precedent=wParam;
-				}
-				
-			
-			}
-		    
-		}
-		return (0);
-
 		case WM_KEYDOWN:
 		/*http://msdn.microsoft.com/library/default.asp?url=/library/en-us/winui/winui/WindowsUserInterface/UserInput/VirtualKeyCodes.asp*/
 		{
-			/*check_pointer_win(&x,&y,&iwin);
-			if (GetKeyState (VK_CONTROL) < 0)
-			{
-				PushClickQueue (ScilabGC->CurWindow, x,y,1000+MapVirtualKey(wParam,0),0,0);
-			}
-			else
-			{
-				PushClickQueue (ScilabGC->CurWindow, x,y,MapVirtualKey(wParam,0),0,0);
-			}*/
 		}
 		return (0);
 
@@ -92,29 +38,10 @@ int GetEventKeyboardAndMouse(  UINT message, WPARAM wParam, LPARAM lParam,struct
 
 		case WM_LBUTTONDOWN:
 		{
-			Precedent=message;
-			SetTimer(ScilabGC->CWindow,message,GetDoubleClickTime(),NULL);
-		}
-		return (0);
-
-		case WM_LBUTTONUP:
-		{
-			Precedent=message;
-			SetTimer(ScilabGC->CWindow,message,GetDoubleClickTime(),NULL);
-		}
-		return (0);
-
-
-		case WM_LBUTTONDBLCLK:
-		{
-			KillTimer( ScilabGC->CWindow, WM_LBUTTONDOWN );
-			KillTimer(ScilabGC->CWindow, WM_LBUTTONUP );
-
 			PushClickQueue (ScilabGC->CurWindow, ((int) LOWORD (lParam)) + 
-			ScilabGC->horzsi.nPos,HIWORD (lParam) + ScilabGC->vertsi.nPos, DBL_CLCK_LEFT,0, 0);
+			ScilabGC->horzsi.nPos,HIWORD (lParam) + ScilabGC->vertsi.nPos, 0, 0, 0);
 		}
 		return (0);
-
 
 		case WM_MBUTTONDOWN:
 		{
@@ -130,6 +57,12 @@ int GetEventKeyboardAndMouse(  UINT message, WPARAM wParam, LPARAM lParam,struct
 		}
 		return (0);
 
+		case WM_LBUTTONUP:
+		{
+			PushClickQueue (ScilabGC->CurWindow, ((int) LOWORD (lParam)) + 
+			ScilabGC->horzsi.nPos,HIWORD (lParam) + ScilabGC->vertsi.nPos, -5, 0, 1);
+		}
+		return (0);
 
 		case WM_MBUTTONUP:
 		{
@@ -144,8 +77,140 @@ int GetEventKeyboardAndMouse(  UINT message, WPARAM wParam, LPARAM lParam,struct
 			ScilabGC->horzsi.nPos,HIWORD (lParam) + ScilabGC->vertsi.nPos, -3, 0, 1);
 		}
 		return (0);
-
+		
 	}
+	//switch(message)
+	//{
+	//	case WM_TIMER:
+	//	{
+	//		
+	//	    KillTimer(ScilabGC->CWindow, wParam );
+
+	//		if ( wParam == WM_LBUTTONDOWN ) 
+	//		{
+	//				//sciprint("bouton pressed\n");
+
+	//				PushClickQueue (ScilabGC->CurWindow, ((int) LOWORD (lParam)) + 
+	//				ScilabGC->horzsi.nPos,HIWORD (lParam) + ScilabGC->vertsi.nPos, 0, 0, 0);
+	//				DoubleClick=FALSE;
+	//				
+	//		}
+
+	//		if ( wParam == WM_LBUTTONUP )
+	//		{
+	//			if (!DoubleClick)
+	//			{
+	//				if (Precedent==WM_LBUTTONDOWN)
+	//					{
+	//						//sciprint("bouton simple\n");
+	//					}
+	//				else
+	//					{
+	//						//sciprint("bouton released\n");
+	//						Precedent=wParam;
+	//					}
+
+	//				PushClickQueue (ScilabGC->CurWindow, ((int) LOWORD (lParam)) + 
+	//				ScilabGC->horzsi.nPos,HIWORD (lParam) + ScilabGC->vertsi.nPos, -5, 0, 1);
+	//		
+	//			}
+	//			else 
+	//			{
+	//				DoubleClick=FALSE;
+	//				Precedent=wParam;
+	//			}
+	//			
+	//		
+	//		}
+	//	    
+	//	}
+	//	return (0);
+
+	//	case WM_KEYDOWN:
+	//	/*http://msdn.microsoft.com/library/default.asp?url=/library/en-us/winui/winui/WindowsUserInterface/UserInput/VirtualKeyCodes.asp*/
+	//	{
+	//		/*check_pointer_win(&x,&y,&iwin);
+	//		if (GetKeyState (VK_CONTROL) < 0)
+	//		{
+	//			PushClickQueue (ScilabGC->CurWindow, x,y,1000+MapVirtualKey(wParam,0),0,0);
+	//		}
+	//		else
+	//		{
+	//			PushClickQueue (ScilabGC->CurWindow, x,y,MapVirtualKey(wParam,0),0,0);
+	//		}*/
+	//	}
+	//	return (0);
+
+	//	case WM_CHAR:
+	//	{
+	//		check_pointer_win(&x,&y,&iwin);
+	//		PushClickQueue (ScilabGC->CurWindow, x,y,wParam,0,0);
+	//	}
+	//	return (0);
+
+	//	case WM_MOUSEMOVE:
+	//	{
+	//		PushClickQueue (ScilabGC->CurWindow, ((int) LOWORD (lParam)) + 
+	//		ScilabGC->horzsi.nPos,HIWORD (lParam) + ScilabGC->vertsi.nPos, -1, 1, 0);
+	//	}
+	//	return 0;
+
+	//	case WM_LBUTTONDOWN:
+	//	{
+	//		Precedent=message;
+	//		SetTimer(ScilabGC->CWindow,message,GetDoubleClickTime(),NULL);
+	//	}
+	//	return (0);
+
+	//	case WM_LBUTTONUP:
+	//	{
+	//		Precedent=message;
+	//		SetTimer(ScilabGC->CWindow,message,GetDoubleClickTime(),NULL);
+	//	}
+	//	return (0);
+
+
+	//	case WM_LBUTTONDBLCLK:
+	//	{
+	//		KillTimer( ScilabGC->CWindow, WM_LBUTTONDOWN );
+	//		KillTimer(ScilabGC->CWindow, WM_LBUTTONUP );
+
+	//		PushClickQueue (ScilabGC->CurWindow, ((int) LOWORD (lParam)) + 
+	//		ScilabGC->horzsi.nPos,HIWORD (lParam) + ScilabGC->vertsi.nPos, DBL_CLCK_LEFT,0, 0);
+	//	}
+	//	return (0);
+
+
+	//	case WM_MBUTTONDOWN:
+	//	{
+	//		PushClickQueue (ScilabGC->CurWindow, ((int) LOWORD (lParam)) + 
+	//		ScilabGC->horzsi.nPos,HIWORD (lParam) + ScilabGC->vertsi.nPos, 1, 0, 0);
+	//	}
+	//	return (0);
+
+	//	case WM_RBUTTONDOWN:
+	//	{
+	//		PushClickQueue (ScilabGC->CurWindow, ((int) LOWORD (lParam)) + 
+	//		ScilabGC->horzsi.nPos,HIWORD (lParam) + ScilabGC->vertsi.nPos, 2, 0, 0);
+	//	}
+	//	return (0);
+
+
+	//	case WM_MBUTTONUP:
+	//	{
+	//		PushClickQueue (ScilabGC->CurWindow, ((int) LOWORD (lParam)) + 
+	//		ScilabGC->horzsi.nPos,HIWORD (lParam) + ScilabGC->vertsi.nPos, -4, 0, 1);
+	//	}
+	//	return (0);
+
+	//	case WM_RBUTTONUP:
+	//	{
+	//		PushClickQueue (ScilabGC->CurWindow, ((int) LOWORD (lParam)) + 
+	//		ScilabGC->horzsi.nPos,HIWORD (lParam) + ScilabGC->vertsi.nPos, -3, 0, 1);
+	//	}
+	//	return (0);
+
+	//}
 
  	return CodeKey;
 }
