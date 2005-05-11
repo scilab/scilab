772c6bd0970039743426ce2255845a3e520d785b
Pierre Marechal <pierre.marechal@scilab.org>
2005-05-11 12:57:05 +0000

    Patch for Sun Solaris 5.8

--- scilab/routines/interf/dassl.c
+++ scilab/routines/interf/dassl.c
@@ -14,8 +14,12 @@ void  C2F(dassl)(void *res, int *neq, double *t, double *y, double *yprime, doub
 	    int *info, double *rtol, double *atol, int *idid, double *rwork, 
 	    int *lrw, int *iwork, int *liw, double *rpar, int *ipar, void *jac) 
 {
-
+  int itemp[12],i;
   if (setjmp(slatec_jmp_env)) return;
-  C2F(ddassl)(res, neq, t, y, yprime, tout, info, rtol, atol, idid, rwork, 
+  /* Nest instruction is a patch for sun solaris 5.8, 
+  without this trick the call to ddassl hangs Scilab */
+  for (i=0;i<11;i++) itemp[i]=info[i];
+  C2F(ddassl)(res, neq, t, y, yprime, tout, itemp, rtol, atol, idid, rwork, 
 	      lrw, iwork, liw, rpar, ipar, jac);
 }
+
