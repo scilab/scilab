4775ef58f10be7fdd835c6fe94338eb3d4024f2b
Serge Steer <serge.steer@scilab.org>
2005-06-27 12:45:34 +0000

    sun directory renamed os_specific

--- scilab/routines/sun/addinter.h
+++
@@ -1,7 +0,0 @@
-#ifndef __ADDINTER_H__
-#define  __ADDINTER_H__
-/** the first dynamic interface is at position 500+1 **/
-/* Copyright ENPC */
-#define DynInterfStart 500
-
-#endif /*  __ADDINTER_H__ */
