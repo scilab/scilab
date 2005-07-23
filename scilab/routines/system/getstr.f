b341d238c6acd4bf9de7df814b5a2b4da731a0af
Allan Cornet <allan.cornet@scilab.org>
2005-07-23 11:51:57 +0000

    modification temporaire pour compilation

--- scilab/routines/system/getstr.f
+++ scilab/routines/system/getstr.f
@@ -48,7 +48,7 @@ c     on ne sait pas encore combien il y en a
 c     Begin : reading the string 
       l=ln
       lpt(4) = lpt(3)
-      call getchfortran
+      call getch
    16 if (abs(char1) .eq. quote) go to 18
    17 ln = l+n
       if (char1 .eq. eol) then
@@ -62,9 +62,9 @@ c     Begin : reading the string
       endif
       if(err1.le.0) istk(ln) = char1
       n = n+1
-      call getchfortran
+      call getch
       go to 16
-   18 call getchfortran
+   18 call getch
       if (abs(char1) .eq. quote) go to 17
 c      if (n .le. 0) then
 c         call error(31)
