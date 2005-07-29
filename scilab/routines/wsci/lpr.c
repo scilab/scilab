c5fe99672a7af3a7be46f6eb7aec7776deb711cb
Allan Cornet <allan.cornet@scilab.org>
2005-07-29 12:08:16 +0000

    mise a jour appel explicite a libscilab.dll

--- scilab/routines/wsci/lpr.c
+++ scilab/routines/wsci/lpr.c
@@ -1,17 +1,40 @@
 /* Copyright (C) 2005 Allan CORNET */
+/***********************************************************************/
 #include <Windows.h>
-#pragma comment(lib, "../../bin/libscilab.lib")
-
-extern int gp_printfile (HINSTANCE hInstance, HWND hwnd, char *filename, char *port);
-
+/***********************************************************************/
+typedef int (*MYPROC) (HINSTANCE , HWND , char *, char *); 
+/***********************************************************************/
 int main (int argc, char *argv[])
 {
-	HWND parent;
-	HANDLE x = GetModuleHandleA (0);
-	parent = GetActiveWindow ();
-	if (argc == 2)
-		gp_printfile (x, parent, argv[1], (char *) 0);
-	else
-		gp_printfile (x, parent, (char *) 0, (char *) 0);
-	return (0);
+    HINSTANCE hinstLib; 
+    MYPROC gp_printfile; 
+    BOOL fFreeResult, fRunTimeLinkSuccess = FALSE; 
+    
+    hinstLib = LoadLibrary(TEXT("Libscilab")); 	
+ 
+    if (hinstLib != NULL) 
+    { 
+        gp_printfile = (MYPROC) GetProcAddress(hinstLib, TEXT("gp_printfile")); 
+ 
+        if (NULL != gp_printfile) 
+        {
+		HWND parent;
+		HANDLE x = GetModuleHandleA (0);
+						
+		fRunTimeLinkSuccess = TRUE;
+		parent = GetActiveWindow ();
+						
+		if (argc == 2)
+			(gp_printfile) (x, parent, argv[1], (char *) 0);
+		else
+			(gp_printfile) (x, parent, (char *) 0, (char *) 0);
+        }
+
+        fFreeResult = FreeLibrary(hinstLib); 
+    } 
+ 
+    if (! fRunTimeLinkSuccess) MessageBox(NULL,"Lpr : Libscilab.dll not found !","Warning",MB_ICONERROR); 
+	
+    return (0);
 }
+/***********************************************************************/
