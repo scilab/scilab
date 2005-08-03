60f590976500430761eea9820d4a0449700ecc46
Allan Cornet <allan.cornet@scilab.org>
2005-08-03 17:53:50 +0000

    Compilation pour Windows

--- scilab/routines/sci_mem_alloc.h
+++ scilab/routines/sci_mem_alloc.h
@@ -23,22 +23,28 @@
 #undef REALLOC
 #endif
 
-extern void * MyReAlloc(void * lpAddress,int dwSize,char *fichier,int ligne);
-extern void * MyAlloc(unsigned int dwSize,char *fichier,int ligne);
-extern void * MyCalloc(unsigned  int x, unsigned int y, char *fichier,int ligne);
+#ifndef WIN32
+ extern void * MyReAlloc(void * lpAddress,int dwSize,char *fichier,int ligne);
+ extern void * MyAlloc(unsigned int dwSize,char *fichier,int ligne);
+ extern void * MyCalloc(unsigned  int x, unsigned int y, char *fichier,int ligne);
 
-
-#ifdef lint5
-#include <sys/stdtypes.h>
-#define MALLOC(x) MyAlloc((size_t)x,__FILE__,__LINE__)
-#define FREE(x) if (x  != NULL) free((void *) x);
-#define REALLOC(x,y) MyReAlloc((void *)x, y,__FILE__,__LINE__)
-#define CALLOC(x,y) MyCalloc((size_t) x, (size_t) y,__FILE__,__LINE__)
+ #ifdef lint5
+   #include <sys/stdtypes.h>
+   #define MALLOC(x) MyAlloc((size_t)x,__FILE__,__LINE__)
+   #define FREE(x) if (x  != NULL) free((void *) x);
+   #define REALLOC(x,y) MyReAlloc((void *)x, y,__FILE__,__LINE__)
+   #define CALLOC(x,y) MyCalloc((size_t) x, (size_t) y,__FILE__,__LINE__)
+ #else
+   #define MALLOC(x) MyAlloc((unsigned)x,__FILE__,__LINE__)
+   #define FREE(x) if (x  != NULL) free((char *) x);
+   #define REALLOC(x,y) MyReAlloc((char *)x, y,__FILE__,__LINE__)
+   #define CALLOC(x,y) MyCalloc((unsigned) x, (unsigned) y,__FILE__,__LINE__)
+ #endif
 #else
-#define MALLOC(x) MyAlloc((unsigned)x,__FILE__,__LINE__)
-#define FREE(x) if (x  != NULL) free((char *) x);
-#define REALLOC(x,y) MyReAlloc((char *)x, y,__FILE__,__LINE__)
-#define CALLOC(x,y) MyCalloc((unsigned) x, (unsigned) y,__FILE__,__LINE__)
-#endif
+	#define MALLOC(x) malloc(((unsigned) x))
+	#define FREE(x) if (x  != NULL) free((char *) x);
+	#define REALLOC(x,y) realloc((char *) x,(unsigned) y)
+	#define CALLOC(x,y) calloc( x,(unsigned) y)
+#endif	
 
 #endif /* __SCI_MEM_ALLOC__ */
