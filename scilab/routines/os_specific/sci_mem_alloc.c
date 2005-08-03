0832414b7ae8052ab83e59002d351abd4475afac
Fabrice Leray <fabriceyarel@hotmail.com>
2005-08-03 14:53:57 +0000

    Add detection for bad memory allocation

---
+++ scilab/routines/os_specific/sci_mem_alloc.c
@@ -0,0 +1,94 @@
+#include "../sci_mem_alloc.h" /* For now, its header is in SCI/routines */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/* Allan CORNET */
+/* INRIA 2005 */
+
+#ifndef NULL
+#define NULL 0
+#endif
+
+/*-----------------------------------------------------------------------------------*/
+void * MyReAlloc(void * lpAddress,int dwSize,char *fichier,int ligne)
+{
+  
+  void * NewPointer=NULL;
+  
+  NewPointer = realloc(lpAddress,dwSize);
+  
+  if(NewPointer == NULL)
+    {
+#ifndef NDEBUG
+      printf("REALLOC Error File %s Line %d \n",fichier,ligne);
+      fflush(NULL);
+      exit(1);
+#endif
+    }
+  
+  return NewPointer;
+}
+/*-----------------------------------------------------------------------------------*/
+void * MyAlloc(unsigned int dwSize,char *fichier,int ligne)
+{
+  void * NewPointer=NULL;
+  
+  if (dwSize>0)
+    {
+      NewPointer = malloc(dwSize);
+      
+      if (NewPointer == NULL)
+	{
+#ifndef NDEBUG
+	  printf("MALLOC Error File %s Line %d \n",fichier,ligne);
+	  fflush(NULL);
+	  exit(1);
+#endif
+	}
+    }
+  else
+    {
+#ifndef NDEBUG
+      printf("MALLOC Error File %s Line %d \n",fichier,ligne);
+      fflush(NULL);
+      exit(1);
+#endif
+    }
+  
+  return NewPointer;
+  
+}
+
+
+/*-----------------------------------------------------------------------------------*/
+void * MyCalloc(unsigned int x, unsigned int y, char *fichier,int ligne)
+{
+  void * NewPointer=NULL;
+  
+  if ((x)*(y)>0)
+    {
+      NewPointer = calloc(x,y);
+      
+      if (NewPointer == NULL)
+	{
+#ifndef NDEBUG
+	  printf("CALLOC Error File %s Line %d \n",fichier,ligne);
+	  fflush(NULL);
+	  exit(1);
+#endif
+	}
+    }
+  else
+    {
+#ifndef NDEBUG
+	  printf("CALLOC Error File %s Line %d \n",fichier,ligne);
+	  fflush(NULL);
+	  exit(1);
+#endif
+    }
+
+  return NewPointer;
+
+}
+/*-----------------------------------------------------------------------------------*/
--- scilab/routines/sci_mem_alloc.h
+++ scilab/routines/sci_mem_alloc.h
@@ -23,17 +23,22 @@
 #undef REALLOC
 #endif
 
+extern void * MyReAlloc(void * lpAddress,int dwSize,char *fichier,int ligne);
+extern void * MyAlloc(unsigned int dwSize,char *fichier,int ligne);
+extern void * MyCalloc(unsigned  int x, unsigned int y, char *fichier,int ligne);
+
+
 #ifdef lint5
-	#include <sys/stdtypes.h>
-	#define MALLOC(x) malloc(((size_t) x))
-	#define FREE(x) if (x  != NULL) free((void *) x);
-	#define REALLOC(x,y) realloc((void *) x,(size_t) y)
-	#define CALLOC(x,y) calloc(x,(size_t) y)
+#include <sys/stdtypes.h>
+#define MALLOC(x) MyAlloc((size_t)x,__FILE__,__LINE__)
+#define FREE(x) if (x  != NULL) free((void *) x);
+#define REALLOC(x,y) MyReAlloc((void *)x, y,__FILE__,__LINE__)
+#define CALLOC(x,y) MyCalloc((size_t) x, (size_t) y,__FILE__,__LINE__)
 #else
-	#define MALLOC(x) malloc(((unsigned) x))
-	#define FREE(x) if (x  != NULL) free((char *) x);
-	#define REALLOC(x,y) realloc((char *) x,(unsigned) y)
-	#define CALLOC(x,y) calloc( x,(unsigned) y)
+#define MALLOC(x) MyAlloc((unsigned)x,__FILE__,__LINE__)
+#define FREE(x) if (x  != NULL) free((char *) x);
+#define REALLOC(x,y) MyReAlloc((char *)x, y,__FILE__,__LINE__)
+#define CALLOC(x,y) MyCalloc((unsigned) x, (unsigned) y,__FILE__,__LINE__)
 #endif
 
 #endif /* __SCI_MEM_ALLOC__ */
