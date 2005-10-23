93b0675449226217c062855d350a16639a16c503
Allan Cornet <allan.cornet@scilab.org>
2005-10-23 09:45:06 +0000

    Update "host" under Windows 2k and more no console created by "host" Correction for Didier ;)

--- scilab/routines/os_specific/systemc.c
+++ scilab/routines/os_specific/systemc.c
@@ -1,20 +1,104 @@
-/* Copyright INRIA/ENPC */
-/*
-  Interface with system C function
-  */
-#include<stdio.h>
-#include "../machine.h"
-
-#ifdef __STDC__
-#include <stdlib.h>
-#else
-int  system();
+/*-----------------------------------------------------------------------------------*/
+/* Interface with system C function */
+/* Copyright (Allan CORNET) INRIA 2005 */
+/*-----------------------------------------------------------------------------------*/
+#ifdef WIN32
+  #include <Windows.h>
+  #include "../wsci/GetOs.h"
 #endif
 
+#include<stdio.h>
+#include "../machine.h"
+/*-----------------------------------------------------------------------------------*/
+#ifdef __STDC__
+	#include <stdlib.h>
+#else
+	int  system();
+#endif
+
+#ifdef WIN32
+int Windows_system(char *command);
+extern BOOL IsAFile(char *chainefichier);
+#endif
+/*-----------------------------------------------------------------------------------*/
 int C2F(systemc)(char *command, integer *stat)
 {
-  int status;
-  status=system(command);
-  *stat=(integer)status;
-  return(0);
+	#ifndef WIN32
+		int status;
+		status=system(command);
+		*stat=(integer)status;
+	#else
+		*stat=(integer)Windows_system(command);
+	#endif
+
+	return(0);
 }
+/*-----------------------------------------------------------------------------------*/
+#ifdef WIN32
+int Windows_system(char *command)
+{
+	int CurrentOS=0;
+	int bReturn=FALSE;
+	
+	CurrentOS=GetOSVersion();
+
+	/* Old mode for old Windows ... */
+	if ( (CurrentOS==OS_WIN32_WINDOWS_NT_3_51) ||
+		 (CurrentOS==OS_WIN32_WINDOWS_NT_4_0) ||
+		 (CurrentOS==OS_WIN32_WINDOWS_95) ||
+		 (CurrentOS==OS_WIN32_WINDOWS_98) ||
+		 (CurrentOS==OS_WIN32_WINDOWS_Me) )
+	{
+		bReturn=system(command);
+	}
+	else
+	{
+		BOOL bOK=FALSE;
+		SHELLEXECUTEINFO ShExecInfo;
+		char DefaultShell[MAX_PATH];
+		char CommandLine[MAX_PATH];
+		char HostFilename[MAX_PATH];
+		char *TMPDir=NULL;
+		DWORD _Result=0;
+
+		GetEnvironmentVariable("COMSPEC",DefaultShell,MAX_PATH-1);
+		TMPDir=getenv("TMPDIR");
+
+		wsprintf(HostFilename,"%s\\host.txt",TMPDir);
+		if (IsAFile(HostFilename))
+		{
+			DeleteFile(HostFilename);
+		}
+
+		wsprintf(CommandLine,"/C \"%s &&echo hostOK>%s\"",command,HostFilename);
+
+		ShExecInfo.cbSize = sizeof(SHELLEXECUTEINFO);
+		ShExecInfo.fMask = SEE_MASK_NOCLOSEPROCESS;
+		ShExecInfo.hwnd = NULL;
+		ShExecInfo.lpVerb = "open";
+		ShExecInfo.lpFile = DefaultShell;
+		ShExecInfo.lpParameters = CommandLine;
+		ShExecInfo.lpDirectory = NULL;
+		ShExecInfo.nShow = SW_HIDE;
+		ShExecInfo.hInstApp = NULL;
+		bOK=ShellExecuteEx(&ShExecInfo);
+
+		_Result = WaitForSingleObject(ShExecInfo.hProcess,1000);
+
+		if (IsAFile(HostFilename))
+		{
+			CloseHandle(ShExecInfo.hProcess);
+			DeleteFile(HostFilename);
+			bReturn=FALSE;
+		}
+		else
+		{
+			TerminateProcess(ShExecInfo.hProcess,0);
+			CloseHandle(ShExecInfo.hProcess);
+			bReturn=TRUE;
+		}
+	}
+	return bReturn;
+}
+#endif
+/*-----------------------------------------------------------------------------------*/
