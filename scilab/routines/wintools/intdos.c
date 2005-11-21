4d30115fe6252fa3b5e042e9824329500e92e35f
Allan Cornet <allan.cornet@scilab.org>
2005-11-21 05:08:01 +0000

    Optimisation and Correction Trou Memoire

--- scilab/routines/wintools/intdos.c
+++ scilab/routines/wintools/intdos.c
@@ -11,8 +11,9 @@
 #endif
 /*-----------------------------------------------------------------------------------*/
 #ifdef WIN32
-  char ** CallWindowsShell(char *command,BOOL WaitInput,int *Status,int *NumberOfLines);
   extern BOOL IsAFile(char *chainefichier);
+	BOOL CallWindowsShell(char *command,BOOL WaitInput);
+	char ** CallWindowsShellWithOuput(char *command,BOOL WaitInput,int *Status,int *NumberOfLines);
 #endif
 /*-----------------------------------------------------------------------------------*/
 #define ErrMsgDos "Only for Windows 2k and more."
@@ -23,168 +24,212 @@ int C2F(intdos) _PARAMS((char *fname))
 	static int l1,n1,m1;
 	char **Result=NULL;
 	int *Status=NULL;
-	int NumbersOfLines=1;
+	int NumberOfLines=1;
+	char *CommandLine=NULL;
+	
+	int OS=0;
+	
 
 	CheckRhs(1,1);
 	CheckLhs(1,2);
 
 	Status=(int*)MALLOC(sizeof(int));
 
-	if (GetType(1)==sci_strings)
+	if (GetType(1)!=sci_strings)
 	{
-		int OS=0;
-		char *CommandLine=NULL;
-		GetRhsVar(1,"c",&m1,&n1,&l1);
-		CommandLine=cstk(l1);
-		#ifdef WIN32
-			OS=GetOSVersion();
+		Scierror(999,"Invalid parameter must a string.\r\n");
+		return 0;
+	}
+
+	GetRhsVar(1,"c",&m1,&n1,&l1);
+	CommandLine=cstk(l1);
+
+	if (Lhs == 1)
+	{
+		#if WIN32
+			int OS=GetOSVersion();
 			if ( (OS==OS_WIN32_WINDOWS_NT_3_51) ||
-				 (OS==OS_WIN32_WINDOWS_NT_4_0) ||
-				 (OS==OS_WIN32_WINDOWS_95) ||
-				 (OS==OS_WIN32_WINDOWS_98) ||
-				 (OS==OS_WIN32_WINDOWS_Me) )
+					(OS==OS_WIN32_WINDOWS_NT_4_0) ||
+					(OS==OS_WIN32_WINDOWS_95) ||
+					(OS==OS_WIN32_WINDOWS_98) ||
+					(OS==OS_WIN32_WINDOWS_Me) )
 			{
-				Result=(char**)MALLOC(sizeof(char**));
-				Result[0]=MALLOC((strlen(ErrMsgDos)+1)*sizeof(char));
-				sprintf(Result[0],"%s",ErrMsgDos);
-				NumbersOfLines=1;
-				m1=NumbersOfLines;
-				n1=1;
 				*Status=FALSE;
 			}
 			else
 			{
-				_try
-				{
-					Result=CallWindowsShell(CommandLine,FALSE,Status,&NumbersOfLines);
-				}
-				_except (EXCEPTION_EXECUTE_HANDLER)
-				{
-					Scierror(999,"Error with %s.\r\n",fname);
-					return 0;
-				}
+				*Status=CallWindowsShell(CommandLine,FALSE);
 			}
-		#endif
-	}
-	else
-	{
-		Scierror(999,"Invalid parameter must a string.\r\n");
-	}
-	
-	if (Lhs == 1)
-	{
-		#ifndef WIN32
-			*Status=FALSE;
 		#else
-		{
-			int OS=GetOSVersion();
-			if ( (OS==OS_WIN32_WINDOWS_NT_3_51) ||
-				(OS==OS_WIN32_WINDOWS_NT_4_0) ||
-				(OS==OS_WIN32_WINDOWS_95) ||
-				(OS==OS_WIN32_WINDOWS_98) ||
-				(OS==OS_WIN32_WINDOWS_Me) )
-			{
-				sciprint("%s\n",ErrMsgDos);
-			}
-		}
+			*Status=FALSE;
 		#endif
-        n1=1;
-
-		CreateVarFromPtr(Rhs+1, "b", &n1, &n1, &Status);
-		LhsVar(1)=Rhs+1;
 	}
 	else /* Lhs == 2 */
 	{
-		#ifndef WIN32
-			Result=(char**)MALLOC(sizeof(char**));
-			Result[0]=MALLOC((strlen(ErrMsgDos)+1)*sizeof(char));
-			sprintf(Result[0],"%s",ErrMsgDos);
+		#if WIN32
+			int OS=GetOSVersion();
+			if ( (OS==OS_WIN32_WINDOWS_NT_3_51) ||
+					(OS==OS_WIN32_WINDOWS_NT_4_0) ||
+					(OS==OS_WIN32_WINDOWS_95) ||
+					(OS==OS_WIN32_WINDOWS_98) ||
+					(OS==OS_WIN32_WINDOWS_Me) )
+					{
+						m1=1;
+						n1=1;
+						*Status=FALSE;
+						CreateVarFromPtr(Rhs+1, "b", &n1, &n1, &Status);
+						LhsVar(1)=Rhs+1;
+
+						m1=NumberOfLines;
+						n1=1;
+						Result=(char**)MALLOC(sizeof(char**));
+						Result[0]=MALLOC((strlen(ErrMsgDos)+1)*sizeof(char));
+						sprintf(Result[0],"%s",ErrMsgDos);
+						CreateVarFromPtr(Rhs+2, "S",&m1, &n1, Result);
+						LhsVar(2)=Rhs+2;
+					}
+					else
+					{
+						Result=CallWindowsShellWithOuput(CommandLine,FALSE,Status,&NumberOfLines);
+
+						m1=1;
+						n1=1;
+						CreateVarFromPtr(Rhs+1, "b", &n1, &n1, &Status);
+						LhsVar(1)=Rhs+1;
+
+						m1=NumberOfLines;
+						n1=1;
+						CreateVarFromPtr(Rhs+2, "S",&m1, &n1, Result);
+						LhsVar(2)=Rhs+2;
+					}
+		#else
 			m1=1;
 			n1=1;
 			*Status=FALSE;
-
 			CreateVarFromPtr(Rhs+1, "b", &n1, &n1, &Status);
 			LhsVar(1)=Rhs+1;
 
+			m1=NumberOfLines;
+			n1=1;
+			Result=(char**)MALLOC(sizeof(char**));
+			Result[0]=MALLOC((strlen(ErrMsgDos)+1)*sizeof(char));
+			sprintf(Result[0],"%s",ErrMsgDos);
 			CreateVarFromPtr(Rhs+2, "S",&m1, &n1, Result);
 			LhsVar(2)=Rhs+2;
-		
-		#else
-		{
-			int OS=GetOSVersion();
-			if ( !((OS==OS_WIN32_WINDOWS_NT_3_51) ||
-				(OS==OS_WIN32_WINDOWS_NT_4_0) ||
-				(OS==OS_WIN32_WINDOWS_95) ||
-				(OS==OS_WIN32_WINDOWS_98) ||
-				(OS==OS_WIN32_WINDOWS_Me)) )
-			{
-				m1=NumbersOfLines;
-				n1=1;
-			}
-
-			CreateVarFromPtr(Rhs+1, "b", &n1, &n1, &Status);
-			LhsVar(1)=Rhs+1;
-
-			if (m1)
-			{
-				CreateVarFromPtr(Rhs+2, "S",&m1, &n1, Result);
-			}
-			else
-			{
-				m1=0;
-				n1=0;
-				l1=0;
-				CreateVar(Rhs+2,"d",  &m1, &n1, &l1);
-			}
-			LhsVar(2)=Rhs+2;
-
-		}	
 		#endif
 	}
 
 	C2F(putlhsvar)();
-
 	if (Status) {FREE(Status);Status=NULL;}
-	if (Result)
+	if (Lhs == 2)
 	{
-	#ifdef WIN32
-		_try
-		{
-			for(i=0;i<m1;i++)
-			{
-				if (Result[i]) {FREE(Result[i]);Result[i]=NULL;}
-			}
-			FREE(Result);
-			Result=NULL;
-		}
-		_except (EXCEPTION_ACCESS_VIOLATION)
-		{
-		}
-#else
-		for(i=0;i<m1;i++)
+		for(i=0;i<NumberOfLines;i++)
 		{
 			if (Result[i]) {FREE(Result[i]);Result[i]=NULL;}
 		}
 		FREE(Result);
 		Result=NULL;
-#endif
 	}
 	return 0;
 }
 /*-----------------------------------------------------------------------------------*/
-#ifdef WIN32
-char ** CallWindowsShell(char *command,BOOL WaitInput,int *Status,int *NumberOfLines)
+#if WIN32
+BOOL CallWindowsShell(char *command,BOOL WaitInput)
 {
-	#define BUFSIZE 4096
+	BOOL ReturnStatus=FALSE;
+	char shellCmd[_MAX_PATH];
+	char *CmdLine=NULL;
+	
+	PROCESS_INFORMATION piProcInfo; 
+	STARTUPINFO siStartInfo;
+	BOOL bFuncRetn = FALSE; 
+	SECURITY_ATTRIBUTES saAttr; 
+
+	DWORD ExitCode;
+
+	char *TMPDir=NULL;
+	char FileTMPDir[MAX_PATH];
+
+	saAttr.nLength = sizeof(SECURITY_ATTRIBUTES); 
+	saAttr.bInheritHandle = TRUE; 
+	saAttr.lpSecurityDescriptor = NULL; 
+	
+	ZeroMemory( &piProcInfo, sizeof(PROCESS_INFORMATION) );
+	
+	ZeroMemory( &siStartInfo, sizeof(STARTUPINFO) );
+	siStartInfo.cb = sizeof(STARTUPINFO); 
+	siStartInfo.dwFlags      = STARTF_USESHOWWINDOW | STARTF_USESTDHANDLES;
+	siStartInfo.wShowWindow  = SW_HIDE;
+	if (WaitInput)
+	{
+		siStartInfo.hStdInput=GetStdHandle(STD_INPUT_HANDLE);
+	}
+	else
+	{
+		siStartInfo.hStdInput=NULL;
+	}
+	
+	GetEnvironmentVariable("ComSpec", shellCmd, _MAX_PATH);
+	TMPDir=getenv("TMPDIR");
+	sprintf(FileTMPDir,"%s\\DOS.OK",TMPDir);
+
+	CmdLine=(char*)MALLOC( (strlen(shellCmd)+strlen(command)+strlen(FileTMPDir)+strlen("%s /a /c %s && echo DOS>%s")+1)*sizeof(char) );
+	sprintf(CmdLine,"%s /a /c %s && echo DOS>%s",shellCmd,command,FileTMPDir);
+	if (IsAFile(FileTMPDir)) DeleteFile(FileTMPDir);
+
+	if (!CreateProcess(NULL, CmdLine, NULL, NULL, TRUE,0, NULL, NULL, &siStartInfo, &piProcInfo))
+	{
+		if (CmdLine) {FREE(CmdLine);CmdLine=NULL;}
+		ReturnStatus=FALSE;
+		return ReturnStatus;
+	}
+	else if (CmdLine) {FREE(CmdLine);CmdLine=NULL;}
+
+	if (GetExitCodeProcess(piProcInfo.hProcess,&ExitCode))
+	{
+		if (ExitCode==0)
+		{
+			ReturnStatus=TRUE;
+		}
+		else
+		{
+			ReturnStatus=FALSE;
+		}
+		CloseHandle(piProcInfo.hProcess);
+	}
+	else
+	{
+		TerminateProcess(piProcInfo.hProcess,0);
+		CloseHandle(piProcInfo.hProcess);
+
+		if (IsAFile(FileTMPDir))
+		{
+			DeleteFile(FileTMPDir);
+			ReturnStatus=TRUE;
+		}
+		else
+		{
+			ReturnStatus=FALSE;
+		}
+	}
+
+	return ReturnStatus;
+}
+#endif
+/*-----------------------------------------------------------------------------------*/
+#if WIN32
+char ** CallWindowsShellWithOuput(char *command,BOOL WaitInput,int *Status,int *NumberOfLines)
+{
+	#define BUFSIZE 1024
 	char shellCmd[_MAX_PATH];
 	char *CmdLine=NULL;
 	HANDLE hChildStdinRd= INVALID_HANDLE_VALUE;
 	HANDLE hChildStdinWr= INVALID_HANDLE_VALUE;
 	HANDLE hChildStdinWrDup= INVALID_HANDLE_VALUE;
-    HANDLE hChildStdoutRd= INVALID_HANDLE_VALUE;
+  HANDLE hChildStdoutRd= INVALID_HANDLE_VALUE;
 	HANDLE hChildStdoutWr= INVALID_HANDLE_VALUE;
 	HANDLE hChildStdoutRdDup= INVALID_HANDLE_VALUE;
-    HANDLE hInputFile= INVALID_HANDLE_VALUE;
+  HANDLE hInputFile= INVALID_HANDLE_VALUE;
 	HANDLE hSaveStdin= INVALID_HANDLE_VALUE;
 	HANDLE hSaveStdout= INVALID_HANDLE_VALUE; 
 
@@ -271,7 +316,9 @@ char ** CallWindowsShell(char *command,BOOL WaitInput,int *Status,int *NumberOfL
 	while (moreOutput) 
 	{
 		BOOL bres = ReadFile( hChildStdoutRdDup, op, BUFSIZE-1, &dwRead, NULL);
+
 		moreOutput = bres || (dwRead != 0);
+
 		if (moreOutput) 
 		{
 			readSoFar += dwRead;
@@ -285,9 +332,11 @@ char ** CallWindowsShell(char *command,BOOL WaitInput,int *Status,int *NumberOfL
 	Output=NULL;
 	Output=(char*)MALLOC((strlen(OEMstring)+1)*sizeof(char));
 	OemToChar(OEMstring,Output);
+	if (OEMstring) {FREE(OEMstring);OEMstring=NULL;}
 
-	CopyOutput=(char*)MALLOC((strlen(OEMstring)+1)*sizeof(char));
+	CopyOutput=(char*)MALLOC((strlen(Output)+1)*sizeof(char));
 	strcpy(CopyOutput,Output);
+
 
 	i=0;
 	for (line=strtok(CopyOutput,"\n");line;line=strtok(NULL,"\n"))
@@ -295,6 +344,7 @@ char ** CallWindowsShell(char *command,BOOL WaitInput,int *Status,int *NumberOfL
 		i++;
 	}
 	*NumberOfLines=i;
+	if (CopyOutput) {FREE(CopyOutput);CopyOutput=NULL;}
 
 	if (*NumberOfLines)
 	{
@@ -307,9 +357,12 @@ char ** CallWindowsShell(char *command,BOOL WaitInput,int *Status,int *NumberOfL
 			if (line[strlen(line)-1] == '\r') line[strlen(line)-1] = 0;
 			OuputStrings[i]=MALLOC((strlen(line)+1)*sizeof(char));
 			sprintf(OuputStrings[i],"%s",line);
+			if (line) {FREE(line);line=NULL;}
 			i++;
 		}
 	}
+
+	if (op) {FREE(op);op=NULL;}
 
 	if (GetExitCodeProcess(piProcInfo.hProcess,&ExitCode))
 	{
@@ -340,6 +393,7 @@ char ** CallWindowsShell(char *command,BOOL WaitInput,int *Status,int *NumberOfL
 	}
 
 	return OuputStrings;
+
 }
 #endif
 /*-----------------------------------------------------------------------------------*/
