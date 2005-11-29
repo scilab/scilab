1bd6f5f832a9b60e6c326005e426e1687027c31a
Allan Cornet <allan.cornet@scilab.org>
2005-11-29 13:03:31 +0000

    Mise a jour gestion clavier (gestion tout type de clavier azerty, qwerty ...)

--- scilab/routines/wsci/KeyEvents.c
+++ scilab/routines/wsci/KeyEvents.c
@@ -11,123 +11,118 @@ extern int check_pointer_win __PARAMS ((int *x1,int *y1,int *win));
 #ifndef VK_SLEEP
   #define VK_SLEEP 0x5F
 #endif
+
+/* Code à la linux */
+#define SCI_VK_CANCEL	65288
+#define SCI_VK_PAUSE	65299
+#define SCI_VK_SLEEP	65300 
+#define SCI_VK_INSERT	65379
+#define SCI_VK_DELETE	65535
+#define SCI_VK_END	65367
+#define SCI_VK_HOME	65360
+#define SCI_VK_PRIOR	65365
+#define SCI_VK_NEXT	65366
+#define SCI_VK_BACK	65289
+#define SCI_VK_NUMLOCK 65407 
+#define SCI_VK_SCROLL 65300 
+#define SCI_VK_SNAPSHOT 0
+#define SCI_VK_TAB 65289
+#define SCI_VK_ESCAPE 65307
+#define SCI_VK_CLEAR 65408
+#define SCI_VK_LEFT 65361
+#define SCI_VK_F1 65470
+
 /*-----------------------------------------------------------------------------------*/
 BOOL IsASpecialKey(UINT vk);
 UINT GetScilabSpecialKeyCode(UINT vk);
+BOOL is_dead_key (int vk); 
 /*-----------------------------------------------------------------------------------*/
 /* http://msdn.microsoft.com/library/default.asp?url=/library/en-us/winui/winui/WindowsUserInterface/UserInput/VirtualKeyCodes.asp */
 /*-----------------------------------------------------------------------------------*/
 void ON_EVENT_GRAPH_WM_KEYUP(HWND hwnd, UINT vk, BOOL fDown, int cRepeat, UINT flags)
 {
-	if ( IsASpecialKey(vk) )
+	if ( is_dead_key (vk) )
 	{
-		struct BCG *ScilabGC = (struct BCG *) GetWindowLong (hwnd, 0);
-		int x,y,iwin;
-
-		check_pointer_win(&x,&y,&iwin);
-		PushClickQueue (ScilabGC->CurWindow, x,y,-(int)GetScilabSpecialKeyCode(vk),0,1);
+		// Nothing to do
 	}
 	else
 	{
-		int CodeKey=0;
-		int RetToAscii=0;
-		BYTE KeyState[256]; // address of key-state array
-		WORD Char; // buffer for translated key
-		UINT VirtualKey=0;
-
-		GetKeyboardState(KeyState);
-		RetToAscii=ToAscii ( vk, 0, KeyState, &Char, 0 );
-		VirtualKey=MapVirtualKey(vk,2);
-		if (RetToAscii == 1)
+		if (IsASpecialKey(vk))
 		{
-			int x,y,iwin;
 			struct BCG *ScilabGC = (struct BCG *) GetWindowLong (hwnd, 0);
-
-			if ( (GetKeyState (VK_CONTROL) < 0) && (GetKeyState (VK_SHIFT) <0) )
-			{
-				CodeKey=VirtualKey+CTRL_KEY;
-			}
-			else
-			{
-				if (GetKeyState (VK_CONTROL) < 0)
-				{
-					CodeKey=tolower((char)VirtualKey)+CTRL_KEY;
-				}
-				else if (GetKeyState (VK_SHIFT) < 0)
-				{
-					CodeKey=VirtualKey;
-				}
-				else
-				{
-					CodeKey= tolower((char)Char);
-				}
-			}
-
+			int x,y,iwin;
 			check_pointer_win(&x,&y,&iwin);
-			PushClickQueue (ScilabGC->CurWindow, x,y,-(int)CodeKey,0,1);
+			PushClickQueue (ScilabGC->CurWindow, x,y,-(int)GetScilabSpecialKeyCode(vk),0,1);
 		}
 		else
 		{
-			/* Not a character */
-			/* A key */
+			WORD Char=0;
+			BYTE KeyState[256]; // Etat des 256 touches du clavier 
+			memset ( KeyState, 0, sizeof(KeyState) ); 
+			if ( GetKeyboardState ( KeyState ) ) 
+			{
+				int RetToAscii=0;
+				int x,y,iwin;
+				struct BCG *ScilabGC = (struct BCG *) GetWindowLong (hwnd, 0);
+				int CodeKey=0;
+
+				RetToAscii=ToAscii ( (UINT) vk,MapVirtualKey(vk, 0), KeyState, &Char, 0 );
+				if (RetToAscii)
+				{
+					if (GetKeyState (VK_CONTROL) < 0)
+					{
+						CodeKey=Char+CTRL_KEY;
+					}
+					else CodeKey=Char;
+
+					check_pointer_win(&x,&y,&iwin);
+					PushClickQueue (ScilabGC->CurWindow, x,y,-(int)CodeKey,0,1);
+				}
+			}
 		}
 	}
-
 }
 /*-----------------------------------------------------------------------------------*/
 void ON_EVENT_GRAPH_WM_KEYDOWN(HWND hwnd, UINT vk, BOOL fDown, int cRepeat, UINT flags)
 {
-	if ( IsASpecialKey(vk) )
+	if ( is_dead_key (vk) )
 	{
-		struct BCG *ScilabGC = (struct BCG *) GetWindowLong (hwnd, 0);
-		int x,y,iwin;
-
-		check_pointer_win(&x,&y,&iwin);
-		PushClickQueue (ScilabGC->CurWindow, x,y,(int)GetScilabSpecialKeyCode(vk),0,0);
+		// Nothing to do
 	}
 	else
 	{
-		int CodeKey=0;
-		int RetToAscii=0;
-		BYTE KeyState[256]; // address of key-state array
-		WORD Char; // buffer for translated key
-		UINT VirtualKey=0;
-
-		GetKeyboardState(KeyState);
-		RetToAscii=ToAscii ( vk, 0, KeyState, &Char, 0 );
-		VirtualKey=MapVirtualKey(vk,2);
-		if (RetToAscii == 1)
+		if (IsASpecialKey(vk))
 		{
-			int x,y,iwin;
 			struct BCG *ScilabGC = (struct BCG *) GetWindowLong (hwnd, 0);
-
-			if ( (GetKeyState (VK_CONTROL) < 0) && (GetKeyState (VK_SHIFT) <0) )
-			{
-				CodeKey=VirtualKey+CTRL_KEY;
-			}
-			else
-			{
-				if (GetKeyState (VK_CONTROL) < 0)
-				{
-					CodeKey=tolower((char)VirtualKey)+CTRL_KEY;
-				}
-				else if (GetKeyState (VK_SHIFT) < 0)
-				{
-					CodeKey=VirtualKey;
-				}
-				else
-				{
-					CodeKey= tolower((char)Char);
-				}
-			}
-
+			int x,y,iwin;
 			check_pointer_win(&x,&y,&iwin);
-			PushClickQueue (ScilabGC->CurWindow, x,y,(int)CodeKey,0,0);
+			PushClickQueue (ScilabGC->CurWindow, x,y,(int)GetScilabSpecialKeyCode(vk),0,0);
 		}
 		else
 		{
-			/* Not a character */
-			/* A key */
+			WORD Char=0;
+			BYTE KeyState[256]; // Etat des 256 touches du clavier 
+			memset ( KeyState, 0, sizeof(KeyState) ); 
+			if ( GetKeyboardState ( KeyState ) ) 
+			{
+				int RetToAscii=0;
+				int x,y,iwin;
+				struct BCG *ScilabGC = (struct BCG *) GetWindowLong (hwnd, 0);
+				int CodeKey=0;
+
+				RetToAscii=ToAscii ( (UINT) vk,MapVirtualKey(vk, 0), KeyState, &Char, 0 );
+				if (RetToAscii)
+				{
+					if (GetKeyState (VK_CONTROL) < 0)
+					{
+						CodeKey=Char+CTRL_KEY;
+					}
+					else CodeKey=Char;
+
+					check_pointer_win(&x,&y,&iwin);
+					PushClickQueue (ScilabGC->CurWindow, x,y,(int)CodeKey,0,0);
+				}
+			}
 		}
 	}
 }
@@ -138,17 +133,20 @@ BOOL IsASpecialKey(UINT vk)
 
 	if (vk == VK_CANCEL) return TRUE;
 	if (vk == VK_PAUSE) return TRUE;
-	if (vk == VK_SNAPSHOT) return TRUE;
 	if (vk == VK_SLEEP) return TRUE;
 	if (vk == VK_INSERT) return TRUE;
+	if (vk == VK_DELETE) return TRUE;
 	if (vk == VK_END) return TRUE;
 	if (vk == VK_HOME) return TRUE;
 	if (vk == VK_PRIOR) return TRUE;
 	if (vk == VK_NEXT) return TRUE;
 	if (vk == VK_BACK) return TRUE;
 	if (vk == VK_NUMLOCK) return TRUE;
+	if (vk == VK_SCROLL) return TRUE;
 	if (vk == VK_TAB) return TRUE;
-	if (vk ==  VK_ESCAPE ) return TRUE;
+	if (vk == VK_ESCAPE ) return TRUE;
+	if (vk == VK_SNAPSHOT ) return TRUE;
+	if (vk == VK_CLEAR ) return TRUE;
 	if ( (vk>=VK_LEFT) && (vk<=VK_DOWN) ) return TRUE;
 	if ( (vk>=VK_F1) && (vk<=VK_F24) ) return TRUE;
 
@@ -159,27 +157,33 @@ UINT GetScilabSpecialKeyCode(UINT vk)
 {
 	UINT ScilabSpecialKeyCode=0;
 
-	if (vk == VK_CANCEL)  ScilabSpecialKeyCode=65288;
-	if (vk == VK_PAUSE)  ScilabSpecialKeyCode=65299;
-	if (vk == VK_SNAPSHOT)  ScilabSpecialKeyCode=0;
-	if (vk == VK_SLEEP)  ScilabSpecialKeyCode=65300; 
-
-	if (vk == VK_INSERT) ScilabSpecialKeyCode=65379;
-
-	if (vk == VK_END)  ScilabSpecialKeyCode=65367;
-	if (vk == VK_HOME)  ScilabSpecialKeyCode=65360;
-	if (vk == VK_PRIOR)  ScilabSpecialKeyCode=65365;
-	if (vk == VK_NEXT)  ScilabSpecialKeyCode=65366;
-	if (vk == VK_BACK)  ScilabSpecialKeyCode=65289;
-
-	if (vk == VK_NUMLOCK)  ScilabSpecialKeyCode=65407; 
-
-	if (vk == VK_TAB)   ScilabSpecialKeyCode=65289;
-	if (vk ==  VK_ESCAPE ) ScilabSpecialKeyCode=65307;
-
-	if ( (vk>=VK_LEFT) && (vk<=VK_DOWN) )  ScilabSpecialKeyCode= (65361-VK_LEFT)+vk;
-	if ( (vk>=VK_F1) && (vk<=VK_F24) )  ScilabSpecialKeyCode= (65470-VK_F1)+vk;
+	if (vk == VK_CANCEL)  ScilabSpecialKeyCode=SCI_VK_CANCEL;
+	if (vk == VK_PAUSE)  ScilabSpecialKeyCode=SCI_VK_PAUSE;
+	if (vk == VK_SLEEP)  ScilabSpecialKeyCode=SCI_VK_SLEEP; 
+	if (vk == VK_INSERT) ScilabSpecialKeyCode=SCI_VK_INSERT;
+	if (vk == VK_DELETE) ScilabSpecialKeyCode=SCI_VK_DELETE;
+	if (vk == VK_END)  ScilabSpecialKeyCode=SCI_VK_END;
+	if (vk == VK_HOME)  ScilabSpecialKeyCode=SCI_VK_HOME;
+	if (vk == VK_PRIOR)  ScilabSpecialKeyCode=SCI_VK_PRIOR;
+	if (vk == VK_NEXT)  ScilabSpecialKeyCode=SCI_VK_NEXT;
+	if (vk == VK_BACK)  ScilabSpecialKeyCode=SCI_VK_BACK;
+	if (vk == VK_NUMLOCK)  ScilabSpecialKeyCode=SCI_VK_NUMLOCK; 
+	if (vk == VK_SCROLL)  ScilabSpecialKeyCode=SCI_VK_SCROLL; 
+	if (vk == VK_SNAPSHOT ) ScilabSpecialKeyCode=SCI_VK_SNAPSHOT;
+	if (vk == VK_TAB)   ScilabSpecialKeyCode=SCI_VK_TAB;
+	if (vk ==  VK_ESCAPE ) ScilabSpecialKeyCode=SCI_VK_ESCAPE;
+  if (vk == VK_CLEAR ) ScilabSpecialKeyCode=SCI_VK_CLEAR;
+	if ( (vk>=VK_LEFT) && (vk<=VK_DOWN) )  ScilabSpecialKeyCode= (SCI_VK_LEFT-VK_LEFT)+vk;
+	if ( (vk>=VK_F1) && (vk<=VK_F24) )  ScilabSpecialKeyCode= (SCI_VK_F1-VK_F1)+vk;
 
 	return ScilabSpecialKeyCode;
 }
+/*-----------------------------------------------------------------------------------*/
+BOOL is_dead_key (int vk) 
+{ 
+	unsigned int code = MapVirtualKey ( vk, 2 ); 
+
+	// Windows 95 retourne 0x8000, NT retourne 0x80000000 
+	return (code & 0x80008000) ? TRUE : FALSE; 
+} 
 /*-----------------------------------------------------------------------------------*/
