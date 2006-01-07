a4af5d9eed3b0d8967a1fc198b219d8c12aefccf
Jean-philippe Chancelier <jpc@cermics.enpc.fr>
2006-01-07 22:12:53 +0000

    But on gtk2,  Str in not properly dimensionned

--- scilab/routines/system/storeversion.c
+++ scilab/routines/system/storeversion.c
@@ -21,26 +21,24 @@ int C2F(intversion)(char * fname,unsigned long fname_len)
   CreateVarFromPtr( 1, "c",(m1=strlen(Version), &m1),&n1,&v);
   LhsVar(1) = 1;
   if (Lhs==2) {
-    static char *Str[8];
+    static char *Str[12];
     char tk[]="tk";
     char pvm[]="pvm";
     char gtk[]="gtk";
     char ocaml[]="ocaml";
-	char javasci[]="javasci";
-	char atlas[]="atlas";
-	char debugmode[]="debug";
-	char releasemode[]="release";
+    char javasci[]="javasci";
+    char atlas[]="atlas";
+    char debugmode[]="debug";
+    char releasemode[]="release";
+    
+#ifdef __TIME__
+    char TimeBuild[]=__TIME__;
+#endif
 
-	#ifdef __TIME__
-	char TimeBuild[]=__TIME__;
-	#endif
-
-	#ifdef __DATE__
-	char DateBuild[]=__DATE__;
-	#endif
-
+#ifdef __DATE__
+    char DateBuild[]=__DATE__;
+#endif
     static int irep,nbuf;
-
     n1=0;
     C2F(getcomp)( C2F(cha1).buf,&nbuf,128);
     Str[n1]=C2F(cha1).buf;n1++;
@@ -54,33 +52,31 @@ int C2F(intversion)(char * fname,unsigned long fname_len)
     if (irep) {Str[n1]=ocaml;n1++;}
 	C2F(withjavasci)(&irep);
     if (irep) {Str[n1]=javasci;n1++;}
+#ifdef WITH_ATLAS
+    {Str[n1]=atlas;n1++;}
+#endif
 
-	#ifdef WITH_ATLAS
-		{Str[n1]=atlas;n1++;}
-	#endif
-
-	#ifdef WIN32
-		#if _DEBUG
-			{Str[n1]=debugmode;n1++;}
-		#else
-			{Str[n1]=releasemode;n1++;}
-		#endif
-	#else
-		#ifdef NDEBUG
-			{Str[n1]=releasemode;n1++;}
-		#else
-			{Str[n1]=debugmode;n1++;}
-		#endif
-	#endif
-
-	#ifdef __TIME__
-		{Str[n1]=DateBuild;n1++;}
-	#endif
-
-	#ifdef __DATE__
-		{Str[n1]=TimeBuild;n1++;}
-	#endif
-
+#ifdef WIN32
+#if _DEBUG
+    {Str[n1]=debugmode;n1++;}
+#else
+    {Str[n1]=releasemode;n1++;}
+#endif
+#else
+#ifdef NDEBUG
+    {Str[n1]=releasemode;n1++;}
+#else
+    {Str[n1]=debugmode;n1++;}
+#endif
+#endif
+    
+#ifdef __TIME__
+    {Str[n1]=DateBuild;n1++;}
+#endif
+    
+#ifdef __DATE__
+    {Str[n1]=TimeBuild;n1++;}
+#endif
     m1=1;
     CreateVarFromPtr( 2, "S", &m1, &n1, Str);
     LhsVar(2) = 2;
