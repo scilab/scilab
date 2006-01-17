47d8917c4821fde7df469473c7c55a828035ef1b
Allan Cornet <allan.cornet@scilab.org>
2006-01-17 15:15:32 +0000

    initialisation variables(bug on windows debug mode)

--- scilab/routines/fileio/fileio.c
+++ scilab/routines/fileio/fileio.c
@@ -555,8 +555,9 @@ int int_objfscanf(char *fname,unsigned long fname_len)
 {
   static int l1, m1, n1,l2,m2,n2,iarg,maxrow,nrow,rowcount,ncol;
   FILE  *f;
-  int args,retval,retval_s,err;
-  entry *data;
+  int args,retval,err;
+	int retval_s=0;
+  entry *data=NULL;
   long int pos;
 
   rec_entry buf[MAXSCAN];
@@ -565,16 +566,19 @@ int int_objfscanf(char *fname,unsigned long fname_len)
   Nbvars = 0;
   CheckRhs(2,3);
 
-  if (Rhs==3) {
+  if (Rhs==3) 
+	{
     GetRhsVar(1,"i",&m1,&n1,&l1);
-    if (m1*n1 != 1 ) {
+    if (m1*n1 != 1 ) 
+		{
       Scierror(999,"Error: in fscanf: incorrect first argument\r\n");
       return 0;
     }
     iarg=2;
     maxrow=*istk(l1);
   }
-  else {
+  else
+	{
     iarg=1;
     maxrow=1;
   }
@@ -584,41 +588,48 @@ int int_objfscanf(char *fname,unsigned long fname_len)
 
   StringConvert(cstk(l2));  /* conversion */
   if ((f= GetFile(istk(l1))) == (FILE *)0)
-    {
-      Scierror(999,"fprintf:\t wrong file descriptor %d\r\n",*istk(l1));
-      return 0;
-    }
+  {
+    Scierror(999,"fprintf:\t wrong file descriptor %d\r\n",*istk(l1));
+    return 0;
+  }
+
   nrow=maxrow; 
   rowcount = -1;
-  while (1) {
+
+  while (1) 
+	{
     rowcount++;
     if ((maxrow >= 0) && (rowcount >= maxrow)) break;
     args = Rhs; /* args set to Rhs on entry */
     pos=ftell(f);
     if ( do_scanf("fscanf",f,cstk(l2),&args,(char *)0,&retval,buf,type) < 0 )  return 0;
-    if ( retval == EOF) {
+    if ( retval == EOF) 
+		{
       /* 
       Scierror(999,"Error: in %s: end of file reached\r\n",fname);
 
       */
       break;
     }
-    if ((err=Store_Scan(&nrow,&ncol,type_s,type,&retval,&retval_s,
-			buf,&data,rowcount,args)) <0 ) {
-      switch (err) {
-      case MISMATCH:
-	if (maxrow>=0) {
-	  Free_Scan(rowcount,ncol,type_s,&data);
-	  Scierror(999,"Error: in fscanf: data mismatch\r\n");
-	  return 0;
-	}
-	fseek(f,pos,SEEK_SET);
-	break;
-      case MEM_LACK:
-	Free_Scan(rowcount,ncol,type_s,&data);
-	Scierror(999,"Error: in fscanf: cannot allocate more memory \r\n");
-	return 0;
-	break;
+    if ((err=Store_Scan(&nrow,&ncol,type_s,type,&retval,&retval_s,buf,&data,rowcount,args)) <0 )
+		{
+      switch (err) 
+			{
+				case MISMATCH:
+					if (maxrow>=0) 
+					{
+						Free_Scan(rowcount,ncol,type_s,&data);
+						Scierror(999,"Error: in fscanf: data mismatch\r\n");
+						return 0;
+					}
+					fseek(f,pos,SEEK_SET);
+				break;
+
+				case MEM_LACK:
+					Free_Scan(rowcount,ncol,type_s,&data);
+					Scierror(999,"Error: in fscanf: cannot allocate more memory \r\n");
+					return 0;
+				break;
       }
       if (err==MISMATCH) break;
     }
