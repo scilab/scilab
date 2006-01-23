74135c3d3cd5e609a8b2dba427b03b3e9a1d9762
Allan Cornet <allan.cornet@scilab.org>
2006-01-23 15:45:37 +0000

    Add a warning if version not supported by the operational team of Scilab Consortium.

--- scilab/routines/system/banier.c
+++ scilab/routines/system/banier.c
@@ -1,14 +1,17 @@
+/*-----------------------------------------------------------------------------------*/
+/* INRIA */
+/*-----------------------------------------------------------------------------------*/
 #include "../machine.h"
 #include "../version.h"
-
+/*-----------------------------------------------------------------------------------*/
 extern void sciprint __PARAMS((char *fmt,...));
-
+/*-----------------------------------------------------------------------------------*/
 extern int sci_show_banner ; 
-
+/*-----------------------------------------------------------------------------------*/
 void banner(void)
 {
 	#ifdef WIN32
-	sciprint("        ___________________________________________\r\n");
+	  sciprint("        ___________________________________________\r\n");
 	#else
     sciprint("        -------------------------------------------\r\n");
 	#endif
@@ -17,20 +20,35 @@ void banner(void)
     sciprint("                  Copyright (c) 1989-2006          \r\n");
     sciprint("              Consortium Scilab (INRIA, ENPC)      \r\n");
 
-    #ifdef WIN32
-	sciprint("        ___________________________________________\r\n");
+  #ifdef WIN32
+	  sciprint("        ___________________________________________\r\n");
 	#else
     sciprint("        -------------------------------------------\r\n");
 	#endif
-}
 
+	#if ( defined(WITH_GTK) || defined(__CYGWIN32__) || defined(__MINGW32__) )
+		sciprint("\r\n\r\nWarning :No Scilab GTK version is provided and supported by the operational\r\nteam of Scilab Consortium.\r\n");
+		sciprint("Same way about MinGW and Cygwin.\r\n\r\n");
+	#endif
+
+	#if defined(__MSC__) && ( (_MSC_VER >= 1200) && (_MSC_VER < 1300) ) /* Visual Studio C++ 6.0 */
+		sciprint("\r\n\r\nWarning : No Scilab version built with Microsoft Visual C++ 6.0 is provided\r\nand supported by the operational team of Scilab Consortium\r\n\r\n");
+	#endif
+
+	#if defined(__APPLE__)
+		sciprint("\r\n\r\nWarning : No Scilab Apple version is provided and supported by the operational\r\nteam of Scilab Consortium.\r\n\r\n");
+	#endif
+
+}
+/*-----------------------------------------------------------------------------------*/
 int C2F(banier)(integer *flag)
 {
   if (*flag != 999 && sci_show_banner == 1)
   {
-	banner();
+		banner();
   }
   return 0;
 } 
-  
+/*-----------------------------------------------------------------------------------*/
+
 
