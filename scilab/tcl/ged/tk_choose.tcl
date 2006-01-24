11946783d02bd906e1b1c120e4fb2f34983a30d6
Stephane Mottelet <stephane.mottelet@utc.fr>
2006-01-24 12:32:46 +0000

    Improved look/more clever+readable source of tk_choose, using the new feature of TCL_SetVar, which now stores Scilab matrices as Tcl arrays.
    S.

--- scilab/tcl/ged/tk_choose.tcl
+++ scilab/tcl/ged/tk_choose.tcl
@@ -1,69 +1,68 @@
-global tkc_comment tkc_thelist text_button
+namespace eval tkChoose {
 
-set tk_choose_ww .tk_choose_menu
-catch {destroy $tk_choose_ww}
-toplevel $tk_choose_ww
-wm title $tk_choose_ww "Tk_Choose"
-wm iconname $tk_choose_ww ""
-wm protocol $tk_choose_ww WM_DELETE_WINDOW "Return_zero"
-wm geometry $tk_choose_ww +100+100
+    # Author : S. Mottelet, Tue Jan 24 12:01:25 CET 2006
+    # Use of a namespace makes things easier (no global variables to pass
+    # arguments from the Scilab level).
 
-#remove some special caracters for tkc_comment
-#character string treatment to remove "[" and "]" and replace "," and ";" by " "
-set tkc_comment [string trimright $tkc_comment \] ]
-set tkc_comment [string trimleft $tkc_comment \[ ]
-regsub -all \" $tkc_comment "\n" tkc_comment
-regsub -all "," $tkc_comment " " tkc_comment
-regsub -all ";" $tkc_comment " " tkc_comment
+    set relief raised
+    set t .tkChoose
+    set fontSize 12
 
-#remove some special caracters for text_button
-#character string treatment to remove "[" and "]" and replace "," and ";" by " "
-set text_button [string trimright $text_button \] ]
-set text_button [string trimleft $text_button \[ ]
-regsub -all \" $text_button "\n" text_button
-regsub -all "," $text_button " " text_button
-regsub -all ";" $text_button " " text_button
+    catch {font create chooseFont -family Helvetica -size $fontSize}
+    catch {font create chooseBoldFont -family Helvetica -size $fontSize -weight bold}
+    
+    toplevel $t
+    bind $t <Destroy> {
+        set tkChoose::result -1
+    }
+   
+    # Convert (if necessary) the array of strings to a \n
+    # separated string
 
-# character string treatment to remove "[" and "]" and replace "," and ";" by " "
-set tkc_thelist [string trimright $tkc_thelist \] ]
-set tkc_thelist [string trimleft $tkc_thelist \[ ]
+    if ![set length [array size title]] {
+        set titleString $title
+    } else {
+        for {set k 1} {$k<$length} {incr k} {
+            append titleString $title($k,1)\n
+        }
+        append titleString $title($k,1)
+    }
 
-regsub -all "," $tkc_thelist " " tkc_thelist
-regsub -all ";" $tkc_thelist " " tkc_thelist
-set tkc_thelist [list $tkc_thelist]
+    label $t.label -text $titleString -font chooseBoldFont
+    
+    listbox $t.list -bg white -height 0 -width 0 -font chooseFont
+    bind  $t.list  <ButtonRelease> {
+      bind $tkChoose::t <Destroy> {}
+      catch {set tkChoose::result [$tkChoose::t.list nearest %y]}
+    }
 
-frame .tk_choose_menu.main_frame
-set tk_choose_w .tk_choose_menu.main_frame
-pack $tk_choose_w -in  $tk_choose_ww  -side left -expand yes
+    # Insert the string or the array strings in the listbox
 
-label $tk_choose_w.label  -height 0 -text $tkc_comment -width 0 
-pack $tk_choose_w.label -in $tk_choose_w -side top
-set lbf [listbox $tk_choose_w.mylist  -height 0  -width 0]
+    if ![set length [array size items]] {
+        $t.list insert end $items    
+    } else {
+        for {set k 1} {$k<=$length} {incr k} {
+            $t.list insert end $items($k,1)
+        }
+    }
 
-#if one choice only, different treatment
-if { [catch {set titi [expr $tkc_thelist]}] } {
-    eval [list $lbf insert end] $tkc_thelist
-} else {
-    eval [list $lbf insert end] $titi
-}
+    button $t.b -text $button -font chooseFont -relief $relief -command {
+        destroy $tkChoose::t
+    }
 
-$tk_choose_w.mylist activate 0
-bind  $tk_choose_w.mylist  <<ListboxSelect>> {
-    Tk_Choose_Select [$tk_choose_w.mylist curselection]
-}
-pack $tk_choose_w.mylist -in $tk_choose_w -side top -fill x
+    # Adjust padding space when there is no title
 
-button $tk_choose_w.cancel_button -text $text_button -command "Return_zero; destroy $tk_choose_ww" -font {Arial 9}
-pack  $tk_choose_w.cancel_button -in $tk_choose_w -side top
+    set listPady {0 0}
+    if {$titleString!=""} {
+        pack $t.label -pady 10 -padx 10
+    } else {
+        set listPady {10 0}
+    }
 
-
-proc Tk_Choose_Select {sel} {
-#    set index [$sel curselection]
-    incr sel
-    ScilabEval "global tk_choose_num; tk_choose_num = $sel" "seq"
-}
-
-
-proc Return_zero { } {
-    ScilabEval "global tk_choose_num; tk_choose_num = 0"
+    pack $t.list  -pady $listPady -padx 10
+    pack $t.b -pady 10
+    wm resizable $t 0 0
+    
+    # The event loop occurs at the Scilab level 
+    # see SCI/macros/xdess/tk_choose.sci
 }
