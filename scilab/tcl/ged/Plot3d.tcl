241abc67d233214eee5d58099d1c28c754b66882
Fabrice Leray <fabriceyarel@hotmail.com>
2006-01-24 13:35:02 +0000

    hiddencolor can be negative (see manual)

--- scilab/tcl/ged/Plot3d.tcl
+++ scilab/tcl/ged/Plot3d.tcl
@@ -243,7 +243,7 @@ frame $w.frame.clrh  -borderwidth 0
 pack $w.frame.clrh  -in $w.frame -side top  -fill x
 
 label $w.frame.colorlabelh -height 0 -text "Hidden color:" -width 0  -font $gedFont -anchor e -width $largeur
-scale $w.frame.colorh -orient horizontal -from 0 -to $ncolors \
+scale $w.frame.colorh -orient horizontal -from -1 -to $ncolors \
 	 -resolution 1.0 -command "setHiddenColor $w.frame.colorh" -tickinterval 0  -font $gedFont
 
 pack $w.frame.colorlabelh -in $w.frame.clrh -side left
@@ -655,8 +655,20 @@ proc setHiddenColor {w index} {
     variable BLUCOL
     
     #ScilabEval "global ged_handle;"
-   
-    if { $index == 0 } {
+    if { $index < 0 } {
+	ScilabEval "global ged_handle; if ged_handle.hiddencolor <> $index then ged_handle.hiddencolor=$index; end;"
+	
+	set index [expr -$index]
+	
+	set REDCOL $RED($index)
+	set GRECOL $GREEN($index)
+	set BLUCOL $BLUE($index)
+	
+	set color [format \#%02x%02x%02x [expr int($REDCOL*255)]  [expr int($GRECOL*255)]  [expr int($BLUCOL*255)]]
+	
+	$w config  -activebackground $color -troughcolor $color
+	
+    } elseif { $index == 0 } {
 	ScilabEval "global ged_handle; if ged_handle.hiddencolor <> $index then ged_handle.hiddencolor=$index; end;"
 	#like $index==1: display first color
 	set color [format \#%02x%02x%02x 255 255 255]
@@ -664,9 +676,9 @@ proc setHiddenColor {w index} {
     } else { 
 	ScilabEval "global ged_handle; if ged_handle.hiddencolor <> $index then ged_handle.hiddencolor=$index; end;"
 	
-	set REDCOL $RED($index) 
-	set GRECOL $GREEN($index) 
-	set BLUCOL $BLUE($index) 
+	set REDCOL $RED($index)
+	set GRECOL $GREEN($index)
+	set BLUCOL $BLUE($index)
 	
 	set color [format \#%02x%02x%02x [expr int($REDCOL*255)]  [expr int($GRECOL*255)]  [expr int($BLUCOL*255)]]
 	
