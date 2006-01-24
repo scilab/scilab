c7ef668c1d4cecd1df411aea29a5f2b55d620717
Jean-baptiste Silvy <jean-baptiste.silvy@scilab.org>
2006-01-24 14:50:50 +0000

    Add a function to know if ged is opened or not.

--- scilab/routines/tclsci/GedManagement.c
+++ scilab/routines/tclsci/GedManagement.c
@@ -11,18 +11,52 @@
 
 int sciDestroyGed( void )
 {
-  if ( TCLinterp == NULL )
+  Tcl_Interp * gedInterp = getGedInterpreter() ;
+  if ( isGedAlive() )
   {
-    Scierror( 999, TCL_ERROR13, "sciDestroyGed" ) ;
-    return 1 ;
+    /* try to close the ticks editor */
+    Tcl_Eval( gedInterp, "catch { destroy .ticks }" ) ;
+    /* try to close the editor */
+    Tcl_Eval( gedInterp, "catch { destroy .axes }"  ) ;
+    return 0 ;
   }
   else
   {
-    /* try to close the ticks editor */
-    Tcl_Eval( TCLinterp, "catch { destroy .ticks }" ) ;
-    /* try to close the editor */
-    Tcl_Eval( TCLinterp, "catch { destroy .axes }"  ) ;
-    return 0 ;
+    return 1 ;
   }
 }
-/*------------------------------------------------------------------------------------------*/
+
+/*-----------------------------------------------------------------------------------------*/
+
+int isGedAlive( void )
+{
+  Tcl_Interp * gedInterp = getGedInterpreter() ;
+  if ( gedInterp != NULL )
+  {
+    /* check is sciGedIsAlive variable exists */
+    if ( Tcl_GetVar( gedInterp, "sciGedIsAlive", TCL_GLOBAL_ONLY ) )
+    {
+      return TRUE ;
+    }
+    else
+    {
+      return FALSE ;
+    }
+  }
+  else
+  {
+    /* tcl has not been initialized yet */
+    return FALSE ;
+  }
+}
+
+/*-----------------------------------------------------------------------------------------*/
+
+/*-----------------------------------------------------------------------------------------*/
+
+Tcl_Interp * getGedInterpreter( void )
+{
+  return TCLinterp ;
+}
+
+/*-----------------------------------------------------------------------------------------*/
