fb085645e12887f364279893f8fcadd4f942cec6
Francois Delebecque <Francois.Delebecque@inria.fr>
2006-01-25 16:50:36 +0000

    short ex of ND array

---
+++ scilab/examples/interface-tour-so/ex19intc.c
@@ -0,0 +1,72 @@
+#include  "stack-c.h"
+
+/*   
+Example of ND Array in Scilab  
+FD for Mengzhen (Jan-2006)
+usage: w=ex16c()
+returns a 2x2x2 array filled with entries taken from values.
+
+*/
+#define DIM 32
+#define VAL  2+(3+header[4])-1
+#define HEAD  ((3+header[4]) + (3+header[4]) -1) -1
+#define SIZE_DIMS  4 + round( (ndim-3)/2)
+#define SIZE_VALUES  2 + prod
+
+/*********************************************************
+  ndim = # of dimensions
+  dims = int array of dimensions, dimension ndim
+  prod = product of dimensions = dims[0]*dims[1]*dims[2] (int)
+  values = double array of values, dimension prod
+ *********************************************************/ 
+ 
+int intex16c(fname)
+     char* fname;
+{ 
+#define  FIRST_DIM  2
+#define  SECOND_DIM 2
+#define  THIRD_DIM  2
+#define  TOTAL_ELEMENTS (FIRST_DIM * SECOND_DIM * THIRD_DIM)
+  static  int   ndim = 3, dims[3] = {FIRST_DIM, SECOND_DIM, THIRD_DIM};
+  static double values[8] = {1,2,3,4,5,6,7,8};
+
+
+  int *header;  double *data;
+  int i,prod;
+
+  static int template[32]={
+  17,3,1,12,0,0,10,1,3,0,1,3,7,14,17,22,13,18,22,28,14,23,29,27,18,14,28,
+  23,8,1,3,4};
+
+  CheckRhs(0,1); CheckLhs(1,1);
+
+  prod=TOTAL_ELEMENTS;
+  CreateData(1, (33+ndim)*sizeof(int) + (2+prod)*sizeof(double));
+  header = GetData(1); data = (double *) &header[0];  
+  for ( i = 0 ; i < 32 ; i++ ) {
+    header[i]=template[i];
+   }
+  header[4]=header[3] + SIZE_DIMS;
+  header[5]=header[4] + SIZE_VALUES;
+  /*   entries = real (prod x 1) vector */ 
+  header[HEAD+0]=1;
+  header[HEAD+1]=prod;
+  header[HEAD+2]=1;
+  header[HEAD+3]=0;
+  /*    fill with dimensions  */
+  header[DIM+0]=  FIRST_DIM; 
+  header[DIM+1]=  SECOND_DIM; 
+  header[DIM+2]=  THIRD_DIM;  
+    /*   fill with values  */
+    data[VAL+0] =  values[0];
+    data[VAL+1] =  values[1];
+    data[VAL+2] =  values[2];
+    data[VAL+3] =  values[3];
+    data[VAL+4] =  values[4];
+    data[VAL+5] =  values[5];
+    data[VAL+6] =  values[6];
+    data[VAL+7] =  values[7];
+  LhsVar(1)=1;
+  return 0;
+}
+
