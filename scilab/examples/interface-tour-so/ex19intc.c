c09e075df023f567ca3b9caa8f2d5bb95e0170ed
Francois Delebecque <Francois.Delebecque@inria.fr>
2006-02-03 13:43:40 +0000

    fix

--- scilab/examples/interface-tour-so/ex19intc.c
+++ scilab/examples/interface-tour-so/ex19intc.c
@@ -1,8 +1,8 @@
 #include  "stack-c.h"
 
 /*   
-Example of ND Array in Scilab  
-FD for Mengzhen (Jan-2006)
+Example of ND Array in Scilab (here 3 dimensions).  
+FD   for Mengzhen (Jan-2006) and digiplantes...
 usage: w=ex19c()
 returns a 2x2x2 array filled with entries taken from values.
 
@@ -36,21 +36,23 @@ int intex19c(fname)
 
   static int template[32]={
   17,3,1,12,0,0,10,1,3,0,1,3,7,14,17,22,13,18,22,28,14,23,29,27,18,14,28,
-  23,8,1,3,4};
+  23,8,1,0,4};
 
   CheckRhs(0,1); CheckLhs(1,1);
   /*   1: set ndim and prod here:   */
   ndim=3;
-  prod=TOTAL_ELEMENTS;
+  prod=TOTAL_ELEMENTS;     
   /************************************/
 
-  CreateData(1, (33+ndim)*sizeof(int) + (2+prod)*sizeof(double));
+  CreateData(1, (33+ndim)*sizeof(int) + (2+prod)*sizeof(double));   /*  don't change! */
   header = GetData(1); data = (double *) &header[0];  
   for ( i = 0 ; i < DIM ; i++ )
     header[i]=template[i];
   
   header[4]=header[3] + SIZE_DIMS;
   header[5]=header[4] + SIZE_VALUES;
+  header[30]=ndim;
+
   /*   entries = real (prod x 1) vector */ 
   header[HEAD+0]=1;
   header[HEAD+1]=prod;
@@ -60,7 +62,7 @@ int intex19c(fname)
   /*   2: fill with dimensions  */
   header[DIM+0]=  FIRST_DIM; 
   header[DIM+1]=  SECOND_DIM; 
-  header[DIM+2]=  THIRD_DIM;  
+  header[DIM+2]=  THIRD_DIM;   /*  etc ...*/ 
  /************************************/
     /*  3: fill with values  */
    for ( i = 0 ; i < prod ; i++ )
