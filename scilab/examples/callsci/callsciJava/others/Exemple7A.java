3b9361d7366a1943e5bc2d12c5ebe99eebf9b0c6
Allan Cornet <allan.cornet@scilab.org>
2006-03-01 13:25:48 +0000

    add a example implements java.io.Serializable

---
+++ scilab/examples/callsci/callsciJava/others/Exemple7A.java
@@ -0,0 +1,43 @@
+/* Allan CORNET INRIA 2006 */
+import java.io.*;
+import javasci.*; 
+
+public class  Exemple7A {
+
+  public static void main(String[] args)
+  {
+  	int col=4;
+ 	  int row=2;
+ 	  int r,c;
+ 	  double [] b=new double[row*col];
+ 	  
+ 	  SciDoubleArray a = new SciDoubleArray("A",row,col);
+ 	  Scilab.Exec("A=[1,2,3,4;5 6 7 8];");
+ 		a.Get();
+ 		b=a.getData();
+ 		
+ 		System.out.println("Exemple 7 A Serialize");
+ 		System.out.println("----------------------------------------------------");
+  	for(r=0;r<=(a.getRow()*a.getCol())-1;r++)
+    {
+       System.out.print(""+b[r]+"  ");
+    }
+    System.out.println("");
+  	System.out.println("----------------------------------------------------");
+  	
+ 		try 
+    {
+     	FileOutputStream fichier = new FileOutputStream("Exemple7.ser");
+     	ObjectOutputStream oos = new ObjectOutputStream(fichier);
+     	oos.writeObject(a);
+     	oos.flush();
+     	oos.close();
+   	}
+    	
+    catch (java.io.IOException e) 
+    {
+      e.printStackTrace();
+    }
+    
+  }
+}
---
+++ scilab/examples/callsci/callsciJava/others/Exemple7B.java
@@ -0,0 +1,45 @@
+/* Allan CORNET INRIA 2006 */
+import java.io.*;
+import javasci.*; 
+
+class  Exemple7B {
+
+  public static void main(String[] args)
+  {
+  	  try 
+    {
+      FileInputStream fichier = new FileInputStream("Exemple7.ser");
+      ObjectInputStream ois = new ObjectInputStream(fichier);
+      
+      int col=4;
+  	  int row=2;
+  	  int r,c;
+  	  double [] b=new double[row*col];
+      
+      SciDoubleArray a = (SciDoubleArray) ois.readObject();
+      a.Send(); /* a MUST be send to Scilab just after ois.readObject*/
+      
+      b=a.getData();
+      
+      System.out.println("Exemple 7 B DeSerialize");
+      System.out.println("----------------------------------------------------");
+  		for(r=0;r<=(a.getRow()*a.getCol())-1;r++)
+      {
+         System.out.print(""+b[r]+"  ");
+      }
+      System.out.println("");
+  	  System.out.println("----------------------------------------------------");
+  	  
+    } 
+    
+    catch (java.io.IOException e) 
+    {
+      e.printStackTrace();
+    }
+    catch (ClassNotFoundException e) 
+    {
+      e.printStackTrace();
+    }
+  }
+}
+  	
