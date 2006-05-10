448fe96644710d656fce3703982e0d54cb79ea71
Allan Cornet <allan.cornet@scilab.org>
2006-05-10 09:14:52 +0000

    update Intel Compilers 9.1

--- scilab/libs/LibScilab/DllmainLibscilab.c
+++ scilab/libs/LibScilab/DllmainLibscilab.c
@@ -4,6 +4,12 @@
 /*-----------------------------------------------------------------------------------*/ 
 #include <windows.h> 
 /*-----------------------------------------------------------------------------------*/ 
+#pragma comment(lib,"C:\\PROGRA~1\\Intel\\Compiler\\Fortran\\9.1\\IA32\\Lib\\ifconsol.lib")
+#pragma comment(lib,"C:\\PROGRA~1\\Intel\\Compiler\\Fortran\\9.1\\IA32\\Lib\\libifcore.lib")
+#pragma comment(lib,"C:\\PROGRA~1\\Intel\\Compiler\\Fortran\\9.1\\IA32\\Lib\\libifport.lib")
+#pragma comment(lib,"C:\\PROGRA~1\\Intel\\Compiler\\Fortran\\9.1\\IA32\\Lib\\libm.lib")
+#pragma comment(lib,"C:\\PROGRA~1\\Intel\\Compiler\\Fortran\\9.1\\IA32\\Lib\\libirc.lib")
+/*-----------------------------------------------------------------------------------*/ 
 int WINAPI DllMain (HINSTANCE hInstance , DWORD reason, PVOID pvReserved)
 {
   switch (reason) 
