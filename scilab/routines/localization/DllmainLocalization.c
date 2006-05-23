287ff5419c7f7ca9918cd498063ebe0300c06861
Allan Cornet <allan.cornet@scilab.org>
2006-05-23 08:28:49 +0000

    add localization project

---
+++ scilab/routines/localization/DllmainLocalization.c
@@ -0,0 +1,27 @@
+/*-----------------------------------------------------------------------------------*/
+/* INRIA 2006 */
+/* Allan CORNET */
+/*-----------------------------------------------------------------------------------*/ 
+#include <windows.h> 
+/*-----------------------------------------------------------------------------------*/ 
+#pragma comment(lib,"../../bin/glib-2.0.lib")
+#pragma comment(lib,"../../bin/libxml2.lib")
+#pragma comment(lib,"../../bin/iconv.lib")
+/*-----------------------------------------------------------------------------------*/ 
+int WINAPI DllMain (HINSTANCE hInstance , DWORD reason, PVOID pvReserved)
+{
+  switch (reason) 
+    {
+    case DLL_PROCESS_ATTACH:
+      break;
+    case DLL_PROCESS_DETACH:
+      break;
+    case DLL_THREAD_ATTACH:
+      break;
+    case DLL_THREAD_DETACH:
+      break;
+    }
+  return 1;
+}
+/*-----------------------------------------------------------------------------------*/ 
+
