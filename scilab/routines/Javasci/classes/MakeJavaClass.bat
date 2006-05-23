ce81cc3d2fdebfeb32232f907475e43ebe3efdca
Allan Cornet <allan.cornet@scilab.org>
2006-05-23 07:20:34 +0000

    uses prerequirements

--- scilab/routines/Javasci/classes/MakeJavaClass.bat
+++ scilab/routines/Javasci/classes/MakeJavaClass.bat
@@ -1,15 +1,13 @@
 @echo off
 rem A modifier selon votre répertoire d'installation de Java(TM) 2 SDK
-
-PATH=%PATH%;C:\Wintools\JBuilder9\jdk1.4\bin;C:\j2sdk1.4.2_05\bin;C:\Wintools\j2sdk1.4.2_05\bin;C:\Wintools\Java\jdk1.5.0_01\bin;C:\Wintools\jdk1.5.0_01\bin
-
+SET JAVAC="../../../Java/jdk1.5.0_06/bin/javac.exe"
 rem Compilation des classes Java d'interfaces
 
-javac -classpath ..\..\..\bin -d ..\..\..\bin BadDataArgumentException.java
-javac -classpath ..\..\..\bin -d ..\..\..\bin Scilab.java
-javac -classpath ..\..\..\bin -d ..\..\..\bin SciDoubleArray.java
-javac -classpath ..\..\..\bin -d ..\..\..\bin SciDouble.java
-javac -classpath ..\..\..\bin -d ..\..\..\bin SciStringArray.java
-javac -classpath ..\..\..\bin -d ..\..\..\bin SciString.java
+%JAVAC% -classpath ..\..\..\bin -d ..\..\..\bin BadDataArgumentException.java
+%JAVAC% -classpath ..\..\..\bin -d ..\..\..\bin Scilab.java
+%JAVAC% -classpath ..\..\..\bin -d ..\..\..\bin SciDoubleArray.java
+%JAVAC% -classpath ..\..\..\bin -d ..\..\..\bin SciDouble.java
+%JAVAC% -classpath ..\..\..\bin -d ..\..\..\bin SciStringArray.java
+%JAVAC% -classpath ..\..\..\bin -d ..\..\..\bin SciString.java
 
 @echo on
