3aa148b711dd7f874f43f1f57cb848af8f2ae8ad
Allan Cornet <allan.cornet@scilab.org>
2006-06-06 17:54:23 +0000

    Reorganization (Scilab 5)

--- scilab/routines/gtksci/helpbrowser.c
+++
@@ -1,9 +0,0 @@
-
-#include <gtk/gtk.h> 
-
-#if GTK_MAJOR_VERSION == 1 
-#include "helpbrowser-gtk1.c"
-#else 
-#include "helpbrowser-gtk2.c"
-#endif 
-
--- scilab/routines/gtksci/queue.c
+++
@@ -1,9 +0,0 @@
- 
-#include <gtk/gtk.h> 
-
-#if  GTK_MAJOR_VERSION == 1 
-#include "queue-gtk1.c"
-#else 
-#include "queue-gtk2.c"
-#endif 
-
