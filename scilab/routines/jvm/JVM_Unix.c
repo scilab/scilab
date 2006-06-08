47cf759ae26897a796a3c97785ed53554fc05a4f
Allan Cornet <allan.cornet@scilab.org>
2006-06-08 07:11:07 +0000

    correction compilation (linux)

--- scilab/routines/jvm/JVM_Unix.c
+++ scilab/routines/jvm/JVM_Unix.c
@@ -2,7 +2,17 @@
 /* INRIA 2006 */
 /* Allan CORNET */
 /*-----------------------------------------------------------------------------------*/ 
-#include "JVM_UNIX.h"
+#include "JVM_Unix.h"
+/*-----------------------------------------------------------------------------------*/ 
+#ifndef NULL
+  #define NULL 0
+#endif
+#ifndef TRUE 
+#define TRUE  1
+#endif 
+#ifndef FALSE
+#define FALSE 0
+#endif
 /*-----------------------------------------------------------------------------------*/ 
 static void *hLibJVM = NULL;
 /*-----------------------------------------------------------------------------------*/ 
@@ -11,7 +21,7 @@ jint MyJNI_CreateJavaVM(JavaVM **pvm, void **penv, void *args)
   jint (*createJavaVM)(JavaVM**, JNIEnv**, JavaVMInitArgs*); 
   createJavaVM = (jint (*)(JavaVM**, JNIEnv**, JavaVMInitArgs*))dlsym(hLibJVM, "JNI_CreateJavaVM"); 
 
-	return createJavaVM(pvm,penv,args); 
+	return createJavaVM(pvm,(JNIEnv**)penv,args); 
 }
 /*-----------------------------------------------------------------------------------*/ 
 jint MyJNI_GetCreatedJavaVMs(JavaVM **vmBuf, jsize BufLen, jsize *nVMs)
@@ -28,7 +38,7 @@ int GetJVMDll(char *SCILAB_PATH)
 	char *JVMDLLFULLNAME=NULL;
 	
 	JVMDLLFULLNAME=(char*)malloc( (strlen(SCILAB_PATH)+strlen(JRE_PATH)+strlen("/bin/client/libjava.so")+1)*sizeof(char));
-	/* A voir pour H.P(.sl) et autres come Mac*/
+	/* A voir pour H.P(.sl) et autres comme Mac*/
 	/* http://www.informatik.uni-frankfurt.de/doc/man/hpux/shl_load.3x.html */
 	sprintf(JVMDLLFULLNAME,"%s%s%s",SCILAB_PATH,JRE_PATH,"/bin/client/libjava.so");
 
