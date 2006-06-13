c4669dcaa0ed8e3aa566c5273e1a00a6621a5de7
Allan Cornet <allan.cornet@scilab.org>
2006-06-13 12:50:29 +0000

    add system_f2c

--- scilab/routines/system/diary.f
+++
@@ -1,9 +0,0 @@
-      subroutine diary(str,n)
-c     write string str in diary file if required
-      include '../stack.h'
-      character*(*) str
-      integer n
-
-      if(wio.ne.0) write(wio,'(a)') str(1:n)
-      return
-      end
