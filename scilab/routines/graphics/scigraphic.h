411b43b3230db44af42ec0c7339e96e25c5cd1db
Pierre Marechal <pierre.marechal@scilab.org>
2006-06-14 08:12:54 +0000

    Fix the doxygen documentation

--- scilab/routines/graphics/scigraphic.h
+++ scilab/routines/graphics/scigraphic.h
@@ -1,90 +1,67 @@
 
-/**sciSetScrollInfo
- *@description: Sets the dimension of the scroll bars
- * Do not call SetScrollInfo windows function,
+/**
+ * Sets the dimension of the scroll bars
+ * @attention Do not call SetScrollInfo windows function,
  * sciSetScrollInfo do that and more things !
- *@input: struct BCG *Scilabgc, int sb_ctl, SCROLLINFO *si, BOOLEAN bRedraw
- *@output: int
- *@author: Matthieu PHILIPPE 
- *@date: Dec 1999
+ * @param[in] Scilabgc
+ * @param[in] sb_ctl
+ * @param[in] si
+ * @param[in] bRedraw
+ *@author Matthieu PHILIPPE 
+ *@date Dec 1999
  **/
 extern int sciSetScrollInfo(struct BCG *Scilabgc, int sb_ctl,SCROLLINFO *si, BOOLEAN bRedraw);
 
-/**sciGetScrollInfo
- *@description: Returns the dimension of the scroll bars
- * Do not call GetScrollInfo windows function,
+/**
+ * Returns the dimension of the scroll bars
+ * @attention Do not call GetScrollInfo windows function,
  * sciGetScrollInfo do that and more things !
- *@input: struct BCG *Scilabgc, int sb_ctl, SCROLLINFO *si
- *@output: int
- *@author: Matthieu PHILIPPE 
- *@date: Dec 1999
+ * @param[in] Scilabgc
+ * @param[in] sb_ctl
+ * @param[in] si
+ *@author Matthieu PHILIPPE 
+ *@date Dec 1999
  **/
 extern int sciGetScrollInfo(struct BCG *Scilabgc, int sb_ctl, SCROLLINFO *si);
 
-/**sciGetScrollInfo
- *@description: Returns the wresize status.
+/**
+ * Returns the wresize status.
  * 0: it's in scroll bars mode
  * 1: it's in wresize mode
- *@input: NONE
- *@output: integer
- *@author: Matthieu PHILIPPE 
- *@date: Dec 1999
+ *@author Matthieu PHILIPPE 
+ *@date Dec 1999
  **/
 extern integer sciGetwresize();
 
-/**sciGetPixmapStatus
- *@description: Returns the pixmap status.
+/**
+ * Returns the pixmap status.
  * 0: it's drawn directly on screen
  * 1: it's drawn by a pixmap first
- *@input: NONE
- *@output: integer
- *@author: Matthieu PHILIPPE 
- *@date: Dec 1999
+ *@author Matthieu PHILIPPE 
+ *@date Dec 1999
  **/
 extern integer sciGetPixmapStatus();
 
-/**SciViewportGet
- *@description: used to get panner position through scilab command.
- *
- *@input: struct BCG *ScilabGC : structure associated to a Scilab Graphic window
- *        int x,y : the x,y point of the graphic window to be moved at 
- *        the up-left position of the viewport
- *
- *@output: NONE
- *
- *@author: 
- *@date:
+/**
+ * used to get panner position through scilab command.
+ * @param[in] struct BCG *ScilabGC : structure associated to a Scilab Graphic window
+ * @param[in] int x,y : the x,y point of the graphic window to be moved at the up-left position of the viewport
  **/
 extern void SciViewportGet __PARAMS((struct BCG *ScilabXgc,int *x,int *y));
 
-/**SciViewportMove
- *@description: used to move the panner and the viewport interactively 
- *              through scilab command.
- *
- *@input: struct BCG *ScilabGC : structure associated to a Scilab Graphic window
- *        int x,y : the x,y point of the graphic window to be moved at 
- *        the up-left position of the viewport
- *
- *@output: NONE
- *
- *@author: 
- *@date:
+/**
+ * used to move the panner and the viewport interactively 
+ * through scilab command.
+ * @param[in] struct BCG *ScilabGC : structure associated to a Scilab Graphic window
+ * @param[in] int x,y : the x,y point of the graphic window to be moved at the up-left position of the viewport
  **/
 extern void SciViewportMove __PARAMS((struct BCG *ScilabXgc,int x,int y));
 
-/**GPopupResize
- *@description: a little beat different to windowdim. GPopupResize sets the visible
- * window (parents dimension)
- *
- *@input: struct BCG *ScilabXgc,int *x,int *y , where x,y are
- * the new dimension
- *
- *@output: NONE
- *
- *@see: setwindowdim
- *
- *@author: 
- *@date: 
+/**
+ * A little beat different to windowdim. GPopupResize sets the visible window (parents dimension)
+ * @param[in] struct BCG *ScilabXgc
+ * @param[in] int * x,y , where x,y are the new dimension
+ * @see setwindowdim
  **/
 extern void GPopupResize __PARAMS((struct BCG *ScilabXgc,int *x,int *y));
 
