0e03b970e90da1e8d481d7dc8753cb97dd756f30
Allan Cornet <allan.cornet@scilab.org>
2006-06-15 18:42:18 +0000

    update example

--- scilab/examples/callsci/callsciBC++/myprog.c
+++ scilab/examples/callsci/callsciBC++/myprog.c
@@ -4,46 +4,17 @@
 #include "..\..\..\routines\machine.h"
 #include "..\..\..\routines\stack-c.h"
 
+/* Allan CORNET */
+/* INRIA */
 
-extern void add_sci_argv(char *p);
+#define TRUE 1
+#define FALSE 0
 
-/* this function should be moved inside scilab */
 /************************************************************************/
-static void Initialize()
-{
-  static char initstr[]="exec(\"..\\scilab.star\",-1);quit;";
-  static iflag=-1, stacksize = 1000000, ierr=0;
-
-  _control87(0x0008001F,0x0008001F); // Remove floating point exception
-
-  /* Chemins à modifier suivant votre configuration */
-  set_sci_env("..\\");
-
-  /* Scilab Initialization */
-  C2F(settmpdir)();
-  C2F(inisci)(&iflag,&stacksize,&ierr);
-  if ( ierr > 0 )
-    {
-      fprintf(stderr,"Scilab initialization failed !\n");
-      exit(1);
-    }
-  /* running the startup */
-  C2F(scirun)(initstr,strlen(initstr));
-  return;
-}
-/************************************************************************/
-int send_scilab_job(char *job)
-{
-  static char buf[1024];
-  static char format[]="Err=execstr('%s','errcatch','n');quit;";
-  int m,n,lp;
-  sprintf(buf,format,job);
-  fprintf(stderr,"job envoye %s\n",buf);
-  C2F(scirun)(buf,strlen(buf));
-  GetMatrixptr("Err", &m, &n, &lp);
-  return 0;
-  //return (int) *stk(lp);
-}
+/* See routines/system/CallScilab.h */
+extern int StartScilab(char *SCIpath,char *ScilabStartup,int *Stacksize);
+extern int TerminateScilab(char *ScilabQuit);
+extern int SendScilabJob(char *job); 
 /************************************************************************/
 static int my_job()
 {
@@ -59,15 +30,15 @@ static int my_job()
    WriteMatrix("b", &mb, &nb, b);
    // Affichage de la matrice b dans Scilab
    fprintf(stdout,"Affichage de la matrice b depuis Scilab\n");
-   send_scilab_job("disp(b);");
+   SendScilabJob("disp(b);");
    // Affichage de la matrice A dans Scilab
    fprintf(stdout,"Affichage de la matrice A depuis Scilab\n");
-   send_scilab_job("disp(A);");
+   SendScilabJob("disp(A);");
    // demande d'addition de A+b dans la matrice x dans scilab
-   send_scilab_job("x=A+b;");
+   SendScilabJob("x=A+b;");
    // Affichage de la matrice x dans Scilab
    fprintf(stdout,"Affichage de la matrice x depuis Scilab\n");
-   send_scilab_job("disp(x);");
+   SendScilabJob("disp(x);");
 
    // Recuperation de la matrice dans le programme C/C++
    ReadMatrix("x", &m, &n, &x);
@@ -79,19 +50,14 @@ static int my_job()
    }
 }
 /************************************************************************/
-int MAIN__(void)
+int main(void)
 {
 
-  static char nw[]="-nw";
-  static char nb[]="-nb";
-  add_sci_argv(nw);
-  add_sci_argv(nb);
-
-  Initialize();
+	if ( StartScilab(NULL,NULL,NULL) == FALSE ) printf("Error : StartScilab \n");
   my_job();
   getch();
-  C2F(sciquit)(); // quitte scilab
+	if ( TerminateScilab(NULL) == FALSE ) printf("Error : TerminateScilab \n");
 
 }
-
+/************************************************************************/
 
