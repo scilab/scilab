befab3385d0b5da7811739c1fdcc9d47fd404f15
Allan Cornet <allan.cornet@scilab.org>
2006-06-15 14:02:00 +0000

    remove mainwin95.f not used

--- scilab/routines/default/mainwin95.f
+++
@@ -1,9 +0,0 @@
-c     The main program is written in C for gc-win32
-c     this routine is mainly used for transfer to Fortran 
-c     from C main 
-c     ---------------------------------------------
-c     Copyright INRIA
-c     Unused function see wsci/winmain.c 
-      subroutine mainsci(nos1,path1,lpath)
-      end
-
