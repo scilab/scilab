e85312905de9ec844f4963330586f254fc5a4c12
Allan Cornet <allan.cornet@scilab.org>
2006-07-11 18:58:31 +0000

    add scilex -nogui  mode no window no interactive no gui no events
    %gui variable in scilab to detect this mode
    (correction compilation linux)

--- scilab/routines/system/CallScilab.c
+++ scilab/routines/system/CallScilab.c
@@ -12,6 +12,19 @@
 #if _MSC_VER
 #define putenv _putenv
 #endif
+
+#ifndef BOOL
+typedef int BOOL;
+#endif
+
+#ifndef TRUE
+ #define TRUE 1
+#endif
+
+#ifndef FALSE
+ #define FALSE 0
+#endif
+
 /*-----------------------------------------------------------------------------------*/
 static char DefaultScilabStartup[]="SCI/scilab.star";
 static char DefaultScilabQuit[]="SCI/scilab.quit";
--- scilab/routines/system/inisci-c.c
+++ scilab/routines/system/inisci-c.c
@@ -17,6 +17,20 @@
 #include "../os_specific/sci_mem_alloc.h"  /* malloc */
 #endif
 
+/*************************************************************************************************/
+#ifndef BOOL
+typedef int BOOL;
+#endif
+
+#ifndef TRUE
+ #define TRUE 1
+#endif
+
+#ifndef FALSE
+ #define FALSE 0
+#endif
+/*************************************************************************************************/
+
 #ifdef _MSC_VER
 	BOOL ExistScicos(void);
 	BOOL FileExist(char *filename);
