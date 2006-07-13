06b7af47d9fb042952bc92b189ac8c4866fd5aeb
Allan Cornet <allan.cornet@scilab.org>
2006-07-13 09:19:21 +0000

    define types of struct STACK_struct --> C2F(stack) VSTK_struct -->  C2F(vstk) RECU_struct --> C2F(recu) IOP_struct --> C2F(iop) ERRGST_struct --> C2F(errgst) COM_struct --> C2F(com) ADRE_struct --> C2F(adre) INTERSCI_struct --> C2F(intersci) CHA1_struct --> C2F(cha1) DBG_struct --> C2F(dbg)

--- scilab/routines/stack-def.h
+++ scilab/routines/stack-def.h
@@ -3,9 +3,6 @@
 #ifndef STACK_DEF 
 #define STACK_DEF 
 
-/* XXXXX pour gcc -3.1 il faut un extern devant le 
- * __declspec(dllimport) 
- */ 
 
 #if _LCC_ & FORDLL 
   #define IMPORT __declspec (dllimport)
@@ -16,7 +13,6 @@
    #define IMPORT extern
  #endif
 #endif
-/* typedef int integer ; */
 
 #define csiz 63  
 #define bsiz 4096  
@@ -30,52 +26,75 @@
 #define maxdb 100
 #define maxbpt 1000
 
-IMPORT struct {
-    double Stk[vsiz];
-} C2F(stack);
+typedef struct 
+{
+ double Stk[vsiz];
+} STACK_struct;
 
+IMPORT STACK_struct C2F(stack);
 
-IMPORT struct {
-    int bot, top, idstk[nsiz*isizt], lstk[isizt], leps, 
-	    bbot, bot0, infstk[isizt], gbot, gtop, isiz;
-} C2F(vstk);
+typedef struct 
+{
+ int bot, top, idstk[nsiz*isizt], lstk[isizt], leps,bbot, bot0, infstk[isizt], gbot, gtop, isiz;
+} VSTK_struct;
 
+IMPORT VSTK_struct C2F(vstk);
 
-IMPORT struct {
-    int ids[nsiz*psiz], pstk[psiz], rstk[psiz], pt, niv, 
-	    macr, paus, icall, krec;
-} C2F(recu);
+typedef struct 
+{
+ int ids[nsiz*psiz], pstk[psiz], rstk[psiz], pt, niv, macr, paus, icall, krec;
+} RECU_struct;
 
-IMPORT struct {
-    int ddt, err, lct[8], lin[lsiz], lpt[6], hio, rio, wio, rte, wte;
-} C2F(iop);
+IMPORT RECU_struct C2F(recu);
 
-IMPORT struct {
-    int err1, err2, errct, toperr, errpt, ieee ,errcatch;
-} C2F(errgst); 
+typedef struct 
+{
+ int ddt, err, lct[8], lin[lsiz], lpt[6], hio, rio, wio, rte, wte;
+} IOP_struct;
 
-IMPORT struct {
-    int sym, syn[nsiz], char1, fin, fun, lhs, rhs, ran[2], comp[3];
-} C2F(com);
+IMPORT IOP_struct C2F(iop);
 
+typedef struct 
+{
+ int err1, err2, errct, toperr, errpt, ieee ,errcatch;
+} ERRGST_struct; 
 
-IMPORT struct {
-    int lbot, ie, is, ipal, nbarg, ladr[intersiz];
-} C2F(adre);
+IMPORT ERRGST_struct C2F(errgst);
 
+typedef struct 
+{
+ int sym, syn[nsiz], char1, fin, fun, lhs, rhs, ran[2], comp[3];
+} COM_struct;
 
-IMPORT struct {
+IMPORT COM_struct C2F(com);
+
+typedef struct 
+{
+ int lbot, ie, is, ipal, nbarg, ladr[intersiz];
+} ADRE_struct;
+
+IMPORT ADRE_struct C2F(adre);
+
+typedef struct 
+{
   int nbvars, iwhere[intersiz], nbrows[intersiz], nbcols[intersiz], itflag[intersiz], ntypes[intersiz], lad[intersiz],ladc[intersiz],lhsvar[intersiz];
-} C2F(intersci);
+} INTERSCI_struct;
 
+IMPORT INTERSCI_struct C2F(intersci);
 
-IMPORT struct {
+typedef struct
+{
   char alfa[csiz], alfb[csiz], buf[bsiz];
-} C2F(cha1);
+} CHA1_struct;
 
-IMPORT struct {
-    int wmac, lcntr, nmacs, macnms[nsiz*maxdb]	, lgptrs[maxdb+1], bptlg[maxbpt];
-} C2F(dbg);
+IMPORT CHA1_struct C2F(cha1);
+
+typedef struct 
+{
+  int wmac, lcntr, nmacs, macnms[nsiz*maxdb], lgptrs[maxdb+1], bptlg[maxbpt];
+} DBG_struct;
+
+IMPORT DBG_struct C2F(dbg);
 
 #endif /** STACK_DEF  **/
 
