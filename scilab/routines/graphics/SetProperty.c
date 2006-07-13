30ddc10d2e8169aa565d19c65f27128a01a55890
Jean-baptiste Silvy <jean-baptiste.silvy@scilab.org>
2006-07-13 14:07:14 +0000

    Fix bug 1587.

--- scilab/routines/graphics/SetProperty.c
+++ scilab/routines/graphics/SetProperty.c
@@ -155,13 +155,13 @@ sciSetEntityType (sciPointObj * pobj, sciEntityType value)
  * a[i] = RED, a[i+m] = GREEN, a[i+2*m] = BLUE 
  */
 int
-sciSetColormap (sciPointObj * pobj, double *rgbmat, integer m, integer n)
+sciSetColormap ( sciPointObj * pobj, double *rgbmat, integer m, integer n )
 {
-  /*  double *pc;*/
   int k,old_m,m1;
-  sciPointObj * pcurwin;
+  int curWinIndex = sciGetNum( sciGetCurrentFigure() ) ;
   double *cmap;
-  int succeed = 0;
+  int notSucceed = 0 ;
+  sciFigure * pFigure = NULL ;
 
   if(n != 3)
   {
@@ -174,53 +174,66 @@ sciSetColormap (sciPointObj * pobj, double *rgbmat, integer m, integer n)
     sciprint("sciSetColormap Error: Object must be a SCI_FIGURE\n");
     return 0;
   }
+  
+  pFigure = pFIGURE_FEATURE( pobj ) ;
 
   old_m = sciGetNumColors(pobj);
-  m1=m;
+  m1 = m ;
   if ( pobj != pfiguremdl )
   {
     int verbose = 0 ;
-    pcurwin=sciGetCurrentFigure ();
-    sciSetCurrentFigure ( pobj);
+    sciSetUsedWindow( sciGetNum( pobj ) ) ;
     /*It should be impossible to set the colormap because of restriction on max 
       number of colors. In this case the old one is kept*/
-    C2F(dr)("xset","colormap",&m,&n,&succeed,PI0,PI0,PI0,rgbmat,PD0,PD0,PD0,0L,0L);
+    C2F(dr)("xset","colormap",&m,&n,&notSucceed,PI0,PI0,PI0,rgbmat,PD0,PD0,PD0,0L,0L);
     C2F(dr)("xget","cmap_size", &verbose, &m1, PI0, PI0, PI0, PI0, PD0, PD0, PD0, PD0, 0L, 0L ) ;
     sciSetNumColors( pobj, m1 ) ;
-    sciSetCurrentFigure (pcurwin);
+    sciSetUsedWindow( curWinIndex ) ;
   }
   
-  if(succeed == 1){ /* failed to allocate or xinit (for Gif driver) was missing */
+  if ( notSucceed )
+  {
+    /* failed to allocate or xinit (for Gif driver) was missing */
     sciprint ("Failed to change colormap : Allocation failed or missing xinit detected\n");
     return -1;
   }
   
-  if (m1 != old_m){ /* color map size changes, reallocate it */
-    if ((cmap = (double *)MALLOC (m*n*sizeof(double))) == (double *) NULL) {
-      if (pobj != pfiguremdl) {
-	sciSetCurrentFigure ( pobj);
-	C2F(dr)("xset","colormap",&old_m,&n,&succeed,PI0,PI0,PI0,
-		pFIGURE_FEATURE( (sciPointObj *) pobj)->pcolormap,PD0,PD0,PD0,0L,0L);
+  if (m1 != old_m)
+  {
+   /* color map size changes, reallocate it */
+    if ( ( cmap = MALLOC ( m * n * sizeof(double) ) ) == NULL )
+    {
+      /* error allocating colormap */
+      if (pobj != pfiguremdl)
+      {
+	sciSetUsedWindow( sciGetNum( pobj ) ) ;
+	C2F(dr)("xset","colormap",&old_m,&n,&notSucceed,PI0,PI0,PI0,
+		pFigure->pcolormap,PD0,PD0,PD0,0L,0L);
 	
-	if(succeed == 1){ /* failed to allocate or xinit (for Gif driver) was missing */
+	if( notSucceed )
+        {
+          /* failed to allocate or xinit (for Gif driver) was missing */
 	  sciprint ("Failed to change colormap : Allocation failed or missing xinit detected\n");
 	  return -1;
 	}
-	sciSetCurrentFigure (pcurwin);
+	sciSetUsedWindow( curWinIndex ) ;
       }
       sciprint ("Not enough memory available for colormap, previous one kept\n");
       return -1;
     }  
-    FREE(pFIGURE_FEATURE( (sciPointObj *) pobj)->pcolormap);
-    pFIGURE_FEATURE( (sciPointObj *) pobj)->pcolormap=cmap;
+    FREE( pFigure->pcolormap ) ;
+    pFigure->pcolormap = cmap;
   }
-  for (k=0;k<m1*n;k++)
+  for ( k =  0  ; k < m1 * n ; k++ )
   {
-    pFIGURE_FEATURE(pobj)->pcolormap[k] = rgbmat[k];
+    pFigure->pcolormap[k] = rgbmat[k];
   }
-  pFIGURE_FEATURE (pobj)->numcolors = m1;
+  pFigure->numcolors = m1 ;
   
-  if (pobj != pfiguremdl) sciRecursiveUpdateBaW(pobj,old_m, m); /* missing line F.Leray */
+  if ( pobj != pfiguremdl )
+  {
+    sciRecursiveUpdateBaW( pobj, old_m, m ) ; /* missing line F.Leray */
+  }
 
   return 0;
 }
