e2f82d6e1941861d97386b746265403aef205d5e
Allan Cornet <allan.cornet@scilab.org>
2006-07-28 09:32:47 +0000

    Reorganization
    io module
    
    move routines/interf/intsetenv.c --> modules/io/sci_gateway/c/intsetenv.c

--- scilab/modules/io/sci_gateway/c/intsetenv.c
+++ scilab/modules/io/sci_gateway/c/intsetenv.c
@@ -5,12 +5,9 @@
 #include <stdio.h> 
 #include <string.h> 
 
-#ifdef _MSC_VER
-#include "../os_specific/win_mem_alloc.h" /* MALLOC */
-#else
-#include "../os_specific/sci_mem_alloc.h" /* MALLOC */
-#endif
-#include "../stack-c.h"
+
+#include "MALLOC.h" /* MALLOC */
+#include "stack-c.h"
 
 /*-----------------------------------------------------------------------------------*/
 extern int setenvc(char *string,char * value);
