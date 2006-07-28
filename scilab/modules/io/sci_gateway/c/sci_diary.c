18a1cf4aed5b11fdf2929e9442c93a4d4f5cf552
Allan Cornet <allan.cornet@scilab.org>
2006-07-28 12:33:38 +0000

    Reorganization
    move routines/system/diary.c --> modules/io/sci_gateway/c/sci_diary.c

--- scilab/modules/io/sci_gateway/c/sci_diary.c
+++ scilab/modules/io/sci_gateway/c/sci_diary.c
@@ -2,14 +2,52 @@
 /* INRIA 2006 */
 /* Allan CORNET */
 /*-----------------------------------------------------------------------------------*/ 
+#include <stdio.h>
 #include "machine.h"
 #include "stack-c.h"
 /*-----------------------------------------------------------------------------------*/
-extern int C2F(intdiary)();
+extern FILE *GetFile(integer *fd);
+/*-----------------------------------------------------------------------------------*/
+int C2F(intdiary)();
 /*-----------------------------------------------------------------------------------*/
 int C2F(sci_diary) _PARAMS((char *fname,unsigned long fname_len))
 {
 	C2F(intdiary)();
 	return 0;
+}
+/*-----------------------------------------------------------------------------------*/
+int getdiary()
+{
+	return C2F(iop).wio;
+} 
+/*-----------------------------------------------------------------------------------*/
+void C2F(diary)(char *str,int *n)
+{
+#if defined(_MSC_VER)
+	char newline[2]="\r\n";
+	int nn=2;
+#else
+	char newline[1]="\n";
+	int nn=1;
+#endif
+	int u;
+
+	u=getdiary();
+	if ( u != 0 ) {
+		FILE *fd= GetFile(&u);
+		fwrite(str,sizeof(unsigned char),*n,fd);
+		fwrite(newline,sizeof(unsigned char),nn,fd);
+	}
+}
+/*-----------------------------------------------------------------------------------*/
+void diary_nnl(char *str,int *n)
+{
+	int u;
+
+	u=getdiary();
+	if (u) {
+		FILE *fd= GetFile(&u);
+		fwrite(str,sizeof(unsigned char),*n,fd);
+	}
 }
 /*-----------------------------------------------------------------------------------*/
