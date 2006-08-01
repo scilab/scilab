50790d9797483720438e6ce534d900a5cddc45c9
Allan Cornet <allan.cornet@scilab.org>
2006-08-01 08:48:33 +0000

    CreateVarFromPtr --> CreateVar not used a MALLOC and FREE (optimization)

--- scilab/modules/time/sci_gateway/sci_timer.c
+++ scilab/modules/time/sci_gateway/sci_timer.c
@@ -3,7 +3,6 @@
 /* Allan CORNET */
 /*-----------------------------------------------------------------------------------*/
 #include "sci_timer.h"
-#include "MALLOC.h" /* MALLOC */
 /*-----------------------------------------------------------------------------------*/
 extern int C2F(timer)(double *etime);
 /*-----------------------------------------------------------------------------------*/
@@ -11,22 +10,19 @@ int C2F(sci_timer) _PARAMS((char *fname,unsigned long fname_len))
 {
 	static int l1,n1,m1;
 	double timeval=0;
-	double *paramout=NULL;
 	
 	Rhs = Max(0, Rhs);
 	CheckLhs(0,1);
 	CheckRhs(0,0);
+
 	C2F(timer)(&timeval);
-	
-	paramout=(double*)MALLOC(sizeof(int));
-	*paramout=timeval;
+
 	n1=1;
-	CreateVarFromPtr(1, "d", &n1, &n1, &paramout);
+	CreateVar(1, "d", &n1, &n1,&l1);
+	*stk(l1) = (double)timeval;
 	LhsVar(1)=Rhs+1;
 	C2F(putlhsvar)();
-	if (paramout) {FREE(paramout);paramout=NULL;}
-
-
+	
 	return 0;
 }
 /*-----------------------------------------------------------------------------------*/
