5c138b653fa28ce4fb413e32c55508bccbd4c076
Allan Cornet <allan.cornet@scilab.org>
2006-08-09 05:53:27 +0000

    update path (x64, x86) fortran

--- scilab/libs/LibScilab/DllmainLibscilab.c
+++ scilab/libs/LibScilab/DllmainLibscilab.c
@@ -6,29 +6,17 @@
 /*-----------------------------------------------------------------------------------*/ 
 #ifndef USE_F2C
 
-	#pragma comment(lib,"C:/PROGRA~1/Intel/Compiler/Fortran/9.1/IA32/Lib/ifconsol.lib")
-	#pragma comment(lib,"C:/PROGRA~1/Intel/Compiler/Fortran/9.1/IA32/Lib/libifport.lib")
+	#pragma comment(lib,"ifconsol.lib")
+	#pragma comment(lib,"libifport.lib")
 	#if _DEBUG
-		#pragma comment(lib,"C:/PROGRA~1/Intel/Compiler/Fortran/9.1/IA32/Lib/libifcoremdd.lib")
-		#pragma comment(lib,"C:/PROGRA~1/Intel/Compiler/Fortran/9.1/IA32/Lib/libmmdd.lib")
+		#pragma comment(lib,"libifcoremdd.lib")
+		#pragma comment(lib,"libmmdd.lib")
 	#else
-		#pragma comment(lib,"C:/PROGRA~1/Intel/Compiler/Fortran/9.1/IA32/Lib/libifcoremd.lib")
-		#pragma comment(lib,"C:/PROGRA~1/Intel/Compiler/Fortran/9.1/IA32/Lib/libmmd.lib")
+		#pragma comment(lib,"libifcoremd.lib")
+		#pragma comment(lib,"libmmd.lib")
 	#endif
-	#pragma comment(lib,"C:/PROGRA~1/Intel/Compiler/Fortran/9.1/IA32/Lib/libirc.lib")
+	#pragma comment(lib,"libirc.lib")
 #endif
-/*  On Windows x64 Default path are differents (32 bits):
-	#pragma comment(lib,"C:/PROGRA~2/Intel/Compiler/Fortran/9.1/IA32/Lib/ifconsol.lib")
-	#pragma comment(lib,"C:/PROGRA~2/Intel/Compiler/Fortran/9.1/IA32/Lib/libifport.lib")
-	#if _DEBUG
-		#pragma comment(lib,"C:/PROGRA~2/Intel/Compiler/Fortran/9.1/IA32/Lib/libifcoremdd.lib")
-		#pragma comment(lib,"C:/PROGRA~2/Intel/Compiler/Fortran/9.1/IA32/Lib/libmmdd.lib")
-	#else
-		#pragma comment(lib,"C:/PROGRA~2/Intel/Compiler/Fortran/9.1/IA32/Lib/libifcoremd.lib")
-		#pragma comment(lib,"C:/PROGRA~2/Intel/Compiler/Fortran/9.1/IA32/Lib/libmmd.lib")
-	#endif
-	#pragma comment(lib,"C:/PROGRA~2/Intel/Compiler/Fortran/9.1/IA32/Lib/libirc.lib")
-*/
 /*-----------------------------------------------------------------------------------*/ 
 #if WITH_TK
 #pragma comment(lib,"../../bin/tcl84.lib")
