ffcc4c521e509fb2d7f44a23f59649350035fcd0
Allan Cornet <allan.cornet@scilab.org>
2006-08-16 08:12:03 +0000

    Reorganization
    split  sigelm.f --> sci_xxx.f
    
    (signal_processing module)

---
+++ scilab/routines/signal/sci_ffir.f
@@ -0,0 +1,12 @@
+c     =======================================
+c     INRIA
+c     =======================================
+      subroutine sciffir(fname)
+c     implicit undefined (a-z)
+      include '../stack.h'
+      character*(*) fname
+      return
+      end
+c     =======================================
+
+
