8f1670182ff3921bbc2edc6d65c652bc34a764dd
Allan Cornet <allan.cornet@scilab.org>
2006-08-18 12:11:44 +0000

    Reorganization
    CACSD module
    
    split interf/matqz.f

---
+++ scilab/routines/interf/sci_gschur.f
@@ -0,0 +1,22 @@
+c     --------------------------
+c     Copyright INRIA
+c     --------------------------
+      subroutine scigschur(fname)
+c     Copyright INRIA
+      INCLUDE '../stack.h'
+c     
+      character*(*) fname
+      integer iadr,sadr
+     
+      sadr(l)=(l/2)+1
+      iadr(l)=l+l-1
+      
+c     obsolete 
+c     call macro for emulation
+      call cvname(ids(1,pt+1),'gschur',0)
+      fun=-1
+      return
+      end
+c     --------------------------
+      
+      
---
+++ scilab/routines/interf/sci_gspec.f
@@ -0,0 +1,21 @@
+c     --------------------------
+c     Copyright INRIA
+c     --------------------------
+      subroutine scigspec(fname)
+      INCLUDE '../stack.h'
+c     
+      character*(*) fname
+      integer iadr,sadr
+     
+      sadr(l)=(l/2)+1
+      iadr(l)=l+l-1
+
+c     osolete
+c     call macro for emulation
+      call cvname(ids(1,pt+1),'gspec',0)
+      fun=-1
+      return
+
+      end
+c     --------------------------
+      
