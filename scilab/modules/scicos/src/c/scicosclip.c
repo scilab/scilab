0ab34b833bbdc79e9aebcfa4dfecb3834c5867a1
Jean-baptiste Silvy <jean-baptiste.silvy@scilab.org>
2006-08-23 06:48:27 +0000

    Add a new include.

--- scilab/modules/scicos/src/c/scicosclip.c
+++ scilab/modules/scicos/src/c/scicosclip.c
@@ -3,6 +3,7 @@
 #include "core_math.h" 
 #include "../../graphics/includes/Graphics.h"
 #include "../../graphics/includes/PloEch.h"
+#include "../../graphics/includes/Xcall1.h"
 
 void C2F(scicosclip)(int *n)
 {
