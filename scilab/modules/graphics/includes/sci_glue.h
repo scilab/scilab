193e0c0132ff2bf74d12bce49b81725d0f1f9f7f
Jean-baptiste Silvy <jean-baptiste.silvy@scilab.org>
2006-08-24 12:45:53 +0000

    Move glue from matdes to a newly created file.

--- scilab/modules/graphics/includes/sci_glue.h
+++ scilab/modules/graphics/includes/sci_glue.h
@@ -1,20 +1,20 @@
 /*------------------------------------------------------------------------*/
-/* file: sci_move.h                                                       */
+/* file: sci_glue.h                                                       */
 /* Copyright INRIA 2006                                                   */
 /* Authors : Fabrice Leray, Jean-Baptiste Silvy                           */
-/* desc : interface for move routine                                      */
+/* desc : interface for glue routine                                      */
 /*------------------------------------------------------------------------*/
 
-#ifndef _INT_MOVE_H_
-#define _INT_MOVE_H_
+#ifndef _INT_GLUE_H_
+#define _INT_GLUE_H_
 
 /**
-* interface function for the move routine.
+* interface function for the glue routine.
 *
-* @param[in] fname     name of the routine (ie move)
+* @param[in] fname     name of the routine (ie glue)
 * @param[in] fname_len length of fname
 * @return 0 if success, -1 otherwise
 */
-int sci_move( char * fname, unsigned long fname_len ) ;
+int sci_glue( char * fname, unsigned long fname_len ) ;
 
-#endif /* _INT_MOVE_H_ */
+#endif /* _INT_GLUE_H_ */
--- scilab/modules/graphics/sci_gateway/gw_graphics.c
+++ scilab/modules/graphics/sci_gateway/gw_graphics.c
@@ -81,7 +81,7 @@ extern int sci_delete                     _PARAMS((char *fname, unsigned long fn
 extern int sci_addcb                      _PARAMS((char *fname, unsigned long fname_len));
 extern int sci_copy                       _PARAMS((char *fname, unsigned long subwinparenttarget));
 extern int sci_move                       _PARAMS((char *fname, unsigned long fname_len));
-extern int glue                           _PARAMS((char *fname, unsigned long fname_len));
+extern int sci_glue                       _PARAMS((char *fname, unsigned long fname_len));
 extern int unglue                         _PARAMS((char *fname, unsigned long fname_len));
 extern int drawnow                        _PARAMS((char *fname, unsigned long fname_len));
 extern int drawlater                      _PARAMS((char *fname, unsigned long fname_len));
@@ -182,7 +182,7 @@ static MatdesTable Tab[]={
 	{sci_StringBox,"StringBox"},
 	/* NG beg */
 	{sci_move,"move"},
-	{glue,"glue"},
+	{sci_glue,"glue"},
 	{unglue,"unglue"}, 
 	{drawnow,"drawnow"},
 	{drawlater,"drawlater"},  
