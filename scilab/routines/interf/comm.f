7c005bcea2969c20eb7f4342fa9a3572ac99d7f8
Allan Cornet <allan.cornet@scilab.org>
2006-08-28 14:47:35 +0000

    Reorganization
    not used --> removed

--- scilab/routines/interf/comm.f
+++
@@ -1,9 +0,0 @@
-c  interface function 
-c   ********************
-       subroutine  comm
-c
-      include '../stack.h'
-c
-      buf='Communication toolbox GeCi no more supported'
-      call error(999)
-       end
