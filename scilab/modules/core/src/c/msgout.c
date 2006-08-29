cd3fc3a0655040e9b934ad72f9dbb499d7801fa2
Allan Cornet <allan.cornet@scilab.org>
2006-08-29 11:39:06 +0000

    correction for matsys.tst

--- scilab/modules/core/src/c/msgout.c
+++ scilab/modules/core/src/c/msgout.c
@@ -3,7 +3,6 @@
 /* Allan CORNET */
 /*-----------------------------------------------------------------------------------*/ 
 #include "machine.h"
-#include <string.h>
 /*-----------------------------------------------------------------------------------*/ 
 extern integer C2F(msgstore)();
 extern int C2F(basout)();
@@ -11,11 +10,11 @@ extern int C2F(basout)();
 int C2F(msgout)(integer *io,integer *lunit,char *str,long int str_len)
 {
 	static integer ierr;
-	int len=strlen(str);
+	integer i__1=str_len;
 
-    C2F(basout)(io, lunit, str, str_len);
-    ierr = C2F(msgstore)(str, &len,str_len);
-    
+    C2F(basout)(io, lunit, str, str_len);;
+    ierr = C2F(msgstore)(str, &i__1, str_len);
+
     return 0;
 }
 /*-----------------------------------------------------------------------------------*/ 
