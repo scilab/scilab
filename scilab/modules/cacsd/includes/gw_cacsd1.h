67e42e4b94e6d1bb000622f4c9f363aa91cd6db2
Allan Cornet <allan.cornet@scilab.org>
2006-08-30 09:07:29 +0000

    move gw_cacsd1 and interfaces
    rename sci_ricc --> sci_ricc2

---
+++ scilab/modules/cacsd/includes/gw_cacsd1.h
@@ -0,0 +1,29 @@
+/*-----------------------------------------------------------------------------------*/
+/* INRIA 2006 */
+/* Allan CORNET */
+/*-----------------------------------------------------------------------------------*/ 
+#ifndef __GW_CACSD1__
+#define __GW_CACSD1__
+
+#ifdef _MSC_VER
+  #include <windows.h>
+  #include <stdio.h>
+#endif
+
+#include <string.h>
+
+#include "../machine.h"
+#include "../stack-c.h"
+
+
+typedef int (*Cacsd1_Interf) __PARAMS((char *fname));
+
+typedef struct table_struct 
+{
+  Cacsd1_Interf f;    /** function **/
+  char *name;      /** its name **/
+} Cacsd1Table;
+
+#endif /*  __GW_CACSD1__ */
+/*-----------------------------------------------------------------------------------*/
+
---
+++ scilab/modules/cacsd/sci_gateway/c/gw_cacsd1.c
@@ -0,0 +1,45 @@
+#include "gw_cacsd1.h"
+/*-----------------------------------------------------------------------------------*/
+/* INRIA 2006 */
+/* Allan CORNET */
+/*-----------------------------------------------------------------------------------*/
+#if _MSC_VER
+#include <Windows.h>
+#include "MALLOC.h"
+extern char *GetExceptionString(DWORD ExceptionCode);
+#endif
+/*-----------------------------------------------------------------------------------*/
+extern int C2F(scilyap) _PARAMS((char *fname));
+extern int C2F(scisylv) _PARAMS((char *fname));
+extern int C2F(intricc) _PARAMS((char *fname));
+/*-----------------------------------------------------------------------------------*/
+static Cacsd1Table Tab[]=
+{
+	{C2F(scilyap),"lyap"},
+	{C2F(scisylv),"sylv"},
+	{C2F(intricc),"ricc"}
+};
+/*-----------------------------------------------------------------------------------*/
+int C2F(gw_cacsd1)()
+{  
+#if _MSC_VER
+	#ifndef _DEBUG
+		_try
+		{
+			(*(Tab[Fin-1].f)) (Tab[Fin-1].name);
+		}
+		_except (EXCEPTION_EXECUTE_HANDLER)
+		{	
+			char *ExceptionString=GetExceptionString(GetExceptionCode());
+			sciprint("Warning !!!\nScilab has found a critical error (%s)\nwith \"%s\" function.\nScilab may become unstable.\n",ExceptionString,Tab[Fin-1].name);
+			if (ExceptionString) {FREE(ExceptionString);ExceptionString=NULL;}
+		}
+	#else
+		(*(Tab[Fin-1].f)) (Tab[Fin-1].name);
+	#endif
+#else
+		(*(Tab[Fin-1].f)) (Tab[Fin-1].name);
+#endif
+	return 0;
+}
+/*-----------------------------------------------------------------------------------*/
