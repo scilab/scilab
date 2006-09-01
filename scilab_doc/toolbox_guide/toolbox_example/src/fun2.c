272c19bfb20ef5c9db9e272e5f14392d4e6aeee3
Farid Belahcene <farid.belahcene@inria.fr>
2006-09-01 09:26:00 +0000

    New modifications

--- scilab_doc/toolbox_guide/toolbox_example/src/fun2.c
+++ scilab_doc/toolbox_guide/toolbox_example/src/fun2.c
@@ -1,8 +1,8 @@
-void  fun2(double * a, int na, int * m)
+void  fun2(double * a, int na, int * nx)
 {
-int i;
-*m=0;
-for(i=0;i<na;i++)
-    if (a[i]>0) 
-      (*m)++;
+        int i;
+        *nx=0;
+        for(i=0;i<na;i++)
+        if (a[i]>0)  
+                (*nx)++;
 }
