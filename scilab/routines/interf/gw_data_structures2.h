e9a3d5bc6c0cf2c8536aa460e61aa95bb3951458
Allan Cornet <allan.cornet@scilab.org>
2006-09-01 08:49:10 +0000

    Reorganization
    rewrite lstelmi.f --> gw_data_structures2.c

---
+++ scilab/routines/interf/gw_data_structures2.h
@@ -0,0 +1,29 @@
+/*-----------------------------------------------------------------------------------*/
+/* INRIA 2006 */
+/* Allan CORNET */
+/*-----------------------------------------------------------------------------------*/ 
+#ifndef __GW_DATA_STRUCTURES2__
+#define __GW_DATA_STRUCTURES2__
+
+#ifdef _MSC_VER
+#include <windows.h>
+#include <stdio.h>
+#endif
+
+#include <string.h>
+
+#include "machine.h"
+#include "stack-c.h"
+
+
+typedef int (*Data_structures2_Interf) __PARAMS((char *fname));
+
+typedef struct table_struct 
+{
+	Data_structures2_Interf f;    /** function **/
+	char *name;      /** its name **/
+} DataStructures2Table;
+
+#endif /*  __GW_DATA_STRUCTURES2__ */
+/*-----------------------------------------------------------------------------------*/
+
