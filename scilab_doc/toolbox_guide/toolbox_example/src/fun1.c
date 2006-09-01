f81763976443c5e3ab6af2d0b72b0c319e3f0229
Farid Belahcene <farid.belahcene@inria.fr>
2006-09-01 09:26:06 +0000

    New modifications

--- scilab_doc/toolbox_guide/toolbox_example/src/fun1.c
+++ scilab_doc/toolbox_guide/toolbox_example/src/fun1.c
@@ -1,17 +1,15 @@
-void  fun2(double *, int, int *);
+extern void  fun2(double *, int, int *);
 
-void fun1(double * a,  int na, int * nb,  double ** b , double * positive_sum)
-{
-int i, k1=0;
-*positive_sum=0;
-fun2(a, na, nb);
-*b=(double *)malloc((*nb)*sizeof(double));
-*positive_sum=0;
-for(i=0;i<na;i++)
-  if(a[i]>0)
-    {
-    *(*b+k1)=a[i];
-    *positive_sum += a[i];
-    k1++;
-    };
+void fun1(double * a,  int na, int * nx,  double ** x , double * y){
+        int i, k1=0;
+        *y=0;
+        fun2(a, na, nx);
+        *x=(double *)malloc((*nx)*sizeof(double));
+        *y=0;
+        for(i=0;i<na;i++)
+        if(a[i]>0) {
+                *(*x+k1)=a[i];
+                *y += a[i];
+                k1++;
+        };
 }
