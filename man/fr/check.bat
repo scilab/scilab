84287932162e06294332de9c3eebd708ad70a14e
Allan Cornet <allan.cornet@scilab.org>
2006-09-09 20:31:40 +0000

    Reorganization
    sort and move man

--- man/fr/check.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-@..\..\..\Win-util\xmlint\xmlint.exe *.xml > xmlint.tmp
-IF NOT ERRORLEVEL 1 GOTO OK
-echo          Problem(s) detected (See xmlint.tmp)
-GOTO END
-:OK
-del xmlint.tmp
-:END
-@echo on
