84b62d751d142b75b88766cfbcdc2a5a7cf88574
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2006-10-13 14:46:59 +0000

    Bug : missing one parameter which caused a seg fault

--- scilab/modules/core/sci_gateway/c/sci_chdir.c
+++ scilab/modules/core/sci_gateway/c/sci_chdir.c
@@ -5,11 +5,11 @@
 #include "machine.h"
 #include "stack-c.h"
 /*-----------------------------------------------------------------------------------*/
-extern int C2F(intschdir) _PARAMS((char *fname));
+extern int C2F(intschdir) _PARAMS((char *fname, unsigned long fname_len));
 /*-----------------------------------------------------------------------------------*/
 int C2F(sci_chdir) _PARAMS((char *fname,unsigned long fname_len))
 {
-	C2F(intschdir)(fname);
+	C2F(intschdir)(fname, fname_len);
 	return 0;
 }
 /*-----------------------------------------------------------------------------------*/
