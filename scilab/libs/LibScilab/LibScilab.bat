5b8bad897f8d3639d54753067e6871cdfd9697d6
Allan Cornet <allan.cornet@scilab.org>
2006-10-23 13:12:24 +0000

    update for Intel compilers 9.1.32

--- scilab/libs/LibScilab/LibScilab.bat
+++ scilab/libs/LibScilab/LibScilab.bat
@@ -1,24 +1,19 @@
-@rem Allan CORNEI
+@rem Allan CORNET
 @rem INRIA 2006
 @rem Create Libscilab.def with all .vcdef 
-if NOT EXIST ..\LibScilab.def @call :NewLibScilabDef
+
+@type headerLibScilab.vcdef > ..\LibScilab.def
+
 :Concatfiles
 setlocal
-for %%f in (%*) do @call :AddDef %%f
+for %%f in (..\*.vcdef) do @call :AddDef %%f
 endlocal
 goto :EOF
-
-:NewLibScilabDef
-@copy /Y ..\headerLibScilab.vcdef ..\LibScilab.def >NUL
-@copy /Y ..\LibScilab.def+..\ExplicitExportsLibScilab.vcdef ..\LibScilab.def >NUL
-goto :Concatfiles
-
 
 :AddDef
 @echo ; --------------------------------------- >>..\LibScilab.def
 @echo ; %1 >>..\LibScilab.def
 @echo ; --------------------------------------- >>..\LibScilab.def
-@copy /Y ..\LibScilab.def+%1 ..\LibScilab.def >NUL
-
+@type %1 >> ..\LibScilab.def
 
 :EOF
