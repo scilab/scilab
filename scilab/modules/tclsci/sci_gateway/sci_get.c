3ef449b203679c456c33faf87c1462b3f5e025da
Allan Cornet <allan.cornet@scilab.org>
2006-11-02 13:40:33 +0000

    bug 2184

--- scilab/modules/tclsci/sci_gateway/sci_get.c
+++ scilab/modules/tclsci/sci_gateway/sci_get.c
@@ -15,9 +15,17 @@ int C2F(sci_TCL_get) _PARAMS((char *fname,unsigned long l))
 	{
 		static int l1,n1,m1;
 		int Handle=0;
+		char VarName[64];
 
 		GetRhsVar(Rhs-1,"i",&m1,&n1,&l1);
 		Handle=*istk(l1);
+
+		sprintf(VarName,"Win(%d)",Handle);
+		if ( ! Tcl_GetVar(TCLinterp, VarName, TCL_GLOBAL_ONLY) )
+		{
+			Scierror(999,TCL_ERROR27,Handle);
+			return 0;
+		}
 
 		TCL_UiGet(Handle,Rhs);
 		flushTKEvents ();
