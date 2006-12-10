936580038c3a21c7a601dfaaf51600ab64663035
Allan Cornet <allan.cornet@scilab.org>
2006-12-10 17:42:19 +0000

    indentation

--- scilab/modules/gui/sci_gateway/c/gw_gui.c
+++ scilab/modules/gui/sci_gateway/c/gw_gui.c
@@ -23,9 +23,9 @@ static GuiTable Tab[]=
 	{C2F(sci_unsetmenu_old),"unsetmenu_old"},
 	{C2F(sci_xgetfile),"xgetfile"},
 	{C2F(sci_x_message_modeless),"x_message_modeless"},
-        {sci_clc,"clc"},
-        {sci_show_window,"show_window"},
-        {sci_tohome,"tohome"}
+    {sci_clc,"clc"},
+    {sci_show_window,"show_window"},
+    {sci_tohome,"tohome"}
 };
 /*-----------------------------------------------------------------------------------*/
 int C2F(gw_gui)(void)
