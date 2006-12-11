e22f83cabe30b223c9a7567dd14d5496917c51ee
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2006-12-11 16:45:37 +0000

    Incremental link with autotools (draft)

---
+++ scilab/modules/incremental_link/src/scripts/compilerDetection.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+PROGNAME="$0"
+PATHTOCONFIGURE=`dirname "$PROGNAME"`
+cd $PATHTOCONFIGURE
+
+# Relaunch configure if files are missing
+if test ! -s Makefile.orig; then 
+	echo "Detection of C/C++/Fortran Compilers"
+	./configure
+	mv Makefile Makefile.orig	
+else 
+	echo "Detection already done"
+fi
---
+++ scilab/modules/incremental_link/src/scripts/scicompile.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+ORIGINALLIBNAME=libsciexternal
+
+if test ! -s Makefile.orig; then 
+	echo "The compiler detection has been performed. Please launch compilerDetection.sh before"
+	exit -1
+fi
+
+# Check number of parameters
+if test $# -lt 2; then
+	echo "Syntax : $0 libname sources"
+	exit -2
+fi
+
+# retrieve parameters
+LIB=$1
+shift
+SOURCES=$*
+
+# Check if files exist
+for file in $SOURCES; do
+	if test ! -s $file; then 
+		echo "Cannot find $file"
+		exit -3
+	fi
+done
+
+# Replace fake filenames to the one we want 
+sed -s "s|libsciexternal_la_SOURCES = foo.c foo2.f foo3.cxx|lib"$LIB"_la_SOURCES = $SOURCES|g" Makefile.orig > Makefile
+
+## Replace  sources by .lo file
+SOURCES=`echo $SOURCES|sed -s 's|\.c|\.lo|g'`
+SOURCES=`echo $SOURCES|sed -s 's|\.f|\.lo|g'`
+SOURCES=`echo $SOURCES|sed -s 's|\.cxx|\.lo|g'`
+
+### Changes objects in the source code
+sed -si "s|am_libsciexternal_la_OBJECTS = foo.lo foo2.lo foo3.lo|am_lib"$LIB"_la_OBJECTS = $SOURCES|g" Makefile
+
+### Changes objects in the source code
+sed -si "s|libsciexternal|lib"$LIB"|g" Makefile
+
