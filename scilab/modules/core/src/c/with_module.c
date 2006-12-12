b520ea27c0a4be9180114579a2b4a78f5e402b58
Allan Cornet <allan.cornet@scilab.org>
2006-12-12 07:27:38 +0000

    add with_module C function add ExceptionMessage in vcproj

---
+++ scilab/modules/core/src/c/with_module.c
@@ -0,0 +1,29 @@
+/*-----------------------------------------------------------------------------------*/
+/* INRIA 2006 */
+/* Allan CORNET */
+/*-----------------------------------------------------------------------------------*/
+#include <string.h>
+#include "with_module.h"
+#include "getmodules.h"
+/*-----------------------------------------------------------------------------------*/
+BOOL with_module(char *modulename)
+{
+	BOOL bOK=FALSE;
+	if (modulename)
+	{
+		struct MODULESLIST *Modules=getmodules();
+		int nrow = Modules->numberofModules;
+		int i = 0;
+
+		for(i=0;i<nrow;i++)
+		{
+			if ( strcmp(Modules->ModuleList[i],modulename) == 0)
+			{
+				return TRUE;
+			}
+		}
+	}
+	return bOK;
+
+}
+/*-----------------------------------------------------------------------------------*/
