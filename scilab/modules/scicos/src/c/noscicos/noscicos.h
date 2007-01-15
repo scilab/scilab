6c46ee5e1d6855682944c0222693e63434134022
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2007-01-15 11:45:49 +0000

    Warnings fixed

---
+++ scilab/modules/scicos/src/c/noscicos/noscicos.h
@@ -0,0 +1,9 @@
+/*-----------------------------------------------------------------------------------*/
+/* INRIA 2007 */
+/* Sylvestre LEDRU */
+/*-----------------------------------------------------------------------------------*/ 
+
+int C2F(gw_scicos)(void);
+int C2F(gw_cscicos)(void);
+int haltscicos(char *name, integer *win, integer *entry,long int name_len);
+BOOL ExistScicos(void);
