85ecc649e50395eda9ef86e859574d8f48119761
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2007-02-16 12:48:28 +0000

    Sun CC is complainy about the ';' after the function declaration (C++ style)

--- scilab/libs/hashtable/hashtable_private.h
+++ scilab/libs/hashtable/hashtable_private.h
@@ -24,14 +24,13 @@ struct hashtable {
 };
 
 /*****************************************************************************/
-unsigned int
-hash(struct hashtable *h, void *k);
+unsigned int hash(struct hashtable *h, void *k);
 
 /*****************************************************************************/
 /* indexFor */
 static unsigned int indexFor(unsigned int tablelength, unsigned int hashvalue) {
     return (hashvalue % tablelength);
-};
+}
 
 /* Only works if tablelength == 2^N */
 /*static inline unsigned int
