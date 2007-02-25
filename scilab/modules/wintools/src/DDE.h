b6ed98a3ac0afd376ffb9874f381f0f6690df3f0
Allan Cornet <allan.cornet@scilab.org>
2007-02-25 11:24:23 +0000

    correction include

--- scilab/modules/wintools/src/DDE.h
+++ scilab/modules/wintools/src/DDE.h
@@ -3,11 +3,9 @@
 /* INRIA 2004 */
 /**********************************************************************************/
 #ifndef __DDE__
-
 #define __DDE__
-#include "windows.h"
+#include <windows.h>
 /**********************************************************************************/
-HDDEDATA CALLBACK DdeCallback(UINT uType,UINT uFmt,HCONV hconv,HSZ hsz1,HSZ hsz2,HDDEDATA hdata,DWORD dwData1,DWORD dwData2);
 BOOL DDEExecute(char* szCommand,int TimeOut);
 BOOL DDERequest(char* szItem,int TimeOut,char *Result);
 BOOL DDEPoke(char* szItem, char* szData,int TimeOut);
