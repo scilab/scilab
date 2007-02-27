b9908fd6016b600de3f6abcea05eb00a85d72135
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2007-02-27 08:16:08 +0000

    Template for comment

--- scilab/modules/core/src/c/inisci-c.h
+++ scilab/modules/core/src/c/inisci-c.h
@@ -3,32 +3,130 @@
 /* Ledru Sylvestre */
 /*-----------------------------------------------------------------------------------*/ 
 
-
+/** 
+ * TODO : comment
+ * @param rep
+ * @return 
+ */
 int C2F(withpvm)(int *rep);
+
+/** 
+ * TODO : comment
+ * @param rep
+ * @return 
+ */
 int C2F(withgtk)(int *rep);
+
+/** 
+ * TODO : comment
+ * @param rep
+ * @return 
+ */
 int C2F(withscicos)(int *rep);
+
+/** 
+ * TODO : comment
+ * @param rep
+ * @return 
+ */
 int C2F(withmodelicac)(int *rep);
+
+/** 
+ * TODO : comment
+ * @param rep
+ * @return 
+ */
 int C2F(withjavasci)(int *rep);
+
+/** 
+ * TODO : comment
+ * @param rep
+ * @return 
+ */
 int C2F(withmsdos)(int *rep);
+
+/** 
+ * TODO : comment
+ * @param buf
+ * @param nbuf
+ * @param lbuf
+ * @return 
+ */
 int C2F(getcomp)(char *buf,int *nbuf,long int lbuf);
 
+
+/** 
+ * TODO : comment
+ * @return 
+ */
 int ExistJavaSciUnix(void);
+
+/** 
+ * TODO : comment
+ * @param ON
+ */
 void SetWITH_GUI(int ON);
+
+/** 
+ * TODO : comment
+ * @return 
+ */
 BOOL GetWITH_GUI(void);
+
+/** 
+ * TODO : comment
+ * @return 
+ */
 BOOL ExistModelicac(void);
+
+/** 
+ * TODO : comment
+ * @return 
+ */
 BOOL ExistScicos(void);
+
+/** 
+ * TODO : comment
+ * @return 
+ */
 BOOL ExistJavaSciWin(void);
+
+/** 
+ * TODO : comment
+ * @return 
+ */
 int SetSci(void);
 
+/** 
+ * TODO : comment 
+ * @param buf
+ * @param nbuf
+ * @param lbuf
+ * @return 
+ */
 int C2F(gettmpdir)(char *buf,int *nbuf,long int lbuf);
+
+/** 
+ * TODO : comment
+ * @param rep
+ * @return 
+ */
 int C2F(withgui)(int *rep);
+
 /**
  * Get the SCI path and initialize the scilab environment path
- *
+ * @param buf
+ * @param nbuf
+ * @param lbuf
+ * @return 
  */
 int C2F(getsci)(char *buf,int *nbuf,long int lbuf);
+
 /**
 * Get the SCIHOME path and initialize the scilab environment path
-*
+ * @param buf
+ * @param nbuf
+ * @param lbuf
+ * @return 
 */
 int C2F(getscihome)(char *buf,int *nbuf,long int lbuf);
