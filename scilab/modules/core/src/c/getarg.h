c10c42d93307ee4672efe0f1ff57caea7be00cbd
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2007-02-27 08:09:55 +0000

    template for comment

--- scilab/modules/core/src/c/getarg.h
+++ scilab/modules/core/src/c/getarg.h
@@ -3,5 +3,17 @@
 /* Sylvestre Ledru */
 /*-----------------------------------------------------------------------------------*/ 
 
+/**
+ * TODO : comment
+ * @param n
+ * @param str
+ * @param ln
+ * @return 
+ */
 int C2F(scigetarg)(int *n,char *str,long int ln);
+
+/** 
+ * TODO : comment 
+ * @return 
+ */
 int C2F(sciiargc)(void);
