1e8c11921bdd720ae1484320630aa9eec71aebff
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2007-03-29 08:06:16 +0000

    With comments

--- scilab/modules/core/includes/sciprint.h
+++ scilab/modules/core/includes/sciprint.h
@@ -10,12 +10,37 @@
 
 /* any string of length greater than MAXPRINTF gets truncated in sciprint */
 #define MAXPRINTF 512
+
 /* MAXCHARSSCIPRINT_FULL is for sciprint_full - more than this gets truncated */
 #define MAXCHARSSCIPRINT_FULL 5000  
 
-void  sciprint(char *fmt,...) ;
-int  sciprint2(int iv,char *fmt,...);
-void  sciprint_full(char *fmt,...) ;
+/**
+ * Print
+ * @param fmt Format of the format string
+ * @param ...
+ */
+void sciprint(char *fmt,...) ;
+
+/**
+ * Print
+ * works like as sciprint but with an added first argument which is 
+ * ignored (used in do_printf) 
+ * @see sciprint
+ * @param iv
+ * @param fmt Format of the format string
+ * @param ...
+ * @return return the number of characters printed
+ */
+int sciprint2(int iv,char *fmt,...);
+
+/**
+ * sciprint geared towards long strings (>MAXPRINTF) 
+ * the long string is splitted in elements of length 
+ * equal to the number of columns from lines()                   
+ * @param fmt Format of the format string
+ * @param ...
+ */
+void sciprint_full(char *fmt,...) ;
 
 #endif /* __SCIPRINT_H__ */
 /*-----------------------------------------------------------------------------------*/ 
