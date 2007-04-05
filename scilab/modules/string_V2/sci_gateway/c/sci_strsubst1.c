f2155e6b72d453e0f4c0c4eb37a761e813220fd5
Cong Wu <cong.wu@inria.fr>
2007-04-05 12:31:44 +0000

    fix some warning

--- scilab/modules/string_V2/sci_gateway/c/sci_strsubst1.c
+++ scilab/modules/string_V2/sci_gateway/c/sci_strsubst1.c
@@ -20,9 +20,11 @@
 int next[20];
 int numRow;
 int numCol;
-char *newstr1;
+char *_replacedstr;
 /*-------------------------------------------------------------------------------------*/
 
+void getnext(char T[],int *next);
+int kmp(char S[],char T[],int pos);
 
 
 
@@ -30,8 +32,7 @@ char *newstr1;
 
 
 
-
-static char* newstr(const char*str, unsigned start, unsigned end, char*newstr1)
+static char* newstr(const char*str, unsigned start, unsigned end, char*_replacedstr1)
 
 {
 
@@ -49,8 +50,8 @@ for (i=0;i<256;++i) {
    stbuf4[i]=0;
 }
 strncpy(stbuf1, str , start);
-strncpy(stbuf2, newstr1,strlen(newstr1));
-strncpy(stbuf3, str+end ,strlen(str)-n+strlen(newstr1) );
+strncpy(stbuf2, _replacedstr1,strlen(_replacedstr1));
+strncpy(stbuf3, str+end ,strlen(str)-n+strlen(_replacedstr1) );
 j=0;
 for (i=0;i<strlen(stbuf1);i++) stbuf4[j++]=stbuf1[i];
 for (i=0;i<strlen(stbuf2);i++) stbuf4[j++]=stbuf2[i];
@@ -67,22 +68,22 @@ int C2F(sci_strsubst1) _PARAMS((char *fname,unsigned long fname_len))
 char typ = '*';
 
   char **Str,**Str2,**Str3;
-  static char def_sep[] ="";
-  char *sep = def_sep;
-  static int un=1;
-  int x,m1,n1,mn,mn2,mn3,m2,n2,m3,n3,m4,n4,l4,nchars,i=0;
+  
+  
+  
+  int x,m1,n1,mn,mn2,mn3,m2,n2,m3,n3,m4,n4,l4=0;
   unsigned x1;
-  int lenth=0;
-  int lenthrow=1;
-  int lenthcol=1;
-  int outIndex = 0 ;
+  
+  
+  
+  
   char ebuf[128];
   char *lbuf;
   const size_t nmatch = 10;
   char *pattern;
   int w;
   int pos=0;
-  int  z, lno = 0, cflags = 0;
+  int  z, cflags = 0;
   regmatch_t pm[10];
   regex_t *out1;
   Rhs = Max(0, Rhs);
@@ -117,9 +118,9 @@ char typ = '*';
 					z = regexec(out1, Str[x], nmatch, pm, 0);
 					if (z == REG_NOMATCH) { 
 						int outIndex2= Rhs +x+1 ;
-						int numRow   = 1 ;
-						int numCol   = 1 ;
 						int loutIndex = 0 ;
+						numCol  = 1;
+                  numRow   = 1;
 						CreateVar(Rhs+1+x,"c",&numRow,&numCol,&loutIndex);
   						LhsVar(x+1) = outIndex2 ;
 						continue;
@@ -127,8 +128,8 @@ char typ = '*';
 					for (x1 = 0; x1 < nmatch && pm[x1].rm_so != -1; ++ x1) {
 					
 						
-                        newstr1=newstr(Str[x], pm[x1].rm_so, pm[x1].rm_eo,*Str3);
-                        strcpy(Str[x],newstr1);
+                        _replacedstr=newstr(Str[x], pm[x1].rm_so, pm[x1].rm_eo,*Str3);
+                        strcpy(Str[x],_replacedstr);
 					}    
 			
               }
@@ -150,10 +151,10 @@ char typ = '*';
 			 CreateVar(Rhs+1+x,"c",&numRow,&numCol,&loutIndex);
 			 strncpy(cstk(loutIndex),newstr(Str[x], w-1, w+strlen(Str2[0])-1,*Str3), numCol);
 			 LhsVar(x+1) = outIndex2 ;*/
-             newstr1=newstr(Str[x], w-1, w+strlen(Str2[0])-1,*Str3);
-			//*(Str[x])=*newstr1;
-			 strcpy(Str[x],newstr1);
-			 // for(i=0;i<strlen(newstr1);++i) Str[x][i]=newstr1[i];
+             _replacedstr=newstr(Str[x], w-1, w+strlen(Str2[0])-1,*Str3);
+			//*(Str[x])=*_replacedstr;
+			 strcpy(Str[x],_replacedstr);
+			 // for(i=0;i<strlen(_replacedstr);++i) Str[x][i]=_replacedstr[i];
 			// Str[x][i]='/0';
 		
 		
