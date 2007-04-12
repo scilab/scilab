1be3839e41238447d226753038f1e4e55ca7fb5f
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2007-04-12 12:57:35 +0000

    comment with the right syntax

--- scilab/modules/pvm/src/c/varpack.c
+++ scilab/modules/pvm/src/c/varpack.c
@@ -33,34 +33,36 @@ static void F2C(bsppak)(int *il, int *pack, int *n, int *nmax);
 static void C2F(libpak)(int *il, int *pack, int *np, int *npMax);
 static int pak_object_info (int ilk,int stk_pos, int *pack, int *np, int *npmax);
 
-/*-----------------------------------------------------------------
+/** 
  *
- *     Given a scilab variable, stored in the stack at the position k (in 
- *     lstk) this function returns a "packing" vector pack for pvm. 
- *     A scilab variable is a consecutive memory region formed by a 
- *     succession of int and double precision vectors. 
- *     pack contains a sequence of pairs of ints. 
- *     varpak returns a vector which contains the size of this int 
- *     and double precision vectors. pack contains a sequence of pairs of 
- *     ints:  pack=[i1,r1,i2,r2,...,in,rn] 
- *     il is a number of int words 
- *     rl is a number of double precision word 
- *     n    : is the returned length of pack 
- *     nMax : is the Maximum size allowed for pack  
+ * Given a scilab variable, stored in the stack at the position k (in 
+ * lstk) this function returns a "packing" vector pack for pvm. 
+ * A scilab variable is a consecutive memory region formed by a 
+ * succession of int and double precision vectors. 
+ * n must be initialized before calling pak_object_info
+ * varpak returns a vector which contains the size of this int 
+ * and double precision vectors. pack contains a sequence of pairs of 
+ * ints:  pack=[i1,r1,i2,r2,...,in,rn] 
+ * il is a number of int words 
+ * rl is a number of double precision word 
+ * @param k    : position in lstk
+ * @param pack : contains a sequence of pairs of ints. 
+ * @param n    : is the returned length of pack 
+ * @param nMax : is the Maximum size allowed for pack  
+ * @param ierr : 0 OK 
+ * @param ierr : 1 nMax is not enough 
+ * @param ierr : 2 unknown data type 
+ * @return 
  *-----------------------------------------------------------------*/ 
 
 int C2F(varpak)(int *k, int *pack, int *n, int *nMax, int *ierr) {
-  *n = 0; /* n must be initialized before calling pak_object_info */
-  /* ierr = 0 OK 
-   * ierr = 1 nMax is not enough 
-   * ierr = 2 unknown data type 
-   */
+  *n = 0; 
   *ierr= pak_object_info(*k,1,pack,n,nMax);
   return 0;
 }
 
 
-static int  pak_object_info(int ilk,int stk_pos, int *pack, int *np, int *nMax)
+static int pak_object_info(int ilk,int stk_pos, int *pack, int *np, int *nMax)
 {
   int type,ne,il,ilp,i,li,ill,l,kp;
 
