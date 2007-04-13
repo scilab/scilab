0c9fac8defa4435692de5c3e95c87794698685b2
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2007-04-13 16:30:36 +0000

    comment

--- scilab/modules/core/includes/loadversion.h
+++ scilab/modules/core/includes/loadversion.h
@@ -4,6 +4,17 @@
 /*-----------------------------------------------------------------------------------*/ 
 #include "machine.h"
 /*-----------------------------------------------------------------------------------*/ 
+/**
+ * Retrieve the version of the module from the XML description file 
+ *
+ * @param[in] modulename name of the module
+ * @param[out] sci_version_major 
+ * @param[out] sci_version_minor 
+ * @param[out] sci_version_maintenance   
+ * @param[out] sci_version_string    
+ * @param[out] sci_version_revision  
+ * @return if the function has been able to load the informations
+ */
 BOOL getversionmodule(char *modulename,
 					  int *sci_version_major,
 					  int *sci_version_minor,
