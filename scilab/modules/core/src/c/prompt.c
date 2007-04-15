0344febca303b150233028072a0a3d771c984546
Allan Cornet <allan.cornet@scilab.org>
2007-04-15 15:49:40 +0000

    add a message when you are at prompt level 1 -1-> for the first time
    -->
    
    Type 'resume' or 'abort' to return to standard level prompt.
    
    -1->

--- scilab/modules/core/src/c/prompt.c
+++ scilab/modules/core/src/c/prompt.c
@@ -5,19 +5,32 @@
 #include <string.h>
 #include "machine.h"
 #include "prompt.h"
+#include "message_scilab.h"
+#include "sciprint.h"
+#include "warningmode.h"
 /*------------------------------------------------------------------------*/
 static char Sci_Prompt[10];
+static BOOL dispWarningLevelPrompt=TRUE;
 /*------------------------------------------------------------------------*/
 /* setprlev : set the current prompt string */
 /*------------------------------------------------------------------------*/
 void C2F(setprlev)( int *pause)
 {
-	if ( *pause == 0 ) 
-		sprintf(Sci_Prompt,SCIPROMPT);
+	if ( *pause == 0 ) sprintf(Sci_Prompt,SCIPROMPT);
 	else if ( *pause > 0 )
+	{
+		if (dispWarningLevelPrompt)
+		{
+			if (getWarningMode())
+			{
+				message_scilab("core_message_186");
+				sciprint("\n");
+				dispWarningLevelPrompt=FALSE;
+			}
+		}
 		sprintf(Sci_Prompt,SCIPROMPT_INTERRUPT,*pause);
-	else
-		sprintf(Sci_Prompt,SCIPROMPT_PAUSE);
+	}
+	else sprintf(Sci_Prompt,SCIPROMPT_PAUSE);
 }
 /*------------------------------------------------------------------------*/
 void GetCurrentPrompt(char *CurrentPrompt)
