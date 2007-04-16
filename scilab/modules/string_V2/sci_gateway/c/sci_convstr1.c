27d63d77a9230e44ea3c420b81e262cf1e287870
Cong Wu <cong.wu@inria.fr>
2007-04-16 16:34:56 +0000

    removeed all the empty lines

--- scilab/modules/string_V2/sci_gateway/c/sci_convstr1.c
+++ scilab/modules/string_V2/sci_gateway/c/sci_convstr1.c
@@ -12,50 +12,26 @@
 #include "machine.h"
 #include "stack-c.h"
 #include "MALLOC.h" 
-
 int numRow;
 int numCol;
-
 /*-------------------------------------------------------------------------------------*/
-
-
-/*-----------------------------------------------------------------------------------*/
 
 /*-----------------------------------------------------------------------------------*/
 int C2F(sci_convstr1) _PARAMS((char *fname,unsigned long fname_len))
 {
-
-
   char **Str,**Str3;
   char typ = '*';
-
-
-  
-  
-  
   int x,y,m1,n1,mn,m2,n2,mn3,i,l2,m4,n4,l4=0;
-
   unsigned *m;
-  
-  
-  
-
-
-
-
-  
   Rhs = Max(0, Rhs);
   CheckRhs(1,4);
-
   switch ( VarType(1)) {
   case 10 :
     GetRhsVar(1,"S",&m1,&n1,&Str);
-	
     mn = m1*n1;  
     GetRhsVar(2,"c",&m4,&n4,&l4);
     if ( m4*n4 != 0) 
 	     typ = cstk(l4)[0]; 
-
 	Str3=(char**)MALLOC(sizeof(char*)*(m1*n1));
 	for (i=0;i<m1*n1;i++)
 	{
@@ -63,7 +39,6 @@ int C2F(sci_convstr1) _PARAMS((char *fname,unsigned long fname_len))
 		strcpy(Str3[i],"");
 	}
 	for (x=0;x<mn;x++){
-        
 		for (y=0;y<strlen(Str[x]);y++){
 			if (typ=='u') {
 				if    ( Str[x][y]>=97 && Str[x][y]<=122 ) 
@@ -76,21 +51,13 @@ int C2F(sci_convstr1) _PARAMS((char *fname,unsigned long fname_len))
                 else Str3[x][y]=Str[x][y];
 			}
 		}
-
         Str3[x][y]=0;
 	}
-
 	numRow   = m1;
     numCol   = n1 ;
-	
     CreateVarFromPtr( Rhs+1, "S", &numRow, &numCol, Str3 ) ;
 	LhsVar(1) = Rhs+1 ; 
-    
-
-
-
-
-  C2F(putlhsvar)();
+    C2F(putlhsvar)();
 
   }
     return 0;
