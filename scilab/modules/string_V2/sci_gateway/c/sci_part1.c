bc5aea4960341c05f9e376cd15af2a05477068bc
Cong Wu <cong.wu@inria.fr>
2007-04-16 13:41:43 +0000

    added part1

---
+++ scilab/modules/string_V2/sci_gateway/c/sci_part1.c
@@ -0,0 +1,85 @@
+/*------------------------------------------------------------------------*/
+/* File: sci_strsubst1.c                                                  */
+/* Copyright INRIA 2007                                                   */
+/* Authors : Cong Wu                                                      */
+/* desc : substitute a character string by another in a character string
+          using regular express .                                         */
+/*------------------------------------------------------------------------*/
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h> 
+#include "gw_string.h"
+#include "machine.h"
+#include "stack-c.h"
+#include "MALLOC.h" 
+
+int numRow;
+int numCol;
+
+/*-------------------------------------------------------------------------------------*/
+
+
+/*-----------------------------------------------------------------------------------*/
+
+/*-----------------------------------------------------------------------------------*/
+int C2F(sci_part1) _PARAMS((char *fname,unsigned long fname_len))
+{
+
+
+  char **Str,**Str3;
+
+
+  
+  
+  
+  int x,y,m1,n1,mn,m2,n2,mn3,i,l2=0;
+
+  unsigned *m;
+  
+  
+  
+
+
+
+
+  
+  Rhs = Max(0, Rhs);
+  CheckRhs(1,4);
+
+  switch ( VarType(1)) {
+  case 10 :
+    GetRhsVar(1,"S",&m1,&n1,&Str);
+	
+    mn = m1*n1;  
+	GetRhsVar(2,"i",&m2,&n2,&l2);
+    m=istk(l2); 
+
+	Str3=(char**)MALLOC(sizeof(char*)*(m1*n1));
+	for (i=0;i<m1*n1;i++)
+	{
+		Str3[i]=(char*)MALLOC(sizeof(char*)*(1));
+		strcpy(Str3[i],"");
+	}
+	for (x=0;x<n1;x++){
+        mn3=0;
+		for (y=0;y<n2;y++)
+			 if (m[y]-1<=strlen(Str[x])) Str3[x][mn3++]=Str[x][m[y]-1]		;
+	}
+
+	numRow   = m1 ;
+    numCol   = n1 ;
+	
+    CreateVarFromPtr( Rhs+1, "S", &numRow, &numCol, Str3 ) ;
+	LhsVar(1) = Rhs+1 ; 
+    
+
+
+
+
+  C2F(putlhsvar)();
+
+  }
+    return 0;
+}
+
+/*-----------------------------------------------------------------------------------*/
