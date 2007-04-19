ea22d1bca126e6fc5e19adfc34300d58e64cd753
Cong Wu <cong.wu@inria.fr>
2007-04-19 07:59:56 +0000

    added some information of the  comments and remove the empty line

--- scilab/modules/string_V2/sci_gateway/c/sci_convstr1.c
+++ scilab/modules/string_V2/sci_gateway/c/sci_convstr1.c
@@ -1,9 +1,13 @@
 /*------------------------------------------------------------------------*/
-/* File: sci_strsubst1.c                                                  */
+/* File: sci_convstr1.c                                                   */
 /* Copyright INRIA 2007                                                   */
 /* Authors : Cong Wu                                                      */
-/* desc : substitute a character string by another in a character string
-          using regular express .                                         */
+/* desc : converts the matrix of strings  str-matrix  into lower case (for*/  
+/*          "l"  ;default value) or upper case (for  "u" ).               */
+/*        [y]=convstr(str-matrix, ["flag"])
+/*              str-matrix, y : matrices of strings                       */
+/*              "flag"  : string ( "u"  for upper or  "l"  for lower 
+/*                        (default value))                                */
 /*------------------------------------------------------------------------*/
 #include <string.h>
 #include <stdio.h>
@@ -21,8 +25,7 @@ int C2F(sci_convstr1) _PARAMS((char *fname,unsigned long fname_len))
 {
   char **Str,**Str3;
   char typ = '*';
-  int x,y,m1,n1,mn,m2,n2,mn3,i,l2,m4,n4,l4=0;
-  unsigned *m;
+  int x,y,m1,n1,mn,i,m4,n4,l4=0;
   Rhs = Max(0, Rhs);
   CheckRhs(1,4);
   switch ( VarType(1)) {
@@ -40,12 +43,12 @@ int C2F(sci_convstr1) _PARAMS((char *fname,unsigned long fname_len))
 	}
 	for (x=0;x<mn;x++){
 		for (y=0;y<strlen(Str[x]);y++){
-			if (typ=='u') {
+			if (typ=='u') {     /*converts the matrix of strings  str-matrix  into upper case */
 				if    ( Str[x][y]>=97 && Str[x][y]<=122 ) 
             			Str3[x][y]=Str[x][y]-32		;
 				else Str3[x][y]=Str[x][y];
 			}
-			else if (typ=='l') { 
+			else if (typ=='l') { /*converts the matrix of strings  str-matrix  into lower case */
 				if    ( Str[x][y]>=65 && Str[x][y]<=90 ) 
             			Str3[x][y]=Str[x][y]+32		;
                 else Str3[x][y]=Str[x][y];
@@ -53,12 +56,12 @@ int C2F(sci_convstr1) _PARAMS((char *fname,unsigned long fname_len))
 		}
         Str3[x][y]=0;
 	}
-	numRow   = m1;
+	numRow   = m1;  /*Output */
     numCol   = n1 ;
     CreateVarFromPtr( Rhs+1, "S", &numRow, &numCol, Str3 ) ;
 	LhsVar(1) = Rhs+1 ; 
     C2F(putlhsvar)();
-
+    FREE(Str3);
   }
     return 0;
 }
