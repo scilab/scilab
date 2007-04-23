d72e26c31dbd6063b3b0f476969c719c309bd9e4
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2007-04-23 07:57:39 +0000

    no  newline & comment

--- scilab/modules/integer/src/c/mgeti.h
+++ scilab/modules/integer/src/c/mgeti.h
@@ -7,8 +7,24 @@
 
 #include "machine.h"
 
+/**
+ * <long-description>
+ *
+ * @param char type[]   
+ * @param fd    
+ * @return <ReturnValue>
+ */
 int SWAP(char type[],integer *fd); 
 
+/**
+ * <long-description>
+ *
+ * @param fd    
+ * @param res   
+ * @param n 
+ * @param char type[]   
+ * @param ierr  
+ */
 void C2F(mgeti) (integer *fd,integer *res,integer *n,char type[],integer *ierr);
 
 #endif /* __MGETI_H__ */
--- scilab/modules/integer/src/c/mputi.h
+++ scilab/modules/integer/src/c/mputi.h
@@ -7,6 +7,15 @@
 
 #include "machine.h"
 
+/**
+ * TODO: add comment
+ *
+ * @param fd    
+ * @param res   
+ * @param n 
+ * @param char type[]   
+ * @param ierr  
+ */
 void C2F(mputi) (integer *fd,integer *res,integer *n,char type[],integer *ierr);
 
 #endif /* __MPUTI_H__ */
