123ae195aa0480181a7dd23eada22c408c148a29
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2007-04-23 07:50:17 +0000

    no  newline & comment

--- scilab/modules/fileio/includes/addfile.h
+++ scilab/modules/fileio/includes/addfile.h
@@ -9,9 +9,16 @@
 #include "machine.h"
 
 /**
-* add a file in the files table
-*/
-
+ *add a file in the files table
+ *
+ * @param fd    
+ * @param fa    
+ * @param swap2 
+ * @param type  
+ * @param mode  
+ * @param filename  
+ * @param ierr  
+ */
 void C2F(addfile)(integer *fd, FILE *fa, integer *swap2, integer *type, integer *mode, char *filename, integer *ierr);
 
 #endif /*  __ADDFILE_H__ */
