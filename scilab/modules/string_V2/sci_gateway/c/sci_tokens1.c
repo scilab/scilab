691a80a82ca39edcf2120533f749d936ee44137b
Cong Wu <cong.wu@inria.fr>
2007-04-24 14:32:43 +0000

    added a new function

--- scilab/modules/string_V2/sci_gateway/c/sci_tokens1.c
+++ scilab/modules/string_V2/sci_gateway/c/sci_tokens1.c
@@ -1,9 +1,14 @@
 /*------------------------------------------------------------------------*/
-/* File: sci_strindex1.c                                                  */
+/* File: sci_tokens1.c                                                    */
 /* Copyright INRIA 2007                                                   */
 /* Authors : Cong Wu                                                      */
-/* desc : search position of a character string in an other string
-          using regular express .                                         */
+/* desc : returns the tokens of a character string.  
+          T=tokens(str [,delimiter])   
+		  str : a character string. The string where to search the tokens. 
+          delimiter : (optional) a character or a vector of characters. 
+		              The tokens delimeters. 
+		  T : column vector of found tokens 
+		                                                                  */
 /*------------------------------------------------------------------------*/
 #include <string.h>
 #include <stdio.h>
@@ -11,133 +16,67 @@
 #include <string.h>
 #include "gw_string.h"
 #include "machine.h"
-
 #include "stack-c.h"
 #include "returnProperty.h"
 #include "machine.h"
 #include "MALLOC.h" 
-
-
 /*-----------------------------------------------------------------------------------*/
-
 /*-----------------------------------------------------------------------------------*/
 int C2F(sci_tokens1) _PARAMS((char *fname,unsigned long fname_len))
 {
-//char typ = '*';
-//
-//  char **Str,**Str2;
-//  static char def_sep[] ="";
-//  char *sep = def_sep;
-//  static int un=1;
-//  int x1,m1,n1,mn,mn2,i,j,k,m2,n2,nchars=0;
-//  unsigned x;
-//  int lenth=0;
-//  int boolen;
-//  int outIndex = 0 ;
-//  char ebuf[128];
-//
-//
-//  int  z, lno = 0, cflags = 0;
-//  char *values[100];
-//  int colValues=0;
-//  int rowValues=0;
-//
-//  int numRow   = 1 ;
-//  int numCol   = 1 ;
-// 
-//  regmatch_t pm[10];
-//
-//
-//
-//  regex_t **out1;
-//  Rhs = Max(0, Rhs);
-//
-//  CheckRhs(1,2);
-//
-//  
-//  switch ( VarType(1)) {
-//  case 10 :
-//    GetRhsVar(1,"S",&m1,&n1,&Str);
-//    mn = m1*n1;  
-//	GetRhsVar(2,"S",&m2,&n2,&Str2);
-//    mn2 = m2*n2;  
-//
-//	for (i=0;i<mn;++i){
-//		if (Str[0][i]==Str2[0][0]) { 
-//			colValues++;
-//			rowValues=0;
-//		}
-//		else {
-//
-//    	values[colValues][rowValues++]=Str[0][i];
-//		}
-//	}
-//
-//   
-//
-//	
-//	
-//		
-//	
-//	
-//	
-//	for (x=0;x<mn2;++x){
-//		z = regexec(out1[x], Str[0], nmatch, pm, 0);
-//		if (z == REG_NOMATCH) { 
-//			int outIndex2= Rhs +x+1 ;
-//			int numRow   = 1 ;
-//            int numCol   = 1 ;
-//            int outIndex = 0 ;
-//            CreateVar(Rhs+1+x,"c",&numRow,&numCol,&outIndex);
-//  			LhsVar(x+1) = outIndex2 ;
-//			
-//			continue;
-//		}
-//		for (x1 = 0; x1 < nmatch && pm[x1].rm_so != -1; ++ x1) {
-//		
-//          
-//			values[nbValues++]=pm[x1].rm_so+1;
-//			position[nbposition++]=x+1;
-//		
-//
-//			
-//			
-//
-//        }     
-//
-//	}
-//
-//		numRow   = 1        ;
-//        outIndex = 0        ;
-//  
-//        CreateVar(Rhs+1,"d",&numRow,&nbValues,&outIndex) ;
-//        for ( i = 0 ; i < nbposition ; i++ )
-//  {
-//    stk(outIndex)[i] = (double)values[i] ;
-//  }
-//		LhsVar(1) = Rhs+1 ;
-//
-//
-//		numRow   = 1        ;
-//        outIndex = 0        ;
-//  
-//        CreateVar(Rhs+2,"d",&numRow,&nbposition,&outIndex) ;
-//        for ( i = 0 ; i < nbposition ; i++ )
-//  {
-//    stk(outIndex)[i] = (double)position[i] ;
-//  }
-//		LhsVar(2) = Rhs+2;	
-//		
-//
-//
-//
-//    
-//   
-//  
-//  }
-  
-  C2F(putlhsvar)();
-	return 0;
+
+  char **Str,**Str2,**Str3;
+  int x,y,m1,n1,m2,n2,mn2,mn,i,m4,n4,l4=0;
+  int u=0;
+  int w=0;
+  int numRow;
+  int numCol;
+  int bo=0;
+  Rhs = Max(0, Rhs);
+  CheckRhs(1,2);
+  switch ( VarType(1)) {
+  case 10 :
+    GetRhsVar(1,"S",&m1,&n1,&Str);
+    mn = m1*n1;  
+	if (Rhs == 2) {
+		GetRhsVar(2,"S",&m2,&n2,&Str2);
+		mn2=m2*n2;
+	}
+	else { 
+		Str2=(char**)MALLOC(sizeof(char*)*(20*20));
+		Str2[0]=(char*)MALLOC(sizeof(char*)*(1));
+		strcpy(Str2[0],"");
+		Str2[0][0]=32;
+		m2=1;
+		n2=1;
+		mn2=1;
+	}
+	Str3=(char**)MALLOC(sizeof(char*)*(20*20));
+	for (i=0;i<20*20;i++)
+	{
+		Str3[i]=(char*)MALLOC(sizeof(char*)*(1));
+		strcpy(Str3[i],"");
+	}
+	for (x=0;x<mn;x++){
+		for (y=0;y<strlen(Str[x]);y++){
+				for (i=0; i<mn2; i++)  if (Str[x][y]==Str2[i][0]) bo=1;
+			    if    (bo==0) Str3[u][w++]=Str[0][y];
+				else {
+					Str3[u][w]=0;
+					u++;
+					w=0;
+				}
+				bo=0;
+		}
+	}
+	numRow   = u+1;  /*Output */
+    numCol   = 1 ;
+    CreateVarFromPtr( Rhs+1, "S", &numRow, &numCol, Str3 ) ;
+	LhsVar(1) = Rhs+1 ; 
+    C2F(putlhsvar)();
+    FREE(Str3);
+  }
+    return 0;
 }
 
 /*-----------------------------------------------------------------------------------*/
