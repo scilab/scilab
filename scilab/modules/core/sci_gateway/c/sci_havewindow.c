71892aec7cd1aa2dc0904972a3cec0e0adf17ad1
Allan Cornet <allan.cornet@scilab.org>
2007-04-25 06:38:13 +0000

    replace sci_havewindow.f --> sci_havewindow.c

--- scilab/modules/core/sci_gateway/c/sci_havewindow.c
+++ scilab/modules/core/sci_gateway/c/sci_havewindow.c
@@ -5,12 +5,28 @@
 #include "gw_core.h"
 #include "machine.h"
 #include "stack-c.h"
-/*-----------------------------------------------------------------------------------*/
-extern int C2F(inthavewindow)();
+#include "MALLOC.h"
+#include "scilabmode.h"
 /*-----------------------------------------------------------------------------------*/
 int C2F(sci_havewindow) _PARAMS((char *fname,unsigned long fname_len))
 {
-	C2F(inthavewindow)();
+	static int n1,m1;
+	int *Status=NULL;
+
+	CheckRhs(0,1);
+	CheckLhs(1,1);
+
+	Status=(int*)MALLOC(sizeof(int));
+	*Status = (int) ( getScilabMode() == SCILAB_STD );
+
+	m1=1;n1=1;
+	CreateVarFromPtr(Rhs+1, "b", &n1, &n1, &Status);
+	LhsVar(1)=Rhs+1;
+
+	C2F(putlhsvar)();
+
+	if (Status) {FREE(Status);Status=NULL;}
+
 	return 0;
 }
 /*-----------------------------------------------------------------------------------*/
