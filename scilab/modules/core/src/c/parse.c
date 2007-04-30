1f7cff18452da24b75c2aa1601e5be0e2544bc9c
Francois Vogel <fvogelnew1@free.fr>
2007-04-30 18:55:35 +0000

    Fix bug 2384

--- scilab/modules/core/src/c/parse.c
+++ scilab/modules/core/src/c/parse.c
@@ -1550,7 +1550,7 @@ int C2F(syncexec)(char *str, int *ns, int *ierr, int *seq, long int str_len)
 
   Pt = max(Pt,0);Pts=Pt;
   Top = max(Top,0);Tops=Top;
-  C2F(basbrk).interruptible = *seq != 0;
+  C2F(basbrk).interruptible = *seq == 0;
   C2F(bexec)(str, ns, ierr, (*ns));
   if (*ierr != 0) {
     goto L9998;
@@ -1685,13 +1685,10 @@ int C2F(syncexec)(char *str, int *ns, int *ierr, int *seq, long int str_len)
   *ierr = 0;
   C2F(recu).icall = 0;
   C2F(com).fin = 3;
-  C2F(basbrk).interruptible = TRUE_;
   return 0;
  L9998:
   *ierr = 1;
   Pt=Pts;Top=Tops;
-  C2F(basbrk).interruptible = TRUE_;
-
   return 0;
  L9999:
   /* Err == 9999999 arises if abort has been used to terminate the callback execution */
@@ -1699,7 +1696,6 @@ int C2F(syncexec)(char *str, int *ns, int *ierr, int *seq, long int str_len)
   --Top;
   --C2F(recu).niv;
   Pt=Pts;Top=Tops;
-  C2F(basbrk).interruptible = TRUE_;
   return 0;
 } /* syncexec */
 
