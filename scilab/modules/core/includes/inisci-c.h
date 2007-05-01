a5deaf5e9e0147a5db628309f87d02ac02f3c447
Allan Cornet <allan.cornet@scilab.org>
2007-05-01 16:22:53 +0000

    move C2F(withtk) in tclsci

--- scilab/modules/core/includes/inisci-c.h
+++ scilab/modules/core/includes/inisci-c.h
@@ -19,13 +19,6 @@ int C2F(withpvm)(int *rep);
  * @param rep
  * @return 
  */
-int C2F(withtk)(int *rep);
-
-/** 
- * TODO : comment
- * @param rep
- * @return 
- */
 int C2F(withgtk)(int *rep);
 
 /** 
