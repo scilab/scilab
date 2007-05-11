30877e0230c1b79d7b3f26960a9798343f1e39bf
Allan Cornet <allan.cornet@scilab.org>
2007-05-11 20:44:39 +0000

    add eclipse project for javasci
    checkstyle on ClassPath.java & SciAbstractArray.java

--- scilab/modules/javasci/src/java/javasci/ClassPath.java
+++ scilab/modules/javasci/src/java/javasci/ClassPath.java
@@ -1,56 +1,79 @@
-/*-----------------------------------------------------------------------------------*/
+/*--------------------------------------------------------------------------*/
 /**
-* Loading classes at runtime.
-*/
-/*-----------------------------------------------------------------------------------*/
+ * Loading classes at runtime.
+ */
+/*--------------------------------------------------------------------------*/
 package javasci;
-/*-----------------------------------------------------------------------------------*/ 
-import java.lang.reflect.*;
-import java.io.*;
-import java.net.*;
-/*-----------------------------------------------------------------------------------*/ 
+/*--------------------------------------------------------------------------*/
+import java.lang.reflect.Method;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.net.URI;
+/*--------------------------------------------------------------------------*/
+/**
+ * ClassPath to overload java classpath.
+ */
 public class ClassPath {
- 
-private static final Class[] parameters = new Class[]{URL.class};
- 
-public static void addFile(String s) throws IOException {
-	File f = new File(s);
-	addFile(f);
+
+private static final Class[] Parameters = new Class[]{URL.class};
+
+/**
+ * add a filename to java classpath.
+ * @param String s a filename
+ * @throws IOException
+ */
+public static void addFile(final String s) throws IOException {
+File f = new File(s);
+addFile(f);
 }
-/*-----------------------------------------------------------------------------------*/ 
-public static void addFile(File f) throws IOException {
-	URI uri = f.toURI();
-	addURL(uri.toURL());
+/*--------------------------------------------------------------------------*/
+/**
+ * add a file to java classpath.
+ * @param File
+ */
+public static void addFile(final File f) throws IOException {
+	
+URI uri = f.toURI();
+addURL(uri.toURL());
 }
-/*-----------------------------------------------------------------------------------*/  
-public static void addURL(URL u) throws IOException {
-		
-	URLClassLoader sysloader = (URLClassLoader) ClassLoader.getSystemClassLoader();
-	Class sysclass = URLClassLoader.class;
- 
- getClassPath();
-	try {
-		Method method = sysclass.getDeclaredMethod("addURL", parameters);
-		method.setAccessible(true);
-		method.invoke(sysloader , new Object[] {u });
-	} catch (Throwable t) {
-		t.printStackTrace();
-		throw new IOException("Error, could not add URL to system classloader");
-	}
-	getClassPath();
+/*--------------------------------------------------------------------------*/
+/**
+ * add a URL to classpath.
+ * @param u URL
+ * @throws t 
+ */
+public static void addURL(final URL u) throws IOException {
+
+URLClassLoader sysloader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+Class sysclass = URLClassLoader.class;
+
+try {
+Method method = sysclass.getDeclaredMethod("addURL", Parameters);
+method.setAccessible(true);
+method.invoke(sysloader , new Object[] {u });
+} catch (Throwable t) {
+t.printStackTrace();
+throw new IOException("Error, could not add URL to system classloader");
 }
-/*-----------------------------------------------------------------------------------*/ 
+
+}
+/*--------------------------------------------------------------------------*/
+/**
+ * 
+ */
 public static String[] getClassPath() {
-	URLClassLoader sysloader = (URLClassLoader) ClassLoader.getSystemClassLoader();
-	URL path[] = sysloader.getURLs();
-	String[] paths = new String[path.length];
-	for (int i = 0; i < path.length; i++) {
-		paths[i] = path[i].getFile();
-		//System.out.println(paths[i]);
-    }
+	
+URLClassLoader sysloader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+URL[] path = sysloader.getURLs();
+String[] paths = new String[path.length];
+for (int i = 0; i < path.length; i++) {
+paths[i] = path[i].getFile();
+}
     return paths;
 }
-/*-----------------------------------------------------------------------------------*/ 
+/*--------------------------------------------------------------------------*/
 }
-/*-----------------------------------------------------------------------------------*/ 
+/*--------------------------------------------------------------------------*/
 
