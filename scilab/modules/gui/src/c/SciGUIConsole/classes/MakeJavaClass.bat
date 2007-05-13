f86df1c5ae0ba8dacd017b6dddf93ef9606a24b7
Allan Cornet <allan.cornet@scilab.org>
2007-05-13 07:27:07 +0000

    remove SciGUIConsole

--- scilab/modules/gui/src/c/SciGUIConsole/classes/MakeJavaClass.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-SET JAVAC="../../../../../jvm/bin/jdk/bin/javac.exe"
-rem Compilation des classes 
-
-%JAVAC% -classpath ../../../../../jvm/bin/SWT/SWT.jar -d ../../../../../jvm/classes SciGUIConsole.java
-
-@echo on
