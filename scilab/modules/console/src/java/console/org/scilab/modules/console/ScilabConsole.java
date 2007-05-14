d73c957a71057f6c89932354b23fa25500b56bfc
Allan Cornet <allan.cornet@scilab.org>
2007-05-14 06:36:31 +0000

    add src/java/console + package + project

---
+++ scilab/modules/console/src/java/console/org/scilab/modules/console/ScilabConsole.java
@@ -0,0 +1,14 @@
+package org.scilab.modules.console;
+/**
+ * Scilab Console
+ * @author Allan CORNET INRIA 2007
+  */
+public class ScilabConsole {
+	/**
+	 * Constructor ScilabConsole Class.
+	 */
+	public ScilabConsole() {
+	}
+}
+/*--------------------------------------------------------------------------*/
+
---
+++ scilab/modules/jvm/src/java/jvm/org/scilab/modules/jvm/Scilab.java
@@ -0,0 +1,17 @@
+package org.scilab.modules.jvm;
+/**
+ * Main Class for Scilab
+ * @author Allan CORNET INRIA 2007
+  */
+public class Scilab {
+	private int mode;
+	/**
+	 * Constructor Scilab Class.
+	 * @param mode Mode Scilab -NW -NWNI -STD -API
+	 */
+	public Scilab(int mode) {
+		this.mode = mode; 
+	}
+}
+/*--------------------------------------------------------------------------*/
+
