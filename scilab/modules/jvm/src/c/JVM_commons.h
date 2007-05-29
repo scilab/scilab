567ece71ed664671fcec6080a8305e5a20711447
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2007-05-29 12:57:36 +0000

    the path has changed

--- scilab/modules/jvm/src/c/JVM_commons.h
+++ scilab/modules/jvm/src/c/JVM_commons.h
@@ -10,6 +10,11 @@
 #ifdef _MSC_VER
 #define JRE_PATH "/java/jre"
 #else
+/* 
+TODO : 
+Change this part
+The Path of the JRE should be hardcoded for Linux/Unix. 
+*/
 #define JRE_PATH "/java/jre"
 #endif
 /*-----------------------------------------------------------------------------------*/ 
@@ -23,12 +28,19 @@
 #define DEFAULT_SCILAB_CLASSPATH "/bin" /* SCI/bin */
 #define DEFAULT_SCILAB_LIBRARYPATH "/bin" /* SCI/bin */
 #else
+/* 
+TODO :
+Change this part 
+Won't work in production... 
+1. this should not be hardcoded (this part should be delegated to the system)
+2. the path is not the same in the dev env & the installed env 
+*/
 #define DEFAULT_SCILAB_CLASSPATH "/.libs/" /* SCI/.libs */
 #define DEFAULT_SCILAB_LIBRARYPATH "/.libs/" /* SCI/.libs */
 #endif
 /*-----------------------------------------------------------------------------------*/ 
 #define USER_CLASSPATH "."
-#define JVM_CLASSPATH "/java/jar/modules/jvm.jar"
+#define JVM_CLASSPATH "/modules/jvm/jar/org.scilab.modules.jvm.jar"
 /*-----------------------------------------------------------------------------------*/ 
 #endif /*  __JVM_COMMONS_H__ */
 /*-----------------------------------------------------------------------------------*/ 
