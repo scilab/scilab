b988d1593fab03b85c68f2707c2d2dffd1827543
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2007-06-04 11:51:30 +0000

    Function to get and display an exception

---
+++ scilab/modules/jvm/includes/catchIfJavaException.h
@@ -0,0 +1,13 @@
+/*-----------------------------------------------------------------------------------*/
+/* INRIA 2007 */
+/* @author Sylvestre Ledru */
+/*-----------------------------------------------------------------------------------*/
+
+#include "machine.h"
+
+/**
+ * Check if there is an exception and print if it is the case
+ * @param errorMsg the error message which will be displayed if an exception is catched 
+ * @return 
+ */
+BOOL catchIfJavaException(char *errorMsg);
---
+++ scilab/modules/jvm/src/c/catchIfJavaException.c
@@ -0,0 +1,22 @@
+/*-----------------------------------------------------------------------------------*/
+/* INRIA 2007 */
+/* @author Sylvestre Ledru */
+/*-----------------------------------------------------------------------------------*/
+#include <jni.h>
+#include <string.h>
+#include "catchIfJavaException.h"
+
+
+BOOL catchIfJavaException(char *errorMsg) {
+	JNIEnv * currentENV = getScilabJNIEnv();
+
+	if ((*currentENV)->ExceptionCheck(currentENV) == JNI_TRUE)
+		{
+			printf(errorMsg);
+			fflush(NULL);
+			(*currentENV)->ExceptionDescribe(currentENV);
+			(*currentENV)->ExceptionClear(currentENV);
+			return FALSE;
+		}
+	return TRUE;
+}
