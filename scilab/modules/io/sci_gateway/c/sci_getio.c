51b7dfbd3d1c5710a7edf851ddcf00be1481867f
Allan Cornet <allan.cornet@scilab.org>
2007-06-13 13:25:27 +0000

    rewrite getio primitive .f -> .c

--- scilab/modules/io/sci_gateway/c/sci_getio.c
+++ scilab/modules/io/sci_gateway/c/sci_getio.c
@@ -1,14 +1,33 @@
 /*-----------------------------------------------------------------------------------*/
-/* INRIA 2006 */
+/* INRIA 2007 */
 /* Allan CORNET */
 /*-----------------------------------------------------------------------------------*/ 
 #include "gw_io.h"
-/*-----------------------------------------------------------------------------------*/
-extern int C2F(intgetio) _PARAMS((char *fname,unsigned long fname_len));
+#include "stack-c.h"
+#include "MALLOC.h"
 /*-----------------------------------------------------------------------------------*/
 int C2F(sci_getio) _PARAMS((char *fname,unsigned long fname_len))
 {
-	C2F(intgetio)(fname,fname_len);
+	int one = 1;
+	int four = 4;
+	int *values = NULL;
+
+	CheckRhs(0,0);
+	CheckLhs(0,1);
+
+	values = (int*)MALLOC(sizeof(int)*(one*four));
+
+	values[0] =  C2F(iop).rio;
+	values[1] =  C2F(iop).rte;
+	values[2] =  C2F(iop).wio;
+	values[3] =  C2F(iop).wte;
+
+	CreateVarFromPtr(Rhs+1,"i",&one,&four,&values);
+	LhsVar(1) = Rhs+1;
+	C2F(putlhsvar)();
+
+	if (values) {FREE(values);values = NULL;}
+
 	return 0;
 }
 /*-----------------------------------------------------------------------------------*/
