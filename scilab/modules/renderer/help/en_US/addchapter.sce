778edec15e078911182197ba8493e7a83fbd6350
Allan Cornet <allan.cornet@scilab.org>
2007-06-13 04:22:53 +0000

    add renderer module (not finished)

---
+++ scilab/modules/renderer/help/en_US/addchapter.sce
@@ -0,0 +1,10 @@
+// ====================================================================
+// help renderer module
+// Add chapter
+// Copyright INRIA 2007
+// Allan CORNET
+// ====================================================================
+path = SCI+'/modules/renderer/help/'+getlanguage('LANGUAGE_DEFAULT');
+add_help_chapter("renderer",path,%T);
+clear path add_help_chapter;
+// ====================================================================
---
+++ scilab/modules/renderer/sci_gateway/c/gw_renderer.c
@@ -0,0 +1,49 @@
+/*-----------------------------------------------------------------------------------*/
+/**
+  * @INRIA 2007
+ */
+/*-----------------------------------------------------------------------------------*/
+#include <string.h>
+#ifdef _MSC_VER
+#include <Windows.h>
+#include "ExceptionMessage.h"
+#endif
+#include "gw_renderer.h"
+#include "stack-c.h"
+/*-----------------------------------------------------------------------------------*/
+typedef int (*Renderer_Interf) __PARAMS((char *fname,unsigned long l));
+
+typedef struct table_struct
+{
+  Renderer_Interf f;    /** function **/
+  char *name;      /** its name **/
+} RendererTable;
+/*-----------------------------------------------------------------------------------*/
+static RendererTable Tab[]=
+{
+//{C2F(sci_skeleton_c),"skeleton_c"},
+};
+/*-----------------------------------------------------------------------------------*/
+int C2F(gw_renderer)()
+{  
+	Rhs = Max(0, Rhs);
+
+#ifdef _MSC_VER
+	#ifndef _DEBUG
+		_try
+		{
+			(*(Tab[Fin-1].f)) (Tab[Fin-1].name,(unsigned long)strlen(Tab[Fin-1].name));
+		}
+		_except (EXCEPTION_EXECUTE_HANDLER)
+		{	
+			ExceptionMessage(GetExceptionCode(),Tab[Fin-1].name);
+		}
+	#else
+		(*(Tab[Fin-1].f)) (Tab[Fin-1].name,(unsigned long)strlen(Tab[Fin-1].name));
+	#endif
+#else
+		(*(Tab[Fin-1].f)) (Tab[Fin-1].name,(unsigned long)strlen(Tab[Fin-1].name));
+#endif
+	return 0;
+}
+/*-----------------------------------------------------------------------------------*/
