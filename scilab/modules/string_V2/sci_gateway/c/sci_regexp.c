429c48492d32f47eebad1856347a087f2e8e4b73
Cong Wu <cong.wu@inria.fr>
2007-06-14 15:02:45 +0000

    remove some warnings

--- scilab/modules/string_V2/sci_gateway/c/sci_regexp.c
+++ scilab/modules/string_V2/sci_gateway/c/sci_regexp.c
@@ -75,8 +75,8 @@ int C2F(sci_regexp) _PARAMS((char *fname,unsigned long fname_len))
 		z = regexec(out1, Str[0], nmatch, pm, 0);
 		if (z == REG_NOMATCH) { 
 			int outIndex2= Rhs +x+1 ;
+			int numCol   = 1 ;
 			numRow   = 1 ;
-            int numCol   = 1 ;
             outIndex = 0 ;
             CreateVar(Rhs+1+x,"c",&numRow,&numCol,&outIndex);
   			LhsVar(x+1) = outIndex2 ;
