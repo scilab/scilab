a90dc260e5552a10dd6e2c9ad5c8c767d2001308
Jean-baptiste Silvy <jean-baptiste.silvy@scilab.org>
2007-06-15 13:41:31 +0000

    Rename Menubar and Toolbar to upper-case B.

---
+++ scilab/modules/gui/src/java/org/scilab/modules/gui/widget/ToolBar.java
@@ -0,0 +1,12 @@
+
+/* Copyright INRIA 2007 */
+
+package org.scilab.modules.gui.widget;
+
+/**
+ * Interface for toolbar associated to objects in Scilab GUIs
+ * @author Vincent COUVERT
+ */
+public interface Toolbar {
+	// TODO : Add the methods that are usefull for a Toolbar
+}
