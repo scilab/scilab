a36c6b4d6f902bf1a36ea50aa19f8c4b84d4baf7
Serge Steer <serge.steer@scilab.org>
2007-06-18 12:47:01 +0000

    bug 2429 fix

--- scilab/modules/double/sci_gateway/fortran/vecimpl.f
+++ scilab/modules/double/sci_gateway/fortran/vecimpl.f
@@ -83,10 +83,11 @@ c
 
 c     check for clause
       if (rstk(pt-1) .eq. 801.or.rstk(pt).eq.611) go to 54
-      if(rstk(pt-1).eq.611.and.rstk(pt).eq.601) then
-c     : in compiled for, next line to differentiate from extraction
-         if(istk(pstk(pt)).eq.16) goto  54
-      endif
+      if(rstk(pt-1).eq.611.and.rstk(pt).eq.601) go to 54
+C       if(rstk(pt-1).eq.611.and.rstk(pt).eq.601) then
+C c     : in compiled for, next line to differentiate from extraction
+C          if(istk(pstk(pt)).eq.16) goto  54
+C       endif
 c
       if (st .eq. 0.0d+0) then
          istk(il1+1)=1
---
+++ tests/nonRegression/bug2429.sci
@@ -0,0 +1,18 @@
+// Non-regression test file for bug 2429
+// Copyright INRIA
+// Scilab Project - Serge Steer
+// Copyright INRIA 2007
+// Date : June 2007
+
+mode(-1) ;
+clear ;
+
+
+S=stacksize();
+function test(),for k=1:2d5,end,endfunction
+
+stacksize(1d5)
+
+ierr=execstr('test()','errcatch')
+stacksize(S(1))
+affich_result(ierr=0,2429) ;
