758edaa8181a25718a6a85f78d866298f0087344
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2007-06-27 13:28:15 +0000

    factorized and move to core (used by pvm for example)

---
+++ scilab/modules/core/includes/sciprint_nd.h
@@ -0,0 +1,14 @@
+/*-----------------------------------------------------------------------------------*/
+/* INRIA 2007 */
+/* @author Ledru Sylvestre */
+/*-----------------------------------------------------------------------------------*/
+#include "machine.h"
+
+/**
+ * almost the same but no diary record
+ * usefull for automatic tests 
+ *
+ * @param fmt   
+ * @param ...   
+ */
+void sciprint_nd(char *fmt,...);
---
+++ scilab/modules/core/src/c/sciprint_nd.c
@@ -0,0 +1,44 @@
+/*-----------------------------------------------------------------------------------*/
+/* INRIA 2007 */
+/* @author Allan CORNET */
+/* @author Sylvestre LEDRU */
+/*-----------------------------------------------------------------------------------*/ 
+
+#include "sciprint_nd.h"
+
+#define MAXPRINTF 512
+
+void sciprint_nd(char *fmt,...) 
+{
+	int i, count=0, lstr;
+  va_list args;
+  char buf[MAXPRINTF];
+  va_start(args,fmt);
+#if defined(linux) || defined(_MSC_VER)
+	count = vsnprintf (buf,MAXPRINTF-1, fmt, args);
+	if (count == -1)
+	{
+		buf[MAXPRINTF-1]='\0';
+	}
+#else
+	(void ) vsprintf(buf, fmt, args );
+#endif
+  lstr=strlen(buf);
+
+  C2F(xscion)(&i);
+  if (i == 0) 
+    {
+      printf("%s",buf); 
+    }
+  else 
+    {
+#ifdef _MSC_VER
+	TextPutS (&textwin, buf);
+#else
+    C2F(xscisrn)(buf,&lstr,0L);
+#endif
+
+    }
+  va_end(args);
+}
+
