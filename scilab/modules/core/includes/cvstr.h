af16d6da71434be293ef0828b251155d904637b0
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2007-06-27 12:44:09 +0000

    comment

--- scilab/modules/core/includes/cvstr.h
+++ scilab/modules/core/includes/cvstr.h
@@ -6,14 +6,15 @@
 #define __CVSTR_H__
 
 #include "machine.h" /* C2F */
+
 /**
-*   converts from ascii to Scilab internal coding 
+* converts from ascii to Scilab internal coding 
 * @param n integer, length of the string to be converted integer
 * @param line: integer array (where Scilab coded string are stored ) 
 * @param string: string 
 * @param job: integer flag 
-* 1: code-->ascii 
-* 0: ascii-->code 
+* @return 1: code-->ascii 
+* @return 0: ascii-->code 
 */
 int C2F(cvstr)(integer * n,integer * line,char * str,integer * job,unsigned long str_len);
 
