63fcadf5edc392910bf8bd5e348e311e9bbdf15c
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2007-06-28 16:31:40 +0000

    Comment

--- scilab/modules/core/includes/xscion.h
+++ scilab/modules/core/includes/xscion.h
@@ -6,18 +6,15 @@
 /*-----------------------------------------------------------------------------------*/ 
 
 /**
- * @TODO: comment
- *
- * @param i 
- * @return <ReturnValue>
+ * Is Scilab launched in the standard mode ?
+ * @param i the result of the test (1 if Scilab is in the standard mode)
+ * @return 0
  */
 int C2F(xscion) (int *i);
 
 /**
- * @TODO: comment
- *
- * @param void  
- * @return <ReturnValue>
+ * Is Scilab launched in the standard mode ?
+ * @return 1 or 0 (1 if Scilab is in the standard mode)
  */
 int getINXscilab(void);
 /*-----------------------------------------------------------------------------------*/ 
