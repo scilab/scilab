a7d43b60f73efe2cd1b9fc349d23cbf99a02467e
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2007-06-29 15:32:34 +0000

    Comment

--- scilab/modules/gui/includes/IsNoInteractiveWindow.h
+++ scilab/modules/gui/includes/IsNoInteractiveWindow.h
@@ -5,10 +5,12 @@
 
 #ifndef __ISNOINTERACTIVEMODEWINDOW_H__
 #define __ISNOINTERACTIVEMODEWINDOW_H__
-/**
+
+/*
+ * Used to know if we are in window mode (returned value is 1)
  * check if it is -NWNI mode (obsolete)
- * @param void  
- * @return 1 if TRUE
+ * or in console mode (returned value is 0) 
+ * @return 1 if we are in Windows mode & 0 if not
  */
 int IsNoInteractiveWindow(void);
 
