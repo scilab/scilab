5f94c623c17a3c0b72ea0697bf3423451a240559
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2007-07-02 08:14:39 +0000

    Where are the Comments ??? You have to be a forecaster to be a Scilab dev ...

--- scilab/modules/core/src/c/Funtab.c
+++ scilab/modules/core/src/c/Funtab.c
@@ -15,14 +15,14 @@ int C2F(funtab)(int *id, int *fptr, int *job,char *namefunction, unsigned long n
     {
     case 0 : /* before do a printf for What OBSOLETE */
     break;
-    case 1 :
+		case 1 : /* @TODO : add comment : what is 1 ? */
       *fptr=0;
       action_hashtable_scilab_functions(id,NULL,fptr,SCI_HFUNCTIONS_FIND);
       break;
-    case 2 : 
+    case 2 :  /* @TODO : add comment : what is 2 ? */
       action_hashtable_scilab_functions(id,NULL,fptr,SCI_HFUNCTIONS_BACKSEARCH);
       break;
-    case 3 : 
+    case 3 :   /* @TODO : add comment : what is 3 ? */
 		if ( (namefunction_len) && (strcmp(namefunction,"NULL_NAME")) )
 		{
 			action_hashtable_scilab_functions(id,namefunction,fptr,SCI_HFUNCTIONS_ENTER);
@@ -32,7 +32,7 @@ int C2F(funtab)(int *id, int *fptr, int *job,char *namefunction, unsigned long n
 			action_hashtable_scilab_functions(id,NULL,fptr,SCI_HFUNCTIONS_ENTER);
 		}
       break;
-    case 4 : 
+    case 4 :   /* @TODO : add comment : what is 4 ? */
       action_hashtable_scilab_functions(id,NULL,fptr,SCI_HFUNCTIONS_DELETE);
       break;
     }
