3934369a9c7e5c36b6c1ae4c31f86d1d6f6e1f89
Cong Wu <cong.wu@inria.fr>
2007-07-06 13:00:08 +0000

    modify a bug, added some commences

--- scilab/modules/string_V2/sci_gateway/c/sci_tokens1.c
+++ scilab/modules/string_V2/sci_gateway/c/sci_tokens1.c
@@ -1,7 +1,7 @@
 /*------------------------------------------------------------------------*/
 /* File: sci_tokens1.c                                                    */
 /* Copyright INRIA 2007                                                   */
-/* Authors : Cong Wu                                                      */
+/* @Authors : Cong Wu                                                      */
 /* desc : returns the tokens of a character string.  
           T=tokens(str [,delimiter])   
 		  str : a character string. The string where to search the tokens. 
@@ -20,10 +20,10 @@
 #include "returnProperty.h"
 #include "machine.h"
 #include "MALLOC.h" 
+#define MAXSIZE 20
 /*-----------------------------------------------------------------------------------*/
 int C2F(sci_tokens1) _PARAMS((char *fname,unsigned long fname_len))
 {
-
   char **Str,**Str2,**Str3;
   int m1,n1,m2,n2,mn2,mn,i=0;
   int u=0;
@@ -32,48 +32,36 @@ int C2F(sci_tokens1) _PARAMS((char *fname,unsigned long fname_len))
   int numCol;
   Rhs = Max(0, Rhs);
   CheckRhs(1,2);
-  switch ( VarType(1)) {
-  case 10 :
     GetRhsVar(1,"S",&m1,&n1,&Str);
     mn = m1*n1;  
-	if (Rhs == 2) {
+	if (Rhs == 2) 
+	{
 		GetRhsVar(2,"S",&m2,&n2,&Str2);
 		mn2=m2*n2;
 	}
-	else { 
-		Str2=(char**)MALLOC(sizeof(char*)*(20*20));
+	else 
+	{ 
+		Str2=(char**)MALLOC(sizeof(char*)*(MAXSIZE*MAXSIZE));
 		Str2[0]=(char*)MALLOC(sizeof(char*)*(1));
 		strcpy(Str2[0],"");
-		Str2[0][0]=32;
+		Str2[0][0]=32;              /*The ascii of 32 is a ' ' */
 		m2=1;
 		n2=1;
 		mn2=1;
 	}
-	Str3=(char**)MALLOC(sizeof(char*)*(20*20));
-	for (i=0;i<20*20;i++)
+	Str3=(char**)MALLOC(sizeof(char*)*(MAXSIZE*MAXSIZE));
+	for (i=0;i<MAXSIZE*MAXSIZE;i++)         /*Malloc*/
 	{
 		Str3[i]=(char*)MALLOC(sizeof(char*)*(1));
 		strcpy(Str3[i],"");
 	}
-	tokens(Str,Str2,Str3,&u,&w,mn,mn2);
-	/*for (x=0;x<mn;x++){
-		for (y=0;y<strlen(Str[x]);y++){
-				for (i=0; i<mn2; i++)  if (Str[x][y]==Str2[i][0]) bo=1;
-				if    (bo==0) Str3[u][w++]=Str[0][y];
-				else {
-					Str3[u][w]=0;
-					u++;
-					w=0;
-				}
-				bo=0;
-		}
-	}*/
-    numRow   = u+1;  /*Output */
+	tokens(Str,Str2,Str3,&u,&w,mn,mn2);   /*The main function of tokens*/
+	numRow   = u+1;  /*Output */
     numCol   = 1 ;
     CreateVarFromPtr( Rhs+1, "S", &numRow, &numCol, Str3 ) ;
-    LhsVar(1) = Rhs+1 ; 
+	LhsVar(1) = Rhs+1 ; 
     C2F(putlhsvar)();
     FREE(Str3);
-  }
     return 0;
 }
+/*-----------------------------------------------------------------------------------*/
