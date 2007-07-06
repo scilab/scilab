bc389b680e2e63aa3d74d41d5bf91d93d5ece5a8
Cong Wu <cong.wu@inria.fr>
2007-07-06 07:42:14 +0000

    seperated from gateway and sourcecode

--- scilab/modules/string_V2/sci_gateway/c/sci_tokens1.c
+++ scilab/modules/string_V2/sci_gateway/c/sci_tokens1.c
@@ -1,7 +1,7 @@
 /*------------------------------------------------------------------------*/
 /* File: sci_tokens1.c                                                    */
 /* Copyright INRIA 2007                                                   */
-/* @Authors : Cong Wu                                                      */
+/* Authors : Cong Wu                                                      */
 /* desc : returns the tokens of a character string.  
           T=tokens(str [,delimiter])   
 		  str : a character string. The string where to search the tokens. 
@@ -21,20 +21,21 @@
 #include "machine.h"
 #include "MALLOC.h" 
 /*-----------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------*/
 int C2F(sci_tokens1) _PARAMS((char *fname,unsigned long fname_len))
 {
 
   char **Str,**Str2,**Str3;
-  int x,y,m1,n1,m2,n2,mn2,mn,i=0;
+  int m1,n1,m2,n2,mn2,mn,i=0;
   int u=0;
   int w=0;
   int numRow;
   int numCol;
-  int bo=0; /* @TODO : What is bo ? */
+  int bo=0;
   Rhs = Max(0, Rhs);
   CheckRhs(1,2);
   switch ( VarType(1)) {
-	  case 10 : /* @TODO : use a the #define or explain what is 10 */
+  case 10 :
     GetRhsVar(1,"S",&m1,&n1,&Str);
     mn = m1*n1;  
 	if (Rhs == 2) {
@@ -42,35 +43,33 @@ int C2F(sci_tokens1) _PARAMS((char *fname,unsigned long fname_len))
 		mn2=m2*n2;
 	}
 	else { 
-		Str2=(char**)MALLOC(sizeof(char*)*(20*20)); /* @TODO : where 20 comes from ? It should be a #define */
-		Str2[0]=(char*)MALLOC(sizeof(char*)*(1)); /* @TODO : where 1 comes from ? */
+		Str2=(char**)MALLOC(sizeof(char*)*(20*20));
+		Str2[0]=(char*)MALLOC(sizeof(char*)*(1));
 		strcpy(Str2[0],"");
-		Str2[0][0]=32; /* @TODO : where 32 comes from ? */
+		Str2[0][0]=32;
 		m2=1;
 		n2=1;
 		mn2=1;
 	}
-	Str3=(char**)MALLOC(sizeof(char*)*(20*20)); /* @TODO : explain what is 20*20 ? */
-	/* @TODO : what is this loop ? */
-	for (i=0;i<20*20;i++)  /* @TODO : explain what is 20*20 ? */
+	Str3=(char**)MALLOC(sizeof(char*)*(20*20));
+	for (i=0;i<20*20;i++)
 	{
 		Str3[i]=(char*)MALLOC(sizeof(char*)*(1));
 		strcpy(Str3[i],"");
 	}
-	for (x=0;x<mn;x++){ /* @TODO : explain what is this loop */
-		for (y=0;y<strlen(Str[x]);y++){ /* @TODO : explain what is this loop */
-				for (i=0; i<mn2; i++)  /* @TODO : explain what is this loop */
-					if (Str[x][y]==Str2[i][0]) bo=1; /* @TODO : explain what is this if */
-			    if    (bo==0) Str3[u][w++]=Str[0][y];  /* @TODO : explain what is this if */
+	tokens(Str,Str2,Str3,&u,&w,mn,mn2);
+	/*for (x=0;x<mn;x++){
+		for (y=0;y<strlen(Str[x]);y++){
+				for (i=0; i<mn2; i++)  if (Str[x][y]==Str2[i][0]) bo=1;
+				if    (bo==0) Str3[u][w++]=Str[0][y];
 				else {
-					/* @TODO : explain what we are doing here */
 					Str3[u][w]=0;
 					u++;
 					w=0;
 				}
 				bo=0;
 		}
-	}
+	}*/
 	numRow   = u+1;  /*Output */
     numCol   = 1 ;
     CreateVarFromPtr( Rhs+1, "S", &numRow, &numCol, Str3 ) ;
@@ -80,5 +79,3 @@ int C2F(sci_tokens1) _PARAMS((char *fname,unsigned long fname_len))
   }
     return 0;
 }
-
-/*-----------------------------------------------------------------------------------*/
