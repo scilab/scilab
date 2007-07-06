2c1cd705333333f7bec7c84ca4d8acf08cc0f4b5
Cong Wu <cong.wu@inria.fr>
2007-07-06 13:16:15 +0000

    modify a bug, added some commences

--- scilab/modules/string_V2/src/c/strsplit1.c
+++ scilab/modules/string_V2/src/c/strsplit1.c
@@ -1,15 +1,25 @@
+/*------------------------------------------------------------------------*/
+/* File: sci_convstr1.c                                                   */
+/* Copyright INRIA 2007                                                   */
+/* Authors : Cong Wu                                                      */
+/* desc : converts the matrix of strings  str-matrix  into lower case (for*/  
+/*          "l"  ;default value) or upper case (for  "u" ).               */
+/*                                                                        */
+/*------------------------------------------------------------------------*/
 #include <string.h>
 #include <stdio.h>
 #include <ctype.h> 
 #include "gw_string.h"
 #include "MALLOC.h" 
-
+/*------------------------------------------------------------------------*/
 void strsplit1(char **Str,char **Str3,int *u,int *v,int *w,int mn,int *m4)
 {
 	int x;
-	for (x=0;x<mn;x++){
-		if (x!=m4[*v]) Str3[*u][(*w)++]=Str[0][x];
-		else {
+	for (x=0;x<mn;x++)       /*To traverse every string of the input matrix*/
+	{
+		if (x!=m4[*v]) Str3[*u][(*w)++]=Str[0][x];   /* To put them in the correct order */
+		else 
+		{
 			(*u)++;
 			(*v)++;
 			*w=0;
@@ -17,3 +27,4 @@ void strsplit1(char **Str,char **Str3,int *u,int *v,int *w,int mn,int *m4)
         Str3[*u][*w]=0;
 	}
 }
+/*------------------------------------------------------------------------*/
