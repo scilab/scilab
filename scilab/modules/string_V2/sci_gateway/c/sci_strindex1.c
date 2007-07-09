400e5e80fc621b5a7567481800a10545bc703f8f
Cong Wu <cong.wu@inria.fr>
2007-07-09 10:06:16 +0000

    at last, find out where the error came from--nearly crazy;  but something strange still

--- scilab/modules/string_V2/sci_gateway/c/sci_strindex1.c
+++ scilab/modules/string_V2/sci_gateway/c/sci_strindex1.c
@@ -19,12 +19,9 @@
 #include "machine.h"
 #include "MALLOC.h" 
 int next[20];
-
-
-
-
 int C2F(sci_strindex1) _PARAMS((char *fname,unsigned long fname_len))
 {
+	
 	char typ = '*';
     char **Str,**Str2;
     int x,m1,n1,mn,mn2,i,m2,n2,m3,n3,l3=0;
@@ -43,104 +40,102 @@ int C2F(sci_strindex1) _PARAMS((char *fname,unsigned long fname_len))
     int numCol   = 1 ;
     int w;
     int pos=0;
-	
-	
-	
-	
-	
 	regmatch_t pm[10];
     regex_t *out1[10];
     Rhs = Max(0, Rhs);
     CheckRhs(1,3);
     CheckLhs(1,2);
-    
-    switch ( VarType(1)) {
-		case 10 :
-			GetRhsVar(1,"S",&m1,&n1,&Str);
-			mn = m1*n1;  
-			if (mn != 1) {
-				/*give an error message that the first is not correct.*/
-				return 1;
-			}
-			GetRhsVar(2,"S",&m2,&n2,&Str2);
-			mn2 = m2*n2;  
-			lbuf=*Str;
-			if (Rhs >= 3) {
-				GetRhsVar(3,"c",&m3,&n3,&l3);
-				if ( m3*n3 != 0) 
-					typ = cstk(l3)[0];
-				if (typ == 'r' ) {            /*When we use the regexp;*/
-					for (i=0;i<mn2;++i){      /*  To compile the regexp pattern;*/
-						pattern[i]=Str2[i];
-						out1[i]=(regex_t *)malloc(sizeof(regex_t));
-						z = regcomp(out1[i], pattern[i], cflags);
-						if (z != 0){
-							regerror(z, out1[i], ebuf, sizeof(ebuf));
-							Scierror(999, "%s: pattern '%s' \n", ebuf, pattern);
-							return 1;
-						}
-					}
-	
-	
-					for (x=0;x<mn2;++x){
-						z = regexec(out1[x], Str[0], nmatch, pm, 0);
-						if (z == REG_NOMATCH) { 
-							int outIndex2= Rhs +x+1 ;
-							numRow   = 1 ;
-							numCol   = 1 ;
-							outIndex = 0 ;
-							CreateVar(Rhs+1+x,"c",&numRow,&numCol,&outIndex);
-  							LhsVar(x+1) = outIndex2 ;
-							continue;
-						}
-						for (x1 = 0; x1 < nmatch && pm[x1].rm_so != -1; ++ x1) {         
-							values[nbValues++]=pm[x1].rm_so+1;
-							position[nbposition++]=x+1;
-						}     
-
-					}
-
-	        
-
-	           
-			  }
-			} 
-			else {       /* When we do not use the regexp.*/
-				for (x=0;x<mn2;++x){
-					if (strlen(Str2[x])==0) {
-						 Scierror(999, "2th argument must not be an empty string");
-				       return 1;
-					}
-					getnext(Str2[x],next); 
-					w=kmp(*Str,Str2[x],pos);
-					if (w !=0) { 	       
-						values[nbValues++]=w;
-						position[nbposition++]=x+1;
-					} 	 
-				}
-			} 
-		
-	    
-			numRow   = 1        ;/* Output values[] and position[]*/
-			outIndex = 0        ;
-  
-			CreateVar(Rhs+1,"d",&numRow,&nbValues,&outIndex) ;
-			for ( i = 0 ; i < nbposition ; i++ ){
-				  stk(outIndex)[i] = (double)values[i] ;
-			}
-			LhsVar(1) = Rhs+1 ;
-			numRow   = 1        ;
-			outIndex = 0        ;
-			CreateVar(Rhs+2,"d",&numRow,&nbposition,&outIndex) ;
-			for ( i = 0 ; i < nbposition ; i++ ){
-				stk(outIndex)[i] = (double)position[i] ;
-			}
-			LhsVar(2) = Rhs+2;	
-		
+	GetRhsVar(1,"S",&m1,&n1,&Str);
+	mn = m1*n1;  
+	if (mn != 1) 
+	{
+		/*give an error message that the first is not correct.*/
+		return 1;
 	}
-  
+	GetRhsVar(2,"S",&m2,&n2,&Str2);
+	mn2 = m2*n2;  
+	lbuf=*Str;
+	if (Rhs >= 3) 
+	{
+		GetRhsVar(3,"c",&m3,&n3,&l3);
+		if ( m3*n3 != 0) 
+			typ = cstk(l3)[0];
+		if (typ == 'r' ) 
+		{            /*When we use the regexp;*/
+			for (i=0;i<mn2;++i)
+			{      /*  To compile the regexp pattern;*/
+				pattern[i]=Str2[i];
+				out1[i]=(regex_t *)malloc(sizeof(regex_t));
+				z = regcomp(out1[i], pattern[i], cflags);
+				if (z != 0)
+				{
+					regerror(z, out1[i], ebuf, sizeof(ebuf));
+					Scierror(999, "%s: pattern '%s' \n", ebuf, pattern);
+					return 1;
+				}
+			}
+			for (x=0;x<mn2;++x)
+			{
+				z = regexec(out1[x], Str[0], nmatch, pm, 0);
+				if (z == REG_NOMATCH) 
+				{ 
+					int outIndex2= Rhs +x+1 ;
+					numRow   = 1 ;
+					numCol   = 1 ;
+					outIndex = 0 ;
+					CreateVar(Rhs+1+x,"c",&numRow,&numCol,&outIndex);
+					LhsVar(x+1) = outIndex2 ;
+					continue;
+				}
+				for (x1 = 0; x1 < nmatch && pm[x1].rm_so != -1; ++ x1) 
+				{         
+					values[nbValues++]=pm[x1].rm_so+1;
+					position[nbposition++]=x+1;
+				}     
 
-  C2F(putlhsvar)();
+			}
+	  }
+	} 
+	else 
+	{                                    /* When we do not use the regexp.*/
+		for (x=0;x<mn2;++x)
+		{
+			if (strlen(Str2[x])==0) 
+			{
+				 Scierror(999, "2th argument must not be an empty string");
+		       return 1;
+			}
+			do
+			{
+				getnext(Str2[x],next); 
+				w=kmp(*Str,Str2[x],pos);      /*Str is the input string matrix, Str2[x] is the substring to match; pos is the start point*/
+				if (w !=0) 
+				{ 	       
+					values[nbValues++]=w;
+					position[nbposition++]=x+1;
+				}
+                pos=w;
+			}
+            while(w!=0);		  /* w is the answer of the kmp algorithem*/
+		}
+	} 
+	numRow   = 1        ;/* Output values[] and position[]*/
+	outIndex = 0        ;
+	CreateVar(Rhs+1,"d",&numRow,&nbValues,&outIndex) ;
+	for ( i = 0 ; i < nbposition ; i++ )
+	{
+		  stk(outIndex)[i] = (double)values[i] ;
+	}
+	LhsVar(1) = Rhs+1 ;
+	numRow   = 1        ;
+	outIndex = 0        ;
+	CreateVar(Rhs+2,"d",&numRow,&nbposition,&outIndex) ;
+	for ( i = 0 ; i < nbposition ; i++ )
+	{
+		stk(outIndex)[i] = (double)position[i] ;
+	}
+	LhsVar(2) = Rhs+2;	
+    C2F(putlhsvar)();
 	return 0;
 }
 
