b173ad5817fed336193ba9f84892e848d39270b1
Cong Wu <cong.wu@inria.fr>
2007-07-10 13:02:34 +0000

    warning

--- scilab/modules/string_V2/sci_gateway/c/sci_stripblanks1.c
+++ scilab/modules/string_V2/sci_gateway/c/sci_stripblanks1.c
@@ -15,6 +15,7 @@
 #include "machine.h"
 #include "stack-c.h"
 #include "MALLOC.h" 
+#include "src_string.h"
 /*-------------------------------------------------------------------------------------*/
 int C2F(sci_stripblanks1) _PARAMS((char *fname,unsigned long fname_len))
 {
