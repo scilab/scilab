314ffedabbaabef7b821a37926f62e3614cba07b
Bruno Jofret <bruno.jofret@scilab.org>
2007-07-17 15:27:28 +0000

    Please avoid Override and Commit !

--- scilab/modules/gui/src/java/org/scilab/modules/gui/window/Window.java
+++ scilab/modules/gui/src/java/org/scilab/modules/gui/window/Window.java
@@ -16,9 +16,26 @@ import org.scilab.modules.gui.toolbar.ToolBar;
  */
 public interface Window extends UIElement {
 
+	
 	/**
-	 * Gets this Bridge component object
-	 * @return this Bridge component object
+	 * Associate the Tool bar to the UIElement
+	 * @param toolBarToAdd - the Tool bar.
+	 */
+	// FIXME : Must be removed
+	// Comes from inherited UIElement
+	void addToolBar(ToolBar toolBarToAdd);
+
+	/**
+	 * Associate the Menu bar to the UIElement
+	 * @param toolMenuToAdd - the Tool bar.
+	 */
+	// FIXME : Must be removed
+	// Comes from inherited UIElement
+	void addMenuBar(MenuBar toolMenuToAdd);
+	
+	/**
+	 * Gets a much more dummy Objects
+	 * @return the component.
 	 */
 	SimpleWindow getAsSimpleWindow();
 	
