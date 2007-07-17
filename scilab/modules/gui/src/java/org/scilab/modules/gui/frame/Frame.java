ea894d5f2154570934537e69dcd7427082efd199
Bruno Jofret <bruno.jofret@scilab.org>
2007-07-17 09:46:12 +0000

    No way you add some basic's component in our Scilab high-level objects.
    Try to fit the modelisation and not clone Swing one...

--- scilab/modules/gui/src/java/org/scilab/modules/gui/frame/Frame.java
+++ scilab/modules/gui/src/java/org/scilab/modules/gui/frame/Frame.java
@@ -3,20 +3,12 @@
 
 package org.scilab.modules.gui.frame;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-
-import javax.swing.border.LineBorder;
-
 import org.scilab.modules.gui.canvas.Canvas;
 import org.scilab.modules.gui.checkbox.CheckBox;
 import org.scilab.modules.gui.container.Container;
 import org.scilab.modules.gui.dockable.Dockable;
 import org.scilab.modules.gui.editbox.EditBox;
-import org.scilab.modules.gui.layout.LayoutManager;
 import org.scilab.modules.gui.listbox.ListBox;
-import org.scilab.modules.gui.menu.Menu;
 import org.scilab.modules.gui.pushbutton.PushButton;
 import org.scilab.modules.gui.radiobutton.RadioButton;
 import org.scilab.modules.gui.slider.Slider;
@@ -90,7 +82,10 @@ public interface Frame extends Container {
 	 * @param borderLayout the BorderLayout to use
 	 * @return the position of the PushButton in the member list.
 	 */
-	int addMember(PushButton member, String borderLayout);
+	/**
+	 * JUST THINK SCILAB AND NOT JAVA !!! 
+	 */
+	//int addMember(PushButton member, String borderLayout);
 	
 	/**
 	 * We want to be able to add a PushButton in a Frame with a FlowLayout.
@@ -98,7 +93,10 @@ public interface Frame extends Container {
 	 * @param flowLayoutPosition the FlowLayout position to use
 	 * @return the position of the PushButton in the member list.
 	 */
-	int addMember(PushButton member, int flowLayoutPosition);
+	/**
+	 * JUST THINK SCILAB AND NOT JAVA !!! 
+	 */
+	//int addMember(PushButton member, int flowLayoutPosition);
 	
 	/**
 	 * We want to be able to add a Frame in a Frame.
@@ -113,7 +111,10 @@ public interface Frame extends Container {
 	 * @param borderLayout the BorderLayout to use
 	 * @return the position of the Frame in the member list.
 	 */
-	int addMember(Frame member, String borderLayout);
+	/**
+	 * JUST THINK SCILAB AND NOT JAVA !!! 
+	 */
+	//int addMember(Frame member, String borderLayout);
 	
 	/**
 	 * We want to be able to add a Frame in a Frame with a FlowLayout.
@@ -121,7 +122,10 @@ public interface Frame extends Container {
 	 * @param flowLayoutPosition the FlowLayout position to use
 	 * @return the position of the Frame in the member list.
 	 */
-	int addMember(Frame member, int flowLayoutPosition);
+	/**
+	 * JUST THINK SCILAB AND NOT JAVA !!! 
+	 */
+	//int addMember(Frame member, int flowLayoutPosition);
 	
 	// TODO : Check if it should be possible to add a Tab to a frame and how it should behave
 	/**
@@ -131,124 +135,133 @@ public interface Frame extends Container {
 	 */
 	int addMember(Tab member);
 
-	/**
-	 * To set the Border color and size of the element.
-	 * @param lineBorder the LineBorder
-	 */
-	void setBorder(LineBorder lineBorder);
-	
-	/**
-	 * To set the Layout of the element.
-	 * @param layout the layout
-	 */
-	void setLayout(LayoutManager layout);
-	
-	/**
-	 * To set the Background color of the element.
-	 * @param color the Color
-	 */
-	void setBackground(Color color);
-	
-	/**
-	 * To get the Background color of the element.
-	 * @return color the Color
-	 */
-	Color getBackground();
-	
-	/**
-	 * To set the Foreground color of the element.
-	 * @param color the Color
-	 */
-	void setForeground(Color color);
-	
-	/**
-	 * To get the Foreground color of the element.
-	 * @return color the Color
-	 */
-	Color getForeground();
-	
-	/**
-	 * To set the Font of the element.
-	 * @param font the Font
-	 */
-	void setFont(Font font);
-	
-	/**
-	 * To get the Font of the element.
-	 * @return font the Font
-	 */
-	Font getFont();
-	
-	/**
-	 * To set the Dimension of the element.
-	 * @param dimension the Dimension
-	 */
-	void setSize(Dimension dimension);
-	
-	/**
-	 * To get the Dimension of the element.
-	 * @return the Dimension
-	 */
-	Dimension getSize();
-	
-	/**
-	 * Moves this component to a new location. The top-left corner of the new location is specified by the x and y 
-	 * parameters in the coordinate space of this component's parent.
-	 * @param x - the x-coordinate of the new location's top-left corner in the parent's coordinate space
-	 * @param y - the y-coordinate of the new location's top-left corner in the parent's coordinate space
-	 */
-	void setLocation(int x, int y);
-	
-	/**
-	 * To get the x coordinate  of the element.
-	 * @return the x coordinate
-	 */
-	int getX();
-	
-	/**
-	 * To get the y coordinate  of the element.
-	 * @return the y coordinate
-	 */
-	int getY();
-	
-	/**
-	 * Sets the preferred size of the element.
-	 * @param dimension the Dimension
-	 */
-	void setPreferredSize(Dimension dimension);
-	
-	/**
-	 * If the preferredSize has been set to a non-null value just returns it. If the UI delegate's getPreferredSize
-	 *  method returns a non null value then return that; otherwise defer to the component's layout manager.
-	 * @return the preferredSize
-	 */
-	Dimension getPreferredSize();
 
 	/**
-	 * Sets the maximum size of the element.
-	 * @param dimension the Dimension
+	 * WE ABSOLUTELY WANT TO AVOID
+	 * USE OF ANY GUI IMPLEMENTATION OBJECTS !!!!
+	 * 
+	 * THE AIM IS NOT TO MIRROR THE IMPLEMENTATION !!!!
 	 */
-	void setMaximumSize(Dimension dimension);
-	
-	/**
-	 * If the maximum size has been set to a non-null value just returns it.
-	 * If the UI delegate's getMaximumSize  method returns a non-null value then return that;
-	 * otherwise defer to the component's layout manager.
-	 * @return the maximum size
-	 */
-	Dimension getMaximumSize();
-	
-	/**
-	 * Sets the minimum size of the element.
-	 * @param dimension the Dimension
-	 */
-	void setMinimumSize(Dimension dimension);
-	
-	/**
-	 * If the minimum size has been set to a non-null value just returns it.
-	 * If the UI delegate's getMaximumSize  method returns a non-null value then return that;
-	 * otherwise defer to the component's layout manager.
-	 * @return the minimum size
-	 */
-	Dimension getMinimumSize();
+	 
+//	
+//	/**
+//	 * To set the Border color and size of the element.
+//	 * @param lineBorder the LineBorder
+//	 */
+//	void setBorder(LineBorder lineBorder);
+//	
+//	/**
+//	 * To set the Layout of the element.
+//	 * @param layout the layout
+//	 */
+//	void setLayout(LayoutManager layout);
+//	
+//	/**
+//	 * To set the Background color of the element.
+//	 * @param color the Color
+//	 */
+//	void setBackground(Color color);
+//	
+//	/**
+//	 * To get the Background color of the element.
+//	 * @return color the Color
+//	 */
+//	Color getBackground();
+//	
+//	/**
+//	 * To set the Foreground color of the element.
+//	 * @param color the Color
+//	 */
+//	void setForeground(Color color);
+//	
+//	/**
+//	 * To get the Foreground color of the element.
+//	 * @return color the Color
+//	 */
+//	Color getForeground();
+//	
+//	/**
+//	 * To set the Font of the element.
+//	 * @param font the Font
+//	 */
+//	void setFont(Font font);
+//	
+//	/**
+//	 * To get the Font of the element.
+//	 * @return font the Font
+//	 */
+//	Font getFont();
+//	
+//	/**
+//	 * To set the Dimension of the element.
+//	 * @param dimension the Dimension
+//	 */
+//	void setSize(Dimension dimension);
+//	
+//	/**
+//	 * To get the Dimension of the element.
+//	 * @return the Dimension
+//	 */
+//	Dimension getSize();
+//	
+//	/**
+//	 * Moves this component to a new location. The top-left corner of the new location is specified by the x and y 
+//	 * parameters in the coordinate space of this component's parent.
+//	 * @param x - the x-coordinate of the new location's top-left corner in the parent's coordinate space
+//	 * @param y - the y-coordinate of the new location's top-left corner in the parent's coordinate space
+//	 */
+//	void setLocation(int x, int y);
+//	
+//	/**
+//	 * To get the x coordinate  of the element.
+//	 * @return the x coordinate
+//	 */
+//	int getX();
+//	
+//	/**
+//	 * To get the y coordinate  of the element.
+//	 * @return the y coordinate
+//	 */
+//	int getY();
+//	
+//	/**
+//	 * Sets the preferred size of the element.
+//	 * @param dimension the Dimension
+//	 */
+//	void setPreferredSize(Dimension dimension);
+//	
+//	/**
+//	 * If the preferredSize has been set to a non-null value just returns it. If the UI delegate's getPreferredSize
+//	 *  method returns a non null value then return that; otherwise defer to the component's layout manager.
+//	 * @return the preferredSize
+//	 */
+//	Dimension getPreferredSize();
+//
+//	/**
+//	 * Sets the maximum size of the element.
+//	 * @param dimension the Dimension
+//	 */
+//	void setMaximumSize(Dimension dimension);
+//	
+//	/**
+//	 * If the maximum size has been set to a non-null value just returns it.
+//	 * If the UI delegate's getMaximumSize  method returns a non-null value then return that;
+//	 * otherwise defer to the component's layout manager.
+//	 * @return the maximum size
+//	 */
+//	Dimension getMaximumSize();
+//	
+//	/**
+//	 * Sets the minimum size of the element.
+//	 * @param dimension the Dimension
+//	 */
+//	void setMinimumSize(Dimension dimension);
+//	
+//	/**
+//	 * If the minimum size has been set to a non-null value just returns it.
+//	 * If the UI delegate's getMaximumSize  method returns a non-null value then return that;
+//	 * otherwise defer to the component's layout manager.
+//	 * @return the minimum size
+//	 */
+//	Dimension getMinimumSize();
 }
--- scilab/modules/gui/src/java/org/scilab/modules/gui/frame/ScilabFrame.java
+++ scilab/modules/gui/src/java/org/scilab/modules/gui/frame/ScilabFrame.java
@@ -20,11 +20,13 @@ import org.scilab.modules.gui.editbox.EditBox;
 import org.scilab.modules.gui.label.Label;
 import org.scilab.modules.gui.layout.LayoutManager;
 import org.scilab.modules.gui.listbox.ListBox;
+import org.scilab.modules.gui.menubar.MenuBar;
 import org.scilab.modules.gui.pushbutton.PushButton;
 import org.scilab.modules.gui.radiobutton.RadioButton;
 import org.scilab.modules.gui.slider.Slider;
 import org.scilab.modules.gui.tab.Tab;
 import org.scilab.modules.gui.textbox.TextBox;
+import org.scilab.modules.gui.toolbar.ToolBar;
 import org.scilab.modules.gui.utils.Position;
 import org.scilab.modules.gui.utils.Size;
 
@@ -259,227 +261,248 @@ public class ScilabFrame extends ScilabContainer implements Frame {
 		//return ScilabBridge.addMember(this, member);
 		return 0;
 	}
-	
-	/**
-	 * Add a member (dockable element) to a Scilab frame and returns its index
-	 * @param member the member to add
-	 * @param borderLayout the BorderLayout to use
-	 * @return index of member in frame components
-	 */
-	public int addMember(PushButton member, String borderLayout) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
 
-	/**
-	 * Add a member (dockable element) to a Scilab frame and returns its index
-	 * @param member the member to add
-	 * @param flowLayoutPosition the flowLayout Position to use
-	 * @return index of member in frame components
-	 */
-	public int addMember(PushButton member, int flowLayoutPosition) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	/**
-	 * Add a member (dockable element) to a Scilab frame and returns its index
-	 * @param member the member to add
-	 * @param borderLayout the BorderLayout to use
-	 * @return index of member in frame components
-	 */
-	public int addMember(Frame member, String borderLayout) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	/**
-	 * Add a member (dockable element) to a Scilab frame and returns its index
-	 * @param member the member to add
-	 * @param flowLayoutPosition the flowLayout Position to use
-	 * @return index of member in frame components
-	 */
-	public int addMember(Frame member, int flowLayoutPosition) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	/**
-	 * To get the Background color of the element.
-	 * @return color the Color
-	 */
-	public Color getBackground() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/**
-	 * To get the Font of the element.
-	 * @return font the Font
-	 */
-	public Font getFont() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	/**
-	 * To get the Foreground color of the element.
-	 * @return color the Color
-	 */
-	public Color getForeground() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	/**
-	 * If the maximum size has been set to a non-null value just returns it.
-	 * If the UI delegate's getMaximumSize  method returns a non-null value then return that;
-	 * otherwise defer to the component's layout manager.
-	 * @return the maximum size
-	 */
-	public Dimension getMaximumSize() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	/**
-	 * If the minimum size has been set to a non-null value just returns it.
-	 * If the UI delegate's getMaximumSize  method returns a non-null value then return that;
-	 * otherwise defer to the component's layout manager.
-	 * @return the minimum size
-	 */
-	public Dimension getMinimumSize() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	/**
-	 * If the preferredSize has been set to a non-null value just returns it. If the UI delegate's getPreferredSize
-	 *  method returns a non null value then return that; otherwise defer to the component's layout manager.
-	 * @return the preferredSize
-	 */
-	public Dimension getPreferredSize() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	/**
-	 * To get the Dimension of the element.
-	 * @return the Dimension
-	 */
-	public Dimension getSize() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	/**
-	 * To get the x coordinate  of the element.
-	 * @return the x coordinate
-	 */
-	public int getX() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	/**
-	 * To get the y coordinate  of the element.
-	 * @return the y coordinate
-	 */
-	public int getY() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	/**
-	 * To set the Background color of the element.
-	 * @param color the Color
-	 */
-	public void setBackground(Color color) {
+	@Override
+	public void addMenuBar(MenuBar toolMenuToAdd) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	/**
-	 * To set the Border color and size of the element.
-	 * @param lineBorder the LineBorder
-	 */
-	public void setBorder(LineBorder lineBorder) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	/**
-	 * To set the Font of the element.
-	 * @param font the Font
-	 */
-	public void setFont(Font font) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	/**
-	 * To set the Foreground color of the element.
-	 * @param color the Color
-	 */
-	public void setForeground(Color color) {
+	@Override
+	public void addToolBar(ToolBar toolBarToAdd) {
 		// TODO Auto-generated method stub
 		
 	}
 	
 	/**
-	 * To set the Layout of the element.
-	 * @param layout the layout
+	 * WE ABSOLUTELY WANT TO AVOID
+	 * USE OF ANY GUI IMPLEMENTATION OBJECTS !!!!
+	 * 
+	 * THE AIM IS NOT TO MIRROR THE IMPLEMENTATION !!!!
 	 */
-	public void setLayout(LayoutManager layout) {
-		// TODO Auto-generated method stub
-		
-	}
 	
-	/**
-	 * Moves this component to a new location. The top-left corner of the new location is specified by the x and y 
-	 * parameters in the coordinate space of this component's parent.
-	 * @param x - the x-coordinate of the new location's top-left corner in the parent's coordinate space
-	 * @param y - the y-coordinate of the new location's top-left corner in the parent's coordinate space
-	 */
-	public void setLocation(int x, int y) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	/**
-	 * Sets the maximum size of the element.
-	 * @param dimension the Dimension
-	 */
-	public void setMaximumSize(Dimension dimension) {
-		// TODO Auto-generated method stub
-		
-	}
 	
-	/**
-	 * Sets the minimum size of the element.
-	 * @param dimension the Dimension
-	 */
-	public void setMinimumSize(Dimension dimension) {
-		// TODO Auto-generated method stub
-		
-	}
 	
-	/**
-	 * Sets the preferred size of the element.
-	 * @param dimension the Dimension
-	 */
-	public void setPreferredSize(Dimension dimension) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	/**
-	 * To set the Dimension of the element.
-	 * @param dimension the Dimension
-	 */
-	public void setSize(Dimension dimension) {
-		// TODO Auto-generated method stub
-		
-	}
+//	/**
+//	 * Add a member (dockable element) to a Scilab frame and returns its index
+//	 * @param member the member to add
+//	 * @param borderLayout the BorderLayout to use
+//	 * @return index of member in frame components
+//	 */
+//	public int addMember(PushButton member, String borderLayout) {
+//		// TODO Auto-generated method stub
+//		return 0;
+//	}
+//
+//	/**
+//	 * Add a member (dockable element) to a Scilab frame and returns its index
+//	 * @param member the member to add
+//	 * @param flowLayoutPosition the flowLayout Position to use
+//	 * @return index of member in frame components
+//	 */
+//	public int addMember(PushButton member, int flowLayoutPosition) {
+//		// TODO Auto-generated method stub
+//		return 0;
+//	}
+//
+//	/**
+//	 * Add a member (dockable element) to a Scilab frame and returns its index
+//	 * @param member the member to add
+//	 * @param borderLayout the BorderLayout to use
+//	 * @return index of member in frame components
+//	 */
+//	public int addMember(Frame member, String borderLayout) {
+//		// TODO Auto-generated method stub
+//		return 0;
+//	}
+//
+//	/**
+//	 * Add a member (dockable element) to a Scilab frame and returns its index
+//	 * @param member the member to add
+//	 * @param flowLayoutPosition the flowLayout Position to use
+//	 * @return index of member in frame components
+//	 */
+//	public int addMember(Frame member, int flowLayoutPosition) {
+//		// TODO Auto-generated method stub
+//		return 0;
+//	}
+//
+//	/**
+//	 * To get the Background color of the element.
+//	 * @return color the Color
+//	 */
+//	public Color getBackground() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	/**
+//	 * To get the Font of the element.
+//	 * @return font the Font
+//	 */
+//	public Font getFont() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//
+//	/**
+//	 * To get the Foreground color of the element.
+//	 * @return color the Color
+//	 */
+//	public Color getForeground() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//	
+//	/**
+//	 * If the maximum size has been set to a non-null value just returns it.
+//	 * If the UI delegate's getMaximumSize  method returns a non-null value then return that;
+//	 * otherwise defer to the component's layout manager.
+//	 * @return the maximum size
+//	 */
+//	public Dimension getMaximumSize() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//	
+//	/**
+//	 * If the minimum size has been set to a non-null value just returns it.
+//	 * If the UI delegate's getMaximumSize  method returns a non-null value then return that;
+//	 * otherwise defer to the component's layout manager.
+//	 * @return the minimum size
+//	 */
+//	public Dimension getMinimumSize() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//	
+//	/**
+//	 * If the preferredSize has been set to a non-null value just returns it. If the UI delegate's getPreferredSize
+//	 *  method returns a non null value then return that; otherwise defer to the component's layout manager.
+//	 * @return the preferredSize
+//	 */
+//	public Dimension getPreferredSize() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//	
+//	/**
+//	 * To get the Dimension of the element.
+//	 * @return the Dimension
+//	 */
+//	public Dimension getSize() {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+//	
+//	/**
+//	 * To get the x coordinate  of the element.
+//	 * @return the x coordinate
+//	 */
+//	public int getX() {
+//		// TODO Auto-generated method stub
+//		return 0;
+//	}
+//
+//	/**
+//	 * To get the y coordinate  of the element.
+//	 * @return the y coordinate
+//	 */
+//	public int getY() {
+//		// TODO Auto-generated method stub
+//		return 0;
+//	}
+//
+//	/**
+//	 * To set the Background color of the element.
+//	 * @param color the Color
+//	 */
+//	public void setBackground(Color color) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	/**
+//	 * To set the Border color and size of the element.
+//	 * @param lineBorder the LineBorder
+//	 */
+//	public void setBorder(LineBorder lineBorder) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	/**
+//	 * To set the Font of the element.
+//	 * @param font the Font
+//	 */
+//	public void setFont(Font font) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	/**
+//	 * To set the Foreground color of the element.
+//	 * @param color the Color
+//	 */
+//	public void setForeground(Color color) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//	
+//	/**
+//	 * To set the Layout of the element.
+//	 * @param layout the layout
+//	 */
+//	public void setLayout(LayoutManager layout) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//	
+//	/**
+//	 * Moves this component to a new location. The top-left corner of the new location is specified by the x and y 
+//	 * parameters in the coordinate space of this component's parent.
+//	 * @param x - the x-coordinate of the new location's top-left corner in the parent's coordinate space
+//	 * @param y - the y-coordinate of the new location's top-left corner in the parent's coordinate space
+//	 */
+//	public void setLocation(int x, int y) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	/**
+//	 * Sets the maximum size of the element.
+//	 * @param dimension the Dimension
+//	 */
+//	public void setMaximumSize(Dimension dimension) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//	
+//	/**
+//	 * Sets the minimum size of the element.
+//	 * @param dimension the Dimension
+//	 */
+//	public void setMinimumSize(Dimension dimension) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//	
+//	/**
+//	 * Sets the preferred size of the element.
+//	 * @param dimension the Dimension
+//	 */
+//	public void setPreferredSize(Dimension dimension) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	/**
+//	 * To set the Dimension of the element.
+//	 * @param dimension the Dimension
+//	 */
+//	public void setSize(Dimension dimension) {
+//		// TODO Auto-generated method stub
+//		
+//	}
 
 }
