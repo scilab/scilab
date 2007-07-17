97a15298c07bca400738c2d1fe178b2cdcfd6b39
Bruno Jofret <bruno.jofret@scilab.org>
2007-07-17 16:11:00 +0000

    NO COMMENT !

--- scilab/modules/gui/src/java/org/scilab/modules/gui/bridge/editbox/SwingScilabEditBox.java
+++ scilab/modules/gui/src/java/org/scilab/modules/gui/bridge/editbox/SwingScilabEditBox.java
@@ -6,9 +6,7 @@ package org.scilab.modules.gui.bridge.editbox;
 import javax.swing.JTextArea;
 import javax.swing.border.LineBorder;
 
-import org.scilab.modules.gui.editbox.SimpleEditBox;
-import org.scilab.modules.gui.menubar.SimpleMenuBar;
-import org.scilab.modules.gui.toolbar.SimpleToolBar;
+import org.scilab.modules.gui.editbox.EditBoxBridge;
 import org.scilab.modules.gui.utils.Position;
 import org.scilab.modules.gui.utils.Size;
 
@@ -16,7 +14,10 @@ import org.scilab.modules.gui.utils.Size;
  * Swing implementation for Scilab EditBox in GUIs
  * @author Marouane BEN JELLOUL
  */
-public class SwingScilabEditBox extends JTextArea implements SimpleEditBox {
+public class SwingScilabEditBox extends JTextArea {
+	// FIXME : Must have some Interface here...
+	//implements EditBoxBridge {
+
 	
 	/**
 	 * Constructor
@@ -24,22 +25,28 @@ public class SwingScilabEditBox extends JTextArea implements SimpleEditBox {
 	public SwingScilabEditBox() {
 		super();
 	}
-	
-	/**
-	 * Sets a MenuBar to an element
-	 * @param newMenuBar the MenuBar to set to the element
-	 */
-	public void addMenuBar(SimpleMenuBar newMenuBar) {
-		// TODO Auto-generated method stub
-	}
 
-	/**
-	 * Sets a ToolBar to an element
-	 * @param newToolBar the ToolBar to set to the element
-	 */
-	public void addToolBar(SimpleToolBar newToolBar) {
-		// TODO Auto-generated method stub	
-	}
+/*
+** DO NOT ADD METHODS THAT HAVE NOTHING TO DO
+** WITH WHAT WE ARE DEALING WITH !!!!!
+*/
+
+	
+//	/**
+//	 * Sets a MenuBar to an element
+//	 * @param newMenuBar the MenuBar to set to the element
+//	 */
+//	public void addMenuBar(SimpleMenuBar newMenuBar) {
+//		// TODO Auto-generated method stub
+//	}
+
+//	/**
+//	 * Sets a ToolBar to an element
+//	 * @param newToolBar the ToolBar to set to the element
+//	 */
+//	public void addToolBar(SimpleToolBar newToolBar) {
+//		// TODO Auto-generated method stub	
+//	}
 	
 	/**
 	 * Sets the text of a Text Widget
