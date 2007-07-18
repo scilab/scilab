f4b4f3040dd0e83fcd466bac4cd276da1e87a994
Bruno Jofret <bruno.jofret@scilab.org>
2007-07-18 09:24:08 +0000

    Interface...

---
+++ scilab/modules/gui/src/java/org/scilab/modules/gui/text/SimpleText.java
@@ -0,0 +1,24 @@
+
+/* Copyright INRIA 2007 */
+
+package org.scilab.modules.gui.text;
+
+
+/**
+ * Interface for TextBridge the associated object to Scilab GUIs Text
+ * @author Marouane BEN JELLOUL
+ */
+public interface SimpleText {
+
+	/**
+	 * Sets the text of a Text Widget
+	 * @param newText the text to set to the Text Widget
+	 */
+	void setText(String newText);
+
+	/**
+	 * Gets the text of a Text Widget
+	 * @return the text of the Text Widget
+	 */
+	String getText();
+}
