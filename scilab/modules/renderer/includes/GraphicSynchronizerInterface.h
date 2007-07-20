d5f50ceb1347b03ce717c7574020bcabd3f76dca
Jean-baptiste Silvy <jean-baptiste.silvy@scilab.org>
2007-07-20 12:19:23 +0000

    Add synchronization routines for graphics data. Also remove cross dependencies between gui and renderer.

---
+++ scilab/modules/renderer/includes/GraphicSynchronizerInterface.h
@@ -0,0 +1,92 @@
+/*------------------------------------------------------------------------*/
+/* file: GraphicSynchronizerInterface.h                                   */
+/* Copyright INRIA 2007                                                   */
+/* Authors : Jean-Baptiste Silvy                                          */
+/* desc : Interface to use class GraphicSynchronizer from C code          */
+/*------------------------------------------------------------------------*/
+
+#ifndef _GRAPHIC_SYNCHRONIZER_INTERFACE_H_
+#define _GRAPHIC_SYNCHRONIZER_INTERFACE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "ObjectStructure.h"
+
+/**
+ * Protect the data of a figure and its children from being accessed from an other thread.
+ */
+void startFigureDataWriting(sciPointObj * pFigure) ;
+
+/**
+ * Should be called after a call to startFigureDataWriting to remove figure locking.
+ */
+void endFigureDataWriting(sciPointObj * pFigure);
+
+/**
+ * Protect the data of a figure from being writen by an other thread.
+ */
+void startFigureDataReading(sciPointObj * pFigure);
+
+/**
+ * Should be called after a call to startFigureDataReading to remove figure locking.
+ */
+void endFigureDataReading(sciPointObj * pFigure);
+
+/**
+ * Protect the data of a figure from being writen or displayed by an other thread.
+ */
+void startFigureDataDisplaying(sciPointObj * pFigure);
+
+/**
+ * Should be called after a call to startFigureDataDisplaying to remove figure locking.
+ */
+void endFigureDataDisplaying(sciPointObj * pFigure);
+
+/**
+ * Protect all graphic hierarchy from being accessed from an other thread.
+ */
+void startGraphicDataWriting( void );
+
+/**
+ * Should be called after a call to startGraphicDataWriting.
+ */
+void endGraphicDataWriting( void );
+
+/**
+ * Protect all graphic hierarchy from being written by an other thread.
+ */
+void startGraphicDataReading( void );
+
+/**
+ * Should be called after a call to startGraphicDataWriting.
+ */
+void endGraphicDataReading( void );
+
+/**
+ * Protect all graphic hierarchy from being written by an other thread.
+ */
+void startGraphicDataDisplaying( void ) ;
+
+/**
+ * Should be called after a call to endFigureDataDisplaying.
+ */
+void endGraphicDataDisplaying( void ) ;
+
+/**
+ * To be called before any call to startGraphicData***
+ * Allocates data needed by these functions.
+ */
+void createGraphicSynchronizer( void ) ;
+
+/**
+ * Deallocates data needed by startGraphicData*** functions.
+ */
+void destroyGraphicSynchronizer( void ) ;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _GRAPHIC_SYNCHRONIZER_INTERFACE_H_ */
