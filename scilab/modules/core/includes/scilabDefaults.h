41b44e366aac5e07df9c87834e1f6f25cfba1dfb
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2007-07-24 08:46:46 +0000

    A scilab default declaration file

---
+++ scilab/modules/core/includes/scilabDefaults.h
@@ -0,0 +1,68 @@
+/*
+ * This file contains default paths for Scilab
+ * @copyright INRIA 2007
+ * @author Sylvestre LEDRU 
+ */
+#ifndef __SCILABDEFAULT_H__
+#define __SCILABDEFAULT_H__
+#include "machine.h"
+
+/* Where is stored the history by default */
+#ifdef _MSC_VER
+#define DEFAULT_HISTORY_FILE "history.scilab"
+#else
+#define DEFAULT_HISTORY_FILE ".history.scilab"
+#endif
+
+/* Where is the modelicac compiler 
+ * @TODO : update for Linux ... it is a wrong path */
+#ifdef _MSC_VER
+#define ModelicacName "/modules/scicos/bin/modelicac.exe"
+#else
+#define ModelicacName "/modules/scicos/bin/modelicac"
+#endif
+
+/* What is the name of the javasci librairies and where to find it (Windows) */
+#ifdef _MSC_VER
+#define JavaSCIName "/bin/javasci.dll"
+#else
+#define JavaSCIName "libjavasci"
+#endif
+
+/* Where the module list file is */
+#define basenamemodulesfile "etc/modules.xml" 
+
+/* Where the classpath list file is */
+#define XMLCLASSPATH "%s/etc/classpath.xml"
+
+/* Where the library list file is */
+#define XMLLIBRARYPATH "%s/etc/librarypath.xml"
+
+/* Name of the localisation XML file containing the errors message strings */
+#define FILELOCALIZATIONERROR "errors"
+
+/* Name of the localisation XML file containing the message strings */
+#define FILELOCALIZATIONMSGS "messages"
+
+/* Name of the localisation XML file containing the menu strings */
+#define FILELOCALIZATIONMENUS "menus"
+
+/* Where the localizations files can be found */
+#define FILELOCALIZATIONFORMATPATH "%s/modules/%s/languages/%s/%s.xml"
+
+/* Where is the gateway file of a module */
+#define FORMATGATEWAYFILENAME "%s/modules/%s/sci_gateway/%s_gateway.xml"
+
+/* Where is the version file of a module */
+#define FORMATVERSIONFILENAME  "%s/modules/%s/version.xml" 
+
+
+
+static char DefaultScilabStartup[]="SCI/etc/scilab.start";
+// not used at the moment
+//static char DefaultScilabQuit[]="SCI/etc/scilab.quit";
+//static char DefaultSCIenv[]="../..";
+static int DefaultStacksize=1000000;
+
+
+#endif /*__SCILABDEFAULT_H__ */
