4b977373061911d4a49a7e4296fdd648a831c770
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2007-07-24 14:14:06 +0000

    console in command line

--- scilab/modules/gui/src/nogui/nogui.c
+++ scilab/modules/gui/src/nogui/nogui.c
@@ -3,6 +3,7 @@
 /* @author Allan CORNET */
 /* @author Sylvestre LEDRU */
 /*-----------------------------------------------------------------------------------*/ 
+#include <errno.h>
 #include "machine.h" 
 #include "Scierror.h"
 #include "sciprint.h"
@@ -53,8 +54,69 @@ int GetScreenProperty(char *prop, char *value){
 	return 0;
 }
 /*-----------------------------------------------------------------------------------*/
+#define LF                    0x000a
 int Xorgetchar(int interrupt){
-	return getchar();
+	int i;
+int inter_max_plus1 = 0;
+	static fd_set Select_mask_ref,select_mask,Write_mask_ref,write_mask;
+	static struct timeval select_timeout;
+	static int fd_in=0,fd_out=0,fd_err=0 ;
+  static int state = 0;
+  fd_in = fileno(stdin) ;
+  fd_out = fileno(stdout);
+  fd_err = fileno(stderr);
+
+  FD_ZERO(&Select_mask_ref);
+  FD_SET(fd_in , &Select_mask_ref);
+  FD_ZERO(&Write_mask_ref);
+
+  inter_max_plus1 = fd_in;
+
+  inter_max_plus1 = Max(fd_out,inter_max_plus1);
+  inter_max_plus1 = Max(fd_err,inter_max_plus1);
+  inter_max_plus1++;
+
+	  for( ; ; ) {
+    fflush(stdout); 
+    fflush(stderr); 
+
+    /* Initialize masks  */
+    select_mask = Select_mask_ref;
+    write_mask  = Write_mask_ref;
+
+    select_timeout.tv_sec = 0;
+    select_timeout.tv_usec = 10;
+    i = select(inter_max_plus1, &select_mask, &write_mask, (fd_set *)NULL, &select_timeout);
+    if (i < 0) {
+		if (errno != EINTR) /* EINTR  A signal was caught. */
+	{ 
+	  sciprint("Error.\n");
+	  exit(0);
+	  continue;
+	}
+    } 
+
+    /* if there's something to output */
+    if ( FD_ISSET(fd_out,&write_mask)) { 
+      fflush(stdout); 
+    }
+    if ( FD_ISSET(fd_err,&write_mask)) { 
+      fflush(stderr); 
+    }
+
+    /* if there's something to read */
+    if (FD_ISSET(fd_in,&select_mask)) 
+      state=1;
+
+    if (state) {
+		i=getchar();
+      if (i==LF) state=0;
+      return(i);
+    }
+	
+    if (interrupt&&(ismenu()==1)) return(-1);
+
+  }
 }
 /*-----------------------------------------------------------------------------------*/
 int XEvorgetchar(int interrupt){
