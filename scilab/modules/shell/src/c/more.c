412b8c7a7e4431ad9a1e090a7f7927630a896863
Allan Cornet <allan.cornet@scilab.org>
2007-08-29 06:41:17 +0000

    update linesmore

--- scilab/modules/shell/src/c/more.c
+++ scilab/modules/shell/src/c/more.c
@@ -7,8 +7,9 @@
 #include "scilabmode.h"
 #include "sciprint.h"
 #include "../../../console/includes/GetCharWithoutOutput.h"
+#include "../../../console/includes/ShellPrintf.h"
 /*-----------------------------------------------------------------------------------*/ 
-#define MSG_MORE_NW " more ? "
+#define MSG_MORE "[More (y or n ) ?] "
 /*-----------------------------------------------------------------------------------*/ 
 int linesmore(void)
 {
@@ -17,15 +18,18 @@ int linesmore(void)
 	{
 		int ch = 0;
 		/* Scilab has not his own window */
-		sciprint(MSG_MORE_NW);
+		sciprint(MSG_MORE);
 		ch = getchar();
 		if ( (ch != ' ') && (ch != '\n') && (ch != 'y') ) retval = 1;
 	}
 	else
 	{
-		int ch = 0;
 		/* scilab has his own window */
-		/* retval = 0 */
+		int ch = 0;
+		ShellPrintf(MSG_MORE);
+		ch = GetCharWithoutOutput();
+		if ( ch == 110 ) retval = 1;
+		ShellPrintf("\n");
 	}
 	return retval;
 }
