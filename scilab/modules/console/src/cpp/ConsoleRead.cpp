5f8c3cc68e3245e26a7e24d0d720c6ebb7deabc3
Bruno Jofret <bruno.jofret@scilab.org>
2007-10-01 13:43:09 +0000

    Let's try this : avoid useless stuff

--- scilab/modules/console/src/cpp/ConsoleRead.cpp
+++ scilab/modules/console/src/cpp/ConsoleRead.cpp
@@ -3,16 +3,15 @@
 * @author Vincent COUVERT - INRIA 2007
 */
 /*-----------------------------------------------------------------------------------*/
-#include <string>
 #include "ConsoleRead.hxx"
 /*-----------------------------------------------------------------------------------*/
+#define WK_BUF_SIZE 520
+/*-----------------------------------------------------------------------------------*/
 using namespace  org_scilab_modules_gui_bridge_console;
-
 char *ConsoleRead()
 {
   /* Character string to be returned */
-  std::string strRead;
-  char *strValue = NULL;
+  char *strRead = NULL;
 
   JavaVM scilabJVM = *getScilabJavaVM();
   jobject scilabConsoleObj = GetScilabConsoleObject();
@@ -21,19 +20,12 @@ char *ConsoleRead()
     {
       SwingScilabConsole *jConsole = new SwingScilabConsole(&scilabJVM, scilabConsoleObj);
 
-      strValue = (*jConsole).readLine();
-
-      if (strValue)
-      {
-	   strRead = *new std::string(strRead);
-	   free(strValue); // malloc in (*jConsole).readLine
-	   strValue = NULL;
-      }
+      strRead = (*jConsole).readLine();
 
       delete(jConsole);
     }
 
-  return (char *) strRead.c_str();
+  return strRead;
 }
 /*-----------------------------------------------------------------------------------*/
 
