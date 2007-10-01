ef2bdb313369f120a594d69d360388a80ef45c7b
Bruno Jofret <bruno.jofret@scilab.org>
2007-10-01 13:08:32 +0000

    Well, let's have some C++ code in the .cpp files...

--- scilab/modules/console/src/cpp/ConsoleRead.cpp
+++ scilab/modules/console/src/cpp/ConsoleRead.cpp
@@ -3,38 +3,37 @@
 * @author Vincent COUVERT - INRIA 2007
 */
 /*-----------------------------------------------------------------------------------*/
+#include <string>
 #include "ConsoleRead.hxx"
 /*-----------------------------------------------------------------------------------*/
-#define WK_BUF_SIZE 520
-/*-----------------------------------------------------------------------------------*/
 using namespace  org_scilab_modules_gui_bridge_console;
+
 char *ConsoleRead()
 {
   /* Character string to be returned */
-  char *strRead = NULL;
+  std::string strRead;
   char *strValue = NULL;
 
   JavaVM scilabJVM = *getScilabJavaVM();
   jobject scilabConsoleObj = GetScilabConsoleObject();
 
-  if (scilabConsoleObj) 
+  if (scilabConsoleObj)
     {
       SwingScilabConsole *jConsole = new SwingScilabConsole(&scilabJVM, scilabConsoleObj);
-      
+
       strValue = (*jConsole).readLine();
 
       if (strValue)
       {
-         strRead = (char*)MALLOC(sizeof(char)*(WK_BUF_SIZE+1));
-         if (strRead) strcpy(strRead,strValue);
-		 free(strValue); // malloc in (*jConsole).readLine
-		 strValue = NULL;
+	   strRead = *new std::string(strRead);
+	   free(strValue); // malloc in (*jConsole).readLine
+	   strValue = NULL;
       }
-      
+
       delete(jConsole);
     }
 
-  return strRead;
+  return (char *) strRead.c_str();
 }
 /*-----------------------------------------------------------------------------------*/
 
