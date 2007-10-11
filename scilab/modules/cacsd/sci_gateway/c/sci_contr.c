447e79558c888e441e483970bd1b274fbe365a25
Bruno Jofret <bruno.jofret@scilab.org>
2007-10-11 11:44:29 +0000

    Warning down... Who's next ??

--- scilab/modules/cacsd/sci_gateway/c/sci_contr.c
+++ scilab/modules/cacsd/sci_gateway/c/sci_contr.c
@@ -1,23 +1,23 @@
-/*-----------------------------------------------------------------------------------*/ 
+/*-----------------------------------------------------------------------------------*/
 /* INRIA */
-/*-----------------------------------------------------------------------------------*/ 
+/*-----------------------------------------------------------------------------------*/
 #include "machine.h"
 #include "stack-c.h"
 #include <string.h>
 #include "sci_contr.h"
-/*-----------------------------------------------------------------------------------*/ 
+/*-----------------------------------------------------------------------------------*/
 #define MAX(x,y)	(((x)>(y))?(x):(y))
-/*-----------------------------------------------------------------------------------*/ 
+/*-----------------------------------------------------------------------------------*/
 extern double C2F(dlamch)  __PARAMS((char *CMACH, unsigned long int));
 extern int C2F(ab01od)();
-/*-----------------------------------------------------------------------------------*/ 
+/*-----------------------------------------------------------------------------------*/
 int intab01od(char* fname)
-{ 
+{
 	int mA,nA,ptrA, mB,nB, ptrB;
 	int A,B,U,KSTAIR,V, ptrIWORK,ptrU,ptrTOL;
 	int ptrKSTAIR,ptrV,ptrDWORK,ptrJUNK,ptrNCONT;
 	int LDA, LDB, LDU, LDV, LDWORK;
-	int N, M, mtol, ntol; int un; int INFO, INDCON, NCONT;
+	int N, M, mtol, ntol; int un, one; int INFO, INDCON, NCONT;
 	char  *JOBU, *JOBV;
 	double theTOL;
 	int minlhs=1, minrhs=2, maxlhs=6, maxrhs=3;
@@ -37,7 +37,7 @@ int intab01od(char* fname)
 		/*    TOL is given:   ab01od(A,B,tol)   */
 		GetRhsVar(3,MATRIX_OF_DOUBLE_DATATYPE,&mtol,&ntol,&ptrTOL);  theTOL=*stk(ptrTOL);    /*     TOL */
 		if (theTOL>1.0||theTOL<0.0) {
-			Scierror(999,"TOL must be in [0 1]\n");  return 0; 
+			Scierror(999,"TOL must be in [0 1]\n");  return 0;
 		}
 	}
 
@@ -56,9 +56,9 @@ int intab01od(char* fname)
 	CreateVar(Rhs+4,MATRIX_OF_DOUBLE_DATATYPE,&M,&M,&ptrV);  V=Rhs+4;
 	CreateVar(Rhs+5,MATRIX_OF_INTEGER_DATATYPE,(un=1,&un),&M,&ptrIWORK);
 	CreateVar(Rhs+6,MATRIX_OF_DOUBLE_DATATYPE,(un=1,&un),&LDWORK,&ptrDWORK);
-	C2F(ab01od)( "A", JOBU, JOBV, &N, &M, stk(ptrA), &LDA, 
-		stk(ptrB), &LDB, stk(ptrU), &LDU, stk(ptrV), &LDV, 
-		istk(ptrNCONT), &INDCON, istk(ptrKSTAIR), &theTOL,  
+	C2F(ab01od)( "A", JOBU, JOBV, &N, &M, stk(ptrA), &LDA,
+		stk(ptrB), &LDB, stk(ptrU), &LDU, stk(ptrV), &LDV,
+		istk(ptrNCONT), &INDCON, istk(ptrKSTAIR), &theTOL,
 		istk(ptrIWORK), stk(ptrDWORK), &LDWORK, &INFO );
 	if (INFO != 0) {
 		C2F(errorinfo)("ab01od", &INFO, 6L);
@@ -66,13 +66,13 @@ int intab01od(char* fname)
 	}
 	if (Lhs >= 3) {
 		/*     resizing KSTAIR      */
-		CreateVar(Rhs+7,MATRIX_OF_INTEGER_DATATYPE,(un=1,&un),&INDCON,&ptrJUNK); 
+		CreateVar(Rhs+7,MATRIX_OF_INTEGER_DATATYPE,(un=1,&un),&INDCON,&ptrJUNK);
 		KSTAIR=Rhs+7;
-		C2F(icopy)(&INDCON,istk(ptrKSTAIR),(un=1,&un),istk(ptrJUNK),(un=1,&un)); }
+		C2F(icopy)(&INDCON,istk(ptrKSTAIR),(un=1,&un),istk(ptrJUNK),(one=1,&one)); }
 	/*     lhs variables: [NCONT,U,KSTAIR,V,A,B]=ab01od(A,B)   */
-	LhsVar(1)=NCONT; LhsVar(2)=U; 
-	LhsVar(3)=KSTAIR; LhsVar(4)=V; 
+	LhsVar(1)=NCONT; LhsVar(2)=U;
+	LhsVar(3)=KSTAIR; LhsVar(4)=V;
 	LhsVar(5)=A; LhsVar(6)=B;
 	return 0;
 }
-/*-----------------------------------------------------------------------------------*/ 
+/*-----------------------------------------------------------------------------------*/
