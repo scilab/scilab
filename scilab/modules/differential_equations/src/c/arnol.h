28fea2162b4d82db3368f14b01906eb0b931baf7
Bruno Jofret <bruno.jofret@scilab.org>
2007-10-17 15:15:05 +0000

    Warning Down... Adding some include...

---
+++ scilab/modules/differential_equations/src/c/arnol.h
@@ -0,0 +1,21 @@
+/*
+**  -*- C -*-
+**
+** arnol.h
+** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
+**
+** Started on  Tue Oct 16 16:13:34 2007 bruno
+** Last update Tue Oct 16 16:15:37 2007 bruno
+**
+** Copyright INRIA 2007
+*/
+
+#ifndef __ARNOL__
+#define __ARNOL__
+
+#include <math.h>
+#include "machine.h"
+
+int C2F(arnol)(int *neq, double *t, double *y,	double *ydot);
+
+#endif /* !__ARNOL__ */
