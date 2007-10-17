f203816d188ab7d509ad52d243c6e509a2553bbc
Bruno Jofret <bruno.jofret@scilab.org>
2007-10-17 15:13:45 +0000

    Warning Down... Moving some static references...

--- scilab/modules/core/src/c/callinterf.c
+++ scilab/modules/core/src/c/callinterf.c
@@ -1,6 +1,6 @@
 /**************************************
- * Copyright Jean-Philippe Chancelier 
- * ENPC 
+ * Copyright Jean-Philippe Chancelier
+ * ENPC
  **************************************/
 #include <setjmp.h>
 #include <stdio.h>
@@ -20,40 +20,114 @@
 
 #include "machine.h"
 
-static  jmp_buf jmp_env; 
+static  jmp_buf jmp_env;
 
 extern int C2F(error) __PARAMS((int *));
 
 static void sci_sigint_addinter(int n);
 
+/**
+ ** Static function table
+ ** Watch out the positions are crutial !!!
+ ** @TODO : Make this less crappy...
+ **/
+static OpTab Interfaces[] = {
+    /* 01  */ {C2F(gw_user)}, /* free position may be used */
+	/* 02  */ {C2F(gw_linear_algebra)},
+	/* 03  */ {C2F(gw_user)}, /* free position may be used */
+	/* 04  */ {C2F(gw_user)}, /* free position may be used */
+	/* 05  */ {C2F(gw_io)},
+	/* 06  */ {C2F(gw_elementaries_functions)},
+	/* 07  */ {C2F(gw_graphics)},
+	/* 08  */ {C2F(gw_cacsd0)},
+	/* 09  */ {C2F(gw_cacsd1)},
+	/* 10  */ {C2F(gw_cacsd2)},
+	/* 11  */ {C2F(gw_optim)},
+	/* 12  */ {C2F(gw_differential_equations1)},
+	/* 13  */ {C2F(gw_core)},
+	/* 14  */ {C2F(gw_user)},
+	/* 15  */ {C2F(gw_metanet)},
+	/* 16  */ {C2F(gw_polynomials)},
+	/* 17  */ {C2F(gwdatastructures1)},
+	/* 18  */ {C2F(gw_signal)},
+	/* 19  */ {C2F(gw_interpolation)},
+	/* 20  */ {C2F(gw_cacsd3)},
+	/* 21  */ {C2F(gw_string)},
+	/* 22  */ {C2F(gw_symbolic)},
+	/* 23  */ {C2F(gw_boolean)},
+	/* 24  */ {C2F(gw_user2)},
+	/* 25  */ {C2F(gw_gui)},
+	/* 26  */ {C2F(gw_differential_equations2)},
+	/* 27  */ {C2F(gw_sparse)},
+	/* 28  */ {C2F(gw_slicot)},
+	/* 29  */ {C2F(gw_differential_equations3)},
+	/* 30  */ {C2F(gw_differential_equations4)},
+	/* 31  */ {C2F(gw_differential_equations5)},
+	/* 32  */ {C2F(gw_differential_equations6)},
+	/* 33  */ {C2F(gw_user2)}, /* free position may be used */
+	/* 34  */ {C2F(gw_fileio)},
+	/* 46  */ {C2F(gw_arnoldi)},
+	/* 36  */ {C2F(gw_special_functions1)},
+	/* 37  */ {C2F(gw_statistics)},
+	/* 38  */ {C2F(gw_randlib)},
+	/* 39  */ {C2F(gw_special_functions2)},
+	/* 40  */ {C2F(gw_tclsci)},
+	/* 41  */ {C2F(gwdatastructures2)},
+	/* 42  */ {C2F(gw_pvm)},
+	/* 43  */ {C2F(gw_integer)},
+	/* 44  */ {C2F(gw_linear_algebra2)},
+	/* 45  */ {C2F(gw_scicos)},
+	/* 46  */ {C2F(gw_cscicos)},
+	/* 47  */ {C2F(gw_windows_tools)},
+	/* 48  */ {C2F(gw_time)},
+	/* 49  */ {C2F(gw_sound)},
+	/* 50  */ {C2F(gw_localization)},
+	/* 51  */ {C2F(gw_fftw)},
+	/* 52  */ {C2F(gw_jvm)},
+	/* 53  */ {C2F(gw_shell)},
+	/* 54  */ {C2F(gw_completion)},
+	/* 55  */ {C2F(gw_history_manager)},
+};
+
+
+
 /***********************************************************
- * interface function 
+ * interface function
  ***********************************************************/
 
-static int c_local_interf = 9999;
+/**
+ ** Bruno : Unused, but may be used one day ?? Just commented
+ ** Just for you Sly... God bless my kindness
+ **/
 
-int C2F(Nogw_slicot)(void)
-{
-	message_scilab(_("%s interface not loaded."),"slicot");
-	C2F(error)(&c_local_interf);
-	return 0;
-}
+/** { */
+
+/* static int c_local_interf = 9999; */
+
+/* int C2F(Nogw_slicot)(void) */
+/* { */
+/* 	message_scilab("%s interface not loaded.","slicot"); */
+/* 	C2F(error)(&c_local_interf); */
+/* 	return 0; */
+/* } */
+
+/** } */
 
 static int sig_ok = 0;
 
 
 /**
- * call the apropriate interface according to the value of k 
+ * call the apropriate interface according to the value of k
  * iflagint is no more used here ....
  * @param k the number of the interface
  * @param iflagint obsolete (no longer used)
- * @return 
+ * @return
  */
 int C2F(callinterf) (int *k)
 {
   int returned_from_longjump ;
   static int count = 0;
-  if ( count == 0) 
+  if ( count == 0)
     {
       if (sig_ok) signal(SIGINT,sci_sigint_addinter);
       if (( returned_from_longjump = setjmp(jmp_env)) != 0 )
@@ -65,12 +139,12 @@ int C2F(callinterf) (int *k)
 	}
     }
   count++;
-  if (*k > DynInterfStart) 
+  if (*k > DynInterfStart)
     C2F(userlk)(k);
   else
     (*(Interfaces[*k-1].fonc))();
   count--;
-  if (count == 0) { 
+  if (count == 0) {
     if (sig_ok) signal(SIGINT, controlC_handler);
   }
   return 0;
@@ -88,7 +162,10 @@ static void sci_sigint_addinter(int n)
 }
 
 
+/*-------------------------------------
+ * long jump to stop interface computation
+ *-------------------------------------*/
 void errjump(int n)
 {
-  longjmp(jmp_env,-1); 
+  longjmp(jmp_env,-1);
 }
--- scilab/modules/core/src/c/callinterf.h
+++ scilab/modules/core/src/c/callinterf.h
@@ -4,18 +4,18 @@
 #include "machine.h"
 
 /********************************************************
- * Table of hard coded Scilab interface 
- *  to add a new interface add a declaration and a new entry 
- *  at the end of the Interfaces array 
- *  The maximum number of interfaces is bound to DynInterfStart 
- *  ( file /core/src/c/addinter.h ) 
+ * Table of hard coded Scilab interface
+ *  to add a new interface add a declaration and a new entry
+ *  at the end of the Interfaces array
+ *  The maximum number of interfaces is bound to DynInterfStart
+ *  ( file /core/src/c/addinter.h )
  ********************************************************/
 
-/* 01  C2F(gw_user)(); FREE */	#include "gw_user.h" 
-/* 02 */						#include "../../../linear_algebra/includes/gw_linear_algebra.h" 
+/* 01  C2F(gw_user)(); FREE */	#include "gw_user.h"
+/* 02 */						#include "../../../linear_algebra/includes/gw_linear_algebra.h"
 /* 03  C2F(gw_user)(); FREE */
 /* 04  C2F(gw_user)(); FREE */
-/* 05 */						#include "../../../io/includes/gw_io.h" 
+/* 05 */						#include "../../../io/includes/gw_io.h"
 /* 06 */						#include "../../../elementaries_functions/includes/gw_elementaries_functions.h"
 /* 07 */						#include "../../../graphics/includes/gw_graphics.h"
 /* 08 */						#include "../../../cacsd/includes/gw_cacsd0.h"
@@ -34,7 +34,7 @@
 /* 21 */						#include "../../../string/includes/gw_string.h"
 /* 22 */						#include "../../../symbolic/includes/gw_symbolic.h"
 /* 23 */						#include "../../../boolean/includes/gw_boolean.h"
-/* 24  C2F(gw_user2)(); FREE */	#include "gw_user2.h" 
+/* 24  C2F(gw_user2)(); FREE */	#include "gw_user2.h"
 /* 25 */						#include "../../../gui/includes/gw_gui.h"
 /* 26 */						#include "../../../differential_equations/includes/gw_differential_equations2.h"
 /* 27 */						#include "../../../sparse/includes/gw_sparse.h"
@@ -58,7 +58,7 @@
 /* 42 */						#include "../../../pvm/includes/gw_pvm.h"
 
 /* 43 */						#include "../../../integer/includes/gw_integer.h"
-/* 44 */						#include "../../../linear_algebra/includes/gw_linear_algebra2.h" 
+/* 44 */						#include "../../../linear_algebra/includes/gw_linear_algebra2.h"
 
 /* 45 */						#include "../../../scicos/includes/gw_scicos.h"
 
@@ -80,7 +80,7 @@
 
 
 /*-------------------------------------
- * long jump to stop interface computation 
+ * long jump to stop interface computation
  * @param n
  *-------------------------------------*/
 
@@ -93,71 +93,10 @@ typedef struct {
 	int  (*fonc)(void);
 } OpTab ;
 
-
-static OpTab Interfaces[] = {
-    /* 01  */ {C2F(gw_user)}, /* free position may be used */
-	/* 02  */ {C2F(gw_linear_algebra)},
-	/* 03  */ {C2F(gw_user)}, /* free position may be used */
-	/* 04  */ {C2F(gw_user)}, /* free position may be used */
-	/* 05  */ {C2F(gw_io)},
-	/* 06  */ {C2F(gw_elementaries_functions)},
-	/* 07  */ {C2F(gw_graphics)},
-	/* 08  */ {C2F(gw_cacsd0)},
-	/* 09  */ {C2F(gw_cacsd1)},
-	/* 10  */ {C2F(gw_cacsd2)},
-	/* 11  */ {C2F(gw_optim)},
-	/* 12  */ {C2F(gw_differential_equations1)},
-	/* 13  */ {C2F(gw_core)},
-	/* 14  */ {C2F(gw_user)},
-	/* 15  */ {C2F(gw_metanet)},
-	/* 16  */ {C2F(gw_polynomials)},
-	/* 17  */ {C2F(gwdatastructures1)},
-	/* 18  */ {C2F(gw_signal)},
-	/* 19  */ {C2F(gw_interpolation)},
-	/* 20  */ {C2F(gw_cacsd3)},
-	/* 21  */ {C2F(gw_string)},
-	/* 22  */ {C2F(gw_symbolic)},
-	/* 23  */ {C2F(gw_boolean)},
-	/* 24  */ {C2F(gw_user2)},
-	/* 25  */ {C2F(gw_gui)},
-	/* 26  */ {C2F(gw_differential_equations2)},
-	/* 27  */ {C2F(gw_sparse)},
-	/* 28  */ {C2F(gw_slicot)},
-	/* 29  */ {C2F(gw_differential_equations3)},
-	/* 30  */ {C2F(gw_differential_equations4)},
-	/* 31  */ {C2F(gw_differential_equations5)},
-	/* 32  */ {C2F(gw_differential_equations6)},
-	/* 33  */ {C2F(gw_user2)}, /* free position may be used */
-	/* 34  */ {C2F(gw_fileio)},
-	/* 46  */ {C2F(gw_arnoldi)},
-	/* 36  */ {C2F(gw_special_functions1)},
-	/* 37  */ {C2F(gw_statistics)},
-	/* 38  */ {C2F(gw_randlib)},
-	/* 39  */ {C2F(gw_special_functions2)},
-	/* 40  */ {C2F(gw_tclsci)},
-	/* 41  */ {C2F(gwdatastructures2)},
-	/* 42  */ {C2F(gw_pvm)},
-	/* 43  */ {C2F(gw_integer)},
-	/* 44  */ {C2F(gw_linear_algebra2)},
-	/* 45  */ {C2F(gw_scicos)},  
-	/* 46  */ {C2F(gw_cscicos)},
-	/* 47  */ {C2F(gw_windows_tools)},
-	/* 48  */ {C2F(gw_time)},
-	/* 49  */ {C2F(gw_sound)},
-	/* 50  */ {C2F(gw_localization)},
-	/* 51  */ {C2F(gw_fftw)},
-	/* 52  */ {C2F(gw_jvm)},
-	/* 53  */ {C2F(gw_shell)},
-	/* 54  */ {C2F(gw_completion)},
-	/* 55  */ {C2F(gw_history_manager)},
-	//	/* 55  */ {C2F(gw_mpi)}
-};
-
-
 /**
- * call the apropriate interface according to the value of k 
+ * call the apropriate interface according to the value of k
  * @param k the number of the interface
- * @return 
+ * @return
  */
 int C2F(callinterf) (int *k);
 
