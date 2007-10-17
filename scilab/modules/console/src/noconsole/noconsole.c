1b25efb1aa7b71622a39217217c756a42a2f10d6
Bruno Jofret <bruno.jofret@scilab.org>
2007-10-17 08:35:56 +0000

    Warning Down... Comment useless function. Add some includes.

--- scilab/modules/console/src/noconsole/noconsole.c
+++ scilab/modules/console/src/noconsole/noconsole.c
@@ -1,29 +1,35 @@
 /*-----------------------------------------------------------------------------------*/
 /* INRIA 2007 */
 /* Allan CORNET */
-/*-----------------------------------------------------------------------------------*/ 
+/*-----------------------------------------------------------------------------------*/
 #include <stdlib.h>
-#include "machine.h" 
+#include "machine.h"
 #include "ConsolePrintf.h"
 #include "ConsoleRead.h"
 #include "ClearConsolePart.h"
 #include "ClearConsole.h"
+#include "SetConsolePrompt.h"
+#include "InitializeConsole.h"
+#include "TerminateConsole.h"
 #include "PromptToHome.h"
 #include "MALLOC.h"
 #include "Scierror.h"
 #include "GetCharWithoutOutput.h"
-/*-----------------------------------------------------------------------------------*/ 
+/*-----------------------------------------------------------------------------------*/
+/*
+** Bruno : Useless...
 int C2F(gw_console)()
 {
-	Scierror(999,"console interface not installed.\n");
-	return 0;
+Scierror(999,"console interface not installed.\n");
+return 0;
 }
-/*-----------------------------------------------------------------------------------*/ 
+*/
+/*-----------------------------------------------------------------------------------*/
 char *ConsoleRead(void)
 {
 	return NULL;
 }
-/*-----------------------------------------------------------------------------------*/ 
+/*-----------------------------------------------------------------------------------*/
 int ConsolePrintf(char *line)
 {
 	int len = 0;
