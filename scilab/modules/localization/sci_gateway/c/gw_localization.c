7ab2afda3cb04d7a11cb453bf028eb7127c2d101
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2007-10-19 15:38:45 +0000

    _ is an alias of gettext

--- scilab/modules/localization/sci_gateway/c/gw_localization.c
+++ scilab/modules/localization/sci_gateway/c/gw_localization.c
@@ -11,7 +11,8 @@ static gw_generic_table Tab[]=
 {
  		{C2F(sci_setlanguage),"setlanguage"},
 		{C2F(sci_getlanguage),"getlanguage"},
-		{C2F(sci_gettext),"gettext"}
+		{C2F(sci_gettext),"gettext"},
+		{C2F(sci_gettext),"_"}
 };
 			/*	{C2F(sci_getlanguagealias),"getlanguagealias"},
 	{C2F(sci_getlanguagestable),"getlanguagestable"},
