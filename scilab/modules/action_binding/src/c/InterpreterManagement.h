d44ed6ae2b1b4a5055c2688f9ecaeb1058b37e61
Vincent Couvert <vincent.couvert@scilab.org>
2007-10-26 08:34:12 +0000

    First version of this module, not yet used...see my following commits

---
+++ scilab/modules/action_binding/src/c/InterpreterManagement.h
@@ -0,0 +1,34 @@
+/*-----------------------------------------------------------------------------------*/
+/** 
+ * @copyright INRIA 2007
+ * @author Vincent COUVERT
+ */
+/*-----------------------------------------------------------------------------------*/
+#ifndef __INTERPRETERMANAGEMENT_H__
+#define __INTERPRETERMANAGEMENT_H__
+
+#include "machine.h"
+
+/**
+ * Put a command in Scilab command queue so that Scilab executes it
+ * The command is displayed in Scilab Window
+ *
+ * @param command the command to execute
+ * @return execution status
+ */
+int putCommandInScilabQueue(char *command);
+
+/*
+* exec ScilabEventLoop
+* @return execution status
+*/
+int execScilabEventLoop(void);
+
+/*
+* Stops Scilab current work
+* @return execution status
+*/
+int interruptScilab(void);
+/*-----------------------------------------------------------------------------------*/
+
+#endif /* __INTERPRETERMANAGEMENT_H__ */
