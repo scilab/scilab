d314c1d3407602fbc35dc4844cb251bf9e1c6576
Bruno Jofret <bruno.jofret@scilab.org>
2007-10-26 13:50:57 +0000

    erro function addition.

---
+++ scilab/modules/output_stream/includes/erro.h
@@ -0,0 +1,21 @@
+/*
+**  -*- C -*-
+**
+** erro.h
+** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
+**
+** Started on  Thu Oct 25 11:02:46 2007 bruno
+** Last update Thu Oct 25 11:03:45 2007 bruno
+**
+** Copyright INRIA 2007
+*/
+
+#ifndef __ERRO_H__
+#define __ERRO_H__
+
+#include "machine.h"
+#include "Scierror.h"
+
+int C2F(erro)(char *str,unsigned int str_len);
+
+#endif /* !__ERRO_H__ */
