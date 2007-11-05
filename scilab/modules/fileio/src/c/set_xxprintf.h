1c954270ca79e45d1e248cf3c56ae99c76cd9e48
Allan Cornet <allan.cornet@scilab.org>
2007-11-05 10:51:31 +0000

    add a comment

--- scilab/modules/fileio/src/c/set_xxprintf.h
+++ scilab/modules/fileio/src/c/set_xxprintf.h
@@ -15,8 +15,13 @@ typedef int (*FLUSH) __PARAMS((FILE *));
 #define  MAX_SPRINTF_SIZE  bsiz
 /*-----------------------------------------------------------------------------------*/
 
-
-
+/**
+* set output function  sprintf, sciprint2, fprintf
+* @param[in] fp "Pointer to a FILE object"
+* @param[out] xxprintf "Pointer to output function"
+* @param[in] flush flush
+* @param[out] target output "buffer"
+*/
 void set_xxprintf(FILE *fp,XXPRINTF *xxprintf,FLUSH *flush,char **target);
 
 #endif /* __SET_XXPRINTF_H__ */
