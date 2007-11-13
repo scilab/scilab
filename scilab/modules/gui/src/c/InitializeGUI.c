00c2b6591f58b098f9b391831d924eecd983719c
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2007-11-13 12:46:29 +0000

    Warning removed

--- scilab/modules/gui/src/c/InitializeGUI.c
+++ scilab/modules/gui/src/c/InitializeGUI.c
@@ -2,6 +2,7 @@
 /* INRIA 2007 */
 /* Allan CORNET */
 /*-----------------------------------------------------------------------------------*/ 
+#include <stdlib.h>
 #include "InitializeGUI.h"
 #include "scilabmode.h"
 #include "setMainWindowTitle.h"
@@ -10,19 +11,19 @@
 /*-----------------------------------------------------------------------------------*/ 
 BOOL InitializeGUI(void)
 {
-	BOOL bOK = FALSE;
 	if (getScilabMode() == SCILAB_STD)
 	{
 		char *title = buildMainWindowTitle();
 		if (title)
 		{
-			bOK = setMainWindowTitle(title);
+			BOOL bOK = setMainWindowTitle(title);
 			FREE(title);
 			title = NULL;
+			return bOK;
 		}
 	}
-	else bOK = TRUE;
+	else return TRUE;
 
-	return bOK; 
+	return FALSE; 
 }
 /*-----------------------------------------------------------------------------------*/ 
