e07f00ecbc98ceabad5cc78d2ef246ff17f261b3
Cong Wu <cong.wu@inria.fr>
2007-11-21 13:58:01 +0000

    added convstr.h

---
+++ scilab/modules/STRING_V2/src/c/convstr.c
@@ -0,0 +1,44 @@
+/*-----------------------------------------------------------------------------------*/
+/* INRIA 2007 */
+/*-----------------------------------------------------------------------------------*/
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h> 
+#include "gw_string.h"
+#include "MALLOC.h" 
+#include "convstr.h"
+/*-----------------------------------------------------------------------------------*/
+void convstr(char **Input_Matrix, char **Output_Matrix, char typ, int mn)
+{
+	int x = 0;
+	for (x =0 ; x < mn; x++)
+	{
+		int y = 0;
+		for (y=0;y < (int)strlen(Input_Matrix[x]);y++)
+		{
+			/*To traverse every string in the string matrix */
+			if (typ == UPPER || typ == UPPER_B) 
+			{
+				/*converts the matrix of strings  str-matrix  into upper case */
+				if    ( (Input_Matrix[x][y] >= Letter_a) && (Input_Matrix[x][y] <= Letter_z) )  
+				{
+					/* If it is the Lowercase letters */
+            		Output_Matrix[x][y] = Input_Matrix[x][y] - Conversion_Letter		;
+				}
+				else Output_Matrix[x][y]=Input_Matrix[x][y];
+			}
+			else if (typ==LOW || typ==LOW_B)  
+			{
+				/*converts the matrix of strings  str-matrix  into lower case */
+				if ( (Input_Matrix[x][y] >= Letter_A) && (Input_Matrix[x][y] <= Letter_Z) )
+				{
+					/* if it is the uppercase letters*/
+					Output_Matrix[x][y]=Input_Matrix[x][y]+Conversion_Letter		;
+				}
+                else Output_Matrix[x][y]=Input_Matrix[x][y];
+			}
+		}
+        Output_Matrix[x][y]=0;
+	}
+}
+/*-----------------------------------------------------------------------------------*/
---
+++ scilab/modules/STRING_V2/src/c/convstr.h
@@ -0,0 +1,27 @@
+/*-----------------------------------------------------------------------------------*/
+/* INRIA 2007 */
+/*-----------------------------------------------------------------------------------*/
+#ifndef __CONVSTR_H__
+#define __CONVSTR_H__
+#define LOW 'l'
+#define LOW_B 'L'
+#define UPPER 'u'
+#define UPPER_B 'U'
+#define Letter_a 97
+#define Letter_z 122
+#define Letter_A 65
+#define Letter_Z 90
+#define	Conversion_Letter 32
+
+/**
+* convstr case conversion 
+* param[in] Input_Matrix Input string matrix
+* param[out] Output_Matrix 
+* param[in] typ  'l' or 'u'  means low or upper
+* param[in] Number_Input   the number of input string matrix
+*/
+void convstr(char **Input_Matrix, char **Output_Matrix, char typ, int Number_Input);
+
+#endif /* __CONVSTR_H__ */
+/*-----------------------------------------------------------------------------------*/
+
