66ad4160e5ce4fd10b5ea3142a12ec20c637e65a
Allan Cornet <allan.cornet@scilab.org>
2007-11-21 14:49:03 +0000

    add Get_length.h

---
+++ scilab/modules/STRING_V2/src/c/Get_length.h
@@ -0,0 +1,18 @@
+/*------------------------------------------------------------------------*/
+/* Copyright INRIA 2007*/
+/*------------------------------------------------------------------------*/
+#ifndef __GET_LENGTH_H__
+#define __GET_LENGTH_H__
+
+/**
+* Get_length to get the length of output string matrix
+* @param[in] Input_String_One The first input string
+* @param[in] Input_String_Two The second input string
+* @param[in] Number_Input_One the number of input string matrix
+* @return  the length of the output string matrix
+*/
+
+int Get_length(char **Input_String_One,char **Input_String_Two,int Number_Input_One);
+
+#endif /* __GET_LENGTH_H__ */
+/*------------------------------------------------------------------------*/
