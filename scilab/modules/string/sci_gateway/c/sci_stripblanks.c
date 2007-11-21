e3278dcc24096104c7c51843fbb8b33ecddb3a1b
Bruno Jofret <bruno.jofret@scilab.org>
2007-11-21 15:37:37 +0000

    Warning Down...

--- scilab/modules/string/sci_gateway/c/sci_stripblanks.c
+++ scilab/modules/string/sci_gateway/c/sci_stripblanks.c
@@ -2,24 +2,25 @@
 /* File: sci_stripblanks.c                                                */
 /* Copyright INRIA 2007                                                   */
 /* @Authors : Cong Wu                                                     */
-/* desc : strips leading and trailing blanks (and tabs) of strings  
-          txt=stripblanks(txt[,tabs])   
-          Parameters 
-          txt : string or matrix of strings 
-          tabs : if TRUE then tabs are also stripped (default value is FALSE)*/                                    
+/* desc : strips leading and trailing blanks (and tabs) of strings
+          txt=stripblanks(txt[,tabs])
+          Parameters
+          txt : string or matrix of strings
+          tabs : if TRUE then tabs are also stripped (default value is FALSE)*/
 /*------------------------------------------------------------------------*/
 #include <string.h>
 #include <stdio.h>
 #include "machine.h"
 #include "gw_string.h"
 #include "stack-c.h"
-#include "MALLOC.h" 
+#include "MALLOC.h"
 #include "stripblanks.h"
+#include "Scierror.h"
 /*-------------------------------------------------------------------------------------*/
 int C2F(sci_stripblanks) _PARAMS((char *fname,unsigned long fname_len))
 {
 	char **Input_String_Matrix_One = NULL;
-	char **Output_String_Matrix = NULL; 
+	char **Output_String_Matrix = NULL;
 	int numRow = 0;        /*@ The row number of the output string matrix*/
 	int numCol = 0;        /*@ The col number of the output string matrix*/
 	int m1 = 0,n1 = 0,mn = 0,i = 0,j = 0;
@@ -33,13 +34,13 @@ int C2F(sci_stripblanks) _PARAMS((char *fname,unsigned long fname_len))
 	{
 		int Type_Two = VarType(2);
 
-		switch (Type_Two) 
+		switch (Type_Two)
 		{
 			case sci_boolean :
 			{
 				int m2 = 0, n2 = 0, l2 = 0;
 				GetRhsVar(2,MATRIX_OF_BOOLEAN_DATATYPE,&m2,&n2,&l2);
-				bREMOVE_TAB = (BOOL)*istk(l2);  
+				bREMOVE_TAB = (BOOL)*istk(l2);
 			}
 			break;
 			default :
@@ -47,10 +48,10 @@ int C2F(sci_stripblanks) _PARAMS((char *fname,unsigned long fname_len))
 				Scierror(999,"%s : second argument has a wrong type, expecting a boolean.\n",fname);
 				return 0;
 			}
-		} 
+		}
 	}
 
-	switch (Type_One) 
+	switch (Type_One)
 	{
 		case sci_matrix :
 			{
@@ -69,14 +70,14 @@ int C2F(sci_stripblanks) _PARAMS((char *fname,unsigned long fname_len))
 			break;
 		case sci_strings :
 			GetRhsVar(1,MATRIX_OF_STRING_DATATYPE,&m1,&n1,&Input_String_Matrix_One);
-	        mn = m1*n1; 
+	        mn = m1*n1;
 		break;
 		default :
 			Scierror(999,"%s : first argument has a wrong type, expecting scalar or string matrix.\n",fname);
 		return 0;
-	} 
+	}
 
-	if (mn > 0) Output_String_Matrix = (char**)MALLOC(sizeof(char*)*(mn));	
+	if (mn > 0) Output_String_Matrix = (char**)MALLOC(sizeof(char*)*(mn));
 	else Output_String_Matrix = NULL;
 
 	if (Output_String_Matrix == NULL)
@@ -90,7 +91,7 @@ int C2F(sci_stripblanks) _PARAMS((char *fname,unsigned long fname_len))
 		Output_String_Matrix[i] = (char*)MALLOC(sizeof(char)*(strlen(Input_String_Matrix_One[i])+1));
 		if (Output_String_Matrix[i] == NULL)
 		{
-			for (j=0; j<i ; j++) 
+			for (j=0; j<i ; j++)
 			{
 				if (Output_String_Matrix[j])  { FREE(Output_String_Matrix[j]) ; Output_String_Matrix[j] = NULL;}
 			}
@@ -105,7 +106,7 @@ int C2F(sci_stripblanks) _PARAMS((char *fname,unsigned long fname_len))
 	numRow   = m1;
 	numCol   = n1;
 	CreateVarFromPtr( Rhs+1,MATRIX_OF_STRING_DATATYPE, &numRow, &numCol, Output_String_Matrix); /*@ Output*/
-	LhsVar(1) = Rhs+1 ; 
+	LhsVar(1) = Rhs+1 ;
 	C2F(putlhsvar)();
 
 	/* free pointers */
@@ -114,7 +115,7 @@ int C2F(sci_stripblanks) _PARAMS((char *fname,unsigned long fname_len))
 		if (Output_String_Matrix[i])  { FREE(Output_String_Matrix[i]) ; Output_String_Matrix[i]=NULL;}
 	}
 	if (Output_String_Matrix)  { FREE(Output_String_Matrix) ; Output_String_Matrix=NULL;}
-	 
+
 	return 0;
   }
 /*-----------------------------------------------------------------------------------*/
