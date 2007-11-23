ca556f8f0855bbb472056f75b1ec17809c02de84
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2007-11-23 16:03:26 +0000

    Catch the exception is case of problem

--- scilab/modules/jvm/src/java/org/scilab/modules/jvm/Scilab.java
+++ scilab/modules/jvm/src/java/org/scilab/modules/jvm/Scilab.java
@@ -15,6 +15,7 @@ import org.scilab.modules.gui.utils.LookAndFeel;
 /**
  * Main Class for Scilab
  * @author Allan CORNET INRIA 2007
+ * @author Sylvestre Ledru INRIA 2007
   */
 public class Scilab {
 	
@@ -34,19 +35,39 @@ public class Scilab {
 		this.mode = mode;
 		LookAndFeel.setSystemLookAndFeel();
 		if (mode == 2) {
-			
-			mainView = ScilabWindow.createWindow();
-			mainView.draw();
-			mainView.setTitle("Scilab-5.0");
+
+			try {
+				mainView = ScilabWindow.createWindow();
+				mainView.draw();
+				mainView.setTitle("Scilab-5.0");
+			}catch (NoClassDefFoundError exception){
+				System.err.println("Cannot create Scilab Window.\nCheck if the thirdparties are available (Flexdock, JOGL...).");
+				System.err.println("Could not find class: "+exception.getLocalizedMessage());
+				System.exit(-1);
+			}
 			/* CONSOLE */
 			/* Create a tab to put console into */
 			Tab consoleTab = ScilabTab.createTab("Console Scilab");
 			consoleTab.setName("Console");
 			mainView.addTab(consoleTab);
+
 			/* Create the console */
-			sciConsole = ScilabConsole.createConsole();
-			consoleTab.addMember(sciConsole);
-			mainView.draw();
+			try {
+				sciConsole = ScilabConsole.createConsole();
+				consoleTab.addMember(sciConsole);
+			}catch (NoClassDefFoundError exception){
+				System.err.println("Cannot create Scilab Console.\nCheck if the thirdparties are available (Rosetta/Jrosetta...).");
+				System.err.println("Could not find class: "+exception.getLocalizedMessage());
+				System.exit(-1);
+			}
+
+			try {
+				mainView.draw();
+			}catch(UnsatisfiedLinkError exception) {
+				System.err.println("Problem while loading the native library (link not resolved)");
+				System.err.println(exception.getLocalizedMessage());
+				System.exit(-1);
+			}
 		} else {
 			System.out.println("mode -NW");
 		}
