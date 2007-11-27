6d41c853077ceee3a49f233e2be0726200fdfc9a
Bruno Jofret <bruno.jofret@scilab.org>
2007-11-27 13:00:45 +0000

    Warning down : Remove unused parameter

--- scilab/modules/string/src/c/cvstr.c
+++ scilab/modules/string/src/c/cvstr.c
@@ -13,32 +13,31 @@ int C2F(cvstr)(integer * n,integer * line,char * str,integer * job,unsigned long
   if (*job == 0) C2F(asciitocode)(n, line, str, &cx1, str_len);
   else C2F(codetoascii)(n, line, str, str_len);
   return 0;
-} 
+}
 /*--------------------------------------------------------------------------*/
 int C2F(cvstr1)(integer *n,integer * line,char * str,integer * job,	unsigned long  str_len)
 {
   if (*job == 0) C2F(asciitocode)(n, line, str, &c_n1, str_len);
   else C2F(codetoascii)(n, line, str, str_len);
   return 0;
-} 
+}
 /*--------------------------------------------------------------------------*/
 int C2F(codetoascii)(integer *n,integer * line,char * str, unsigned long str_len)
 {
-  static integer eol = 99;
   int j = 0;
 
   /* conversion code ->ascii */
-  for (j = 0; j < *n; ++j) 
+  for (j = 0; j < *n; ++j)
   {
 	str[j] = convertScilabCodeToAsciiCode(line[j]);
   }
   return 0;
-} 
+}
 /*--------------------------------------------------------------------------*/
 int C2F(asciitocode)(integer * n,integer * line,char * str,integer * flagx,unsigned long  str_len)
 {
   integer j = 0;
-  if (*flagx == 1) 
+  if (*flagx == 1)
   {
     for (j = 0; j < *n ; ++j) line[j] = convertAsciiCodeToScilabCode(str[j]);
   }
@@ -47,5 +46,5 @@ int C2F(asciitocode)(integer * n,integer * line,char * str,integer * flagx,unsig
     for (j = *n -1 ; j >= 0; --j) line[j] = convertAsciiCodeToScilabCode(str[j]);
   }
   return 0;
-} 
+}
 /*--------------------------------------------------------------------------*/
