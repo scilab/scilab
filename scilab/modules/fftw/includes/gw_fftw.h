df14361cd43f18cdd4c9077670b2eec50a455c80
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2007-11-29 15:02:14 +0000

    Cleanning ...

--- scilab/modules/fftw/includes/gw_fftw.h
+++ scilab/modules/fftw/includes/gw_fftw.h
@@ -6,17 +6,17 @@
 #define __GW_FFTW__
 /*--------------------------------------------------------------------------*/
 #include "machine.h"
+#include "sci_get_fftw_wisdom.h"
+#include "sci_fftw_flags.h"
+#include "sci_loadfftwlibrary.h"
+#include "sci_set_fftw_wisdom.h"
+#include "sci_fftw_forget_wisdom.h"
 /*--------------------------------------------------------------------------*/
 int C2F(gw_fftw)(void);
 /*--------------------------------------------------------------------------*/
 /* interface for the previous function Table */ 
 /*--------------------------------------------------------------------------*/
 int sci_fftw __PARAMS((char *fname,unsigned long fname_len));
-int sci_fftw_flags __PARAMS((char *fname,unsigned long fname_len));
-int sci_get_fftw_wisdom __PARAMS((char *fname,unsigned long fname_len));
-int sci_set_fftw_wisdom __PARAMS((char *fname,unsigned long fname_len));
-int sci_fftw_forget_wisdom __PARAMS((char *fname,unsigned long fname_len));
-int sci_loadfftwlibrary __PARAMS((char *fname,unsigned long fname_len));
 int sci_disposefftwlibrary __PARAMS((char *fname,unsigned long fname_len));
 int sci_fftwlibraryisloaded __PARAMS((char *fname,unsigned long fname_len));
 /*--------------------------------------------------------------------------*/
