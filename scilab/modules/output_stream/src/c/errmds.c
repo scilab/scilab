caf63cffa80787895b88bcb067eaf6b757a6f0d0
Allan Cornet <allan.cornet@scilab.org>
2007-11-29 10:30:40 +0000

    bugs 2619,2621,2622

--- scilab/modules/output_stream/src/c/errmds.c
+++ scilab/modules/output_stream/src/c/errmds.c
@@ -7,20 +7,20 @@
 /*--------------------------------------------------------------------------*/ 
 int C2F(errmds)(integer *num, integer *imess, integer *imode)
 {
+	/* 100000 is a constant used in fortran to code errct value :( */
+	#define ERRCATCH_CONST_ERRCT 100000
 	*num = 0;
-
 	if (C2F(errgst).errct > 0) 
 	{
-		*num = C2F(errgst).errct - C2F(errgst).errct / isizt * isizt;
-		*imode = C2F(errgst).errct / isizt;
-	} else if (C2F(errgst).errct < 0) 
+		*num = C2F(errgst).errct - ERRCATCH_CONST_ERRCT * (int)( C2F(errgst).errct / ERRCATCH_CONST_ERRCT );
+		*imode = C2F(errgst).errct / ERRCATCH_CONST_ERRCT;
+	} 
+	else if (C2F(errgst).errct < 0) 
 	{
 		*num = -1;
-		*imode = -C2F(errgst).errct / isizt;
+		*imode = -C2F(errgst).errct / ERRCATCH_CONST_ERRCT;
 	}
 	*imess = *imode / 8;
-	*imode -= *imess << 3;
-
-	return 0;
+	*imode = *imode - 8* (*imess);
 } 
 /*--------------------------------------------------------------------------*/ 
