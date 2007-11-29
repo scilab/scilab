3e1bef3e5717a9b0c9862eded7e46bdf60d74bc9
Jean-baptiste Silvy <jean-baptiste.silvy@scilab.org>
2007-11-29 15:11:55 +0000

    Add logarithmic mode on rectangles.

--- scilab/modules/renderer/src/cpp/rectangleDrawing/ConcreteDrawableRectangle.cpp
+++ scilab/modules/renderer/src/cpp/rectangleDrawing/ConcreteDrawableRectangle.cpp
@@ -7,6 +7,12 @@
 /*------------------------------------------------------------------------*/
 
 #include "ConcreteDrawableRectangle.h"
+#include "getHandleDrawer.h"
+
+extern "C"
+{
+#include "GetProperty.h"
+}
 
 namespace sciGraphics
 {
@@ -38,6 +44,12 @@ void ConcreteDrawableRectangle::getCornersCoordinates( double corner1[3], double
   corner4[0] = ppRect->x + ppRect->width ;
   corner4[1] = ppRect->y ;
   corner4[2] = ppRect->z ;
+
+  // apply log scale if needed
+  pointScale(corner1[0], corner1[1], corner1[2], &(corner1[0]), &(corner1[1]), &(corner1[2]));
+  pointScale(corner2[0], corner2[1], corner2[2], &(corner2[0]), &(corner2[1]), &(corner2[2]));
+  pointScale(corner3[0], corner3[1], corner3[2], &(corner3[0]), &(corner3[1]), &(corner3[2]));
+  pointScale(corner4[0], corner4[1], corner4[2], &(corner4[0]), &(corner4[1]), &(corner4[2]));
 
 }
 /*---------------------------------------------------------------------------------*/
