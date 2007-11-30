278e9b7da11c79159bb5ebd004b52dcf4f17ee17
Allan Cornet <allan.cornet@scilab.org>
2007-11-30 13:13:28 +0000

    move on trunk (pcre headers)

--- scilab/modules/STRING_V2/src/c/pcre_private.h
+++ scilab/modules/STRING_V2/src/c/pcre_private.h
@@ -1,2 +1,10 @@
+/*------------------------------------------------------------------------*/
+/* Copyright INRIA 2007*/
+/*------------------------------------------------------------------------*/
+#ifndef __PCRE_PRIVATE_H__
+#define __PCRE_PRIVATE_H__
 
 int pcre_private(char *INPUT_LINE,char *INPUT_PAT,int *Output_Start,int *Output_End);
+
+#endif /* __PCRE_PRIVATE_H__ */
+/*------------------------------------------------------------------------*/
