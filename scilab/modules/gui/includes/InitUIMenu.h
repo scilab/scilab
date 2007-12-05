a31662f507104ef0b0b7ccdbe3464194ef81ae8f
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2007-12-05 16:13:12 +0000

    template for comments

--- scilab/modules/gui/includes/InitUIMenu.h
+++ scilab/modules/gui/includes/InitUIMenu.h
@@ -4,14 +4,87 @@
 #include "ObjectStructure.h"
 #include "returnProperty.h"
 
+/**
+ * @TODO add comment
+ *
+ * @param sciObj    
+ */
 void InitUIMenu(sciPointObj* sciObj);
+
+/**
+ * @TODO add comment
+ *
+ * @param sciObj    
+ * @param stackPointer  
+ * @param valueType 
+ * @param nbRow 
+ * @param nbCol 
+ * @return <ReturnValue>
+ */
 int setMenuParent(sciPointObj* sciObj, int stackPointer, int valueType, int nbRow, int nbCol);
+
+/**
+ * @TODO add comment
+ *
+ * @param sciObj    
+ * @param stackPointer  
+ * @param valueType 
+ * @param nbRow 
+ * @param nbCol 
+ * @return <ReturnValue>
+ */
 int setMenuCallback(sciPointObj* sciObj, int stackPointer, int valueType, int nbRow, int nbCol);
-int set_label_property(        sciPointObj * pobj, int stackPointer, int valueType, int nbRow, int nbCol ) ;
+
+/**
+ * @TODO add comment
+ *
+ * @param pobj  
+ * @param stackPointer  
+ * @param valueType 
+ * @param nbRow 
+ * @param nbCol 
+ * @return <ReturnValue>
+ */
+int set_label_property(sciPointObj * pobj, int stackPointer, int valueType, int nbRow, int nbCol ) ;
+
+/**
+ * @TODO add comment
+ *
+ * @param sciObj    
+ * @return <ReturnValue>
+ */
 int get_label_property(sciPointObj* sciObj);
+
+/**
+ * @TODO add comment
+ *
+ * @param name  
+ * @param status    
+ */
 void EnableRootMenu(char *name, BOOL status);
+
+/**
+ * @TODO add comment
+ *
+ * @param figurenum 
+ * @param name  
+ * @param status    
+ */
 void EnableFigureMenu(int figurenum, char *name, BOOL status);
+
+/**
+ * @TODO add comment
+ *
+ * @param name  
+ */
 void DeleteRootMenu(char *name);
+
+/**
+ * @TODO add comment
+ *
+ * @param figurenum 
+ * @param name  
+ */
 void DeleteFigureMenu(int figurenum, char *name);
 
 #endif
