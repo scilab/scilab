c73d3027ac84dcf2c3a407c71e169e855289fb3f
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2007-12-07 08:58:01 +0000

    Java 1.5 compatibility (bruno change)

--- scilab/modules/gui/src/java/org/scilab/modules/gui/events/GlobalMouseEventWatcher.java
+++ scilab/modules/gui/src/java/org/scilab/modules/gui/events/GlobalMouseEventWatcher.java
@@ -9,8 +9,8 @@ import org.scilab.modules.gui.bridge.canvas.SwingScilabCanvas;
 public class GlobalMouseEventWatcher implements AWTEventListener {
 
 	public void eventDispatched(AWTEvent mouseEvent) {
-		ClickInfos.getInstance().setXCoordinate(((MouseEvent) mouseEvent).getLocationOnScreen().getX());
-		ClickInfos.getInstance().setYCoordinate(((MouseEvent) mouseEvent).getLocationOnScreen().getY());
+		ClickInfos.getInstance().setXCoordinate(((MouseEvent) mouseEvent).getPoint().getX());
+		ClickInfos.getInstance().setYCoordinate(((MouseEvent) mouseEvent).getPoint().getY());
 		if (mouseEvent.getID() == MouseEvent.MOUSE_CLICKED) {
 			if (mouseEvent.getSource() instanceof SwingScilabCanvas) {
 				GlobalEventFilter.filter((MouseEvent) mouseEvent, (SwingScilabCanvas) mouseEvent.getSource());
