a4a7b18561da5da8e8a92118af5bbbd21c160182
Allan Cornet <allan.cornet@scilab.org>
2007-12-18 09:46:12 +0000

    update strindex add some tests

--- scilab/modules/STRING_V2/sci_gateway/c/sci_strindex.c
+++ scilab/modules/STRING_V2/sci_gateway/c/sci_strindex.c
@@ -19,175 +19,225 @@
 #include "localization.h"
 #include "freeArrayOfString.h"
 #include "pcre_private.h"
+#include "BOOL.h"
 /*------------------------------------------------------------------------*/
-#define CHAR_S 's'
-#define CHAR_R 'r'
+#define CHAR_S "s"
+#define CHAR_R "r"
 /*------------------------------------------------------------------------*/
 int C2F(sci_strindex) _PARAMS((char *fname,unsigned long fname_len))
 {
-	char typ = CHAR_S;
-    char **Str = NULL;
-	char **Str2 = NULL;
+	BOOL bStrindex_with_pattern = FALSE;
+	int outIndex = 0;
+	int numRow = 1;
 
-	int i = 0; /* loop indice */
+	int i = 0;
 
-    int mn = 0; /* dimension parameter 1 m*n */
+	CheckRhs(2,3);
+	CheckLhs(1,2);
 
-    int m2 = 0,n2 = 0;
-	int mn2 = 0; /* m2*n2 */
+	if (Rhs == 3)
+	{
+		int m3 = 0;
+		int n3 = 0;
+		char **Strings_Input3 = NULL;
+		int m3n3 = 0; /* m3 * n3 */
 
-    int outIndex = 0;
-    int numRow = 1;
+		if (VarType(3) != sci_strings)
+		{
+			Scierror(999,_("%s : Wrong type for third input argument: String expected.\n"),fname);
+			return 0;
+		}
+		GetRhsVar(3,MATRIX_OF_STRING_DATATYPE,&m3,&n3,&Strings_Input3);
+		m3n3 = m3*n3;  
 
-	int *values = NULL;
-    int *position = NULL;
+		if (m3n3 != 1)
+		{
+			freeArrayOfString(Strings_Input3,m3n3);
+			Scierror(999,_("%s : Wrong type for third input argument: String expected.\n"),fname);
+			return 0;
+		}
 
-	int nbValues = 0;
-    int nbposition = 0;
-	
-    CheckRhs(1,3);
-    CheckLhs(1,2);
+		if ( (strcmp(Strings_Input3[0],CHAR_R) == 0) || (strcmp(Strings_Input3[0],CHAR_S) == 0) )
+		{
+			if (strcmp(Strings_Input3[0],CHAR_R) == 0)
+			{
+				bStrindex_with_pattern = TRUE;
+			}
+			else
+			{
+				bStrindex_with_pattern = FALSE;
+			}
+			freeArrayOfString(Strings_Input3,m3n3);
+		}
+		else
+		{
+			freeArrayOfString(Strings_Input3,m3n3);
+			Scierror(999,_("%s : Wrong type for third input argument: 's' or 'r' expected.\n"),fname);
+			return 0;
+		}
+	}
 
-    if (VarType(1) == sci_matrix)
+	if (VarType(1) == sci_matrix)
 	{
 		int m1 = 0;
 		int n1 = 0;
+		int l1 = 0;
 
-		GetRhsVar(1,MATRIX_OF_DOUBLE_DATATYPE,&m1,&n1,&Str);
+		GetRhsVar(1,MATRIX_OF_DOUBLE_DATATYPE,&m1,&n1,&l1);
 		if ((m1 == 0) && (n1 == 0))
 		{
-			int l = 0;
-			CreateVar(Rhs+1,MATRIX_OF_DOUBLE_DATATYPE,&m1,&n1,&l);
+			CreateVar(Rhs+1,MATRIX_OF_DOUBLE_DATATYPE,&m1,&n1,&l1);
 			LhsVar(1) = Rhs+1 ;
 			C2F(putlhsvar)();
 			return 0;
 		}
+		else
+		{
+			Scierror(999,_("%s : Wrong type for third input argument: String or [] expected.\n"),fname);
+			return 0;
+		}
 	}
-	else
-	{
-		int m1 = 0;
-		int n1 = 0;
-
-		GetRhsVar(1,MATRIX_OF_STRING_DATATYPE,&m1,&n1,&Str);
-		mn = m1*n1;  
-	}
-
 	
-	if (mn != 1)
-    {
-		freeArrayOfString(Str,mn);
-        Scierror(36, _("%s : Wrong size for first input argument: scalar expected.\n"));
-        return 0;
-    }
-
-    GetRhsVar(2,MATRIX_OF_STRING_DATATYPE,&m2,&n2,&Str2);
-    mn2 = m2*n2;  
-
-	if ( (int)strlen(Str[0]) == 0 ) 
+	if ( (VarType(1) == sci_strings) && (VarType(2) == sci_strings) )
 	{
-		values = (int *)MALLOC(sizeof(int));
-		position = (int *)MALLOC(sizeof(int));
-	}
-	else
-	{
-		values = (int *)MALLOC( sizeof(int) * ( strlen(Str[0]) ) );
-		position = (int *)MALLOC( sizeof(int) * ( strlen(Str[0])) );
-	}
+		int m1 = 0, n1 = 0;
+		char **Strings_Input1 = NULL;
+		int m1n1 = 0; /* m1 * n1 */
 
-    if (Rhs >= 3)
-    {
-		int m3 = 0;
-		int n3 = 0;
-		int l3 = 0;
+		int m2 = 0, n2 = 0;
+		char **Strings_Input2 = NULL;
+		int m2n2 = 0; /* m2 * n2 */
 
-        GetRhsVar(3,STRING_DATATYPE,&m3,&n3,&l3);
+		int *values = NULL;
+		int *position = NULL;
 
-        if ( m3*n3 != 0) typ = cstk(l3)[0];
+		int nbValues = 0;
+		int nbposition = 0;
 
-        if (typ == CHAR_R )
-        {
+		GetRhsVar(1,MATRIX_OF_STRING_DATATYPE,&m1,&n1,&Strings_Input1);
+		m1n1 = m1*n1;
+
+		if (m1n1 != 1)
+		{
+			freeArrayOfString(Strings_Input1,m1n1);
+			Scierror(999,_("%s : Wrong type for first input argument: String expected.\n"),fname);
+			return 0;
+		}
+
+		GetRhsVar(2,MATRIX_OF_STRING_DATATYPE,&m2,&n2,&Strings_Input2);
+		m2n2 = m2*n2;
+
+		if ( (m2 != 1) && (n2 != 1) )
+		{
+			freeArrayOfString(Strings_Input1,m1n1);
+			freeArrayOfString(Strings_Input2,m2n2);
+			Scierror(999,_("%s : Wrong type for second input argument: String or string vector expected.\n"),fname);
+			return 0;
+		}
+
+		if ( (int)strlen(Strings_Input1[0]) == 0 )
+		{
+			values = (int *)MALLOC(sizeof(int));
+			position = (int *)MALLOC(sizeof(int));
+		}
+		else
+		{
+			values = (int *)MALLOC( sizeof(int) * ( strlen(Strings_Input1[0]) ) );
+			position = (int *)MALLOC( sizeof(int) * ( strlen(Strings_Input1[0])) );
+		}
+
+		if (bStrindex_with_pattern)
+		{
 			int x = 0;
 			int w = 0;
 
 			int Output_Start = 0;
 			int Output_End = 0;
 
-			/*When we use the regexp;*/
-            for (x = 0; x < mn2; ++x)
-            {
-                w = pcre_private(Str[0],Str2[x],&Output_Start,&Output_End);
-                if ( w == 0)
-                {         
-                    values[nbValues++]=Output_Start+1;         /*adding the answer into the outputmatrix*/
-                    position[nbposition++]=x+1;                /*The number according to the str2 matrix*/
-                }     
-            }
-        }
-    }
-    else
-    {   
-		int x = 0;
-		int pos = 0;
-		/* When we do not use the regexp.*/
-		for (x=0; x < mn2 ;++x)
-        {
-			int w = 0;
-			int w1=0;
-			
-            if ( strlen(Str2[x]) == 0 )
-            {
-				if (values) {FREE(values); values = NULL;}
-				if (position) {FREE(position); position = NULL;}
-				Scierror(999, _("%s : Wrong size for second input argument: a not empty string expected.\n"));
-				return 0;
-            }
-
-			if (Str2)
+			/* We use pcre library */
+			for (x = 0; x < m2n2; ++x)
 			{
-				
-				for (w=0; w<(int)strlen(Str[0]);w++)
+				w = pcre_private(Strings_Input1[0],Strings_Input2[x],&Output_Start,&Output_End);
+				if ( w == 0)
+				{         
+					values[nbValues++] = Output_Start+1; /* adding the answer into the outputmatrix */
+					position[nbposition++] = x+1;        /* The number according to the str2 matrix */
+				}     
+			}
+		}
+		else
+		{
+			int x = 0;
+			int pos = 0;
+
+			/* We don't use pcre library */
+			for (x = 0; x < m2n2 ; ++x)
+			{
+				int w = 0;
+				int w1 = 0;
+
+				if ( strlen(Strings_Input2[x]) == 0 )
 				{
-					pos=0;
-					w1=w;
-			        for ( ; Str[0][w1]==Str2[x][pos] && Str[0][w1]!='\0';w1++,pos++) ;
-                    if (pos == (int)strlen(Str2[x])) 
+					freeArrayOfString(Strings_Input2,m2n2);
+					freeArrayOfString(Strings_Input1,m1n1);
+					if (values) {FREE(values); values = NULL;}
+					if (position) {FREE(position); position = NULL;}
+					Scierror(999, _("%s : Wrong size for second input argument: a not empty string expected.\n"));
+					return 0;
+				}
+
+				if (Strings_Input2)
+				{
+					for (w = 0; w<(int)strlen(Strings_Input1[0]);w++)
+					{
+						pos = 0;
+						w1 = w;
+						for ( ; (Strings_Input1[0][w1] == Strings_Input2[x][pos]) && (Strings_Input1[0][w1] != '\0'); w1++,pos++ ) ;
+						if (pos == (int)strlen(Strings_Input2[x])) 
 						{
-							values[nbValues++] = w1-(int)strlen(Str2[x])+1;
+							values[nbValues++] = w1-(int)strlen(Strings_Input2[x])+1;
 							position[nbposition++] = x+1;
-							pos=0;
+							pos = 0;
 						}
+					}
 				}
 			}
-        }
-    }
+		}
 
-	freeArrayOfString(Str,mn);
-	freeArrayOfString(Str2,mn2);
+		freeArrayOfString(Strings_Input1,m1n1);
+		freeArrayOfString(Strings_Input2,m2n2);
 
-    numRow   = 1;/* Output values[] */ 
-    outIndex = 0;
-    CreateVar(Rhs+1,MATRIX_OF_DOUBLE_DATATYPE,&numRow,&nbValues,&outIndex);
-    for ( i = 0 ; i < nbValues ; i++ )
-    {
-		stk(outIndex)[i] = (double)values[i] ;
-    }
-    LhsVar(1) = Rhs+1 ;
+		numRow   = 1;
+		outIndex = 0;
+		CreateVar(Rhs+1,MATRIX_OF_DOUBLE_DATATYPE,&numRow,&nbValues,&outIndex);
+		for ( i = 0 ; i < nbValues ; i++ )
+		{
+			stk(outIndex)[i] = (double)values[i] ;
+		}
+		LhsVar(1) = Rhs+1 ;
 
-	/* position[]*/
-    numRow   = 1;
-    outIndex = 0;
-    CreateVar(Rhs+2,MATRIX_OF_DOUBLE_DATATYPE,&numRow,&nbposition,&outIndex);
-    for ( i = 0 ; i < nbposition ; i++ )
-    {
-		stk(outIndex)[i] = (double)position[i] ;
-    }
-    LhsVar(2) = Rhs+2;    
+		if (Lhs == 2)
+		{
+			numRow   = 1;
+			outIndex = 0;
+			CreateVar(Rhs+2,MATRIX_OF_DOUBLE_DATATYPE,&numRow,&nbposition,&outIndex);
+			for ( i = 0 ; i < nbposition ; i++ )
+			{
+				stk(outIndex)[i] = (double)position[i] ;
+			}
+			LhsVar(2) = Rhs+2;    
+		}
 
-    C2F(putlhsvar)();
+		C2F(putlhsvar)();
 
-    if (values) {FREE(values); values = NULL;}
-    if (position) {FREE(position); position = NULL;}
-
-    return 0;
+		if (values) {FREE(values); values = NULL;}
+		if (position) {FREE(position); position = NULL;}
+	}
+	else
+	{
+		Scierror(999,_("%s : Wrong type for input arguments: Strings expected.\n"),fname);
+		return 0;
+	}
+	return 0;
 }
-/*-----------------------------------------------------------------------------------*/
+/*------------------------------------------------------------------------*/
--- scilab/modules/STRING_V2/tests/benchmarks/bench_strindex.tst
+++ scilab/modules/STRING_V2/tests/benchmarks/bench_strindex.tst
@@ -1,23 +1,23 @@
 // =============================================================================
-// tokens benchmark
-// exec(SCI+'/modules/string_V2/tests/benchmarks/bench_tokens.tst');
+// strindex benchmark
+// exec(SCI+'/modules/string_V2/tests/benchmarks/bench_strindex.tst');
 // =============================================================================
 // Bench 1
 // =============================================================================
-function time = bench_tokens_1(runs)
+function time = bench_strindex_1(runs)
   cumulate = 0;
   for i = 1:runs
     timer();
-		a = tokens('This is a character string');
+		[k,w]=strindex('1+3*abc/2.33',['+','-','*','/'])
 	  timing = timer();
     cumulate = cumulate + timing;
   end
   time = cumulate / runs;
-  clear('a'); clear('cumulate');; clear('i');
+  clear('k');clear('w'); clear('cumulate');; clear('i');
 endfunction
 // =============================================================================
 runs = 100000;
-printf("\nBench tokens\n");
-printf("Time bench 1 = %f seconds.\n",bench_tokens_1(runs));
+printf("\nBench strindex\n");
+printf("Time bench 1 = %f seconds.\n",bench_strindex_1(runs));
 // =============================================================================
 
