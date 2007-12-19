8abae7a067a46a8a43fc9d14468de9039f513c85
Jean-baptiste Silvy <jean-baptiste.silvy@scilab.org>
2007-12-19 15:41:57 +0000

    Correct wrong spelling phase 2.

---
+++ scilab/modules/renderer/src/cpp/subwinDrawing/ZTicksDrawerJavaMapper.hxx
@@ -0,0 +1,72 @@
+/*------------------------------------------------------------------------*/
+/* file: ZTicksDrawerJavaMapper.hxx                                       */
+/* Copyright INRIA 2007                                                   */
+/* Authors : Jean-Baptiste Silvy                                          */
+/* desc : Class containing java methods needed by ZTicksDrawerJoGL        */
+/*------------------------------------------------------------------------*/
+
+#ifndef _Z_TICKS_DRAWER_JAVA_MAPPER_HXX_
+#define _Z_TICKS_DRAWER_JAVA_MAPPER_HXX_
+
+#include "TicksDrawerJavaMapper.hxx"
+#include "ZTicksDrawerGL.hxx"
+
+
+namespace sciGraphics
+{
+
+/**
+ * Contains mapping of java method used by XTicksDrawerJoGL 
+ */
+class ZTicksDrawerJavaMapper : public virtual TicksDrawerJavaMapper
+{
+public:
+
+  ZTicksDrawerJavaMapper(void);
+
+  virtual ~ZTicksDrawerJavaMapper(void);
+  /*----------------------------------------------------------------------*/
+  // Inherited from DrawableObjectJavaMapper
+  virtual void display(void);
+
+  virtual void initializeDrawing(int figureIndex);
+  virtual void endDrawing(void);
+
+  virtual void show(int figureIndex);
+
+  virtual void destroy(int figureIndex);
+
+  virtual void setFigureIndex(int figureIndex);
+  /*----------------------------------------------------------------------*/
+  // Inherited from TicksDrawer
+  virtual void drawTicks(const double ticksPositions[], char * ticksLabels[],
+                         int nbTicks, const double subticksPositions[], int nbSubticks);
+
+  virtual bool checkTicks(const double ticksPositions[], char * ticksLabels[], int nbTicks);
+
+  virtual void drawTicks(const double ticksPositions[], char * ticksLabels[], char * labelsExponents[],
+                         int nbTicks, const double subticksPositions[], int nbSubticks);
+
+  virtual bool checkTicks(const double ticksPositions[], char * ticksLabels[],
+                          char * labelsExponents[], int nbTicks);
+
+  virtual void setAxesBounds(double xMin, double xMax,
+                             double yMin, double yMax,
+                             double zMin, double zMax);
+
+  virtual void setAxisParamerters(int lineStyle, float lineWidth, int lineColor,
+                                  int fontType, double fontSize, int fontColor);
+  /*----------------------------------------------------------------------*/
+
+protected:
+
+  /**
+   * Giws generated wrapper
+   */
+  org_scilab_modules_renderer_subwinDrawing::ZTicksDrawerGL * m_pJavaObject;
+
+};
+
+}
+
+#endif /* _TOP_X_TICKS_DRAWER_JAVA_MAPPER_HXX_ */
