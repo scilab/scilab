656340a949a77cb523429f19fb597b5ef16420e0
Cong Wu <cong.wu@inria.fr>
2007-12-21 10:24:46 +0000

    new strsplit   maybe faster, can not test due to the problem of emptystr

--- scilab/modules/STRING_V2/src/c/strsplit.c
+++ scilab/modules/STRING_V2/src/c/strsplit.c
@@ -6,25 +6,47 @@
 #include <string.h>
 #include "strsplit.h"
 /*------------------------------------------------------------------------*/
-void strsplit(char **Input_StringMatrix,char **Output_StringMatrix,int *Row_Pointer,int *Row_Pointer_Two,int *Col_Pointer,int *Input_IntMatrix)
+void strsplit(char **Input_StringMatrix,char **Output_StringMatrix,int *Row_Two,int *Col_Two,int *Col_Pointer,int *Input_IntMatrix)
 {
 	int x = 0;
+	int i=0;
+	int l;
 	/*To loop on every string of the input matrix*/
-	for (x = 0; x < (int)strlen(Input_StringMatrix[0]) ; x++)
+	//for (x = 0; x < (int)strlen(Input_StringMatrix[0]) ; x++)
+	//{
+	//	/* To put them in the correct order */
+	//	if (x != Input_IntMatrix[*Row_Pointer_Two]) 
+	//	{
+	//		Output_StringMatrix[*Row_Pointer][(*Col_Pointer)++] = Input_StringMatrix[0][x];
+	//	}
+	//	else 
+	//	{
+	//		(*Row_Pointer)++;
+	//		(*Row_Pointer_Two)++;
+	//		*Col_Pointer=0;
+	//		Output_StringMatrix[*Row_Pointer][(*Col_Pointer)++] = Input_StringMatrix[0][x];
+	//	}
+ //       Output_StringMatrix[*Row_Pointer][*Col_Pointer]=0;
+	//}
+	strncpy(Output_StringMatrix[0],Input_StringMatrix[0],Input_IntMatrix[0]);
+    for (x = 1; x <= (int)strlen(Input_IntMatrix)+1 ; x++)
 	{
-		/* To put them in the correct order */
-		if (x != Input_IntMatrix[*Row_Pointer_Two]) 
+		for(i=Input_IntMatrix[x-1];i<Input_IntMatrix[x];i++)
 		{
-			Output_StringMatrix[*Row_Pointer][(*Col_Pointer)++] = Input_StringMatrix[0][x];
+             Output_StringMatrix[x][i-Input_IntMatrix[x-1]]=Input_StringMatrix[0][i];
 		}
-		else 
-		{
-			(*Row_Pointer)++;
-			(*Row_Pointer_Two)++;
-			*Col_Pointer=0;
-			Output_StringMatrix[*Row_Pointer][(*Col_Pointer)++] = Input_StringMatrix[0][x];
-		}
-        Output_StringMatrix[*Row_Pointer][*Col_Pointer]=0;
+		
+        Output_StringMatrix[x][Input_IntMatrix[x]-Input_IntMatrix[x-1]]='\0';
 	}
+	l=(*Row_Two)*(*Col_Two);
+    if (Input_IntMatrix[l-1]<(int)strlen(Input_StringMatrix[0])) 
+	{
+		for (i=Input_IntMatrix[l-1];i<=(int)strlen(Input_StringMatrix[0]);i++)
+		{
+			Output_StringMatrix[l][i-Input_IntMatrix[l-1]]=Input_StringMatrix[0][i];
+		}
+	}
+
+
 }
 /*------------------------------------------------------------------------*/
