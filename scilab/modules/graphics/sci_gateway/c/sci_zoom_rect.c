3a184c5595be10f1fe968519a20b388f4a720baf
Jean-baptiste Silvy <jean-baptiste.silvy@scilab.org>
2008-01-02 15:19:09 +0000

    Remove some warnings.

--- scilab/modules/graphics/sci_gateway/c/sci_zoom_rect.c
+++ scilab/modules/graphics/sci_gateway/c/sci_zoom_rect.c
@@ -7,16 +7,18 @@
 
 #include "sci_zoom_rect.h"
 #include "stack-c.h"
+#include "getPropertyAssignedValue.h"
 #include "PloEch.h"
 #include "BuildObjects.h"
 #include "gw_graphics.h"
 #include "axesScale.h"
+#include "CurrentObjectsManagement.h"
 
 /*--------------------------------------------------------------------------*/
 int sci_zoom_rect(char *fname,unsigned long fname_len)
 {
   int m,n,l;
-  int x_pixel=0,y_pixel=0;
+  /* int x_pixel=0,y_pixel=0; */
   CheckRhs(0,1) ;
   CheckLhs(0,1) ;
   if (Rhs <= 0) 
