f017192c7199c85cae969d993ae98af600802c11
Vincent Couvert <vincent.couvert@scilab.org>
2008-01-09 08:10:29 +0000

    Do not change number of lines when the console is resized if the current value is 0 (do not ask for more display)

--- scilab/modules/console/src/c/GuiManagement.c
+++ scilab/modules/console/src/c/GuiManagement.c
@@ -9,6 +9,18 @@
 /*--------------------------------------------------------------------------*/
 void setScilabLines(int nbRows, int nbCols)
 {
-  scilines(nbRows, nbCols);
+  if (getLinesSize() == 0) 
+    {
+      /* The user do not want to be asked for more display */
+      /* Modify only the number of columns used to format displayed data */
+
+      scilines(getLinesSize(), nbCols);
+    }
+  else 
+    {
+      /* Modify number of lines and columns used to format displayed data */
+
+      scilines(nbRows, nbCols);
+    }
 }
 /*--------------------------------------------------------------------------*/
