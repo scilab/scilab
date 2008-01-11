5d5557a72c43b841d04338652011992c4d1be371
Vincent Couvert <vincent.couvert@scilab.org>
2008-01-11 22:53:35 +0000

    uicontrol position can be something else than a double matrix (string)

--- scilab/modules/graphics/src/c/getHandleProperty/set_position_property.c
+++ scilab/modules/graphics/src/c/getHandleProperty/set_position_property.c
@@ -18,6 +18,12 @@
 int set_position_property( sciPointObj * pobj, int stackPointer, int valueType, int nbRow, int nbCol )
 {
 
+  if(sciGetEntityType(pobj) == SCI_UICONTROL)
+  {
+    SetUicontrolPosition(pobj, stackPointer, valueType, nbRow, nbCol);
+    return SET_PROPERTY_SUCCEED ;
+  }
+
   if ( !isParameterDoubleMatrix( valueType ) )
   {
     sciprint(_("Incompatible type for property %s.\n"),"position") ;
@@ -34,17 +40,17 @@ int set_position_property( sciPointObj * pobj, int stackPointer, int valueType, 
     pUIMENU_FEATURE(pobj)->MenuPosition = (int) getDoubleFromStack( stackPointer ) ;
     return SET_PROPERTY_SUCCEED ;
   }
-  else if(sciGetEntityType(pobj) == SCI_UICONTROL)
-  {
-    SetUicontrolPosition(pobj, stackPointer, valueType, nbRow, nbCol);
-    return SET_PROPERTY_SUCCEED ;
-  }
   else if( sciGetEntityType(pobj) == SCI_LABEL )
   {
     double * values = getDoubleMatrixFromStack( stackPointer ) ;
     double curPos[3];
     sciGetTextPos(pobj, curPos);
     return sciSetTextPos( pobj, values[0], values[1], curPos[2] ) ;
+  }
+  else if( sciGetEntityType(pobj) == SCI_FIGURE ) /* Added for compatibility with old TK figure */
+  {
+    double * values = getDoubleMatrixFromStack( stackPointer ) ;
+    return (sciInitScreenPosition(pobj, values[0], values[1]) & sciSetWindowDim(pobj, values[2], values[3]));
   }
   else
   {
