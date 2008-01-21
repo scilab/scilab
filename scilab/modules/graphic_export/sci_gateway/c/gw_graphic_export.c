ebb28083ffb51a0cc6e3adb7f10a265dee792a64
Allan Cornet <allan.cornet@scilab.org>
2008-01-21 07:33:24 +0000

    gateway and dependencies

--- scilab/modules/graphic_export/sci_gateway/c/gw_graphic_export.c
+++ scilab/modules/graphic_export/sci_gateway/c/gw_graphic_export.c
@@ -1,26 +1,16 @@
 /*--------------------------------------------------------------------------*/
-/**
-  * @INRIA 2008
- */
+/* INRIA 2008 */
+/* Allan CORNET */
+/* Jean-Baptiste SILVY */
 /*--------------------------------------------------------------------------*/
 #include <string.h>
-#ifdef _MSC_VER
-#include <Windows.h>
-#include "ExceptionMessage.h"
-#endif
 #include "gw_graphic_export.h"
 #include "stack-c.h"
+#include "scilabmode.h"
+#include "callFunctionFromGateway.h"
+#include "localization.h"
 /*--------------------------------------------------------------------------*/
-typedef int (*Graphic_Export_Interf) __PARAMS((char *fname,unsigned long l));
-
-typedef struct table_struct
-{
-  Graphic_Export_Interf f;    /** function **/
-  char *name;      /** its name **/
-} Graphic_ExportTable;
-/*--------------------------------------------------------------------------*/
-
-static Graphic_ExportTable Tab[]=
+static gw_generic_table Tab[]= 
 {
   {sci_xs2gif,"xs2gif"},
   {sci_xs2ppm,"xs2ppm"},
@@ -30,29 +20,19 @@ static Graphic_ExportTable Tab[]=
   {sci_xs2fig,"xs2fig"},
   {sci_xs2fig,"xg2ps"}
 };
-
 /*--------------------------------------------------------------------------*/
 int C2F(gw_graphic_export)()
 {  
 	Rhs = Max(0, Rhs);
-
-#ifdef _MSC_VER
-	#ifndef _DEBUG
-		_try
-		{
-			(*(Tab[Fin-1].f)) (Tab[Fin-1].name,(unsigned long)strlen(Tab[Fin-1].name));
-		}
-		_except (EXCEPTION_EXECUTE_HANDLER)
-		{	
-			ExceptionMessage(GetExceptionCode(),Tab[Fin-1].name);
-		}
-	#else
-		(*(Tab[Fin-1].f)) (Tab[Fin-1].name,(unsigned long)strlen(Tab[Fin-1].name));
-	#endif
-#else
-		(*(Tab[Fin-1].f)) (Tab[Fin-1].name,(unsigned long)strlen(Tab[Fin-1].name));
-#endif
-
+	if ( getScilabMode() != SCILAB_NWNI )
+	{
+		callFunctionFromGateway(Tab);
+		C2F(putlhsvar)();
+	}
+	else
+	{
+		Scierror(999,_("Scilab graphic module disabled -nogui or -nwni mode.\n"));
+	}
 	return 0;
 }
 /*--------------------------------------------------------------------------*/
