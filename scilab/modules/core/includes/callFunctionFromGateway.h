fd07bbd3d6c114a7173ee97d017161b87febb76b
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2008-01-27 19:46:42 +0000

    Comment

--- scilab/modules/core/includes/callFunctionFromGateway.h
+++ scilab/modules/core/includes/callFunctionFromGateway.h
@@ -9,9 +9,12 @@
 
 typedef int (*function_Interf) __PARAMS((char *fname,unsigned long l));
 
+/** 
+ * See SCI/modules/<module_name>/sci_gateway/c/gw_<module_name>.c for example 
+*/
 typedef struct functions_table_struct {
-	function_Interf f; /** function **/
-	char *name;      /** its name **/
+	function_Interf f; /** the function itself **/
+	char *name;      /** its name in Scilab **/
 } gw_generic_table;
 
 
