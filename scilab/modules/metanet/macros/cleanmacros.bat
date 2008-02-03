30da7056fe44430faebab892934824c5084c26d6
Allan Cornet <allan.cornet@scilab.org>
2008-02-03 17:18:27 +0000

    call .bat in directories

--- scilab/modules/metanet/macros/cleanmacros.bat
+++ scilab/modules/metanet/macros/cleanmacros.bat
@@ -1,6 +1,7 @@
-@del editor\*.bin 2>NUL
-@del editor\lib 2>NUL
-@del editor\names 2>NUL
-@del graph_tools\*.bin 2>NUL
-@del graph_tools\lib 2>NUL
-@del graph_tools\names 2>NUL
+@cd editor
+@cleanmacros.bat 
+@cd ..
+@cd graph_tools
+@cleanmacros.bat
+@cd ..
+
