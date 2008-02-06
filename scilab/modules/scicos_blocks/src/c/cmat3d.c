fcb200a28e7d737d169de6a1f9323743209bbcb0
Allan Cornet <allan.cornet@scilab.org>
2008-02-06 13:01:37 +0000

    comments some old graphics routines J-B needs to implement C2F(dr)("xset","wshow",PI0,PI0,PI0,PI0,PI0,PI0,PD0,PD0,PD0,PD0,0L,0L);
    Thanks J-B ;)

--- scilab/modules/scicos_blocks/src/c/cmat3d.c
+++ scilab/modules/scicos_blocks/src/c/cmat3d.c
@@ -203,7 +203,8 @@ void cmat3d(scicos_block * block, int flag)
 	    sciSetUsedWindow(scoGetWindowID(pScopeMemory));
 	    if(sciGetPixmapMode(scoGetPointerScopeWindow(pScopeMemory)))
 	      {
-		C2F(dr)("xset","wshow",PI0,PI0,PI0,PI0,PI0,PI0,PD0,PD0,PD0,PD0,0L,0L);
+			  /* TODO : not implemented */
+			  /*C2F(dr)("xset","wshow",PI0,PI0,PI0,PI0,PI0,PI0,PD0,PD0,PD0,PD0,0L,0L);*/
 	      }
 	    sciDrawObj(scoGetPointerScopeWindow(pScopeMemory));
 	  }
