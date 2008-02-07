7c93d3abe630ab5c18650586cb65ad6937222d6e
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2008-02-07 10:59:12 +0000

    Warning removed

--- scilab/modules/string/includes/stricmp.h
+++ scilab/modules/string/includes/stricmp.h
@@ -1,12 +1,12 @@
 /*-----------------------------------------------------------------------*/
-/* INRIA 2008
+/* INRIA 2008 */
 /*-----------------------------------------------------------------------*/
 #ifndef __STRICMP_H__
 #define __STRICMP_H__
 
 #include <string.h>
 
-/* compare  two strings ignoring case */
+/* compare two strings ignoring the case */
 
 #if _MSC_VER
 	#define stricmp _stricmp 
