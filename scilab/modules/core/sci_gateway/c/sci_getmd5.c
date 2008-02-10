3aa65941539435ed2639de2758ce7a5e67cc18ec
Pierre Marechal <pierre.marechal@scilab.org>
2008-02-10 20:14:42 +0000

    Extend getmd5 to matrix of string

--- scilab/modules/core/sci_gateway/c/sci_getmd5.c
+++ scilab/modules/core/sci_gateway/c/sci_getmd5.c
@@ -10,48 +10,66 @@
 #include "localization.h"
 #include "Scierror.h"
 #include "stricmp.h"
+#include "freeArrayOfString.h"
 /*--------------------------------------------------------------------------*/
 int C2F(sci_getmd5) _PARAMS((char *fname,unsigned long fname_len))
 {
-	static int l1,n1,m1;
-	char *output=NULL ;
-
+	static int l1 = 0;
+	static int m1 = 0;
+	static int n1 = 0;
+	static int m2 = 0;
+	static int n2 = 0;
+	static int mn = 0;
+	static int i  = 0;
+	
+	static int md5_length = 32;
+	
+	char **Input_Matrix  = NULL;
+	char **Output_Matrix = NULL;
+	
 	Rhs=Max(Rhs,0);
 	CheckRhs(1,2) ;
 	CheckLhs(1,1) ;
-
+	
 	if (Rhs==1)
 	{
 		if (GetType(1) == sci_strings)
 		{
-			char *Param1=NULL;
-			FILE *fp=NULL;
-			char *MD5=NULL;
-
-			GetRhsVar(1,STRING_DATATYPE,&m1,&n1,&l1);
-			Param1=cstk(l1);
-
-			fp = fopen(Param1, "rb");
-			if (fp)
+			GetRhsVar(1,MATRIX_OF_STRING_DATATYPE,&m1,&n1,&Input_Matrix);
+			mn = m1 * n1;
+			
+			Output_Matrix = (char**)MALLOC(sizeof(char*)*(mn));
+			for (i = 0; i < mn; i++)
 			{
-				MD5=md5_file(fp);
-				fclose(fp);
-
-				output=(char*)MALLOC((strlen(MD5)+1)*sizeof(char));
-				strcpy(output,MD5);
-				if (MD5) {FREE(MD5);MD5=NULL;}
-
-				n1=1;
-				CreateVarFromPtr(Rhs+1,STRING_DATATYPE,(m1=(int)strlen(output), &m1),&n1,&output);
-				if (output) {FREE(output);output=NULL;}
-
-				LhsVar(1) = Rhs+1;
-				C2F(putlhsvar)();
+				Output_Matrix[i] = (char*)MALLOC((md5_length+1)*sizeof(char));
 			}
-			else
+			
+			for (i = 0; i < mn; i++)
 			{
-				Scierror(999,_("%s: The file %s does not exist.\n"),fname, Param1);
+				FILE *fp  = NULL;
+				char *MD5 = NULL;
+				
+				fp        = fopen(Input_Matrix[i],"rb");
+				
+				if (fp)
+				{
+					MD5 = md5_file(fp);
+					fclose(fp);
+					strcpy(Output_Matrix[i],MD5);
+					if (MD5) {FREE(MD5);MD5=NULL;}
+				}
+				else
+				{
+					Scierror(999,_("%s: The file %s does not exist.\n"),fname, Input_Matrix[i]);
+					freeArrayOfString(Output_Matrix,mn);
+					freeArrayOfString(Input_Matrix,mn);
+					return 0;
+				}
 			}
+			
+			CreateVarFromPtr( Rhs+1, MATRIX_OF_STRING_DATATYPE, &m1, &n1, Output_Matrix );
+			LhsVar(1) = Rhs+1 ;
+			C2F(putlhsvar)();
 		}
 		else
 		{
@@ -62,29 +80,38 @@ int C2F(sci_getmd5) _PARAMS((char *fname,unsigned long fname_len))
 	{
 		if ( (GetType(1) == sci_strings) && (GetType(2) == sci_strings) )
 		{
-			char *Param1=NULL;
 			char *Param2=NULL;
-
-			GetRhsVar(1,STRING_DATATYPE,&m1,&n1,&l1);
-			Param1=cstk(l1);
-
-			GetRhsVar(2,STRING_DATATYPE,&m1,&n1,&l1);
+			
+			GetRhsVar(1,MATRIX_OF_STRING_DATATYPE,&m1,&n1,&Input_Matrix);
+			mn = m1 * n1;
+			
+			GetRhsVar(2,STRING_DATATYPE,&m2,&n2,&l1);
 			Param2=cstk(l1);
-
+			
+			Output_Matrix = (char**)MALLOC(sizeof(char*)*(mn));
+			
 			if ( stricmp(Param2,"string")==0 )
 			{
-				char *MD5=NULL;
-				MD5=md5_str(Param1);
-
-				output=(char*)MALLOC((strlen(MD5)+1)*sizeof(char));
-				strcpy(output,MD5);
-				if (MD5) {FREE(MD5);MD5=NULL;}
-
-				n1=1;
-				CreateVarFromPtr( Rhs+1,STRING_DATATYPE,(m1=(int)strlen(output), &m1),&n1,&output);
-				if (output) {FREE(output);output=NULL;}
-
-				LhsVar(1) = Rhs+1;
+				for (i = 0; i < mn; i++)
+				{
+					char *MD5        = NULL;
+					
+					MD5              = md5_str(Input_Matrix[i]);
+					Output_Matrix[i] = (char*)MALLOC((md5_length+1)*sizeof(char));
+					
+					if (Output_Matrix[i] == NULL)
+					{
+						freeArrayOfString(Output_Matrix,i);
+						Scierror(999,("%s: No more memory.\n"),fname);
+						return 0;
+					}
+					
+					strcpy(Output_Matrix[i],MD5);
+					if (MD5) {FREE(MD5);MD5=NULL;}
+				}
+				
+				CreateVarFromPtr( Rhs+1, MATRIX_OF_STRING_DATATYPE, &m1, &n1, Output_Matrix );
+				LhsVar(1) = Rhs+1 ;
 				C2F(putlhsvar)();
 			}
 			else
@@ -97,6 +124,11 @@ int C2F(sci_getmd5) _PARAMS((char *fname,unsigned long fname_len))
 			Scierror(999,_("%s: Wrong type for input arguments: Strings expected.\n"),fname);
 		}
 	}
+	
+	freeArrayOfString(Input_Matrix,mn);
+	freeArrayOfString(Output_Matrix,mn);
+	
 	return 0;
 }
+
 /*--------------------------------------------------------------------------*/
