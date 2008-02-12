d367891469c7bb19659f92867e6c9216665f0bbc
Allan Cornet <allan.cornet@scilab.org>
2008-02-12 08:57:09 +0000

    free pointers

--- scilab/modules/core/sci_gateway/c/sci_getdebuginfo.c
+++ scilab/modules/core/sci_gateway/c/sci_getdebuginfo.c
@@ -4,40 +4,46 @@
 /*--------------------------------------------------------------------------*/
 #include <string.h>
 #include <stdio.h>
-#ifndef _MSC_VER
-#include <unistd.h>
-#endif
 #include "machine.h"
 #include "getstaticdebuginfo.h"
 #include "gw_core.h"
 #include "stack-c.h"
 #include "version.h"
 #include "MALLOC.h"
+#include "freeArrayOfString.h"
 /*--------------------------------------------------------------------------*/
 
 int C2F(sci_getdebuginfo) _PARAMS((char *fname,unsigned long fname_len))
 {
+	char **outputDynamicList = NULL;
+	char **outputStaticList = NULL;
+	static int n1=1,m1=0;
+	static int n2=1,m2=0;
 
-	static int n1=1,m1=0,n2=1,m2=0;
 	CheckRhs(0,0);
 	CheckLhs(0,2);
-	
 
 #ifndef _MSC_VER
 
-	char **outputDynamicList=getDynamicDebugInfo(&m1);
-	char **outputStaticList=getStaticDebugInfo(&m2);
+	outputDynamicList = getDynamicDebugInfo(&m1);
+	outputStaticList = getStaticDebugInfo(&m2);
 
 	CreateVarFromPtr(Rhs+1,MATRIX_OF_STRING_DATATYPE, &m1, &n1, outputDynamicList);
 	LhsVar(1) = Rhs+1;
-	CreateVarFromPtr(Rhs+2,MATRIX_OF_STRING_DATATYPE, &m2, &n2, outputStaticList);
-	LhsVar(2) = Rhs+2;
+
+	if (Lhs == 2)
+	{
+		CreateVarFromPtr(Rhs+2,MATRIX_OF_STRING_DATATYPE, &m2, &n2, outputStaticList);
+		LhsVar(2) = Rhs+2;
+	}
 #else
 	/* TO DO : Windows part */
 	/* In my todo list ;) A.C */
 	LhsVar(1) = 0;
 #endif
 	C2F(putlhsvar)();
-return 0;
+	freeArrayOfString(outputDynamicList,m1);
+	freeArrayOfString(outputStaticList,m2);
+	return 0;
 }
 /*--------------------------------------------------------------------------*/
