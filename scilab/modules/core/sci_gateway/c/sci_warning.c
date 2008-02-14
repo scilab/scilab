ab0fe9a5cb223188b7acde20e3896a24eaac5460
Allan Cornet <allan.cornet@scilab.org>
2008-02-14 11:52:55 +0000

    update warning to manage matrix of string warning(['a','b'])

--- scilab/modules/core/sci_gateway/c/sci_warning.c
+++ scilab/modules/core/sci_gateway/c/sci_warning.c
@@ -12,61 +12,80 @@
 #include "MALLOC.h"
 #include "Scierror.h"
 #include "localization.h"
+#include "freeArrayOfString.h"
 /*--------------------------------------------------------------------------*/
 int C2F(sci_warning) _PARAMS((char *fname,unsigned long fname_len))
 {
-	static int l1,n1,m1;
+	static int l1 = 0,n1 = 0,m1 = 0;
 
 	CheckRhs(1,1);
 	CheckLhs(1,1);
 
 	if ( GetType(1) == sci_strings )
 	{
+		char **Input_Strings = NULL;
 		char *Param=NULL;
 
-		GetRhsVar(1,STRING_DATATYPE,&m1,&n1,&l1);
-		Param=cstk(l1);
+		GetRhsVar(1,MATRIX_OF_STRING_DATATYPE,&m1,&n1,&Input_Strings);
 
-		if ( (strcmp(Param,"off") == 0) || (strcmp(Param,"on") == 0) )
+		if ( (m1*n1) == 1 )
 		{
-			if (strcmp(Param,"off") == 0)
+			if ( (strcmp(Input_Strings[0],"off") == 0) || (strcmp(Input_Strings[0],"on") == 0) )
 			{
-				setWarningMode(FALSE);
+				if (strcmp(Input_Strings[0],"off") == 0)
+				{
+					setWarningMode(FALSE);
+				}
+				else  /* on */
+				{
+					setWarningMode(TRUE);
+				}
 			}
-			else  /* on */
+			else
 			{
-				setWarningMode(TRUE);
+				if (strcmp(Input_Strings[0],"query") == 0)
+				{
+					char *Output=NULL;
+
+					Output=(char*)MALLOC(4*sizeof(char));
+
+					if (getWarningMode())
+					{
+						strcpy(Output,"on");
+					}
+					else
+					{
+						strcpy(Output,"off");
+					}
+
+					n1=1;
+					CreateVarFromPtr(Rhs+ 1,STRING_DATATYPE,(m1=(int)strlen(Output), &m1),&n1,&Output);
+					LhsVar(1) = Rhs+1;
+					C2F(putlhsvar)();
+					if (Output) {FREE(Output);Output=NULL;}
+					freeArrayOfString(Input_Strings,n1);
+					return 0;
+				}
+				else if ( getWarningMode() )
+				{
+					sciprint(_("WARNING : %s"),Input_Strings[0]);
+					sciprint("\n");
+					freeArrayOfString(Input_Strings,n1);
+				}
 			}
 		}
 		else
 		{
-			if (strcmp(Param,"query") == 0)
+			int i = 0;
+			if ( getWarningMode() )
 			{
-				char *Output=NULL;
-
-				Output=(char*)MALLOC(4*sizeof(char));
-
-				if (getWarningMode())
+				for (i = 0;i < m1*n1;i++)
 				{
-					strcpy(Output,"on");
+					sciprint(_("WARNING : %s\n"),Input_Strings[i]);
 				}
-				else
-				{
-					strcpy(Output,"off");
-				}
-
-				n1=1;
-				CreateVarFromPtr(Rhs+ 1,STRING_DATATYPE,(m1=(int)strlen(Output), &m1),&n1,&Output);
-				LhsVar(1) = Rhs+1;
-				C2F(putlhsvar)();
-				if (Output) {FREE(Output);Output=NULL;}
-				return 0;
-			}
-			else if ( getWarningMode() )
-			{
-				sciprint(_("WARNING : %s"),Param);
 				sciprint("\n");
 			}
+			freeArrayOfString(Input_Strings,m1*n1);
 		}
 	}
 	else
