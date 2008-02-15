d6f7e0353a57334226a5fdb8f24ab610476a6abb
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2008-02-15 17:16:19 +0000

    CeCILL headers added

--- scilab/modules/localization/sci_gateway/c/sci_setlanguage.c
+++ scilab/modules/localization/sci_gateway/c/sci_setlanguage.c
@@ -1,9 +1,16 @@
-/*--------------------------------------------------------------------------*/
-/* INRIA 2007/2008 */
-/* @author Allan CORNET */
-/* @author Sylvestre LEDRU */
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2007/2008 - INRIA - Sylvestre LEDRU
+ * Copyright (C) 2007 - INRIA - Allan CORNET
+ * 
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at    
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 
-/*--------------------------------------------------------------------------*/
 #include "gw_localization.h"
 #include "setgetlanguage.h"
 #include "Scierror.h"
--- scilab/modules/localization/src/c/defaultlanguage.h
+++ scilab/modules/localization/src/c/defaultlanguage.h
@@ -1,14 +1,14 @@
 /*
-**  -*- C -*-
-**
-** defaultlanguage.h
-** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
-**
-** Started on  Tue Oct 30 10:28:24 2007 bruno
-** Last update Tue Oct 30 10:30:19 2007 bruno
-**
-** Copyright INRIA 2007
-*/
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2007 - INRIA - Bruno JOFRET
+ * 
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at    
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 
 #ifndef __DEFAULTLANGUAGE_H__
 #define __DEFAULTLANGUAGE_H__
