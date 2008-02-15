aec01401e19a5ec9a50392a49539ea44c9ff9e11
Allan Cornet <allan.cornet@scilab.org>
2008-02-15 18:05:08 +0000

    Add license header (jvm module)

--- scilab/modules/jvm/src/java/org/scilab/modules/jvm/ClassPath.java
+++ scilab/modules/jvm/src/java/org/scilab/modules/jvm/ClassPath.java
@@ -1,8 +1,16 @@
 /*--------------------------------------------------------------------------*/
-/**
-* Loading classes at runtime.
-* @author Allan CORNET - INRIA 2007
-*/
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) INRIA - Allan CORNET
+ * 
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at    
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
 /*--------------------------------------------------------------------------*/
 package org.scilab.modules.jvm;
 /*--------------------------------------------------------------------------*/ 
