f7be64615e591ae422b3772252cf95035f044e29
Pierre Marechal <pierre.marechal@scilab.org>
2008-02-15 13:16:51 +0000

    Add license header

--- scilab/modules/double/sci_gateway/fortran/matcmp.f
+++ scilab/modules/double/sci_gateway/fortran/matcmp.f
@@ -1,12 +1,17 @@
-c			================================================
-c     Copyright INRIA
-c			================================================
+
+c Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+c Copyright (C) INRIA
+c
+c This file must be used under the terms of the CeCILL.
+c This source file is licensed as described in the file COPYING, which
+c you should have received as part of this distribution.  The terms
+c are also available at
+c http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
 
       subroutine matcmp
 c     
 c     == <= >= <>
-c     
-c     Copyright INRIA
+
       include 'stack.h'
 c     
       integer top0,op
