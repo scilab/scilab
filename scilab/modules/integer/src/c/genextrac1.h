b02c627c85bbf129312499fba7313326dbc9fd86
Allan Cornet <allan.cornet@scilab.org>
2008-02-15 15:37:22 +0000

    Add license header (integer module)

--- scilab/modules/integer/src/c/genextrac1.h
+++ scilab/modules/integer/src/c/genextrac1.h
@@ -1,6 +1,15 @@
 /*--------------------------------------------------------------------------*/
-/* INRIA 2006 */
-/* Sylvestre Ledru */
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) INRIA - Sylvestre LEDRU
+ * 
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at    
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 /*--------------------------------------------------------------------------*/
 #ifndef __GENEXTRACT1_H__
 #define __GENEXTRACT1_H__
--- scilab/modules/integer/src/c/genmmin.c
+++ scilab/modules/integer/src/c/genmmin.c
@@ -1,6 +1,16 @@
 #include "machine.h"
 #include "genmmin.h"
-/*     Copyright INRIA */
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) INRIA -
+ * 
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at    
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 
 
 /* NEED COMMENTS !!!! */
