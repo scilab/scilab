115cf89d6acf246df7dc1006418dab741d9e2550
Bruno Jofret <bruno.jofret@scilab.org>
2008-02-19 10:52:58 +0000

    BUG FIX Scierror

--- scilab/modules/core/sci_gateway/c/sci_getversion.c
+++ scilab/modules/core/sci_gateway/c/sci_getversion.c
@@ -1,11 +1,12 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
- * Copyright (C) 2006 - INRIA Allan CORNET
- * 
+ * Copyright (C) 2006 - INRIA - Allan CORNET
+ * Copyright (C) 2008 - INRIA - Bruno JOFRET
+ *
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
  * you should have received as part of this distribution.  The terms
- * are also available at    
+ * are also available at
  * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
  *
  */
@@ -234,7 +235,7 @@ static int getversion_one_rhs(void)
 	}
 	else
 	{
-		Scierror(999,_("%s: Wrong type for first input argument: String expected.\n"));
+		Scierror(999,_("%s: Wrong type for first input argument: String expected.\n"), "getversion");
 		return 0;
 	}
 	return 0;
