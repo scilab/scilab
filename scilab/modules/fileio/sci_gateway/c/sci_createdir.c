634c3fdb4d62be123eaab3c6dd538c3d2257a041
Bruno Jofret <bruno.jofret@scilab.org>
2008-02-19 10:58:49 +0000

    BUG FIX Scierror

--- scilab/modules/fileio/sci_gateway/c/sci_createdir.c
+++ scilab/modules/fileio/sci_gateway/c/sci_createdir.c
@@ -2,11 +2,11 @@
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2007 - INRIA - Allan CORNET
  * ...
- * 
+ *
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
  * you should have received as part of this distribution.  The terms
- * are also available at    
+ * are also available at
  * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
  *
  */
@@ -60,7 +60,7 @@ int C2F(sci_createdir) _PARAMS((char *fname,unsigned long l))
 	}
 	else
 	{
-		Scierror(999,_("%s: Wrong type for first input argument: String expected.\n"));
+		Scierror(999,_("%s: Wrong type for first input argument: String expected.\n"), fname);
 	}
 	return 0;
 }
