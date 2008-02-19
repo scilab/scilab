1ce9764656ab8eb948a896cc8885bf9a2263dfa8
Bruno Jofret <bruno.jofret@scilab.org>
2008-02-19 11:01:58 +0000

    BUG FIX Scierror

--- scilab/modules/fileio/sci_gateway/c/sci_removedir.c
+++ scilab/modules/fileio/sci_gateway/c/sci_removedir.c
@@ -1,12 +1,12 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2007 - INRIA - Allan CORNET
- * ...
- * 
+ * Copyright (C) 2008 - INRIA - Bruno JOFRET
+ *
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
  * you should have received as part of this distribution.  The terms
- * are also available at    
+ * are also available at
  * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
  *
  */
@@ -14,7 +14,7 @@
 #include <string.h>
 #include "gw_fileio.h"
 #include "stack-c.h"
-#include "MALLOC.h" 
+#include "MALLOC.h"
 #include "removedir.h"
 #include "isdir.h"
 #include "Scierror.h"
@@ -22,7 +22,7 @@
 #include "localization.h"
 /*--------------------------------------------------------------------------*/
 int C2F(sci_removedir) _PARAMS((char *fname,unsigned long l))
-{	
+{
 	CheckRhs(1,1);
 	CheckLhs(0,1);
 
@@ -49,11 +49,11 @@ int C2F(sci_removedir) _PARAMS((char *fname,unsigned long l))
 		*istk(l1) = bOK;
 
 		LhsVar(1)=Rhs+1;
-		C2F(putlhsvar)();		
+		C2F(putlhsvar)();
 	}
 	else
 	{
-		Scierror(999,_("%s: Wrong type for first input argument: String expected.\n"));
+		Scierror(999,_("%s: Wrong type for first input argument: String expected.\n"), fname);
 	}
 	return 0;
 }
