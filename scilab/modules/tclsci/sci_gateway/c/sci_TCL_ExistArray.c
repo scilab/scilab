ba26b18d1c26d814c3b6748ed568c7712a4f4d4b
Bruno Jofret <bruno.jofret@scilab.org>
2008-02-19 11:57:33 +0000

    BUG FIX Scierror

--- scilab/modules/tclsci/sci_gateway/c/sci_TCL_ExistArray.c
+++ scilab/modules/tclsci/sci_gateway/c/sci_TCL_ExistArray.c
@@ -1,6 +1,15 @@
-/*--------------------------------------------------------------------------*/
-/* INRIA 2005 */
-/* Allan CORNET */
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2005-2008 - INRIA - Allan CORNET
+ * Copyright (C) 2008-2008 - INRIA - Bruno JOFRET
+ *
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 /*--------------------------------------------------------------------------*/
 #include "TCL_Global.h"
 #include "gw_tclsci.h"
@@ -19,7 +28,7 @@ int C2F(sci_TCL_ExistArray) _PARAMS((char *fname,unsigned long l))
 
 	CheckRhs(1,2);
 	CheckLhs(1,1);
-	
+
 	if (GetType(1) == sci_strings)
 	{
 		char *VarName=NULL;
@@ -32,7 +41,7 @@ int C2F(sci_TCL_ExistArray) _PARAMS((char *fname,unsigned long l))
 			Scierror(999,_("%s: Error main TCL interpreter not initialized.\n"),fname);
 			return 0;
 		}
-		
+
 		if (Rhs==2)
 		{
 			/* two arguments given - get a pointer on the slave interpreter */
@@ -48,7 +57,7 @@ int C2F(sci_TCL_ExistArray) _PARAMS((char *fname,unsigned long l))
 			}
 			else
 			{
-				Scierror(999,_("%s: Wrong input argument: String expected.\n"));
+				Scierror(999,_("%s: Wrong input argument: String expected.\n"), fname);
 				return 0;
 			}
 		}
@@ -59,7 +68,7 @@ int C2F(sci_TCL_ExistArray) _PARAMS((char *fname,unsigned long l))
 		}
 
 		ValRet=TCL_ArrayExist(TCLinterpreter,VarName);
-		
+
 		n1=1;
 		if ( ValRet )
 		{
@@ -71,7 +80,7 @@ int C2F(sci_TCL_ExistArray) _PARAMS((char *fname,unsigned long l))
 			CreateVar(Rhs+1,MATRIX_OF_BOOLEAN_DATATYPE, &n1,&n1,&l1);
 			*istk(l1)=(int)(FALSE);
 		}
-	
+
 		LhsVar(1)=Rhs+1;
 		C2F(putlhsvar)();
 	}
@@ -80,7 +89,7 @@ int C2F(sci_TCL_ExistArray) _PARAMS((char *fname,unsigned long l))
 		Scierror(999,_("%s: Wrong input argument: String expected.\n"),fname);
 		return 0;
 	}
-	
+
 	return 0;
 }
 /*--------------------------------------------------------------------------*/
