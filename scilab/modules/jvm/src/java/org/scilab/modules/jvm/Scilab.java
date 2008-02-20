04b4e2bf2cdbff20fef007fe2a4d1733cb486460
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2008-02-20 12:42:25 +0000

    Allan, please respect the work of other

--- scilab/modules/jvm/src/java/org/scilab/modules/jvm/Scilab.java
+++ scilab/modules/jvm/src/java/org/scilab/modules/jvm/Scilab.java
@@ -1,6 +1,9 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) INRIA - Allan CORNET
+ * Copyright (C) INRIA - Vincent COUVERT
+ * Copyright (C) 2007-2008 - INRIA - Sylvestre LEDRU
+ * Copyright (C) INRIA - Bruno JOFRET
  * 
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
