9278768a6b13f77849fa0e3a8f46662a6d986abf
Laurent VAYLET <laurent.vaylet@scilab.org>
2008-02-21 09:29:12 +0000

    Cosmetics

--- scilab/modules/scicos_blocks/src/c/prod.c
+++ scilab/modules/scicos_blocks/src/c/prod.c
@@ -30,21 +30,22 @@ prod(flag,nevprt,t,xd,x,nx,z,nz,tvec,ntvec,rpar,nrpar,
      integer *nin,outsz[],*nout;
      double x[],xd[],z[],tvec[],rpar[];
      double *inptr[],*outptr[],*t;
-
 {
-    int k,i,n;
-    double *y;
-    double *u;
-    
-    n=outsz[0]; /* insz[0]==insz[1] .. ==insz[*nin]== outsz[0] */
+  int k,i,n;
+  double *y;
+  double *u;
+  
+  n=outsz[0]; /* insz[0]==insz[1] .. ==insz[*nin]== outsz[0] */
 
-    y=(double *)outptr[0];
+  y=(double *)outptr[0];
 
-    for (i=0;i<n;i++) {
-      y[i]=1.0;
-      for (k=0;k<*nin;k++) {
-	u=(double *)inptr[k];
-	y[i]=y[i]*u[i];
-      }
+  for (i=0;i<n;i++)
+  {
+    y[i]=1.0;
+    for (k=0;k<*nin;k++)
+    {
+      u=(double *)inptr[k];
+      y[i]=y[i]*u[i];
     }
+  }
 }
