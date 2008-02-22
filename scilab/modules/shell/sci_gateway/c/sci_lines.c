62b9203a5d5e29157bbb11a88b965ffeb5eb81ce
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2008-02-22 13:05:42 +0000

    really useless variable ...

--- scilab/modules/shell/sci_gateway/c/sci_lines.c
+++ scilab/modules/shell/sci_gateway/c/sci_lines.c
@@ -48,11 +48,9 @@ int C2F(sci_lines) _PARAMS((char *fname,unsigned long fname_len))
 			if ( GetType(2) == sci_matrix )
 			{
 				int l2 = 0,n2 = 0, m2 = 0;
-				int columns = 0;
 
 				GetRhsVar(2,MATRIX_OF_DOUBLE_DATATYPE,&m2,&n2,&l2);
-				columns = (int)*stk(l2);
-				setColumnsSize(columns);
+				setColumnsSize((int)*stk(l2));
 			}
 			else
 			{
@@ -64,11 +62,9 @@ int C2F(sci_lines) _PARAMS((char *fname,unsigned long fname_len))
 		if ( GetType(1) == sci_matrix )
 		{
 			int l1 = 0,n1 = 0, m1 = 0;
-			int lines = 0;
 
 			GetRhsVar(1,MATRIX_OF_DOUBLE_DATATYPE,&m1,&n1,&l1);
-			lines = (int)*stk(l1);
-			setLinesSize(lines);
+			setLinesSize((int)*stk(l1));
 		}
 		else
 		{
