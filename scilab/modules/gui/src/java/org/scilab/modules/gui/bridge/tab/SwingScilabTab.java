2a7da9b7a3ddd4c0711e93ea375a74bae21466a0
Vincent Couvert <vincent.couvert@scilab.org>
2008-02-26 16:27:53 +0000

    Remove unused imports

--- scilab/modules/gui/src/java/org/scilab/modules/gui/bridge/tab/SwingScilabTab.java
+++ scilab/modules/gui/src/java/org/scilab/modules/gui/bridge/tab/SwingScilabTab.java
@@ -1,6 +1,6 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
- * Copyright (C) 2007 - INRIA - Vincent Couvert
+ * Copyright (C) 20072008 - INRIA - Vincent Couvert
  * Copyright (C) 2007 - INRIA - Bruno JOFRET
  * Copyright (C) 2007 - INRIA - Marouane BEN JELLOUL
  * 
@@ -14,14 +14,6 @@
 
 package org.scilab.modules.gui.bridge.tab;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.FocusEvent;
-import java.awt.event.FocusListener;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-
-import javax.swing.AbstractAction;
 import javax.swing.Action;
 
 import org.flexdock.docking.DockingConstants;
