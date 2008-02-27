1df60948b16991773c3aa9228668aa7ca8dad8f2
Bruno Jofret <bruno.jofret@scilab.org>
2008-02-27 08:25:15 +0000

    * Adding Thread Cancellation

--- scilab/modules/core/includes/Thread_Wrapper.h
+++ scilab/modules/core/includes/Thread_Wrapper.h
@@ -1,14 +1,15 @@
 /*
-**  -*- C -*-
-**
-** Thread_Wrapper.h
-** Made by  Bruno JOFRET <bruno.jofret@inria.fr>
-**
-** Started on  Thu Jan 10 15:00:38 2008 bruno
-** Last update Thu Jan 10 15:39:20 2008 bruno
-**
-** Copyright INRIA 2008
-*/
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2008-2008 - INRIA - Bruno JOFRET
+ *  Copyright (C) 2008-2008 - INRIA - Allan CORNET
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 
 /*
 ** Thread wrapper to have an easy way to manage
@@ -28,18 +29,18 @@
 	#define	pthread_t								HANDLE
 	#define	pthread_create(t,u,f,d)					*(t)=CreateThread(NULL,0,(LPTHREAD_START_ROUTINE)f,d,0,NULL)
 	#define	pthread_join(thread, result)			((WaitForSingleObject((thread),INFINITE)!=WAIT_OBJECT_0) || !CloseHandle(thread))
-	
+
     /* returns to previous version with mutex */
     /* I don't want to manage a exception here on some windows and not some others */
 	/* http://msdn2.microsoft.com/en-us/library/ms682411.aspx */
 	/* http://msdn2.microsoft.com/en-us/library/ms683472(VS.85).aspx */
     /* Windows Server 2003 and Windows XP/2000:  In low memory situations,
 	   InitializeCriticalSection can raise a STATUS_NO_MEMORY exception.
-	   This exception was eliminated starting with Windows Vista. 
+	   This exception was eliminated starting with Windows Vista.
 	 */
 
 	#define pthread_mutex_t							HANDLE
-		
+
 	#define pthread_mutex_init(pobject,pattr)       (*(pobject)=CreateMutex(NULL,FALSE,NULL))
 	#define pthread_mutex_destroy(pobject)          CloseHandle(*pobject)
 	#define pthread_mutex_lock(pobject)				WaitForSingleObject(*pobject,INFINITE)
@@ -64,6 +65,7 @@ typedef pthread_cond_t __threadSignal;
 #define __UnLock(lockName)			pthread_mutex_unlock(lockName)
 
 #define __InitSignal(signalName)		pthread_cond_init(signalName, NULL)
+
 #define __Signal(signalName)			pthread_cond_signal(signalName)
 
 #define __Wait(signalName, lockName)		pthread_cond_wait(signalName, lockName)
@@ -72,6 +74,7 @@ typedef pthread_cond_t __threadSignal;
 
 #define __WaitThreadDie(threadId)		pthread_join(threadId, NULL)
 
+#define __TerminateThread(threadId)		pthread_cancel(threadId)
 
 #endif /* !__THREAD_WRAPPER_H__ */
 
