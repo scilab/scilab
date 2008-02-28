993c8648333a902039c15e9d0d7eed9808d526e0
Bruno Jofret <bruno.jofret@scilab.org>
2008-02-28 13:31:10 +0000

    Leon : Code cleaner...

--- scilab/modules/core/src/c/realmain.c
+++ scilab/modules/core/src/c/realmain.c
@@ -1,11 +1,12 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2006 - INRIA - Allan CORNET
- * 
+ * Copyright (C) 2008 - INRIA - Bruno JOFRET
+ *
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
  * you should have received as part of this distribution.  The terms
- * are also available at    
+ * are also available at
  * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
  *
  */
@@ -31,136 +32,129 @@ static int no_startup_flag=0;
 /*--------------------------------------------------------------------------*/
 void realmain(int no_startup_flag_l, char *initial_script, InitScriptType initial_script_type, int memory)
 {
-	static int initialization=-1;
-	int ierr=0;
-	char *startup=(char*)MALLOC(sizeof(char)*PATH_MAX+1);
-	Set_no_startup_flag(no_startup_flag_l);
+  static int initialization=-1;
+  int ierr=0;
+  char *startup=(char*)MALLOC(sizeof(char)*PATH_MAX+1);
+  Set_no_startup_flag(no_startup_flag_l);
 
-	/* create temp directory */
-	C2F(settmpdir)();
+  /* create temp directory */
+  C2F(settmpdir)();
 
-	/* signals */
+  /* signals */
 #ifdef _MSC_VER
   /* Don't want catch signal under Linux ... */
 #ifdef ENABLESIG
 #ifndef DEBUG
-	signal(SIGINT,sci_clear_and_exit);
+  signal(SIGINT,sci_clear_and_exit);
 #ifdef SIGBUS
-	signal(SIGBUS,sci_clear_and_exit);
+  signal(SIGBUS,sci_clear_and_exit);
 #endif
-	signal(SIGSEGV,sci_clear_and_exit);
+  signal(SIGSEGV,sci_clear_and_exit);
 #if SIGQUIT
-	signal(SIGQUIT,sci_clear_and_exit);
+  signal(SIGQUIT,sci_clear_and_exit);
 #endif
 #ifdef SIGHUP
-	signal(SIGHUP,sci_clear_and_exit);
+  signal(SIGHUP,sci_clear_and_exit);
 #endif
 #ifdef SIGUSR1
-	signal(SIGUSR1,sci_usr1_signal);
+  signal(SIGUSR1,sci_usr1_signal);
 #endif
 #endif
 
 #ifdef _MSC_VER
-	signal(SIGILL,sci_clear_and_exit);
-	signal(SIGFPE,sci_clear_and_exit);
-	signal(SIGTERM,sci_clear_and_exit);
-	signal(SIGBREAK,sci_clear_and_exit);
-	signal(SIGABRT,sci_clear_and_exit);
+  signal(SIGILL,sci_clear_and_exit);
+  signal(SIGFPE,sci_clear_and_exit);
+  signal(SIGTERM,sci_clear_and_exit);
+  signal(SIGBREAK,sci_clear_and_exit);
+  signal(SIGABRT,sci_clear_and_exit);
 #endif
 #endif
 #endif
 
-	/*  prepare startup script  */
+  /*  prepare startup script  */
 
-	if ( no_startup_flag_l == 0) 
-		{
-			/* execute a startup */
-			if ( initial_script != NULL ) 
-				{
-					switch ( initial_script_type ) 
-						{
-							case SCILAB_SCRIPT : 
-								sprintf(startup,"%s;exec('%s',-1)",get_sci_data_strings(STARTUP_ID),initial_script);
-								break;
-							case SCILAB_CODE : 
-								sprintf(startup,"%s;%s;",get_sci_data_strings(STARTUP_ID),initial_script);
-								break;
-						}
-				}
-			else 
-				{
-					sprintf(startup,"%s;",get_sci_data_strings(STARTUP_ID));
-				}
-		}
-	else 
-		{
-			/* No startup but maybe an initial script  */
-			if ( initial_script != NULL ) 
-				{
-					switch ( initial_script_type ) 
-						{
-							case SCILAB_SCRIPT : 
-								sprintf(startup,"exec('%s',-1)",initial_script); 
-								break;
-							case SCILAB_CODE : 
-								sprintf(startup,"%s;",initial_script);   
-								break;
-						}
-				}
-			else sprintf(startup," ");
-		}
+  if ( no_startup_flag_l == 0)
+    {
+      /* execute a startup */
+      if ( initial_script != NULL )
+	{
+	  switch ( initial_script_type )
+	    {
+	    case SCILAB_SCRIPT :
+	      sprintf(startup,"%s;exec('%s',-1)",get_sci_data_strings(STARTUP_ID),initial_script);
+	      break;
+	    case SCILAB_CODE :
+	      sprintf(startup,"%s;%s;",get_sci_data_strings(STARTUP_ID),initial_script);
+	      break;
+	    }
+	}
+      else
+	{
+	  sprintf(startup,"%s;",get_sci_data_strings(STARTUP_ID));
+	}
+    }
+  else
+    {
+      /* No startup but maybe an initial script  */
+      if ( initial_script != NULL )
+	{
+	  switch ( initial_script_type )
+	    {
+	    case SCILAB_SCRIPT :
+	      sprintf(startup,"exec('%s',-1)",initial_script);
+	      break;
+	    case SCILAB_CODE :
+	      sprintf(startup,"%s;",initial_script);
+	      break;
+	    }
+	}
+      else sprintf(startup," ");
+    }
 
-#ifndef _MSC_VER
-		/* initialize scilab interp  */
-		C2F(inisci)(&initialization, &memory, &ierr);
-		if (ierr > 0) C2F(sciquit)() ;
-		/* execute the initial script and enter scilab */ 
-		C2F(scirun)(startup,strlen(startup));
+  /* initialize scilab interp  */
+  C2F(inisci)(&initialization, &memory, &ierr);
+  if (ierr > 0) C2F(sciquit)() ;
+  /* execute the initial script and enter scilab */
 
+
+#if !defined(_DEBUG) && defined(_MSC_VER)
+  _try
+    {
+      C2F(scirun)(startup,strlen(startup));
+    }
+  _except (EXCEPTION_EXECUTE_HANDLER)
+    {
+    Rerun:
+      {
+	ExceptionMessage(GetExceptionCode(),NULL);
+      }
+      _try
+	{
+	  C2F(scirun)("",strlen(""));
+	}
+      _except (EXCEPTION_EXECUTE_HANDLER)
+	{
+	  goto Rerun;
+	}
+
+    }
 #else
-	/* initialize scilab interp under Microsoft Windows  */
-	C2F(inisci)(&initialization, &memory, &ierr);
-	if (ierr > 0) C2F(sciquit)() ;
-
-	/* execute the initial script and enter scilab */ 
-#ifndef _DEBUG
-	_try
-		{
-			C2F(scirun)(startup,strlen(startup));
-		}
-	_except (EXCEPTION_EXECUTE_HANDLER) 
-		{
-		Rerun:
-			{
-				ExceptionMessage(GetExceptionCode(),NULL);
-			}
-			_try
-				{
-					C2F(scirun)("",strlen(""));
-				}
-			_except (EXCEPTION_EXECUTE_HANDLER) 
-				{
-					goto Rerun;
-				}
-
-		}
-#else
-	C2F(scirun)(startup,(long int)strlen(startup));
+  C2F(scirun)(startup,(long int)strlen(startup));
 #endif
-#endif
-	FREE(startup);
-	/* cleaning */
-	C2F(sciquit)();
+
+  FREE(startup);
+  /* cleaning */
+  C2F(sciquit)();
 }
 /*--------------------------------------------------------------------------*/
 void Set_no_startup_flag(int start)
 {
-	no_startup_flag=start;
+  no_startup_flag=start;
 }
 /*--------------------------------------------------------------------------*/
 int Get_no_startup_flag(void)
 {
-	return no_startup_flag;
+  return no_startup_flag;
 }
 /*--------------------------------------------------------------------------*/
 
