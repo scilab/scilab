5741e217d7edf735830d8662b6cf07955021664e
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2008-03-17 13:10:56 +0000

    missing file

---
+++ scilab/modules/umfpack/src/noumfpack/noumfpack.c
@@ -0,0 +1,32 @@
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2008 - INRIA - Sylvestre LEDRU
+ * 
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at    
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+#include "machine.h" 
+#include "Scierror.h"
+#include "localization.h"
+/*--------------------------------------------------------------------------*/ 
+int gw_umfpack(void)
+{
+	Scierror(999,_("Scilab UMFPACK module not installed.\n"));
+	return 0;
+}
+/*--------------------------------------------------------------------------*/ 
+BOOL withumfpack(void)
+{ 
+	return FALSE;
+}
+/*--------------------------------------------------------------------------*/ 
+int C2F(withumfpack)(int *rep)
+{ 
+	*rep =0;
+	return 0;
+}
+/*--------------------------------------------------------------------------*/ 
