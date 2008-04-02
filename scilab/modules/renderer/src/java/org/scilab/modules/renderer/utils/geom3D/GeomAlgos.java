24bce3ead92c69ee4798ea043144ab13626a644f
Jean-baptiste Silvy <jean-baptiste.silvy@scilab.org>
2008-04-02 09:48:53 +0000

    Simplify rectangle concealing algorithm.

--- scilab/modules/renderer/src/java/org/scilab/modules/renderer/utils/geom3D/GeomAlgos.java
+++ scilab/modules/renderer/src/java/org/scilab/modules/renderer/utils/geom3D/GeomAlgos.java
@@ -31,57 +31,28 @@ public final class GeomAlgos {
 	}
 	
 	/**
-	 * Find if a two point are on the same side of a line
-	 * @param pointOnline1 first point to define the line
-	 * @param pointOnline2 second point to define the line
-	 * @param point1 first point to test 
-	 * @param point2 second point to test
-	 * @return true if the two vector are on the same side of the line
-	 */
-	private static boolean sameSide(Vector3D pointOnline1, Vector3D pointOnline2,
-							 Vector3D point1, Vector3D point2) {
-		Vector3D lineVector = pointOnline2.substract(pointOnline1);
-		Vector3D side1 = lineVector.crossProduct(point1.substract(pointOnline1));
-		Vector3D side2 = lineVector.crossProduct(point2.substract(pointOnline1));
-		
-		return side1.dotProduct(side2) >= 0.0;
-	}
-	
-	/**
-	 * Find a point is inside a rectangle in 2D
-	 * @param rectangle four corners of the rectangle
-	 * @param point point to chack
-	 * @return true if the poinr is inside, false otherwise
-	 */
-	private static boolean isPointInsideRectangle(Vector3D[] rectangle, Vector3D point) {
-		for (int i = 0; i < RECTANGLE_NB_CORNERS; i++) {
-			if (!sameSide(rectangle[i % RECTANGLE_NB_CORNERS],
-					      rectangle[(i + 1) % RECTANGLE_NB_CORNERS],
-					      rectangle[(i + 2) % RECTANGLE_NB_CORNERS],
-					      point)) {
-				return false;
-			}
-		}
-		return true;
-	}
-	
-	/**
 	 * Find if the two rectangles given by their corners are concealing or not in 2D
 	 * @param rectangle1 four corners of the first rectangle
 	 * @param rectangle2 four corners of the second rectangle
 	 * @return true if the rectangle are concealing, false otherwise
 	 */
 	public static boolean areRectangleConcealing(Vector3D[] rectangle1, Vector3D[] rectangle2) {
-		for (int i = 0; i < RECTANGLE_NB_CORNERS; i++) {
-			// we need to cjeck twice since a rectangle might be smaller than the other
-			// and fit inside
-			if (isPointInsideRectangle(rectangle1, rectangle2[i])) {
-				return true;
-			}
-			if (isPointInsideRectangle(rectangle2, rectangle1[i])) {
-				return true;
-			}
+		// get min and max in 2D for each rectangle
+		double xMin1 = Math.min(rectangle1[0].getX(), rectangle1[2].getX());
+		double yMin1 = Math.min(rectangle1[0].getY(), rectangle1[2].getY());
+		double xMax1 = Math.max(rectangle1[0].getX(), rectangle1[2].getX());
+		double yMax1 = Math.max(rectangle1[0].getY(), rectangle1[2].getY());
+		double xMin2 = Math.min(rectangle2[0].getX(), rectangle2[2].getX());
+		double yMin2 = Math.min(rectangle2[0].getY(), rectangle2[2].getY());
+		double xMax2 = Math.max(rectangle2[0].getX(), rectangle2[2].getX());
+		double yMax2 = Math.max(rectangle2[0].getY(), rectangle2[2].getY());
+		
+		// rectangle are concealing if 2 bounds are within the others
+		if (((xMin1 >= xMin2 && xMin1 <= xMax2) || (xMax1 >= xMin2 && xMax1 <= xMax2))
+			&& ((yMin1 >= yMin2 && yMin1 <= yMax2)  || (yMax1 >= yMin2 && yMax1 <= yMax2))) {
+			return true;
 		}
+					
 		return false;
 	}
 	
