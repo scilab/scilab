2b0245533c2b58e5246037061a7a3f9c340edcac
Allan Cornet <allan.cornet@scilab.org>
2008-04-03 08:09:22 +0000

    uses freeArrayOfString

--- scilab/modules/core/sci_gateway/c/sci_sciargs.c
+++ scilab/modules/core/sci_gateway/c/sci_sciargs.c
@@ -14,6 +14,7 @@
 #include "stack-c.h"
 #include "getcommandlineargs.h"
 #include "MALLOC.h"
+#include "freeArrayOfString.h"
 /*--------------------------------------------------------------------------*/
 int C2F(sci_sciargs) _PARAMS((char *fname,unsigned long fname_len))
 {
@@ -31,20 +32,7 @@ int C2F(sci_sciargs) _PARAMS((char *fname,unsigned long fname_len))
 	LhsVar(1)=Rhs+1;
 	C2F(putlhsvar)();
 
-	if (arguments)
-	{
-		int i = 0;
-		for (i = 0;i < nb_arguments; i++)
-		{
-			if (arguments[i])
-			{
-				FREE(arguments[i]);
-				arguments[i] = NULL;
-			}
-		}
-		FREE(arguments);
-		arguments = NULL;
-	}
+	freeArrayOfString(arguments,nb_arguments);
 
 	return 0;
 }
