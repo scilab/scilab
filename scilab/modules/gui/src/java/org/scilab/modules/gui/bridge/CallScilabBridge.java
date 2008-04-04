0e711590e51b1968233ec484b2a10b3cdc6f08f7
Vincent Couvert <vincent.couvert@scilab.org>
2008-04-04 16:27:33 +0000

    gcbo temporary disabled for menus

--- scilab/modules/gui/src/java/org/scilab/modules/gui/bridge/CallScilabBridge.java
+++ scilab/modules/gui/src/java/org/scilab/modules/gui/bridge/CallScilabBridge.java
@@ -766,7 +766,15 @@ public class CallScilabBridge {
 	 * @param callbackType the type of the callback
 	 */
 	public static void setWidgetCallback(int objID, String callbackString, int callbackType) {
-		((Widget) UIElementMapper.getCorrespondingUIElement(objID)).setCallback(CallBack.createCallback(callbackString, callbackType, objID));
+		if (UIElementMapper.getCorrespondingUIElement(objID) instanceof Menu) {
+			((Menu) UIElementMapper.getCorrespondingUIElement(objID)).setCallback(CallBack.createCallback(callbackString, callbackType));
+		} else if (UIElementMapper.getCorrespondingUIElement(objID) instanceof MenuItem) {
+			((MenuItem) UIElementMapper.getCorrespondingUIElement(objID))
+								.setCallback(CallBack.createCallback(callbackString, callbackType));
+		} else {
+			((Widget) UIElementMapper.getCorrespondingUIElement(objID))
+								.setCallback(CallBack.createCallback(callbackString, callbackType, objID));
+		}
 	}
 	
 	/**
