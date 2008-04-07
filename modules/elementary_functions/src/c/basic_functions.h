2d7683973c1986075a643ce0b3fcd1c7bf5da139
Antoine Elias <antoine.elias@scilab.org>
2008-04-07 14:38:42 +0000

    add functions :  - vCupro  - vCuproi  - vDvmul  - vWvmul  - vDadd

--- modules/elementary_functions/src/c/basic_functions.h
+++ modules/elementary_functions/src/c/basic_functions.h
@@ -9,7 +9,6 @@
  * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
  *
  */
-
 #include "stack-c.h"
 #include <stdlib.h>
 #include <math.h>
@@ -17,6 +16,19 @@
 extern double C2F(dlamch)  __PARAMS((char *CMACH, unsigned long int));
 extern double C2F(logp1) _PARAMS((double *x));
 
+#ifndef _CST_BASIC__
+#define _CST_BASIC__
+/*Constants*/
+#define ROW_LETTER		'r'
+#define COL_LETTER		'c'
+#define STAR_LETTER		'*'
+#define MTLB_LETTER		'm'
+
+#define BY_ROWS			1
+#define BY_COLS			2
+#define BY_ALL			0
+#define	BY_MTLB			-1
+#endif //_CST_BASIC__
 
 double lnp1m1(double _dblVar);
 double dabss(double _dblVal);
@@ -41,3 +53,10 @@ int dceils(double _dblVal);
 double wasums(int _iNbElem, double* _pdblReal, double* _pdblImg);
 double dasums(int _iNbElem, double* _pdblReal);
 void ddscals(double* _pdblIn, int _iNbElem, double _dblMulti, double* _pdblOut);
+void vCupro(int _iNbElem, double* _piIn, double* _piOut);
+void vCuproi(int _iNbElem, double* _piRealIn, double* _piImgIn, double* _piRealOut, double* _piImgOut);
+void vDvmul(int _iNbElem, double* _piIn1, double* _piIn2, int _iIncIn1, int _iIncIn2, double* _piOut);
+void vWvmul(int _iNbElem, double* _piRealIn1, double* _piImgIn1, double* _piRealIn2, double* _piImgIn2, int _iIncIn1, int _iIncIn2, double* _piRealOut, double* _piImgOut);
+void vCusum(int _iNbElem, double *_dblIn, double *_dblOut);
+void vDadd(int _iNbElem, double* _piIn1, double* _piIn2, int _iIncIn1, int _iIncIn2, double* _piOut);
+
