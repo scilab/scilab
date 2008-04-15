eb43b6367065938f0559389066d661f3f5c12656
Allan Cornet <allan.cornet@scilab.org>
2008-04-15 09:46:02 +0000

    remove (not in 5.0)

--- scilab/modules/windows_tools/sci_gateway/c/gw_windows_tools.c
+++ scilab/modules/windows_tools/sci_gateway/c/gw_windows_tools.c
@@ -31,7 +31,6 @@ static gw_generic_table Tab[]=
 {sci_win64,"win64"},
 {sci_istssession,"istssession"},
 {sci_getsystemmetrics,"getsystemmetrics"},
-{sci_winhttpdownload,"winhttpdownload"},
 {sci_createGUID,"createGUID"}
 };
 /*--------------------------------------------------------------------------*/
