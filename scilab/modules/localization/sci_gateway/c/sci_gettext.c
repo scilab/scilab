189593582b3134c7f95cdd79df8107d051a79e56
Vincent Couvert <vincent.couvert@scilab.org>
2008-04-24 07:39:18 +0000

    Removed slashes have to be added after translation

--- scilab/modules/localization/sci_gateway/c/sci_gettext.c
+++ scilab/modules/localization/sci_gateway/c/sci_gettext.c
@@ -9,13 +9,14 @@
  * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
  *
  */
-
+#include <string.h>
 #include "MALLOC.h"
 #include "machine.h"
 #include "stack-c.h"
 #include "localization.h"
 #include "gw_localization.h"
 #include "Scierror.h"
+#include "strsubst.h"
 /*--------------------------------------------------------------------------*/
 int C2F(sci_gettext)(char *fname,unsigned long fname_len)
 {
@@ -28,13 +29,46 @@ int C2F(sci_gettext)(char *fname,unsigned long fname_len)
 
 			char *msgid=NULL;
 			char *TranslatedString=NULL;
-
+                        
+                        int revertStrsub = FALSE;
+                        
 			GetRhsVar(1,STRING_DATATYPE,&m1,&n1,&l1);
 			msgid=cstk(l1);
 
-			/* We always have something from this functions because gettext
-			 * is returning the same string if it cannot find it */
+			/* This stupid stuff is necessary because scilab is add slashes
+			 * and we need to remove them
+			 * An other solution might be to replace the string "\x" by it
+			 * real code 
+			*/
+			if (strchr(msgid, '\\')!=NULL){
+				/* There is an \ in the string process to replace */
+				
+				/* We always have something from this functions because gettext
+				 * is returning the same string if it cannot find it */
+
+				msgid=strsub(msgid, "\\n", "\n"); /* linefeed */
+				msgid=strsub(msgid, "\\t", "\t"); /* horizontal tab */
+				msgid=strsub(msgid, "\\r", "\r"); /* carriage return */
+				msgid=strsub(msgid, "\\v", "\v"); /* vertical tab */
+				msgid=strsub(msgid, "\\f", "\f"); /* form feed */
+				msgid=strsub(msgid, "\\\\", "\\"); /* backslash */
+				msgid=strsub(msgid, "\\\"", "\""); /* double quote */
+
+                                revertStrsub = TRUE;
+			}
 			TranslatedString=gettext(msgid);
+
+                        /* Add removed slashes */
+                        if (revertStrsub)
+                          {
+                            TranslatedString=strsub(TranslatedString, "\\", "\\\\"); /* backslash */
+                            TranslatedString=strsub(TranslatedString, "\"", "\\\""); /* double quote */
+                            TranslatedString=strsub(TranslatedString, "\n", "\\n"); /* linefeed */
+                            TranslatedString=strsub(TranslatedString, "\t", "\\t"); /* horizontal tab */
+                            TranslatedString=strsub(TranslatedString, "\r", "\\r"); /* carriage return */
+                            TranslatedString=strsub(TranslatedString, "\v", "\\v"); /* vertical tab */
+                            TranslatedString=strsub(TranslatedString, "\f", "\\f"); /* form feed */
+                          }
 
 			n1=1;
 			CreateVarFromPtr(Rhs+1,STRING_DATATYPE,(m1=(int)strlen(TranslatedString), &m1),&n1,&TranslatedString);
@@ -44,7 +78,7 @@ int C2F(sci_gettext)(char *fname,unsigned long fname_len)
 		}
 	else
 		{
-			Scierror(999,"%s: Wrong type for first input argument: String expected.\n",fname);
+			Scierror(999,_("%s: Wrong type for first input argument: String expected.\n"),fname);
 			return 0;
 		}
 }
