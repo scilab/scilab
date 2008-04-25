9744cef972bacb8e34826bddf75df34b40178f24
Allan Cornet <allan.cornet@scilab.org>
2008-04-25 06:50:43 +0000

    TCL_LIBRARY & TK_LIBRARY were not defined with TCL/TK 8.5 on Windows

--- scilab/modules/tclsci/src/c/EnvTclTk.c
+++ scilab/modules/tclsci/src/c/EnvTclTk.c
@@ -1,14 +1,14 @@
 /*
- * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
- * Copyright (C) 2006 - INRIA - Allan CORNET
- * 
- * This file must be used under the terms of the CeCILL.
- * This source file is licensed as described in the file COPYING, which
- * you should have received as part of this distribution.  The terms
- * are also available at    
- * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
- *
- */
+* Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+* Copyright (C) 2006 - INRIA - Allan CORNET
+* 
+* This file must be used under the terms of the CeCILL.
+* This source file is licensed as described in the file COPYING, which
+* you should have received as part of this distribution.  The terms
+* are also available at    
+* http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+*
+*/
 
 #include <windows.h>
 #include <stdio.h>
@@ -17,106 +17,132 @@
 
 #include "machine.h"
 #include "MALLOC.h"
+#include "GlobalTclInterp.h"
 #include "../../localization/includes/localization.h"
 #include "../../fileio/includes/ConvertPathUnixToWindows.h"
 /*--------------------------------------------------------------------------*/
 BOOL Set_TCL_LIBRARY_PATH(char *DefaultPath)
 {
-	BOOL bOK=FALSE;
-	
-	int major=8;
-	int minor=4; /* Par defaut */
-	int patchLevel=0;
-	int type=0;
+#define TCL_LIBRARY "TCL_LIBRARY"
+#define TCL_ENV "env"
+#define TCL_LIBRARY_FORMAT "%s\\modules\\tclsci\\tcl\\tcl%d.%d"
 
-	char env[PATH_MAX];
-	
+	BOOL bOK=FALSE;
+	int major = 8;
+	int minor = 4; /* default */
+	int patchLevel = 0;
+	int type = 0;
+
+	char TCL_LIBRARY_PATH[PATH_MAX];
 	char ShortPath[PATH_MAX];
 	char *CopyOfDefaultPath=NULL;
 
-	CopyOfDefaultPath=MALLOC(((int)strlen(DefaultPath)+1)*sizeof(char));
+	CopyOfDefaultPath = MALLOC(((int)strlen(DefaultPath)+1)*sizeof(char));
+	if (CopyOfDefaultPath == NULL)
+	{
+		return FALSE;
+	}
 
-	
 	Tcl_GetVersion(&major, &minor, &patchLevel, &type);
 
 	/* to be sure that it's windows format */
 	/* c:\progra~1\scilab-5.0\modules\tclsci\tcl\tcl8.4 */
+
 	if (GetShortPathName(DefaultPath,ShortPath,PATH_MAX)==0)
 	{
 		fprintf(stderr,"\n%s\n%s%s.\n",_("Incorrect TCL_LIBRARY environment variable"),_("TCL_LIBRARY has been redefined to "),DefaultPath);
-		if (CopyOfDefaultPath) {FREE(CopyOfDefaultPath);CopyOfDefaultPath=NULL;}
-
 		ConvertPathUnixToWindowsFormat(ShortPath,CopyOfDefaultPath);
-		wsprintf (env, "TCL_LIBRARY=%s\\modules\\tclsci\\tcl\\tcl%d.%d",CopyOfDefaultPath,major,minor);
-
-		if (CopyOfDefaultPath) {FREE(CopyOfDefaultPath);CopyOfDefaultPath=NULL;}
+		wsprintf (TCL_LIBRARY_PATH, TCL_LIBRARY_FORMAT,CopyOfDefaultPath,major,minor);
 	}
 	else
 	{
 		ConvertPathUnixToWindowsFormat(ShortPath,CopyOfDefaultPath);
-		wsprintf (env, "TCL_LIBRARY=%s\\modules\\tclsci\\tcl\\tcl%d.%d",CopyOfDefaultPath,major,minor);
-		
-		if (CopyOfDefaultPath) {FREE(CopyOfDefaultPath);CopyOfDefaultPath=NULL;}
+		wsprintf (TCL_LIBRARY_PATH, TCL_LIBRARY_FORMAT,CopyOfDefaultPath,major,minor);
 	}
 
-	if (_putenv (env))
+	if (CopyOfDefaultPath) {FREE(CopyOfDefaultPath);CopyOfDefaultPath=NULL;}
+
+	if ( SetEnvironmentVariable(TCL_LIBRARY,TCL_LIBRARY_PATH) )
 	{
-		bOK=FALSE;
+		if (getTclInterp() != NULL)
+		{
+			releaseTclInterp();
+			Tcl_SetVar2(getTclInterp(), TCL_ENV, TCL_LIBRARY, TCL_LIBRARY_PATH, TCL_GLOBAL_ONLY);
+			bOK = TRUE;
+		}
+		else
+		{
+			bOK = FALSE;
+		}
+		releaseTclInterp();
 	}
 	else
 	{
-		bOK=TRUE;
+		bOK = FALSE;
 	}
-
 	return bOK;
 }
 /*--------------------------------------------------------------------------*/
 BOOL Set_TK_LIBRARY_PATH(char *DefaultPath)
 {
-	BOOL bOK=FALSE;
-	char env[PATH_MAX];
+#define TK_LIBRARY "TK_LIBRARY"
+#define TCL_ENV "env"
+#define TK_LIBRARY_FORMAT "%s\\modules\\tclsci\\tcl\\tk%d.%d"
+
+	BOOL bOK = FALSE;
+	char TK_LIBRARY_PATH[PATH_MAX];
 	char ShortPath[PATH_MAX];
 	char *CopyOfDefaultPath=NULL;
 
-	int major=8;
-	int minor=4; /* Par defaut */
-	int patchLevel=0;
-	int type=0;
+	int major = 8;
+	int minor = 4; /* default */
+	int patchLevel = 0;
+	int type = 0;
 
-	CopyOfDefaultPath=MALLOC(((int)strlen(DefaultPath)+1)*sizeof(char));
+	CopyOfDefaultPath = MALLOC(((int)strlen(DefaultPath)+1)*sizeof(char));
+
+	if (CopyOfDefaultPath == NULL)
+	{
+		return FALSE;
+	}
 
 	Tcl_GetVersion(&major, &minor, &patchLevel, &type);
 
 	/* to be sure that it's windows format */
-	/* c:\progra~1\scilab-3.1\modules\tclsci\tcl\tk8.4 */
+	/* c:\progra~1\scilab-5.0\modules\tclsci\tcl\tk8.4 */
+
 	if (GetShortPathName(DefaultPath,ShortPath,PATH_MAX)==0)
 	{
 		fprintf(stderr,"\n%s\n%s%s.\n",_("Incorrect TK_LIBRARY environment variable"),_("TK_LIBRARY has been redefined to "),DefaultPath);
-		if (CopyOfDefaultPath) {FREE(CopyOfDefaultPath);CopyOfDefaultPath=NULL;}
-
 		ConvertPathUnixToWindowsFormat(ShortPath,CopyOfDefaultPath);
-		wsprintf (env, "TK_LIBRARY=%s\\modules\\tclsci\\tcl\\tk%d.%d",CopyOfDefaultPath,major,minor);
-
-		if (CopyOfDefaultPath) {FREE(CopyOfDefaultPath);CopyOfDefaultPath=NULL;}
-		
+		wsprintf (TK_LIBRARY_PATH, TK_LIBRARY_FORMAT,CopyOfDefaultPath,major,minor);
 	}
 	else
 	{
 		ConvertPathUnixToWindowsFormat(ShortPath,CopyOfDefaultPath);
-		wsprintf (env, "TK_LIBRARY=%s\\modules\\tclsci\\tcl\\tk%d.%d",CopyOfDefaultPath,major,minor);
-
-		if (CopyOfDefaultPath) {FREE(CopyOfDefaultPath);CopyOfDefaultPath=NULL;}
+		wsprintf (TK_LIBRARY_PATH, TK_LIBRARY_FORMAT,CopyOfDefaultPath,major,minor);
 	}
 
-	if (_putenv (env))
+	if (CopyOfDefaultPath) {FREE(CopyOfDefaultPath);CopyOfDefaultPath=NULL;}
+
+	if ( SetEnvironmentVariable(TK_LIBRARY,TK_LIBRARY_PATH) )
 	{
-		bOK=FALSE;
+		if (getTclInterp() != NULL)
+		{
+			releaseTclInterp();
+			Tcl_SetVar2(getTclInterp(), TCL_ENV, TK_LIBRARY, TK_LIBRARY_PATH, TCL_GLOBAL_ONLY);
+			bOK = TRUE;
+		}
+		else
+		{
+			bOK = FALSE;
+		}
+		releaseTclInterp();
 	}
 	else
 	{
-		bOK=TRUE;
+		bOK = FALSE;
 	}
-
 	return bOK;
 }
 /*--------------------------------------------------------------------------*/
--- scilab/modules/tclsci/src/c/InitTclTk.c
+++ scilab/modules/tclsci/src/c/InitTclTk.c
@@ -1,15 +1,15 @@
 /*
- * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
- * Copyright (C) 2005-2008 - INRIA - Allan CORNET
- * Copyright (C) 2007-2008 - INRIA - Bruno JOFRET
- *
- * This file must be used under the terms of the CeCILL.
- * This source file is licensed as described in the file COPYING, which
- * you should have received as part of this distribution.  The terms
- * are also available at
- * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
- *
- */
+* Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+* Copyright (C) 2005-2008 - INRIA - Allan CORNET
+* Copyright (C) 2007-2008 - INRIA - Bruno JOFRET
+*
+* This file must be used under the terms of the CeCILL.
+* This source file is licensed as described in the file COPYING, which
+* you should have received as part of this distribution.  The terms
+* are also available at
+* http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+*
+*/
 
 #include <string.h>
 #ifndef _MSC_VER
@@ -33,7 +33,7 @@
 BOOL TK_Started=FALSE;
 
 /* The tclLoop thread Id
-   in order to wait it ends when closing Scilab */
+in order to wait it ends when closing Scilab */
 __threadId TclThread;
 
 __threadSignal InterpReady;
@@ -44,180 +44,183 @@ __threadLock InterpReadyLock;
 static char *GetSciPath(void);
 /*--------------------------------------------------------------------------*/
 static void *DaemonOpenTCLsci(void* in)
-     /* Checks if tcl/tk has already been initialised and if not */
-     /* initialise it. It must find the tcl script */
+/* Checks if tcl/tk has already been initialised and if not */
+/* initialise it. It must find the tcl script */
 {
-  char *SciPath=NULL;
-  char TkScriptpath[PATH_MAX];
-  char MyCommand[2048];
-  BOOL tkStarted=TRUE;
+	char *SciPath=NULL;
+	char TkScriptpath[PATH_MAX];
+	char MyCommand[2048];
+	BOOL tkStarted=TRUE;
 
 #ifndef _MSC_VER
-  DIR *tmpdir=NULL;
+	DIR *tmpdir=NULL;
 #endif
 
-  FILE *tmpfile2=NULL;
+	FILE *tmpfile2=NULL;
 
-  SciPath=GetSciPath();
+	SciPath=GetSciPath();
 
-  /* test SCI validity */
-  if (SciPath==NULL)
-  {
-      sciprint(_("The SCI environment variable is not set.\nTCL initialisation failed !\n"));
-  }
+	/* test SCI validity */
+	if (SciPath==NULL)
+	{
+		sciprint(_("The SCI environment variable is not set.\nTCL initialisation failed !\n"));
+	}
 
-#ifdef _MSC_VER
-    /* Initialize TCL_LIBRARY & TK_LIBRARY variables environment */
-    /* Windows only */
-	Set_TCL_LIBRARY_PATH(SciPath);
-	Set_TK_LIBRARY_PATH(SciPath);
-#endif
+
 
 #ifdef TCL_MAJOR_VERSION
 #ifdef TCL_MINOR_VERSION
 #if TCL_MAJOR_VERSION >= 8
 #if TCL_MINOR_VERSION > 0
-  Tcl_FindExecutable(" ");
+	Tcl_FindExecutable(" ");
 #endif
 #endif
 #endif
 #endif
 
 #ifdef _MSC_VER
-  
-  strcpy(TkScriptpath, SciPath);
-  strcat(TkScriptpath, "/modules/tclsci/tcl/TK_Scilab.tcl");
 
-  tmpfile2 = fopen(TkScriptpath,"r");
-  if (tmpfile2==NULL)
-    {
-      sciprint(_("Unable to find Tcl initialisation scripts.\nCheck your SCI environment variable.\nTcl initialisation failed !"));
-     }
-  else fclose(tmpfile2);
+	strcpy(TkScriptpath, SciPath);
+	strcat(TkScriptpath, "/modules/tclsci/tcl/TK_Scilab.tcl");
+
+	tmpfile2 = fopen(TkScriptpath,"r");
+	if (tmpfile2==NULL)
+	{
+		sciprint(_("Unable to find Tcl initialisation scripts.\nCheck your SCI environment variable.\nTcl initialisation failed !"));
+	}
+	else fclose(tmpfile2);
 #else
-  tmpdir=opendir(SciPath);
-  if (tmpdir==NULL)
-    {
-      sciprint(_("The SCI environment variable is not set.\nTcl initialisation failed !\n"));
-     }
-  else closedir(tmpdir);
-  strcpy(TkScriptpath,SciPath);
-  strcat(TkScriptpath,"/modules/tclsci/tcl/TK_Scilab.tcl");
-  tmpfile2 = fopen(TkScriptpath,"r");
-  if (tmpfile2==NULL)
-    {
-      sciprint(_("Unable to find Tcl initialisation scripts.\nCheck your SCI environment variable.\nTcl initialisation failed !"));
-     }
-  else fclose(tmpfile2);
+	tmpdir=opendir(SciPath);
+	if (tmpdir==NULL)
+	{
+		sciprint(_("The SCI environment variable is not set.\nTcl initialisation failed !\n"));
+	}
+	else closedir(tmpdir);
+	strcpy(TkScriptpath,SciPath);
+	strcat(TkScriptpath,"/modules/tclsci/tcl/TK_Scilab.tcl");
+	tmpfile2 = fopen(TkScriptpath,"r");
+	if (tmpfile2==NULL)
+	{
+		sciprint(_("Unable to find Tcl initialisation scripts.\nCheck your SCI environment variable.\nTcl initialisation failed !"));
+	}
+	else fclose(tmpfile2);
 #endif /* _MSC_VER */
 
-  if (getTclInterp() == NULL)
-    {
-      releaseTclInterp();
-      initTclInterp();
-      if ( getTclInterp() == NULL )
+	if (getTclInterp() == NULL)
 	{
-	  Scierror(999,_("Tcl Error: Unable to create Tcl interpreter (Tcl_CreateInterp).\n"));
-	}
-      releaseTclInterp();
-      if ( Tcl_Init(getTclInterp()) == TCL_ERROR)
-	{
-	  Scierror(999,_("Tcl Error: Error during the Tcl initialization (Tcl_Init): %s\n"),getTclInterp()->result);
-	}
-      releaseTclInterp();
-      if ( Tk_Init(getTclInterp()) == TCL_ERROR)
-	{
-	  Scierror(999,_("Tcl Error: Error during the TK initialization (Tk_Init):%s\n"),getTclInterp()->result);
-	  tkStarted=FALSE;
-	}
-      releaseTclInterp();
-      sprintf(MyCommand, "set SciPath \"%s\";",SciPath);
+		releaseTclInterp();
+		initTclInterp();
 
-      if ( Tcl_Eval(getTclInterp(),MyCommand) == TCL_ERROR  )
-	{
-	  Scierror(999,_("Tcl Error: Error during the Scilab/Tcl init process. Could not set SciPath: %s\n"),getTclInterp()->result);
+#ifdef _MSC_VER
+		/* Initialize TCL_LIBRARY & TK_LIBRARY variables environment */
+		/* Windows only */
+		Set_TCL_LIBRARY_PATH(SciPath);
+		Set_TK_LIBRARY_PATH(SciPath);
+#endif
+
+		if ( getTclInterp() == NULL )
+		{
+			Scierror(999,_("Tcl Error: Unable to create Tcl interpreter (Tcl_CreateInterp).\n"));
+		}
+		releaseTclInterp();
+		if ( Tcl_Init(getTclInterp()) == TCL_ERROR)
+		{
+			Scierror(999,_("Tcl Error: Error during the Tcl initialization (Tcl_Init): %s\n"),getTclInterp()->result);
+		}
+		releaseTclInterp();
+		if ( Tk_Init(getTclInterp()) == TCL_ERROR)
+		{
+			Scierror(999,_("Tcl Error: Error during the TK initialization (Tk_Init):%s\n"),getTclInterp()->result);
+			tkStarted=FALSE;
+		}
+		releaseTclInterp();
+		sprintf(MyCommand, "set SciPath \"%s\";",SciPath);
+
+		if ( Tcl_Eval(getTclInterp(),MyCommand) == TCL_ERROR  )
+		{
+			Scierror(999,_("Tcl Error: Error during the Scilab/Tcl init process. Could not set SciPath: %s\n"),getTclInterp()->result);
+		}
+		releaseTclInterp();
+		Tcl_CreateCommand(getTclInterp(),"ScilabEval",TCL_EvalScilabCmd,(ClientData)1,NULL);
+		releaseTclInterp();
 	}
-      releaseTclInterp();
-      Tcl_CreateCommand(getTclInterp(),"ScilabEval",TCL_EvalScilabCmd,(ClientData)1,NULL);
-      releaseTclInterp();
-    }
 
 	if (TKmainWindow == NULL && tkStarted)
-    {
-			TKmainWindow = Tk_MainWindow(getTclInterp());
-			releaseTclInterp();
-			Tk_GeometryRequest(TKmainWindow,2,2);
-			if ( Tcl_EvalFile(getTclInterp(),TkScriptpath) == TCL_ERROR  )
-			{
-				Scierror(999,_("Tcl Error: Error during the Scilab/TK init process. Error while loading %s: %s\n"),TkScriptpath, getTclInterp()->result);
-			}
-			releaseTclInterp();
+	{
+		TKmainWindow = Tk_MainWindow(getTclInterp());
+		releaseTclInterp();
+		Tk_GeometryRequest(TKmainWindow,2,2);
+		if ( Tcl_EvalFile(getTclInterp(),TkScriptpath) == TCL_ERROR  )
+		{
+			Scierror(999,_("Tcl Error: Error during the Scilab/TK init process. Error while loading %s: %s\n"),TkScriptpath, getTclInterp()->result);
+		}
+		releaseTclInterp();
 	}
 
 
-  if (SciPath) {FREE(SciPath);SciPath=NULL;}
+	if (SciPath) {FREE(SciPath);SciPath=NULL;}
 
-  // Signal TclInterpreter init is now over.
-  __Signal(&InterpReady);
-  __UnLock(&InterpReadyLock);
+	// Signal TclInterpreter init is now over.
+	__Signal(&InterpReady);
+	__UnLock(&InterpReadyLock);
 
-  // This start a periodic and endless call to "update"
-  // TCL command. This causes any TCL application to start
-  // and run as if it's in the main program thread.
-  startTclLoop();
-  return(0);
+	// This start a periodic and endless call to "update"
+	// TCL command. This causes any TCL application to start
+	// and run as if it's in the main program thread.
+	startTclLoop();
+	return(0);
 
 }
 /*--------------------------------------------------------------------------*/
 int OpenTCLsci(void)
 {
-  __InitSignal(&InterpReady);
-  // Open TCL interpreter in a separated thread.
-  // Allows all Tcl application not to freeze nor decrease Scilab speed.
-  // Causes also Scilab let those application live their own lifes.
-  //__InitSignal(&InterpReady);
-  __CreateThread(&TclThread, &DaemonOpenTCLsci);
-  // Wait to be sure initialisation is complete.
-  __Wait(&InterpReady, &InterpReadyLock);
-  return 0;
+	__InitSignal(&InterpReady);
+	// Open TCL interpreter in a separated thread.
+	// Allows all Tcl application not to freeze nor decrease Scilab speed.
+	// Causes also Scilab let those application live their own lifes.
+	//__InitSignal(&InterpReady);
+	__CreateThread(&TclThread, &DaemonOpenTCLsci);
+	// Wait to be sure initialisation is complete.
+	__Wait(&InterpReady, &InterpReadyLock);
+	return 0;
 }
 /*--------------------------------------------------------------------------*/
 BOOL CloseTCLsci(void)
 {
-  if ( getScilabMode() != SCILAB_NWNI )
-  {
-    if (isTkStarted())
+	if ( getScilabMode() != SCILAB_NWNI )
 	{
-	  setTkStarted(FALSE);
-	  __Terminate(TclThread);
-	  __WaitThreadDie(TclThread);
-	  deleteTclInterp();
-	  TKmainWindow=NULL;
-	  return TRUE;
+		if (isTkStarted())
+		{
+			setTkStarted(FALSE);
+			__Terminate(TclThread);
+			__WaitThreadDie(TclThread);
+			deleteTclInterp();
+			TKmainWindow=NULL;
+			return TRUE;
+		}
 	}
-  }
-  return FALSE;
+	return FALSE;
 }
 /*--------------------------------------------------------------------------*/
 static char *GetSciPath(void)
-     /* force SciPath to Unix format for compatibility (Windows) */
+/* force SciPath to Unix format for compatibility (Windows) */
 {
-  char *PathUnix=NULL;
-  char *SciPathTmp=NULL;
-  int i=0;
+	char *PathUnix=NULL;
+	char *SciPathTmp=NULL;
+	int i=0;
 
-  SciPathTmp=getSCIpath();
+	SciPathTmp=getSCIpath();
 
-  if (SciPathTmp)
-  {
-      PathUnix = strdup(SciPathTmp);
-      for (i=0;i<(int)strlen(PathUnix);i++)
-	  {
-	    if (PathUnix[i]=='\\') PathUnix[i]='/';
-	  }
-  }
-  if (SciPathTmp) {FREE(SciPathTmp);SciPathTmp=NULL;}
-  return PathUnix;
+	if (SciPathTmp)
+	{
+		PathUnix = strdup(SciPathTmp);
+		for (i=0;i<(int)strlen(PathUnix);i++)
+		{
+			if (PathUnix[i]=='\\') PathUnix[i]='/';
+		}
+	}
+	if (SciPathTmp) {FREE(SciPathTmp);SciPathTmp=NULL;}
+	return PathUnix;
 }
 /*--------------------------------------------------------------------------*/
 BOOL isTkStarted(void)
