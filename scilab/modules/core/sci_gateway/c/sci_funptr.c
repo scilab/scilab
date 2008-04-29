d8f4e17e6dfe8a3c139268cf50d3556b0c1786c3
Allan Cornet <allan.cornet@scilab.org>
2008-04-29 17:53:31 +0000

    rewrite funptr bug 2791

--- scilab/modules/core/sci_gateway/c/sci_funptr.c
+++ scilab/modules/core/sci_gateway/c/sci_funptr.c
@@ -12,12 +12,43 @@
 #include "gw_core.h"
 #include "machine.h"
 #include "stack-c.h"
-/*--------------------------------------------------------------------------*/
-extern int C2F(intfunptr)(void);
+#include "localization.h"
+#include "hashtable_core.h"
+#include "Scierror.h"
+#include "Funtab.h"
+#include "stack-def.h"
 /*--------------------------------------------------------------------------*/
 int C2F(sci_funptr) (char *fname,unsigned long fname_len)
 {
- 	C2F(intfunptr)();
+	CheckRhs(1,1);
+	CheckLhs(1,1);
+
+	if ( GetType(1) == sci_strings )
+	{
+		char *VarName = NULL;
+		int m1 = 0, n1 = 0, l1 = 0;
+		int id[nsiz];
+		int funptr = 0;
+		int zero = 0;
+		int one = 1;
+		int job = SCI_HFUNCTIONS_FIND; /* Find function & returns fptr value */
+
+		GetRhsVar(1,STRING_DATATYPE,&m1,&n1,&l1);
+		VarName=cstk(l1);
+
+		C2F(cvname)(id,VarName,&zero,(unsigned long)strlen(VarName));
+		C2F(funtab)(id,&funptr,&job,"NULL_NAME",0);
+
+		CreateVar(Rhs+1,MATRIX_OF_INTEGER_DATATYPE,&one,&one,&l1);
+		*istk(l1) = funptr;
+
+		LhsVar(1)=Rhs+1;
+		C2F(putlhsvar)();
+	}
+	else
+	{
+		Scierror(999,_("%s: Wrong type for first input argument: String expected.\n"),fname);
+	}
 	return 0;
 }
 /*--------------------------------------------------------------------------*/
