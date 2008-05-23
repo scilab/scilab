8ce197413a194eff8de63b3459100811db057e8f
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2008-05-23 08:20:11 +0000

    Current syntax 'd' => MATRIX_OF_DOUBLE_DATATYPE

--- scilab/modules/dynamic_link/tests/unit_tests/addinter.c
+++ scilab/modules/dynamic_link/tests/unit_tests/addinter.c
@@ -18,7 +18,7 @@ int intfun1(char *fname)
   int m1,n1,l1;
   CheckRhs(1,1);
   CheckLhs(1,1);
-  GetRhsVar(1, "d", &m1, &n1, &l1);
+  GetRhsVar(1, MATRIX_OF_DOUBLE_DATATYPE, &m1, &n1, &l1);
   fun1(stk(l1),stk(l1));
   LhsVar(1) = 1;
   return 0;
