cb1c6f20d9669f53b9ecb908bc76a4ba958cfa9f
Simone Mannori <simone.mannori@scilab.org>
2008-06-06 10:30:15 +0000

    single file/ single function Scicos standard for Scilab 5, by LV&SM

---
+++ scilab/modules/scicos_blocks/src/c/cosblk.c
@@ -0,0 +1,47 @@
+/*  Scicos
+*
+*  Copyright (C) INRIA - METALAU Project <scicos@inria.fr>
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*
+* See the file ./license.txt
+*/
+#include <stdio.h>
+#include <math.h>
+
+#include "machine.h"
+#include "core_math.h"
+
+#include "scicos.h"
+
+/*------------------------------------------------
+ *     Scicos block simulator 
+ *     A set of elementary blocks 
+ *------------------------------------------------*/
+
+/*------------------------------------------------
+ *     Scicos block simulator 
+ *     
+ *------------------------------------------------*/
+
+void cosblk (flag, nevprt, t, xd, x, nx, z, nz, tvec, 
+	     ntvec, rpar, nrpar, ipar, nipar, u, nu, y, ny)
+             integer *flag, *nevprt,*nx,*nz,*nrpar, *ipar, *nipar,*ntvec,*nu,*ny;
+             double *t, *xd, *x, *z, *tvec, *rpar, *u, *y;
+{
+
+  int i ;
+  for ( i=0; i < *nu ; i++)     y[i]= cos(u[i]);
+}
