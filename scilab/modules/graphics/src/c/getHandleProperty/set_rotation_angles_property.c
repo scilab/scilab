58e6586b01c71c8f29ed2f133e83f57702f7a3ac
Jean-baptiste Silvy <jean-baptiste.silvy@scilab.org>
2008-06-10 12:57:34 +0000

    Fix bug 3133.

--- scilab/modules/graphics/src/c/getHandleProperty/set_rotation_angles_property.c
+++ scilab/modules/graphics/src/c/getHandleProperty/set_rotation_angles_property.c
@@ -46,7 +46,6 @@ int set_rotation_angles_property( sciPointObj * pobj, int stackPointer, int valu
   }
 
   Obj_RedrawNewAngle( pobj, values[1], values[0] ) ;
-  setInfoMessageWithRotationAngles(sciGetParentFigure(pobj), values[0], values[1]);
 
   return SET_PROPERTY_SUCCEED ;
 
