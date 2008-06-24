053ba673ce40f32303b31f4e31d130254f6658a9
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2008-06-24 15:32:56 +0000

    Better error message

--- scilab/modules/gui/sci_gateway/c/gw_gui.c
+++ scilab/modules/gui/sci_gateway/c/gw_gui.c
@@ -58,7 +58,7 @@ int gw_gui(void)
 
 	if ( getScilabMode() == SCILAB_NWNI)
 	{
-		Scierror(999,_("Scilab '%s' module disabled -nogui or -nwni mode."), "GUI");
+		Scierror(999,_("Scilab '%s' module disabled in -nogui or -nwni mode."), "GUI");
 		return 0;
 	}
 
