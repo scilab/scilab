8936ab59b5cdf24b476608dcb7a0ae468d419e63
Allan Cornet <allan.cornet@scilab.org>
2008-07-02 06:23:37 +0000

    warning + include + indent

--- scilab/modules/graphics/src/c/scitokenize.c
+++ scilab/modules/graphics/src/c/scitokenize.c
@@ -10,48 +10,59 @@
  * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
  *
  */
+
+/*--------------------------------------------------------------------------*/ 
 #include <string.h> 
+#include "scitokenize.h"
 #include "MALLOC.h" /* MALLOC */
+#include "freeArrayOfString.h"
+/*--------------------------------------------------------------------------*/ 
 int scitokenize(char * legend, char *** Strptr, int *n)
 {
-  int arobaseIndex = 0;
-  int i;
-  int count = 1;
-  int prevIndex = 0;
-  int stringLength, j;
-  char **Str;
+	int arobaseIndex = 0;
+	int i = 0;
+	int count = 1;
+	int prevIndex = 0;
+	int stringLength = 0, j = 0;
+	char **Str = NULL;
 
-  /* first get the number of tokens */
-  for (i=0; i<strlen(legend);i++) {
-    if ( legend[i] == '@') count++;  }
-  *Strptr=(char **)MALLOC(count*sizeof(char *));
-  Str = *Strptr;
-  if (Str==NULL)  return 1; /* not enough memory */
-  for (i = 0; i < count; i++) Str[i]=NULL;
+	/* first get the number of tokens */
+	for (i=0; i<(int)strlen(legend);i++) 
+	{
+		if ( legend[i] == '@') count++;  
+	}
+	*Strptr = (char **)MALLOC(count*sizeof(char *));
+	Str = *Strptr;
+	if (Str == NULL)  return 1; /* not enough memory */
+	for (i = 0; i < count; i++) Str[i]=NULL;
 
+	for (i = 0; i < count; i++) 
+	{
+		while( legend[arobaseIndex] != '@' && legend[arobaseIndex] != 0)
+		{
+			arobaseIndex++;
+		}
+		/* we found a token */
+		stringLength =  arobaseIndex - prevIndex;
+		Str[i]=(char *)MALLOC((stringLength+1)*sizeof(char));/*  +1 for null terminating character*/
 
-  for (i = 0; i < count; i++) {
-    while( legend[arobaseIndex] != '@' && legend[arobaseIndex] != 0)
-    {
-      arobaseIndex++;
-    }
-    /* we found a token */
-     stringLength =  arobaseIndex - prevIndex;
-     Str[i]=(char *)MALLOC((stringLength+1)*sizeof(char));/*  +1 for null terminating character*/
-     if (Str[i]==NULL) {
-	  for (j = 0; j < i-1; j++) FREE(Str[j]);
-	  FREE(Str);
-	  return 1;
-     }
-     for (j = 0; j < stringLength; j++) {
-       Str[i][j] = legend[prevIndex + j];
-     }
-     Str[i][stringLength] = 0;
+		if (Str[i]==NULL) 
+		{
+			freeArrayOfString(Str,i-1);
+			return 1;
+		}
 
-     /* skip the @*/
-     arobaseIndex++;
-     prevIndex = arobaseIndex;
-   }
-  *n=count;
-  return 0;
+		for (j = 0; j < stringLength; j++) 
+		{
+			Str[i][j] = legend[prevIndex + j];
+		}
+		Str[i][stringLength] = 0;
+
+		/* skip the @*/
+		arobaseIndex++;
+		prevIndex = arobaseIndex;
+	}
+	*n=count;
+	return 0;
  }
+/*--------------------------------------------------------------------------*/ 
