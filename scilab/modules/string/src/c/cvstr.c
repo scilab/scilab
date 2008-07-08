083d9d32d0e9b535865d6e4101fb552bc21ffc9c
Allan Cornet <allan.cornet@scilab.org>
2008-07-08 14:31:55 +0000

    loop

--- scilab/modules/string/src/c/cvstr.c
+++ scilab/modules/string/src/c/cvstr.c
@@ -38,32 +38,35 @@ int C2F(cvstr1)(integer *n,integer * line,char * str,integer * job,	unsigned lon
 int C2F(codetoascii)(integer *n,integer * line,char * str, unsigned long str_len)
 {
   int j = 0;
-
-  /* conversion code ->ascii */
-  for (j = 0; j < *n; ++j)
+  int nn = *n;
+  /* conversion code -> ascii */
+  while (nn--)
   {
-	str[j] = convertScilabCodeToAsciiCode(line[j]);
+	  str[j] = convertScilabCodeToAsciiCode(line[j]);
+	  j++;
   }
   return 0;
 }
 /*--------------------------------------------------------------------------*/
 int C2F(asciitocode)(integer * n,integer * line,char * str,integer * flagx,unsigned long  str_len)
 {
-	integer j = 0;
+	int nn = *n;
 	if (*flagx == 1) 
 	{
-		for (j = 0; j < *n ; ++j) 
+		integer j = 0;
+		while (nn--)
 		{
 			unsigned char current_char = str[j];
 			line[j] = convertAsciiCodeToScilabCode(current_char);
+			j++;
 		}
 	} 
 	else 
 	{
-		for (j = *n -1 ; j >= 0; --j) 
+		while (nn--)
 		{
-			unsigned char current_char = str[j];
-			line[j] = convertAsciiCodeToScilabCode(current_char);
+			unsigned char current_char = str[nn];
+			line[nn] = convertAsciiCodeToScilabCode(current_char);
 		}
 	}
 	return 0;
