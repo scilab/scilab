2cee18bca59b39ef1d4ea5a1ca9d257ae47f8093
Jean-baptiste Silvy <jean-baptiste.silvy@scilab.org>
2008-07-10 11:23:24 +0000

    Remove deadlock in scicos.

--- scilab/modules/graphics/src/c/getHandleProperty/set_viewport_property.c
+++ scilab/modules/graphics/src/c/getHandleProperty/set_viewport_property.c
@@ -28,6 +28,7 @@
 int set_viewport_property( sciPointObj * pobj, int stackPointer, int valueType, int nbRow, int nbCol )
 {
   int values[4];
+  int status;
 
   if ( !isParameterDoubleMatrix( valueType ) )
   {
@@ -55,6 +56,12 @@ int set_viewport_property( sciPointObj * pobj, int stackPointer, int valueType, 
   values[3] = 0;
 
   /* force auto_resize. With auto_resize disable, resize does not work */
-  return sciSetViewport(pobj, values);
+
+  /* disable protection since this function will call Java */
+  endFigureDataWriting(pobj);
+  status = sciSetViewport(pobj, values);
+  startFigureDataWriting(pobj);
+
+  return status;
 }
 /*------------------------------------------------------------------------*/
