155e49941235611d8a7669f5c793ccf6a75e139b
Bruno Jofret <bruno.jofret@scilab.org>
2008-07-16 12:46:42 +0000

    Backgroundcolor is dedicated to uicontrol.
    TODO : try to gather behaviour background vs backgroundcolor

--- scilab/modules/gui/src/cpp/SetUicontrolBackgroundColor.cpp
+++ scilab/modules/gui/src/cpp/SetUicontrolBackgroundColor.cpp
@@ -1,12 +1,13 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2007 - INRIA - Vincent COUVERT
+ * Copyright (C) 2008 - INRIA - Bruno JOFRET
  * Sets the background of an uicontrol object
- * 
+ *
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
  * you should have received as part of this distribution.  The terms
- * are also available at    
+ * are also available at
  * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
  *
  */
@@ -22,9 +23,18 @@ int SetUicontrolBackgroundColor(sciPointObj* sciObj, int stackPointer, int value
   int redInt = 0, greenInt = 0, blueInt = 0, nbvalues = 0;
 
   double * allcolors = NULL;
-  
+
   double redDouble = 0.0, greenDouble = 0.0, blueDouble = 0.0;
-  
+
+  /*
+  **  No backgroundcolor property except on UICONTROL
+  */
+  if(sciGetEntityType(sciObj) != SCI_UICONTROL)
+    {
+      sciprint(_("No '%s' property for this object.\n"), "BackgroundColor");
+      return SET_PROPERTY_ERROR;
+    }
+
   if (valueType == sci_strings)
     {
       if(nbCol != 1)
@@ -33,7 +43,7 @@ int SetUicontrolBackgroundColor(sciPointObj* sciObj, int stackPointer, int value
           sciprint(_("Wrong size for '%s' property: 1 x 3 real vector or a string expected.\n"), "BackgroundColor");
           return SET_PROPERTY_ERROR;
         }
-      
+
       nbvalues = sscanf(getStringFromStack(stackPointer), "%lf|%lf|%lf", &redDouble, &greenDouble, &blueDouble);
 
       if (nbvalues != 3)
@@ -46,7 +56,7 @@ int SetUicontrolBackgroundColor(sciPointObj* sciObj, int stackPointer, int value
       redInt = (int) (redDouble * 255);
       greenInt = (int) (greenDouble * 255);
       blueInt = (int) (blueDouble * 255);
-      
+
       /* Store the values in Scilab */
       if (pUICONTROL_FEATURE(sciObj)->backgroundcolor == NULL)
         {
@@ -69,7 +79,7 @@ int SetUicontrolBackgroundColor(sciPointObj* sciObj, int stackPointer, int value
        redInt = (int) (allcolors[0] * 255);
        greenInt = (int) (allcolors[1] * 255);
        blueInt = (int) (allcolors[2] * 255);
-     
+
        /* Store the values in Scilab */
        if (pUICONTROL_FEATURE(sciObj)->backgroundcolor == NULL)
          {
@@ -88,14 +98,14 @@ int SetUicontrolBackgroundColor(sciPointObj* sciObj, int stackPointer, int value
 
   if (pUICONTROL_FEATURE(sciObj)->style == SCI_UIFRAME) /* Frame style uicontrol */
     {
-      CallScilabBridge::setFrameBackgroundColor(getScilabJavaVM(), 
-                                                 pUICONTROL_FEATURE(sciObj)->hashMapIndex, 
+      CallScilabBridge::setFrameBackgroundColor(getScilabJavaVM(),
+                                                 pUICONTROL_FEATURE(sciObj)->hashMapIndex,
                                                  redInt, greenInt, blueInt);
     }
   else /* All other uicontrol styles */
     {
-      CallScilabBridge::setWidgetBackgroundColor(getScilabJavaVM(), 
-                                                 pUICONTROL_FEATURE(sciObj)->hashMapIndex, 
+      CallScilabBridge::setWidgetBackgroundColor(getScilabJavaVM(),
+                                                 pUICONTROL_FEATURE(sciObj)->hashMapIndex,
                                                  redInt, greenInt, blueInt);
     }
   return SET_PROPERTY_SUCCEED;
