3bf590a9a1970535368a0fce6d32291c0f053d02
Bruno Jofret <bruno.jofret@scilab.org>
2008-07-17 13:24:22 +0000

    One step further to accelerted GLHybrid.

--- scilab/modules/gui/src/java/org/scilab/modules/gui/bridge/canvas/GLHybrid.java
+++ scilab/modules/gui/src/java/org/scilab/modules/gui/bridge/canvas/GLHybrid.java
@@ -18,6 +18,8 @@ import java.awt.Graphics;
 import java.awt.GraphicsDevice;
 import java.awt.Panel;
 import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.util.Timer;
 
 import javax.media.opengl.GL;
 import javax.media.opengl.GLAutoDrawable;
@@ -27,149 +29,252 @@ import javax.media.opengl.GLCapabilitiesChooser;
 import javax.media.opengl.GLContext;
 import javax.media.opengl.GLEventListener;
 import javax.media.opengl.GLException;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import com.sun.opengl.util.Animator;
 import com.sun.opengl.util.Screenshot;
 
 
-public class GLHybrid extends JPanel implements GLAutoDrawable {
+public class GLHybrid extends JLabel implements GLAutoDrawable {
 
 
-	private Frame frame;
-	private Panel panel;
+    private final static int WIDTH_OFFSET = 20;
+    private final static int HEIGHT_OFFSET = 40;
 
-	private GLCanvas canvas;
+    private final static boolean VERBOSE = false;
+    private final static boolean TIMED = false;
+    
+    private boolean needReDump = true;
 
-	private static int WIDTH_OFFSET = 20;
-	private static int HEIGHT_OFFSET = 40;
+    private Frame frame;
+    private Panel panel;
 
-	public GLHybrid() {
-		this(null);
+    private GLCanvas canvas;
+    private BufferedImage dump;
+    private long tic = 0;
+    private long tac = 0;
+    private long lastGetGLTime = System.currentTimeMillis();
+
+    private void call(String msg) {
+	if(VERBOSE) {
+	    System.err.println("[GLHybrid] call --> "+msg);
 	}
-
-	public GLHybrid(GLCapabilities capabilities) {
-		this(capabilities, null, null, null);
+    }
+    private void time(String msg, long tic, long tac) {
+	if(TIMED) {
+	    System.err.println("[GLHybrid] "+msg+" --> "+(tac - tic)+"ms");
 	}
+    }
+    
 
-	public GLHybrid(GLCapabilities capabilities,
-			GLCapabilitiesChooser chooser,
-			GLContext shareWith,
-			GraphicsDevice device) {
-		super();
+    public GLHybrid() {
+	this(null);
+	call("GLHybrid()");
+    }
 
-		canvas = new GLCanvas(capabilities, chooser, shareWith, device);
+    public GLHybrid(GLCapabilities capabilities) {
+	this(capabilities, null, null, null);
+	call("GLHybrid(GLCapabilities capabilities)");
+    }
 
+    public GLHybrid(GLCapabilities capabilities,
+	    GLCapabilitiesChooser chooser,
+	    GLContext shareWith,
+	    GraphicsDevice device) {
+	super();
+	call("GLHybrid(GLCapabilities capabilities, GLCapabilitiesChooser chooser, GLContext shareWith, GraphicsDevice device)");
 
-		frame = new Frame();
-		panel = new Panel();
-		panel.add(canvas);
-		frame.add(panel);
-		frame.setVisible(false);
+	canvas = new GLCanvas(capabilities, chooser, shareWith, device);
 
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		System.err.println("Width = "+screenSize.width);
-		System.err.println("Height = "+screenSize.height);
-		canvas.setSize(screenSize.width, screenSize.height);
-		canvas.setLocation(0, 0);
-		panel.setSize(screenSize.width, screenSize.height);
-		panel.setLocation(0, 0);
-		frame.setSize(screenSize.width + WIDTH_OFFSET, screenSize.height + HEIGHT_OFFSET);
-		frame.setLocation(0, -HEIGHT_OFFSET / 2);
-		frame.pack();
+	frame = new Frame();
+	panel = new Panel();
+	panel.add(canvas);
+	frame.add(panel);
+	frame.setVisible(false);
+
+	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	canvas.setSize(screenSize.width, screenSize.height);
+	canvas.setLocation(0, 0);
+	panel.setSize(screenSize.width, screenSize.height);
+	panel.setLocation(0, 0);
+	frame.setSize(screenSize.width + WIDTH_OFFSET, screenSize.height + HEIGHT_OFFSET);
+	frame.setLocation(0, - HEIGHT_OFFSET / 2);
+	frame.pack();
+
+	//Animator animator = new Animator(canvas);
+	//animator.setRunAsFastAsPossible(true);
+	//animator.start();
+    }
+
+    public void paintComponent(final Graphics g) {
+	//super.paintComponents(g);
+	call("paintComponent(final Graphics g)");
+	//if (needReDump) {
+	//    dumpCanvas();
+	//}
+	//drawDumpedImage(g);
+    }
+
+    public void paint(Graphics g) {
+	//super.paint(g);
+	call("paint(Graphics g)");
+	if (needReDump) {
+	    dumpCanvas();
 	}
+	drawDumpedImage(g);
+    }
 
-	public void paintComponent(final Graphics g) {
-		super.paintComponent(g);
-		canvas.display();
+    private void dumpCanvas()
+    {
+	call("dumpCanvas()");
+	/** DISPLAY */
+	tic = System.currentTimeMillis();
+	canvas.display();
+	tac = System.currentTimeMillis();
+	time("canvas.display();", tic, tac);
+	
+	/** MAKE CURRENT */
+	tic = System.currentTimeMillis();
+	canvas.getContext().makeCurrent();
+	tac = System.currentTimeMillis();
+	time("canvas.getContext().makeCurrent();", tic, tac);
+	
+	/** SCREEN SHOT*/
+	tic = System.currentTimeMillis();
+	dump = Screenshot.readToBufferedImage(getWidth(), getHeight());
+	tac = System.currentTimeMillis();
+	time("Screenshot.readToBufferedImage...", tic, tac);
+	
+	/** RELEASE CONTEXT */
+	tic = System.currentTimeMillis();
+	canvas.getContext().release();	
+	tac = System.currentTimeMillis();
+	time("canvas.getContext().release();", tic, tac);
+	
+	synchronized (this) {
+	    needReDump = false;    
 	}
+    }
 
-	public void paint(Graphics g) {
-		super.paint(g);
+    private void drawDumpedImage(Graphics g) {
+	call("drawDumpedImage(Graphics g)");
+	/** CANVAS DISPLAY */
+	tic = System.currentTimeMillis();
+	canvas.display();
+	tac = System.currentTimeMillis();
+	time("canvas.display();", tic, tac);
+	
+	/** DRAWIMAGE */
+	tic = System.currentTimeMillis();
+	g.drawImage(dump, 0, 0, getWidth(), getHeight(), this);
+	tac = System.currentTimeMillis();
+	time("g.drawImage...", tic, tac);
+    }
 
-		canvas.display();
-		canvas.getContext().makeCurrent();
-		g.drawImage(Screenshot.readToBufferedImage(getWidth(), getHeight()), 0, 0, getWidth(), getHeight(), this);
-		canvas.getContext().release();
+    public void repaint() {
+	super.repaint();
+	call("repaint()");
+	if (canvas != null) {
+	    canvas.repaint();
 	}
+    }
 
-	public void repaint() {
-		super.repaint();
-		if (canvas != null) {
-			canvas.repaint();
-		}
+    public void addNotify() {
+	super.addNotify();
+	call("addNotify()");
+	canvas.addNotify();
+    }
+
+    public void addGLEventListener(GLEventListener listener) {
+	call("addGLEventListener(GLEventListener listener)");
+	canvas.addGLEventListener(listener);
+    }
+
+    public boolean getAutoSwapBufferMode() {
+	call("getAutoSwapBufferMode()");
+	return canvas.getAutoSwapBufferMode();
+    }
+
+    public GLContext getContext() {
+	call("getContext()");
+	return canvas.getContext();
+    }
+
+    public GL getGL() {
+	call("getGL()");
+	synchronized (this) {
+	    long currentTime = System.currentTimeMillis();
+	    if(currentTime - lastGetGLTime > 50)
+	    {
+		lastGetGLTime = currentTime;
+		this.needReDump = true;
+	    }
 	}
+	return canvas.getGL();
+    }
 
-	public void addNotify() {
-		super.addNotify();
-		canvas.addNotify();
-	}
+    public void removeGLEventListener(GLEventListener listener) {
+	call("removeGLEventListener(GLEventListener listener)");
+	canvas.removeGLEventListener(listener);
+    }
 
-	public void addGLEventListener(GLEventListener listener) {
-		canvas.addGLEventListener(listener);
-	}
+    public void setAutoSwapBufferMode(boolean arg0) {
+	call("setAutoSwapBufferMode(boolean arg0)");
+	canvas.setAutoSwapBufferMode(arg0);
+    }
 
-	public boolean getAutoSwapBufferMode() {
-		return canvas.getAutoSwapBufferMode();
-	}
+    public void setGL(GL gl) {
+	call("setGL(GL gl)");
+	canvas.setGL(gl);
+    }
 
-	public GLContext getContext() {
-		return canvas.getContext();
-	}
+    public GLContext createContext(GLContext arg0) {
+	call("createContext(GLContext arg0)");
+	return canvas.createContext(arg0);
+    }
 
-	public GL getGL() {
-		return canvas.getGL();
-	}
+    public GLCapabilities getChosenGLCapabilities() {
+	call("getChosenGLCapabilities()");
+	return canvas.getChosenGLCapabilities();
+    }
 
-	public void removeGLEventListener(GLEventListener listener) {
-		canvas.removeGLEventListener(listener);
-	}
+    public void setRealized(boolean arg0) {
+	call("setRealized(boolean arg0)");
+	canvas.setRealized(arg0);
+    }
 
-	public void setAutoSwapBufferMode(boolean arg0) {
-		canvas.setAutoSwapBufferMode(arg0);
-	}
+    public void swapBuffers() throws GLException {
+	call("swapBuffers()");
+	canvas.swapBuffers();
+    }
 
-	public void setGL(GL gl) {
-		canvas.setGL(gl);
-	}
+    public void display() {
+	call("display()");
+	canvas.display();
+    }
 
-	public GLContext createContext(GLContext arg0) {
-		return canvas.createContext(arg0);
-	}
+    public void update(Graphics g) {
+	super.update(g);
+	call("update(Graphics g)");
+	canvas.update(g);
+    }
 
-	public GLCapabilities getChosenGLCapabilities() {
-		return canvas.getChosenGLCapabilities();
-	}
+    /**
+     * Size management
+     * Frame must be bigger than canvas.
+     */
 
-	public void setRealized(boolean arg0) {
-		canvas.setRealized(arg0);
-	}
+    public void setSize(int width, int height) {
+	super.setSize(width, height);
+	call("setSize(int width, int height)");
+	canvas.setSize(width, height);
+    }
 
-	public void swapBuffers() throws GLException {
-		canvas.swapBuffers();
-	}
-
-	public void display() {
-		canvas.display();
-	}
-
-	public void update(Graphics g) {
-		super.update(g);
-		canvas.update(g);
-	}
-
-	/**
-	 * Size management
-	 * Frame must be bigger than canvas.
-	 */
-
-	public void setSize(int width, int height) {
-		super.setSize(width, height);
-		canvas.setSize(width, height);
-	}
-
-	public void setSize(Dimension dim) {
-		super.setSize(dim);
-		canvas.setSize(dim);
-	}
+    public void setSize(Dimension dim) {
+	super.setSize(dim);
+	call("setSize(Dimension dim)");
+	canvas.setSize(dim);
+    }
 
 }
