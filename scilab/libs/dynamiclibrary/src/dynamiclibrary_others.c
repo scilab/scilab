06eda10ca230d12766d7c2476f51aca36f274683
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2008-07-17 12:49:34 +0000

    Header changed + code cleanup

--- scilab/libs/dynamiclibrary/src/dynamiclibrary_others.c
+++ scilab/libs/dynamiclibrary/src/dynamiclibrary_others.c
@@ -1,22 +1,28 @@
-/*-----------------------------------------------------------------------------------*/
-/* INRIA 2007/2008 */
-/* Allan CORNET */
-/* Sylvestre LEDRU */
-/*-----------------------------------------------------------------------------------*/ 
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2007-2008 - INRIA - Allan CORNET
+ * Copyright (C) 2007-2008 - INRIA - Sylvestre LEDRU
+ * 
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at    
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 #include "dynamiclibrary_others.h"
 #include <stdlib.h>
 #include <stdio.h>
-/*-----------------------------------------------------------------------------------*/ 
+/*---------------------------------------------------------------------------*/ 
 #ifndef NULL
 #define NULL 0
 #endif
-/*-----------------------------------------------------------------------------------*/ 
+/*---------------------------------------------------------------------------*/
 BOOL FreeDynLibrary(DynLibHandle hInstance)
 {
-	BOOL bOK = FALSE;
 	if (hInstance)
 		{
-			if (dlclose( hInstance)) bOK = TRUE;
+			if (dlclose( hInstance)) return TRUE;
 		}
 	#ifndef NDEBUG
 	else 
@@ -26,17 +32,15 @@ BOOL FreeDynLibrary(DynLibHandle hInstance)
 		}
 	#endif
 
-	return bOK;
+	return FALSE;
 }
-/*-----------------------------------------------------------------------------------*/ 
+/*---------------------------------------------------------------------------*/
 DynLibFuncPtr GetDynLibFuncPtr(DynLibHandle hInstance,char *funcName)
 {
-	DynLibFuncPtr retFuncPtr = NULL ;
-	
 	if (hInstance)
 	{
-		retFuncPtr = dlsym(hInstance, funcName);
+		return dlsym(hInstance, funcName);
 	}
-	return retFuncPtr;
+	return NULL;
 }
-/*-----------------------------------------------------------------------------------*/ 
+/*---------------------------------------------------------------------------*/
