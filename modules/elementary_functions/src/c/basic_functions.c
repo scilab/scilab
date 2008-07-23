fc9851bdd564a3bebfba2b5a0b52cdd19174ac0b
Antoine Elias <antoine.elias@scilab.org>
2008-07-23 07:42:09 +0000

    add  - danints  - ddmsums  - zdmsums  - franck_matrix  - hilb_matrix  - magic_matrix

--- modules/elementary_functions/src/c/basic_functions.c
+++ modules/elementary_functions/src/c/basic_functions.c
@@ -31,8 +31,7 @@
 *        in "States of the Art in Numerical Analysis"
 *        Oxford, Clarendon Press, 1987
 *        ISBN 0-19-853614-3
-*
-*     AUTHOR
+**     AUTHOR
 *        Bruno Pincon <Bruno.Pincon@iecn.u-nancy.fr>,
 *        Thanks to Lydia van Dijk <lvandijk@hammersmith-consulting.com>
 */
@@ -832,15 +831,196 @@ int nint(double _iVal)
 }
 
 /*aint return double round val */
-/*
 double danints(double _dblVal)
 {
+	/* ATTENTION CECI N'EST PAS ENCORE COMPLETEMENT TESTE */
+	/* 13/06/2008 ELAIS Antoine */
+
 	double dblSign = dsigns(1, _dblVal);
 	if(finite(dabss(_dblVal)) == 0)
 	{//Infinite case !
 		return _dblVal;
 	}
-
-	return (double)(__int64)(_dblVal + (0.5 * dblSign));
+	if(dblSign < 0)
+		return ceil(_dblVal - 0.5);
+	else
+		return floor(_dblVal + 0.5);
+	//return (double)(_dblVal + (0.5 * dblSign));
 }
-*/
+
+
+void ddmsums(int _iMode, double *_pdblIn, int _iRows, int _iCols, double *_pdblOut)
+{
+	int iIndex1		= 0;
+	int iIndex2		= 0;
+
+	switch(_iMode)
+	{
+	case BY_ALL : 
+		_pdblOut[0] = 0;
+		for(iIndex1 = 0 ; iIndex1 < _iRows * _iCols ; iIndex1++)
+			_pdblOut[0] += _pdblIn[iIndex1];
+		break;
+	case BY_ROWS :
+		for(iIndex2 = 0 ; iIndex2 < _iCols ; iIndex2++)
+		{
+			_pdblOut[iIndex2] = 0;
+			for(iIndex1 = 0 ; iIndex1 < _iRows ; iIndex1++)
+				_pdblOut[iIndex2] += _pdblIn[iIndex2 * _iRows + iIndex1];
+		}
+		break;
+	case BY_COLS :
+		for(iIndex2 = 0 ; iIndex2 < _iRows ; iIndex2++)
+		{
+			_pdblOut[iIndex2] = 0;
+			for(iIndex1 = 0 ; iIndex1 < _iCols ; iIndex1++)
+				_pdblOut[iIndex2] += _pdblIn[iIndex1 * _iRows + iIndex2];
+		}
+		break;
+	}
+}
+
+void zdmsums(int _iMode, double *_pdblRealIn, double *_pdblImgIn, int _iRows, int _iCols, double *_pdblRealOut, double *_pdblImgOut)
+{
+	ddmsums(_iMode, _pdblRealIn, _iRows, _iCols, _pdblRealOut);
+	ddmsums(_iMode, _pdblImgIn, _iRows, _iCols, _pdblImgOut);
+}
+
+void franck_matrix(int _iSize, double *_pData)
+{
+	int iIndex1		= 0;
+	int iIndex2		= 0;
+	double dblVal	= _iSize;
+
+	memset(_pData, 0x00, sizeof(double) * _iSize * _iSize);
+	_pData[0]		= dblVal;
+
+	if(_iSize == 1)
+		return;
+
+	for(iIndex1 = 1 ; iIndex1 < _iSize ; iIndex1++)
+	{
+		dblVal = _iSize - iIndex1;
+		_pData[(iIndex1-1) * _iSize + iIndex1] = dblVal;
+		for(iIndex2 = 0 ; iIndex2 <= iIndex1 ; iIndex2++)
+			_pData[iIndex1 * _iSize + iIndex2] = dblVal;
+	}
+}
+
+void hilb_matrix(int _iSize, double *_pData)
+{
+	int iIndex1		= 0;
+	int iIndex2		= 0;
+	double dblVal	= _iSize;
+	double dblTemp	= 0;
+
+	for(iIndex1 = 0 ; iIndex1 < _iSize ; iIndex1++)
+	{
+		if(iIndex1 != 0)
+			dblVal = ((_iSize - iIndex1) * dblVal * (_iSize + iIndex1)) / pow(iIndex1,2);
+		dblTemp = dblVal * dblVal;
+
+		_pData[iIndex1 * _iSize + iIndex1]	= dblTemp / ( 2 * iIndex1 + 1);
+		if(iIndex1 == _iSize - 1)
+			break;
+		for(iIndex2 = iIndex1 + 1 ; iIndex2 < _iSize ; iIndex2++)
+		{
+			dblTemp = -((_iSize - iIndex2) * dblTemp * (_iSize + iIndex2)) / pow(iIndex2,2);
+			_pData[iIndex1 * _iSize + iIndex2] = dblTemp / (iIndex1 + iIndex2 + 1);
+			_pData[iIndex2 * _iSize + iIndex1] = _pData[iIndex1 * _iSize + iIndex2];
+		}
+	}
+}
+
+void magic_matrix(int _iSize, double *_pData)
+{
+	int iNewSize	= 0;
+	int iIndex1		= 0;
+	int iIndex2		= 0;
+	int iUn			= 1;
+	int iTemp1		= 0;
+	int iTemp2		= 0;
+
+	if(_iSize % 4 != 0)
+	{
+		int iRow	= 0;
+		int iCol	= 0;
+		if(_iSize % 2 == 0)
+			iNewSize = _iSize / 2;
+		if(_iSize % 2 != 0)
+			iNewSize = _iSize;
+
+		//odd order or upper corner of even order
+
+		iRow		= 0;
+		iCol		= iNewSize / 2;
+		memset(_pData, 0x00, sizeof(double) * _iSize * _iSize);
+		for(iIndex1 = 0 ; iIndex1 < iNewSize * iNewSize ; iIndex1++)
+		{
+			int iRowTemp	= 0;
+			int iColTemp	= 0;
+
+			_pData[iRow + iCol * _iSize] = iIndex1 + 1;
+			iRowTemp		= iRow - 1;
+			iColTemp		= iCol + 1;
+
+			if(iRowTemp < 0)
+				iRowTemp	= iNewSize - 1;
+			if(iColTemp >= iNewSize)
+				iColTemp	= 0;
+
+			if(_pData[iRowTemp + iColTemp * _iSize] != 0)
+			{
+				iRowTemp	= iRow + 1;
+				iColTemp	= iCol;
+			}
+			iRow		= iRowTemp;
+			iCol		= iColTemp;
+		}
+		if(_iSize % 2 != 0)
+			return;
+
+		//rest of even order
+		for(iIndex1 = 0 ; iIndex1 < iNewSize ; iIndex1++)
+		{
+			for(iIndex2 = 0 ; iIndex2 < iNewSize ; iIndex2++)
+			{
+				int iRow = iIndex1 + iNewSize;
+				int iCol = iIndex2 + iNewSize;
+				
+				_pData[iIndex1 + iCol * _iSize] = _pData[iIndex1 + iIndex2 * _iSize] + 2 * iNewSize * iNewSize;
+				_pData[iRow + iIndex2 * _iSize] = _pData[iIndex1 + iIndex2 * _iSize] + 3 * iNewSize * iNewSize;
+				_pData[iRow + iCol * _iSize]	= _pData[iIndex1 + iIndex2 * _iSize] + iNewSize * iNewSize;
+			}
+		}
+		if((iNewSize - 1) / 2 == 0)
+			return;
+		for(iIndex1 = 0 ; iIndex1 < (iNewSize - 1) / 2 ; iIndex1++)
+			C2F(dswap)(&iNewSize, &_pData[iIndex1 * _iSize], &iUn, &_pData[iNewSize + iIndex1 * _iSize], &iUn);
+		
+		iTemp1	= (iNewSize + 1) / 2 - 1;
+		iTemp2	= iTemp1 + iNewSize;
+		C2F(dswap)(&iUn, &_pData[iTemp1], &iUn, &_pData[iTemp2], &iUn);
+		C2F(dswap)(&iUn, &_pData[iTemp1 * _iSize + iTemp1], &iUn, &_pData[iTemp1 * _iSize + iTemp2], &iUn);
+		iTemp1 = _iSize - (iNewSize - 3) / 2;
+		if(iTemp1 > _iSize)
+			return;
+		for(iIndex1 = iTemp1 ; iIndex1 < _iSize ; iIndex1++)
+			C2F(dswap)(&iNewSize, &_pData[iIndex1 * _iSize], &iUn, &_pData[iNewSize + iIndex1 * _iSize], &iUn);
+	}
+	else
+	{
+		int iVal = 1;
+		for(iIndex1 = 0 ; iIndex1 < _iSize ; iIndex1++)
+		{
+			for(iIndex2 = 0 ; iIndex2 < _iSize ; iIndex2++)
+			{
+				_pData[iIndex2 * _iSize + iIndex1] = iVal;
+				if(((iIndex1 + 1) % 4) / 2 == ((iIndex2 + 1) % 4) / 2)
+					_pData[iIndex2 * _iSize + iIndex1] = _iSize * _iSize + 1 - iVal;
+				iVal++;
+			}
+		}
+	}
+}
+
