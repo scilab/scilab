2452761ca17c162f5d1f7fd10ad0180ec59609d6
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2008-07-28 14:01:23 +0000

    stupid.

--- scilab/modules/helptools/src/java/org/scilab/modules/helptools/BuildDocObject.java
+++ scilab/modules/helptools/src/java/org/scilab/modules/helptools/BuildDocObject.java
@@ -123,7 +123,7 @@ public class BuildDocObject extends StyleSheet {
 		args.add("html.stylesheet=" + styleSheet);
 		args.addAll(specificArgs);
 		
-		//		doMain(args.toArray(new String [args.size()]), new StyleSheet(), "java com.icl.saxon.StyleSheet");
+		doMain(args.toArray(new String [args.size()]), new StyleSheet(), "java com.icl.saxon.StyleSheet");
 		this.postProcess();
 	}
 	
