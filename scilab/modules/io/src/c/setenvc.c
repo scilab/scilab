4c2d1846c0c1bb4e43dd2a89d4cb7e5166008897
Allan Cornet <allan.cornet@scilab.org>
2008-07-31 13:26:51 +0000

    setenv for windows

--- scilab/modules/io/src/c/setenvc.c
+++ scilab/modules/io/src/c/setenvc.c
@@ -10,54 +10,67 @@
  *
  */
 
-#include "machine.h"
+#ifdef _MSC_VER
+#include <windows.h>
+#endif
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include "machine.h"
 #include "setenvc.h"
 #include "../../tclsci/includes/setenvtcl.h"
 #include "MALLOC.h" /* MALLOC */
 
 #ifdef _MSC_VER
 #define putenv _putenv
-static char *env=NULL;
+static char *env = NULL;
 #endif
 
 static int UpdateEnvVar=0;
 /*--------------------------------------------------------------------------*/
 int setenvc(char *string,char *value)
 {
-  int ret=0;
-  char* env;
+	int ret=0;
+	char* env;
 
 #ifdef LINUX
-  if ( setenv(string,value,1) ) ret=FALSE;
-  else 
-    {
-      ret=TRUE;
-      UpdateEnvVar=1;
+	if ( setenv(string,value,1) ) ret=FALSE;
+	else 
+	{
+		ret = TRUE;
+		UpdateEnvVar = 1;
     }
 #else /* others HP Solaris WIN32*/
-  env=(char*)MALLOC((strlen(string)+strlen(value)+2)*sizeof(char));
-  sprintf(env,"%s=%s",string,value);
-  if ( putenv(env) )
-	  { 
-		  ret=FALSE;
+	env = (char*)MALLOC((strlen(string)+strlen(value)+2)*sizeof(char));
+	#ifdef _MSC_VER
+	/* 
+	 On Windows :
+     each process has two copies of the environment variables,
+     one managed by the OS and one managed by the C library. We set
+     the value in both locations, so that other software that looks in
+     one place or the other is guaranteed to see the value.
+	*/
+	SetEnvironmentVariableA(string,value);
+	#endif
 
-	  }
-  else 
-  {
-      setenvtcl(string,value);
-      ret=TRUE;
-      UpdateEnvVar=1;
-  }
-#ifdef _MSC_VER
-  if (env)
-    {
-      FREE(env);
-      env=NULL;
+	sprintf(env,"%s=%s",string,value);
+	if ( putenv(env) )
+	{ 
+		ret = FALSE;
+	}
+	else 
+	{
+		setenvtcl(string,value);
+		ret=TRUE;
+		UpdateEnvVar=1;
+	}
+	#ifdef _MSC_VER
+	if (env)
+	{
+		FREE(env);
+		env = NULL;
     }
-#endif
+	#endif
 #endif
 
   return ret;
