bc622245efec852fc22a1e15e0068c9942406aae
Simon Lipp <simon.lipp@scilab.org>
2008-08-04 15:06:23 +0000

    Revert "comments temp. to buid linux version grr"
    This reverts commit 55625109697feafbe7c7d03167ae7c1fe3248b18.

--- scilab/modules/gui/sci_gateway/cpp/sci_getlookandfeel.cpp
+++ scilab/modules/gui/sci_gateway/cpp/sci_getlookandfeel.cpp
@@ -15,45 +15,45 @@
 
 extern "C"
 {
-//#include <stdlib.h>
-//#include "gw_gui.h"
-//#include "stack-c.h"
-//#include "getScilabJavaVM.h"
-//#include "Scierror.h"
-//#include "localization.h"
+#include <stdlib.h>
+#include "gw_gui.h"
+#include "stack-c.h"
+#include "getScilabJavaVM.h"
+#include "Scierror.h"
+#include "localization.h"
 /*--------------------------------------------------------------------------*/
 int sci_getlookandfeel(char *fname,unsigned long fname_len)
 {
-	//static integer n1 = 0,m1 = 0,l1 = 0;
-	//char *look= NULL;
+	static integer n1 = 0,m1 = 0,l1 = 0;
+	char *look= NULL;
 
-	//Rhs = Max(0, Rhs);
-	//CheckRhs(0,0);
-	//CheckLhs(1,1);
+	Rhs = Max(0, Rhs);
+	CheckRhs(0,0);
+	CheckLhs(1,1);
 
-	//org_scilab_modules_gui_utils::LookAndFeelManager *lnf = new org_scilab_modules_gui_utils::LookAndFeelManager(getScilabJavaVM());
-	//if (lnf)
-	//{
-	//	look = lnf->getCurrentLookAndFeel();
-	//	delete lnf;
-	//	if (look)
-	//	{	
-	//		m1 = (int)strlen(look);
-	//		n1 = 1;
-	//		CreateVarFromPtr(Rhs+1,STRING_DATATYPE,&m1,&n1,&look);
-	//		if (look) {free(look); look = NULL;} /* free because giws uses malloc */
-	//		LhsVar(1) = Rhs+1;
-	//		C2F(putlhsvar)();
-	//	}
-	//	else
-	//	{
-	//		Scierror(999,_("%s: An error occurred: %s.\n"),fname,_("Impossible to get current look and feel"));
-	//	}
-	//}
-	//else
-	//{
-	//	Scierror(999,_("%s: No more memory.\n"),fname);
-	//}
+	org_scilab_modules_gui_utils::LookAndFeelManager *lnf = new org_scilab_modules_gui_utils::LookAndFeelManager(getScilabJavaVM());
+	if (lnf)
+	{
+		look = lnf->getCurrentLookAndFeel();
+		delete lnf;
+		if (look)
+		{	
+			m1 = (int)strlen(look);
+			n1 = 1;
+			CreateVarFromPtr(Rhs+1,STRING_DATATYPE,&m1,&n1,&look);
+			if (look) {free(look); look = NULL;} /* free because giws uses malloc */
+			LhsVar(1) = Rhs+1;
+			C2F(putlhsvar)();
+		}
+		else
+		{
+			Scierror(999,_("%s: An error occurred: %s.\n"),fname,_("Impossible to get current look and feel"));
+		}
+	}
+	else
+	{
+		Scierror(999,_("%s: No more memory.\n"),fname);
+	}
 
 	return 0;
 }
