e49063f37e7664418ea8e04a5d0e367ac69c7fb0
Vincent Couvert <vincent.couvert@scilab.org>
2008-08-12 06:09:52 +0000

    Remove unused imports

--- scilab/modules/gui/src/java/org/scilab/modules/gui/bridge/window/SwingScilabWindow.java
+++ scilab/modules/gui/src/java/org/scilab/modules/gui/bridge/window/SwingScilabWindow.java
@@ -20,16 +20,12 @@ import java.lang.reflect.InvocationTargetException;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JSplitPane;
 import javax.swing.SwingUtilities;
 
-import org.flexdock.docking.Dockable;
 import org.flexdock.docking.DockingConstants;
 import org.flexdock.docking.DockingManager;
 import org.flexdock.docking.DockingPort;
-import org.flexdock.docking.DockingStrategy;
 import org.flexdock.docking.defaults.DefaultDockingPort;
-import org.flexdock.docking.drag.DragOperation;
 import org.flexdock.docking.drag.effects.EffectsManager;
 import org.flexdock.docking.drag.preview.GhostPreview;
 import org.flexdock.view.View;
