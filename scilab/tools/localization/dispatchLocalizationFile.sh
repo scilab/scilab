e1bec0cac164ac28879288bb1b9eb002153d1a9f
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2008-08-19 12:56:03 +0000

    # This script takes a list of localization files dumped by launchpad # here https://translations.launchpad.net/scilab/trunk/+export # and dispatch them into Scilab source tree

---
+++ scilab/tools/localization/dispatchLocalizationFile.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# Copyright INRIA/Scilab 2008
+# Author : Sylvestre Ledru <sylvestre.ledru@inria.fr>
+# This script takes a list of localization files dumped by launchpad
+# here https://translations.launchpad.net/scilab/trunk/+export 
+# and dispatch them into Scilab source tree
+
+if test $# -ne 1; then
+	echo "This script takes a list of localization files dumped by launchpad"
+	echo "here https://translations.launchpad.net/scilab/trunk/+export "
+	echo "and dispatch them into Scilab source tree"
+	echo ""
+	echo "Syntax : $0 <path_to_launchpad_localization_file"
+	exit -1
+fi
+
+if test -z "$SCI"; then
+        echo "Please define the variable SCI" 
+        exit -2
+fi
+
+LAUNCHPAD_DIRECTORY=$1
+if test ! -d $LAUNCHPAD_DIRECTORY; then
+	echo "Could not find $LAUNCHPAD_DIRECTORY."
+	echo "Exiting..."
+	exit -3
+fi
+
+# Workaround because launchpad does not allow to delete a directory
+rm $LAUNCHPAD_DIRECTORY/string-v2*.po 
+
+for file in $LAUNCHPAD_DIRECTORY/*.po; do 
+	file=`echo $file|awk -F / '{print $NF}'`
+	LOC=`echo $file|cut -d. -f1|awk -F - '{print $NF}'`
+
+	if test "$LOC" == "fr"; then LOC="fr_FR"; fi
+	if test "$LOC" == "ru"; then LOC="ru_RU"; fi
+
+	# check that it is the right format
+	echo "$LOC"|grep -E "(.*_.*)" > /dev/null
+	if test $? -eq 0; then
+		MODULE=`echo $file|sed -e "s|\(.*\)-.*|\1|"|sed -e "s|-|_|g"`
+
+		DIR=$SCI/modules/$MODULE/locales/$LOC/
+		if test ! -d $DIR; then
+			mkdir $DIR
+		fi
+		echo "cp $file $DIR/$MODULE.po"
+		cp $file $DIR/$MODULE.po
+		if test $? -ne 0; then
+			echo "Error detected in the copy"
+			exit 1;
+		fi
+	else
+		echo "Ignore locale $LOC"
+	fi
+done
