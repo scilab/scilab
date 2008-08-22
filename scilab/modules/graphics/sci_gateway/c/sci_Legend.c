3773ffd9e25a03e280a5043961228389c02e091b
Allan Cornet <allan.cornet@scilab.org>
2008-08-22 12:17:07 +0000

    fix wrong cast

--- scilab/modules/graphics/sci_gateway/c/sci_Legend.c
+++ scilab/modules/graphics/sci_gateway/c/sci_Legend.c
@@ -93,7 +93,10 @@ int sci_Legend( char * fname, unsigned long fname_len )
   for (i = 0; i < n;i++)
   {
     handelsvalue = (unsigned long) (hstk(l1))[n-1-i];
-    if (psubwin!=sciGetParentSubwin(handelsvalue)) {
+	/* Serge Please check this : */
+	/* if (psubwin!=sciGetParentSubwin(handelsvalue)) { */
+
+    if (psubwin!=sciGetParentSubwin( sciGetPointerFromHandle(handelsvalue) )) {
       Scierror(999,_("%s: Objects must have the same axes.\n"),fname);
       return 0;
     }
