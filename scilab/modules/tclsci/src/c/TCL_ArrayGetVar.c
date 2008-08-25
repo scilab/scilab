74d670c510adf8ec05544c20901e0494a5d763a9
Bruno Jofret <bruno.jofret@scilab.org>
2008-08-25 06:28:03 +0000

    Access TCL hashtables through string index

--- scilab/modules/tclsci/src/c/TCL_ArrayGetVar.c
+++ scilab/modules/tclsci/src/c/TCL_ArrayGetVar.c
@@ -1,11 +1,12 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) INRIA - Allan CORNET
- * 
+ * Copyright (C) 2008-2088 - INRIA - Bruno JOFRET
+ *
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
  * you should have received as part of this distribution.  The terms
- * are also available at    
+ * are also available at
  * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
  *
  */
@@ -20,12 +21,21 @@
 /*--------------------------------------------------------------------------*/
 #define TCL_NOT_DEFINE "#NOT DEF.#"
 /*--------------------------------------------------------------------------*/
-char *TCL_ArrayGetVar(Tcl_Interp *TCLinterpreter,char *VarName,int i,int j)
+/*
+** TCL Arrays are Hashtable in fact.
+** So we have to acces it through string indexes.
+*/
+char *TCL_ArrayGetVar(Tcl_Interp *TCLinterpreter,char *VarName,char *index)
 {
 	char *RetStr=NULL;
 	char ArrayName[2048];
 
-	sprintf(ArrayName,"%s(%d,%d)",VarName,i,j);
+	if (index == NULL)
+	  {
+	    return strdup(TCL_NOT_DEFINE);
+	  }
+
+	sprintf(ArrayName,"%s(%s)",VarName,index);
 
 	RetStr = (char*)Tcl_GetVar(TCLinterpreter, ArrayName, TCL_GLOBAL_ONLY);
 
--- scilab/modules/tclsci/src/c/TCL_ArrayGetVar.h
+++ scilab/modules/tclsci/src/c/TCL_ArrayGetVar.h
@@ -1,11 +1,12 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) INRIA - Allan CORNET
- * 
+ * Copyright (C) 2008-2008 - INRIA - Bruno JOFRET
+ *
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
  * you should have received as part of this distribution.  The terms
- * are also available at    
+ * are also available at
  * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
  *
  */
@@ -19,11 +20,10 @@
 * TCL_ArrayGetVar
 * @param[in] Tcl_Interp : TCL interpreter
 * @param[in] VarName : Variable Name (Tcl Array)
-* @param[in] i position
-* @param[in] j position
+* @param[in] index : index in TCL VarName Hashtabl
 * @return value (string)
 */
-char *TCL_ArrayGetVar(Tcl_Interp *TCLinterpreter,char *VarName,int i,int j);
+char *TCL_ArrayGetVar(Tcl_Interp *TCLinterpreter,char *VarName,char *index);
 
 #endif /* __TCL_ARRAYGETVAR_H__ */
 /*--------------------------------------------------------------------------*/
