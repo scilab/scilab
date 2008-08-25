f08be6e978f328d4f1a26233c92a8ad088c52ddd
Bruno Jofret <bruno.jofret@scilab.org>
2008-08-25 06:24:12 +0000

    ArrayDim now return a list of index

--- scilab/modules/tclsci/src/c/TCL_ArrayDim.h
+++ scilab/modules/tclsci/src/c/TCL_ArrayDim.h
@@ -1,11 +1,12 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) INRIA - Allan CORNET
- * 
+ * Copyright (C) 2008-2008 - INRIA - Bruno JOFRET
+ *
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
  * you should have received as part of this distribution.  The terms
- * are also available at    
+ * are also available at
  * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
  *
  */
@@ -19,11 +20,11 @@
 * TCL_ArrayDim get dimension of a TCL Array
 * @param[in] TCLinterpreter : TCL Interpreter
 * @param[in] VarName : TCL variable name
-* @param[out] m size row 
-* @param[out] n size col
-* @return FALSE or TRUE (OK)
+* @param[out] nb_lines lines found in TCL array
+* @param[out] nb_columns is 1 if TCL Array is an Hastable
+* @return index_size number of indexes
 */
-BOOL TCL_ArrayDim(Tcl_Interp *TCLinterpreter,char *VarName,int *m,int *n);
+char **TCL_ArrayDim(Tcl_Interp *TCLinterpreter,char *VarName,int *nb_lines, int* nb_columns);
 
 #endif /* __TCL_ARRAYDIM_H__ */
 /*--------------------------------------------------------------------------*/
