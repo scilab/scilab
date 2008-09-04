301297a657d53b03d6f7431eaaeaa8806db3b128
Jean-baptiste Silvy <jean-baptiste.silvy@scilab.org>
2008-09-04 16:09:02 +0000

    Fix bug 3404.

--- scilab/modules/graphics/src/c/Format.c
+++ scilab/modules/graphics/src/c/Format.c
@@ -43,6 +43,10 @@
 static double spans[18] = {10,12,14,15,16,18,20,25,30,35,40,45,50,60,70,80,90,100};
 static int ticks[18] = {11,7,8,4,9,10,11,6,7,8,9,10,11,7,8,9,10,11};
 static double width[18] = {1,2,2,5,2,2,2,5,5,5,5,5,5,10,10,10,10,10};
+
+/** Maximum of ticks for log mode */
+#define MAX_LOG_TICKS 15
+
 /* end here */
 
 extern double C2F(dlamch)  (char *CMACH, unsigned long int);
@@ -976,7 +980,7 @@ int GradLog( double   _min   ,
 
   *n_grads = 0 ;
 
-  if(size<=7)    {
+  if(size <= MAX_LOG_TICKS)    {
     for(i=0;i<size;i++)
     {
       /*    _grads[i] = exp10(tab[i]); */
@@ -990,19 +994,22 @@ int GradLog( double   _min   ,
     int pas = 0, old_pas= 0,j;
     int val = size, passed = 0;
 
+    /* Try to reduce number of ticks, by finding the greatest divider */
     for(j=val-1;j>1;j--)
       if(val%j == 0){
         old_pas = pas;
         pas=j; 
         passed = 1;
 
-        if((7*pas)<=val){ 
+        if((MAX_LOG_TICKS * pas)<=val){ 
           if(old_pas != 0) {pas = old_pas; }
           break;
         }
       }
 
-      if(passed != 1 || (size/pas)>15 ) pas = size;
+      /* If we haven't found a divider or if the number of ticks will be to large */
+      /* Use only towo ticks */
+      if(passed != 1 || (size/pas) >  MAX_LOG_TICKS ) { pas = size; }
 
       if(pas==size)
       {
