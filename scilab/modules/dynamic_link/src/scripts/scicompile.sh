dbc8cde18a570c224c7d22fae958691562ae3b1a
Michael Baudin <michael.baudin@scilab.org>
2008-09-15 14:40:10 +0000

    Added some comments.

--- scilab/modules/dynamic_link/src/scripts/scicompile.sh
+++ scilab/modules/dynamic_link/src/scripts/scicompile.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+#
+# scicompile.sh --
+#   Consider the Makefile.orig as a template and create the Makefile
+#   so that the current project compiles.
+#   Process two template lines of the Makefile.orig :
+#   - "libsciexternal_la_SOURCES = foo.c foo2.f foo3.cxx foo4.cpp foo5.f90" is 
+#      processed so that it reflects the file names given as arguments #2 to the end
+#   - "am_libsciexternal_la_OBJECTS = foo.lo foo2.lo foo3.lo foo4.lo foo5.lo" is 
+#      processed so that it reflects the list of object files
+#   - "sciexternal" is replaced with the real name of the library, given as argument #1
+# Arguments
+#   $1 : the name of the library, e.g. mylib
+#   $2,$3, etc... : the names of the source files to compile, e.g. file1.c file2.c
+#
 ORIGINALLIBNAME=libsciexternal
 
 if test ! -s Makefile.orig; then 
@@ -35,7 +49,7 @@ done
 # Replace fake filenames to the one we want 
 sed -e "s|libsciexternal_la_SOURCES = foo.c foo2.f foo3.cxx foo4.cpp foo5.f90|lib"$LIB"_la_SOURCES = $SOURCES|g" Makefile.orig > Makefile
 
-## Replace sources by .lo file (manage also when it is at the end of the line
+##Â Replace sources by .lo file (manage also when it is at the end of the line
 SOURCES=`echo $SOURCES|sed -e 's|\.c |\.lo |g' -e 's|\.c$|\.lo|g'`
 SOURCES=`echo $SOURCES|sed -e 's|\.f |\.lo |g' -e 's|\.f$|\.lo|g'`
 SOURCES=`echo $SOURCES|sed -e 's|\.cxx |\.lo |g' -e 's|\.cxx$|\.lo|g'`
@@ -52,7 +66,7 @@ mv  Makefile.swap Makefile
 sed -e "s|libsciexternal|lib"$LIB"|g" Makefile > Makefile.swap
 mv  Makefile.swap Makefile
 
-### Remove the dependencies computed by the system
+###ï¿½Remove the dependencies computed by the system
 # No longer used because of  --disable-dependency-tracking when calling 
 # ./configure (comment left here : we never know)
 #sed -e "s|^include\(.*\)Plo||" Makefile > Makefile.swap
