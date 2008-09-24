cce6f11785b0c2fa790a961333a35bbf8ce982e3
Allan Cornet <allan.cornet@scilab.org>
2008-09-24 04:18:11 +0000

    fortran file

--- scilab/modules/interpolation/src/fortran/constinterp.h
+++ scilab/modules/interpolation/src/fortran/constinterp.h
@@ -8,9 +8,9 @@
 *     http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
 
 *     various constant used in somespline.f
-      int    NOT_A_KNOT,   NATURAL,   CLAMPED,   PERIODIC
+      integer    NOT_A_KNOT,   NATURAL,   CLAMPED,   PERIODIC
       parameter (NOT_A_KNOT=0, NATURAL=1, CLAMPED=2, PERIODIC=3)
-      int    FAST,   FAST_PERIODIC,   MONOTONE,   BY_ZERO
+      integer    FAST,   FAST_PERIODIC,   MONOTONE,   BY_ZERO
       parameter (FAST=4, FAST_PERIODIC=5, MONOTONE=6, BY_ZERO=7)
-      int    C0,   LINEAR,    BY_NAN
+      integer    C0,   LINEAR,    BY_NAN
       parameter (C0=8, LINEAR=9,  BY_NAN=10)
