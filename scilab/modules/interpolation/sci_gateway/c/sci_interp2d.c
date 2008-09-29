e826085ae10458b27db4c0b9df1d1e68ad4c4b71
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2008-09-29 13:50:17 +0000

    Update of the license file + update of headers

--- scilab/modules/interpolation/sci_gateway/c/sci_interp2d.c
+++ scilab/modules/interpolation/sci_gateway/c/sci_interp2d.c
@@ -1,7 +1,13 @@
-/*--------------------------------------------------------------------------*/
-/* INRIA */
-/* AUTHOR : Bruno Pincon */
-/*--------------------------------------------------------------------------*/
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) Bruno Pincon
+ * 
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at    
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+*/
 #include <string.h>
 #include "gw_interpolation.h"
 #include "stack-c.h"
--- scilab/modules/interpolation/src/c/interpolation.c
+++ scilab/modules/interpolation/src/c/interpolation.c
@@ -1,11 +1,16 @@
-/*--------------------------------------------------------------------------*/
 /*
- *  PURPOSE
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) Bruno Pincon
+ * 
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at    
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+*/
+/*  PURPOSE
  *     interface code for some interpolation / approximation
  *     routines
- *
- *  AUTHOR
- *     Bruno Pincon
  */
 /*--------------------------------------------------------------------------*/
 #include <math.h>
