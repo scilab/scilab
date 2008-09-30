863c17c7062e0ff427295867e9610de8b51ceed7
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2008-09-30 09:21:55 +0000

    Update of the license file + header updated

--- scilab/modules/optimization/src/fortran/fremf1.f
+++ scilab/modules/optimization/src/fortran/fremf1.f
@@ -1,3 +1,12 @@
+c Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+c Copyright (C) INRIA
+c 
+c This file must be used under the terms of the CeCILL.
+c This source file is licensed as described in the file COPYING, which
+c you should have received as part of this distribution.  The terms
+c are also available at    
+c http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+c
       subroutine fremf1 (prosca,iflag,n,ntot,nta,mm1,p,alfa,
      1 e,a,r,izs,rzs,dzs)
       implicit double precision (a-h,o-z)
--- scilab/modules/optimization/src/fortran/shanph.f
+++ scilab/modules/optimization/src/fortran/shanph.f
@@ -1,3 +1,13 @@
+c Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+c Copyright (C) INRIA
+c 
+c This file must be used under the terms of the CeCILL.
+c This source file is licensed as described in the file COPYING, which
+c you should have received as part of this distribution.  The terms
+c are also available at    
+c http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+c
+
       subroutine shanph(diag,n,nt,np,y,s,ys,scal,index,io,imp)
 c     mise a l echelle de diag par la methode de shanno-phua
 c     calcul du facteur d echelle scal
