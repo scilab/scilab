74d527952d81fa82b977768adbf28785ad025b8f
Allan CORNET <allan.cornet@scilab.org>
2008-10-22 09:14:55 +0200

    SVN revision replaced by GIT timestamp

--- scilab/modules/core/src/c/version.c
+++ scilab/modules/core/src/c/version.c
@@ -21,7 +21,7 @@ void disp_scilab_version(void)
 {
 	if ( (getScilabMode() == SCILAB_NWNI) || (getScilabMode() == SCILAB_NW) || (getScilabMode() == SCILAB_API) )
 	{
-		printf(_("Scilab version \"%d.%d.%d.%d\"\n"),SCI_VERSION_MAJOR,SCI_VERSION_MINOR,SCI_VERSION_MAINTENANCE,SCI_VERSION_REVISION);
+		printf(_("Scilab version \"%d.%d.%d.%d\"\n"),SCI_VERSION_MAJOR,SCI_VERSION_MINOR,SCI_VERSION_MAINTENANCE,SCI_VERSION_TIMESTAMP);
 		printf("%s\n\n",SCI_VERSION_STRING);
 	}
 	else
@@ -33,7 +33,7 @@ void disp_scilab_version(void)
 			MessageBox(NULL,msg,_("Scilab Version Info."),MB_ICONINFORMATION);
 		}
 		#else
-		printf(_("Scilab version \"%d.%d.%d.%d\"\n"),SCI_VERSION_MAJOR,SCI_VERSION_MINOR,SCI_VERSION_TIMESTAMP,SCI_VERSION_REVISION);
+		printf(_("Scilab version \"%d.%d.%d.%d\"\n"),SCI_VERSION_MAJOR,SCI_VERSION_MINOR,SCI_VERSION_MAINTENANCE,SCI_VERSION_TIMESTAMP);
 		printf("%s\n\n",SCI_VERSION_STRING);
 		#endif
 	}
