ba9aadf032dfdfefc1d3dde596b731444cc95371
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2008-11-04 11:58:26 +0100

    workaround to fix gfortran 4.2.1 & 4.3.2 bug: 0.999999999--> 2

--- scilab/modules/core/src/fortran/formatnumber.f
+++ scilab/modules/core/src/fortran/formatnumber.f
@@ -8,6 +8,7 @@ c are also available at
 c http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
 
       subroutine formatnumber(a,ifmt,maxc,str,fl)
+c maxc: Maximun character authorized
       double precision a
       integer n1,n2,fl
       character str*(*)
@@ -25,7 +26,13 @@ c http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
           write(form,120) n1,n2
           write(str(1:n1),form) a
 C     workaround to fix gfortran4.0 bug: 0.999999999--> 0
-          if (str(1:n1).eq.' 0.'.and.a.gt.0.9999d0) str(2:2)='1'
+C     See: http://bugzilla.scilab.org/show_bug.cgi?id=1985
+C     See: http://gcc.gnu.org/bugzilla/show_bug.cgi?id=28354
+           if (str(1:n1).eq.' 0.'.and.a.gt.0.9999d0) str(2:2)='1'
+C     workaround to fix gfortran 4.2.1 & 4.3.2 bug: 0.999999999--> 2
+C     See: http://bugzilla.scilab.org/show_bug.cgi?id=2647
+C     See: http://gcc.gnu.org/bugzilla/show_bug.cgi?id=37863
+          if (str(1:n1).eq.' 2.'.and.a.lt.1.0d0) str(2:2)='1'
        elseif (ifmt .eq. -1) then
 C     Inf
           fl = 3
