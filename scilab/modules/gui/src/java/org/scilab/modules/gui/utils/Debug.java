4a07742966a733cb4812a416190d9b211120b53c
Bruno JOFRET <bruno.jofret@scilab.org>
2008-11-19 17:54:55 +0100

    Revert "Revert "Use Listeners""
    This reverts commit d727ecba269fa2b2dd77c55bd3af8d293267a5ee.

--- scilab/modules/gui/src/java/org/scilab/modules/gui/utils/Debug.java
+++ scilab/modules/gui/src/java/org/scilab/modules/gui/utils/Debug.java
@@ -1,15 +1,21 @@
 package org.scilab.modules.gui.utils;
 
 public class Debug {
+
+    private static final boolean enableDebug = false;
     /**
      * DEBUG function
      */
     public static void DEBUG(String callingClass, String msg) {
-	System.err.println("[DEBUG] __"+callingClass+"__ "+Thread.currentThread().toString()+" : "+msg);
+	if (enableDebug) {
+	    System.err.println("[DEBUG] __"+callingClass+"__ "+Thread.currentThread().toString()+" : "+msg);
+	}
     }
 
     public static void DEBUG(String msg) {
-	System.err.println("[DEBUG] __unknown_class__ "+Thread.currentThread().toString()+" : "+msg);
+	if (enableDebug) {
+	    System.err.println("[DEBUG] __unknown_class__ "+Thread.currentThread().toString()+" : "+msg);
+	}
     }
-    
+
 }
