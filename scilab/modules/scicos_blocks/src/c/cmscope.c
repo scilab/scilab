fe1beeae6f1e91be65db0652daecd0a408c57ebc
Jean-Baptiste Silvy <jean-baptiste.silvy@scilab.org>
2008-12-19 16:02:14 +0100

    Wrong typo in includes, not ok for Linux

--- scilab/modules/scicos_blocks/src/c/cmscope.c
+++ scilab/modules/scicos_blocks/src/c/cmscope.c
@@ -33,7 +33,7 @@
 #include "scoGetProperty.h"
 #include "scoSetProperty.h"
 #include "scicos_block4.h"
-#include "setJavaProperty.h"
+#include "SetJavaProperty.h"
 
 /** \fn cmscope_draw(scicos_block * block, ScopeMemory ** pScopeMemory, int firstdraw)
     \brief Function to draw or redraw the window
