98b99d9df31d859c1fcc49ad64f7a07e6b4654cc
Pierre MARECHAL <pierre.marechal@scilab.org>
2009-01-13 10:43:35 +0100

    Branch Name

--- scilab/modules/core/includes/version.h
+++ scilab/modules/core/includes/version.h
@@ -15,7 +15,7 @@
 #define SCI_VERSION_MAJOR 5
 #define SCI_VERSION_MINOR 0
 #define SCI_VERSION_MAINTENANCE 0
-#define SCI_VERSION_STRING "scilab-branch-master"
+#define SCI_VERSION_STRING "scilab-branch-5.1"
 /* SCI_VERSION_REVISION --> hash key commit */
 #define SCI_VERSION_REVISION 0
 #define SCI_VERSION_TIMESTAMP 0
