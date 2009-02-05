5cac434a5885076d80a0abd80b54ae7dc6b0f8b7
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2009-02-05 09:48:55 +0100

    Revert "Uses termios instead of termio"
    This reverts commit 05d144b62f4da1bc65538fc6429c26aa68787d16.
    (cherry picked from commit 0c75f58717c80dce569d23e97f03047fd3fcb335)
    
    Signed-off-by: Pierre MARECHAL <pierre.marechal@scilab.org>

--- scilab/modules/shell/src/c/others/zzledt.c
+++ scilab/modules/shell/src/c/others/zzledt.c
@@ -82,13 +82,13 @@
 #define TERMCAP
 #endif
 
-#if !defined(linux) && !defined(netbsd) && !defined(freebsd) && !defined(__APPLE__)
+#if !defined(linux) && !defined(netbsd) && !defined(freebsd)
 #ifdef  __alpha
 #define B42UNIX
 #endif
 #endif
 
-#if defined(linux) || defined(__APPLE__)
+#ifdef linux
 #define ATTUNIX
 #define TERMCAP
 #endif
@@ -112,15 +112,9 @@ static struct tchars arg1;
 
 #ifdef ATTUNIX
 #define KEYPAD
-#ifdef HAVE_TERMIOS_H
-#include <termios.h>
-static struct termios save_term;
-static struct termios arg;
-#else
 #include <termio.h>
 static struct termio save_term;
 static struct termio arg;
-#endif
 #endif
 
 #define EXCL                  0x0021
@@ -320,6 +314,7 @@ char *TermReadAndProcess(void)
   sendprompt=1;
 
   setSearchedTokenInScilabHistory(NULL);
+
 
   while(1)
     {
@@ -1165,11 +1160,7 @@ static void set_cbreak()
   arg.c_oflag &= ~OPOST;
   arg.c_cc [VMIN] = 1;
   arg.c_cc [VTIME] = 0;
-#ifdef HAVE_TERMIOS_H
-  (void) tcsetattr (fd, TCSANOW, &arg);
-#else
   ioctl(fd, TCSETAW, &arg);
-#endif
 #endif
   cbreak_crmod = 0;
   return;
@@ -1189,11 +1180,7 @@ static void set_crmod()
   ioctl(fd, TIOCSETC, &arg1);
 #endif
 #ifdef ATTUNIX
-#ifdef HAVE_TERMIOS_H
-  (void) tcsetattr (fd, TCSANOW, &save_term);
-#else
   ioctl(fd, TCSETAW, &save_term);
-#endif
 #endif
   cbreak_crmod = 1;
   return;
@@ -1225,13 +1212,8 @@ static void init_io()
 #endif
 
 #ifdef ATTUNIX
-#ifdef HAVE_TERMIOS_H
-  (void) tcgetattr (fd, &arg);
-  (void) tcgetattr (fd, &save_term);
-#else
   ioctl(fd, TCGETA, &arg);
   ioctl(fd, TCGETA, &save_term);
-#endif
   erase_char = save_term.c_cc [VERASE];
 #endif
 
