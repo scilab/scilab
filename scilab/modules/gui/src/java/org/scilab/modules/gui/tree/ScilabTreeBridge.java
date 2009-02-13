887355a5b265af8bfb71937babc59b349903d2f4
Sylvestre Koumar <sylvestre.koumar@scilab.org>
2009-02-13 17:39:32 +0100

    begening of tree implementation

---
+++ scilab/modules/gui/src/java/org/scilab/modules/gui/tree/ScilabTreeBridge.java
@@ -0,0 +1,11 @@
+package org.scilab.modules.gui.tree;
+
+import org.scilab.modules.gui.bridge.tree.SwingScilabTree;
+
+public class ScilabTreeBridge {
+
+	public static void showTree(Tree tree) {
+		SwingScilabTree.showTree(tree);
+	}
+
+}
