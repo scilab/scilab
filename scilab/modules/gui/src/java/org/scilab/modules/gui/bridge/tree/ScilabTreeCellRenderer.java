76f6f03500be9e46dcd6a7f8f642b71135d246dc
Bruno JOFRET <bruno.jofret@scilab.org>
2009-02-16 18:35:36 +0100

    Change PATH to default Icons

--- scilab/modules/gui/src/java/org/scilab/modules/gui/bridge/tree/ScilabTreeCellRenderer.java
+++ scilab/modules/gui/src/java/org/scilab/modules/gui/bridge/tree/ScilabTreeCellRenderer.java
@@ -22,101 +22,101 @@ import javax.swing.JTree;
 import javax.swing.tree.DefaultTreeCellRenderer;
 
 public class ScilabTreeCellRenderer extends DefaultTreeCellRenderer {
-	
-	// Images
-	public static Toolkit toolkit = Toolkit.getDefaultToolkit();
-	public static Image plus = toolkit.getImage("SCI/modules/gui/images/icons/plus.png");
-	public static Image moins = toolkit.getImage("SCI/modules/gui/images/icons/moins.gif");
-	public static Image scilab = toolkit.getImage("SCI/modules/gui/images/icons/scilab.png");
-	public static Image puff = toolkit.getImage("SCI/modules/gui/images/icons/puff.gif");
-	
-	public static Image myNewPlus = plus.getScaledInstance(25, 25, Image.SCALE_DEFAULT);
-	public static Image myNewMoins = moins.getScaledInstance(25, 25, Image.SCALE_DEFAULT);
-	public static Image myNewScilab = scilab.getScaledInstance(25, 25, Image.SCALE_DEFAULT);
-	public static Image myNewPuff = puff.getScaledInstance(25, 25, Image.SCALE_DEFAULT);
-	
-	public static Icon iconePlus = new ImageIcon(myNewPlus);
-	public static Icon iconeMoins = new ImageIcon(myNewMoins);        
-	public static Icon iconeScilab = new ImageIcon(myNewScilab);
-	public static Icon iconePuff = new ImageIcon(myNewPuff);
-	
-	public static Image defaut = toolkit.getImage("");
-	public static Image myNewDefaut = defaut.getScaledInstance(25, 25, Image.SCALE_DEFAULT);
-	public static Icon iconeDefaut = new ImageIcon(myNewDefaut);
-	
-	
-	private static final long serialVersionUID = 1L;
 
-	/**
-	 * Default Constructor
-	 */
-	public ScilabTreeCellRenderer () {
-		super();
+    // Images
+    public static Toolkit toolkit = Toolkit.getDefaultToolkit();
+    public static Image plus = toolkit.getImage(System.getenv("SCI")+"/modules/gui/images/icons/plus.png");
+    public static Image moins = toolkit.getImage(System.getenv("SCI")+"/modules/gui/images/icons/moins.gif");
+    public static Image scilab = toolkit.getImage(System.getenv("SCI")+"/modules/gui/images/icons/scilab.png");
+    public static Image puff = toolkit.getImage(System.getenv("SCI")+"/modules/gui/images/icons/puff.gif");
+
+    public static Image myNewPlus = plus.getScaledInstance(25, 25, Image.SCALE_DEFAULT);
+    public static Image myNewMoins = moins.getScaledInstance(25, 25, Image.SCALE_DEFAULT);
+    public static Image myNewScilab = scilab.getScaledInstance(25, 25, Image.SCALE_DEFAULT);
+    public static Image myNewPuff = puff.getScaledInstance(25, 25, Image.SCALE_DEFAULT);
+
+    public static Icon iconePlus = new ImageIcon(myNewPlus);
+    public static Icon iconeMoins = new ImageIcon(myNewMoins);        
+    public static Icon iconeScilab = new ImageIcon(myNewScilab);
+    public static Icon iconePuff = new ImageIcon(myNewPuff);
+
+    public static Image defaut = toolkit.getImage("");
+    public static Image myNewDefaut = defaut.getScaledInstance(25, 25, Image.SCALE_DEFAULT);
+    public static Icon iconeDefaut = new ImageIcon(myNewDefaut);
+
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Default Constructor
+     */
+    public ScilabTreeCellRenderer () {
+	super();
+    }
+
+    /**
+     * Get tree renderer 
+     * @param tree we want to display
+     * @param value of the node
+     * @param sel is selected
+     * @param expanded is expanded
+     * @param leaf is a leaf
+     * @param row of the tree
+     * @param hasFocus 
+     * @return component renderer
+     */
+    public Component getTreeCellRendererComponent(JTree tree, Object value, 
+	    boolean sel, boolean expanded, boolean leaf, 
+	    int row, boolean hasFocus)  {
+
+	SwingScilabTree node = (SwingScilabTree) value;	
+
+	// Case of leaf
+	if(leaf){
+	    // Test the kind of selected component in the tree 
+	    if (node instanceof SwingScilabTree) {
+		// Set icon for the type of leaf
+		if (node.getIcon() != null) {
+		    if (node.getIcon() == iconeDefaut){				
+			this.setLeafIcon(iconeScilab);
+		    } else {	
+			this.setLeafIcon(node.getIcon());
+		    }
+		} else {				
+		    this.setLeafIcon(null);
+		}
+	    }
 	}
 
-	/**
-	 * Get tree renderer 
-	 * @param tree we want to display
-	 * @param value of the node
-	 * @param sel is selected
-	 * @param expanded is expanded
-	 * @param leaf is a leaf
-	 * @param row of the tree
-	 * @param hasFocus 
-	 * @return component renderer
-	 */
-	public Component getTreeCellRendererComponent(JTree tree, Object value, 
-												  boolean sel, boolean expanded, boolean leaf, 
-												  int row, boolean hasFocus)  {
-		
-		SwingScilabTree node = (SwingScilabTree) value;	
-		
-		// Case of leaf
-		if(leaf){
-			// Test the kind of selected component in the tree 
-			if (node instanceof SwingScilabTree) {
-				// Set icon for the type of leaf
-				if (node.getIcon() != null) {
-					if (node.getIcon() == iconeDefaut){				
-						this.setLeafIcon(iconeScilab);
-					} else {	
-						this.setLeafIcon(node.getIcon());
-					}
-				} else {				
-					this.setLeafIcon(null);
-				}
-			}
+	// Case of node
+	if(expanded){
+	    // Node is opened
+	    if (node.getIcon() != null) {
+		if (node.getIcon() == iconeDefaut) {
+		    this.setOpenIcon(iconeMoins);
+		} else {
+		    this.setOpenIcon(node.getIcon());
 		}
-		
-		// Case of node
-		if(expanded){
-			// Node is opened
-			if (node.getIcon() != null) {
-				if (node.getIcon() == iconeDefaut) {
-					this.setOpenIcon(iconeMoins);
-				} else {
-					this.setOpenIcon(node.getIcon());
-				}
-			} else {
-				this.setOpenIcon(null);
-			}				
-		}
-		else{
-			// Node is closed
-			if (node.getIcon() != null) {
-				if (node.getIcon() == iconeDefaut) {
-					this.setClosedIcon(iconePlus);
-				} else {
-					this.setClosedIcon(node.getIcon());
-				}
-			} else {
-				this.setClosedIcon(null);
-			}
-		}
-		
-		// Calling super class
-		super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf,row, hasFocus);
-		return this;
+	    } else {
+		this.setOpenIcon(null);
+	    }				
 	}
+	else{
+	    // Node is closed
+	    if (node.getIcon() != null) {
+		if (node.getIcon() == iconeDefaut) {
+		    this.setClosedIcon(iconePlus);
+		} else {
+		    this.setClosedIcon(node.getIcon());
+		}
+	    } else {
+		this.setClosedIcon(null);
+	    }
+	}
+
+	// Calling super class
+	super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf,row, hasFocus);
+	return this;
+    }
 
 }
