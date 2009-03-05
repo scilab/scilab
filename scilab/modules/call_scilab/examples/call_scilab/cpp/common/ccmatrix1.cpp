9ac1312852e879124bfd9b514edf829c599f6db1
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2009-03-05 14:25:58 +0100

    Cosmetic

--- scilab/modules/call_scilab/examples/call_scilab/cpp/common/ccmatrix1.cpp
+++ scilab/modules/call_scilab/examples/call_scilab/cpp/common/ccmatrix1.cpp
@@ -20,14 +20,13 @@ static char buf[256];
 CMatrix::CMatrix(char *name1,char *job)
 {
   sprintf(buf,"%s=%s;",name1,job);
-  if ( send_scilab_job(buf,strlen(buf)) != 0) 
+  if ( send_scilab_job(buf,strlen(buf)) != 0) {
     throw SciError();
-  else 
-    {
+  } else {
       int lp;
       C2F(cmatptr)(name1, &m, &n, &lp,strlen(name1));
       v = stk(lp);
-    }
+  }
   name = new char[strlen(name1)+1];
   strcpy(name,name1);
 }
@@ -36,15 +35,17 @@ CMatrix::CMatrix(char *name1,char *job)
 void CMatrix::scijob(char *jobname)
 {
   sprintf(buf,jobname,name);
-  if ( send_scilab_job(buf,strlen(buf)) != 0) 
+  if ( send_scilab_job(buf,strlen(buf)) != 0) {
     throw SciError();
+  }
 }
 
 CMatrix& CMatrix::plus(CMatrix &B)
 {
   sprintf(buf,"%s=%s+%s",this->name,this->name,B.name);
-  if ( send_scilab_job(buf,strlen(buf)) != 0) 
+  if ( send_scilab_job(buf,strlen(buf)) != 0) {
     throw SciError();
+  }
   int lp;
   C2F(cmatptr)(name, &m, &n, &lp,strlen(name));
   v = stk(lp);
@@ -60,8 +61,9 @@ void CMatrix::print()
 void CMatrix::inv()
 {
   sprintf(buf,"%s=inv(%s)",name,name);
-  if ( send_scilab_job(buf,strlen(buf)) != 0) 
+  if ( send_scilab_job(buf,strlen(buf)) != 0) {
     throw SciError();
+  }
   int lp;
   C2F(cmatptr)(name, &m, &n, &lp,strlen(name));
   v = stk(lp);
@@ -79,7 +81,3 @@ void cc_test()
   A.inv();
   A.print();
 }
-
-
-
-
