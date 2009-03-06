6fd70f30e1bd7717cc68a301f54e80dd802e44cc
Bruno JOFRET <bruno.jofret@scilab.org>
2009-03-06 20:14:06 +0100

    Revert "adding core/epsilon"
    This reverts commit c43d8c30408c0c758801dbd77eb8b31e59c79a06.

--- scilab/modules/core/includes/epsilon.h
+++
@@ -1,6 +0,0 @@
-#ifndef EPSILON_H
-#define EPSILON_H
-
-extern double epsilon(void);
-
-#endif
--- scilab/modules/core/src/c/epsilon.c
+++
@@ -1,8 +0,0 @@
-#include "epsilon.h"
-#include "machine.h" // C2F
-
-extern double C2F(dlamch)(const char[1]);
-
-double epsilon(void){
-  return C2F(dlamch)("e");
-}
