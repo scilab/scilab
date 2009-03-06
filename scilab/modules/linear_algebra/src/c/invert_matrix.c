1c7d1a5ca46ddfb7fb4e476c2f6c0bd78b86eeb8
Bruno JOFRET <bruno.jofret@scilab.org>
2009-03-06 20:01:49 +0100

    Remove new function and use Lapack one.

--- scilab/modules/linear_algebra/src/c/invert_matrix.c
+++ scilab/modules/linear_algebra/src/c/invert_matrix.c
@@ -1,8 +1,19 @@
+/*
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2009-2009 - DIGITEO - Bernard HUGUENEY
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
 #include "invert_matrix.h"
 #include "MALLOC.h"
 #include "core_math.h" // Max
 #include "machine.h" // C2F
-#include "epsilon.h"
 #include <stdio.h> // debug
 #include <math.h> //sqrt
 
@@ -56,27 +67,29 @@ static double zlange_1(int n_row, int n_col, double* data){
 }
 
 
-/* 
+/*
    TODO replace ugly constant 17 with properly #define d (enum ed ? ) constant
    for malloc error.
 */
 int iInvertMatrixM(int iRows, int iCols, double* pData, int complexArg
 		   , double* pdblRcond);
+
 int iInvertMatrix(int iRows, int iCols, double* pData, int complexArg
 		  , double * pdblRcond, int* piPivot, void* pWork
 		  , unsigned long lWorkSize, double* pdblWork);
+
 int iInvertMatrixM(int iRows, int iCols, double* pData, int complexArg
 		   , double* pdblRcond){
   int ret=0;
   int* piPivot=(int*)MALLOC(iCols*sizeof(int));
   if(piPivot){
-    unsigned long lWorkSize= Max(1, 4*iCols); 
-    void* pWork= complexArg 
-      ? MALLOC(lWorkSize*sizeof(double)*2) 
+    unsigned long lWorkSize= Max(1, 4*iCols);
+    void* pWork= complexArg
+      ? MALLOC(lWorkSize*sizeof(double)*2)
       : MALLOC(iCols*sizeof(int));
     if(pWork){
       double* pdblWork=(double*)MALLOC(lWorkSize*sizeof(double) *(complexArg  ? 2: 1));
-				       
+
       if(pdblWork){
 	ret = iInvertMatrix(iRows, iCols, pData, complexArg, pdblRcond, piPivot
 			    , pWork, lWorkSize, pdblWork);
@@ -105,16 +118,16 @@ int iInvertMatrix(int iRows, int iCols, double* pData, int complexArg
   int ret = 0; // >0 erreur <0 warning
   /* ANORM = dlange( '1', M, N, stk(lA), M, stk(lDWORK) )
      computes  one norm of a matrix (maximum column sum)
-     last work area is not used for norm '1' 
+     last work area is not used for norm '1'
      see http://publib.boulder.ibm.com/infocenter/clresctr/vxrx/topic/com.ibm.cluster.essl44.guideref.doc/am501_llange.html
   */
   /* using "1" to pass '1' to Fortran ("by ref"->pointer to char)*/
-#ifdef C_LANGE 
-  double dblAnorm = complexArg 
+#ifdef C_LANGE
+  double dblAnorm = complexArg
     ?  zlange_1(iRows, iCols, pData)
     : dlange_1(iRows, iCols, pData);
 #else
-  double dblAnorm = complexArg 
+  double dblAnorm = complexArg
     ?  C2F(zlange)("1", &iRows, &iCols, pData, &iRows,pdblWork /* see comment above */)
     : C2F(dlange)("1", &iRows, &iCols, pData, &iRows,pdblWork/*see above*/);
 #endif
@@ -142,7 +155,7 @@ int iInvertMatrix(int iRows, int iCols, double* pData, int complexArg
       C2F(dgecon)("1", &iCols, pData, &iCols, &dblAnorm, pdblRcond, pdblWork
 		  , (int*)pWork, &iInfo);
     }
-    if(*pdblRcond <= sqrt(epsilon())){ ret = -1; }
+    if(*pdblRcond <= sqrt(F2C(dlamch)("e",1L))){ ret = -1; }
     if(complexArg){
       C2F(zgetri)( &iCols, pData, &iCols, piPivot, pdblWork, &lWorkSize
 		   , &iInfo);
