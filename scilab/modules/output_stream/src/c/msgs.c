4e6bce3d776d90b2e62659f1471aa62f2ea28898
Bruno JOFRET <bruno.jofret@scilab.org>
2009-03-12 08:20:26 +0100

    Readded while merging. Removed

--- scilab/modules/output_stream/src/c/msgs.c
+++ scilab/modules/output_stream/src/c/msgs.c
@@ -20,22 +20,12 @@
 #include "localization.h"
 #include "cvstr.h"
 #include "do_error_number.h"
+#include <stdio.h>
 /*--------------------------------------------------------------------------*/
 #ifdef BUF
 #undef BUF
 #endif
 #define BUF C2F(cha1).buf
-
-int printToBuffer(const char* format,...){
-    va_list args;
-    va_start(args, format);
-    int r = vsprintf(C2F(cha1).buf, format, args);
-    va_end(args);
-    return r;
-
-}
-
-
 /*--------------------------------------------------------------------------*/
 extern int C2F(showstack)(void);
 /*--------------------------------------------------------------------------*/
