945bbddeb5028142d9e2384ae8d3647832caaf5a
Bernard HUGUENEY <bernard.hugueney@scilab.org>
2009-03-31 13:11:05 +0200

    SASification of linear_algebra svd

---
+++ scilab/modules/linear_algebra/includes/svd.h
@@ -0,0 +1,37 @@
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) ????-2009 - INRIA Bernard Hugueney
+ *
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
+
+/*
+ * perform singular value decomposition (cf. http://www.scilab.org/product/man/svd.html ), MALLOCating temporary buffers as needed.
+ *
+ * in  :
+ *
+ * @param pData double* (doublecomplex*) in / scratch data of the argument matrix
+ * @param iRows int in  nb of rows of the argument matrix
+ * @param iCols int in  nb of cols of the argument matrix
+ * @param complexArg int (bool semantics) in  if argument matrix is complex
+ * @param economy int (bool semantics) in  if economy mode
+ * @param tol double in  treshold for computing rank
+ *
+ * out :
+ *
+ * @param pSV double* out  result matrix s for lhs==1, NULL for lhs >1
+ * @param pU double* out  result matrix U for lhs>1, NULL for lhs ==1
+ * @param pS double* out  result matrix S for lhs>1, NULL for lhs ==1
+ * @param pV double* out  result matrix V for lhs>1, NULL for lhs ==1
+ * @param pRk double* out  result rank for lhs==4, NULL for lhs !=4
+ *
+ * @return 0 success, -1 MALLOC failure >0 Lapack convergence problems
+ *
+ */
+int iSvdM(double* pData, int iRows, int iCols, int complexArg, int economy, double tol, double* pSV, double* pU, double* pS, double* pV, double* pRk);
--- scilab/modules/linear_algebra/sci_gateway/c/sci_svd.c
+++ scilab/modules/linear_algebra/sci_gateway/c/sci_svd.c
@@ -1,7 +1,6 @@
-
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
- * Copyright (C) ????-2008 - INRIA
+ * Copyright (C) ????-2009 - INRIA Bernard Hugueney
  *
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
@@ -16,117 +15,196 @@
 #include "stack-c.h"
 #include "gw_linear_algebra.h"
 #include "Scierror.h"
+#include "MALLOC.h"
 #include "localization.h"
-/*--------------------------------------------------------------------------*/
-extern int C2F(intdgesvd1)(char *fname, unsigned long fname_len);
-extern int C2F(intzgesvd1)(char *fname, unsigned long fname_len);
-extern int C2F(intdgesvd2)(char *fname, unsigned long fname_len);
-extern int C2F(intzgesvd2)(char *fname, unsigned long fname_len);
-extern int C2F(intdoldsvd)(double *tol, char *fname, unsigned long fname_len);
-extern int C2F(intzoldsvd)(double *tol, char *fname, unsigned long fname_len);
-/*--------------------------------------------------------------------------*/
+
+#include "svd.h"
+
+/*
+  s=svd(X): [R: min(rows, cols) x 1]
+  [U,S,V]=svd(X) [U,S,V]:  [ [C|R: rows x rows], [R: rows x cols ],  [R|C: cols x cols ] ]
+  [U,S,V]=svd(X,0) (obsolete) [U,S,V]=svd(X,"e"): [ [C|R:rows x min(rows,cols)], [R: min(rows,cols) x min(rows,cols)], [C|R:cols x min(rows,cols)] ]
+  [U,S,V,rk]=svd(X [,tol]) : cf. supra, rk[R 1 x 1]
+ */
+
+extern int C2F(vfinite)(int *n, double *v);
+
 int C2F(intsvd)(char *fname,unsigned long fname_len)
 {
-	int *header1;int *header2;
-	int Cmplx;int ret;double tol;
+  int ret=0, economy=0, complexArg, iRows, iCols;
+  double* pData= NULL;
+  double* pDataReal= NULL;
+  double* pDataImg= NULL;
 
-	if (GetType(1)!=sci_matrix) 
+  double* pSV= NULL;
+
+  double* pU= NULL;
+  double* pUReal= NULL;
+  double* pUImg= NULL;
+
+  double* pS= NULL;
+
+  double* pV= NULL;
+  double* pVReal= NULL;
+  double* pVImg= NULL;
+
+  double tol= 0.;
+  double* pRk= NULL;
+
+  if ( (Rhs >=1) && (GetType(1)!=sci_matrix))
+    {
+      OverLoad(1);
+      return 0;
+    }
+  CheckRhs(1,2);
+  CheckLhs(1, 4);
+
+  economy= (Rhs==2) && (Lhs==3);
+  if( (complexArg=iIsComplex(1)) )
+    {
+      GetRhsVarMatrixComplex(1, &iRows, &iCols, &pDataReal, &pDataImg);
+      /* c -> z */
+      pData=(double*)oGetDoubleComplexFromPointer( pDataReal, pDataImg, iRows * iCols);
+      if(!pData)
 	{
-		OverLoad(1);
-		return 0;
+	  Scierror(999,_("%s: Cannot allocate more memory.\n"),fname);
+	  ret = 1;
 	}
-	header1 = (int *) GetData(1);
-	Cmplx=header1[3];
-
-	switch (Rhs) 
+    }
+  else
+    {
+      GetRhsVarMatrixDouble(1, &iRows, &iCols, &pData);
+    }
+  if(iRows == 0) /* empty matrix */
+    {
+      switch (Lhs){
+	case 4:
 	{
-		case 1:   /* s=svd(A)   or   [U,s,V]=svd(A)   */
-		switch (Lhs) 
-		{
-			case 1: case 2: case 3:
-				if (Cmplx==0) 
-				{
-					ret = C2F(intdgesvd1)("svd",3L);
-					return 0; 
-				}
-				if (Cmplx==1) 
-				{
-					ret = C2F(intzgesvd1)("svd",3L);
-					return 0; 
-				}
-			break;
-
-			case 4:
-				if (Cmplx==0) 
-				{
-					ret = C2F(intdoldsvd)((tol=0,&tol),"svd",3L);
-					return 0;
-				}
-				if (Cmplx==1) 
-				{
-					ret = C2F(intzoldsvd)((tol=0,&tol),"svd",3L);
-					return 0;
-				}
-			break;
-	 }
-	 case 2 :   /* svd(A, something)   */
-		header2 = (int *) GetData(2);
-		switch (header2[0]) 
-		{
-			case SCI_DOUBLE :
-			if (Lhs == 4) 
-			{
-				/*  old svd, tolerance is passed: [U,S,V,rk]=svd(A,tol)  */
-				/*   ret = C2F(intsvdold)("svd",2L);  */
-				if (Cmplx==0) 
-				{
-					tol = ((double *) header2)[2];
-					ret = C2F(intdoldsvd)(&tol,"svd",3L);
-					return 0;
-				}
-				if (Cmplx==1) 
-				{
-					tol = ((double *) header2)[2];
-					ret = C2F(intzoldsvd)(&tol,"svd",3L);
-					return 0;
-				}
-			}
-			else 
-			{
-				/* old Economy size:  [U,S,V]=svd(A,0)  */
-				if (Cmplx==0) 
-				{
-					ret = C2F(intdgesvd2)("svd",3L);
-					return 0;
-				}
-				if (Cmplx==1) 
-				{
-					ret = C2F(intzgesvd2)("svd",3L);
-					return 0;
-				}
-			}
-			break;
-
-			case STRING  :
-			/* Economy size:  [U,S,V]=svd(A,"e")  */
-			if (Cmplx==0) 
-			{
-				ret = C2F(intdgesvd2)("svd",3L);
-				return 0;
-			}
-			if (Cmplx==1) 
-			{
-				ret = C2F(intzgesvd2)("svd",3L);
-				return 0;
-			}
-			break;
-	  }
-	  break;
-
-	  default :   /*  rhs > 2 */
-		Scierror(999,("%s: Wrong number of input argument(s).\n"),fname);
-	  break;
+	  iAllocMatrixOfDouble(Rhs+5, 1,1, &pRk);
+	  *pRk= 0.;
+	  LhsVar(4)= Rhs + 5; /* no break */
 	}
-	return 0;
+	case 3:
+	{
+	  double* dummy;
+	  iAllocMatrixOfDouble(Rhs+3, iRows, iCols, &dummy); /* yes original Fortran code does this... (no check on iCols) */
+	  LhsVar(3)= Rhs + 3; /* no break */
+	}
+	case 2: /* illegal according to doc, but there was Fortran code to handle this, so... */
+	{
+	  double* dummy;
+	  iAllocMatrixOfDouble(Rhs+1, iRows, iCols, &dummy);
+	  LhsVar(2)= Rhs + 1; /* no break */
+	}
+	case 1:
+	{
+	  LhsVar(1)= 1;
+	}
+      }
+    }
+  else
+    {
+      if((iRows == -1) || (iCols== -1))
+	{
+	  Scierror(999,_("Size varying argument a*eye(), (arg %d) not allowed here.\n"), 1);
+	   ret= 1;
+	}
+      else
+	{
+	  int const totalsize= iRows * iCols * (complexArg ? 2 : 1); 
+	  if(!C2F(vfinite)(&totalsize, pData))
+	    {
+	      Scierror(999,_("Wrong value for argument %d: Must not contain NaN or Inf.\n"), 1);
+	      ret= 1;
+	    }
+	  else
+	    {
+	      switch(Lhs){
+	      case 4:
+		{
+		  if( (Rhs == 2) && ( GetType(2) == sci_matrix) ) /* other cases with Rhs==2 are handled as economy mode */
+		    {
+		      int dummy; /* original code does not check iRows == iCols == 1 */
+		      double* tmpData;
+		      GetRhsVarMatrixDouble(2, &dummy, &dummy, &tmpData);
+		      tol= *tmpData;
+		    }
+		  iAllocMatrixOfDouble(Rhs+4, 1, 1, &pRk);
+		}/* no break */
+	      case 3:
+		{
+		  int const economyRows= economy ? Min(iRows, iCols) : iRows;
+		  int const economyCols= economy ? Min(iRows, iCols) : iCols;
+		  if(complexArg)
+		    {
+		      iAllocComplexMatrixOfDouble(Rhs+1, iRows, economyRows , &pUReal, &pUImg);
+		      pU= (double*)MALLOC(iRows * economyRows*(complexArg ? sizeof(doublecomplex): sizeof(double)));
+		      iAllocMatrixOfDouble(Rhs+2, economyRows, economyCols, &pS);
+		      iAllocComplexMatrixOfDouble(Rhs+3, iCols, economyCols , &pVReal, &pVImg);
+		      pV= (double*)MALLOC(iCols * economyCols*(complexArg ? sizeof(doublecomplex): sizeof(double)));
+		    }
+		  else
+		    {
+		      iAllocMatrixOfDouble(Rhs+1, iRows, economyRows , &pU);
+		      iAllocMatrixOfDouble(Rhs+2, economyRows, economyCols, &pS);
+		      iAllocMatrixOfDouble(Rhs+3, iCols, economyCols , &pV);
+		    }
+		 
+		  break;
+		}
+	      case 2:
+		{
+		  Scierror(999,_("%s: Wrong number of output arguments.\n"),fname);
+		  break;
+		}
+	      case 1:
+		{
+		  iAllocMatrixOfDouble(Rhs+1, Min(iRows, iCols), 1, &pSV);
+		  break;
+		}
+	      }
+	      ret=  iSvdM(pData, iRows, iCols, complexArg, economy, tol, pSV, pU, pS, pV, pRk);
+	      if(ret){
+		if( ret == -1)
+		  {
+		    Scierror(999,_("%s: Cannot allocate more memory.\n"),fname);
+		  }
+		else
+		  {
+		    Scierror(999,_("Convergence problem...\n"));
+		  }
+	      }
+	      else
+		{
+		  if(complexArg)
+		    {
+		      vFreeDoubleComplexFromPointer((doublecomplex*)pData);
+		      if(Lhs != 1)
+			{
+			  {/* multicore: omp sections */
+			    vGetPointerFromDoubleComplex((doublecomplex*)pU, iRows* (economy ? Min(iRows, iCols) : iRows), pUReal, pUImg);
+			    FREE(pU);
+			  }
+			  {
+			    vGetPointerFromDoubleComplex((doublecomplex*)pV, iCols* (economy ? Min(iRows, iCols) : iCols), pVReal, pVImg);
+			    FREE(pV);
+			  }
+			}
+		    }
+		  switch(Lhs)
+		    {
+		    case 4: LhsVar(4)= Rhs + 4; /* no break */
+		    case 3:
+		      {
+			LhsVar(3)= Rhs + 3;
+			LhsVar(2)= Rhs + 2;
+		      } /* no break */
+		    case 1: LhsVar(1)= Rhs + 1 ;
+		    }
+		}
+	    }
+	}
+    }
+  return ret;
 }
 /*--------------------------------------------------------------------------*/
