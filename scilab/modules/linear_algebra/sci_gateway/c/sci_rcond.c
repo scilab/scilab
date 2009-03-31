2703d4bd56e8d5c629ad26995e8d390dca114946
Bernard HUGUENEY <bernard.hugueney@scilab.org>
2009-03-31 16:40:29 +0200

    SASification of linear_algebra rcond

--- scilab/modules/linear_algebra/sci_gateway/c/sci_rcond.c
+++ scilab/modules/linear_algebra/sci_gateway/c/sci_rcond.c
@@ -1,7 +1,7 @@
 
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
- * Copyright (C) ????-2008 - INRIA
+ * Copyright (C) ????-2009 - INRIA
  *
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
@@ -10,47 +10,70 @@
  * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
  *
  */
-
-#include <string.h>
-#include <stdio.h>
 #include "stack-c.h"
+#include "stack3.h"
 #include "gw_linear_algebra.h"
 #include "Scierror.h"
 #include "localization.h"
-/*--------------------------------------------------------------------------*/
-extern int C2F(intdgecon)(char *fname, unsigned long fname_len);
-extern int C2F(intzgecon)(char *fname, unsigned long fname_len);
-/*--------------------------------------------------------------------------*/
+
+#include "rcond.h"
+
 int C2F(intrcond)(char *fname,unsigned long fname_len)
 {
-	int *header1;
-	int CmplxA;int ret;
-
-	/*   rcond(A)  */
-	if (GetType(1)!=sci_matrix) 
-	{
-		OverLoad(1);
-		return 0;
-	}
-
-	header1 = (int *) GetData(1);
-	CmplxA=header1[3];
-
-	switch (CmplxA) 
-	{
-		case REAL:
-			ret = C2F(intdgecon)("rcond",5L);
-		break;
-
-		case COMPLEX:
-			ret = C2F(intzgecon)("rcond",5L);
-		break;
-
-		default:
-			Scierror(999,_("%s: Wrong type for input argument #%d: Real or Complex matrix expected.\n"),
-			fname,1);
-		break;
-	}
-	return 0;
+  int ret= 0;
+  /*   rcond(A)  */
+  if ( (Rhs>=1) && (GetType(1)!=sci_matrix) )
+    {
+      OverLoad(1);
+      return 0;
+    }
+  
+  CheckRhs(1,1);
+  CheckLhs(1,1);
+  {
+    double* pData;
+    double* pDataReal;
+    double* pDataImg;
+    int iRows, iCols;
+    int complexArg;
+    if( (complexArg= iIsComplex(1)) )
+      {
+	GetRhsVarMatrixComplex(1, &iRows, &iCols, &pDataReal, &pDataImg);
+	/* c -> z */
+	pData=(double*)oGetDoubleComplexFromPointer( pDataReal, pDataImg, iRows * iCols);
+	if(!pData)
+	  {
+	    Scierror(999,_("%s: Cannot allocate more memory.\n"),fname);
+	    ret = 1;
+	  }
+      }
+    else
+      {
+	GetRhsVarMatrixDouble(1, &iRows, &iCols, &pData);
+      }
+    if( iRows != iCols)
+      {
+	Scierror(999,_("%s: Wrong type for input argument #%d: Square matrix expected.\n"), fname, 1);
+	ret= 1;
+      }
+    else
+      {
+	double* pRcond;
+	int dim= iRows ? 1 : 0 ;
+	iAllocMatrixOfDouble(2, dim, dim, &pRcond);
+	if(iRows)
+	  {
+	    if( iRows == -1 )
+	      {
+		*pRcond= 1.;
+	      }
+	    else
+	      {
+		ret= iRcondM(pData, iCols, complexArg, pRcond);
+	      }
+	  }
+	LhsVar(1)= 2;
+      }
+  }
+  return ret;
 }
-/*--------------------------------------------------------------------------*/
