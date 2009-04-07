db25eddf075e2b24edd275320c86f878baf5bf25
antoine ELIAS <antoine.elias@scilab.org>
2009-04-07 13:27:06 +0200

    move configvariable from core_yasp to system_env

---
+++ scilab/modules/system_env/includes/configvariable.hxx
@@ -0,0 +1,43 @@
+/*
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2008-2008 - DIGITEO - Antoine ELIAS
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
+#include <map>
+#include <string>
+
+#ifdef _MSC_VER
+	#if SYSTEM_ENV_EXPORTS
+		#define EXTERN_SYSTEM_ENV __declspec (dllexport)
+	#else
+		#define EXTERN_SYSTEM_ENV __declspec (dllimport)
+	#endif
+#else
+	#define EXTERN_SYSTEM_ENV 
+#endif
+
+class EXTERN_SYSTEM_ENV ConfigVariable
+{
+private :
+  ConfigVariable(){};
+  ~ConfigVariable(){};
+
+  static ConfigVariable *me;
+
+  std::map<std::string, std::string>m_VarList;
+public :
+
+  static ConfigVariable* getInstance();
+
+  std::string	get(std::string _szVar);
+  bool		set(std::string _szVar, std::string _szVal);
+
+};
+
---
+++ scilab/modules/system_env/src/cpp/configvariable.cpp
@@ -0,0 +1,47 @@
+/*
+*  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+*  Copyright (C) 2008-2008 - DIGITEO - Antoine ELIAS
+*
+*  This file must be used under the terms of the CeCILL.
+*  This source file is licensed as described in the file COPYING, which
+*  you should have received as part of this distribution.  The terms
+*  are also available at
+*  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+*
+*/
+
+#include "configvariable.hxx"
+
+ConfigVariable* ConfigVariable::me;
+
+ConfigVariable* ConfigVariable::getInstance()
+{
+	if (me == NULL)
+	{
+		me = new ConfigVariable();
+	}
+	return me;
+}
+
+std::string ConfigVariable::get(std::string _szVar)
+{
+	std::map<std::string, std::string>::const_iterator it;
+	it = m_VarList.find(_szVar);
+	if(it != m_VarList.end())
+	{
+		return it->second;
+	}
+	else
+	{
+		return "";
+	}
+}
+
+bool ConfigVariable::set(std::string _szVar, std::string _szVal)
+{
+	bool bReturn = true;
+	//std::map <std::string, std::string> ::const_iterator it;
+	m_VarList.insert(std::pair<std::string,std::string>(_szVar,_szVal));
+	//m_VarList[_szVar] = _szVal;
+	return bReturn;
+}
