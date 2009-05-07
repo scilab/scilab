bd72588bbc8450466ae0c960121ea3ad89d7f666
Allan CORNET <allan.cornet@scilab.org>
2009-05-07 14:05:57 +0200

    remove depencendency between some modules on polynomials

---
+++ scilab/modules/polynomials/src/c/DllmainPolynomial.c
@@ -0,0 +1,45 @@
+
+/*
+* Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+* Copyright (C) 2009 - DIGITEO - Allan CORNET
+*
+* This file must be used under the terms of the CeCILL.
+* This source file is licensed as described in the file COPYING, which
+* you should have received as part of this distribution.  The terms
+* are also available at
+* http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+*
+*/
+
+#include <windows.h>
+#include "machine.h"
+/*--------------------------------------------------------------------------*/
+#pragma comment(lib,"../../../../bin/libintl.lib")
+/*--------------------------------------------------------------------------*/
+/* COMMON used by fortran */
+typedef struct{
+	double p[101], qp[101], k[101], qk[101], svk[101], sr, si, u, v, a, b,
+		c__, d__, a1, a2, a3, a6, a7, e, f, g, h__, szr, szi, lzr, lzi;
+	float eta, are, mre;
+	int n, nn;
+} GLOGLO_struct;
+
+__declspec (dllexport) GLOGLO_struct C2F(gloglo);
+/*--------------------------------------------------------------------------*/
+int WINAPI DllMain (HINSTANCE hInstance , DWORD reason, PVOID pvReserved)
+{
+	switch (reason) 
+	{
+	case DLL_PROCESS_ATTACH:
+		break;
+	case DLL_PROCESS_DETACH:
+		break;
+	case DLL_THREAD_ATTACH:
+		break;
+	case DLL_THREAD_DETACH:
+		break;
+	}
+	return 1;
+}
+/*--------------------------------------------------------------------------*/ 
+
