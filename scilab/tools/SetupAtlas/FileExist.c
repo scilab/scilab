caf61dd2990c50f1be20f66675007e380ef2d482
Allan CORNET <allan.cornet@scilab.org>
2009-06-01 17:17:14 +0200

    rewrite to manage xml file & char encoding on filename

---
+++ scilab/tools/SetupAtlas/FileExist.c
@@ -0,0 +1,29 @@
+/*
+* Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+* Copyright (C) 2009 - DIGITEO - Allan CORNET
+* 
+* This file must be used under the terms of the CeCILL.
+* This source file is licensed as described in the file COPYING, which
+* you should have received as part of this distribution.  The terms
+* are also available at    
+* http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+*
+*/
+/*--------------------------------------------------------------------------*/
+#include <windows.h>
+#include "FileExist.h"
+/*--------------------------------------------------------------------------*/
+BOOL FileExist(wchar_t  *wcfilename)
+{
+	BOOL exist = FALSE;	
+
+	WIN32_FIND_DATAW FindFileData;
+	HANDLE handle = FindFirstFileW (wcfilename, &FindFileData);
+	if (handle != INVALID_HANDLE_VALUE)
+	{
+		FindClose (handle);
+		return TRUE;
+	}
+	return FALSE;
+}
+/*--------------------------------------------------------------------------*/
