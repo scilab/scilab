cbab00f576562406bf9f37fc8dfc759f48b87d2d
antoine ELIAS <antoine.elias@scilab.org>
2009-06-11 17:53:46 +0200

    fix offset in stack

--- scilab/modules/core/src/c/list_api.c
+++ scilab/modules/core/src/c/list_api.c
@@ -663,7 +663,7 @@ int createMatrixOfStringInList(int _iVar, int* _piParent, int _iItemPos, int _iR
 		return 1;
 	}
 
-	piEnd = piItemAddr + iTotalLen;
+	piEnd = piItemAddr + iTotalLen + 5 + _iRows * _iCols + !((_iRows * _iCols) % 2);
 	closeList(iNewPos, _piParent, _iItemPos, piEnd);
 	return 0;
 }
@@ -696,11 +696,10 @@ int fillCommonMatrixOfStringInList(int _iVar, int* _piParent, int _iItemPos, int
 		return 1;
 	}
 
-	*_piTotalLen				+= 4 + _iRows * _iCols + 1 + 1;//+ 1 for scale on double precision
 	piOffset						= _piParent + 2;
-	piOffset[_iItemPos] = piOffset[_iItemPos - 1] + (*_piTotalLen / 2);
+	piOffset[_iItemPos] = piOffset[_iItemPos - 1] + (*_piTotalLen + 5 + _iRows * _iCols + !((_iRows * _iCols) %2)) / 2;
 
-	piEnd = piAddr + *_piTotalLen;
+	piEnd = piAddr + (*_piTotalLen + 5 + _iRows * _iCols + !((_iRows * _iCols) %2));
 	if(_iItemPos == iNbItem)
 	{
 		updateOffset(_iVar, _piParent, _iItemPos, piEnd);
@@ -732,7 +731,7 @@ int createMatrixOfStringInNamedList(char* _pstName, int _iNameLen, int* _piParen
 		return 1;
 	}
 
-	piEnd = piItemAddr + iTotalLen;
+	piEnd = piItemAddr + iTotalLen + 5 + _iRows * _iCols;
 	closeList(Top, _piParent, _iItemPos, piEnd);
 
 	Top = iSaveTop;
--- scilab/modules/fileio/sci_gateway/cpp/sci_import_from_hdf5.cpp
+++ scilab/modules/fileio/sci_gateway/cpp/sci_import_from_hdf5.cpp
@@ -26,6 +26,11 @@ extern "C"
 #include "h5_readDataFromFile.h"
 }
 
+int iTab = 0;
+
+
+void prtinf_tree(char* _pstMsg);
+
 int	import_data(int _iDatasetId, int _iItemPos, int* _piAddress);
 int import_double(int _iDatasetId, int _iItemPos, int* _piAddress);
 int import_string(int _iDatasetId, int _iItemPos, int* _piAddress);
@@ -33,10 +38,10 @@ int import_list(int _iDatasetId, int _iVarType, int _iItemPos, int* _piAddress);
 
 int sci_import_from_hdf5(char *fname,unsigned long fname_len)
 {
-	int* piAddr = NULL;
-	int i1 = *Lstk(2) - *Lstk(1);
+	int i1 = *Lstk(Rhs) - *Lstk(Rhs - 1);
 	sciprint("1 -- diff : %d\n", i1);
 
+	//int* piAddr = NULL;
 	//double pdblData[] = {1,2,3,4,5,6,7,8,
 	//											1,2,3,4,5,6,7,8,
 	//											1,2,3,4,5,6,7,8,
@@ -57,6 +62,7 @@ int sci_import_from_hdf5(char *fname,unsigned long fname_len)
 	//								1,0,1,0,1,0,1,0,
 	//								1,0,1,0,1,0,1,0};
 
+	//int* piAddr = NULL;
 	//createMatrixOfBoolean(Rhs + 1, 8, 8, piBool, &piAddr);
 
 	//char **pstData = NULL;
@@ -68,20 +74,131 @@ int sci_import_from_hdf5(char *fname,unsigned long fname_len)
 	//}
 	//createMatrixOfString(Rhs + 1, 8, 1, pstData, &piAddr);
 
-	int* piAddrRoot			= NULL;
-	createList(Rhs + 1, 2, &piAddrRoot);
+	//int* piAddrRoot			= NULL;
+	//createList(Rhs + 1, 2, &piAddrRoot);
 
-	int* piAddr1				= NULL;
-	createListInList(Rhs + 1, piAddrRoot, 1, 1, &piAddr1);
+	//int* piAddr1				= NULL;
+	//createListInList(Rhs + 1, piAddrRoot, 1, 1, &piAddr1);
 
-	int* piAddr11				= NULL;
-	createListInList(Rhs + 1, piAddr1, 1, 1, &piAddr11);
+	//int* piAddr11				= NULL;
+	//createListInList(Rhs + 1, piAddr1, 1, 1, &piAddr11);
 
-	int* piAddr111			= NULL;
-	double iVal					= 1;
-	createMatrixOfDoubleInList(Rhs + 1, piAddr11, 1, 1, 1, &iVal);
+	//int* piAddr111			= NULL;
+	//double iVal					= 1;
+	//createMatrixOfDoubleInList(Rhs + 1, piAddr11, 1, 1, 1, &iVal);
 
-	createMatrixOfDoubleInList(Rhs + 1, piAddrRoot, 2, 1, 1, &iVal);
+	//createMatrixOfDoubleInList(Rhs + 1, piAddrRoot, 2, 1, 1, &iVal);
+
+	//int* piAddr					= NULL;
+
+	//int iRows						= 0;
+	//int iCols						= 0;
+
+	//int iNbItem					= 0;
+	//int* piNbItemRow		= NULL;
+	//int* piColPos				= NULL;
+
+	//double* pdblReal		= NULL;
+
+	//getVarAddressFromNumber(1, &piAddr);
+	//getSparseMatrix(piAddr, &iRows, &iCols, &iNbItem, &piNbItemRow, &piColPos, &pdblReal);
+
+	//double *pdblReal2  = NULL;
+	//pdblReal2 = (double*)malloc(sizeof(double) * iNbItem);
+
+	//for(int i = 0 ; i < iNbItem ; i++)
+	//{
+	//	pdblReal2[i] = pdblReal[i] * 2;
+	//}
+
+	//createSparseMatrix(Rhs + 1, iRows, iCols, iNbItem, piNbItemRow, piColPos, pdblReal2, &piAddr);
+
+	//char** pstA = (char**)malloc(sizeof(char*) * 4);
+	//for(int i = 0 ; i < 4 ; i++)
+	//{
+	//	pstA[i] = (char*)malloc(sizeof(char) * 5);
+	//	strcpy(pstA[i], "1234");
+	//}
+
+	//double* pdblB = (double*)malloc(sizeof(double) * 4);
+	//for(int i = 0 ; i < 4 ; i++)
+	//{
+	//	pdblB[i] = i + 1;
+	//}
+
+	//int* piListRoot				= NULL;
+	//createList(Rhs + 1, 3, &piListRoot);
+
+	//	int *piAddr1				= NULL;
+	//	createMatrixOfStringInList(Rhs + 1, piListRoot, 1, 2, 2, pstA, &piAddr1);
+	//	//createMatrixOfDoubleInList(Rhs + 1, piListRoot, 1, 2, 2, pdblB);
+
+	//	int* piList2				= NULL;
+	//	createListInList(Rhs + 1, piListRoot, 2, 2, &piList2);
+
+	//		//createMatrixOfDoubleInList(Rhs + 1, piList2, 1, 2, 2, pdblB);
+	//		int *piAddr21			= NULL;
+	//		createMatrixOfStringInList(Rhs + 1, piList2, 1, 2, 2, pstA, &piAddr21);
+	//		//int *piAddr22			= NULL;
+	//		//createMatrixOfStringInList(Rhs + 1, piList2, 2, 2, 2, pstA, &piAddr22);
+	//		createMatrixOfDoubleInList(Rhs + 1, piList2, 2, 2, 2, pdblB);
+
+	//	int* piList3				= NULL;
+	//	createListInList(Rhs + 1, piListRoot, 3, 1, &piList3);
+
+	//		int* piList31			= NULL;
+	//		createListInList(Rhs + 1, piList3, 1, 2, &piList31);
+
+	//			//createMatrixOfDoubleInList(Rhs + 1, piList31, 1, 2, 2, pdblB);
+	//			int *piAddr311	= NULL;
+	//			createMatrixOfStringInList(Rhs + 1, piList31, 1, 2, 2, pstA, &piAddr311);
+	//			//int *piAddr312	= NULL;
+	//			//createMatrixOfStringInList(Rhs + 1, piList31, 2, 2, 2, pstA, &piAddr312);
+	//			createMatrixOfDoubleInList(Rhs + 1, piList31, 2, 2, 2, pdblB);
+
+	//int i2 = *Lstk(Rhs + 2) - *Lstk(Rhs + 1);
+	//sciprint("2 -- diff : %d\n", i2);
+
+	//LhsVar(1) = Rhs + 1;
+	//PutLhsVar();
+	//return 0;
+	//CheckRhs(1,1);
+	//CheckLhs(1,1);
+
+	int iRows						= 0;
+	int iCols						= 0;
+	int iLen						= 0;
+	int* piAddr					= NULL;
+	char *pstVarName		= NULL;
+
+
+	getVarAddressFromNumber(1, &piAddr);
+
+	if(getVarType(piAddr) != sci_strings)
+	{
+		Scierror(999,_("%s: Wrong type for input argument #%d: A string.\n"),fname, 2);
+		return 0;
+	}
+
+	getVarDimension(piAddr, &iRows, &iCols);
+	if(iRows != 1 || iCols != 1)
+	{
+		Scierror(999,_("%s: Wrong size for input argument #%d: A string expected.\n"),fname,2);
+	}
+
+	getMatrixOfString(piAddr, &iRows, &iCols, &iLen, NULL);
+	pstVarName = (char*)MALLOC((iRows * iCols + 1) * sizeof(char));
+	getMatrixOfString(piAddr, &iRows, &iCols, &iLen, &pstVarName);
+
+	//open hdf5 file
+	int iFile = openHDF5File(pstVarName);
+	int iDataSetId = getDataSetId(iFile);
+
+	//import all data
+	import_data(iDataSetId, 0, NULL);
+
+	//close the file
+	closeHDF5File(iFile);
 
 	int i2 = *Lstk(Rhs + 2) - *Lstk(Rhs + 1);
 	sciprint("2 -- diff : %d\n", i2);
@@ -89,50 +206,6 @@ int sci_import_from_hdf5(char *fname,unsigned long fname_len)
 	LhsVar(1) = Rhs + 1;
 	PutLhsVar();
 	return 0;
-	//CheckRhs(1,1);
-	//CheckLhs(1,1);
-
-	//int iRows						= 0;
-	//int iCols						= 0;
-	//int iLen						= 0;
-	//int* piAddr					= NULL;
-	//char *pstVarName		= NULL;
-
-
-	//getVarAddressFromNumber(1, &piAddr);
-
-	//if(getVarType(piAddr) != sci_strings)
-	//{
-	//	Scierror(999,_("%s: Wrong type for input argument #%d: A string.\n"),fname, 2);
-	//	return 0;
-	//}
-
-	//getVarDimension(piAddr, &iRows, &iCols);
-	//if(iRows != 1 || iCols != 1)
-	//{
-	//	Scierror(999,_("%s: Wrong size for input argument #%d: A string expected.\n"),fname,2);
-	//}
-
-	//getMatrixOfString(piAddr, &iRows, &iCols, &iLen, NULL);
-	//pstVarName = (char*)MALLOC((iRows * iCols + 1) * sizeof(char));
-	//getMatrixOfString(piAddr, &iRows, &iCols, &iLen, &pstVarName);
-
-	////open hdf5 file
-	//int iFile = openHDF5File(pstVarName);
-	//int iDataSetId = getDataSetId(iFile);
-
-	////import all data
-	//import_data(iDataSetId, 0, NULL);
-
-	////close the file
-	//closeHDF5File(iFile);
-
-	//int iBool = 1;
-	//createMatrixOfBoolean(Rhs + 2, 1, 1, &iBool, &piAddr);
-	//LhsVar(1) = Rhs + 1;
-	//LhsVar(2) = Rhs + 2;
-	//PutLhsVar();
-	//return 0;
 }
 
 int	import_data(int _iDatasetId, int _iItemPos, int* _piAddress)
@@ -287,4 +360,15 @@ int import_list(int _iDatasetId, int _iVarType, int _iItemPos, int* _piAddress)
 	return 0;
 }
 
+void prtinf_tree(char* _pstMsg)
+{
+#ifdef PRINT_DEBUG
+	char pstMsg[515];
+	for(int i = 0 ; i < iTab ; i++)
+	{
+		sprintf(pstMsg + i, "\t");
+	}
+	sciprint("%s%s\n",pstMsg, _pstMsg);
+#endif
+}
 /*--------------------------------------------------------------------------*/
