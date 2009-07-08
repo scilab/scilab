eeeb4852343d42f88739227f8d9df7dec15b4f8c
Allan CORNET <allan.cornet@scilab.org>
2009-07-08 19:23:08 +0200

    add include

--- scilab/modules/output_stream/src/c/DllmainOutput_Stream.c
+++ scilab/modules/output_stream/src/c/DllmainOutput_Stream.c
@@ -9,6 +9,7 @@
  * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
  *
  */
+#include <stdio.h>
 #include <windows.h> 
 /*--------------------------------------------------------------------------*/ 
 #pragma comment(lib,"../../../../bin/libintl.lib")
