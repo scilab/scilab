87fafa35d989259492fc80dceb0d54b3482ac6ac
Bruno JOFRET <bruno.jofret@scilab.org>
2009-07-28 16:19:47 +0200

    adding tasks

---
+++ scilab/modules/core/includes/tasks.hxx
@@ -0,0 +1,60 @@
+/*
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2009-2009 - DIGITEO - Bruno JOFRET
+ * 
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ * 
+ */
+
+#ifndef __TASKS_HXX__
+#define __TASKS_HXX__
+
+#include "parser.hxx"
+
+/*
+** Parse
+**
+** Parse the given file and create the AST.
+*/
+Parser::ParserStatus parseFileTask(bool timed, const char* file_name, const char* prog_name);
+
+/*
+** Parse
+**
+** Parse the given command and create the AST.
+*/
+Parser::ParserStatus parseCommandTask(bool timed, char *command);
+
+/*
+** Dump AST
+**
+** Display the AST in human readable format.
+*/
+void dumpAstTask(bool timed);
+
+/*
+** Pretty Print
+**
+** Pretty print the Stored AST.
+*/
+void printAstTask(bool timed);
+
+/*
+** Exec Tree
+**
+** Execute the stored AST.
+*/
+void execAstTask(bool timed);
+
+/*
+** Dump Stack Trace
+**
+** Display what is stored in scilab.
+*/
+void dumpStackTask(bool timed);
+
+#endif /* !__TASKS_HXX__ */
