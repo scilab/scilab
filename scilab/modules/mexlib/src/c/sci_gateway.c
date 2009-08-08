4898e5380483d0d95aed60edd4f0fd8600871871
Allan CORNET <allan.cornet@scilab.org>
2009-08-08 23:18:38 +0200

    manages PutLhsVar as in scilab internal function

--- scilab/modules/mexlib/src/c/sci_gateway.c
+++ scilab/modules/mexlib/src/c/sci_gateway.c
@@ -1,6 +1,7 @@
 /*
 * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
 * Copyright (C) 2009 - DIGITEO - Sylvestre LEDRU
+* Copyright (C) 2009 - DIGITEO - Allan CORNET
 *
 * This file must be used under the terms of the CeCILL.
 * This source file is licensed as described in the file COPYING, which
@@ -9,14 +10,28 @@
 * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
 *
 */
+/*-----------------------------------------------------------------------------------*/
 #include <string.h>
 #include "stack-c.h"
 #include "sci_gateway.h"
-
+/*-----------------------------------------------------------------------------------*/
+static int callExternalFunction(char *fname, GatefuncS F,BOOL withPutLhsVar);
+/*-----------------------------------------------------------------------------------*/
 /** generic scilab interface **/
 int sci_gateway(char *fname, GatefuncS F)
 {
-  (*F)(fname,(int)strlen(fname));
-  PutLhsVar();
-  return 0;
+	return callExternalFunction(fname,F,TRUE);
 }
+/*-----------------------------------------------------------------------------------*/
+int sci_gateway_without_putlhsvar(char *fname, GatefuncS F)
+{
+	return callExternalFunction(fname,F,FALSE);
+}
+/*-----------------------------------------------------------------------------------*/
+static int callExternalFunction(char *fname, GatefuncS F,BOOL withPutLhsVar)
+{
+	(*F)(fname,(int)strlen(fname));
+	if (withPutLhsVar)PutLhsVar();
+	return 0;
+}
+/*-----------------------------------------------------------------------------------*/
