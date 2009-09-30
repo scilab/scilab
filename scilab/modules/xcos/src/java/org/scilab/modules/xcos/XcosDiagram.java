b65241faf14186e43c0c0e68f23a462542991938
Bruno JOFRET <bruno.jofret@scilab.org>
2009-09-30 14:49:12 +0200

    Remove debug message

--- scilab/modules/xcos/src/java/org/scilab/modules/xcos/XcosDiagram.java
+++ scilab/modules/xcos/src/java/org/scilab/modules/xcos/XcosDiagram.java
@@ -389,8 +389,6 @@ public class XcosDiagram extends ScilabGraph {
 	// Go over all blocks to dump it inside Scilab Structure
 	for (int i = 0 ; i < blockList.size() ; ++i) {
 	    blockList.get(i).setOrdering(i + 1);
-	    System.out.println(blockList.get(i).getGeometry().getX());
-	    System.out.println(blockList.get(i).getGeometry().getY());
 	    data.add(blockList.get(i).getAsScilabObj());
 	}
 
