21e6e0db5b2a6b737ddb0b69fce58603a194c66d
Bruno JOFRET <bruno.jofret@scilab.org>
2009-10-05 16:56:02 +0200

    create SuperBlock

---
+++ scilab/modules/xcos/src/java/org/scilab/modules/xcos/block/SuperBlock.java
@@ -0,0 +1,14 @@
+package org.scilab.modules.xcos.block;
+
+public class SuperBlock extends BasicBlock {
+
+       
+    public SuperBlock(String label) {
+	super(label);
+    }
+
+    public void openBlockSettings() {
+	System.err.println("[DEBUG] I am a superBlock, open me using cryptonyte !!!");
+    }
+    
+}
