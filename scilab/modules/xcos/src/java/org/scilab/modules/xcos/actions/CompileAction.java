fa3a92e444df122da95c0061b70875ea3913977c
Yann Collette <yann.collette@scilab.org>
2009-10-06 10:03:42 +0200

    add the compile funcionnality to xcos

--- scilab/modules/xcos/src/java/org/scilab/modules/xcos/actions/CompileAction.java
+++ scilab/modules/xcos/src/java/org/scilab/modules/xcos/actions/CompileAction.java
@@ -12,10 +12,16 @@
 
 package org.scilab.modules.xcos.actions;
 
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.IOException;
+
+import org.scilab.modules.action_binding.InterpreterManagement;
 import org.scilab.modules.graph.ScilabGraph;
 import org.scilab.modules.graph.actions.DefaultAction;
 import org.scilab.modules.gui.menuitem.MenuItem;
 import org.scilab.modules.gui.pushbutton.PushButton;
+import org.scilab.modules.xcos.XcosDiagram;
 import org.scilab.modules.xcos.utils.XcosMessages;
 
 
@@ -31,5 +37,18 @@ public class CompileAction extends DefaultAction {
 
 	public static MenuItem createMenu(ScilabGraph scilabGraph) {
 		return createMenu(XcosMessages.COMPILE, null, new CompileAction(scilabGraph), null);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		File temp;
+		try {
+			temp = File.createTempFile("xcos",".hdf5");
+			temp.delete();
+			((XcosDiagram) getGraph(e)).dumpToHdf5File(temp.getAbsolutePath());
+			InterpreterManagement.requestScilabExec("import_from_hdf5(\""+temp.getAbsolutePath()+"\");xcos_compile(scs_m);");
+			temp.deleteOnExit();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
 	}
 }
