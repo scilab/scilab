1edb554dca4b18c0714ec6a2c62deb3284a895e6
Bruno JOFRET <bruno.jofret@scilab.org>
2009-10-08 09:38:11 +0200

    Open SuperBlock

--- scilab/modules/xcos/src/java/org/scilab/modules/xcos/block/SuperBlock.java
+++ scilab/modules/xcos/src/java/org/scilab/modules/xcos/block/SuperBlock.java
@@ -1,14 +1,30 @@
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2009 - DIGITEO - Bruno JOFRET
+ *
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
 package org.scilab.modules.xcos.block;
+
+import org.scilab.modules.hdf5.scilabTypes.ScilabMList;
+import org.scilab.modules.xcos.Xcos;
+import org.scilab.modules.xcos.XcosDiagram;
 
 public class SuperBlock extends BasicBlock {
 
-       
     public SuperBlock(String label) {
 	super(label);
     }
 
     public void openBlockSettings() {
-	System.err.println("[DEBUG] I am a superBlock, open me using cryptonyte !!!");
+	XcosDiagram xcosDiagram = Xcos.CreateAndShowGui();
+	xcosDiagram.loadDiagram(BlockReader.convertMListToDiagram((ScilabMList) getRealParameters()));
     }
     
 }
