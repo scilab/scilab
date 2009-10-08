11cc9895d2c7004d1fba4326662a5a7485272628
Vincent COUVERT <vincent.couvert@scilab.org>
2009-10-08 13:30:00 +0200

    Missing file

---
+++ scilab/modules/xcos/src/java/org/scilab/modules/xcos/actions/CloseViewportAction.java
@@ -0,0 +1,60 @@
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2009 - DIGITEO - Vincent COUVERT
+ * 
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at    
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
+package org.scilab.modules.xcos.actions;
+
+import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+
+import javax.swing.KeyStroke;
+
+import org.scilab.modules.graph.ScilabGraph;
+import org.scilab.modules.graph.actions.DefaultAction;
+import org.scilab.modules.gui.menuitem.MenuItem;
+import org.scilab.modules.xcos.XcosDiagram;
+import org.scilab.modules.xcos.utils.XcosMessages;
+
+/**
+ * Close action for viewport tab
+ * @author Vincent COUVERT
+ *
+ */
+public class CloseViewportAction extends DefaultAction {
+
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Constructor
+	 * @param scilabGraph associated Scilab Graph
+	 */
+	public CloseViewportAction(ScilabGraph scilabGraph) {
+		super(XcosMessages.CLOSE, scilabGraph);
+	}
+
+	/**
+	 * Create the close menu for the menu bar 
+	 * @param scilabGraph associated Scilab Graph
+	 * @return the menu
+	 */
+	public static MenuItem createMenu(ScilabGraph scilabGraph) {
+		return createMenu(XcosMessages.CLOSE, null, new CloseViewportAction(scilabGraph),
+				KeyStroke.getKeyStroke(KeyEvent.VK_W, Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()));
+	}
+
+	/**
+	 * Action !!
+	 * @see org.scilab.modules.graph.actions.DefaultAction#doAction()
+	 */
+	public void doAction() {
+		((XcosDiagram) getGraph(null)).setViewPortVisible(false);
+	}
+}
