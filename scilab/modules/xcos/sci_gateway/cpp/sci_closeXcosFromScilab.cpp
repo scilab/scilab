a9f27a6fde087968e87a11b697824c4dd17a9716
Vincent COUVERT <vincent.couvert@scilab.org>
2009-11-06 09:11:50 +0100

    Missing file

---
+++ scilab/modules/xcos/sci_gateway/cpp/sci_closeXcosFromScilab.cpp
@@ -0,0 +1,42 @@
+/*
+* Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+* Copyright (C) 2009 - DIGITEO - Vincent COUVERT
+*
+* This file must be used under the terms of the CeCILL.
+* This source file is licensed as described in the file COPYING, which
+* you should have received as part of this distribution.  The terms
+* are also available at
+* http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+*
+*/
+/*--------------------------------------------------------------------------*/
+#include "Xcos.hxx"
+
+extern "C"
+{
+#include "gw_xcos.h"
+#include "stack-c.h"
+#include "callxcos.h"
+#include "api_common.h"
+#include "api_string.h"
+#include "localization.h"
+#include "Scierror.h"
+#include "MALLOC.h"
+#include "freeArrayOfString.h"
+#include "getScilabJavaVM.h"
+}
+/*--------------------------------------------------------------------------*/
+using namespace org_scilab_modules_xcos;
+/*--------------------------------------------------------------------------*/
+int sci_closeXcosFromScilab(char *fname,unsigned long fname_len)
+{
+	CheckRhs(0,0);
+	CheckLhs(0,1);
+
+	Xcos::closeXcosFromScilab(getScilabJavaVM());
+
+	LhsVar(1) = 0;
+	PutLhsVar();
+	return 0;
+}
+/*--------------------------------------------------------------------------*/
