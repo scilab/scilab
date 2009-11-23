9ce7a5f727f6b9ec141dc66fc029b3c37bb770bc
Clément DAVID <clement.david@scilab.org>
2009-11-23 17:14:06 +0100

    Removing partial undo

--- scilab/modules/xcos/src/java/org/scilab/modules/xcos/XcosDiagram.java
+++ scilab/modules/xcos/src/java/org/scilab/modules/xcos/XcosDiagram.java
@@ -30,6 +30,7 @@ import java.util.List;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
+import javax.swing.undo.UndoManager;
 
 import org.scilab.modules.graph.ScilabGraph;
 import org.scilab.modules.graph.actions.PasteAction;
@@ -128,7 +129,7 @@ public class XcosDiagram extends ScilabGraph {
     private SetContextAction action;
     
     /** Undo counter (0 is previous saved state) */
-    private int undo_counter = 0;
+    private int undo_counter = 1;
     
     protected mxIEventListener undoEnabler = new mxIEventListener()
     {
@@ -1602,8 +1603,6 @@ public class XcosDiagram extends ScilabGraph {
 	public void undo() {
 		super.undo();
 		
-		increment_undo();
-		
 		if (getParentTab() != null) {
 			if (undoManager.canUndo()) {
 				((Xcos) getParentTab()).setEnabledUndo(true);
@@ -1625,8 +1624,6 @@ public class XcosDiagram extends ScilabGraph {
 	 */
 	public void redo() {
 		super.redo();
-		
-		increment_redo();
 		
 		if (getParentTab() != null) {
 			if (undoManager.canUndo()) {
@@ -1669,26 +1666,5 @@ public class XcosDiagram extends ScilabGraph {
 	public SetContextAction getContextAction() {
 		return action;
 	}
-	
-    private void increment_undo() {
-	if (undo_counter >= Integer.MIN_VALUE) {
-	    undo_counter--;
-	    if (undo_counter == 0)
-		setModified(false);
-	    else
-		setModified(true);
-	}
-    }
-
-    private void increment_redo() {
-	if (undo_counter <= Integer.MAX_VALUE) {
-	    undo_counter++;
-	    if (undo_counter == 0)
-		setModified(false);
-	    else
-		setModified(true);
-	}
-    }
-
 }
 
