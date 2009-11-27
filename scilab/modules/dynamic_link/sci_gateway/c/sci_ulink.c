21434b302c1a7f6e4d1562561a6e1acae96f4b8e
Yann Collette <yann.collette@scilab.org>
2009-11-27 16:42:07 +0100

    forbide the ulink when we are using valgrind - C version

--- scilab/modules/dynamic_link/sci_gateway/c/sci_ulink.c
+++ scilab/modules/dynamic_link/sci_gateway/c/sci_ulink.c
@@ -12,6 +12,7 @@
 
 /*--------------------------------------------------------------------------*/
 #include <string.h>
+#include <stdlib.h>
 #include "gw_dynamic_link.h"
 #include "stack-c.h"
 #include "localization.h"
@@ -21,33 +22,39 @@
 int sci_ulink(char *fname,unsigned long fname_len)
 {
 	static int l1,n1,m1;
+	char * ValgrindEnv = NULL;
 
 	Rhs = Max(0, Rhs);
 	CheckRhs(0,1);
 	CheckLhs(1,1);
 
-	if (Rhs == 0)
+	ValgrindEnv = getenv("PROFILE_SCILAB_DYNAMIC_LINK");
+
+	if (ValgrindEnv==NULL)
 	{
-		unlinkallsharedlib();
-	}
-	else
-	{
-		if ( GetType(1) == sci_matrix )
-		{
-			double *params=NULL;
-			int i=0;
-			GetRhsVar(1,MATRIX_OF_DOUBLE_DATATYPE,&m1,&n1,&l1);
-			params=stk(l1);
-			for(i=0;i<m1*n1;i++)
-			{
-				int ilib = (int) params[i];
-				unlinksharedlib(&ilib);
-			}
+		if (Rhs == 0)
+		{	
+			unlinkallsharedlib();
 		}
 		else
 		{
-			Scierror(999,_("%s: Wrong type for input argument #%d: Integer expected.\n"), fname,1);
-			return 0;
+			if ( GetType(1) == sci_matrix )
+			{
+				double *params=NULL;
+				int i=0;
+				GetRhsVar(1,MATRIX_OF_DOUBLE_DATATYPE,&m1,&n1,&l1);
+				params=stk(l1);
+				for(i=0;i<m1*n1;i++)
+				{
+					int ilib = (int) params[i];
+					unlinksharedlib(&ilib);
+				}
+			}
+			else
+			{
+				Scierror(999,_("%s: Wrong type for input argument #%d: Integer expected.\n"), fname,1);
+				return 0;
+			}
 		}
 	}
 
