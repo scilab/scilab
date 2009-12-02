50affd3c2a53c8704faeccd61d90b95cef819fed
Clément DAVID <clement.david@scilab.org>
2009-12-02 12:22:25 +0100

    Revert "Remove TEXT_f in the palette (use double click on diagram background)"
    This reverts commit 3607d6b03fe59f4f6e7dab2183a5e5cb7a440366.

--- scilab/modules/xcos/src/java/org/scilab/modules/xcos/palette/XcosPaletteManager.java
+++ scilab/modules/xcos/src/java/org/scilab/modules/xcos/palette/XcosPaletteManager.java
@@ -67,7 +67,7 @@ public final class XcosPaletteManager {
 			    "CONVERT", "CSCOPXY", "DEMUX", "DOLLAR_f",
 			    "INTEGRAL_f", "IN_f", "LOGICAL_OP", "MUX",
 			    "NRMSOM_f", "OUT_f", "PRODUCT", "RELATIONALOP",
-			    "SATURATION", "SWITCH2_m" }),
+			    "SATURATION", "SWITCH2_m", "TEXT_f" }),
 
 	    /** CONTINUOUS palette */
 	    new PaletteStringDescriptor(XcosMessages.CONTINUOUS_PAL,
@@ -162,7 +162,7 @@ public final class XcosPaletteManager {
 
 	    /** ANNOTATIONS palette */
 	    new PaletteStringDescriptor(XcosMessages.ANNOTATIONS_PAL,
-		    new String[] {}),
+		    new String[] { "TEXT_f" }),
 
 	    /** SINKS palette */
 	    new PaletteStringDescriptor(XcosMessages.SINKS_PAL, new String[] {
