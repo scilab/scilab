ebff9490b9bfb8b73a74eecffab587277103a05f
Yann Collette <yann.collette@scilab.org>
2009-12-03 16:25:06 +0100

    fix a memory leak

--- scilab/modules/localization/sci_gateway/c/sci_gettext.c
+++ scilab/modules/localization/sci_gateway/c/sci_gettext.c
@@ -26,52 +26,59 @@ int sci_gettext(char *fname,unsigned long fname_len)
 		{
 			static int l1,n1,m1;
 
-			char *msgid=NULL;
-			char *TranslatedString=NULL;
+			char *msgid1=NULL;
+			char *msgid2=NULL;
+			char *TranslatedString1=NULL;
+			char *TranslatedString2=NULL;
                         
                         int revertStrsub = FALSE;
                         
 			GetRhsVar(1,STRING_DATATYPE,&m1,&n1,&l1);
-			msgid=cstk(l1);
+			msgid1=cstk(l1);
 
 			/* This stupid stuff is necessary because scilab is add slashes
 			 * and we need to remove them
 			 * An other solution might be to replace the string "\x" by it
 			 * real code 
 			*/
-			if (strchr(msgid, '\\')!=NULL){
+			if (strchr(msgid1, '\\')!=NULL){
 				/* There is an \ in the string process to replace */
 				
 				/* We always have something from this functions because gettext
 				 * is returning the same string if it cannot find it */
 
-				msgid=strsub(msgid, "\\n", "\n"); /* linefeed */
-				msgid=strsub(msgid, "\\t", "\t"); /* horizontal tab */
-				msgid=strsub(msgid, "\\r", "\r"); /* carriage return */
-				msgid=strsub(msgid, "\\v", "\v"); /* vertical tab */
-				msgid=strsub(msgid, "\\f", "\f"); /* form feed */
-				msgid=strsub(msgid, "\\\\", "\\"); /* backslash */
-				msgid=strsub(msgid, "\\\"", "\""); /* double quote */
+			        /* the Scilab strsub function allocates memory */
+
+				msgid2=strsub(msgid1, "\\n", "\n");                /* linefeed */
+				msgid1=strsub(msgid2, "\\t", "\t");  FREE(msgid2); /* horizontal tab */
+				msgid2=strsub(msgid1, "\\r", "\r");  FREE(msgid1); /* carriage return */
+				msgid1=strsub(msgid2, "\\v", "\v");  FREE(msgid2); /* vertical tab */
+				msgid2=strsub(msgid1, "\\f", "\f");  FREE(msgid1); /* form feed */
+				msgid1=strsub(msgid2, "\\\\", "\\"); FREE(msgid2); /* backslash */
+				msgid2=strsub(msgid1, "\\\"", "\""); FREE(msgid1); /* double quote */
+				msgid1=msgid2;
 
                                 revertStrsub = TRUE;
 			}
-			TranslatedString=gettext(msgid);
+			TranslatedString1=gettext(msgid1);
 
                         /* Add removed slashes */
                         if (revertStrsub)
                           {
-                            TranslatedString=strsub(TranslatedString, "\\", "\\\\"); /* backslash */
-                            TranslatedString=strsub(TranslatedString, "\"", "\\\""); /* double quote */
-                            TranslatedString=strsub(TranslatedString, "\n", "\\n"); /* linefeed */
-                            TranslatedString=strsub(TranslatedString, "\t", "\\t"); /* horizontal tab */
-                            TranslatedString=strsub(TranslatedString, "\r", "\\r"); /* carriage return */
-                            TranslatedString=strsub(TranslatedString, "\v", "\\v"); /* vertical tab */
-                            TranslatedString=strsub(TranslatedString, "\f", "\\f"); /* form feed */
+                            TranslatedString2=strsub(TranslatedString1, "\\", "\\\\"); FREE(TranslatedString1); /* backslash */
+                            TranslatedString1=strsub(TranslatedString2, "\"", "\\\""); FREE(TranslatedString2); /* double quote */
+                            TranslatedString2=strsub(TranslatedString1, "\n", "\\n");  FREE(TranslatedString1); /* linefeed */
+                            TranslatedString1=strsub(TranslatedString2, "\t", "\\t");  FREE(TranslatedString2); /* horizontal tab */
+                            TranslatedString2=strsub(TranslatedString1, "\r", "\\r");  FREE(TranslatedString1); /* carriage return */
+                            TranslatedString1=strsub(TranslatedString2, "\v", "\\v");  FREE(TranslatedString2); /* vertical tab */
+                            TranslatedString2=strsub(TranslatedString1, "\f", "\\f");  FREE(TranslatedString1); /* form feed */
+			    TranslatedString1=TranslatedString2;
                           }
 
 			n1=1;
-			CreateVarFromPtr(Rhs+1,STRING_DATATYPE,(m1=(int)strlen(TranslatedString), &m1),&n1,&TranslatedString);
+			CreateVarFromPtr(Rhs+1,STRING_DATATYPE,(m1=(int)strlen(TranslatedString1), &m1),&n1,&TranslatedString1);
 			LhsVar(1)=Rhs+1;
+
 			C2F(putlhsvar)();
 			return 0;
 		}
