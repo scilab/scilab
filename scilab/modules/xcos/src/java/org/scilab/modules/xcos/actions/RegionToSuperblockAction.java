30651e795ddd830bcd271b321f2b017c9bca8558
Clément DAVID <clement.david@scilab.org>
2009-12-08 10:12:52 +0100

    Remov unused import

--- scilab/modules/xcos/src/java/org/scilab/modules/xcos/actions/RegionToSuperblockAction.java
+++ scilab/modules/xcos/src/java/org/scilab/modules/xcos/actions/RegionToSuperblockAction.java
@@ -17,7 +17,6 @@ import java.util.Arrays;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import org.scilab.modules.graph.ScilabGraph;
 import org.scilab.modules.graph.actions.DefaultAction;
