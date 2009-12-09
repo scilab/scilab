45292dd5c2b12345b15024b4f8246edbdfc9fc30
Clément DAVID <clement.david@scilab.org>
2009-12-09 14:33:40 +0100

    Backward compatibility: if the 4th child of a splitblock doesn't exist, create it.

--- scilab/modules/xcos/src/java/org/scilab/modules/xcos/io/BasicPortCodec.java
+++ scilab/modules/xcos/src/java/org/scilab/modules/xcos/io/BasicPortCodec.java
@@ -15,15 +15,19 @@ package org.scilab.modules.xcos.io;
 
 import java.util.Map;
 
+import org.scilab.modules.xcos.block.BasicBlock;
+import org.scilab.modules.xcos.block.SplitBlock;
 import org.scilab.modules.xcos.port.BasicPort;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
 import com.mxgraph.io.mxCodec;
+import com.mxgraph.model.mxCell;
 
 public class BasicPortCodec extends XcosObjectCodec {
 
     private static final String DATA_TYPE = "dataType";
+    private BasicBlock previousBlock;
 
     public BasicPortCodec(Object template) {
 	super(template);
@@ -54,6 +58,24 @@ public class BasicPortCodec extends XcosObjectCodec {
 
 	//update style to replace direction by rotation
 	((BasicPort)obj).setStyle(formatStyle(((Element) node).getAttribute(STYLE), (BasicPort) obj));
+	
+	// SplitBlock specific: a splitbloc must have 4 children even if only 
+	// the first tree are used. In the old format the splitblock had only
+	// 3 children.
+	/* FIXME: This doesn't work in a non-contiguous blocks. 
+	 */
+	BasicPort local = ((BasicPort) obj);
+	if (local.getParent() != previousBlock) {
+	    if (previousBlock instanceof SplitBlock) {
+		SplitBlock block = (SplitBlock) previousBlock;
+		if (previousBlock.getChildCount() != 4) {
+		    block.insert(new mxCell());
+		}
+	    }
+
+	    previousBlock = (BasicBlock) local.getParent();
+	}
+	
 	return super.afterDecode(dec, node, obj);
     }
 
