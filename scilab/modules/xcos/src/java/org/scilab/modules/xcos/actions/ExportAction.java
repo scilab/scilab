c0cc8c9d2c2208bfe2a4e14d19ae04c0645f1e3c
Clément DAVID <clement.david@scilab.org>
2009-12-11 17:21:14 +0100

    Organize import

--- scilab/modules/xcos/src/java/org/scilab/modules/xcos/actions/ExportAction.java
+++ scilab/modules/xcos/src/java/org/scilab/modules/xcos/actions/ExportAction.java
@@ -20,12 +20,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
-import java.util.concurrent.CopyOnWriteArraySet;
 
 import javax.imageio.ImageIO;
 import javax.swing.JOptionPane;
