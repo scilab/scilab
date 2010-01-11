b811e977a091be35ddcfee7d932b173530932ea4
Bruno JOFRET <bruno.jofret@scilab.org>
2010-01-11 09:32:09 +0100

    Container class

---
+++ scilab/modules/types/includes/container.hxx
@@ -0,0 +1,37 @@
+/*
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2010-2010 - DIGITEO - Bruno JOFRET
+ * 
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ * 
+ */
+
+#ifndef __CONTAINER_HXX__
+#define __CONTAINER_HXX__
+
+#include "internal.hxx"
+
+namespace types
+{
+  class Container : public InternalType
+  {
+
+  public :
+    Container() : InternalType() {}
+    virtual ~Container() {}
+
+    void whoAmI(void) { std::cout << "types::Container"; }
+
+    virtual int size_get() = 0 ;
+
+    virtual bool isAssignable(void) { return true; }
+    
+    virtual RealType getType(void) { return RealContainer; }
+  }
+}
+
+#endif /* !__CONTAINER_HXX__ */
