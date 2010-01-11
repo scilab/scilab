391437ad9d2613eaafdcbb81ac77de0d3485ac1c
Clément DAVID <clement.david@scilab.org>
2010-01-11 17:49:54 +0100

    Extract local private functions to reduce drawVertex complexity

--- scilab/modules/xcos/src/java/org/scilab/modules/xcos/utils/XcosCanvas.java
+++ scilab/modules/xcos/src/java/org/scilab/modules/xcos/utils/XcosCanvas.java
@@ -70,25 +70,7 @@ public class XcosCanvas extends mxInteractiveCanvas {
 				}
 			}
 
-			String flip = mxUtils.getString(style, XcosConstants.STYLE_FLIP,
-					"false");
-			String mirror = mxUtils.getString(style,
-					XcosConstants.STYLE_MIRROR, "false");
-
-			// scale, 1st flip, 2nd mirror
-			if (flip.compareTo("true") == 0) {
-				if (mirror.compareTo("true") == 0) {
-					g.scale(-1, -1); // T / T
-				} else {
-					g.scale(-1, 1); // T / F
-				}
-			} else {
-				if (mirror.compareTo("true") == 0) {
-					g.scale(1, -1); // F / T
-				} else {
-					g.scale(1, 1); // F / F
-				}
-			}
+			applyFlipAndMirror(style);
 
 			g.translate(-(x + (w / 2.0)), -(y + (h / 2.0)));
 
@@ -107,27 +89,7 @@ public class XcosCanvas extends mxInteractiveCanvas {
 			Stroke stroke = g.getStroke();
 
 			// Draws a swimlane if start is > 0
-			int start = mxUtils.getInt(style, mxConstants.STYLE_STARTSIZE);
-
-			if (start == 0) {
-				drawShape(x, y, w, h, style);
-			} else {
-				start = (int) Math.round(start * scale);
-
-				// Removes some styles to draw the content area
-				Hashtable<String, Object> cloned = new Hashtable<String, Object>(
-						style);
-				cloned.remove(mxConstants.STYLE_FILLCOLOR);
-				cloned.remove(mxConstants.STYLE_ROUNDED);
-
-				if (mxUtils.isTrue(style, mxConstants.STYLE_HORIZONTAL, true)) {
-					drawShape(x, y, w, start, style);
-					drawShape(x, y + start, w, h - start, cloned);
-				} else {
-					drawShape(x, y, start, h, style);
-					drawShape(x + start, y, w - start, h, cloned);
-				}
-			}
+			drawSwimline(x, y, w, h, style);
 
 			// Restores the stroke
 			g.setStroke(stroke);
@@ -144,6 +106,66 @@ public class XcosCanvas extends mxInteractiveCanvas {
 		}
 
 		return null;
+	}
+
+	/**
+	 * Scale the graphic context depending on the "flip and "mirror" properties
+	 * @param style Style contents
+	 */
+	private void applyFlipAndMirror(Hashtable<String, Object> style) {
+		String flip = mxUtils.getString(style, XcosConstants.STYLE_FLIP,
+				"false");
+		String mirror = mxUtils.getString(style, XcosConstants.STYLE_MIRROR,
+				"false");
+
+		// scale, 1st flip, 2nd mirror
+		if (flip.compareTo("true") == 0) {
+			if (mirror.compareTo("true") == 0) {
+				g.scale(-1, -1); // T / T
+			} else {
+				g.scale(-1, 1); // T / F
+			}
+		} else {
+			if (mirror.compareTo("true") == 0) {
+				g.scale(1, -1); // F / T
+			} else {
+				g.scale(1, 1); // F / F
+			}
+		}
+	}
+
+	/**
+	 * Draws a swimlane if start is > 0
+	 * 
+	 * @param x Horizontal coordinate
+	 * @param y Vertical coordinate
+	 * @param w Width
+	 * @param h Height
+	 * @param style The associated style
+	 */
+	private void drawSwimline(int x, int y, int w, int h,
+			Hashtable<String, Object> style) {
+		int start = mxUtils.getInt(style, mxConstants.STYLE_STARTSIZE);
+
+		if (start == 0) {
+			drawShape(x, y, w, h, style);
+		} else {
+			start = (int) Math.round(start * scale);
+
+			// Removes some styles to draw the content area
+			Hashtable<String, Object> cloned = new Hashtable<String, Object>(
+					style);
+			cloned.remove(mxConstants.STYLE_FILLCOLOR);
+			cloned.remove(mxConstants.STYLE_ROUNDED);
+
+			if (mxUtils.isTrue(style, mxConstants.STYLE_HORIZONTAL, true)) {
+				drawShape(x, y, w, start, style);
+				drawShape(x, y + start, w, h - start, cloned);
+			} else {
+				drawShape(x, y, start, h, style);
+				drawShape(x + start, y, w - start, h, cloned);
+			}
+		}
 	}
 
 	/**
