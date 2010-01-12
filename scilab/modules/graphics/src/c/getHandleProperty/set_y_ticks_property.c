6ee356f56efed17f908a3dbcf800f3fb8f451a99
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2010-01-12 08:26:12 +0100

    (was not related to a bad merge but a bad modification. CF commit bf36dd6b7175c91982269d847555e4ba61787014 Revert "Load the textrendering when necessary ... bloody merge"
    This reverts commit b7ffaa61ffa5f5793457ac515ee682f407cd86d4.

--- scilab/modules/graphics/src/c/getHandleProperty/set_y_ticks_property.c
+++ scilab/modules/graphics/src/c/getHandleProperty/set_y_ticks_property.c
@@ -102,9 +102,7 @@ int set_y_ticks_property( sciPointObj * pobj, size_t stackPointer, int valueType
   // P.Lando
   if( nbTicsCol * nbTicsRow )
   {
-	  /* Check if we should load LaTex / MathML Java libraries */
-	  loadTextRenderingAPI(labels, nbTicsCol, nbTicsRow);
-	  ppSubWin->axes.u_ylabels = getCurrentStringMatrixFromList( tlist, &nbTicsRow, &nbTicsCol );
+    ppSubWin->axes.u_ylabels = getCurrentStringMatrixFromList( tlist, &nbTicsRow, &nbTicsCol );
   }
   else
   {
