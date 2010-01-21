06cb4ecb37932fba1d85a54e2479fd71fc038082
Clément DAVID <clement.david@scilab.org>
2010-01-21 16:38:28 +0100

    Insert at the right index

--- scilab/modules/xcos/src/java/org/scilab/modules/xcos/palette/model/PaletteManagerModel.java
+++ scilab/modules/xcos/src/java/org/scilab/modules/xcos/palette/model/PaletteManagerModel.java
@@ -115,10 +115,13 @@ public final class PaletteManagerModel {
 	public void loadTree() {
 		/* Add the categories */
 		final int length = Palette.getDatas().length;
+		int disableCounter = 0;
 		for (int i = 0; i < length; i++) {
 			Palette p = Palette.getDatas()[i];
 			if (p.getModel().isEnable()) {
-				mainRoot.insert(new DefaultMutableTreeNode(p), i);
+				mainRoot.insert(new DefaultMutableTreeNode(p), i - disableCounter);
+			} else {
+				disableCounter++;
 			}
 		}
 		
