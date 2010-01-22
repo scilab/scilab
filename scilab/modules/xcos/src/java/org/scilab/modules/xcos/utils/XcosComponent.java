b0dbec39e8e0df2aeb9bc747a3831cef2f3106b0
Clément DAVID <clement.david@scilab.org>
2010-01-22 15:41:14 +0100

    5040: work on the fit diagram to view

--- scilab/modules/xcos/src/java/org/scilab/modules/xcos/utils/XcosComponent.java
+++ scilab/modules/xcos/src/java/org/scilab/modules/xcos/utils/XcosComponent.java
@@ -19,7 +19,13 @@ import java.awt.event.ComponentListener;
 
 import org.scilab.modules.xcos.graph.PaletteDiagram;
 
+import com.mxgraph.model.mxCell;
+import com.mxgraph.model.mxGeometry;
+import com.mxgraph.model.mxICell;
+import com.mxgraph.model.mxIGraphModel;
 import com.mxgraph.swing.mxGraphComponent;
+import com.mxgraph.util.mxRectangle;
+import com.mxgraph.view.mxCellState;
 import com.mxgraph.view.mxGraph;
 import com.mxgraph.view.mxGraphView;
 
@@ -30,7 +36,7 @@ import com.mxgraph.view.mxGraphView;
 public class XcosComponent extends mxGraphComponent {
 
     private static final long serialVersionUID = 1592816691330208090L;
-    private static final int ZOOM_BORDER = 20; // px
+    private static final double SCALE_MULTIPLIER = 1.1;
 
     /**
      * Construct the component with the associated graph
@@ -86,43 +92,95 @@ public class XcosComponent extends mxGraphComponent {
      */
     public void zoomAndCenterToCells() {
 	mxGraphView view = graph.getView();
-	Rectangle preference = (Rectangle) view.getBounds(graph.getChildCells(graph.getDefaultParent())).getRectangle().clone();
+	Rectangle preference = (Rectangle) getChildrenBounds(graph.getChildCells(graph.getDefaultParent())).getRectangle();
 	
-	// Feeling values to keep the zoom smart 
-	preference.x -= ZOOM_BORDER;
-	preference.y += ZOOM_BORDER;
-	preference.width += 2 * ZOOM_BORDER;
-	preference.height += 2 * ZOOM_BORDER;
-	
-	Dimension actual = viewport.getSize();
+	Dimension actual = getViewport().getSize();
 
-	double newScale = 1.0;
+	double newScale;
 	double heightScale = actual.getHeight() / preference.getHeight();
 	double widthScale = actual.getWidth() / preference.getWidth();
 	
 	if (heightScale > 1.0) {
 	    if (widthScale > 1.0) {
 		// We need to zoom in (the max applicable zoom is the lowest)
-		newScale = Math.min(heightScale, widthScale);
+		newScale = Math.min(heightScale, widthScale) * (1.0 - (SCALE_MULTIPLIER - 1.0));
 	    } else {
 		// we need to zoom out (only widthScale is < 1.0)
-		newScale = widthScale;
+		newScale = widthScale * SCALE_MULTIPLIER;
 	    }
 	} else {
 	    if (widthScale > 1.0) {
 		// we need to zoom out (only heightScale is < 1.0)
-		newScale = heightScale;
+		newScale = heightScale * SCALE_MULTIPLIER;
 	    } else {
 		// We need to zoom out (the max applicable zoom is the lowest)
-		newScale = Math.min(heightScale, widthScale);
+		newScale = Math.min(heightScale, widthScale) * SCALE_MULTIPLIER;
 	    }
 	}
 
 	zoom(newScale);
 	
 	view.revalidate();
-	Rectangle orig = view.getBounds(graph.getChildCells(graph.getDefaultParent())).getRectangle();
-	
+	Rectangle orig = (Rectangle) getChildrenBounds(graph.getChildCells(graph.getDefaultParent())).getRectangle();
 	getGraphControl().scrollRectToVisible(orig);
     }
+
+	/**
+	 * Get the children bound for the cells
+	 * 
+	 * @param cells
+	 *            the root of the graph
+	 * @return the rectangle or null if not applicable
+	 */
+	private mxRectangle getChildrenBounds(final Object[] cells) {
+		mxRectangle result = null;
+
+		if (cells != null && cells.length > 0) {
+			final mxGraphView view = graph.getView();
+			final mxIGraphModel model = graph.getModel();
+
+			for (int i = 0; i < cells.length; i++) {
+				if (model.isVertex(cells[i]) || model.isEdge(cells[i])) {
+					final mxICell parent = ((mxICell) cells[i]);
+					final int childCount = parent.getChildCount();
+
+					for (int j = 0; j < childCount; j++) {
+						final mxICell child = parent.getChildAt(j);
+
+						result = updateRectangle(result, view, child);
+					}
+
+					result = updateRectangle(result, view, parent);
+				}
+			}
+		}
+
+		return result;
+	}
+
+	/**
+	 * Update the rectangle parameter with the cell status
+	 * 
+	 * @param result
+	 *            the previous result
+	 * @param view
+	 *            the current view
+	 * @param child
+	 *            the child we have to work on
+	 * @return the updated rectangle
+	 */
+	private mxRectangle updateRectangle(mxRectangle result,
+			final mxGraphView view, final mxICell child) {
+		final mxCellState state = view.getState(child);
+		mxRectangle rect = result;
+
+		if (state != null) {
+			if (rect == null) {
+				rect = new mxRectangle(state);
+			} else {
+				rect.add(state);
+			}
+		}
+		return rect;
+	}
 }
