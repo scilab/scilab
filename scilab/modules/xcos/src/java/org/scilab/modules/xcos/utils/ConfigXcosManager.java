9e03234210ca55fe69fdd9396dfd6c8aa030d831
Clément DAVID <clement.david@scilab.org>
2010-01-27 16:28:01 +0100

    Remove unused import

--- scilab/modules/xcos/src/java/org/scilab/modules/xcos/utils/ConfigXcosManager.java
+++ scilab/modules/xcos/src/java/org/scilab/modules/xcos/utils/ConfigXcosManager.java
@@ -3,7 +3,6 @@ package org.scilab.modules.xcos.utils;
 import java.awt.Toolkit;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
