72464409a11776a8538901194abb688341f5f416
Clément DAVID <clement.david@scilab.org>
2010-02-02 16:20:37 +0100

    Use the right serialization type. Otherwise jgraphx serializer will silently failed on loading

--- scilab/modules/xcos/src/java/org/scilab/modules/xcos/block/SuperBlock.java
+++ scilab/modules/xcos/src/java/org/scilab/modules/xcos/block/SuperBlock.java
@@ -106,7 +106,7 @@ public final class SuperBlock extends BasicBlock {
 		super.setDefaultValues();
 		setInterfaceFunctionName("SUPER_f");
 		setSimulationFunctionName("super");
-		setRealParameters(new ScilabDouble());
+		setRealParameters(new ScilabMList());
 		setIntegerParameters(new ScilabDouble());
 		setObjectsParameters(new ScilabList());
 		setExprs(new ScilabDouble());
