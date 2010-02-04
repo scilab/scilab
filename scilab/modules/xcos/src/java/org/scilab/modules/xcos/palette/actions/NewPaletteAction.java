55696bdfb0becd67fbc9a13330568b0067c0d89e
Clément DAVID <clement.david@scilab.org>
2010-02-04 15:00:55 +0100

    Comment header

--- scilab/modules/xcos/src/java/org/scilab/modules/xcos/palette/actions/NewPaletteAction.java
+++ scilab/modules/xcos/src/java/org/scilab/modules/xcos/palette/actions/NewPaletteAction.java
@@ -1,6 +1,7 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2009 - DIGITEO - Vincent COUVERT
+ * Copyright (C) 2010 - DIGITEO - Clément DAVID
  * 
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
