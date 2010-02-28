c27d5d3c5309f4e22d115df8cbebc01bc774f5c5
Bruno JOFRET <bruno.jofret@scilab.org>
2010-02-28 23:23:36 +0100

    Adding SelectExp and CaseExp to each visitor

--- scilab/modules/abstractSyntaxTree/includes/token.hxx
+++ scilab/modules/abstractSyntaxTree/includes/token.hxx
@@ -126,6 +126,11 @@
 #define SCI_CLOSE_ARGS		SCI_RPAREN
 #define SCI_ENDFUNCTION		"endfunction"
 
+// Syntax to have select control structure
+#define SCI_SELECT		"select"
+#define SCI_CASE		"case"
+#define SCI_ENDSELECT		"end"
+
 // Syntax to have return control structure
 #define SCI_RETURN		"return"
 
