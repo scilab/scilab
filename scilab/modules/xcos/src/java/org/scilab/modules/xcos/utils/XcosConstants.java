48102272218703b3c6c3ed7fd64b936107b37bad
Clément DAVID <clement.david@scilab.org>
2010-03-01 17:49:44 +0100

    Xcos: checkstyle and comment

--- scilab/modules/xcos/src/java/org/scilab/modules/xcos/utils/XcosConstants.java
+++ scilab/modules/xcos/src/java/org/scilab/modules/xcos/utils/XcosConstants.java
@@ -18,8 +18,13 @@ public final class XcosConstants extends ScilabConstants {
     public static final int PALETTE_VMARGIN = 5;
     
     /* Events */
+    /**
+     * When a block changed
+     */
     public static final String EVENT_BLOCK_UPDATED = "block";
+    /** Change event old name */
     public static final String EVENT_CHANGE_OLD = "old";
+    /** Change event new name */
     public static final String EVENT_CHANGE_NEW = "new";
     
     /** This class is a static singleton, thus it must not be instantiated */
