a88dafc10355412d826a0a5cf0b9a2c77871f2b7
antoine ELIAS <antoine.elias@scilab.org>
2010-03-02 10:53:39 +0100

    better prompt management

--- scilab/modules/core/src/cpp/scilab.cpp
+++ scilab/modules/core/src/cpp/scilab.cpp
@@ -276,19 +276,19 @@ static void banner()
 
 static void stateShow(Parser::ControlStatus status)
 {
-  switch (status)
+	switch (status)
 	{
-	case Parser::WithinFor : YaspWrite("[ for ]"); break;
-	case Parser::WithinWhile : YaspWrite("[ while ]"); break;
-	case Parser::WithinIf : YaspWrite("[ if ]"); break;
-	case Parser::WithinElse : YaspWrite("[ else ]"); break;
-	case Parser::WithinElseIf : YaspWrite("[ elseif ]"); break;
-	case Parser::WithinTry : YaspWrite("[ try ]"); break;
-	case Parser::WithinCatch : YaspWrite("[ catch ]"); break;
-	case Parser::WithinFunction : YaspWrite("[ function ]"); break;
-	case Parser::WithinSelect : YaspWrite("[ select ]"); break;
-	case Parser::WithinCase : YaspWrite("[ case ]"); break;
-	case Parser::AllControlClosed :/* Do Nothing */ break;
+	case Parser::WithinFor : SetTemporaryPrompt("-for->"); break;
+	case Parser::WithinWhile : SetTemporaryPrompt("-while->"); break;
+	case Parser::WithinIf : SetTemporaryPrompt("-if->"); break;
+	case Parser::WithinElse : SetTemporaryPrompt("-else->"); break;
+	case Parser::WithinElseIf : SetTemporaryPrompt("-elseif->"); break;
+	case Parser::WithinTry : SetTemporaryPrompt("-try->"); break;
+	case Parser::WithinCatch : SetTemporaryPrompt("-catch->"); break;
+	case Parser::WithinFunction : SetTemporaryPrompt("-function->"); break;
+	case Parser::WithinSelect : SetTemporaryPrompt("-select->"); break;
+	case Parser::WithinCase : SetTemporaryPrompt("-case->"); break;
+	case Parser::AllControlClosed : SetTemporaryPrompt(SCIPROMPT); break;
 	}
 }
 
