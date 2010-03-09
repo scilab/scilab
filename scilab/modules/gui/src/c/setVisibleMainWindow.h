2d54f31413aace94d5bdbed5ac6e3821887ff7da
Allan CORNET <allan.cornet@scilab.org>
2010-03-09 16:04:06 +0100

    bug 6740

---
+++ scilab/modules/gui/src/c/setVisibleMainWindow.h
@@ -0,0 +1,32 @@
+/*
+* Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+* Copyright (C) 2010 - DIGITEO - Allan CORNET
+* 
+* This file must be used under the terms of the CeCILL.
+* This source file is licensed as described in the file COPYING, which
+* you should have received as part of this distribution.  The terms
+* are also available at    
+* http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+*
+*/
+#ifndef __SETVISIBLEMAINWINDOW_H__
+#define __SETVISIBLEMAINWINDOW_H__
+
+#include "BOOL.h" /* BOOL */
+#include "dynlib_gui.h"
+
+
+/**
+* set Main Window visibility
+* @param[in] new state
+*/
+GUI_IMPEXP void setVisibleMainWindow(BOOL newVisibleState);
+
+/**
+* isVisibleMainWindow
+* @return current state
+*/
+GUI_IMPEXP BOOL isVisibleMainWindow(void);
+
+#endif /* __SETVISIBLEMAINWINDOW_H__ */
+/*--------------------------------------------------------------------------*/ 
