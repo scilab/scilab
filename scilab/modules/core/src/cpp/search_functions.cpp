f5f3f1723ec54a6a25d5d521e1369f71d437668a
Bernard HUGUENEY <bernard.hugueney@scilab.org>
2010-03-19 15:05:45 +0100

    1°) Replaced C2F(eqid)() call with (inlined) eq_n<nsiz> from unrolled_algorithms.hxx 2°) Provided most efficient specialization for nsiz ints of this template. 3°) moved unrolled_algorihm.hxx content from scilab::core namespace to unnamed namespace as it is used for inlinng. should an out of line definition be provided in a visible (scilab::core) namespace, it should be done in a .cpp file to avoid ODR violation.

--- scilab/modules/core/src/cpp/search_functions.cpp
+++ scilab/modules/core/src/cpp/search_functions.cpp
@@ -1,10 +1,11 @@
 #include <cstdlib>
 #include <algorithm>
+#include "unrolled_algorithms.hxx"
 extern "C" {
 #include "stack-c.h"
 #include "stack-def.h" /* C2F(basbrk) */
 #include "intmacr2tree.h" /*#define idstk(x,y) (C2F(vstk).idstk+(x-1)+(y-1)*nsiz) */
-int C2F(eqid)(int const*x, int const*y);
+
 void C2F(siflibs)(int* id, int* k_ptr, int* istr, int* lbibn, int* nbibn, int* ilp, int* nn, int* should_return);
 void C2F(sivars)(int* id, int* should_return);
 void C2F(namstr)(int* id, int* str, int* n, char const* job);
@@ -17,8 +18,9 @@ namespace {
   int const nclas= 29;
   char const from_id=1;
 
+  // directly convert from id to upper char
   int upper_char(int id){ return std::abs((int)((id & 0x80) ? (id |0xffffff00) :  (id & 0xff))); }
-
+  // gives the discriminating char (either first of second if first=percent)
   int id_char(int const* id){
     int ch(upper_char(*id));
     if(ch == percent){
@@ -27,7 +29,7 @@ namespace {
     return ch;
   }
 }
-
+// search for an id in the libraries
 void C2F(siflibs)(int* id, int* k_ptr, int* istr, int* lbibn_ptr, int* nbibn_ptr, int* ilp_ptr, int* nn_ptr, int* should_return){
   static int const* const lstk_ptr = (int*)C2F(vstk).lstk-1;
   static int const* const istk_ptr = ((int*)C2F(stack).Stk)-1;
@@ -47,7 +49,7 @@ void C2F(siflibs)(int* id, int* k_ptr, int* istr, int* lbibn_ptr, int* nbibn_ptr
 	if(n != 0){
 	  int iln= ilp+nclas+1+(istk_ptr[ilp+ip-1]-1)*nsiz;
 	  for(int i= 1; i<=n; ++i, iln+= nsiz){
-	    if(C2F(eqid)(id, istk_ptr+iln)){ // 39
+	    if(eq_n<nsiz>(id, istk_ptr+iln)){ // 39
 		if((Fin == -1) || (Fin == -3)){
 		  C2F(com).fun= k;
 		  Fin= i;
@@ -81,7 +83,7 @@ void C2F(siflibs)(int* id, int* k_ptr, int* istr, int* lbibn_ptr, int* nbibn_ptr
   return;
 }
 
-
+// search for an id in vars
 void C2F(sivars)(int* id, int* should_return){
   int* const lstk_ptr = (int*)C2F(vstk).lstk-1;
 
@@ -89,7 +91,7 @@ void C2F(sivars)(int* id, int* should_return){
   //  idstk(x,y) (C2F(vstk).idstk+(x-1)+(y-1)*nsiz)
   int* id_addr=C2F(vstk).idstk;
   for(  *should_return= f_false, k= Bot-1, id_addr+=(k-1)*nsiz
-	  ; k <=  C2F(vstk).isiz && !C2F(eqid)(id_addr, id); ++k, id_addr+=nsiz){
+	  ; k <=  C2F(vstk).isiz && !eq_n<nsiz>(id_addr, id); ++k, id_addr+=nsiz){
   }
   if( k <=  C2F(vstk).isiz ){// eq_id
     int il=lstk_ptr[k];//iadr()
--- scilab/modules/core/src/cpp/unrolled_algorithms.hxx
+++ scilab/modules/core/src/cpp/unrolled_algorithms.hxx
@@ -1,69 +1,81 @@
 /*
-* Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
-* Copyright (C) 2010 - DIGITEO - Bernard HUGUENEY
-*
-* This file must be used under the terms of the CeCILL.
-* This source file is licensed as described in the file COPYING, which
-* you should have received as part of this distribution.  The terms
-* are also available at
-* http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
-*
-*/
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2010 - DIGITEO - Bernard HUGUENEY
+ *
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 /*-----------------------------------------------------------------------------------*/
 #ifndef UNROLLED_ALGORITHMS_HXX
 #define UNROLLED_ALGORITHMS_HXX
 
-namespace scilab {
-	namespace core {
-		/*
-		* generic template unrolling for small vectors
-		*/
+namespace {
+  /*
+   * generic template unrolling for small vectors
+   */
 
-		template<typename It1, typename It2, int N> struct eq_n_t {
-			bool operator()(It1 i1, It2 i2) const {
-				return (*i1 == *i2) && eq_n_t<It1, It2, N-1>()(++i1, ++i2) ;
-			}
-		};
-		template<typename It1, typename It2> struct eq_n_t<It1, It2, 0> {
-			bool operator()(It1 i1, It2 i2) const { 
-				return true ;
-			}
-		};
-		template<int N, typename It1, typename It2> bool eq_n(It1 i1, It2 i2) {
-			return eq_n_t<It1, It2, N>()(i1, i2);
-		}
+  template<typename It1, typename It2, int N> struct eq_n_t {
+    bool operator()(It1 i1, It2 i2) const {
+      return (*i1 == *i2) && eq_n_t<It1, It2, N-1>()(++i1, ++i2) ;
+    }
+  };
+  template<typename It1, typename It2> struct eq_n_t<It1, It2, 0> {
+    bool operator()(It1 i1, It2 i2) const {
+      return true ;
+    }
+  };
+  template<int N, typename It1, typename It2> bool eq_n(It1 i1, It2 i2) {
+    return eq_n_t<It1, It2, N>()(i1, i2);
+  }
+  // ensure most efficient inlining irrespectively of optimization compiler options for ids of nsiz=6 ints
+  template<> bool eq_n<6,int const*, int const*>(int const* id1, int const* id2) {
+    return (id1[0]==id2[0]) && (id1[1]==id2[1]) && (id1[2]==id2[2]) && (id1[3]==id2[3]) && (id1[4]==id2[4]) && (id1[5]==id2[5]);
+  }
 
-		template<typename It1, typename It2, int N> struct less_n_t {
-			bool operator()(It1 i1, It2 i2) const {
-				return (*i1<*i2) ? true : ( (*i1>*i2) ? false : less_n_t<It1, It2, N-1>()(++i1, ++i2));
-			}
-		};
-		template<typename It1, typename It2> struct less_n_t<It1, It2, 0> {
-			bool operator()(It1 i1, It2 i2) const {
-				return false ;
-			}
-		};
+    template<> bool eq_n<6,int const*, int*>(int const* id1, int* id2) {
+    return (id1[0]==id2[0]) && (id1[1]==id2[1]) && (id1[2]==id2[2]) && (id1[3]==id2[3]) && (id1[4]==id2[4]) && (id1[5]==id2[5]);
+    }
+    template<> bool eq_n<6,int*, int const*>(int* id1, int const* id2) {
+    return (id1[0]==id2[0]) && (id1[1]==id2[1]) && (id1[2]==id2[2]) && (id1[3]==id2[3]) && (id1[4]==id2[4]) && (id1[5]==id2[5]);
+    }
+    template<> bool eq_n<6,int*, int*>(int* id1, int* id2) {
+    return (id1[0]==id2[0]) && (id1[1]==id2[1]) && (id1[2]==id2[2]) && (id1[3]==id2[3]) && (id1[4]==id2[4]) && (id1[5]==id2[5]);
+    }
 
-		template<int N, typename It1, typename It2> bool less_n(It1 i1, It2 i2) {
-			return less_n_t<It1, It2, N>()(i1, i2);
-		}
+  template<typename It1, typename It2, int N> struct less_n_t {
+    bool operator()(It1 i1, It2 i2) const {
+      return (*i1<*i2) ? true : ( (*i1>*i2) ? false : less_n_t<It1, It2, N-1>()(++i1, ++i2));
+    }
+  };
+  template<typename It1, typename It2> struct less_n_t<It1, It2, 0> {
+    bool operator()(It1 i1, It2 i2) const {
+      return false ;
+    }
+  };
 
-		template<typename In, typename Out, int N> struct copy_n_t {
-			Out operator()(In in, Out out) const {
-				*out= *in;
-				return copy_n_t<In, Out, N-1>()(++in, ++out);
-			}
-		};
-		template<typename In, typename Out> struct copy_n_t<In, Out, 0> {
-			Out operator()(In, Out out) const {
-				return out;
-			}
-		};
-		template<int N, typename In, typename Out> Out copy_n(In in, Out out) {
-			return copy_n_t<In, Out, N>()(in, out);
-		}
+  template<int N, typename It1, typename It2> bool less_n(It1 i1, It2 i2) {
+    return less_n_t<It1, It2, N>()(i1, i2);
+  }
 
-	}
+  template<typename In, typename Out, int N> struct copy_n_t {
+    Out operator()(In in, Out out) const {
+      *out= *in;
+      return copy_n_t<In, Out, N-1>()(++in, ++out);
+    }
+  };
+  template<typename In, typename Out> struct copy_n_t<In, Out, 0> {
+    Out operator()(In, Out out) const {
+      return out;
+    }
+  };
+  template<int N, typename In, typename Out> Out copy_n(In in, Out out) {
+    return copy_n_t<In, Out, N>()(in, out);
+  }
+
 }
 #endif
 /*-----------------------------------------------------------------------------------*/
