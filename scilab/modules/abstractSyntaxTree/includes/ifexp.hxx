adab55240d2f1f6202736afc13a11bb41d0897ce
Bruno JOFRET <bruno.jofret@scilab.org>
2010-04-01 17:18:56 +0200

    Remove compilation warning

--- scilab/modules/abstractSyntaxTree/includes/ifexp.hxx
+++ scilab/modules/abstractSyntaxTree/includes/ifexp.hxx
@@ -19,143 +19,143 @@
 namespace ast
 {
 
-  /*
-  ** \brief Abstract an If Expression node.
-  **
-  ** \b Example: if (2097 = 0) then disp("fail") else disp("OK")
-  */
-  class IfExp : public ControlExp
-  {
-  public:
     /*
-    ** \brief If Expression kind.
+    ** \brief Abstract an If Expression node.
     **
-    ** \brief Specify if the If Expression has a value (set at type checking).
+    ** \b Example: if (2097 = 0) then disp("fail") else disp("OK")
     */
-    enum Kind
+    class IfExp : public ControlExp
     {
-      // \brief Invalid kind
-      invalid_kind,
-      // \brief "Is an instruction" (no value) kind
-      instruction_kind,
-      // \brief "Is an expression" (value) kind
-      expression_kind
+    public:
+        /*
+        ** \brief If Expression kind.
+        **
+        ** \brief Specify if the If Expression has a value (set at type checking).
+        */
+        enum Kind
+        {
+            // \brief Invalid kind
+            invalid_kind,
+            // \brief "Is an instruction" (no value) kind
+            instruction_kind,
+            // \brief "Is an expression" (value) kind
+            expression_kind
+        };
+
+        // \brief Ctor & dtor.
+    public:
+        /*
+        ** \brief Construct an If Expression node.
+        ** \param location scanner position informations
+        ** \param test chose the instruction to execute
+        ** \param then instruction if test is true
+        ** \param else instruction if test is false
+        */
+        IfExp(const Location& location,
+              Exp& test, Exp& t, Exp& e) :
+            ControlExp (location),
+            _test (&test),
+            _then (&t),
+            _else (&e),
+            _has_else (true)
+            {
+            }
+
+        IfExp(const Location& location,
+              Exp& test, Exp& t) :
+            ControlExp (location),
+            _test (&test),
+            _then (&t),
+            _else (new ast::CommentExp(location, new std::string("No else !!"))),
+            // For first display in order to see what appends
+            _has_else (false)
+            {
+            }
+
+        virtual ~IfExp()
+            {
+                delete _test;
+                delete _then;
+                delete _else;
+            }
+
+        // \brief Visitors entry point.
+    public:
+        virtual void	accept(Visitor& v)
+            {
+                v.visit (*this);
+            }
+        virtual void	accept(ConstVisitor& v) const
+            {
+                v.visit (*this);
+            }
+
+        // \brief Accessors.
+    public:
+        // \brief Return the select condition of the loop (read only).
+        const Exp&	test_get() const
+            {
+                return *_test;
+            }
+        // \brief Return the select condition of the loop (read and write).
+        Exp&	test_get()
+            {
+                return *_test;
+            }
+
+        // \brief Return the intructions if test is true (read only).
+        const Exp&	then_get() const
+            {
+                return *_then;
+            }
+        // \brief Return the instructions if test is true (read and write).
+        Exp& then_get()
+            {
+                return *_then;
+            }
+
+        // \brief Return the instruction if test is false (read only).
+        const Exp&	else_get() const
+            {
+                return *_else;
+            }
+        // \brief Return the instruction if test is false (read and write).
+        Exp&	else_get()
+            {
+                return *_else;
+            }
+
+        // \brief Return if there is an else body
+        bool	has_else()
+            {
+                return _has_else;
+            }
+        // \brief Return if there is an else body
+        bool	has_else() const
+            {
+                return _has_else;
+            }
+
+        // \brief Return the kind of the If Expression (read only)
+        Kind kind_get (void) const
+            {
+                assert (_kind != invalid_kind);
+                return _kind;
+            }
+        // \brief Set the kind of the If Expression
+        void kind_set (Kind kind)
+            {
+                this->_kind = kind;
+            }
+
+    protected:
+        // \brief "has a value" qualifier.
+        Exp*	_test;
+        Exp*	_then;
+        Exp*	_else;
+        Kind	_kind;
+        bool	_has_else;
     };
-
-    // \brief Ctor & dtor.
-  public:
-    /*
-    ** \brief Construct an If Expression node.
-    ** \param location scanner position informations
-    ** \param test chose the instruction to execute
-    ** \param then instruction if test is true
-    ** \param else instruction if test is false
-    */
-    IfExp(const Location& location,
-	   Exp& test, Exp& t, Exp& e) :
-      ControlExp (location),
-      _test (&test),
-      _then (&t),
-      _else (&e),
-      _has_else (true)
-    {
-    }
-
-    IfExp(const Location& location,
-	   Exp& test, Exp& t) :
-      ControlExp (location),
-      _test (&test),
-      _then (&t),
-      _else (new ast::CommentExp(location, new std::string("No else !!"))),
-      // For first display in order to see what appends
-      _has_else (false)
-    {
-    }
-
-    virtual ~IfExp()
-    {
-		delete _test;
-		delete _then;
-		delete _else;
-    }
-
-    // \brief Visitors entry point.
-  public:
-    virtual void	accept(Visitor& v)
-    {
-      v.visit (*this);
-    }
-    virtual void	accept(ConstVisitor& v) const
-    {
-      v.visit (*this);
-    }
-
-    // \brief Accessors.
-  public:
-    // \brief Return the select condition of the loop (read only).
-    const Exp&	test_get() const
-    {
-      return *_test;
-    }
-    // \brief Return the select condition of the loop (read and write).
-    Exp&	test_get()
-    {
-      return *_test;
-    }
-
-    // \brief Return the intructions if test is true (read only).
-    const Exp&	then_get() const
-    {
-      return *_then;
-    }
-    // \brief Return the instructions if test is true (read and write).
-    Exp& then_get()
-    {
-      return *_then;
-    }
-
-    // \brief Return the instruction if test is false (read only).
-    const Exp&	else_get() const
-    {
-      return *_else;
-    }
-    // \brief Return the instruction if test is false (read and write).
-    Exp&	else_get()
-    {
-      return *_else;
-    }
-
-    // \brief Return if there is an else body
-    bool	has_else()
-    {
-      return _has_else;
-    }
-    // \brief Return if there is an else body
-    const bool	has_else() const
-    {
-      return _has_else;
-    }
-
-    // \brief Return the kind of the If Expression (read only)
-    Kind kind_get (void) const
-    {
-      assert (_kind != invalid_kind);
-      return _kind;
-    }
-    // \brief Set the kind of the If Expression
-    void kind_set (Kind kind)
-    {
-      this->_kind = kind;
-    }
-
-  protected:
-    // \brief "has a value" qualifier.
-    Exp*	_test;
-    Exp*	_then;
-    Exp*	_else;
-    Kind	_kind;
-    bool	_has_else;
-  };
 
 } // namespace ast
 
