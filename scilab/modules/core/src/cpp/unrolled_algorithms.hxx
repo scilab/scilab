388a3f48470ad6896f60e24752241687486c9a3e
Bernard HUGUENEY <bernard.hugueney@scilab.org>
2010-04-15 17:08:22 +0200

    Code documentation and coding convention conformance (->4 spaces indent) moved to C comments // -> /* */

--- scilab/modules/core/src/cpp/unrolled_algorithms.hxx
+++ scilab/modules/core/src/cpp/unrolled_algorithms.hxx
@@ -14,175 +14,159 @@
 #define UNROLLED_ALGORITHMS_HXX
 
 namespace {
-	/*
-	 * generic template unrolling for small vectors
-	 */
+    /*
+     * generic template unrolling for small vectors
+     */
 
 /**
- * @TODO add comment
+ * C++ does not allow function template partial specialization so eq_n<> delegates to work to a eq_n_t<> struct
+ * which implements the function in operator().
  *
- * @param   
- * @return <ReturnValue>
+ * @param i1 : iterator to the beginning of the first sequence
+ * @param i2 : iterator to the beginning of the second sequence
+ * @return true iff *i1 == *i2 &&...&& *(i1+N-1) == *(i2+N-1)
  */
-	template<typename It1, typename It2, int N> struct eq_n_t 
-	{
-		bool operator()(It1 i1, It2 i2) const 
-			{
-				return (*i1 == *i2) && eq_n_t<It1, It2, N-1>()(++i1, ++i2) ;
-			}
-	};
-
+    template<typename It1, typename It2, int N> struct eq_n_t
+    {
+        bool operator()(It1 i1, It2 i2) const /* generic recursive implementation */
+        {
+            return (*i1 == *i2) && eq_n_t<It1, It2, N-1>()(++i1, ++i2) ;
+        }
+    };
+    template<typename It1, typename It2> struct eq_n_t<It1, It2, 0> {
+        bool operator()(It1 i1, It2 i2) const /* partial specialization for N=0 */
+        {
+            return true ;
+        }
+    };
 /**
- * @TODO add comment
+ * Compares two sequences for equality when the size in know at compile time.
+ * Uses template metaprogramming to ensure loop unrolling.
  *
- * @param   
- * @return <ReturnValue>
+ * @param i1 : iterator to the beginning of the first sequence
+ * @param i2 : iterator to the beginning of the second sequence
+ * @return true iff *i1 == *i2 &&...&& *(i1+N-1) == *(i2+N-1)
  */
-	template<typename It1, typename It2> struct eq_n_t<It1, It2, 0> {
-		bool operator()(It1 i1, It2 i2) const {
-			return true ;
-		}
-	};
-
-/**
- * @TODO add comment
- *
- * @param i1    
- * @param i2    
- * @return <ReturnValue>
- */
-	template<int N, typename It1, typename It2> bool eq_n(It1 i1, It2 i2) {
-		return eq_n_t<It1, It2, N>()(i1, i2);
-	}
+    template<int N, typename It1, typename It2> bool eq_n(It1 i1, It2 i2)
+    {
+        return eq_n_t<It1, It2, N>()(i1, i2);
+    }
 
 /**
  * ensure most efficient inlining irrespectively of optimization compiler options for ids of nsiz=6 ints
  *
- * @param id1
- * @param id2
+ * we make all the (4)  combinations of (const or not) arguments.
+ *
+ * @param i1 : int* beginning of the first int[6]
+ * @param i2 : int* beginning of the second int[6]
+ * @return true iff *i1 == *i2 &&...&& *(i1+5) == *(i2+5)
  */
-	template<> bool eq_n<6,int const*, int const*>(int const* id1, int const* id2) {
-		return (id1[0]==id2[0]) && (id1[1]==id2[1]) && (id1[2]==id2[2]) && (id1[3]==id2[3]) && (id1[4]==id2[4]) && (id1[5]==id2[5]);
-	}
+    template<> bool eq_n<6,int const*, int const*>(int const* id1, int const* id2)
+    {
+        return (id1[0]==id2[0]) && (id1[1]==id2[1]) && (id1[2]==id2[2]) && (id1[3]==id2[3]) && (id1[4]==id2[4]) && (id1[5]==id2[5]);
+    }
 
 /**
- * @TODO add comment
- *
- * @param id1
- * @param id2
+ * See above.
  */
     template<> bool eq_n<6,int const*, int*>(int const* id1, int* id2)
-	{
-		return (id1[0]==id2[0]) && (id1[1]==id2[1]) && (id1[2]==id2[2]) && (id1[3]==id2[3]) && (id1[4]==id2[4]) && (id1[5]==id2[5]);
+    {
+        return (id1[0]==id2[0]) && (id1[1]==id2[1]) && (id1[2]==id2[2]) && (id1[3]==id2[3]) && (id1[4]==id2[4]) && (id1[5]==id2[5]);
     }
 
 /**
- * @TODO add comment
- *
- * @param id1
- * @param id2
+ * See above.
  */
     template<> bool eq_n<6,int*, int const*>(int* id1, int const* id2)
-	{
-		return (id1[0]==id2[0]) && (id1[1]==id2[1]) && (id1[2]==id2[2]) && (id1[3]==id2[3]) && (id1[4]==id2[4]) && (id1[5]==id2[5]);
+    {
+        return (id1[0]==id2[0]) && (id1[1]==id2[1]) && (id1[2]==id2[2]) && (id1[3]==id2[3]) && (id1[4]==id2[4]) && (id1[5]==id2[5]);
     }
 
 
 /**
- * @TODO add comment
- *
- * @param id1
- * @param id2
+ * See above.
  */
     template<> bool eq_n<6,int*, int*>(int* id1, int* id2)
-	{
-		return (id1[0]==id2[0]) && (id1[1]==id2[1]) && (id1[2]==id2[2]) && (id1[3]==id2[3]) && (id1[4]==id2[4]) && (id1[5]==id2[5]);
+    {
+        return (id1[0]==id2[0]) && (id1[1]==id2[1]) && (id1[2]==id2[2]) && (id1[3]==id2[3]) && (id1[4]==id2[4]) && (id1[5]==id2[5]);
     }
 
 /**
- * @TODO add comment
+ * C++ does not allow function template partial specialization so less_n<> delegates to work to a less_n_t<> struct
+ * which implements the function in operator().
  *
- * @param i1    
- * @param i2    
- * @return <ReturnValue>
+ * @param i1 : iterator to the beginning of the first sequence
+ * @param i2 : iterator to the beginning of the second sequence
+ * @return true iff [i1...i1+N[ < [i2...i2+N[ in lexicographical order
  */
-	template<typename It1, typename It2, int N> struct less_n_t 
-	{
-		bool operator()(It1 i1, It2 i2) const 
-			{
-				return (*i1<*i2) ? true : ( (*i1>*i2) ? false : less_n_t<It1, It2, N-1>()(++i1, ++i2));
-			}
-	};
+
+    template<typename It1, typename It2, int N> struct less_n_t
+    {
+        bool operator()(It1 i1, It2 i2) const /*/ generic recursive case */
+        {
+            return (*i1<*i2) ? true : ( (*i1>*i2) ? false : less_n_t<It1, It2, N-1>()(++i1, ++i2));
+        }
+    };
+
+    template<typename It1, typename It2> struct less_n_t<It1, It2, 0>
+    {
+        bool operator()(It1 i1, It2 i2) const /* special case for empty sequences */
+        {
+            return false ;
+        }
+    };
 
 /**
- * @TODO add comment
+ * Compares two sequences for lexicographical order when the size in know at compile time.
+ * Uses template metaprogramming to ensure loop unrolling.
  *
- * @param i1    
- * @param i2    
- * @return <ReturnValue>
+ * @param i1 : iterator to the beginning of the first sequence
+ * @param i2 : iterator to the beginning of the second sequence
+ * @return true iff [i1...i1+N[ < [i2...i2+N[ in lexicographical order
  */
-	template<typename It1, typename It2> struct less_n_t<It1, It2, 0> 
-	{
-		bool operator()(It1 i1, It2 i2) const 
-			{
-				return false ;
-			}
-	};
+    template<int N, typename It1, typename It2> bool less_n(It1 i1, It2 i2)
+    {
+        return less_n_t<It1, It2, N>()(i1, i2);
+    }
 
 /**
- * @TODO add comment
+ * C++ does not allow function template partial specialization so copy_n<> delegates the work to a copy_n_t<> struct
+ * which implements the function in operator().
  *
- * @param i1    
- * @param i2    
- * @return <ReturnValue>
+ * @param i1 : iterator to the beginning of the first sequence
+ * @param i2 : iterator to the beginning of the second sequence
+ * @return i1+N
  */
-	template<int N, typename It1, typename It2> bool less_n(It1 i1, It2 i2)
-	{
-		return less_n_t<It1, It2, N>()(i1, i2);
-	}
+
+    template<typename In, typename Out, int N> struct copy_n_t
+    {
+        Out operator()(In in, Out out) const /* generic case for N>0 */
+        {
+            *out= *in;
+            return copy_n_t<In, Out, N-1>()(++in, ++out);
+        }
+    };
+
+    template<typename In, typename Out> struct copy_n_t<In, Out, 0>
+    {
+        Out operator()(In, Out out) const /* special case for empty sequences */
+        {
+            return out;
+        }
+    };
 
 /**
- * @TODO add comment
+ * Copy one sequence to another when the size in know at compile time.
+ * Uses template metaprogramming to ensure loop unrolling.
  *
- * @param in
- * @param out    
- * @return <ReturnValue>
+ * @param i1 : iterator to the beginning of the input sequence
+ * @param i2 : iterator to the beginning of the output sequence
+ * @return in+N
  */
-	template<typename In, typename Out, int N> struct copy_n_t
-	{
-		Out operator()(In in, Out out) const
-			{
-				*out= *in;
-				return copy_n_t<In, Out, N-1>()(++in, ++out);
-			}
-	};
-
-/**
- * @TODO add comment
- *
- * @param in
- * @param out    
- * @return <ReturnValue>
- */
-	template<typename In, typename Out> struct copy_n_t<In, Out, 0>
-	{
-		Out operator()(In, Out out) const
-			{
-				return out;
-			}
-	};
-
-/**
- * @TODO add comment
- *
- * @param in
- * @param out    
- * @return <ReturnValue>
- */
-	template<int N, typename In, typename Out> Out copy_n(In in, Out out)
-	{
-		return copy_n_t<In, Out, N>()(in, out);
-	}
+    template<int N, typename In, typename Out> Out copy_n(In in, Out out)
+    {
+        return copy_n_t<In, Out, N>()(in, out);
+    }
 
 }
 #endif
