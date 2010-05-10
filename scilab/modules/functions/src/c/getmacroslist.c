195a351e592a7832203caa55cc66adf7f67ec1b2
Allan CORNET <allan.cornet@scilab.org>
2010-05-10 11:10:47 +0200

    get macros on librairies and on stack (request Calixte for xpad)
    Change-Id: Ie643143ab35e02881a5b52e6b797625c844c7f9f

--- scilab/modules/functions/src/c/getmacroslist.c
+++ scilab/modules/functions/src/c/getmacroslist.c
@@ -1,7 +1,7 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2007 - INRIA - Allan CORNET
- * Copyright (C) 2009 - DIGITEO - Allan CORNET
+ * Copyright (C) 2009-2010 - DIGITEO - Allan CORNET
  * 
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
@@ -11,97 +11,257 @@
  *
  */
 #include <stdlib.h>
+#include "api_scilab.h"
+#include "stack-c.h"
 #include "getmacroslist.h"
 #include "librarieslist.h"
 #include "libraryinfo.h"
 #include "MALLOC.h"
 #include "freeArrayOfString.h"
+#include "getvariablesname.h"
+#ifdef _MSC_VER
+#include "strdup_windows.h"
+#endif
 /*--------------------------------------------------------------------------*/
-static int getsizemacroslist(void);
+static char **getmacrosonstacklist(int *sizearray);
+static char **getmacrosinlibrarieslist(int *sizearray);
+static int getsizemacrosinlibrarieslist(void);
+static void RemoveDuplicateStrings(char **Strings, int *SizeStrings);
+static int cmpstr( const void *a ,const void *b);
 /*--------------------------------------------------------------------------*/
 char **getmacroslist(int *sizearray)
 {
-	char **dictionary = NULL;
-	int sizedictionary = getsizemacroslist();
+    char **macroslist = NULL;
 
-	*sizearray = 0;
+    char **macrosonstack = NULL;
+    int sizemacrosonstack = 0;
+    char **macrosinlibraries = NULL;
+    int sizemacrosinlibraries = 0;
 
-	if (sizedictionary > 0)
-	{
-		int sizelibraries = 0;
-		char **libraries = getlibrarieslist(&sizelibraries);
+    *sizearray = 0;
 
-		dictionary = (char**)MALLOC(sizeof(char*)*sizedictionary);
+    macrosonstack = getmacrosonstacklist(&sizemacrosonstack);
+    macrosinlibraries = getmacrosinlibrarieslist(&sizemacrosinlibraries);
 
-		if (dictionary)
-		{
-			int m = 0;
-			if (libraries)
-			{
-				int i = 0;
-				for (i = 0;i < sizelibraries; i++)
-				{
-					int j = 0;
-					char **macros = NULL;
-					int sizemacros = 0;
+    if (macrosinlibraries || macrosonstack)
+    {
+        int nbMacros = sizemacrosonstack + sizemacrosinlibraries;
+        if (sizemacrosonstack)
+        {
+            macroslist = (char**)REALLOC(macrosinlibraries, sizeof(char*) * nbMacros);
+            if (macroslist)
+            {
+                int i = 0;
+                int j = 0;
+                for (i = sizemacrosinlibraries; i < nbMacros; i++)
+                {
+                    macroslist[i] = strdup(macrosonstack[j++]);
+                }
 
-					macros = getlistmacrosfromlibrary(libraries[i],&sizemacros);
+                qsort(macroslist, nbMacros, sizeof(char*), cmpstr);
+                RemoveDuplicateStrings(macroslist, &nbMacros);
+                macroslist = (char**)REALLOC(macroslist, sizeof(char*) * nbMacros);
+                *sizearray = nbMacros;
+            }
+            else
+            {
+                macroslist = macrosinlibraries;
+                *sizearray = sizemacrosinlibraries;
+            }
+        }
+        else
+        {
+            macroslist = macrosinlibraries;
+            *sizearray = sizemacrosinlibraries;
+        }
+    }
 
-					if (macros)
-					{
-						for (j=0;j<sizemacros;j++)
-						{
-							dictionary[m] = macros[j];
-							m++;
-						}
-						FREE(macros);
-						macros = NULL;
-					}
-				}
-				*sizearray = sizedictionary;
-			}
-		}
-		else
-		{
-			*sizearray = 0;
-		}
-		freeArrayOfString(libraries, sizelibraries);
-	}
-	return dictionary;
+    if (macrosonstack)
+    {
+        freeArrayOfString(macrosonstack, sizemacrosonstack);
+        macrosonstack = NULL;
+    }
+    return macroslist;
 }
 /*--------------------------------------------------------------------------*/
-static int getsizemacroslist(void)
+static char **getmacrosonstacklist(int *sizearray)
 {
-	int sizedictionary = 0;
-	int sizelibraries = 0;
-	char **libraries = getlibrarieslist(&sizelibraries);
+    char **macroslist = NULL;
 
-	if (libraries)
-	{
-		int i = 0;
-		for (i = 0;i < sizelibraries; i++)
-		{
-			if (libraries[i])
-			{
-				char **macros = NULL;
-				int sizemacros = 0;
+    char **localvariables = NULL;
+    int sizelocalvariables = 0;
 
-				macros = getlistmacrosfromlibrary(libraries[i],&sizemacros);
-				if (macros)
-				{
-					sizedictionary = sizedictionary + sizemacros;
+    *sizearray = 0;
+    localvariables = getLocalVariablesName(&sizelocalvariables, FALSE);
+    if (localvariables)
+    {
+        int i = 0;
+        int nbMacros = 0;
 
-					freeArrayOfString(macros, sizemacros);
-				}
+        for (i = 0; i < sizelocalvariables; i++)
+        {
+            int iType = 0;
+            SciErr sciErr = getNamedVarType(pvApiCtx, localvariables[i], &iType);
+            if (!sciErr.iErr)
+            {
+                if (iType == sci_c_function) nbMacros++;
+            }
+        }
 
-				FREE(libraries[i]);
-				libraries[i] = NULL;
-			}
-			
-		}
-		FREE(libraries);
-		libraries = NULL;
-	}
-	return sizedictionary;
+        if (nbMacros)
+        {
+            macroslist = (char**)MALLOC(sizeof(char*) * nbMacros);
+        }
+        
+        if (macroslist)
+        {
+            nbMacros = 0;
+            for (i = 0; i < sizelocalvariables; i++)
+            {
+                int iType = 0;
+                SciErr sciErr = getNamedVarType(pvApiCtx, localvariables[i], &iType);
+                if (!sciErr.iErr)
+                {
+                    if (iType == sci_c_function) 
+                    {
+                        macroslist[nbMacros++] = strdup(localvariables[i]);
+                    }
+                }
+            }
+            *sizearray = nbMacros;
+        }
+
+        freeArrayOfString(localvariables, sizelocalvariables);
+        localvariables = NULL;
+    }
+    return macroslist;
 }
 /*--------------------------------------------------------------------------*/
+static char **getmacrosinlibrarieslist(int *sizearray)
+{
+    char **dictionary = NULL;
+    int sizedictionary = getsizemacrosinlibrarieslist();
+
+    *sizearray = 0;
+
+    if (sizedictionary > 0)
+    {
+        int sizelibraries = 0;
+        char **libraries = getlibrarieslist(&sizelibraries);
+
+        dictionary = (char**)MALLOC(sizeof(char*)*sizedictionary);
+
+        if (dictionary)
+        {
+            int m = 0;
+            if (libraries)
+            {
+                int i = 0;
+                for (i = 0;i < sizelibraries; i++)
+                {
+                    int j = 0;
+                    char **macros = NULL;
+                    int sizemacros = 0;
+
+                    macros = getlistmacrosfromlibrary(libraries[i],&sizemacros);
+
+                    if (macros)
+                    {
+                        for (j=0;j<sizemacros;j++)
+                        {
+                            dictionary[m] = macros[j];
+                            m++;
+                        }
+                        FREE(macros);
+                        macros = NULL;
+                    }
+                }
+                *sizearray = sizedictionary;
+            }
+        }
+        else
+        {
+            *sizearray = 0;
+        }
+        freeArrayOfString(libraries, sizelibraries);
+    }
+    return dictionary;
+}
+/*--------------------------------------------------------------------------*/
+static int getsizemacrosinlibrarieslist(void)
+{
+    int sizedictionary = 0;
+    int sizelibraries = 0;
+    char **libraries = getlibrarieslist(&sizelibraries);
+
+    if (libraries)
+    {
+        int i = 0;
+        for (i = 0;i < sizelibraries; i++)
+        {
+            if (libraries[i])
+            {
+                char **macros = NULL;
+                int sizemacros = 0;
+
+                macros = getlistmacrosfromlibrary(libraries[i],&sizemacros);
+                if (macros)
+                {
+                    sizedictionary = sizedictionary + sizemacros;
+
+                    freeArrayOfString(macros, sizemacros);
+                }
+
+                FREE(libraries[i]);
+                libraries[i] = NULL;
+            }
+
+        }
+        FREE(libraries);
+        libraries = NULL;
+    }
+    return sizedictionary;
+}
+/*--------------------------------------------------------------------------*/
+static void RemoveDuplicateStrings(char **Strings,int *SizeStrings)
+{
+    int fin = 0, i = 0;
+    int newsize = *SizeStrings;
+
+    for(fin = *SizeStrings - 1; fin > 0; fin--)
+    {
+        int Sorted = FALSE;
+
+        for(i = 0; i < fin; i++)
+        {
+            if (Strings[i])
+            {
+                if(strcmp(Strings[i], Strings[i+1]) == 0)
+                {
+                    FREE(Strings[i + 1]);
+                    Strings[i + 1] = NULL;
+                    Sorted = TRUE;
+                    newsize--;
+                }
+            }
+            else
+            {
+                Strings[i] = Strings[i+1];
+                Strings[i+1] = NULL;
+                Sorted = TRUE;
+            }
+        }
+
+        if (!Sorted)
+        {
+            break;
+        }
+    }
+    *SizeStrings = newsize;
+}
+/*--------------------------------------------------------------------------*/ 
+static int cmpstr( const void *a ,const void *b)
+{
+    return strcmp(*(const char **)a, *(const char **)b );
+}
+/*--------------------------------------------------------------------------*/ 
