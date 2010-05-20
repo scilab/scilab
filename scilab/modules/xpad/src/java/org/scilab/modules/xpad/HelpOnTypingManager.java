2b34da7492625fc1fa4a9e1708819b474f85a966
Calixte DENIZET <calixte.denizet@ac-rennes.fr>
2010-05-20 12:46:08 +0200

    Fix a bug in the autocompletion
    Change-Id: I1b3e2018b07967b71ac5fa1d066300f3afcf8924

--- scilab/modules/xpad/src/java/org/scilab/modules/xpad/HelpOnTypingManager.java
+++ scilab/modules/xpad/src/java/org/scilab/modules/xpad/HelpOnTypingManager.java
@@ -15,6 +15,7 @@ package org.scilab.modules.xpad;
 import java.awt.event.KeyListener;
 import java.awt.event.KeyEvent;
 
+import javax.swing.text.Element;
 import javax.swing.text.BadLocationException;
 
 /**
@@ -25,6 +26,7 @@ public class HelpOnTypingManager implements KeyListener {
 
     private ScilabEditorPane textPane;
     private ScilabDocument doc;
+    private Element root;
 
     /**
      * Constructor
@@ -33,6 +35,7 @@ public class HelpOnTypingManager implements KeyListener {
     public HelpOnTypingManager(ScilabEditorPane pane) {
         this.textPane = pane;
         this.doc = (ScilabDocument) pane.getDocument();
+        this.root = this.doc.getDefaultRootElement();
     }
 
     /**
@@ -53,30 +56,33 @@ public class HelpOnTypingManager implements KeyListener {
      */
     public void keyTyped(KeyEvent e) {
         char c = e.getKeyChar();
-        if (c != KeyEvent.CHAR_UNDEFINED && textPane.getSelectionStart() == textPane.getSelectionEnd() && e.getModifiers() == 0) {
+        if (c != KeyEvent.CHAR_UNDEFINED && textPane.getSelectionStart() == textPane.getSelectionEnd()) {
             int pos = textPane.getCaretPosition();
-
-            if (c == ' ') {
-                try {
-                    doc.insertString(pos, " ", null);
-                    e.consume();
-                    KeywordEvent kwe = textPane.getKeywordEvent(pos);
-                    switch (kwe.getType()) {
-                    case ScilabLexerConstants.OSKEYWORD :
-                        doc.insertString(pos + 1, "\nend", null);
-                        textPane.getIndentManager().indentDoc(pos + 1, pos + 4);
-                        textPane.setCaretPosition(pos + 1);
-                        break;
-                    case ScilabLexerConstants.FKEYWORD :
-                        /* We have 'function' or 'endfunction' */
-                        if ("f".equals(doc.getText(kwe.getStart(), 1))) {
-                            doc.insertString(pos + 1, "()\nendfunction", null);
-                            textPane.getIndentManager().indentDoc(pos + 3, pos + 14);
+            if (c == ' ' && e.getModifiers() == 0) {
+                int end = root.getElement(root.getElementIndex(pos)).getEndOffset() - 1;
+                /* the following test is used to know if an insertion in the line is done */
+                if (pos == end) {
+                    try {
+                        doc.insertString(pos, " ", null);
+                        e.consume();
+                        KeywordEvent kwe = textPane.getKeywordEvent(pos);
+                        switch (kwe.getType()) {
+                        case ScilabLexerConstants.OSKEYWORD :
+                            doc.insertString(pos + 1, "\nend", null);
+                            textPane.getIndentManager().indentDoc(pos + 1, pos + 4);
                             textPane.setCaretPosition(pos + 1);
+                            break;
+                        case ScilabLexerConstants.FKEYWORD :
+                            /* We have 'function' or 'endfunction' */
+                            if ("f".equals(doc.getText(kwe.getStart(), 1))) {
+                                doc.insertString(pos + 1, "()\nendfunction", null);
+                                textPane.getIndentManager().indentDoc(pos + 3, pos + 14);
+                                textPane.setCaretPosition(pos + 1);
+                            }
+                            break;
                         }
-                        break;
-                    }
-                } catch (BadLocationException exc) { }
+                    } catch (BadLocationException exc) { }
+                }
             } else {
                 String str = null;
                 switch (c) {
