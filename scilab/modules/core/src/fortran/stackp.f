3bb89995c9512f34f526c5dca4c415f7e5f59a5b
Serge Steer <serge.steer@scilab.org>
2010-05-31 15:18:15 +0200

    bug 7189 fix, this bug is a consequence of the bug 7019 fix
    Change-Id: Id3be047289895b17dd381c84016e4c0266d14083

--- scilab/modules/core/src/fortran/stackp.f
+++ scilab/modules/core/src/fortran/stackp.f
@@ -63,6 +63,7 @@ c     find the scope where the variable has to be stored
       if(macr.ne.0.or.paus.ne.0) then
 c     .  inside a macro or a pause
          k=lpt(1) - (13+nsiz)
+      
          if(rstk(pt).eq.504) then
 c     .     [...]=resume(....) case , use the upper scope
             lpt1=lin(k+1)
@@ -75,8 +76,6 @@ c     .     [...]=resume(....) case , use the upper scope
 c     .  main scope
          last=isiz
       endif
-
-
 c
 c     does variable already exist
       vtk=0
