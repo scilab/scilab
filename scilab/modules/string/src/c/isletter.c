ef654f6bd8ec5042d6db7e9ebfa6b90872eafdc0
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2010-06-08 13:29:23 +0200

    Implicit declaration of isw* removed (clang warning)
    Change-Id: I3f6d50e9f524130ca24772ddf852b6e94211349f

--- scilab/modules/string/src/c/isletter.c
+++ scilab/modules/string/src/c/isletter.c
@@ -1,4 +1,3 @@
-
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) INRIA - Allan CORNET
@@ -14,6 +13,7 @@
 /*--------------------------------------------------------------------------*/
 #include <string.h>
 #include <ctype.h>
+#include <wctype.h> /* iswalpha */
 #include "isletter.h"
 #include "MALLOC.h"
 
