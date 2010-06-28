4cb9efd8d067c0cafe4b0215b0399d82ace52157
Artem GLEBOV <artem.glebov@gmail.com>
2010-06-28 17:53:28 +0300

    improvement of comments in api_string.h
    * changed "create"->"get " in comments for
    createMatrixOfString(), createSingleString(),
    createSingleWideString(), createNamedSingleString(),
    createNamedSingleWideString()
    * removed _piAddress from parameters of createMatrixOfString(),
    createMatrixOfWideString(), createNamedMatrixOfString(),
    createNamedMatrixOfWideString()
    * changed "_iRows"->"_piRows", "_iCols"->"_piCols" where the
    arguments are actually pointers
    * changed the type of parameters _piRows, _piCols "in"->"out"
    in readNamedMatrixOfString(), readNamedMatrixOfWideString()
    * some minor formatting improvements
    
    Change-Id: I5d35e431b75e478a46191a1853071bbbd7957227

--- scilab/modules/api_scilab/includes/api_string.h
+++ scilab/modules/api_scilab/includes/api_string.h
@@ -19,55 +19,50 @@
 extern "C" {
 #endif
 
-/*****************************/
+/*******************************/
 /*   string matrix functions   */
-/*****************************/
+/*******************************/
 
 /**
  * Get strings variable data
  * @param[in] _piAddress variable address
- * @param[out] _piRows return number of row 
- * @param[out] _piCols return number of column
- * @param[out] _piLength return strings length
+ * @param[out] _piRows return number of rows
+ * @param[out] _piCols return number of columns
+ * @param[out] _piLength return strings lengths
  * @param[out] _pstStrings return array of char
- * @return if the operation successed (0) or not ( !0 )
+ * @return if the operation succeeded (0) or not ( !0 )
  */
 SciErr getMatrixOfString(void* _pvCtx, int* _piAddress, int* _piRows, int* _piCols, int* _piLength, char** _pstStrings);
 
 /**
- * Get strings variable data
+ * Get wide strings variable data
  * @param[in] _piAddress variable address
- * @param[out] _piRows return number of row 
- * @param[out] _piCols return number of column
- * @param[out] _piLength return strings length
- * @param[out] _pstStrings return array of wide char
- * @return if the operation successed (0) or not ( !0 )
+ * @param[out] _piRows return number of rows
+ * @param[out] _piCols return number of columns
+ * @param[out] _piwLength return strings length
+ * @param[out] _pwstStrings return array of wide char
+ * @return if the operation succeeded (0) or not ( !0 )
  */
-
-SciErr getMatrixOfWideString(void* _pvCtx, int* _piAddress, int* _piRows, int* _piCols, int* _piLength, wchar_t** _pwstStrings);
+SciErr getMatrixOfWideString(void* _pvCtx, int* _piAddress, int* _piRows, int* _piCols, int* _piwLength, wchar_t** _pwstStrings);
 
 /**
- * Get strings variable data
+ * Create strings variable data
  * @param[in] _iVar variable number
- * @param[in] _iRows Number of row
- * @param[in] _iCols Number of column
+ * @param[in] _iRows number of rows
+ * @param[in] _iCols number of columns
  * @param[in] _pstStrings array of strings ( null terminated )
- * @param[out] _piAddress return pointer on new variable
- * @return if the operation successed (0) or not ( !0 )
+ * @return if the operation succeeded (0) or not ( !0 )
  */
-
 SciErr createMatrixOfString(void* _pvCtx, int _iVar, int _iRows, int _iCols, char** _pstStrings);
 
 /**
 * Create wide strings variable data
 * @param[in] _iVar variable number
-* @param[in] _iRows Number of row
-* @param[in] _iCols Number of column
-* @param[in] _pstStrings array of strings ( null terminated )
-* @param[out] _piAddress return pointer on new variable
-* @return if the operation successed (0) or not ( !0 )
+* @param[in] _iRows Number of rows
+* @param[in] _iCols Number of columns
+* @param[in] _pwstStrings array of strings ( null terminated )
+* @return if the operation succeeded (0) or not ( !0 )
 */
-
 SciErr createMatrixOfWideString(void* _pvCtx, int _iVar, int _iRows, int _iCols, wchar_t** _pwstStrings);
 
 /**
@@ -76,45 +71,40 @@ SciErr createMatrixOfWideString(void* _pvCtx, int _iVar, int _iRows, int _iCols,
  * @param[in] _iRows Number of row
  * @param[in] _iCols Number of column
  * @param[in] _pstStrings array of strings ( null terminated )
- * @param[out] _piAddress return pointer on new variable
- * @return if the operation successed (0) or not ( !0 )
+ * @return if the operation succeeded (0) or not ( !0 )
  */
-
 SciErr createNamedMatrixOfString(void* _pvCtx, char* _pstName, int _iRows, int _iCols, char** _pstStrings);
 
 /**
 * Create wide strings variable data
 * @param[in] _pstName variable name
-* @param[in] _iRows Number of row
-* @param[in] _iCols Number of column
-* @param[in] _pstStrings array of wide strings ( null terminated )
-* @param[out] _piAddress return pointer on new variable
-* @return if the operation successed (0) or not ( !0 )
+* @param[in] _iRows number of rows
+* @param[in] _iCols number of columns
+* @param[in] _pwstStrings array of wide strings ( null terminated )
+* @return if the operation succeeded (0) or not ( !0 )
 */
-
 SciErr createNamedMatrixOfWideString(void* _pvCtx, char* _pstName, int _iRows, int _iCols, wchar_t** _pwstStrings);
 
 /**
  * Get strings variable data
  * @param[in] _pstName variable name
- * @param[in] _iRows Number of row
- * @param[in] _iCols Number of column
- * @param[out] _piLength return strings length
+ * @param[out] _piRows number of rows
+ * @param[out] _piCols number of columns
+ * @param[out] _piLength return strings lengths
  * @param[in] _pstStrings array of strings ( null terminated )
- * @return if the operation successed (0) or not ( !0 )
+ * @return if the operation succeeded (0) or not ( !0 )
  */
 SciErr readNamedMatrixOfString(void* _pvCtx, char* _pstName, int* _piRows, int* _piCols, int* _piLength, char** _pstStrings);
 
 /**
 * Get wide strings variable data
 * @param[in] _pstName variable name
-* @param[in] _iRows Number of row
-* @param[in] _iCols Number of column
-* @param[out] _piLength return strings length
-* @param[in] _pstStrings array of strings ( null terminated )
-* @return if the operation successed (0) or not ( !0 )
+* @param[out] _piRows number of rows
+* @param[out] _piCols number of columns
+* @param[out] _piwLength return strings length
+* @param[in] _pstwStrings array of strings ( null terminated )
+* @return if the operation succeeded (0) or not ( !0 )
 */
-
 SciErr readNamedMatrixOfWideString(void* _pvCtx, char* _pstName, int* _piRows, int* _piCols, int* _piwLength, wchar_t** _pwstStrings);
 
 /*shortcut functions*/
@@ -128,15 +118,16 @@ int isStringType(void* _pvCtx, int* _piAddress);
 
 /**
  * check if the variable type is string
- * @param[in] _pstNAme variable name
+ * @param[in] _pstName variable name
  * @return 1 for true and 0 for false
  */
 int isNamedStringType(void* _pvCtx, char* _pstName);
+
 /**
  * Get single string variable data
  * @param[in] _piAddress variable address
  * @param[out] _pstData return allocated string pointer ( null terminated )
- * @return if the operation successed (0) or not ( !0 )
+ * @return if the operation succeeded (0) or not ( !0 )
  */
 int getAllocatedSingleString(void* _pvCtx, int* _piAddress, char** _pstData);
 
@@ -144,27 +135,27 @@ int getAllocatedSingleString(void* _pvCtx, int* _piAddress, char** _pstData);
  * Get single wide string variable data
  * @param[in] _piAddress variable address
  * @param[out] _pwstData return allocated wide string pointer ( null terminated )
- * @return if the operation successed (0) or not ( !0 )
+ * @return if the operation succeeded (0) or not ( !0 )
  */
 int getAllocatedSingleWideString(void* _pvCtx, int* _piAddress, wchar_t** _pwstData);
 
 /**
  * Get strings variable data
  * @param[in] _piAddress variable address
- * @param[out] _piRows return number of row 
- * @param[out] _piCols return number of column
+ * @param[out] _piRows return number of rows
+ * @param[out] _piCols return number of columns
  * @param[out] _pstData return allocated array of string pointer ( null terminated )
- * @return if the operation successed (0) or not ( !0 )
+ * @return if the operation succeeded (0) or not ( !0 )
  */
 int getAllocatedMatrixOfString(void* _pvCtx, int* _piAddress, int* _piRows, int* _piCols, char*** _pstData);
 
 /**
  * Get wide strings variable data
  * @param[in] _piAddress variable address
- * @param[out] _piRows return number of row 
- * @param[out] _piCols return number of column
+ * @param[out] _piRows return number of rows
+ * @param[out] _piCols return number of columns
  * @param[out] _pwstData return allocated array of wide string pointer ( null terminated )
- * @return if the operation successed (0) or not ( !0 )
+ * @return if the operation succeeded (0) or not ( !0 )
  */
 int getAllocatedMatrixOfWideString(void* _pvCtx, int* _piAddress, int* _piRows, int* _piCols, wchar_t*** _pwstData);
 
@@ -173,67 +164,67 @@ int getAllocatedMatrixOfWideString(void* _pvCtx, int* _piAddress, int* _piRows, 
  * Get single named string variable data
  * @param[in] _pstName variable name
  * @param[out] _pstData return allocated string pointer ( null terminated )
- * @return if the operation successed (0) or not ( !0 )
+ * @return if the operation succeeded (0) or not ( !0 )
  */
 int getAllocatedNamedSingleString(void* _pvCtx, char* _pstName, char** _pstData);
 
 /**
  * Get single named string variable data
  * @param[in] _pstName variable name
- * @param[out] _pstData return allocated string pointer ( null terminated )
- * @return if the operation successed (0) or not ( !0 )
+ * @param[out] _pwstData return allocated string pointer ( null terminated )
+ * @return if the operation succeeded (0) or not ( !0 )
  */
 int getAllocatedNamedSingleWideString(void* _pvCtx, char* _pstName, wchar_t** _pwstData);
 
 /**
  * Get named strings variable data
  * @param[in] _pstName variable name
- * @param[out] _iRows Number of row
- * @param[out] _iCols Number of column
+ * @param[out] _piRows number of rows
+ * @param[out] _piCols number of columns
  * @param[out] _pstData return allocated array of string pointer ( null terminated )
- * @return if the operation successed (0) or not ( !0 )
+ * @return if the operation succeeded (0) or not ( !0 )
  */
 int getAllocatedNamedMatrixOfString(void* _pvCtx, char* _pstName, int* _piRows, int* _piCols, char*** _pstData);
 
 /**
  * Get named wide strings variable data
  * @param[in] _pstName variable name
- * @param[out] _iRows Number of row
- * @param[out] _iCols Number of column
+ * @param[out] _piRows number of rows
+ * @param[out] _piCols number of columns
  * @param[out] _pwstData return allocated array of wide string pointer ( null terminated )
- * @return if the operation successed (0) or not ( !0 )
+ * @return if the operation succeeded (0) or not ( !0 )
  */
 int getAllocatedNamedMatrixOfWideString(void* _pvCtx, char* _pstName, int* _piRows, int* _piCols, wchar_t*** _pwstData);
 
 /**
- * Get single string variable data
+ * Create single string variable data
  * @param[in] _iVar variable number
  * @param[in] _pstStrings string ( null terminated )
- * @return if the operation successed (0) or not ( !0 )
+ * @return if the operation succeeded (0) or not ( !0 )
  */
 int createSingleString(void* _pvCtx, int _iVar, char* _pstStrings);
 
 /**
- * Get single wide string variable data
+ * Create single wide string variable data
  * @param[in] _iVar variable number
  * @param[in] _pwstStrings wide string ( null terminated )
- * @return if the operation successed (0) or not ( !0 )
+ * @return if the operation succeeded (0) or not ( !0 )
  */
 int createSingleWideString(void* _pvCtx, int _iVar, wchar_t* _pwstStrings);
 
 /**
- * Get named single string variable data
+ * Create named single string variable data
  * @param[in] _pstName variable name
  * @param[in] _pstStrings string ( null terminated )
- * @return if the operation successed (0) or not ( !0 )
+ * @return if the operation succeeded (0) or not ( !0 )
  */
 int createNamedSingleString(void* _pvCtx, char* _pstName, char* _pstStrings);
 
 /**
- * Get named single wide string variable data
+ * Create named single wide string variable data
  * @param[in] _pstName variable name
  * @param[in] _pwstStrings wide string ( null terminated )
- * @return if the operation successed (0) or not ( !0 )
+ * @return if the operation succeeded (0) or not ( !0 )
  */
 int createNamedSingleWideString(void* _pvCtx, char* _pstName, wchar_t* _pwstStrings);
 
@@ -251,16 +242,16 @@ void freeAllocatedSingleWideString(wchar_t* _pwstData);
 
 /**
  * free data allocated by shortcut functions ( named or not )
- * @param[in] _iRows row number
- * @param[in] _iCols column number
+ * @param[in] _iRows number of rows
+ * @param[in] _iCols number of columns
  * @param[in] _pstData matrix of string data
  */
 void freeAllocatedMatrixOfString(int _iRows, int _iCols, char** _pstData);
 
 /**
  * free data allocated by shortcut functions ( named or not )
- * @param[in] _iRows row number
- * @param[in] _iCols column number
+ * @param[in] _iRows number of rows
+ * @param[in] _iCols number of columns
  * @param[in] _pwstData matrix of wide string data
  */
 void freeAllocatedMatrixOfWideString(int _iRows, int _iCols, wchar_t** _pwstData);
