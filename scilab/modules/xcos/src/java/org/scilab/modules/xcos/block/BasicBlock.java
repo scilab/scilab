a447c1fc818c5cdedf7e1eba46cc27adf8961bfc
Yann Collette <yann.collette@scilab.org>
2010-07-09 10:14:34 +0200

    add the support of implicit C or fortran scicos blocks (AUTOMAT is such a block)
    Change-Id: I9f066c7334cd0a8b19d1e64cdbfa94302dad56ef

--- scilab/modules/xcos/src/java/org/scilab/modules/xcos/block/BasicBlock.java
+++ scilab/modules/xcos/src/java/org/scilab/modules/xcos/block/BasicBlock.java
@@ -173,7 +173,7 @@ public class BasicBlock extends ScilabGraphUniqueObject implements Serializable 
 	 */
 	public static enum SimulationFunctionType {
 		ESELECT(-2.0), IFTHENELSE(-1.0), DEFAULT(0.0), TYPE_1(1.0), TYPE_2(2.0),
-		    TYPE_3(3.0), C_OR_FORTRAN(4.0), SCILAB(5.0), MODELICA(30004.0), UNKNOWN(5.0), OLDBLOCKS(10001.0);
+		    TYPE_3(3.0), C_OR_FORTRAN(4.0), SCILAB(5.0), MODELICA(30004.0), UNKNOWN(5.0), OLDBLOCKS(10001.0), IMPLICIT_C_OR_FORTRAN(10004.0);
 
 		private double value;
 
