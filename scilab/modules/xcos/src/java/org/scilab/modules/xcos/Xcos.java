cf3c8e58a024ef92ccfde4f726b37b12c2d982ff
Clément DAVID <clement.david@scilab.org>
2010-07-26 11:34:50 +0200

    jgraphx-1.4.0.5: update failed 	No changes on mxConnectPreview.createCell
    Change-Id: I73ca577ae2371c530e992732ceff45b1d4e26d51

--- scilab/modules/xcos/src/java/org/scilab/modules/xcos/Xcos.java
+++ scilab/modules/xcos/src/java/org/scilab/modules/xcos/Xcos.java
@@ -60,7 +60,7 @@ public final class Xcos {
 	/*
 	 * Dependencies version
 	 */
-	private static final List<String> MXGRAPH_VERSIONS = Arrays.asList("1.4.0.4");
+	private static final List<String> MXGRAPH_VERSIONS = Arrays.asList("1.4.0.5");
 	private static final List<String> HDF5_VERSIONS = Arrays.asList("[1, 8, 4]");
 	private static final List<String> BATIK_VERSIONS = Arrays.asList("1.7");
 	
