3a490341e0f8bab980b7887c4ddf4f8d92078d0a
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2010-08-01 23:31:55 +0200

    Coding style
    Change-Id: Ib9c5a7c50c2a12cb8d9cbddd24656f2d1552c7b7

--- scilab/modules/output_stream/src/c/msgstore.c
+++ scilab/modules/output_stream/src/c/msgstore.c
@@ -48,12 +48,21 @@ int C2F(msgstore)(char *str,int *n)
   char *line, c;
   int i,count=0;
   if (msg_line_counter >= MAX_MSG_LINES)
+  {
     return(MAX_LINES);
+  }
   if ( (line = (char *) MALLOC((*n + 1)*sizeof(char))) == (char *)0) 
-    return(MEM_LACK);
+  {
+	  return(MEM_LACK);
+  }
   /* do not store\n" */ 
   for ( i= 0 ; i < *n ; i++ ) 
-    if ( (c=str[i]) != '\n' && c != '\r') line[count++]=c;
+  {
+    if ( (c=str[i]) != '\n' && c != '\r')
+	{
+		line[count++]=c;
+	}
+  }
   line[count]='\0';
   msg_buff[msg_line_counter++]=line;
   return 0;
@@ -63,7 +72,9 @@ void C2F(freemsgtable)()
 {
   int k;
   for (k=0 ; k< msg_line_counter ; k++)
+  {
     FREE(msg_buff[k]);
+  }
   msg_line_counter=0;
   err_n = 0;
 }
@@ -76,49 +87,60 @@ int C2F(lasterror)(char *fname, unsigned long fname_len)
   Rhs = Max(0, Rhs);
   CheckRhs(0,1);
   CheckLhs(1,4);
-  if (msg_line_counter == 0) {
+  if (msg_line_counter == 0)
+  {
     CreateVar(1,MATRIX_OF_DOUBLE_DATATYPE,&zero,&zero,&l1);
     LhsVar(1)=1;
-    if (Lhs >= 2) {
+    if (Lhs >= 2) 
+	{
       CreateVar(2,MATRIX_OF_DOUBLE_DATATYPE,&one,&one,&l1);
       *stk(l1) = (double)0.0;
       LhsVar(2)=2;
     }
     /* Francois VOGEL August 2004 - Added initialization of missing lhs vars in case
        there is no current 'last error' (bug 955)*/
-    if (Lhs >= 3) {
+    if (Lhs >= 3)
+    {
       CreateVar(3,MATRIX_OF_DOUBLE_DATATYPE,&one,&one,&l1);
       *stk(l1) = (double)0.0;
       LhsVar(3)=3;
     }
-    if (Lhs >= 4) {
+    if (Lhs >= 4)
+    {
       l1=0;
       CreateVar(4,STRING_DATATYPE, &one,&l1 , &lr);
       strcpy(cstk(lr),"");
       LhsVar(4)=4;
     }
   }
-  else {
+  else
+  {
     clear = 1;
-    if (Rhs==1) {
+    if (Rhs==1)
+    {
       GetRhsVar(1,MATRIX_OF_BOOLEAN_DATATYPE,&m1,&n1,&l1);
       clear = *istk(l1);
     }
-    for (k=0;k<msg_line_counter ; k++) 
-      sz[k]=(int)strlen(msg_buff[k])-1;
+    for (k=0;k<msg_line_counter ; k++)
+	{
+		sz[k]=(int)strlen(msg_buff[k])-1;
+	}
     CreateVarFromPtr(one,MATRIX_OF_STRING_DATATYPE, &msg_line_counter, &one,(void *) msg_buff);
     LhsVar(1) = 1;
-    if (Lhs >= 2) {
+    if (Lhs >= 2)
+    {
       CreateVar(2,MATRIX_OF_DOUBLE_DATATYPE,&one,&one,&l1);
       *stk(l1) = (double)err_n;
       LhsVar(2)=2;
     }
-    if (Lhs >= 3) {
+    if (Lhs >= 3)
+    {
       CreateVar(3,MATRIX_OF_DOUBLE_DATATYPE,&one,&one,&l1);
       *stk(l1) = (double)where;
       LhsVar(3)=3;
     }
-    if (Lhs >= 4) {
+    if (Lhs >= 4)
+    {
       l1=(int)strlen(funname);
       CreateVar(4,STRING_DATATYPE, &one,&l1 , &lr);
       strcpy(cstk(lr),funname);
