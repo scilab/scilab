ee62236fb397921621aec03330e50c4cadb3cd69
Antoine ELIAS <antoine.elias@scilab.org>
2010-11-24 10:18:08 +0100

    add chdir function, link on cd function
    Change-Id: Ic23d4a08144bd1bc3ad6c504e61cdb1695c94c90

--- scilab/modules/fileio/sci_gateway/cpp/fileio_gw.cpp
+++ scilab/modules/fileio/sci_gateway/cpp/fileio_gw.cpp
@@ -24,6 +24,7 @@ using namespace types;
 bool FileioModule::Load()
 {
     symbol::Context::getInstance()->AddFunction(Function::createFunction(L"cd", &sci_chdir, MODULE_NAME));
+    symbol::Context::getInstance()->AddFunction(Function::createFunction(L"chdir", &sci_chdir, MODULE_NAME));
     symbol::Context::getInstance()->AddFunction(Function::createFunction(L"pwd", &sci_pwd, MODULE_NAME));
     symbol::Context::getInstance()->AddFunction(Function::createFunction(L"mopen", &sci_mopen, MODULE_NAME));
     symbol::Context::getInstance()->AddFunction(Function::createFunction(L"mclose", &sci_mclose, MODULE_NAME));
