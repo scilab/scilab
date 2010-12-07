0dabba901c63db446c712a7e99c672ee25f2f7a7
Bruno JOFRET <bruno.jofret@scilab.org>
2010-12-07 14:23:50 +0100

    Fix BUG #8175 : SCI/modules/signal_processing/tests/nonreg_tests/bug_6861.tst freezed Scilab under MacOSX.
    Caused by Lapack Bug: slamch returns wrong values.
    
    Change-Id: I1c6d3ffaab5ee0f275892090c1f7e2ad4e8cb1ce

---
+++ scilab/modules/elementary_functions/src/c/f2c_workaround.c
@@ -0,0 +1,34 @@
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2010 - DIGITEO - Allan CORNET
+ *
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+/*--------------------------------------------------------------------------*/
+/* Workaround for bug 8175 with f2c Visual studio solution ONLY */
+/*--------------------------------------------------------------------------*/
+#include <string.h>
+#include "machine.h"
+/*--------------------------------------------------------------------------*/
+extern double C2F(slamch)();
+/*--------------------------------------------------------------------------*/
+double C2F(radix)(float *n)
+{
+    return C2F(slamch)("b", strlen("b"));
+}
+/*--------------------------------------------------------------------------*/
+double C2F(tiny)(float *n)
+{
+    return C2F(slamch)("u", strlen("u"));
+}
+/*--------------------------------------------------------------------------*/
+double C2F(huge)(float *n)
+{
+    return C2F(slamch)("o", strlen("o"));
+}
+/*--------------------------------------------------------------------------*/
