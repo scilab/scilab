scilab/scilab

commit f9924b536faa82fbb7fdcbc5dcbd821c37826906

Xcos: Link destruction algorithm was not the right one 	Modify SplitBlock#getIn() #getOut1() #getOut2() to handle non ordonned ports 	Add an algorithm to handle link with split destruction
Bug: 8569
Change-Id: Ia88eb9729ba1247a1d462eaa1d1d2c41012e9a20

scilab/modules/xcos/src/java/org/scilab/modules/xcos/block/SplitBlock.java
@@ -21,11 +21,14 @@ import org.scilab.modules.xcos.port.command.CommandPort;
 import org.scilab.modules.xcos.port.control.ControlPort;
 import org.scilab.modules.xcos.port.input.ExplicitInputPort;
 import org.scilab.modules.xcos.port.input.ImplicitInputPort;
+import org.scilab.modules.xcos.port.input.InputPort;
 import org.scilab.modules.xcos.port.output.ExplicitOutputPort;
 import org.scilab.modules.xcos.port.output.ImplicitOutputPort;
+import org.scilab.modules.xcos.port.output.OutputPort;
 import org.scilab.modules.xcos.utils.BlockPositioning;
 
 import com.mxgraph.model.mxGeometry;
+import com.mxgraph.model.mxGraphModel;
 import com.mxgraph.model.mxICell;
 
 /**
@@ -103,23 +106,49 @@ public final class SplitBlock extends BasicBlock {
 	 * @return input port
 	 */
 	public BasicPort getIn() {
-		return (BasicPort) getChildAt(0);
+		return getChild(0, InputPort.class, 1);
 	}
 
 	/**
 	 * @return first output port
 	 */
 	public BasicPort getOut1() {
-		return (BasicPort) getChildAt(1);
+		return getChild(1, OutputPort.class, 1);
 	}
 
 	/**
-	 * @return second ouput port
+	 * @return second output port
 	 */
 	public BasicPort getOut2() {
-		return (BasicPort) getChildAt(2);
+		return getChild(2, OutputPort.class, 2);
 	}
 
+	/**
+	 * Get the child of the kind class from the start to a count.
+	 * @param startIndex the start index (default position)
+	 * @param kind the kind of the port
+	 * @param ordering the ordering of the port 
+	 * @return the found port or null.
+	 */
+	private BasicPort getChild(int startIndex, Class<? extends BasicPort> kind, int ordering) {
+		final int size = children.size();
+		
+		int loopCount = size;
+		for (int i = startIndex; loopCount > 0; i = (i + 1) % size, loopCount--) {
+			Object child = children.get(i);
+			if (kind.isInstance(child)) {
+				BasicPort port = kind.cast(child);
+				
+				if (port.getOrdering() == ordering) {
+					// end of the loop
+					return kind.cast(child);
+				}
+			}
+		}
+		LogFactory.getLog(SplitBlock.class).error("Unable to find a child.");
+		return null;
+	}
+	
 	/**
 	 * delete split block child before delete
 	 */
@@ -171,10 +200,17 @@ public final class SplitBlock extends BasicBlock {
 	 */
 	@Override
 	public boolean isConnectable() {
-		return getChildCount() != 0
-				&& (getIn().getEdgeCount() == 0
-						|| getOut1().getEdgeCount() == 0 || getOut2()
-						.getEdgeCount() == 0);
+		if (getChildCount() != 0) {
+			boolean hasNoEdges = true;
+			for (int i = 0; i < getChildCount() && hasNoEdges; i++) {
+				final mxICell cell = getChildAt(i);
+				hasNoEdges = cell.getEdgeCount() == 0;
+			}
+			
+			return hasNoEdges;
+		} else {
+			return false;
+		}
 	}
 	
 	/**

scilab/modules/xcos/src/java/org/scilab/modules/xcos/graph/XcosDiagram.java
@@ -26,6 +26,8 @@ import java.rmi.server.UID;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -382,8 +384,8 @@ public class XcosDiagram extends ScilabGraph {
 			final int pos = link.findNearestSegment(splitPoint);
 
 			// save points after breaking point
-			final mxPoint[] saveStartPoints = link.getPoints(pos, true);
-			final mxPoint[] saveEndPoints = link.getPoints(pos, false);
+			final List<mxPoint> saveStartPoints = link.getPoints(pos, true);
+			final List<mxPoint> saveEndPoints = link.getPoints(pos, false);
 
 			// disable events
 			getModel().beginUpdate();
@@ -411,21 +413,15 @@ public class XcosDiagram extends ScilabGraph {
 	 * @param points
 	 *            the points
 	 */
-	public void connect(BasicPort src, BasicPort trg, mxPoint[] points) {
+	public void connect(BasicPort src, BasicPort trg, List<mxPoint> points) {
 		BasicLink newLink1 = BasicLink.createLinkFromPorts(src, trg);
 		newLink1.setGeometry(new mxGeometry(0, 0, 80, 80));
-		newLink1.setSource(src);
-		src.insertEdge(newLink1, true);
-		newLink1.setTarget(trg);
-		trg.insertEdge(newLink1, false);
 
 		// add points after breaking point in the new link
 		if (points != null) {
-			for (mxPoint point : points) {
-				newLink1.addPoint(point.getX(), point.getY());
-			}
+			newLink1.getGeometry().setPoints(points);
 		}
-		addCell(newLink1);
+		addCell(newLink1, null, null, src, trg);
 	}
     
     /**
@@ -958,28 +954,151 @@ public class XcosDiagram extends ScilabGraph {
 			return super.removeCells(cells, includeEdges);
 		}
 		
-		final ArrayList<Object> removedCells = new ArrayList<Object>(Arrays.asList(cells));
+		/*
+		 * First remove all links connected to a removed Split if applicable 
+		 */
+		final Object[] initialCells;
+		if (includeEdges) {
+			initialCells = addAllEdges(cells);
+		} else {
+			initialCells = cells;
+		}
 		
-		for (final Object cell : cells) {
-			/*
-			 * Remove split blocks
-			 */
+		// stash used on the loop
+		final LinkedList<Object> loopCells = new LinkedList<Object>(Arrays.asList(initialCells));
+		// the cells that need to be really 
+		final HashSet<Object> removedCells = new HashSet<Object>(loopCells);
+		// couple of cells to reconnect
+		final ArrayList<BasicPort[]> connectedCells = new ArrayList<BasicPort[]>();
+		final ArrayList<List<mxPoint>> connectedPoints = new ArrayList<List<mxPoint>>();
+		
+		/*
+		 * Then loop on the algorithm to select the right edges
+		 */
+		// /!\ not bounded algorithm
+		while (loopCells.size() > 0) {
+			Object cell = loopCells.pop();
+			
 			if (cell instanceof BasicLink) {
+				/*
+				 * Add any split to a link
+				 */
 				final mxICell src = ((BasicLink) cell).getSource().getParent();
 				final mxICell target = ((BasicLink) cell).getTarget().getParent();
 				
 				if (src instanceof SplitBlock) {
-					removedCells.add(src);
+					if (removedCells.add(src)) {
+						loopCells.add(src);
+					}
 				}
 				
 				if (target instanceof SplitBlock) {
-					removedCells.add(target);
+					if (removedCells.add(target)) {
+						loopCells.add(target);
+					}
+				}
+			} else if (cell instanceof SplitBlock) {
+				final SplitBlock splitBlock = (SplitBlock) cell;
+				
+				/*
+				 * Remove related connection or not and reconnect.
+				 */
+				
+				if (splitBlock.getIn().getEdgeCount() == 0
+						|| splitBlock.getOut1().getEdgeCount() == 0
+						|| splitBlock.getOut2().getEdgeCount() == 0) {
+					// corner case, all links will be removed
+					continue;
 				}
 				
+				final mxICell inLink = splitBlock.getIn().getEdgeAt(0);
+				final mxICell out1Link = splitBlock.getOut1().getEdgeAt(0);
+				final mxICell out2Link = splitBlock.getOut2().getEdgeAt(0);
+				
+				/*
+				 * Explicit case, if the in link is deleted; all the out links also should.
+				 */
+				if (inLink instanceof ExplicitLink && removedCells.contains(inLink)) {
+					if (removedCells.add(out1Link)) {
+						loopCells.add(out1Link);
+					}
+					
+					if (removedCells.add(out2Link)) {
+						loopCells.add(out2Link);
+					}
+				}
+				
+				/*
+				 * Global case reconnect if not removed
+				 */
+				BasicPort[] connection = null;
+				List<mxPoint> points = null;
+				if (!removedCells.contains(inLink) && !removedCells.contains(out1Link)) {
+					connection = new BasicPort[] {(BasicPort) inLink.getTerminal(true), (BasicPort) out1Link.getTerminal(false)};
+					points = getDirectPoints(splitBlock, inLink, out1Link);
+				} else if (!removedCells.contains(inLink) && !removedCells.contains(out2Link)) {
+					connection = new BasicPort[] {(BasicPort) inLink.getTerminal(true), (BasicPort) out2Link.getTerminal(false)};
+					points = getDirectPoints(splitBlock, inLink, out2Link);
+				} else if (!removedCells.contains(out1Link) && !removedCells.contains(out2Link)) {
+					// only implicit case, log otherwise
+					if (out1Link instanceof ImplicitLink || out2Link instanceof ImplicitLink) {
+						LOG.error("Reconnection failed for explicit links");
+					}
+					connection = new BasicPort[] {(BasicPort) out1Link.getTerminal(false), (BasicPort) out2Link.getTerminal(false)};
+					points = getDirectPoints(splitBlock, out1Link, out2Link);
+				}
+				
+				if (connection != null) {
+					connectedCells.add(connection);
+					connectedPoints.add(points);
+				}
 			}
 		}
 		
-		return super.removeCells(removedCells.toArray(), includeEdges);
+		final Object[] ret;
+		getModel().beginUpdate();
+		try {
+			ret = super.removeCells(removedCells.toArray(), includeEdges);
+			for (int i = 0; i < connectedCells.size(); i++) {
+				final BasicPort[] connection = connectedCells.get(i);
+				final List<mxPoint> points = connectedPoints.get(i);
+				connect(connection[0], connection[1], points);
+			}
+		} finally {
+			getModel().endUpdate();
+		}
+		return ret;
+	}
+
+
+	/**
+	 * Get the direct points from inLink.getSource() to outLink.getTarget().
+	 * 
+	 * @param splitBlock the current splitblock (added as a mid-point)
+	 * @param inLink the link before the split
+	 * @param outLink the link after the split
+	 * @return the points
+	 */
+	private List<mxPoint> getDirectPoints(final SplitBlock splitBlock,
+			final mxICell inLink, final mxICell outLink) {
+		List<mxPoint> points;
+		// add the points before the split
+		points = new ArrayList<mxPoint>();
+		if (inLink.getGeometry().getPoints() != null) {
+			points.addAll(inLink.getGeometry().getPoints());
+		}
+		
+		// add a new point at the split location 
+		points.add(new mxPoint(
+				snap(splitBlock.getGeometry().getCenterX()),
+				snap(splitBlock.getGeometry().getCenterY())));
+		
+		// add the points after the split
+		if (outLink.getGeometry().getPoints() != null) {
+			points.addAll(outLink.getGeometry().getPoints());
+		}
+		
+		return points;
 	}
 	
 	/**

scilab/modules/xcos/src/java/org/scilab/modules/xcos/link/BasicLink.java
@@ -98,31 +98,25 @@ public abstract class BasicLink extends ScilabGraphUniqueObject {
 	 *            otherwise
 	 * @return The points
 	 */
-    public mxPoint[] getPoints(int index, boolean fromStart) { 
+	public List<mxPoint> getPoints(int index, boolean fromStart) {
 
-	if (getGeometry() == null || getGeometry().getPoints() == null) { 
-	    return new mxPoint[0];
-	}
+		if (getGeometry() == null || getGeometry().getPoints() == null) {
+			return new ArrayList<mxPoint>();
+		}
 
-	int start = 0;
-	int size = 0;
-	if (fromStart) { 
-	    size = Math.min(getGeometry().getPoints().size(), index);
-	    start = 0;
-	} else {
-	    start = index;
-	    size = getGeometry().getPoints().size() - index;
+		final List<mxPoint> points = getGeometry().getPoints();
+		if (fromStart) {
+			return new ArrayList<mxPoint>(points.subList(0,
+					Math.min(points.size(), index)));
+		} else {
+			if (index < points.size()) {
+				return new ArrayList<mxPoint>(points.subList(index,
+						points.size()));
+			} else {
+				return new ArrayList<mxPoint>();
+			}
+		}
 	}
-
-	if (size > 0) {
-	    mxPoint[] points = new mxPoint[size];
-	    for (int i = 0; i < size; i++) { 
-		points[i] = getGeometry().getPoints().get(start + i);
-	    }
-	    return points;
-	}
-	return null;
-    }
 
     /** @return the number of points in this link */
     public int getPointCount() { 

scilab/modules/xcos/tests/nonreg_tests/bug_8569.tst
@@ -1,0 +1,22 @@
+// =============================================================================
+// Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+// Copyright (C) 2010 - DIGITEO - Clément DAVID
+//
+//  This file is distributed under the same license as the Scilab package.
+// =============================================================================
+
+// <-- INTERACTIVE TEST -->
+// <-- TEST WITH XCOS -->
+//
+// <-- Non-regression test for bug 8569 -->
+//
+// <-- Bugzilla URL -->
+// http://bugzilla.scilab.org/show_bug.cgi?id=8569
+//
+// <-- Short Description -->
+// Link destruction algorithm is not the right one
+
+xcos(SCI + "/modules/xcos/tests/nonreg_tests/bug_8569.xcos");
+// Check that all the actions performs fine (comments in the diagram)
+
+

commit afc8ac1d3be2fe2f95bfd76c1cfa09bb8fb57e0e

Correct regression introduced by f9924b536faa82fbb7fdcbc5dcbd821c37826906
Was no more able to split some events links.

Change-Id: I21f2a43444d78552530c6debda195525e27557be

scilab/modules/xcos/src/java/org/scilab/modules/xcos/block/SplitBlock.java
@@ -49,7 +49,7 @@ public final class SplitBlock extends BasicBlock {
 	public SplitBlock() {
 		super();
 	}
-	
+
 	/**
 	 * Add connection port depending on the type of the source.
 	 * @param source the type of the split
@@ -81,7 +81,7 @@ public final class SplitBlock extends BasicBlock {
 	protected void setDefaultValues() {
 		super.setDefaultValues();
 		setInterfaceFunctionName("SPLIT_f");
-		setStyle(getInterfaceFunctionName()); 
+		setStyle(getInterfaceFunctionName());
 		setSimulationFunctionName("lsplit");
 		setRealParameters(new ScilabDouble());
 		setIntegerParameters(new ScilabDouble());
@@ -91,7 +91,7 @@ public final class SplitBlock extends BasicBlock {
 
 	/**
 	 * Add a port on the block.
-	 * 
+	 *
 	 * @param port
 	 *            The port to be added to the block
 	 * @see org.scilab.modules.xcos.block.BasicBlock#addPort(org.scilab.modules.xcos.port.BasicPort)
@@ -106,39 +106,39 @@ public final class SplitBlock extends BasicBlock {
 	 * @return input port
 	 */
 	public BasicPort getIn() {
-		return getChild(0, InputPort.class, 1);
+		return getChild(0, BasicPort.class, 1);
 	}
 
 	/**
 	 * @return first output port
 	 */
 	public BasicPort getOut1() {
-		return getChild(1, OutputPort.class, 1);
+		return getChild(1, BasicPort.class, 1);
 	}
 
 	/**
 	 * @return second output port
 	 */
 	public BasicPort getOut2() {
-		return getChild(2, OutputPort.class, 2);
+		return getChild(2, BasicPort.class, 2);
 	}
 
 	/**
 	 * Get the child of the kind class from the start to a count.
 	 * @param startIndex the start index (default position)
 	 * @param kind the kind of the port
-	 * @param ordering the ordering of the port 
+	 * @param ordering the ordering of the port
 	 * @return the found port or null.
 	 */
 	private BasicPort getChild(int startIndex, Class<? extends BasicPort> kind, int ordering) {
 		final int size = children.size();
-		
+
 		int loopCount = size;
 		for (int i = startIndex; loopCount > 0; i = (i + 1) % size, loopCount--) {
 			Object child = children.get(i);
 			if (kind.isInstance(child)) {
 				BasicPort port = kind.cast(child);
-				
+
 				if (port.getOrdering() == ordering) {
 					// end of the loop
 					return kind.cast(child);
@@ -148,7 +148,7 @@ public final class SplitBlock extends BasicBlock {
 		LogFactory.getLog(SplitBlock.class).error("Unable to find a child.");
 		return null;
 	}
-	
+
 	/**
 	 * delete split block child before delete
 	 */
@@ -168,7 +168,7 @@ public final class SplitBlock extends BasicBlock {
 
 	/**
 	 * Set the geometry of the block
-	 * 
+	 *
 	 * @param geometry
 	 *            change split block geometry
 	 */
@@ -177,7 +177,7 @@ public final class SplitBlock extends BasicBlock {
 		if (geometry != null) {
 			geometry.setWidth(DEFAULT_SIZE);
 			geometry.setHeight(DEFAULT_SIZE);
-			
+
 			/*
 			 * Align the geometry on the grid
 			 */
@@ -190,10 +190,10 @@ public final class SplitBlock extends BasicBlock {
 			BlockPositioning.alignPoint(geometry, gridSize,
 					(geometry.getWidth() / 2.0));
 		}
-		
+
 		super.setGeometry(geometry);
 	}
-	
+
 	/**
 	 * @return true if the split is connectable, false otherwise
 	 * @see org.scilab.modules.xcos.block.BasicBlock#isConnectable()
@@ -206,16 +206,16 @@ public final class SplitBlock extends BasicBlock {
 				final mxICell cell = getChildAt(i);
 				hasNoEdges = cell.getEdgeCount() == 0;
 			}
-			
+
 			return hasNoEdges;
 		} else {
 			return false;
 		}
 	}
-	
+
 	/**
 	 * Insert edges as children port edges
-	 * 
+	 *
 	 * @param edge the current edge
 	 * @param isOutgoing if it is an input port or output one
 	 * @return the inserted edges