b0ead285e0ecf67e19a5d4db537b4490fb8cf41e
Bruno JOFRET <bruno.jofret@scilab.org>
2010-12-21 09:24:19 +0100

    Remove reference to 6.0 in scilab banner
    Change-Id: I67267b10a4c514d0d5c1d0b5dc09d6eab12ccf56

--- scilab/modules/core/src/cpp/banner.cpp
+++ scilab/modules/core/src/cpp/banner.cpp
@@ -1,19 +1,19 @@
 /*
  *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  *  Copyright (C) 2010-2010 - DIGITEO - Bruno JOFRET
- * 
+ *
  *  This file must be used under the terms of the CeCILL.
  *  This source file is licensed as described in the file COPYING, which
  *  you should have received as part of this distribution.  The terms
  *  are also available at
  *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
- * 
+ *
  */
 
 #include "banner.hxx"
 #include "yaspio.hxx"
 
-extern "C" 
+extern "C"
 {
 #include <string.h>
 #include <math.h>
@@ -47,7 +47,7 @@ void banner(void)
     YaspWriteW(line);
     YaspWriteW(L"\n");
     YaspWriteW(L"\n");
-    YaspWriteW(L"           -*- THIS IS SCILAB 6.0 aka YaSp -*-\n");
+    YaspWriteW(L"                     -*- THIS IS YaSp -*-\n");
     YaspWriteW(L"\n");
     YaspWriteW(line);
     YaspWriteW(L"\n");
