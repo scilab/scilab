27769f6029faa451150157f02b30cebe6d16d6b2
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2011-01-02 10:41:34 +0100

    2010 => 2011
    Change-Id: I0351f0a4d195413a2323cdd75395e0f06b81412b

--- scilab/modules/scinotes/src/java/org/scilab/modules/scinotes/utils/SciNotesMessages.java
+++ scilab/modules/scinotes/src/java/org/scilab/modules/scinotes/utils/SciNotesMessages.java
@@ -200,7 +200,7 @@ public class SciNotesMessages {
     /* About box */
     public static final String VERSION = Messages.gettext("SciNotes") + " 1.1";
     public static final String DIGITEO = "The Scilab Consortium (DIGITEO)";
-    public static final String COPYRIGHT = Messages.gettext("Copyright (c)") + " 2009-2010";
+    public static final String COPYRIGHT = Messages.gettext("Copyright (c)") + " 2009-2011";
 
     public static final String SCINOTES_ERROR = Messages.gettext("SciNotes error");
     public static final String COULD_NOT_SAVE_FILE = Messages.gettext("The file could not be saved.");
