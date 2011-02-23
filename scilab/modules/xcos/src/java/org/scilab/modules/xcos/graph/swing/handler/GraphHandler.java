44e721e9cbbce7f316e010e8a0ff9a5e9ffe6ed8
Clément DAVID <clement.david@scilab.org>
2011-02-23 17:14:41 +0100

    Xcos: handle loop-link (regression since 5.2.2)
    Change-Id: Ieff59cfc3eb64eeb7ec04b62269c4bdf51dfa8b1

--- scilab/modules/xcos/src/java/org/scilab/modules/xcos/graph/swing/handler/GraphHandler.java
+++ scilab/modules/xcos/src/java/org/scilab/modules/xcos/graph/swing/handler/GraphHandler.java
@@ -158,6 +158,15 @@ public class GraphHandler extends mxGraphHandler {
 		// get the point
 		final mxPoint pt = graphComponent.getPointForEvent(e);
 		
+		// translate the point if it is a loop link
+		if (cell.getSource() != null && cell.getTarget() != null
+				&& cell.getSource().getParent() == cell.getTarget().getParent()
+				&& cell.getSource().getParent() != null) {
+			final mxGeometry parent = cell.getSource().getParent().getGeometry();
+			pt.setX(pt.getX() - parent.getX());
+			pt.setY(pt.getY() - parent.getY());
+		}
+		
 		// add or remove the point to the list and fire event
 		final mxIGraphModel model = graphComponent.getGraph().getModel();
 		model.beginUpdate();
