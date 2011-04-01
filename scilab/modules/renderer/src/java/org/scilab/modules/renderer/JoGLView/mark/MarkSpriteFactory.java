56a3e4783bfa81765fdd270945f031d3dba6582b
Manuel Juliachs <manuel.juliachs@scilab.org>
2011-04-01 16:38:26 +0200

    MarkSpriteFactory Java class sprite drawers implemented for the diamond, crossed diamond, triangles (up, down, right, left), circle and five-pointed star mark styles.
    Change-Id: I62aac27520ea1a50b563574e7eaa420ac97430d7

--- scilab/modules/renderer/src/java/org/scilab/modules/renderer/JoGLView/mark/MarkSpriteFactory.java
+++ scilab/modules/renderer/src/java/org/scilab/modules/renderer/JoGLView/mark/MarkSpriteFactory.java
@@ -85,9 +85,16 @@ public class MarkSpriteFactory {
             case  2: return new CrossSpriteDrawer(appearance, finalSize);
             case  3: return new StarSpriteDrawer(appearance, finalSize);
             case  4: return new FilledDiamondSpriteDrawer(foregroundColor, finalSize);
-
+            case  5: return new DiamondSpriteDrawer(appearance, finalSize);
+            case  6: return new TriangleUpSpriteDrawer(appearance, finalSize);
+            case  7: return new TriangleDownSpriteDrawer(appearance, finalSize);
+            case  8: return new DiamondPlusSpriteDrawer(appearance, finalSize);
+            case  9: return new CircleSpriteDrawer(appearance, finalSize);
             case 10: return new AsteriskSpriteDrawer(appearance, finalSize);
             case 11: return new SquareSpriteDrawer(appearance, finalSize);
+            case 12: return new TriangleRightSpriteDrawer(appearance, finalSize);
+            case 13: return new TriangleLeftSpriteDrawer(appearance, finalSize);
+            case 14: return new PentagramSpriteDrawer(appearance, finalSize);
 
             default:
                 return new PlusSpriteDrawer(appearance, finalSize);
@@ -237,6 +244,119 @@ public class MarkSpriteFactory {
     }
 
     /**
+     * Abstract class for all the triangle mark sprites.
+     */
+    private static abstract class TriangleSpriteDrawer extends ScilabSpriteDrawer {
+        protected final int r;
+        protected final int basex;
+        protected final int basey;
+        protected int[] coordinates;
+
+        public TriangleSpriteDrawer(Appearance appearance, int size) {
+            super(appearance, size);
+            double tmpr = (double)(size/2);
+
+            r = (int) (tmpr);
+
+            basex = (int) (tmpr * 0.5 * Math.sqrt(3));
+            basey = (int) (tmpr * 0.5);
+        }
+
+        @Override
+        public void draw(SpriteDrawingTools spriteDrawingTools) {
+            spriteDrawingTools.fillPolygon(coordinates, appearance);
+        }
+    }
+
+    /**
+     * Diamond sprite
+     * Scilab ID = 5
+     */
+    private static class DiamondSpriteDrawer extends ScilabSpriteDrawer {
+        protected final int[] coordinates;
+
+        public DiamondSpriteDrawer(Appearance appearance, int size) {
+            super(appearance, size);
+            int r = size / 2;
+
+            coordinates = new int[] {-r,0, 0,-r, r,0, 0,r};
+        }
+
+        @Override
+        public void draw(SpriteDrawingTools spriteDrawingTools) {
+            spriteDrawingTools.fillPolygon(coordinates, appearance);
+        }
+    }
+
+    /**
+     * Triangle up sprite
+     * Scilab ID = 6
+     */
+    private static class TriangleUpSpriteDrawer extends TriangleSpriteDrawer {
+        public TriangleUpSpriteDrawer(Appearance appearance, int size) {
+            super(appearance, size);
+
+            coordinates = new int[] {-basex,basey, 0,-r, basex,basey};
+        }
+    }
+
+    /**
+     * Triangle down sprite
+     * Scilab ID = 7
+     */
+    private static class TriangleDownSpriteDrawer extends TriangleSpriteDrawer {
+        public TriangleDownSpriteDrawer(Appearance appearance, int size) {
+            super(appearance, size);
+
+            coordinates = new int[] {-basex,-basey, basex,-basey, 0,r};
+        }
+    }
+
+    /**
+     * Diamond plus sprite
+     * Scilab ID = 8
+     */
+    private static class DiamondPlusSpriteDrawer extends DiamondSpriteDrawer {
+        private final int[] coordinate1;
+        private final int[] coordinate2;
+
+        public DiamondPlusSpriteDrawer(Appearance appearance, int size) {
+            super(appearance, size);
+            coordinate1 = new int[] {coordinates[0], coordinates[1], coordinates[4], coordinates[5]};
+            coordinate2 = new int[] {coordinates[2], coordinates[3], coordinates[6], coordinates[7]};
+        }
+
+        @Override
+        public void draw(SpriteDrawingTools spriteDrawingTools) {
+            super.draw(spriteDrawingTools);
+            spriteDrawingTools.drawPolyline(coordinate1, appearance);
+            spriteDrawingTools.drawPolyline(coordinate2, appearance);
+        }
+    }
+
+    /**
+     * Circle sprite
+     * Scilab ID = 9
+     */
+    private static class CircleSpriteDrawer extends ScilabSpriteDrawer {
+        private final int[] coordinate1;
+        private final int[] coordinate2;
+
+        public CircleSpriteDrawer(Appearance appearance, int size) {
+            super(appearance, size);
+            int r = size / 2;
+            coordinate1 = new int[] {-r, 0, r, 0};
+            coordinate2 = new int[] { 0,-r, 0, r};
+        }
+
+        @Override
+        public void draw(SpriteDrawingTools spriteDrawingTools) {
+            spriteDrawingTools.fillDisc(0, 0, size, appearance.getFillColor());
+            spriteDrawingTools.drawCircle(0, 0, size, appearance);
+        }
+    }
+
+    /**
      * Asterisk sprite
      * Scilab ID = 10
      */
@@ -281,6 +401,75 @@ public class MarkSpriteFactory {
         @Override
         public void draw(SpriteDrawingTools spriteDrawingTools) {
             spriteDrawingTools.fillPolygon(coordinate, appearance);
+        }
+    }
+
+    /**
+     * Triangle right sprite
+     * Scilab ID = 12
+     */
+    private static class TriangleRightSpriteDrawer extends TriangleSpriteDrawer {
+        public TriangleRightSpriteDrawer(Appearance appearance, int size) {
+            super(appearance, size);
+
+            coordinates = new int[] {-basey,basex, -basey,-basex, r,0};
+        }
+    }
+
+    /**
+     * Triangle left sprite
+     * Scilab ID = 13
+     */
+    private static class TriangleLeftSpriteDrawer extends TriangleSpriteDrawer {
+        public TriangleLeftSpriteDrawer(Appearance appearance, int size) {
+            super(appearance, size);
+
+            coordinates = new int[] {basey,basex, -r,0, basey,-basex};
+        }
+    }
+
+    /**
+     * Pentagram sprite (five-pointed star)
+     * Scilab ID = 14
+     */
+    private static class PentagramSpriteDrawer extends ScilabSpriteDrawer {
+        private final int[] coordinates;
+
+        private static final double COS_PI_OVER_10 = Math.cos(Math.PI/10.0);
+        private static final double SIN_PI_OVER_10 = Math.sin(Math.PI/10.0);
+        private static final double COS_3_PI_OVER_10 = Math.cos(3.0*Math.PI/10.0);
+        private static final double SIN_3_PI_OVER_10 = Math.sin(3.0*Math.PI/10.0);
+
+        public PentagramSpriteDrawer(Appearance appearance, int size) {
+            super(appearance, size);
+
+            int r = size / 2;
+
+            double tmpy = SIN_PI_OVER_10*(double)r;
+
+            int x = (int)(COS_PI_OVER_10*(double)r);
+            int y = (int)(SIN_PI_OVER_10*(double)r);
+
+            int x2 = (int)(COS_3_PI_OVER_10*(double)r);
+            int y2 = (int)(SIN_3_PI_OVER_10*(double)r);
+
+            double tmpinnr = tmpy / SIN_3_PI_OVER_10;
+
+
+            int xinn = (int)(tmpinnr * COS_3_PI_OVER_10);
+            int yinn = y;
+
+            int xinn2 = (int)(tmpinnr * COS_PI_OVER_10);
+            int yinn2 = (int)(tmpinnr * SIN_PI_OVER_10);
+
+            int innr = (int)tmpinnr;
+
+            coordinates = new int[] {-x2,y2, -xinn2,yinn2, -x,-y, -xinn,-yinn, 0,-r, xinn,-yinn, x,-y, xinn2,yinn2, x2,y2, 0,innr};
+        }
+
+        @Override
+        public void draw(SpriteDrawingTools spriteDrawingTools) {
+            spriteDrawingTools.fillPolygon(coordinates, appearance);
         }
     }
 }
