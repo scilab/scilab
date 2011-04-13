4528ba4e031d86fb6b1da3c01eee94b8f2c2293a
Bruno JOFRET <bruno.jofret@scilab.org>
2011-04-13 11:43:15 +0200

    Check if file is known by Scilab before trying to seek.
    Change-Id: I8dca82e3085ba7499afffb6faaaf3371a7d3aab8

--- scilab/modules/fileio/src/cpp/mseek.cpp
+++ scilab/modules/fileio/src/cpp/mseek.cpp
@@ -1,11 +1,11 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2011 - Digiteo - Cedric DELAMARRE
- * 
+ *
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
  * you should have received as part of this distribution.  The terms
- * are also available at    
+ * are also available at
  * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
  *
  */
@@ -18,34 +18,41 @@ extern "C"
 #include <stdio.h>
 #include <errno.h>
 #include "mseek.h"
+#include "sciprint.h"
 #include "filesmanagement.h"
 #include "localization.h"
+#include "charEncoding.h"
 #include "MALLOC.h"
 }
 /*--------------------------------------------------------------------------*/
 
 int mseek(int fd, int offset, int iflag)
-{     
+{
 #if (defined(sun) && !defined(SYSV)) || defined(sgi)
 	int irep;
 #endif
 
     File* pF = FileManager::getFile(fd);
+    if (pF == NULL)
+    {
+        sciprintW(_W("%ls: No input file associated to logical unit %d.\n"), L"mseek", fd);
+        return 1;
+    }
 	FILE* fa = pF->getFiledesc();
 
-	if(fa == NULL) 
+	if(fa == NULL)
 	{
 		return 1;
 	}
 
-	if(iflag != SEEK_SET && iflag != SEEK_CUR && iflag != SEEK_END) 
+	if(iflag != SEEK_SET && iflag != SEEK_CUR && iflag != SEEK_END)
 	{
 		return 1;
 	}
 
 #if (defined(sun) && !defined(SYSV)) || defined(sgi)
 	irep = fseek(fa,(long) offset,iflag) ;
-	if(irep != 0 ) 
+	if(irep != 0 )
 	{
 		return 1;
 	}
@@ -55,18 +62,18 @@ int mseek(int fd, int offset, int iflag)
 	}
 #else
 	#ifdef _MSC_VER
-		#if _WIN64 
-			if(_fseeki64(fa,(long) offset,iflag) == -1 ) 
+		#if _WIN64
+			if(_fseeki64(fa,(long) offset,iflag) == -1 )
 		#else
-			if(fseek(fa,(long) offset,iflag) == -1 ) 
+			if(fseek(fa,(long) offset,iflag) == -1 )
 		#endif
 	#else
-	if(fseek(fa,(long) offset,iflag) == -1) 
+	if(fseek(fa,(long) offset,iflag) == -1)
 	#endif
 	{
 		return errno; // global variable produced by fseek
 	}
-	else 
+	else
 	{
 		return 0;
 	}
