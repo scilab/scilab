d8a4bdfe6b59127b18ae85b049d1b0a11638335c
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2011-04-19 17:15:39 +0200

    Increase the size of the backtrace buffer
    Change-Id: Ib7268c309f39ec428ccbcf09749386664885ee74

--- scilab/modules/core/src/c/signal_mgmt.c
+++ scilab/modules/core/src/c/signal_mgmt.c
@@ -47,7 +47,7 @@ static char *backtrace_print(int niv_debut)
 {
     size_t ind;
     sci_backtrace_t *tr = NULL;
-    char print_buffer[1024];
+    char print_buffer[4096]; /* TODO: make it dynamic */
 
     int size = sizeof(print_buffer);
     char *tmp = print_buffer;
