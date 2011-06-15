44b30a827f97accac0f4a140cb2f059bf52304b6
Calixte DENIZET <calixte@contrib.scilab.org>
2011-06-15 21:45:21 +0200

    Bug 9542 fixed: during export operation, the file could be locked by an other process (Windows bug)
    Change-Id: If2c336a062b32346e6aef29aa0de60bfcffb7cf4

--- scilab/modules/graphic_export/src/java/org/scilab/modules/graphic_export/ExportBitmap.java
+++ scilab/modules/graphic_export/src/java/org/scilab/modules/graphic_export/ExportBitmap.java
@@ -1,6 +1,7 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2008 - INRIA - Sylvestre Koumar
+ * Copyright (C) 2011 - Calixte DENIZET
  *
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
@@ -14,6 +15,8 @@ package org.scilab.modules.graphic_export;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Iterator;
 
@@ -32,9 +35,13 @@ import com.sun.opengl.util.Screenshot;
 /**
  * Class which allows to export screen-shots in format Bitmap (BMP, GIF, JPG, PNG and PPM)
  * @author Sylvestre Koumar
+ * @author Calixte DENIZET
  *
  */
 public class ExportBitmap extends ExportToFile {
+
+    private static final int MAX_ATTEMPT = 100;
+    private static final int SLEEP_TIME = 10;
 
     /** File which contains the screen-shot */
     private File file;
@@ -141,12 +148,41 @@ public class ExportBitmap extends ExportToFile {
                     return ExportRenderer.IOEXCEPTION_ERROR;
                 }
             } else {
-                ImageIO.write(dump, getFileExtension(), file);
+                return writeFile(dump, getFileExtension(), file);
             }
         } catch (IOException e) {
             return ExportRenderer.IOEXCEPTION_ERROR;
         }
         return ExportRenderer.SUCCESS;
+    }
+
+    /**
+     * Write the image in the file. If the file is locked (cf bug 9542), then retry.
+     * @param image the image
+     * @param ext the extension
+     * @param file the file
+     */
+    private static final int writeFile(BufferedImage image, String ext, File file) {
+        int ret = ExportRenderer.IOEXCEPTION_ERROR;
+        if (file.isFile() && file.canWrite()) {
+            FileOutputStream fos;
+            for (int i = 0; i < MAX_ATTEMPT && ret != ExportRenderer.SUCCESS; i++) {
+                try {
+                    fos = new FileOutputStream(file);
+                    ImageIO.write(image, ext, fos);
+                    fos.close();
+                    ret = ExportRenderer.SUCCESS;
+                } catch (FileNotFoundException e) {
+                    try {
+                        Thread.sleep(SLEEP_TIME);
+                    } catch (InterruptedException ex) { }
+                } catch (IOException e) {
+                    return ret;
+                }
+            }
+        }
+
+        return ret;
     }
 
     /**
