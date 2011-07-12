a74a2003e863716f8eed35087f6e4a3d3f5d4cf1
Manuel Juliachs <manuel.juliachs@scilab.org>
2011-07-12 16:50:25 +0200

    Current Axes property set C function adapted to the MVC.
    Change-Id: Ic1f48a031170760b5c5abf858f959c3029dad188

--- scilab/modules/graphics/src/c/getHandleProperty/set_current_axes_property.c
+++ scilab/modules/graphics/src/c/getHandleProperty/set_current_axes_property.c
@@ -4,6 +4,7 @@
  * Copyright (C) 2006 - INRIA - Allan Cornet
  * Copyright (C) 2006 - INRIA - Jean-Baptiste Silvy
  * Copyright (C) 2009 - DIGITEO - Pierre Lando
+ * Copyright (C) 2011 - DIGITEO - Manuel Juliachs
  *
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
@@ -19,65 +20,70 @@
 /*        a handle                                                        */
 /*------------------------------------------------------------------------*/
 
+#include <string.h>
+
 #include "setHandleProperty.h"
-#include "SetProperty.h"
-#include "GetProperty.h"
 #include "getPropertyAssignedValue.h"
 #include "Scierror.h"
 #include "localization.h"
 #include "SetPropertyStatus.h"
 #include "HandleManagement.h"
 
+#include "getGraphicObjectProperty.h"
+#include "setGraphicObjectProperty.h"
+#include "graphicObjectProperties.h"
+#include "CurrentFigure.h"
+#include "CurrentSubwin.h"
+
 /*------------------------------------------------------------------------*/
 int set_current_axes_property(char* pobjUID, size_t stackPointer, int valueType, int nbRow, int nbCol )
 {
-//???
-#if 0
-    sciPointObj * curAxes   = NULL ;
-    sciPointObj * parentFig = NULL ;
-    int num = -1 ;
-    int status1 = 1 ;
-    int status2 = 1 ;
+    char * curAxesUID   = NULL;
+    char * parentFigureUID = NULL;
+    char * type;
 
-
-	if (pobj != NULL)
-	{
-		/* This property should not be called on an handle */
-		Scierror(999, _("'%s' property does not exist for this handle.\n"), "current_axes");
-		return SET_PROPERTY_ERROR;
-	}
+    if (pobjUID != NULL)
+    {
+        /* This property should not be called on a handle */
+        Scierror(999, _("'%s' property does not exist for this handle.\n"), "current_axes");
+        return SET_PROPERTY_ERROR;
+    }
 
     if ( !isParameterHandle( valueType ) )
     {
         Scierror(999, _("Wrong type for '%s' property: Handle expected.\n"), "current_axes");
-        return SET_PROPERTY_ERROR ;
+        return SET_PROPERTY_ERROR;
     }
 
-    curAxes = sciGetPointerFromHandle( getHandleFromStack( stackPointer ) ) ;
+    curAxesUID = getObjectFromHandle( getHandleFromStack( stackPointer ) );
 
-    if ( curAxes == NULL)
+    if ( curAxesUID == NULL)
     {
         Scierror(999, _("Wrong value for '%s' property: Must be a valid handle.\n"), "current_entity");
-        return SET_PROPERTY_ERROR ;
+        return SET_PROPERTY_ERROR;
     }
-    if ( sciGetEntityType( curAxes ) != SCI_SUBWIN )
+
+    getGraphicObjectProperty(curAxesUID, __GO_TYPE__, jni_string, &type);
+
+    if (strcmp(type, __GO_AXES__) != 0)
     {
         Scierror(999, _("Wrong value for '%s' property: Must be a handle on a axes.\n"), "current_axes");
-        return SET_PROPERTY_ERROR ;
+        return SET_PROPERTY_ERROR;
     }
 
-    status1 = sciSetSelectedSubWin( curAxes ) ;
+    /* The current Axes' parent Figure's selected child property must be updated */
+    getGraphicObjectProperty(curAxesUID, __GO_PARENT__, jni_string, &parentFigureUID);
+    setGraphicObjectProperty(parentFigureUID, __GO_SELECTED_CHILD__, curAxesUID, jni_string, 1);
+
+    setCurrentSubWin(curAxesUID);
+
     /* F.Leray 11.02.05 : if the new selected subwin is not inside the current figure, */
-    /* we must also set the current figure to subwin->parent */
-    parentFig = sciGetParentFigure( curAxes );
+    /* we must also set the current figure to the subwin's parent */
+    if (!isCurrentFigure(parentFigureUID))
+    {
+        setCurrentFigure(parentFigureUID);
+    }
 
-    num  = sciGetNum( parentFig ) ;
-
-    status2 = sciSetUsedWindow( num ) ;
-
-    /* End modif. on the 11.02.05 */
-    return sciSetFinalStatus( (SetPropertyStatus)status1, (SetPropertyStatus)status2 ) ;
-#endif
-    return 0;
+    return SET_PROPERTY_SUCCEED;
 }
 /*------------------------------------------------------------------------*/
