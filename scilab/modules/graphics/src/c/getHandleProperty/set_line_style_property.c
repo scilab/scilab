c12c8b888eb435fc23b8772a79a3591696f4f7e6
Bruno JOFRET <bruno.jofret@scilab.org>
2011-07-21 14:25:01 +0200

    Change default line_style value:
    - {0,1} are equivalent values for SOLID rendering.
    - Set 1 as the only representative value.
    - Add warning message if user tries to force it to 0.
    - Update documentation.
    
    Change-Id: I4244dbf7136c6d5fe4354047e3fd4f4d828e563f

--- scilab/modules/graphics/src/c/getHandleProperty/set_line_style_property.c
+++ scilab/modules/graphics/src/c/getHandleProperty/set_line_style_property.c
@@ -25,6 +25,8 @@
 #include "Scierror.h"
 #include "localization.h"
 #include "SetPropertyStatus.h"
+#include "sciprint.h"
+#include "warningmode.h"
 
 /*------------------------------------------------------------------------*/
 int set_line_style_property(char* pobjUID, size_t stackPointer, int valueType, int nbRow, int nbCol )
@@ -35,6 +37,15 @@ int set_line_style_property(char* pobjUID, size_t stackPointer, int valueType, i
         return SET_PROPERTY_ERROR;
     }
 
-    return sciSetLineStyle(pobjUID, (int) getDoubleFromStack(stackPointer));
+    int iLineStyle = (int) getDoubleFromStack(stackPointer);
+
+    if (iLineStyle == 0 && getWarningMode())
+    {
+        sciprint(_("WARNING: %s\n"), _("{0,1} values are equivalent for line_style property."));
+        sciprint(_("WARNING: %s\n"), _("0 will be removed after Scilab 5.4.0."));
+        sciprint(_("WARNING: %s\n"), _("Please use 1 instead."));
+    }
+
+    return sciSetLineStyle(pobjUID, iLineStyle);
 }
 /*------------------------------------------------------------------------*/
