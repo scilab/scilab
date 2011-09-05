b04d685dc79464e761c3ba9d253976887ed1ca28
Sylvestre Ledru <sylvestre.ledru@scilab.org>
2011-09-05 10:56:10 +0200

    Support of the czech version
    Change-Id: Iac6a16f1f59800685b0e0b2aceb74e257ee5256b

--- scilab/tools/localization/dispatchLocalizationFile.sh
+++ scilab/tools/localization/dispatchLocalizationFile.sh
@@ -51,6 +51,7 @@ for file in $LAUNCHPAD_DIRECTORY/*.po; do
     if test "$LOC" = "it"; then LOC="it_IT"; fi
     if test "$LOC" = "uk"; then LOC="uk_UA"; fi
     if test "$LOC" = "pl"; then LOC="pl_PL"; fi
+    if test "$LOC" = "cs"; then LOC="cs_CZ"; fi
 
     # check that it is the right format
     echo "$LOC"|grep -E "(.*_.*)" > /dev/null # it is a real localization name xx_YY
