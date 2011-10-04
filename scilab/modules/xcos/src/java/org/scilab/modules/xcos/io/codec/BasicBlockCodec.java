scilab/scilab

commit 65cb120eace8ce11aa5c4fd6af5fcaafb5a3d139

Xcos: remove deprecated blocks implementation
Deprecated blocks since 5.3.1:
	- PrintBlock
	- ConstBlock
	- GainBlock

Style (value displaying) is kept by using displayedLabel
Default values are now set on afterDecode() if applicable

Update: http://codereview.scilab.org/#change,4421
Change-Id: I57a9821fed10f189c884faccff476b028270aa3b

scilab/modules/xcos/src/java/org/scilab/modules/xcos/io/codec/BasicBlockCodec.java
@@ -36,6 +36,7 @@ import com.mxgraph.model.mxCell;
  */
 public class BasicBlockCodec extends XcosObjectCodec {
 
+	private static final String BASIC_BLOCK = BasicBlock.class.getSimpleName();
 	private static final String SIMULATION_FUNCTION_TYPE = "simulationFunctionType";
 	private static final String[] IGNORED_FIELDS = new String[] {SIMULATION_FUNCTION_TYPE, "locked", "parametersPCS"};
 	private static final Log LOG = LogFactory.getLog(BasicBlockCodec.class);
@@ -80,6 +81,15 @@ public class BasicBlockCodec extends XcosObjectCodec {
 		BasicBlockCodec codec = (BasicBlockCodec) mxCodecRegistry.getCodec("AfficheBlock");
 		codec.exclude.add("printTimer");
 		codec.exclude.add("updateAction");
+		
+		/*
+		 * Compat. to remove old specific implementations
+		 * 
+		 * These implementation was available from Scilab-5.2.0 to Scilab-5.3.3.
+		 */
+		mxCodecRegistry.addAlias("ConstBlock", BASIC_BLOCK);
+		mxCodecRegistry.addAlias("GainBlock", BASIC_BLOCK);
+		mxCodecRegistry.addAlias("PrintBlock", BASIC_BLOCK);
 	}
 	
 	/**
@@ -98,6 +108,19 @@ public class BasicBlockCodec extends XcosObjectCodec {
 	}
 
 	/**
+	 * {@inheritDoc}
+	 * 
+	 * Shortcut method to avoid old-XML-compat (before jgraphx-1.4) from {@link mxCellCodec}.
+	 * 
+	 * Without this shortcut, alias does not works (Class name String comparaison).
+	 * @see http://forum.jgraph.com/questions/1467/aliases-are-not-handled-on-mxcellcodecbeforedecode
+	 */
+	@Override
+	public Node beforeDecode(mxCodec dec, Node node, Object obj) {
+		return node;
+	}
+	
+	/**
 	 * Apply compatibility pattern to the decoded object
 	 * @param dec Codec that controls the decoding process.
 	 * @param node XML node to decode the object from.
@@ -111,22 +134,23 @@ public class BasicBlockCodec extends XcosObjectCodec {
 			LOG.error("Unable to decode " + obj);
 			return obj;
 		}
-		
-	    ((BasicBlock) obj).setSimulationFunctionType(SimulationFunctionType.DEFAULT);
+	    final BasicBlock block = (BasicBlock) obj;
+	    
+		block.setSimulationFunctionType(SimulationFunctionType.DEFAULT);
 
 	    String functionType = (((Element) node).getAttribute(SIMULATION_FUNCTION_TYPE));
 	    if (functionType != null && functionType.compareTo("") != 0) {
 		SimulationFunctionType type = BasicBlock.SimulationFunctionType.valueOf(functionType);
 		if (type != null) {
-		    ((BasicBlock) obj).setSimulationFunctionType(type);
+		    block.setSimulationFunctionType(type);
 		}
 	    }
 
 	    // Re associate the diagram container
-	    if (obj instanceof SuperBlock) {
-	    	final SuperBlock block = (SuperBlock) obj;
-	    	if (block.getChild() != null) {
-	    		block.getChild().setContainer(block);
+	    if (block instanceof SuperBlock) {
+	    	final SuperBlock superBlock = (SuperBlock) block;
+	    	if (superBlock.getChild() != null) {
+	    		superBlock.getChild().setContainer(superBlock);
 	    	}
 	    }
 	    
@@ -134,8 +158,35 @@ public class BasicBlockCodec extends XcosObjectCodec {
 	    // default style if absent
 	    StyleMap map = new StyleMap(((Element) node).getAttribute(STYLE));
 	    formatStyle(map, (BasicBlock) obj);
-		((BasicBlock) obj).setStyle(map.toString());
-	    ((BasicBlock) obj).updateFieldsFromStyle();
+		block.setStyle(map.toString());
+	    block.updateFieldsFromStyle();
+	    
+	    
+		/*
+		 * Compat. to remove old specific implementations
+		 * 
+		 * These implementation was available from Scilab-5.2.0 to Scilab-5.3.3.
+		 * 
+		 * Set default values stolen from the old implementation in case of default value.
+		 */
+	    if (map.containsKey("CONST_m")) {
+	    	if (block.getInterfaceFunctionName().equals(BasicBlock.DEFAULT_INTERFACE_FUNCTION)) {
+	    		block.setInterfaceFunctionName("CONST_m");
+	    	}
+	    	if (block.getSimulationFunctionName().equals(BasicBlock.DEFAULT_SIMULATION_FUNCTION)) {
+	    		block.setSimulationFunctionName("cstblk4");
+	    	}
+	    	if (block.getValue() == null) {
+	    		block.setValue("1");
+	    	}
+	    }
+	    if (map.containsKey("GAINBLK_f")) {
+	    	if (block.getInterfaceFunctionName().equals(BasicBlock.DEFAULT_INTERFACE_FUNCTION)) {
+	    		block.setInterfaceFunctionName("GAINBLK_f");
+	    	}
+	    }
+	    // PrintBlock has not default values
+	    
 	    
 	    return super.afterDecode(dec, node, obj);
 	}

scilab/modules/xcos/etc/Xcos-style.xml
@@ -43,8 +43,10 @@
     <add as="noLabel" value="0"/>
     <add as="strokeColor" value="white"/>
   </add>
-  <!-- Const -->
-  <add as="Const" extend="defaultVertex"/>
+  <!-- Print (Label as block) -->
+  <add as="Print" extend="blockWithLabel">
+    <add as="displayedLabel" value="%s"/>
+  </add>
   <!-- Gain -->
   <add as="Gain" extend="blockWithLabel">
     <add as="shape" value="triangle"/>
@@ -128,9 +130,9 @@
     <add as="strokeColor" value="blue"/>
   </add>
   <!-- SOURCES -->
-  <add as="CONST_m" extend="blockWithLabel"/>
-  <add as="CONST_f" extend="blockWithLabel"/>
-  <add as="CONST" extend="blockWithLabel"/>
+  <add as="CONST_m" extend="Print"/>
+  <add as="CONST_f" extend="Print"/>
+  <add as="CONST" extend="Print"/>
   <add as="GENSQR_f" extend="blockWithLabel">
     <add as="displayedLabel" value="Square wave&lt;BR&gt;         generator"/>
   </add>

scilab/modules/xcos/src/java/org/scilab/modules/xcos/block/BlockFactory.java
@@ -24,7 +24,6 @@ import org.scilab.modules.xcos.block.positionning.Product;
 import org.scilab.modules.xcos.block.positionning.RoundBlock;
 import org.scilab.modules.xcos.block.positionning.Summation;
 import org.scilab.modules.xcos.block.positionning.VoltageSensorBlock;
-import org.scilab.modules.xcos.utils.XcosMessages;
 
 import com.mxgraph.model.mxICell;
 /**
@@ -48,34 +47,10 @@ public final class BlockFactory {
 		DSUPER(new SuperBlock(true)),
 		/** @see SuperBlock */
 		SUPER_f(new SuperBlock()),
-		/** @see ConstBlock */
-		CONST_m(new ConstBlock()),
-		/** @see ConstBlock */
-		CONST(CONST_m.getSharedInstance()),
-		/** @see ConstBlock */
-		CONST_f(CONST_m.getSharedInstance()),
-		/** @see PrintBlock */
-		FROM(new PrintBlock(XcosMessages.BLOCK_FROM)),
-		/** @see PrintBlock */
-		GOTO(new PrintBlock(XcosMessages.BLOCK_GOTO)),
-		/** @see PrintBlock */
-		CLKFROM(FROM.getSharedInstance()),
-		/** @see PrintBlock */
-		CLKGOTO(GOTO.getSharedInstance()),
-		/** @see PrintBlock */
-		FROMMO(FROM.getSharedInstance()),
-		/** @see PrintBlock */
-		GOTOMO(GOTO.getSharedInstance()),
 		/** @see AfficheBlock */
 		AFFICH_m(new AfficheBlock()),
 		/** @see AfficheBlock */
 		AFFICH_f(AFFICH_m.getSharedInstance()),
-		/** @see GainBlock */
-		GAINBLK_f(new GainBlock()),
-		/** @see GainBlock */
-		GAINBLK(GAINBLK_f.getSharedInstance()),
-		/** @see GainBlock */
-		GAIN_f(GAINBLK_f.getSharedInstance()),
 		/** @see ExplicitInBlock */
 		IN_f(new ExplicitInBlock()),
 		/** @see ExplicitOutBlock */

scilab/modules/xcos/src/java/org/scilab/modules/xcos/block/ConstBlock.java
@@ -1,38 +1,0 @@
-/*
- * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
- * Copyright (C) 2010 - DIGITEO - Clément DAVID
- *
- * This file must be used under the terms of the CeCILL.
- * This source file is licensed as described in the file COPYING, which
- * you should have received as part of this distribution.  The terms
- * are also available at
- * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
- *
- */
-
-package org.scilab.modules.xcos.block;
-
-/**
- * Class defined for compatibility issues.
- * 
- * All the logic is now on {@link PrintBlock}.
- */
-public final class ConstBlock extends PrintBlock {
-	/**
-	 * Default constructor
-	 */
-	public ConstBlock() {
-		super();
-	}
-	
-	/**
-	 * Set the default block name.
-	 */
-	@Override
-	protected void setDefaultValues() {
-		super.setDefaultValues();
-		setInterfaceFunctionName("CONST_m");
-		setSimulationFunctionName("cstblk4");
-		setValue("1");
-	}
-}

scilab/modules/xcos/src/java/org/scilab/modules/xcos/block/GainBlock.java
@@ -1,40 +1,0 @@
-/*
- * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
- * Copyright (C) 2009 - DIGITEO - Bruno JOFRET
- * Copyright (C) 2010-2011 - DIGITEO - Clément DAVID 
- *
- * This file must be used under the terms of the CeCILL.
- * This source file is licensed as described in the file COPYING, which
- * you should have received as part of this distribution.  The terms
- * are also available at
- * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
- *
- */
-
-package org.scilab.modules.xcos.block;
-
-
-/**
- * @deprecated use "displayedLabel" = "%s" style property. 
- */
-@Deprecated
-public final class GainBlock extends BasicBlock {
-
-     private static final long serialVersionUID = 7762548847345910801L;
-
-    /**
-     * Constructor
-     */
-    public GainBlock() {
-	super();
-    }
-    
-    /**
-     *  Initialize the block with the default values
-     */  
-    @Override
-    protected void setDefaultValues() {
-		super.setDefaultValues();
-    	setInterfaceFunctionName("GAINBLK_f");
-    }
-}

scilab/modules/xcos/src/java/org/scilab/modules/xcos/block/PrintBlock.java
@@ -1,65 +1,0 @@
-/*
- * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
- * Copyright (C) 2009 - DIGITEO - Bruno JOFRET
- * Copyright (C) 2010 - DIGITEO - Clément DAVID
- *
- * This file must be used under the terms of the CeCILL.
- * This source file is licensed as described in the file COPYING, which
- * you should have received as part of this distribution.  The terms
- * are also available at
- * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
- *
- */
-
-package org.scilab.modules.xcos.block;
-
-import org.scilab.modules.types.ScilabString;
-import org.scilab.modules.types.ScilabType;
-
-/**
- * A {@link BasicBlock} which print getExprs()[0][0] as bloc value.
- * 
- * @deprecated prefer using "displayedLabel" = "%s" style property. 
- */
-@Deprecated
-public class PrintBlock extends BasicBlock {
-	private final String format;
-	
-	/**
-     * Constructor
-     */
-    public PrintBlock() {
-	super();
-	format = "%s";
-    }
-    
-    /**
-     * @param format The string to format the value
-     */
-    public PrintBlock(String format) {
-	super();
-	this.format = format;
-    }
-    
-    /**
-     * Initialize the block with the default values
-     */
-    @Override
-    protected void setDefaultValues() {
-    	super.setDefaultValues();
-    }
-    
-    
-    /**
-     * @param exprs new exprs
-     */
-	@Override
-    public void setExprs(ScilabType exprs) {
-    	super.setExprs(exprs);
-    	
-    	final String data = ((ScilabString) getExprs()).getData()[0][0];
-    	final String str = String.format(format, data);
-    	
-		setValue(str);
-    }    
-}

scilab/modules/xcos/src/java/org/scilab/modules/xcos/utils/XcosMessages.java
@@ -270,8 +270,8 @@ public final class XcosMessages {
     public static final String INIT_MODELICA = Messages.gettext("Modelica initialize");
     
     /* Blocks values */
-    public static final String BLOCK_FROM = Messages.gettext("From %s");
-    public static final String BLOCK_GOTO = Messages.gettext("Goto %s");
+    public static final String BLOCK_FROM = "From %s";
+    public static final String BLOCK_GOTO = "Goto %s";
 
     /* Errors */
 	public static final String ERROR_UNABLE_TO_COMPILE_THIS_SUPER_BLOCK = Messages.gettext("Error: unable to compile this SuperBlock");

commit 9d1fe5fce95d5d65c92aefbd4bcfdfc5049cf6ca

Xcos: fix regression introduced by 65cb120eace8ce11aa5c4fd6af5fcaafb5a3d139
Change-Id: I24bfc17e12b15741b593d97b88b8cc790149ca9e

scilab/modules/xcos/src/java/org/scilab/modules/xcos/io/codec/BasicBlockCodec.java
@@ -36,182 +36,212 @@ import com.mxgraph.model.mxCell;
  */
 public class BasicBlockCodec extends XcosObjectCodec {
 
-	private static final String BASIC_BLOCK = BasicBlock.class.getSimpleName();
-	private static final String SIMULATION_FUNCTION_TYPE = "simulationFunctionType";
-	private static final String[] IGNORED_FIELDS = new String[] {SIMULATION_FUNCTION_TYPE, "locked", "parametersPCS"};
-	private static final Log LOG = LogFactory.getLog(BasicBlockCodec.class);
+    private static final String BASIC_BLOCK = BasicBlock.class.getSimpleName();
+    private static final String SIMULATION_FUNCTION_TYPE = "simulationFunctionType";
+    private static final String[] IGNORED_FIELDS = new String[] {
+            SIMULATION_FUNCTION_TYPE, "locked", "parametersPCS" };
+    private static final Log LOG = LogFactory.getLog(BasicBlockCodec.class);
 
-	/**
-	 * The constructor used on for configuration
-	 * @param template Prototypical instance of the object to be encoded/decoded.
-	 * @param exclude Optional array of fieldnames to be ignored.
-	 * @param idrefs Optional array of fieldnames to be converted to/from references.
-	 * @param mapping Optional mapping from field- to attributenames.
-	 */
-	public BasicBlockCodec(Object template, String[] exclude, String[] idrefs, Map<String, String> mapping)
-	{
-		super(template, exclude, idrefs, mapping);
-	}
-	
-	/**
-	 * Register all known codecs on the {@link mxCodecRegistry}.
-	 */
-	public static void register() {
-		mxCodecRegistry.addPackage("org.scilab.modules.xcos.block");
-		mxCodecRegistry.addPackage("org.scilab.modules.xcos.block.io");
-		mxCodecRegistry.addPackage("org.scilab.modules.xcos.block.positionning");
-		
-		for (BlockInterFunction function : BlockFactory.BlockInterFunction.values()) {
-			XcosObjectCodec codec = new BasicBlockCodec(function.getSharedInstance(),
-					IGNORED_FIELDS, REFS, null);
-			mxCodecRegistry.register(codec);
-		}
-		
-		XcosObjectCodec basicBlockCodec = new BasicBlockCodec(new BasicBlock(),
-				IGNORED_FIELDS, REFS, null);
-		mxCodecRegistry.register(basicBlockCodec);
-		
-		mxCellCodec cellCodec = new mxCellCodec(new mxCell(), null,
-				REFS, null);
-		mxCodecRegistry.register(cellCodec);
-		
-		/*
-		 * per block specific codec setup 
-		 */
-		BasicBlockCodec codec = (BasicBlockCodec) mxCodecRegistry.getCodec("AfficheBlock");
-		codec.exclude.add("printTimer");
-		codec.exclude.add("updateAction");
-		
-		/*
-		 * Compat. to remove old specific implementations
-		 * 
-		 * These implementation was available from Scilab-5.2.0 to Scilab-5.3.3.
-		 */
-		mxCodecRegistry.addAlias("ConstBlock", BASIC_BLOCK);
-		mxCodecRegistry.addAlias("GainBlock", BASIC_BLOCK);
-		mxCodecRegistry.addAlias("PrintBlock", BASIC_BLOCK);
-	}
-	
-	/**
-	 * Things to do before encoding
-	 * @param enc Codec that controls the encoding process.
-	 * @param obj Object to be encoded.
-	 * @param node XML node to encode the object into.
-	 * @return Returns the object to be encoded by the default encoding.
-	 * @see com.mxgraph.io.mxObjectCodec#beforeEncode(com.mxgraph.io.mxCodec, java.lang.Object, org.w3c.dom.Node)
-	 */
-	@Override
-	public Object beforeEncode(mxCodec enc, Object obj, Node node) {
-		((Element) node).setAttribute(SIMULATION_FUNCTION_TYPE,
-				String.valueOf(((BasicBlock) obj).getSimulationFunctionType()));
-		return super.beforeEncode(enc, obj, node);
-	}
+    /**
+     * The constructor used on for configuration
+     * 
+     * @param template
+     *            Prototypical instance of the object to be encoded/decoded.
+     * @param exclude
+     *            Optional array of fieldnames to be ignored.
+     * @param idrefs
+     *            Optional array of fieldnames to be converted to/from
+     *            references.
+     * @param mapping
+     *            Optional mapping from field- to attributenames.
+     */
+    public BasicBlockCodec(Object template, String[] exclude, String[] idrefs,
+            Map<String, String> mapping) {
+        super(template, exclude, idrefs, mapping);
+    }
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * Shortcut method to avoid old-XML-compat (before jgraphx-1.4) from {@link mxCellCodec}.
-	 * 
-	 * Without this shortcut, alias does not works (Class name String comparaison).
-	 * @see http://forum.jgraph.com/questions/1467/aliases-are-not-handled-on-mxcellcodecbeforedecode
-	 */
-	@Override
-	public Node beforeDecode(mxCodec dec, Node node, Object obj) {
-		return node;
-	}
-	
-	/**
-	 * Apply compatibility pattern to the decoded object
-	 * @param dec Codec that controls the decoding process.
-	 * @param node XML node to decode the object from.
-	 * @param obj Object decoded.
-	 * @return The Object transformed 
-	 * @see org.scilab.modules.xcos.io.XcosObjectCodec#afterDecode(com.mxgraph.io.mxCodec, org.w3c.dom.Node, java.lang.Object)
-	 */
-	@Override
-	public Object afterDecode(mxCodec dec, Node node, Object obj) {
-		if (!(obj instanceof BasicBlock)) {
-			LOG.error("Unable to decode " + obj);
-			return obj;
-		}
-	    final BasicBlock block = (BasicBlock) obj;
-	    
-		block.setSimulationFunctionType(SimulationFunctionType.DEFAULT);
+    /**
+     * Register all known codecs on the {@link mxCodecRegistry}.
+     */
+    public static void register() {
+        mxCodecRegistry.addPackage("org.scilab.modules.xcos.block");
+        mxCodecRegistry.addPackage("org.scilab.modules.xcos.block.io");
+        mxCodecRegistry
+                .addPackage("org.scilab.modules.xcos.block.positionning");
 
-	    String functionType = (((Element) node).getAttribute(SIMULATION_FUNCTION_TYPE));
-	    if (functionType != null && functionType.compareTo("") != 0) {
-		SimulationFunctionType type = BasicBlock.SimulationFunctionType.valueOf(functionType);
-		if (type != null) {
-		    block.setSimulationFunctionType(type);
-		}
-	    }
+        for (BlockInterFunction function : BlockFactory.BlockInterFunction
+                .values()) {
+            XcosObjectCodec codec = new BasicBlockCodec(
+                    function.getSharedInstance(), IGNORED_FIELDS, REFS, null);
+            mxCodecRegistry.register(codec);
+        }
 
-	    // Re associate the diagram container
-	    if (block instanceof SuperBlock) {
-	    	final SuperBlock superBlock = (SuperBlock) block;
-	    	if (superBlock.getChild() != null) {
-	    		superBlock.getChild().setContainer(superBlock);
-	    	}
-	    }
-	    
-	    // update style to replace direction by rotation and add the 
-	    // default style if absent
-	    StyleMap map = new StyleMap(((Element) node).getAttribute(STYLE));
-	    formatStyle(map, (BasicBlock) obj);
-		block.setStyle(map.toString());
-	    block.updateFieldsFromStyle();
-	    
-	    
-		/*
-		 * Compat. to remove old specific implementations
-		 * 
-		 * These implementation was available from Scilab-5.2.0 to Scilab-5.3.3.
-		 * 
-		 * Set default values stolen from the old implementation in case of default value.
-		 */
-	    if (map.containsKey("CONST_m")) {
-	    	if (block.getInterfaceFunctionName().equals(BasicBlock.DEFAULT_INTERFACE_FUNCTION)) {
-	    		block.setInterfaceFunctionName("CONST_m");
-	    	}
-	    	if (block.getSimulationFunctionName().equals(BasicBlock.DEFAULT_SIMULATION_FUNCTION)) {
-	    		block.setSimulationFunctionName("cstblk4");
-	    	}
-	    	if (block.getValue() == null) {
-	    		block.setValue("1");
-	    	}
-	    }
-	    if (map.containsKey("GAINBLK_f")) {
-	    	if (block.getInterfaceFunctionName().equals(BasicBlock.DEFAULT_INTERFACE_FUNCTION)) {
-	    		block.setInterfaceFunctionName("GAINBLK_f");
-	    	}
-	    }
-	    // PrintBlock has not default values
-	    
-	    
-	    return super.afterDecode(dec, node, obj);
-	}
+        XcosObjectCodec basicBlockCodec = new BasicBlockCodec(new BasicBlock(),
+                IGNORED_FIELDS, REFS, null);
+        mxCodecRegistry.register(basicBlockCodec);
 
-	/**
-	 * Format the style value
-	 * @param map the read style value
-	 * @param obj the block instance
-	 */
-	private void formatStyle(StyleMap map, BasicBlock obj) {
+        mxCellCodec cellCodec = new mxCellCodec(new mxCell(), null, REFS, null);
+        mxCodecRegistry.register(cellCodec);
 
-		// Append the bloc style if not present.
-		String name = obj.getInterfaceFunctionName();
-		if (!map.containsKey(name)) {
-			map.put(name, null);
-		}
+        /*
+         * per block specific codec setup
+         */
+        BasicBlockCodec codec = (BasicBlockCodec) mxCodecRegistry
+                .getCodec("AfficheBlock");
+        codec.exclude.add("printTimer");
+        codec.exclude.add("updateAction");
 
-		// Remove the abstract blockWithLabel (set as defaultVertex) 
-		map.remove("blockWithLabel");
-		
-		// Remove a custom shape value
-		// This is used for pre-5.2 schema with TEXT_f block with a custom
-		// "shape=label" style attribute.
-		map.remove("shape");
-		
-		formatStyle(map);
-	}
+        /*
+         * Compat. to remove old specific implementations
+         * 
+         * These implementation was available from Scilab-5.2.0 to Scilab-5.3.3.
+         */
+        mxCodecRegistry.addAlias("ConstBlock", BASIC_BLOCK);
+        mxCodecRegistry.addAlias("GainBlock", BASIC_BLOCK);
+        mxCodecRegistry.addAlias("PrintBlock", BASIC_BLOCK);
+    }
+
+    /**
+     * Things to do before encoding
+     * 
+     * @param enc
+     *            Codec that controls the encoding process.
+     * @param obj
+     *            Object to be encoded.
+     * @param node
+     *            XML node to encode the object into.
+     * @return Returns the object to be encoded by the default encoding.
+     * @see com.mxgraph.io.mxObjectCodec#beforeEncode(com.mxgraph.io.mxCodec,
+     *      java.lang.Object, org.w3c.dom.Node)
+     */
+    @Override
+    public Object beforeEncode(mxCodec enc, Object obj, Node node) {
+        ((Element) node).setAttribute(SIMULATION_FUNCTION_TYPE,
+                String.valueOf(((BasicBlock) obj).getSimulationFunctionType()));
+        return super.beforeEncode(enc, obj, node);
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * Shortcut method to avoid old-XML-compat (before jgraphx-1.4) from
+     * {@link mxCellCodec}.
+     * 
+     * Without this shortcut, alias does not works (Class name String
+     * comparaison).
+     * 
+     * @see http://forum.jgraph.com/questions/1467/aliases-are-not-handled-on-
+     *      mxcellcodecbeforedecode
+     */
+    @Override
+    public Node beforeDecode(mxCodec dec, Node node, Object obj) {
+        return node;
+    }
+
+    /**
+     * Apply compatibility pattern to the decoded object
+     * 
+     * @param dec
+     *            Codec that controls the decoding process.
+     * @param node
+     *            XML node to decode the object from.
+     * @param obj
+     *            Object decoded.
+     * @return The Object transformed
+     * @see org.scilab.modules.xcos.io.XcosObjectCodec#afterDecode(com.mxgraph.io.mxCodec,
+     *      org.w3c.dom.Node, java.lang.Object)
+     */
+    @Override
+    public Object afterDecode(mxCodec dec, Node node, Object obj) {
+        if (!(obj instanceof BasicBlock)) {
+            LOG.error("Unable to decode " + obj);
+            return obj;
+        }
+        final BasicBlock block = (BasicBlock) obj;
+
+        block.setSimulationFunctionType(SimulationFunctionType.DEFAULT);
+
+        String functionType = (((Element) node)
+                .getAttribute(SIMULATION_FUNCTION_TYPE));
+        if (functionType != null && functionType.compareTo("") != 0) {
+            SimulationFunctionType type = BasicBlock.SimulationFunctionType
+                    .valueOf(functionType);
+            if (type != null) {
+                block.setSimulationFunctionType(type);
+            }
+        }
+
+        // Re associate the diagram container
+        if (block instanceof SuperBlock) {
+            final SuperBlock superBlock = (SuperBlock) block;
+            if (superBlock.getChild() != null) {
+                superBlock.getChild().setContainer(superBlock);
+            }
+        }
+
+        // update style to replace direction by rotation and add the
+        // default style if absent
+        StyleMap map = new StyleMap(((Element) node).getAttribute(STYLE));
+        formatStyle(map, (BasicBlock) obj);
+        block.setStyle(map.toString());
+        block.updateFieldsFromStyle();
+
+        /*
+         * Compat. to remove old specific implementations
+         * 
+         * These implementation was available from Scilab-5.2.0 to Scilab-5.3.3.
+         * 
+         * Set default values stolen from the old implementation in case of
+         * default value.
+         */
+        if (node.getNodeName().equals("ConstBlock")) {
+            if (block.getInterfaceFunctionName().equals(
+                    BasicBlock.DEFAULT_INTERFACE_FUNCTION)) {
+                block.setInterfaceFunctionName("CONST_m");
+            }
+            if (block.getSimulationFunctionName().equals(
+                    BasicBlock.DEFAULT_SIMULATION_FUNCTION)) {
+                block.setSimulationFunctionName("cstblk4");
+            }
+            if (block.getValue() == null) {
+                block.setValue("1");
+            }
+        }
+        if (node.getNodeName().equals("GainBlock")) {
+            if (block.getInterfaceFunctionName().equals(
+                    BasicBlock.DEFAULT_INTERFACE_FUNCTION)) {
+                block.setInterfaceFunctionName("GAINBLK_f");
+            }
+        }
+        // PrintBlock has no default values
+
+        return super.afterDecode(dec, node, obj);
+    }
+
+    /**
+     * Format the style value
+     * 
+     * @param map
+     *            the read style value
+     * @param obj
+     *            the block instance
+     */
+    private void formatStyle(StyleMap map, BasicBlock obj) {
+
+        // Append the bloc style if not present.
+        String name = obj.getInterfaceFunctionName();
+        if (!map.containsKey(name)) {
+            map.put(name, null);
+        }
+
+        // Remove the abstract blockWithLabel (set as defaultVertex)
+        map.remove("blockWithLabel");
+
+        // Remove a custom shape value
+        // This is used for pre-5.2 schema with TEXT_f block with a custom
+        // "shape=label" style attribute.
+        map.remove("shape");
+
+        formatStyle(map);
+    }
 }