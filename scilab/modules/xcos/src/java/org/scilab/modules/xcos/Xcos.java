51510676e75f1a47cbe315fd69585c61475ad998
Clément DAVID <clement.david@scilab-enterprises.com>
2011-10-19 09:57:59 +0200

    JGraphX: update to jgraphx-1.8.0.1
    Url: http://downloads.jgraph.com/downloads/jgraphx/archive/jgraphx-1_8_0_1.zip
    Change-Id: I90cd34b5cd3c9f670653d971edf22eb902636156

--- scilab/modules/xcos/src/java/org/scilab/modules/xcos/Xcos.java
+++ scilab/modules/xcos/src/java/org/scilab/modules/xcos/Xcos.java
@@ -69,8 +69,7 @@ public final class Xcos {
     /*
      * Dependencies version
      */
-    private static final List<String> MXGRAPH_VERSIONS = Arrays.asList(
-            "1.7.0.6", "1.7.0.7");
+    private static final List<String> MXGRAPH_VERSIONS = Arrays.asList("1.8.0.0", "1.8.0.1");
     private static final List<String> HDF5_VERSIONS = Arrays.asList(
             "[1, 8, 4]", "[1, 8, 5]", "[1, 8, 6]", "[1, 8, 7]");
     private static final List<String> BATIK_VERSIONS = Arrays.asList("1.7");
