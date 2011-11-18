9a28bb0f2869dda0e24afcd88c4494b44f7fc6e0
Vincent COUVERT <vincent.couvert@scilab.org>
2011-11-18 14:44:01 +0100

    xfpolys now uses MVC
    Change-Id: I728741b0f85d28ad8c1849fd1663656a2979bf80

--- scilab/modules/graphics/sci_gateway/c/sci_xfpolys.c
+++ scilab/modules/graphics/sci_gateway/c/sci_xfpolys.c
@@ -2,6 +2,7 @@
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2004-2006 - INRIA - Fabrice Leray
  * Copyright (C) 2006 - INRIA - Jean-Baptiste Silvy
+ * Copyright (C) 2011 - DIGITEO - Vincent COUVERT
  *
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
@@ -21,92 +22,106 @@
 #include "BuildObjects.h"
 #include "Scierror.h"
 #include "sciCall.h"
-#include "GetProperty.h"
 #include "DrawObjects.h"
 #include "localization.h"
-#include "GraphicSynchronizerInterface.h"
-#include "HandleManagement.h"
+
+#include "CurrentObject.h"
+
+#include "graphicObjectProperties.h"
+#include "getGraphicObjectProperty.h"
 
 /*--------------------------------------------------------------------------*/
-int sci_xfpolys( char *fname, unsigned long fname_len )
+int sci_xfpolys(char *fname, unsigned long fname_len)
 {
-  int m1,n1,l1,m2,n2,l2,m3,n3,l3,mn2,v1=0;
-  /* v1 is the flag used for flat (v1==1) or interpolated (v1==2) shading */
+    int m1 = 0, n1 = 0, l1 = 0;
+    int m2 = 0, n2 = 0, l2 = 0;
+    int m3 = 0, n3 = 0, l3 = 0;
+    int mn2 = 0;
 
-  int i,color;
-  long hdl;
-  sciPointObj * psubwin = NULL ;
+    int v1 = 0;                 /* v1 is the flag used for flat (v1==1) or interpolated (v1==2) shading */
+    int i = 0;
 
+    long hdl = 0;
 
-  CheckRhs(2,3);
-  GetRhsVar(1,MATRIX_OF_DOUBLE_DATATYPE,&m1,&n1,&l1);
-  GetRhsVar(2,MATRIX_OF_DOUBLE_DATATYPE,&m2,&n2,&l2);
-  CheckSameDims(1,2,m1,n1,m2,n2);
-  mn2 = m2 * n2;
-  if ( mn2 == 0 ) {
-      LhsVar(1)=0;
-      PutLhsVar();
-      return 0;
-  }
+    char *pstSubWinUID = NULL;
+    int iSubWinForeground = 0;
+    int *piSubWinForeground = &iSubWinForeground;
 
+    CheckRhs(2, 3);
 
-  if (Rhs == 3)
-  {
-    GetRhsVar(3,MATRIX_OF_INTEGER_DATATYPE,&m3,&n3,&l3);
-
-    if (m3*n3==m1*n1)
+    GetRhsVar(1, MATRIX_OF_DOUBLE_DATATYPE, &m1, &n1, &l1);
+    GetRhsVar(2, MATRIX_OF_DOUBLE_DATATYPE, &m2, &n2, &l2);
+    CheckSameDims(1, 2, m1, n1, m2, n2);
+    mn2 = m2 * n2;
+    if (mn2 == 0)
     {
-      CheckSameDims(1,3,m1,n1,m3,n3);
-      v1=2; /* interpolated shading */
-
-      if ( m3 != 3 && m3 != 4 )
-      {
-        Scierror(999,_("%s: Interpolated shading only works for polygons of size %d or %d\n"),fname, 3, 4);
+        LhsVar(1) = 0;
+        PutLhsVar();
         return 0;
-      }
-    } else
-    {
-      CheckVector(3,m3,n3);
-      CheckDimProp(2,3,m3 * n3 != n2);
-      v1=1; /* flat shading */
     }
-  }
-  else
-  {
-    int un=1,ix;
-    CreateVar(3,MATRIX_OF_INTEGER_DATATYPE,&un,&n2,&l3);
-    for (ix = 0 ; ix < n2 ; ++ix) *istk(l3+ix) = 0;
-    m3 = n3 = 1;
-  }
-  psubwin = sciGetCurrentSubWin();
 
-  startFigureDataWriting(sciGetParentFigure(psubwin));
-  for (i = 0; i < n1; ++i) {
-    if(m3 == 1 || n3 == 1) /* color vector specified */
+    if (Rhs == 3)
     {
-      if (*istk(l3+i) == 0)
-      {
-        color= sciGetForeground(psubwin);
-        Objpoly (stk(l1+(i*m1)),stk(l2+(i*m1)),m1,1,color,&hdl);
-      }
-      else
-      {
-        Objfpoly (stk(l1+(i*m1)),stk(l2+(i*m1)),m1,istk(l3+i),&hdl,v1);
-      }
+        GetRhsVar(3, MATRIX_OF_INTEGER_DATATYPE, &m3, &n3, &l3);
+
+        if (m3 * n3 == m1 * n1)
+        {
+            CheckSameDims(1, 3, m1, n1, m3, n3);
+            v1 = 2;             /* interpolated shading */
+
+            if (m3 != 3 && m3 != 4)
+            {
+                Scierror(999, _("%s: Interpolated shading only works for polygons of size %d or %d\n"), fname, 3, 4);
+                return 0;
+            }
+        }
+        else
+        {
+            CheckVector(3, m3, n3);
+            CheckDimProp(2, 3, m3 * n3 != n2);
+            v1 = 1;             /* flat shading */
+        }
     }
-    else /* we have a color matrix used for interpolated shading : one color per vertex */
+    else
     {
-      Objfpoly (stk(l1+(i*m1)),stk(l2+(i*m1)),m1,istk(l3+i*m3),&hdl,v1);
+        int un = 1, ix = 0;
+
+        CreateVar(3, MATRIX_OF_INTEGER_DATATYPE, &un, &n2, &l3);
+        for (ix = 0; ix < n2; ++ix)
+        {
+            *istk(l3 + ix) = 0;
+        }
+        m3 = n3 = 1;
     }
-  }
-  /** construct Compound and make it current object**/
-  sciSetCurrentObj (ConstructCompoundSeq (n1));
-  endFigureDataWriting(sciGetParentFigure(psubwin));
 
-  sciDrawObjIfRequired(sciGetCurrentObj ());
+    pstSubWinUID = getOrCreateDefaultSubwin();
 
-  LhsVar(1)=0;
-  PutLhsVar();
-  return 0;
+    for (i = 0; i < n1; ++i)
+    {
+        if (m3 == 1 || n3 == 1) /* color vector specified */
+        {
+            if (*istk(l3 + i) == 0)
+            {
+                getGraphicObjectProperty(pstSubWinUID, __GO_LINE_COLOR__, jni_int, (void **)&piSubWinForeground);
+                Objpoly(stk(l1 + (i * m1)), stk(l2 + (i * m1)), m1, 1, iSubWinForeground, &hdl);
+            }
+            else
+            {
+                Objfpoly(stk(l1 + (i * m1)), stk(l2 + (i * m1)), m1, istk(l3 + i), &hdl, v1);
+            }
+        }
+        else                    /* we have a color matrix used for interpolated shading : one color per vertex */
+        {
+            Objfpoly(stk(l1 + (i * m1)), stk(l2 + (i * m1)), m1, istk(l3 + i * m3), &hdl, v1);
+        }
+    }
+
+    /** Construct Compound and make it current object**/
+    setCurrentObject(ConstructCompoundSeq(n1));
+
+    LhsVar(1) = 0;
+    PutLhsVar();
+    return 0;
 }
+
 /*--------------------------------------------------------------------------*/
