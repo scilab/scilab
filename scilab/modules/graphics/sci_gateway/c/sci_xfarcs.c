47020c2925c3701b9e3740d742f626aee6faa171
Vincent COUVERT <vincent.couvert@scilab.org>
2011-11-18 22:19:34 +0100

    xfarcs now uses MVC
    Change-Id: Id159eb48b504b6a77f7dacdd5e403643a7cd46e2

--- scilab/modules/graphics/sci_gateway/c/sci_xfarcs.c
+++ scilab/modules/graphics/sci_gateway/c/sci_xfarcs.c
@@ -19,65 +19,75 @@
 #include "gw_graphics.h"
 #include "sciCall.h"
 #include "stack-c.h"
-#include "BuildObjects.h"
-#include "GraphicSynchronizerInterface.h"
 #include "localization.h"
 #include "Scierror.h"
-#include "DrawingBridge.h"
-#include "HandleManagement.h"
+#include "BuildObjects.h"
+
+#include "CurrentObject.h"
+
+#include "graphicObjectProperties.h"
+#include "getGraphicObjectProperty.h"
 
 /*--------------------------------------------------------------------------*/
-int sci_xfarcs( char * fname, unsigned long fname_len )
+int sci_xfarcs(char *fname, unsigned long fname_len)
 {
-  int m1,n1,l1,m2,n2,l2;
-  long  hdl;
-  int i;
-  double angle1;
-  double angle2;
-  sciPointObj * pFigure = NULL;
+    int m1 = 0, n1 = 0, l1 = 0;
+    int m2 = 0, n2 = 0, l2 = 0;
 
-  CheckRhs(1,2);
+    long hdl = 0;
 
-  GetRhsVar(1,MATRIX_OF_DOUBLE_DATATYPE,&m1,&n1,&l1);
-  if (m1 != 6) {
-    Scierror(999,_("%s: Wrong size for input argument #%d: %s expected.\n"),fname, 1, "(6,n)");
-    return 0;
-  }
+    int i = 0;
 
-  if (Rhs == 2) {
-    GetRhsVar(2,MATRIX_OF_INTEGER_DATATYPE,&m2,&n2,&l2);
-    CheckVector(2,m2,n2);
-    if ( n1 != m2*n2) {
-      Scierror(999,_("%s: Wrong size for input arguments #%d and #%d.\n"),fname,1,2);
-      return 0;
+    double angle1 = 0.0;
+    double angle2 = 0.0;
+
+    CheckRhs(1, 2);
+
+    GetRhsVar(1, MATRIX_OF_DOUBLE_DATATYPE, &m1, &n1, &l1);
+    if (m1 != 6)
+    {
+        Scierror(999, _("%s: Wrong size for input argument #%d: %s expected.\n"), fname, 1, "(6,n)");
+        return 0;
     }
-  }
-  else
-  {
-    m2=1,n2=n1; CreateVar(2,MATRIX_OF_INTEGER_DATATYPE,&m2,&n2,&l2);
-    for (i = 0; i < n2; ++i) { *istk(l2 + i) = i+1 ; }
-  }
 
-  pFigure = sciGetCurrentFigure();
+    if (Rhs == 2)
+    {
+        GetRhsVar(2, MATRIX_OF_INTEGER_DATATYPE, &m2, &n2, &l2);
+        CheckVector(2, m2, n2);
+        if (n1 != m2 * n2)
+        {
+            Scierror(999, _("%s: Wrong size for input arguments #%d and #%d.\n"), fname, 1, 2);
+            return 0;
+        }
+    }
+    else
+    {
+        m2 = 1;
+        n2 = n1;
+        CreateVar(2, MATRIX_OF_INTEGER_DATATYPE, &m2, &n2, &l2);
+        for (i = 0; i < n2; ++i)
+        {
+            *istk(l2 + i) = i + 1;
+        }
+    }
 
+    getOrCreateDefaultSubwin();
 
-  startFigureDataWriting(pFigure);
-  for (i = 0; i < n1; ++i)
-  {
-    angle1 = DEG2RAD(*stk(l1+(6*i)+4) / 64.0);
-    angle2 = DEG2RAD(*stk(l1+(6*i)+5) / 64.0);
-    Objarc (&angle1,&angle2,stk(l1+(6*i)),stk(l1+(6*i)+1),
-      stk(l1+(6*i)+2),stk(l1+(6*i)+3),istk(l2+i),istk(l2+i),TRUE,FALSE,&hdl);
-  }
-  /** construct Compound and make it current object **/
-  sciSetCurrentObj(ConstructCompoundSeq (n1));
-  endFigureDataWriting(pFigure);
+    for (i = 0; i < n1; ++i)
+    {
+        angle1 = DEG2RAD(*stk(l1 + (6 * i) + 4) / 64.0);
+        angle2 = DEG2RAD(*stk(l1 + (6 * i) + 5) / 64.0);
+        Objarc(&angle1, &angle2, stk(l1 + (6 * i)), stk(l1 + (6 * i) + 1),
+               stk(l1 + (6 * i) + 2), stk(l1 + (6 * i) + 3), istk(l2 + i), istk(l2 + i), TRUE, FALSE, &hdl);
+    }
 
-  sciDrawObj(sciGetCurrentObj());
+    /** Construct Compound and make it current object **/
+    setCurrentObject(ConstructCompoundSeq(n1));
 
-  LhsVar(1)=0;
-	PutLhsVar();
-  return 0;
+    LhsVar(1) = 0;
+    PutLhsVar();
 
+    return 0;
 }
+
 /*--------------------------------------------------------------------------*/
