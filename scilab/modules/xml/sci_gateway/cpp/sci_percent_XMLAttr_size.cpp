8d78386ecf9f76af1a3259bc1ab43c3bd6030302
Calixte DENIZET <calixte.denizet@scilab-enterprises.com>
2012-01-26 11:11:31 +0100

    Bug 10594 fixed: size could not be used with two arguments on XMLList
    Change-Id: I917d430a76f0102ee0108c32bdda0376d60d4401

--- scilab/modules/xml/sci_gateway/cpp/sci_percent_XMLAttr_size.cpp
+++ scilab/modules/xml/sci_gateway/cpp/sci_percent_XMLAttr_size.cpp
@@ -21,6 +21,7 @@ extern "C"
 #include "api_scilab.h"
 #include "xml_mlist.h"
 #include "localization.h"
+#include <string.h>
 }
 
 using namespace org_modules_xml;
@@ -31,11 +32,13 @@ int sci_percent_XMLAttr_size(char *fname, unsigned long fname_len)
     int id;
     SciErr err;
     double d[2] = { 1, 0 };
-    int *addr = 0;
-    XMLAttr *attrs;
+    int * addr = 0;
+    XMLAttr * attrs;
+    int size;
+    char * option = 0;
 
     CheckLhs(1, 1);
-    CheckRhs(1, 1);
+    CheckRhs(1, 2);
 
     err = getVarAddressFromPosition(pvApiCtx, 1, &addr);
     if (err.iErr)
@@ -53,15 +56,65 @@ int sci_percent_XMLAttr_size(char *fname, unsigned long fname_len)
     }
 
     id = getXMLObjectId(addr, pvApiCtx);
-    attrs = XMLObject::getFromId < XMLAttr > (id);
+    attrs = XMLObject::getFromId<XMLAttr>(id);
     if (!attrs)
     {
-        Scierror(999, gettext("%s: XML Attributes does not exist.\n"), fname);
+        Scierror(999, gettext("%s: XMLList does not exist.\n"), fname);
         return 0;
     }
 
-    d[1] = (double)attrs->getSize();
-    err = createMatrixOfDouble(pvApiCtx, Rhs + 1, 1, 2, d);
+    if (Rhs == 2)
+    {
+        err = getVarAddressFromPosition(pvApiCtx, 2, &addr);
+        if (err.iErr)
+        {
+            printError(&err, 0);
+            Scierror(999, _("%s: Can not read input argument #%d.\n"), fname, 2);
+            return 0;
+        }
+
+        if (!isStringType(pvApiCtx, addr))
+        {
+            Scierror(999, gettext("%s: Wrong type for input argument #%d: A string expected.\n"), fname, 2);
+            return 0;
+        }
+        getAllocatedSingleString(pvApiCtx, addr, &option);
+
+        if (strcmp(option, "r") && strcmp(option, "c") && strcmp(option, "*"))
+        {
+            freeAllocatedSingleString(option);
+            Scierror(44, _("%s: Wrong value for input argument #%d: '%s', '%s' or '%s' expected.\n"), fname, 2, "r", "c", "*");
+            return 0;
+        }
+    }
+
+    size = (double)attrs->getSize();
+    if (Rhs == 1)
+    {
+        d[1] = size;
+        if (size == 0)
+        {
+            d[0] = 0;
+        }
+        err = createMatrixOfDouble(pvApiCtx, Rhs + 1, 1, 2, d);
+    }
+    else
+    {
+        if (option[0] == 'r')
+        {
+            if (size == 0)
+            {
+                d[0] = 0;
+            }
+        }
+        else
+        {
+            d[0] = size;
+        }
+        freeAllocatedSingleString(option);
+        err = createMatrixOfDouble(pvApiCtx, Rhs + 1, 1, 1, d);
+    }
+
     if (err.iErr)
     {
         printError(&err, 0);
---
+++ scilab/modules/xml/tests/unit_tests/size.tst
@@ -0,0 +1,20 @@
+// ===========================================================================
+// Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+// Copyright (C) 2012 - Scilab Enterprises - Calixte DENIZET
+//
+//  This file is distributed under the same license as the Scilab package.
+// ===========================================================================
+
+
+doc = xmlReadStr("<root><a b=""b"" c=""c"" d=""d""><b>b</b></a><c>c</c></root>");
+assert_checkequal(size(doc.root.children), [1 2]);
+assert_checkequal(size(doc.root.children, "r"), 1);
+assert_checkequal(size(doc.root.children, "c"), 2);
+assert_checkequal(size(doc.root.children, "*"), 2);
+
+assert_checkequal(size(doc.root.children(1).attributes), [1 3]);
+assert_checkequal(size(doc.root.children(1).attributes, "r"), 1);
+assert_checkequal(size(doc.root.children(1).attributes, "c"), 3);
+assert_checkequal(size(doc.root.children(1).attributes, "*"), 3);
+
+xmlDelete(doc);
