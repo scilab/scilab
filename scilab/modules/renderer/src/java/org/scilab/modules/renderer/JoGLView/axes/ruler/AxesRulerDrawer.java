49203aad96bf8f7a3bf9f2a35e7bca38d4cf0cc7
Pierre LANDO <pierre.lando@scilab.org>
2012-02-15 17:31:24 +0100

    Unlike scilab 5.3, grids were not drawn at axis bound.
    Change-Id: I783427006d54c218402ee62b38599009cd9bf578

--- scilab/modules/renderer/src/java/org/scilab/modules/renderer/JoGLView/axes/ruler/AxesRulerDrawer.java
+++ scilab/modules/renderer/src/java/org/scilab/modules/renderer/JoGLView/axes/ruler/AxesRulerDrawer.java
@@ -394,18 +394,14 @@ public class AxesRulerDrawer {
      */
     private FloatBuffer getXGridData(double[] values, RulerModel rulerModel) {
         FloatBuffer vertexData = FloatBuffer.allocate(values.length * 16);
-        int limit = 0;
         for (double value : values) {
             float p = (float) rulerModel.getPosition(value).getX();
-            if ((p != -1) && (p!=1)) {
-                vertexData.put(p); vertexData.put(+1); vertexData.put(+1); vertexData.put(1);
-                vertexData.put(p); vertexData.put(-1); vertexData.put(+1); vertexData.put(1);
-                vertexData.put(p); vertexData.put(+1); vertexData.put(+1); vertexData.put(1);
-                vertexData.put(p); vertexData.put(+1); vertexData.put(-1); vertexData.put(1);
-                limit += 16;
-            }
+            vertexData.put(p); vertexData.put(+1); vertexData.put(+1); vertexData.put(1);
+            vertexData.put(p); vertexData.put(-1); vertexData.put(+1); vertexData.put(1);
+            vertexData.put(p); vertexData.put(+1); vertexData.put(+1); vertexData.put(1);
+            vertexData.put(p); vertexData.put(+1); vertexData.put(-1); vertexData.put(1);
         }
-        vertexData.limit(limit);
+        vertexData.rewind();
         return vertexData;
     }
 
@@ -417,18 +413,14 @@ public class AxesRulerDrawer {
      */
     private FloatBuffer getYGridData(double[] values, RulerModel rulerModel) {
         FloatBuffer vertexData = FloatBuffer.allocate(values.length * 16);
-        int limit = 0;
         for (double value : values) {
             float p = (float) rulerModel.getPosition(value).getY();
-            if ((p != -1) && (p!=1)) {
-                vertexData.put(+1); vertexData.put(p); vertexData.put(+1); vertexData.put(1);
-                vertexData.put(-1); vertexData.put(p); vertexData.put(+1); vertexData.put(1);
-                vertexData.put(+1); vertexData.put(p); vertexData.put(+1); vertexData.put(1);
-                vertexData.put(+1); vertexData.put(p); vertexData.put(-1); vertexData.put(1);
-                limit += 16;
-            }
+            vertexData.put(+1); vertexData.put(p); vertexData.put(+1); vertexData.put(1);
+            vertexData.put(-1); vertexData.put(p); vertexData.put(+1); vertexData.put(1);
+            vertexData.put(+1); vertexData.put(p); vertexData.put(+1); vertexData.put(1);
+            vertexData.put(+1); vertexData.put(p); vertexData.put(-1); vertexData.put(1);
         }
-        vertexData.limit(limit);
+        vertexData.rewind();
         return vertexData;
     }
 
@@ -443,13 +435,11 @@ public class AxesRulerDrawer {
         int limit = 0;
         for (double value : values) {
             float p = (float) rulerModel.getPosition(value).getZ();
-            if ((p != -1) && (p!=1)) {
-                vertexData.put(+1); vertexData.put(+1); vertexData.put(p); vertexData.put(1);
-                vertexData.put(-1); vertexData.put(+1); vertexData.put(p); vertexData.put(1);
-                vertexData.put(+1); vertexData.put(+1); vertexData.put(p); vertexData.put(1);
-                vertexData.put(+1); vertexData.put(-1); vertexData.put(p); vertexData.put(1);
-                limit += 16;
-            }
+            vertexData.put(+1); vertexData.put(+1); vertexData.put(p); vertexData.put(1);
+            vertexData.put(-1); vertexData.put(+1); vertexData.put(p); vertexData.put(1);
+            vertexData.put(+1); vertexData.put(+1); vertexData.put(p); vertexData.put(1);
+            vertexData.put(+1); vertexData.put(-1); vertexData.put(p); vertexData.put(1);
+            limit += 16;
         }
         vertexData.limit(limit);
         return vertexData;
