67d82be4085e2b29a45c770f3b13bf82a4f9a8f1
Bruno JOFRET <bruno.jofret@scilab-enterprises.com>
2012-03-07 11:22:55 +0100

    Fix MacOSX freeze using GLJPanel.
    Change-Id: I8038ee3cdcd4f1abf5229ba68c3b6bd0002a62e0

--- scilab/modules/gui/src/java/org/scilab/modules/gui/bridge/canvas/SwingScilabCanvas.java
+++ scilab/modules/gui/src/java/org/scilab/modules/gui/bridge/canvas/SwingScilabCanvas.java
@@ -4,11 +4,12 @@
  * Copyright (C) 2007 - INRIA - Marouane BEN JELLOUL
  * Copyright (C) 2008 - INRIA - Jean-Baptiste Silvy
  * Copyright (C) 2008 - INRIA - Bruno JOFRET
- * 
+ * Copyright (C) 2012 - Scilab Enterprises - Bruno JOFRET
+ *
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
  * you should have received as part of this distribution.  The terms
- * are also available at    
+ * are also available at
  * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
  *
  */
@@ -28,6 +29,7 @@ import javax.media.opengl.GLCanvas;
 import javax.media.opengl.GLCapabilities;
 import javax.media.opengl.GLJPanel;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 import org.scilab.forge.scirenderer.Canvas;
 import org.scilab.forge.scirenderer.implementation.jogl.JoGLCanvasFactory;
@@ -46,7 +48,7 @@ import org.scilab.modules.renderer.utils.RenderingCapabilities;
 /**
  * Swing implementation for Scilab Canvas in GUIs This implementation requires
  * JOGL
- * 
+ *
  * @author Vincent COUVERT
  * @author Marouane BEN JELLOUL
  * @author Jean-Baptiste Silvy
@@ -54,25 +56,52 @@ import org.scilab.modules.renderer.utils.RenderingCapabilities;
 public class SwingScilabCanvas extends JPanel implements SimpleCanvas {
 
 	private static final long serialVersionUID = 6101347094617535625L;
-	
+
+	/*
+	 * Using GLJPanel for MacOSX may lead to a deadlock on deletion.
+	 * Wrap call to removeNotify to ensure we are not outside Swing Thread
+	 * and PBuffer is not locked.
+	 */
+	private final class MacOSXGLJPanel extends GLJPanel
+	{
+        private static final long serialVersionUID = -6166986369022555750L;
+
+        private void superRemoveNotify()
+	    {
+	        super.removeNotify();
+	    }
+
+	    @Override
+	    public void removeNotify() {
+	        final MacOSXGLJPanel panel = this;
+	            SwingUtilities.invokeLater(new Runnable() {
+	                    public void run() {
+	                        panel.superRemoveNotify();
+	                }
+	            });
+	        }
+	}
+
     public SwingScilabCanvas(int figureId, Figure figure)
 	{
 	    super(new PanelLayout());
-	    
+
 	    /*
 	     * Even with the good Java 1.6 version
 	     * MacOSX does not manage mixing ligthweight and heavyweight components
-	     * Keep GLJPanel as OpenGL component for now 
+	     * Use MacOSXGLJPanel as OpenGL component for now since GLJPanel will
+	     * lead to deadlock on deletion.
 	     */
 	    if (OS.get() == OS.MAC) {
-		    GLJPanel glCanvas = new GLJPanel();
+		    GLJPanel glCanvas = new MacOSXGLJPanel();
+		    glCanvas.setEnabled(true);
 		    add(glCanvas, PanelLayout.GL_CANVAS);
-		    	    
+
 	        Canvas canvas = JoGLCanvasFactory.createCanvas(glCanvas);
 	        canvas.setMainDrawer(new DrawerVisitor(canvas, figure));
 	        FigureInteraction figureInteraction = new FigureInteraction(glCanvas, figure.getIdentifier());
 	        figureInteraction.setEnable(true);
-	    	
+
 	    }
 	    else {
 	    	GLCanvas glCanvas = new GLCanvas();
@@ -85,21 +114,22 @@ public class SwingScilabCanvas extends JPanel implements SimpleCanvas {
 	    	figureInteraction.setEnable(true);
 	    }
 	}
+
 	/**
 	 * Constructor
-	 * 
+	 *
 	 * @param cap
 	 *            GLCapabilities associated to the GLJPanel
 	 * @param figureIndex
 	 *            index of the displayed figure
 	 */
 	public SwingScilabCanvas(GLCapabilities cap, int figureIndex) {
-		
+
 	}
 
 	/**
 	 * Create a Scilab Canvas
-	 * 
+	 *
 	 * @param figureIndex index of the displayed figure
 	 * @param antialiasingQuality Specify the number of pass to use for antialiasing.
      *                            If its value is 0, then antialiasing is disable.
@@ -111,7 +141,7 @@ public class SwingScilabCanvas extends JPanel implements SimpleCanvas {
 
 	/**
 	 * Draws a Scilab canvas
-	 * 
+	 *
 	 * @see org.scilab.modules.gui.UIElement#draw()
 	 */
 	public void draw() {
@@ -121,7 +151,7 @@ public class SwingScilabCanvas extends JPanel implements SimpleCanvas {
 
 	/**
 	 * Gets the dimensions (width and height) of a Scilab Canvas
-	 * 
+	 *
 	 * @return the size of the canvas
 	 * @see org.scilab.modules.gui.UIElement#getDims()
 	 */
@@ -131,7 +161,7 @@ public class SwingScilabCanvas extends JPanel implements SimpleCanvas {
 
 	/**
 	 * Gets the position (X-coordinate and Y-coordinate) of a Scilab canvas
-	 * 
+	 *
 	 * @return the position of the canvas
 	 * @see org.scilab.modules.gui.UIElement#getPosition()
 	 */
@@ -141,7 +171,7 @@ public class SwingScilabCanvas extends JPanel implements SimpleCanvas {
 
 	/**
 	 * Sets the dimensions (width and height) of a Scilab Canvas
-	 * 
+	 *
 	 * @param newSize
 	 *            the size we want to set to the canvas
 	 * @see org.scilab.modules.gui.UIElement#setDims(org.scilab.modules.gui.utils.Size)
@@ -149,24 +179,24 @@ public class SwingScilabCanvas extends JPanel implements SimpleCanvas {
 	public void setDims(Size newSize) {
 		// get the greatest size we can use
 		int[] maxSize = RenderingCapabilities.getMaxCanvasSize();
-		
+
 		// make suze size is not greater than the max size
 		Dimension finalDim = new Dimension(Math.min(newSize.getWidth(), maxSize[0]),
 										   Math.min(newSize.getHeight(), maxSize[1]));
-		
+
 		setSize(finalDim);
-		
+
 		// if the size is too large, throw an exception
 		if (newSize.getWidth() > maxSize[0] || newSize.getHeight() > maxSize[1]) {
 			throw new IllegalArgumentException();
 		}
-		
+
 	}
-	
+
 
 	/**
 	 * Sets the position (X-coordinate and Y-coordinate) of a Scilab canvas
-	 * 
+	 *
 	 * @param newPosition
 	 *            the position we want to set to the canvas
 	 * @see org.scilab.modules.gui.UIElement#setPosition(org.scilab.modules.gui.utils.Position)
@@ -176,25 +206,25 @@ public class SwingScilabCanvas extends JPanel implements SimpleCanvas {
 	}
 	/**
 	 * Get the Figure Index : the Scilab ID of the figure.
-	 * 
+	 *
 	 * @return the ID.
 	 */
 	public int getFigureIndex() {
 		// to avoid storing the data everywhere
 		return getParentAxes().getFigureId();
 	}
-	
-	
+
+
 	/**
 	 * Set the background of the Canvas.
 	 * @param red red channel
-	 * @param green green channel 
+	 * @param green green channel
 	 * @param blue blue channel
 	 */
 	public void setBackgroundColor(double red, double green, double blue) {
 		this.setBackground(new Color((float) red, (float) green, (float) blue));
 	}
-	
+
 	/**
 	 * Create an interactive selection rectangle and return its pixel coordinates
 	 * @param isClick specify whether the rubber box is selected by one click for each one of the two edge
@@ -207,21 +237,21 @@ public class SwingScilabCanvas extends JPanel implements SimpleCanvas {
 	public int rubberBox(boolean isClick, boolean isZoom, int[] initialRect, int[] endRect) {
 		return ScilabRubberBox.getRectangle(this, isClick, isZoom, initialRect, endRect);
 	}
-	
+
 	/**
 	 * Disable the canvas befor closing
 	 */
 	public void close() {
 	}
-	
+
 	/**
 	 * Take a screenshot of the figure and put it into a BufferedImage
 	 * @return a BufferedImage
 	 */
-	public BufferedImage dumpAsBufferedImage() {			
+	public BufferedImage dumpAsBufferedImage() {
 		return null;
 	}
-	
+
 	/**
 	 * Set double buffer mode on or Off
 	 * @param useSingleBuffer if true use single buffer if false use double buffering
@@ -229,14 +259,14 @@ public class SwingScilabCanvas extends JPanel implements SimpleCanvas {
 	public void setSingleBuffered(boolean useSingleBuffer) {
 
 	}
-	
+
 	/**
 	 * @return the axes object containing the canvas
 	 */
 	private SwingScilabAxes getParentAxes() {
 		return null;
 	}
-	
+
 	/**
 	 * Override set cursor in order to be able to modify the cursor
 	 * on the axes and not on the canvas itself
@@ -245,7 +275,7 @@ public class SwingScilabCanvas extends JPanel implements SimpleCanvas {
 	public void setCursor(Cursor newCursor) {
 		getParentAxes().setCursor(newCursor);
 	}
-	
+
 	/**
 	 * The canvas is not focusable, so add the listener to the parent instead
 	 * @param listener listener to add
@@ -253,7 +283,7 @@ public class SwingScilabCanvas extends JPanel implements SimpleCanvas {
 	public void addFocusListener(FocusListener listener) {
 		getParentAxes().addFocusListener(listener);
 	}
-	
+
 	/**
 	 * The canvas is not focusable, so add the listener to the parent instead
 	 * @param listener listener to add
@@ -261,7 +291,7 @@ public class SwingScilabCanvas extends JPanel implements SimpleCanvas {
 	public void removeFocusListener(FocusListener listener) {
 		getParentAxes().removeFocusListener(listener);
 	}
-	
+
 	/**
 	 * The canvas is not enabled, so add the listener to the parent instead
 	 * @param listener listener to add
@@ -269,7 +299,7 @@ public class SwingScilabCanvas extends JPanel implements SimpleCanvas {
 	public void addMouseListener(MouseListener listener) {
 		getParentAxes().addMouseListener(listener);
 	}
-	
+
 	/**
 	 * The canvas is not enabled, so add the listener to the parent instead
 	 * @param listener listener to add
@@ -277,7 +307,7 @@ public class SwingScilabCanvas extends JPanel implements SimpleCanvas {
 	public void removeMouseListener(MouseListener listener) {
 		getParentAxes().removeMouseListener(listener);
 	}
-	
+
 	/**
 	 * The canvas is not enabled, so add the listener to the parent instead
 	 * @param listener listener to add
@@ -285,7 +315,7 @@ public class SwingScilabCanvas extends JPanel implements SimpleCanvas {
 	public void addMouseMotionListener(MouseMotionListener listener) {
 		getParentAxes().addMouseMotionListener(listener);
 	}
-	
+
 	/**
 	 * The canvas is not enabled, so add the listener to the parent instead
 	 * @param listener listener to add
@@ -296,7 +326,7 @@ public class SwingScilabCanvas extends JPanel implements SimpleCanvas {
     @Override
     public void display() {
         // TODO Auto-generated method stub
-        
+
     }
     @Override
     public boolean getAutoSwapBufferMode() {
@@ -311,9 +341,9 @@ public class SwingScilabCanvas extends JPanel implements SimpleCanvas {
     @Override
     public void setAutoSwapBufferMode(boolean onOrOff) {
         // TODO Auto-generated method stub
-        
-    }
-	
-	
 
-}	
+    }
+
+
+
+}
