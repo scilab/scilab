e5ebf5aa7a938cc64b799608d539393a38951151
Sylvestre Ledru <sylvestre.ledru@scilab-enterprises.com>
2012-03-14 20:49:33 +0100

    * Bug #10769 fixed - Remove some dead code from bsplin3val.
    Change-Id: I3fda2c4a75e6891763e474ef60ca404dd95a3616

--- scilab/modules/interpolation/sci_gateway/c/sci_bsplin3val.c
+++ scilab/modules/interpolation/sci_gateway/c/sci_bsplin3val.c
@@ -2,11 +2,11 @@
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) Bruno Pincon
  * Copyright (C) DIGITEO - 2012 - Allan CORNET
- * 
+ *
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
  * you should have received as part of this distribution.  The terms
- * are also available at    
+ * are also available at
  * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
 */
 #include <string.h>
@@ -19,10 +19,10 @@
 #include "localization.h"
 /*--------------------------------------------------------------------------*/
 extern double C2F(db3val)(double *xval, double *yval, double *zval, int *idx, int *idy, int *idz,
-		   double *tx, double *ty, double *tz, int *nx, int *ny, int *nz,
-		   int *kx, int *ky, int *kz, double *bcoef, double *work);
+    double *tx, double *ty, double *tz, int *nx, int *ny, int *nz,
+    int *kx, int *ky, int *kz, double *bcoef, double *work);
 /*--------------------------------------------------------------------------*/
-int intbsplin3val(char *fname,unsigned long fname_len)
+int intbsplin3val(char *fname, unsigned long fname_len)
 {
     /*
     *   [fp] = bsplin3val(xp, yp, zp, tlcoef, der)
@@ -38,7 +38,6 @@ int intbsplin3val(char *fname,unsigned long fname_len)
     int lxyzminmax = 0, nsix = 0;
     int i = 0, mder = 0, nder = 0, lder = 0, ox = 0, oy = 0, oz = 0;
     double *fp = NULL, *xp = NULL, *yp = NULL, *zp = NULL, *der = NULL;
-    double *xyzminmax = NULL, xmin = 0, xmax = 0, ymin = 0, ymax = 0, zmin = 0, zmax = 0;
     SciIntMat Order;
     int *order = NULL;
     char **Str = NULL;;
@@ -46,16 +45,19 @@ int intbsplin3val(char *fname,unsigned long fname_len)
     CheckRhs(minrhs, maxrhs);
     CheckLhs(minlhs, maxlhs);
 
-    GetRhsVar(1, MATRIX_OF_DOUBLE_DATATYPE, &mxp, &nxp, &lxp); xp = stk(lxp);
-    GetRhsVar(2, MATRIX_OF_DOUBLE_DATATYPE, &myp, &nyp, &lyp); yp = stk(lyp);
-    GetRhsVar(3, MATRIX_OF_DOUBLE_DATATYPE, &mzp, &nzp, &lzp); zp = stk(lzp);
+    GetRhsVar(1, MATRIX_OF_DOUBLE_DATATYPE, &mxp, &nxp, &lxp);
+    xp = stk(lxp);
+    GetRhsVar(2, MATRIX_OF_DOUBLE_DATATYPE, &myp, &nyp, &lyp);
+    yp = stk(lyp);
+    GetRhsVar(3, MATRIX_OF_DOUBLE_DATATYPE, &mzp, &nzp, &lzp);
+    zp = stk(lzp);
 
     for (i = 1; i <= 3; i++)
     {
         SciErr sciErr;
         int *piAddressVar = NULL;
         sciErr = getVarAddressFromPosition(pvApiCtx, i, &piAddressVar);
-        if(sciErr.iErr)
+        if (sciErr.iErr)
         {
             printError(&sciErr, 0);
             Scierror(999, _("%s: Can not read input argument #%d.\n"), fname, i);
@@ -71,12 +73,12 @@ int intbsplin3val(char *fname,unsigned long fname_len)
 
     if ( mxp != myp  ||  nxp != nyp || mxp != mzp  ||  nxp != nzp)
     {
-        Scierror(999,_("%s: Wrong size for input arguments #%d, #%d and #%d: Same sizes expected.\n"),fname,1,2,3);
+        Scierror(999, _("%s: Wrong size for input arguments #%d, #%d and #%d: Same sizes expected.\n"), fname, 1, 2, 3);
         return 0;
     }
     np = mxp * nxp;
 
-    GetRhsVar(4, TYPED_LIST_DATATYPE,&mt, &nt, &lt);
+    GetRhsVar(4, TYPED_LIST_DATATYPE, &mt, &nt, &lt);
     GetListRhsVar(4, 1, MATRIX_OF_STRING_DATATYPE, &m1,  &n1, &Str);
 
     if ( strcmp(Str[0], "tensbs3d") != 0)
@@ -93,7 +95,7 @@ int intbsplin3val(char *fname,unsigned long fname_len)
             FREE(Str);
             Str = NULL;
         }
-        Scierror(999,_("%s: Wrong type for input argument #%d: %s tlist expected.\n"), fname, 4, "tensbs3d");
+        Scierror(999, _("%s: Wrong type for input argument #%d: %s tlist expected.\n"), fname, 4, "tensbs3d");
         return 0;
     }
     /* Free Str */
@@ -113,15 +115,7 @@ int intbsplin3val(char *fname,unsigned long fname_len)
     GetListRhsVar(4, 3, MATRIX_OF_DOUBLE_DATATYPE, &mty, &n,  &lty);
     GetListRhsVar(4, 4, MATRIX_OF_DOUBLE_DATATYPE, &mtz, &n,  &ltz);
     GetListRhsVar(4, 5, MATRIX_OF_VARIABLE_SIZE_INTEGER_DATATYPE, &m  , &n,  (int *)&Order);
-    GetListRhsVar(4, 6, MATRIX_OF_DOUBLE_DATATYPE, &nxyz,&n,  &lbcoef);
-    GetListRhsVar(4, 7, MATRIX_OF_DOUBLE_DATATYPE, &nsix,&n,  &lxyzminmax);
-    xyzminmax = stk(lxyzminmax);
-    xmin = xyzminmax[0];
-    xmax = xyzminmax[1];
-    ymin = xyzminmax[2];
-    ymax = xyzminmax[3];
-    zmin = xyzminmax[4];
-    zmax = xyzminmax[5];
+    GetListRhsVar(4, 6, MATRIX_OF_DOUBLE_DATATYPE, &nxyz, &n,  &lbcoef);
 
     GetRhsVar(5, MATRIX_OF_DOUBLE_DATATYPE, &mder, &nder, &lder);
     der = stk(lder);
@@ -130,14 +124,15 @@ int intbsplin3val(char *fname,unsigned long fname_len)
         || der[1] != floor(der[1]) || der[1] < 0.0
         || der[2] != floor(der[2]) || der[2] < 0.0 )
     {
-        Scierror(999,_("%s: Wrong values for input argument #%d.\n"), fname, 5);
+        Scierror(999, _("%s: Wrong values for input argument #%d.\n"), fname, 5);
         return 0;
     }
     ox = (int) der[0];
     oy = (int) der[1];
     oz = (int) der[2];
 
-    CreateVar(Rhs+1,MATRIX_OF_DOUBLE_DATATYPE, &mxp, &nxp, &lfp); fp = stk(lfp);
+    CreateVar(Rhs + 1, MATRIX_OF_DOUBLE_DATATYPE, &mxp, &nxp, &lfp);
+    fp = stk(lfp);
 
     order = (int *)Order.D;
     kx = order[0];
@@ -150,7 +145,7 @@ int intbsplin3val(char *fname,unsigned long fname_len)
     mwork = ky * kz + 3 * Max(kx, Max(ky, kz)) + kz;
     CreateVar(Rhs + 2, MATRIX_OF_DOUBLE_DATATYPE, &mwork, &one, &lwork);
 
-    for (i = 0;i < np;i++)
+    for (i = 0; i < np; i++)
     {
         fp[i] = C2F(db3val)(&(xp[i]), &(yp[i]), &(zp[i]), &ox, &oy, &oz,
             stk(ltx), stk(lty), stk(lty), &nx, &ny, &nz,
