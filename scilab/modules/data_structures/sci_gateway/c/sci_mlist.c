0702d214615baf5587153cdd2cb5074b4eab4b96
Allan CORNET <allan.cornet@scilab.org>
2012-05-09 13:55:40 +0200

    Bug #11076 fixed -  mlist('r'), tlist('r') returned a wrong error message.
    Change-Id: I47ce011382844f313ae2a3bce3c5e8ab45eeacf3

--- scilab/modules/data_structures/sci_gateway/c/sci_mlist.c
+++ scilab/modules/data_structures/sci_gateway/c/sci_mlist.c
@@ -12,11 +12,56 @@
 
 /*--------------------------------------------------------------------------*/
 #include "gw_data_structures1.h"
+#include "api_scilab.h"
+#include "localization.h"
+#include "Scierror.h"
+#include "BOOL.h"
 /*--------------------------------------------------------------------------*/
 extern int C2F(scilist)(); /* fortran */
 /*--------------------------------------------------------------------------*/
 int sci_scimlist(char *fname,unsigned long fname_len)
 {
+    if (Rhs >= 1)
+    {
+        #define RLIST_FIELDNAME "r" 
+        SciErr sciErr;
+        int *piAddressVarOne = NULL;
+
+
+        sciErr = getVarAddressFromPosition(pvApiCtx, 1, &piAddressVarOne);
+        if(sciErr.iErr)
+        {
+            printError(&sciErr, 0);
+            Scierror(999, _("%s: Can not read input argument #%d.\n"), fname, 1);
+            return 0;
+        }
+
+        if (isStringType(pvApiCtx, piAddressVarOne))
+        {
+            int m = 0;
+            int n = 0;
+            char **pStrs = NULL;
+            BOOL bIsRfield = FALSE;
+
+            if (getAllocatedMatrixOfString(pvApiCtx, piAddressVarOne, &m, &n, &pStrs) != 0)
+            {
+                Scierror(999, _("%s: No more memory.\n"), fname);
+                return 0;
+            }
+
+            bIsRfield = (BOOL)(strcmp(pStrs[0], RLIST_FIELDNAME) == 0);
+            freeAllocatedMatrixOfString(m, n, pStrs);
+            pStrs = NULL;
+
+            // it is not allowed to create a mlist of type 'r'
+            // 'r' type reserved to rlist
+            if (bIsRfield)
+            {
+                Scierror(999, _("%s: Can not create a mlist with input argument #%d.\n"), fname, 1);
+                return 0;
+            }
+        }
+    }
 	C2F(scilist)(fname,fname_len);
 	return 0;
 }
