30007f1dfb8701903ce4ac0b4665b75f12f41a59
Sylvestre Ledru <sylvestre.ledru@scilab-enterprises.com>
2012-05-25 10:32:44 +0200

    typo
    Change-Id: I62be6d8d4fd083e662dddef94f0fab1aef53befa

--- scilab/modules/core/includes/scilabmode.h
+++ scilab/modules/core/includes/scilabmode.h
@@ -1,11 +1,11 @@
 /*
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2007 - INRIA - Allan CORNET
- * 
+ *
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
  * you should have received as part of this distribution.  The terms
- * are also available at    
+ * are also available at
  * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
  *
  */
@@ -15,18 +15,19 @@
 #include "machine.h" /* C2F */
 #include "BOOL.h"
 
-typedef enum {
-	SCILAB_API=1, /* Scilab is launch as an API */
-	SCILAB_STD, /* The standard Scilab (gui, plot ...) */
-	SCILAB_NW, /* Scilab in command line with the plots */
-	SCILAB_NWNI /* Scilab in command line without any graphics. What not mandataroy here */
+typedef enum
+{
+    SCILAB_API = 1, /* Scilab is launched as an API */
+    SCILAB_STD, /* The standard Scilab (gui, plot ...) */
+    SCILAB_NW, /* Scilab in command line with the plots */
+    SCILAB_NWNI /* Scilab in command line without any graphics. What not mandataroy here */
 } scilabMode;
 
 /* by default mode is API */
 
 /**
- * Set Scilab execution mode. 
- * Note that this should be done at Scilab startup only. 
+ * Set Scilab execution mode.
+ * Note that this should be done at Scilab startup only.
  *
  * @param new scilab mode
  * @return TRUE is newmode is OK
@@ -55,4 +56,4 @@ int C2F(getscilabmode)(scilabMode *mode);
 
 
 #endif /* __SCILABMODE_H__ */
-/*--------------------------------------------------------------------------*/ 
+/*--------------------------------------------------------------------------*/
