3c03c21a786352b235c57a664e351f0803160009
Vincent COUVERT <vincent.couvert@scilab.org>
2012-06-01 10:35:46 +0200

    Fix get("figures_id")
    Change-Id: I1a098f80cf547e583e86037769ddf12b0b25ff55

--- scilab/modules/graphics/sci_gateway/c/sci_get.c
+++ scilab/modules/graphics/sci_gateway/c/sci_get.c
@@ -79,106 +79,106 @@ int sci_get(char *fname, unsigned long fname_len)
      * The first input argument can be an ID or a marker (in this case, get returns the value of the current object */
     switch (VarType(1))
     {
-    case 1:                    /* tclsci handle */
-        GetRhsVar(1, MATRIX_OF_DOUBLE_DATATYPE, &m1, &n1, &l1);
-        if ((int)*stk(l1) == 0) /* Root property */
-        {
-            if (Rhs == 1)
+        case 1:                    /* tclsci handle */
+            GetRhsVar(1, MATRIX_OF_DOUBLE_DATATYPE, &m1, &n1, &l1);
+            if ((int)*stk(l1) == 0) /* Root property */
             {
-                if (sciReturnHandle(getHandle(getConsoleIdentifier())) != 0)    /* Get Console handle */
+                if (Rhs == 1)
                 {
-                    /* An error has occured */
+                    if (sciReturnHandle(getHandle(getConsoleIdentifier())) != 0)    /* Get Console handle */
+                    {
+                        /* An error has occured */
+                        PutLhsVar();
+                        return 0;
+                    }
+                    LhsVar(1) = Rhs + 1;
                     PutLhsVar();
                     return 0;
                 }
-                LhsVar(1) = Rhs + 1;
-                PutLhsVar();
-                return 0;
-            }
-            CheckRhs(2, 2);
-            if (VarType(2) == sci_strings)
-            {
-                GetRhsVar(2, MATRIX_OF_STRING_DATATYPE, &m1, &n1, &stkAdr);
-
-                if (m1 * n1 != 1)
+                CheckRhs(2, 2);
+                if (VarType(2) == sci_strings)
                 {
+                    GetRhsVar(2, MATRIX_OF_STRING_DATATYPE, &m1, &n1, &stkAdr);
+
+                    if (m1 * n1 != 1)
+                    {
+                        freeArrayOfString(stkAdr, m1 * n1);
+                        Scierror(999, _("%s: Wrong type for input argument #%d: Single string expected.\n"), "get", 2);
+                        return SET_PROPERTY_ERROR;
+                    }
+
+                    status = GetScreenProperty(stkAdr[0]);
+
+                    if (status != SET_PROPERTY_SUCCEED) /* Return property */
+                    {
+                        Scierror(999, _("%s: Could not read property '%s' for root object.\n"), "get", stkAdr[0]);
+                        freeArrayOfString(stkAdr, m1 * n1);
+                        return FALSE;
+                    }
                     freeArrayOfString(stkAdr, m1 * n1);
-                    Scierror(999, _("%s: Wrong type for input argument #%d: Single string expected.\n"), "get", 2);
-                    return SET_PROPERTY_ERROR;
-                }
-
-                status = GetScreenProperty(stkAdr[0]);
-
-                if (status != SET_PROPERTY_SUCCEED) /* Return property */
-                {
-                    Scierror(999, _("%s: Could not read property '%s' for root object.\n"), "get", stkAdr[0]);
-                    freeArrayOfString(stkAdr, m1 * n1);
-                    return FALSE;
-                }
-                freeArrayOfString(stkAdr, m1 * n1);
-            }
-            else
-            {
-                Scierror(999, _("%s: Wrong type for input argument #%d: Single string expected.\n"), "get", 2);
-                return FALSE;
-            }
-            LhsVar(1) = Rhs + 1;
-            PutLhsVar();
-        }
-        else                    /* tclsci handle: should no more happen */
-        {
-            lw = 1 + Top - Rhs;
-            C2F(overload) (&lw, "get", 3);
-        }
-        return 0;
-        break;
-    case sci_handles:          /* scalar argument (hdl + string) */
-        CheckRhs(2, 2);
-        GetRhsVar(1, GRAPHICAL_HANDLE_DATATYPE, &m1, &n1, &l1);
-        if (m1 != 1 || n1 != 1)
-        {
-            lw = 1 + Top - Rhs;
-            C2F(overload) (&lw, "get", 3);
-            return 0;
-        }
-        GetRhsVar(2, STRING_DATATYPE, &numrow2, &numcol2, &l2);
-        hdl = (long)*hstk(l1);  /* on recupere le pointeur d'objet par le handle */
-        break;
-    case sci_strings:          /* string argument (string) */
-        CheckRhs(1, 1);
-        GetRhsVar(1, STRING_DATATYPE, &numrow2, &numcol2, &l2);
-        if (strcmp(cstk(l2), "default_figure") != 0 && strcmp(cstk(l2), "default_axes") != 0)
-        {
-            if (strcmp(cstk(l2), "current_figure") == 0 || strcmp(cstk(l2), "current_axes") == 0 || strcmp(cstk(l2), "current_entity") == 0
-                || strcmp(cstk(l2), "hdl") == 0)
-            {
-                hdl = 0;
-            }
-            else
-            {
-                /* Test debug F.Leray 13.04.04 */
-                if ((strcmp(cstk(l2), "children") != 0) && (strcmp(cstk(l2), "zoom_") != 0) && (strcmp(cstk(l2), "clip_box") != 0)
-                    && (strcmp(cstk(l2), "auto_") != 0))
-                {
-                    getOrCreateDefaultSubwin();
-                    hdl = getHandle(getCurrentObject());
                 }
                 else
                 {
-                    hdl = getHandle(getCurrentSubWin());    /* on recupere le pointeur d'objet par le handle */
+                    Scierror(999, _("%s: Wrong type for input argument #%d: Single string expected.\n"), "get", 2);
+                    return FALSE;
                 }
-            }                   /* DJ.A 08/01/04 */
-        }
-        else
-        {
-            hdl = 0;
-        }
-        break;
-    default:
-        lw = 1 + Top - Rhs;
-        C2F(overload) (&lw, "get", 3);
-        return 0;
-        break;
+                LhsVar(1) = Rhs + 1;
+                PutLhsVar();
+            }
+            else                    /* tclsci handle: should no more happen */
+            {
+                lw = 1 + Top - Rhs;
+                C2F(overload) (&lw, "get", 3);
+            }
+            return 0;
+            break;
+        case sci_handles:          /* scalar argument (hdl + string) */
+            CheckRhs(2, 2);
+            GetRhsVar(1, GRAPHICAL_HANDLE_DATATYPE, &m1, &n1, &l1);
+            if (m1 != 1 || n1 != 1)
+            {
+                lw = 1 + Top - Rhs;
+                C2F(overload) (&lw, "get", 3);
+                return 0;
+            }
+            GetRhsVar(2, STRING_DATATYPE, &numrow2, &numcol2, &l2);
+            hdl = (long) * hstk(l1); /* on recupere le pointeur d'objet par le handle */
+            break;
+        case sci_strings:          /* string argument (string) */
+            CheckRhs(1, 1);
+            GetRhsVar(1, STRING_DATATYPE, &numrow2, &numcol2, &l2);
+            if (strcmp(cstk(l2), "default_figure") != 0 && strcmp(cstk(l2), "default_axes") != 0)
+            {
+                if (strcmp(cstk(l2), "current_figure") == 0 || strcmp(cstk(l2), "current_axes") == 0 || strcmp(cstk(l2), "current_entity") == 0
+                        || strcmp(cstk(l2), "hdl") == 0 || strcmp(cstk(l2), "figures_id") == 0)
+                {
+                    hdl = 0;
+                }
+                else
+                {
+                    /* Test debug F.Leray 13.04.04 */
+                    if ((strcmp(cstk(l2), "children") != 0) && (strcmp(cstk(l2), "zoom_") != 0) && (strcmp(cstk(l2), "clip_box") != 0)
+                            && (strcmp(cstk(l2), "auto_") != 0))
+                    {
+                        getOrCreateDefaultSubwin();
+                        hdl = getHandle(getCurrentObject());
+                    }
+                    else
+                    {
+                        hdl = getHandle(getCurrentSubWin());    /* on recupere le pointeur d'objet par le handle */
+                    }
+                }                   /* DJ.A 08/01/04 */
+            }
+            else
+            {
+                hdl = 0;
+            }
+            break;
+        default:
+            lw = 1 + Top - Rhs;
+            C2F(overload) (&lw, "get", 3);
+            return 0;
+            break;
     }
     /* cstk(l2) est la commande, l3 l'indice sur les parametres de la commande */
     CheckLhs(0, 1);
