57d08bbf816787f250b03c03fc75c80052cea884
Calixte DENIZET <calixte.denizet@scilab-enterprises.com>
2012-07-08 17:18:03 +0200

    Desktop: slightly improve refresh in dock
    Change-Id: I725b0c2749e178db398b020cccab60284b8ad9f8

--- scilab/modules/gui/src/java/org/scilab/modules/gui/utils/WindowsConfigurationManager.java
+++ scilab/modules/gui/src/java/org/scilab/modules/gui/utils/WindowsConfigurationManager.java
@@ -36,6 +36,7 @@ import org.flexdock.docking.activation.ActiveDockableTracker;
 import org.flexdock.docking.state.LayoutNode;
 import org.flexdock.perspective.persist.xml.LayoutNodeSerializer;
 import org.flexdock.perspective.persist.xml.PersistenceConstants;
+import org.flexdock.util.SwingUtility;
 import org.scilab.modules.commons.ScilabCommons;
 import org.scilab.modules.commons.ScilabCommonsUtils;
 import org.scilab.modules.commons.ScilabConstants;
@@ -384,6 +385,7 @@ public class WindowsConfigurationManager implements XConfigurationListener {
             }
 
             if (requestFocus) {
+                SwingUtility.focus(mainTab);
                 SwingUtilities.invokeLater(new Runnable() {
                     @Override
                     public void run() {
@@ -400,15 +402,6 @@ public class WindowsConfigurationManager implements XConfigurationListener {
                                     }
                                 }
 
-                                // Be sure that te main tab or one of its subcomponent
-                                // will have the focus on start-up
-                                Component owner = null;
-                                while (owner == null && !mainTab.isAncestorOf(owner)) {
-                                    mainTab.requestFocus();
-                                    Thread.yield();
-
-                                    owner = window.getFocusOwner();
-                                }
                                 ActiveDockableTracker.requestDockableActivation(mainTab);
                                 window.toFront();
                             }
