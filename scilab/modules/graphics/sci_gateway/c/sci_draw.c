4b0db8c933d4e057aadf25bc6bcf3011548f5052
Bruno JOFRET <bruno.jofret@scilab-enterprises.com>
2012-07-18 14:33:23 +0200

    draw function is now obsolete.
    Change-Id: Ib03ef8d235045701c7d02bdf1756473585ef2eed

--- scilab/modules/graphics/sci_gateway/c/sci_draw.c
+++ scilab/modules/graphics/sci_gateway/c/sci_draw.c
@@ -17,58 +17,25 @@
 /*------------------------------------------------------------------------*/
 #include "stack-c.h"
 #include "gw_graphics.h"
-#include "HandleManagement.h"
+#include "warningmode.h"
+#include "sciprint.h"
 #include "localization.h"
-#include "Scierror.h"
-#include "MALLOC.h"
 /*--------------------------------------------------------------------------*/
+//
+// FIXME: Remove GW after Scilab 5.4.0
+//
 int sci_draw( char * fname, unsigned long fname_len )
 {
 
   CheckRhs(0,1) ;
   CheckLhs(0,1) ;
 
-#if 0
-  if (Rhs == 0)
+  if (getWarningMode())
   {
-    sciDrawSingleObj(sciGetCurrentObj()) ;
+      sciprint(_("WARNING: %s\n"), _("draw function is obsolete."));
+      sciprint(_("WARNING: %s\n"), _("It will be removed after Scilab 5.4.0."));
+      sciprint(_("WARNING: %s\n"), _("Please see documentation for more details."));
   }
-  else
-  {
-    // Rhs = 1
-    sciPointObj ** drawnObjects = NULL;
-    int nbObjects = 0;
-    int nbRow;
-    int nbCol;
-    size_t stackPointer = 0;
-    int i;
-
-    GetRhsVar( 1, GRAPHICAL_HANDLE_DATATYPE, &nbRow, &nbCol, &stackPointer );
-
-    nbObjects = nbRow * nbCol;
-
-    /* allocate array of objects */
-    drawnObjects = MALLOC(nbObjects * sizeof(sciPointObj *));
-
-    /* fill it */
-    for (i = 0; i < nbObjects; i++)
-    {
-      /* Convert handle to sciPointObj */
-      unsigned long hdl = (unsigned long) hstk(stackPointer)[i];
-      drawnObjects[i] = sciGetPointerFromHandle(hdl);
-
-      if (drawnObjects[i] == NULL) {
-        FREE(drawnObjects);
-        Scierror(999,_("%s: The handle is not or no more valid.\n"),fname);
-        return 0;
-      }
-    }
-
-    sciDrawSetOfObj(drawnObjects, nbObjects);
-
-    FREE(drawnObjects);
-  }
-#endif
 
   LhsVar(1) = 0;
   PutLhsVar();
