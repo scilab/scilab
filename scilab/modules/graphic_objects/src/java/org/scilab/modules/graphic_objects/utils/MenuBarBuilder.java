6f13a84b575fe20a4e9664fa9ba2745eba2a681f
Sylvestre Ledru <sylvestre.ledru@scilab-enterprises.com>
2012-08-02 16:29:29 +0200

    Revert "Improve export of code in colormap.xml" Cyclic dependency
    This reverts commit 3999618a766707dabee77b4082ef38a95bbb080b.
    
    Change-Id: I4a6afafeac77eefc4c34d3854c300952be4bf914

--- scilab/modules/graphic_objects/src/java/org/scilab/modules/graphic_objects/utils/MenuBarBuilder.java
+++ scilab/modules/graphic_objects/src/java/org/scilab/modules/graphic_objects/utils/MenuBarBuilder.java
@@ -42,7 +42,6 @@ import org.scilab.modules.graphic_objects.graphicController.GraphicController;
 import org.scilab.modules.graphic_objects.graphicObject.CallBack;
 import org.scilab.modules.graphic_objects.graphicObject.GraphicObject.Type;
 import org.scilab.modules.graphic_objects.graphicObject.GraphicObjectProperties;
-import org.scilab.modules.gui.SwingView;
 import org.scilab.modules.localization.Messages;
 import org.w3c.dom.Document;
 import org.w3c.dom.NamedNodeMap;
@@ -63,7 +62,7 @@ public final class MenuBarBuilder {
     private static final String FILE_NOT_FOUND = "Could not find file: ";
 
     private static final String CANNOT_CREATE_MENUBAR = "Cannot create MenuBar.\n"
-        + "Check if file *_menubar.xml is available and valid.";
+            + "Check if file *_menubar.xml is available and valid.";
 
     private static boolean isParentValid = true;;
 
@@ -103,10 +102,8 @@ public final class MenuBarBuilder {
      * @param figureId the figure
      */
     public static void buildFigureMenuBar(String figureId) {
-        if (!SwingView.isHeadless()) {
-            MenuBarBuilder.isParentValid = false;
-            buildMenuBar(GRAPHICSMENUBARXMLFILE, figureId);
-        }
+        MenuBarBuilder.isParentValid = false;
+        buildMenuBar(GRAPHICSMENUBARXMLFILE, figureId);
     }
 
     /**
@@ -193,7 +190,7 @@ public final class MenuBarBuilder {
          * @see java.lang.reflect.InvocationHandler#invoke(java.lang.Object, java.lang.reflect.Method, java.lang.Object[])
          */
         public Object invoke(Object proxy, Method method, Object[] args)
-            throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
             if (internalMethodNames.contains(method.getName())) {
                 return getClass().getMethod(method.getName(), method.getParameterTypes()).invoke(this, args);
             } else {
@@ -207,6 +204,7 @@ public final class MenuBarBuilder {
          * @see org.scilab.modules.MenuBarConfiguration.utils.MenuBarConfiguration#addMenus(org.scilab.modules.gui.menubar.MenuBar)
          */
         public void addMenus(String parentId) {
+
             // delete old menus
             for (String childId : (String []) GraphicController.getController().getProperty(parentId, GraphicObjectProperties.__GO_CHILDREN__)) {
                 if (GraphicController.getController().getProperty(childId, GraphicObjectProperties.__GO_TYPE__).equals(__GO_UIMENU__))
