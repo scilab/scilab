f37d50a80026eeb7411b765404c18bd4d933dbd1
Cedric Delamarre <cedric.delamarre@scilab-enterprises.com>
2012-08-30 12:42:57 +0200

    management of other types in full function.
    Change-Id: I1523e1456f01629960cbbb3b6aeab9e770c99218

--- scilab/modules/types/sci_gateway/cpp/sci_full.cpp
+++ scilab/modules/types/sci_gateway/cpp/sci_full.cpp
@@ -1,6 +1,7 @@
 /*
  *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  *  Copyright (C) 2010-2010 - DIGITEO - Bernard HUGUENEY
+ *  Copyright (C) 2012 - Scilab Enterprises - Cedric Delamarre
  *
  *  This file must be used under the terms of the CeCILL.
  *  This source file is licensed as described in the file COPYING, which
@@ -22,57 +23,39 @@ extern "C"
 #include "localization.h"
 }
 
-using namespace types;
-
-Function::ReturnValue sci_full(typed_list &in, int nbRes, typed_list &out)
+types::Function::ReturnValue sci_full(types::typed_list &in, int _iRetCount, types::typed_list &out)
 {
-    types::Sparse *pRetVal = NULL;
-
-    if(in.size() != 1)
+    if (in.size() != 1)
     {
         ScierrorW(999, _W("%ls: Wrong number of input argument(s): %d expected.\n"), L"full", 1);
-        return Function::Error;
+        return types::Function::Error;
     }
 
-    if(in[0]->isSparse() == false && in[0]->isSparseBool() == false)
-    {
-        ScierrorW(999, _W("%ls: Wrong type for input argument #%d: sparse matrix expected.\n"), L"full", 1);
-        return Function::Error;
-    }
-
-    if(nbRes > 1)
+    if (_iRetCount > 1)
     {
         ScierrorW(999, _W("%ls: Wrong number of output arguments: %d expected.\n"), L"full", 1);
     }
 
-    if(in[0]->isSparse())
+    if (in[0]->isSparse())
     {
-        Sparse* pSp = in[0]->getAs<Sparse>();
-        Double* pOut = NULL;
+        types::Sparse* pSp = in[0]->getAs<types::Sparse>();
+        types::Double* pOut = NULL;
 
-        pOut = new Double(pSp->getRows(), pSp->getCols(), pSp->isComplex());
+        pOut = new types::Double(pSp->getRows(), pSp->getCols(), pSp->isComplex());
         pSp->fill(*pOut);
+        out.push_back(pOut);
+    }
+    else if (in[0]->isSparseBool())
+    {
+        types::SparseBool* pSb = in[0]->getAs<types::SparseBool>();
+        types::Bool* pOut = new types::Bool(pSb->getRows(), pSb->getCols());
+        pSb->fill(*pOut);
         out.push_back(pOut);
     }
     else
     {
-        SparseBool* pSb = in[0]->getAs<SparseBool>();
-        Bool* pOut = new Bool(pSb->getRows(), pSb->getCols());
-        pSb->fill(*pOut);
-        out.push_back(pOut);
+        out.push_back(in[0]->clone());
     }
 
-    //types::Sparse CONST& sp(*(in[0]->getAsSparse()));
-    //double* dataReal;
-    //double *dataImag;
-    //using types::Double;
-    //Double* res (sp.isComplex()
-    //    ? new Double(sp.rows_get(), sp.cols_get(), &dataReal, &dataImag)
-    //    : new Double(sp.rows_get(), sp.cols_get(), &dataReal));
-    //if(res)
-    //{
-    //    sp.fill(*res);
-    //    out.push_back(res);
-    //}
-    return Function::OK;
+    return types::Function::OK;
 }
