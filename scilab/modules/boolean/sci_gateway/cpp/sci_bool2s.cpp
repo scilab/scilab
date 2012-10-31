68fa184cbf13d40c74da51548f7aba7e0084cbae
Cedric Delamarre <cedric.delamarre@scilab-enterprises.com>
2012-10-31 12:22:10 +0100

    management of sparse and boolean sparse in bool2s function.
    	bool2s(sparse([1 2 5 0 3 0 6 9]))
    	bool2s(sparse([%t %f %t %t %f %t]))
    
    Change-Id: Idf5c3616541106f81d5b7a0caf03473db20e4dd3

--- scilab/modules/boolean/sci_gateway/cpp/sci_bool2s.cpp
+++ scilab/modules/boolean/sci_gateway/cpp/sci_bool2s.cpp
@@ -14,7 +14,10 @@
 #include "boolean_gw.hxx"
 #include "function.hxx"
 #include "double.hxx"
+#include "sparse.hxx"
 #include "bool.hxx"
+#include "overload.hxx"
+#include "execvisitor.hxx"
 
 extern "C"
 {
@@ -26,44 +29,101 @@ extern "C"
 types::Function::ReturnValue sci_bool2s(types::typed_list &in, int _iRetCount, types::typed_list &out)
 {
     types::InternalType* pOut = NULL;
-    if(in.size() != 1)
+    if (in.size() != 1)
     {
         ScierrorW(77, _W("%ls: Wrong number of input argument(s): %d expected.\n"), L"bool2s", 1);
         return types::Function::Error;
     }
 
-    if(in[0]->isDouble())
+    if (in[0]->isDouble())
     {
         types::Double* pIn = in[0]->getAs<types::Double>();
 
-        types::Double* pD = new types::Double(pIn->getDims(), pIn->getDimsArray());
-        for(int i = 0 ; i < pIn->getSize() ; i++)
+        if (pIn->isComplex())
         {
-            pD->set(i, pIn->get(i) != 0);
+            std::wstring wstFuncName = L"%"  + in[0]->getShortTypeStr() + L"_bool2s";
+            return Overload::call(wstFuncName, in, _iRetCount, out, new ExecVisitor());
         }
-        pOut = pD;
-    }
-    else if(in[0]->isBool())
-    {
-        types::Bool* pIn = in[0]->getAs<types::Bool>();
 
         types::Double* pD = new types::Double(pIn->getDims(), pIn->getDimsArray());
-        for(int i = 0 ; i < pIn->getSize() ; i++)
+
+        double* pdblIn  = pIn->get();
+        double* pdblOut = pD->get();
+
+        for (int i = 0 ; i < pIn->getSize() ; i++)
         {
-            pD->set(i, pIn->get(i));
+            pdblOut[i] = static_cast<double>(pdblIn[i] != 0);
         }
         pOut = pD;
     }
-    //else if(in[0]->isSparse())
-    //{
-    //}
-    //else if(in[0]->isBooleanSparse())
-    //{
-    //}
+    else if (in[0]->isBool())
+    {
+        types::Bool* pIn = in[0]->getAs<types::Bool>();
+        types::Double* pD = new types::Double(pIn->getDims(), pIn->getDimsArray());
+
+        int* piIn = pIn->get();
+        double* pdblOut = pD->get();
+
+        for (int i = 0 ; i < pIn->getSize() ; i++)
+        {
+            pdblOut[i] = static_cast<double>(piIn[i]);
+        }
+        pOut = pD;
+    }
+    else if (in[0]->isSparse())
+    {
+        types::Sparse* pSpIn = in[0]->getAs<types::Sparse>();
+
+        if (pSpIn->isComplex())
+        {
+            std::wstring wstFuncName = L"%"  + in[0]->getShortTypeStr() + L"_bool2s";
+            return Overload::call(wstFuncName, in, _iRetCount, out, new ExecVisitor());
+        }
+
+        types::Sparse* pSpOut = new types::Sparse(pSpIn->getRows(), pSpIn->getCols());
+
+        size_t iNonZeros = pSpIn->nonZeros();
+
+        //coords
+        double* pRows = new double[iNonZeros * 2];
+        pSpIn->outputRowCol(pRows);
+        double* pCols = pRows + iNonZeros;
+
+        //values
+        double* pNonZeroR = new double[iNonZeros];
+        double* pNonZeroI = new double[iNonZeros];
+        pSpIn->outputValues(pNonZeroR, pNonZeroI);
+
+        for (int i = 0; i < iNonZeros; i++)
+        {
+            pSpOut->set((int)pRows[i] - 1, (int)pCols[i] - 1, static_cast<double>(pNonZeroR[i] != 0));
+        }
+
+        pOut = pSpOut;
+    }
+    else if (in[0]->isSparseBool())
+    {
+        types::SparseBool* pSpbIn = in[0]->getAs<types::SparseBool>();
+        types::Sparse* pSpOut = new types::Sparse(pSpbIn ->getRows(), pSpbIn ->getCols());
+
+        size_t iNonZeros = pSpbIn ->nbTrue();
+
+        //coords
+        double* pRows = new double[iNonZeros * 2];
+        pSpbIn ->outputRowCol(pRows);
+        double* pCols = pRows + iNonZeros;
+
+        for (int i = 0; i < iNonZeros; i++)
+        {
+            pSpOut->set((int)pRows[i] - 1, (int)pCols[i] - 1, 1);
+        }
+
+        pOut = pSpOut;
+    }
     else
     {
-        ScierrorW(999, _W("%ls: Wrong type for input argument #%d: %s or %s expected.\n"), L"bool2s", 1, L"bool", L"double");
-        return types::Function::Error;
+        std::wstring wstFuncName = L"%"  + in[0]->getShortTypeStr() + L"_bool2s";
+        return Overload::call(wstFuncName, in, _iRetCount, out, new ExecVisitor());
     }
 
     out.push_back(pOut);
