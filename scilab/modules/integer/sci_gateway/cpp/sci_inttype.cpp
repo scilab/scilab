a11a7eb0705abd31f8430f90b8648594cab1a54b
Cedric Delamarre <cedric.delamarre@scilab-enterprises.com>
2012-11-09 17:39:26 +0100

    inttype corrected.
    Change-Id: I15557ec82c56d7b04ec036a8f01c30b1ae41169a

--- scilab/modules/integer/sci_gateway/cpp/sci_inttype.cpp
+++ scilab/modules/integer/sci_gateway/cpp/sci_inttype.cpp
@@ -13,7 +13,13 @@
 #include "integer_gw.hxx"
 #include "function.hxx"
 #include "double.hxx"
+
+extern "C"
+{
 #include "api_scilab.h"
+#include "Scierror.h"
+#include "localization.h"
+}
 
 using namespace types;
 
@@ -22,43 +28,48 @@ Function::ReturnValue sci_inttype(typed_list &in, int _piRetCount, typed_list &o
     /* First check if there is only 1 input of type int */
     if (in.size() != 1)
     {
+        Scierror(77, _("%s: Wrong number of input argument(s): %d expected.\n"), "inttype", 1);
         return Function::Error;
     }
 
     InternalType *pitValue = in[0];
 
-    if (!pitValue->isInt())
+    if (pitValue->isInt() == false && pitValue->isDouble() == false)
     {
+        Scierror(999, _("%s: Wrong type for input argument #%d : int or double expected.\n"), "inttype", 1);
         return Function::Error;
     }
 
     Double *pRetVal = NULL;
-    switch(pitValue->getType())
+    switch (pitValue->getType())
     {
-    case InternalType::RealInt8 :
-        pRetVal = new Double(SCI_INT8);
-        break;
-    case InternalType::RealUInt8 :
-        pRetVal = new Double(SCI_UINT8);
-        break;
-    case InternalType::RealInt16 :
-        pRetVal = new Double(SCI_INT16);
-        break;
-    case InternalType::RealUInt16 :
-        pRetVal = new Double(SCI_UINT16);
-        break;
-    case InternalType::RealInt32 :
-        pRetVal = new Double(SCI_INT32);
-        break;
-    case InternalType::RealUInt32 :
-        pRetVal = new Double(SCI_UINT32);
-        break;
-    case InternalType::RealInt64 :
-        pRetVal = new Double(SCI_INT64);
-        break;
-    case InternalType::RealUInt64 :
-        pRetVal = new Double(SCI_UINT64);
-        break;
+        case InternalType::RealDouble :
+            pRetVal = new Double(0);
+            break;
+        case InternalType::RealInt8 :
+            pRetVal = new Double(SCI_INT8);
+            break;
+        case InternalType::RealUInt8 :
+            pRetVal = new Double(SCI_UINT8);
+            break;
+        case InternalType::RealInt16 :
+            pRetVal = new Double(SCI_INT16);
+            break;
+        case InternalType::RealUInt16 :
+            pRetVal = new Double(SCI_UINT16);
+            break;
+        case InternalType::RealInt32 :
+            pRetVal = new Double(SCI_INT32);
+            break;
+        case InternalType::RealUInt32 :
+            pRetVal = new Double(SCI_UINT32);
+            break;
+        case InternalType::RealInt64 :
+            pRetVal = new Double(SCI_INT64);
+            break;
+        case InternalType::RealUInt64 :
+            pRetVal = new Double(SCI_UINT64);
+            break;
     }
 
     out.push_back(pRetVal);
