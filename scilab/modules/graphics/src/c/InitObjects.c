81dad6066de9c92a24ca0197164f344c2a98a425
Vincent COUVERT <vincent.couvert@scilab-enterprises.com>
2012-11-23 15:31:58 +0100

    Revert "Graphics: avoid throw/catch NPE at scilab startup"
    Make graphic_object compilation tests fail.
    
    This reverts commit d9a1b532548631ed13960cbd64ade2c24ceb45c0

--- scilab/modules/graphics/src/c/InitObjects.c
+++ scilab/modules/graphics/src/c/InitObjects.c
@@ -208,10 +208,6 @@ int C2F(graphicsmodels) (void)
     // Create default Axes by Asking MVC a new one.
     paxesmdlUID = createGraphicObject(__GO_AXESMODEL__);
     setAxesModel(paxesmdlUID);
-
-    /* Sets the parent-child relationship between the default Figure and Axes */
-    setGraphicObjectRelationship(pfiguremdlUID, paxesmdlUID);
-
     /* Axes Model properties */
 
     result = InitAxesModel();
@@ -250,6 +246,9 @@ int C2F(graphicsmodels) (void)
 #if 0
     ppaxesmdl->FirstPlot = TRUE;
 #endif
+
+    /* Sets the parent-child relationship between the default Figure and Axes */
+    setGraphicObjectRelationship(pfiguremdlUID, paxesmdlUID);
 
     return 1;
 }
