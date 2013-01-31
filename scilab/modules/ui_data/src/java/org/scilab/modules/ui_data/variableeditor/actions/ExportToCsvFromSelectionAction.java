04f2632ed35eafd0d6cbb4d0ed513e87edfcd738
Sylvestre Ledru <sylvestre.ledru@scilab-enterprises.com>
2013-02-01 00:12:34 +0100

    editvar propose direct save to the CSV format of the whole variable or the selection.
    Change-Id: I2a903fe864d188099d2d0ea2d79f8ed094a6e37e

---
+++ scilab/modules/ui_data/src/java/org/scilab/modules/ui_data/variableeditor/actions/ExportToCsvFromSelectionAction.java
@@ -0,0 +1,101 @@
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2013 - S/E - Sylvestre Ledru
+ *
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
+package org.scilab.modules.ui_data.variableeditor.actions;
+
+import javax.swing.JTable;
+
+import org.scilab.modules.commons.gui.ScilabKeyStroke;
+import org.scilab.modules.gui.bridge.menuitem.SwingScilabMenuItem;
+import org.scilab.modules.gui.events.callback.CommonCallBack;
+import org.scilab.modules.gui.menuitem.MenuItem;
+import org.scilab.modules.gui.menuitem.ScilabMenuItem;
+import org.scilab.modules.gui.utils.ScilabSwingUtilities;
+import org.scilab.modules.ui_data.UI_data;
+import org.scilab.modules.ui_data.datatable.SwingEditvarTableModel;
+import org.scilab.modules.ui_data.utils.UiDataMessages;
+import org.scilab.modules.ui_data.variableeditor.SwingScilabVariableEditor;
+
+/**
+ * Export to Csv class
+ * @author Sylvestre Ledru
+ */
+@SuppressWarnings(value = { "serial" })
+public final class ExportToCsvFromSelectionAction extends CommonCallBack {
+
+    private static final String KEY = "OSSCKEY S";
+    private static final String CREATE = "Export to CSV";
+    private static final int GAP = 5;
+
+    private final SwingScilabVariableEditor editor;
+
+    /**
+     * Constructor
+     * @param editor the editor
+     * @param name the name of the action
+     */
+    private ExportToCsvFromSelectionAction(SwingScilabVariableEditor editor, String name) {
+        super(name);
+        this.editor = editor;
+    }
+
+    /**
+     * @param editor the editor
+     * @param table where to put the action
+     */
+    public static void registerAction(SwingScilabVariableEditor editor, JTable table) {
+        table.getActionMap().put(CREATE, new ExportToCsvFromSelectionAction(editor, CREATE));
+        table.getInputMap().put(ScilabKeyStroke.getKeyStroke(KEY), CREATE);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void callBack() {
+        JTable table = editor.getCurrentTable();
+        int[] cols = table.getSelectedColumns();
+        int[] rows = table.getSelectedRows();
+        if (cols.length > 0 && rows.length > 0) {
+            int[] rowSize = new int[1];
+            int[] colSize = new int[1];
+            SwingEditvarTableModel model = (SwingEditvarTableModel) table.getModel();
+            String datas = model.getScilabSubMatrix(rows, cols, rowSize, colSize);
+            model.execCommand("__export__csv__=uiputfile();__var_to_export__=" + datas + "; if __export__csv__<>'' then csvWrite(__var_to_export__ , __export__csv__);clear __export__csv__ __var_to_export__; end;");
+        }
+    }
+
+    /**
+     * Create a menu item
+     * @param editor the associated editor
+     * @param title the menu title
+     * @return the menu item
+     */
+    public static MenuItem createMenuItem(SwingScilabVariableEditor editor, String title) {
+        MenuItem menu = ScilabMenuItem.createMenuItem();
+        menu.setCallback(new ExportToCsvFromSelectionAction(editor, title));
+        menu.setText(title);
+        ((SwingScilabMenuItem) menu.getAsSimpleMenuItem()).setAccelerator(ScilabKeyStroke.getKeyStroke(KEY));
+
+        return menu;
+    }
+
+    /**
+     * Create a menu item as a SwingScilabMenuItem
+     * @param editor the associated editor
+     * @param title the menu title
+     * @return the menu item
+     */
+    public static SwingScilabMenuItem createJMenuItem(SwingScilabVariableEditor editor, String title) {
+        return (SwingScilabMenuItem) createMenuItem(editor, title).getAsSimpleMenuItem();
+    }
+}
