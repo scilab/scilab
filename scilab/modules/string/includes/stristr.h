7a9fbd60fddbbc7f88b45c8c64d9bf539118a994
Simon Marchetto <simon.marchetto@scilab-enterprises.com>
2013-02-25 17:44:02 +0100

    * Bug #6320 fixed - Completion in Scilab is case sensitive
    Completion in Scilab is now case insensitive.
    -it applies on all syntax elements of Scilab, and file/directory paths.
    -if one hypothesis matches only, then the console applies it automatically.
    
    New unit tests for case insensitive completion.
    Used stristr function moved from scilab_windows to string module.
    Used convstr exported from string module.
    
    Change-Id: I6216a3aacaee11c976f080947d1e7a2ad867f573

--- scilab/modules/string/includes/stristr.h
+++ scilab/modules/string/includes/stristr.h
@@ -1,11 +1,11 @@
 /*
 * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
 * Copyright (C) INRIA - Allan CORNET
-* 
+*
 * This file must be used under the terms of the CeCILL.
 * This source file is licensed as described in the file COPYING, which
 * you should have received as part of this distribution.  The terms
-* are also available at    
+* are also available at
 * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
 *
 */
@@ -14,15 +14,15 @@
 #ifndef __STRISTR_H__
 #define __STRISTR_H__
 
-#include "dynlib_scilab_windows.h"
+#include "dynlib_string.h"
 
 /**
 * strstr case insensitive
 * @param[in] string where we search
 * @param[in] string to search
-* @return result 
+* @return result
 */
-SCILAB_WINDOWS_IMPEXP char *stristr(const char *psz,const char *tofind);
+STRING_IMPEXP char *stristr(const char *psz,const char *tofind);
 
 #endif /* __STRISTR_H__ */
 /*--------------------------------------------------------------------------*/
