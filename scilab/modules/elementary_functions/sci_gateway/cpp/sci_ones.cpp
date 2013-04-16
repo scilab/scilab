be7ae928764c1c5e9bdc39dedefea2948c8726cf
Cedric Delamarre <cedric.delamarre@scilab-enterprises.com>
2013-04-16 15:08:26 +0200

    management of type int in "ones" function.
    ones(int8(2), uint64(4), 3)
    test_run("string", "blanks")
    
    Change-Id: I294d45140f6289adb32d8f138e05e5ebec28bad7

--- scilab/modules/elementary_functions/sci_gateway/cpp/sci_ones.cpp
+++ scilab/modules/elementary_functions/sci_gateway/cpp/sci_ones.cpp
@@ -2,6 +2,7 @@
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2006 - INRIA - Allan CORNET
  * Copyright (C) 2011 - DIGITEO - Antoine ELIAS
+ * Copyright (C) 2013 - Scilab Enterprises - Cedric Delamarre
  *
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
@@ -14,6 +15,7 @@
 #include "elem_func_gw.hxx"
 #include "function.hxx"
 #include "double.hxx"
+#include "int.hxx"
 
 extern "C"
 {
@@ -28,12 +30,12 @@ using namespace types;
 Function::ReturnValue sci_ones(types::typed_list &in, int _iRetCount, types::typed_list &out)
 {
     Double* pOut = NULL;
-    if(in.size() == 0)
+    if (in.size() == 0)
     {
         out.push_back(new Double(1));
         return Function::OK;
     }
-    else if(in.size() == 1)
+    else if (in.size() == 1)
     {
         types::GenericType* pIn = in[0]->getAs<types::GenericType>();
         int iDims = pIn->getDims();
@@ -45,15 +47,54 @@ Function::ReturnValue sci_ones(types::typed_list &in, int _iRetCount, types::typ
     {
         int iDims = static_cast<int>(in.size());
         int* piDims = new int[iDims];
-        for(int i = 0 ; i < iDims ; i++)
+        for (int i = 0 ; i < iDims ; i++)
         {
-            Double* pIn = in[i]->getAs<Double>();
-            if(pIn->isDouble() == false || pIn->isScalar() == false || pIn->isComplex())
+            if (in[i]->isGenericType() == false)
             {
                 Scierror(999, _("%s: Wrong type for input argument #%d: Real scalar expected.\n"), "ones", i + 1);
                 return Function::Error;
             }
-            piDims[i] = static_cast<int>(pIn->getReal()[0]);
+
+            types::GenericType* pGTIn  = in[i]->getAs<types::GenericType>();
+            if (pGTIn->isScalar() == false || pGTIn->isComplex())
+            {
+                Scierror(999, _("%s: Wrong type for input argument #%d: Real scalar expected.\n"), "ones", i + 1);
+                return Function::Error;
+            }
+
+            switch (in[i]->getType())
+            {
+                case types::InternalType::RealDouble :
+                    piDims[i] = static_cast<int>(in[i]->getAs<types::Double>()->get()[0]);
+                    break;
+                case types::InternalType::RealInt8 :
+                    piDims[i] = static_cast<int>(in[i]->getAs<types::Int8>()->get()[0]);
+                    break;
+                case types::InternalType::RealUInt8 :
+                    piDims[i] = static_cast<int>(in[i]->getAs<types::UInt8>()->get()[0]);
+                    break;
+                case types::InternalType::RealInt16 :
+                    piDims[i] = static_cast<int>(in[i]->getAs<types::Int16>()->get()[0]);
+                    break;
+                case types::InternalType::RealUInt16 :
+                    piDims[i] = static_cast<int>(in[i]->getAs<types::UInt16>()->get()[0]);
+                    break;
+                case types::InternalType::RealInt32 :
+                    piDims[i] = in[i]->getAs<types::Int32>()->get()[0];
+                    break;
+                case types::InternalType::RealUInt32 :
+                    piDims[i] = static_cast<int>(in[i]->getAs<types::UInt32>()->get()[0]);
+                    break;
+                case types::InternalType::RealInt64 :
+                    piDims[i] = static_cast<int>(in[i]->getAs<types::Int64>()->get()[0]);
+                    break;
+                case types::InternalType::RealUInt64 :
+                    piDims[i] = static_cast<int>(in[i]->getAs<types::UInt64>()->get()[0]);
+                    break;
+                default :
+                    Scierror(999, _("%s: Wrong type for input argument #%d: Real scalar expected.\n"), "ones", i + 1);
+                    return Function::Error;
+            }
         }
         pOut = new Double(iDims, piDims);
         delete[] piDims;
