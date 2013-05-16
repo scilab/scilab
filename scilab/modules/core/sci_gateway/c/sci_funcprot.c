79a9585122a25b58f57190db753af2307ba725ca
Cedric Delamarre <cedric.delamarre@scilab-enterprises.com>
2013-05-16 11:53:04 +0200

    error message in funcprot corrected.
    Change-Id: I0d8abcc52cdd0580ecc81688732d18161084d90b

--- scilab/modules/core/sci_gateway/c/sci_funcprot.c
+++ scilab/modules/core/sci_gateway/c/sci_funcprot.c
@@ -2,11 +2,11 @@
  * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
  * Copyright (C) 2006 - INRIA - Allan CORNET
  * Copyright (C) 2009-2011 - DIGITEO - Allan CORNET
- * 
+ *
  * This file must be used under the terms of the CeCILL.
  * This source file is licensed as described in the file COPYING, which
  * you should have received as part of this distribution.  The terms
- * are also available at    
+ * are also available at
  * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
  *
  */
@@ -18,7 +18,7 @@
 #include "localization.h"
 #include "MALLOC.h"
 /*--------------------------------------------------------------------------*/
-int C2F(sci_funcprot)(char *fname,unsigned long fname_len)
+int C2F(sci_funcprot)(char *fname, unsigned long fname_len)
 {
     SciErr sciErr;
     CheckLhs(1, 1);
@@ -30,11 +30,11 @@ int C2F(sci_funcprot)(char *fname,unsigned long fname_len)
 
         if (createScalarDouble(pvApiCtx, Rhs + 1, dOut) != 0)
         {
-            Scierror(999,_("%s: Memory allocation error.\n"), fname);
+            Scierror(999, _("%s: Memory allocation error.\n"), fname);
             return 0;
         }
 
-        LhsVar(1) = Rhs + 1; 
+        LhsVar(1) = Rhs + 1;
         PutLhsVar();
     }
     else if (Rhs == 1)
@@ -46,7 +46,7 @@ int C2F(sci_funcprot)(char *fname,unsigned long fname_len)
 
         /* get Address of inputs */
         sciErr = getVarAddressFromPosition(pvApiCtx, 1, &piAddressVarOne);
-        if(sciErr.iErr)
+        if (sciErr.iErr)
         {
             printError(&sciErr, 0);
             Scierror(999, _("%s: Can not read input argument #%d.\n"), fname, 1);
@@ -56,13 +56,13 @@ int C2F(sci_funcprot)(char *fname,unsigned long fname_len)
         /* check input type */
         if (isDoubleType(pvApiCtx, piAddressVarOne) != 1)
         {
-            Scierror(999,_("%s: Wrong type for input argument #%d: A scalar expected.\n"), fname, 1);
+            Scierror(999, _("%s: Wrong type for input argument #%d: A scalar expected.\n"), fname, 1);
             return 0;
         }
 
-        if(isScalar(pvApiCtx, piAddressVarOne) != 1)
+        if (isScalar(pvApiCtx, piAddressVarOne) != 1)
         {
-            Scierror(999,_("%s: Wrong size for input argument #%d: A scalar expected.\n"), fname, 1);
+            Scierror(999, _("%s: Wrong size for input argument #%d: A scalar expected.\n"), fname, 1);
             return 0;
         }
 
@@ -76,24 +76,24 @@ int C2F(sci_funcprot)(char *fname,unsigned long fname_len)
 
         if (dVarOne != (double)ilevel)
         {
-            Scierror(999,_("%s: Wrong type for input argument #%d: An integer value expected.\n"), fname, 1);
+            Scierror(999, _("%s: Wrong type for input argument #%d: An integer value expected.\n"), fname, 1);
             return 0;
         }
 
         if (!setfuncprot(ilevel))
         {
-            Scierror(999,_("%s: Wrong value for input argument #%d: 1,2 or 3 expected.\n"), fname, 1);
+            Scierror(999, _("%s: Wrong value for input argument #%d: 0,1 or 2 expected.\n"), fname, 1);
             return 0;
         }
         else
         {
             if (createScalarDouble(pvApiCtx, Rhs + 1, dPreviousValue) != 0)
             {
-                Scierror(999,_("%s: Memory allocation error.\n"), fname);
+                Scierror(999, _("%s: Memory allocation error.\n"), fname);
                 return 0;
             }
 
-            LhsVar(1) = Rhs + 1; 
+            LhsVar(1) = Rhs + 1;
             PutLhsVar();
         }
     }
