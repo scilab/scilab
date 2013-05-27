8370d8d3f5f4d46c7b77e6903fc95e838cab908e
Cedric Delamarre <cedric.delamarre@scilab-enterprises.com>
2013-05-27 16:56:00 +0200

    mode function corrected and moved in module core.
    test_run("core","mode",["no_check_error_output" ]);
    
    Change-Id: I7e756bcb1bd8402b45754af85524aaf38b1512c0

--- scilab/modules/core/sci_gateway/cpp/sci_mode.cpp
+++ scilab/modules/core/sci_gateway/cpp/sci_mode.cpp
@@ -11,7 +11,7 @@
 */
 /*--------------------------------------------------------------------------*/
 
-#include "functions_gw.hxx"
+#include "core_gw.hxx"
 #include "configvariable.hxx"
 #include "double.hxx"
 #include "function.hxx"
@@ -28,39 +28,52 @@ using namespace types;
 
 Function::ReturnValue sci_mode(types::typed_list &in, int _iRetCount, types::typed_list &out)
 {
-	InternalType *pIT = NULL;
-    std::wstring stModule;
+    InternalType *pIT = NULL;
 
-	/* Check the number of input argument */
-	if(in.size() > 1)
-	{
+    /* Check the number of input argument */
+    if (in.size() > 1)
+    {
         Scierror(999, _("%s: Wrong number of input arguments: %d or %d expected.\n"), "mode" , 0, 1);
-		return Function::Error;
-	}
-
-	/* Check the number of output argument */
-	if(_iRetCount != 1)
-	{
-		Scierror(78, _("%s: Wrong number of output argument(s): %d expected.\n"), "mode", 1);
         return Function::Error;
-	}
+    }
 
-    if(in.size() == 0)
+    /* Check the number of output argument */
+    if (_iRetCount != 1)
+    {
+        Scierror(78, _("%s: Wrong number of output argument(s): %d expected.\n"), "mode", 1);
+        return Function::Error;
+    }
+
+    if (in.size() == 0)
     {
         out.push_back(new Double(ConfigVariable::getPromptMode()));
     }
     else
     {
-        if(in[0]->isDouble() == false || in[0]->getAs<Double>()->getSize() != 1)
-	    {
+        if (in[0]->isDouble() == false)
+        {
             Scierror(999, _("%s: Wrong type for input argument #%d: Scalar expected.\n"), "mode", 1);
             return Function::Error;
         }
 
-        ConfigVariable::setPromptMode((int)in[0]->getAs<Double>()->getReal()[0]);
+        types::Double* pDblIn = in[0]->getAs<types::Double>();
 
+        if (pDblIn->isScalar() == false)
+        {
+            Scierror(999, _("%s: Wrong size for input argument #%d: A scalar expected.\n"), "mode", 1);
+            return types::Function::Error;
+        }
+
+        int iScilabMode = (int)pDblIn->get(0);
+        if (pDblIn->get(0) != (double)iScilabMode)
+        {
+            Scierror(999, _("%s: Wrong type for input argument #%d: An integer value expected.\n"), "mode", 1);
+            return types::Function::Error;
+        }
+
+        ConfigVariable::setPromptMode(iScilabMode);
     }
 
-	return Function::OK;
+    return Function::OK;
 }
 /*--------------------------------------------------------------------------*/
