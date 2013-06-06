ca0a85a91d37062b18ac12bfb86fc1ac50e8c492
Cedric Delamarre <cedric.delamarre@scilab-enterprises.com>
2013-06-06 18:02:25 +0200

    exists : error message corrected.
    test_run("core","bug_3296",["no_check_error_output" ]);
    
    Change-Id: Ic0b5894ad3ca21941e0fb23ad3c70c72e2c9f00a

--- scilab/modules/core/sci_gateway/cpp/sci_exists.cpp
+++ scilab/modules/core/sci_gateway/cpp/sci_exists.cpp
@@ -21,14 +21,16 @@ extern "C"
 #include "Scierror.h"
 }
 
-enum ScopeRange {
+enum ScopeRange
+{
     Local,
     NoLocal,
     All,
     UnknownRange
 };
 
-enum PrivateResult {
+enum PrivateResult
+{
     VariableFound,
     VariableNotFound,
     FunctionFailed
@@ -62,20 +64,20 @@ static PrivateResult sci_existsOrIsdef(types::typed_list &in, const char *fname)
 
     if (!in[0]->isString() || in[0]->getAs<types::String>()->getSize() != 1)
     {
-        Scierror(999,_("%s: Wrong type for input argument #%d: A single string expected.\n"), fname, 1);
+        Scierror(999, _("%s: Wrong type for input argument #%d: A single string expected.\n"), fname, 1);
         return FunctionFailed;
     }
 
     if (in.size() == 2 && (!in[1]->isString() || in[1]->getAs<types::String>()->getSize() != 1))
     {
-        Scierror(999,_("%s: Wrong type for input argument #%d: A single string expected.\n"), fname, 2);
+        Scierror(999, _("%s: Wrong type for input argument #%d: A single string expected.\n"), fname, 2);
         return FunctionFailed;
     }
 
     const wchar_t *psScope = NULL;
     if (in.size() == 2)
     {
-        psScope = in[1]->getAs<types::String>()->get(0,0);
+        psScope = in[1]->getAs<types::String>()->get(0, 0);
     }
     else
     {
@@ -87,18 +89,18 @@ static PrivateResult sci_existsOrIsdef(types::typed_list &in, const char *fname)
     types::InternalType *pitReturn = NULL;
     switch (getScopeFromOption(psScope))
     {
-    case All:
-        pitReturn = symbol::Context::getInstance()->get(symbol::Symbol(in[0]->getAs<types::String>()->get(0,0)));
-        break;
-    case Local:
-        pitReturn = symbol::Context::getInstance()->getCurrentLevel(symbol::Symbol(in[0]->getAs<types::String>()->get(0,0)));
-        break;
-    case NoLocal:
-        pitReturn = symbol::Context::getInstance()->getAllButCurrentLevel(symbol::Symbol(in[0]->getAs<types::String>()->get(0,0)));
-        break;
-    default :
-        Scierror(36, _("%s: Wrong input argument %d.\n"), fname, 2);
-        return FunctionFailed;
+        case All:
+            pitReturn = symbol::Context::getInstance()->get(symbol::Symbol(in[0]->getAs<types::String>()->get(0, 0)));
+            break;
+        case Local:
+            pitReturn = symbol::Context::getInstance()->getCurrentLevel(symbol::Symbol(in[0]->getAs<types::String>()->get(0, 0)));
+            break;
+        case NoLocal:
+            pitReturn = symbol::Context::getInstance()->getAllButCurrentLevel(symbol::Symbol(in[0]->getAs<types::String>()->get(0, 0)));
+            break;
+        default :
+            Scierror(36, _("%s: Wrong input argument %d.\n"), fname, 2);
+            return FunctionFailed;
     }
 
 
@@ -116,14 +118,14 @@ types::Function::ReturnValue sci_exists(types::typed_list &in, int _iRetCount, t
 {
     switch (sci_existsOrIsdef(in, "exists"))
     {
-    case VariableFound :
-        out.push_back(new types::Double(1.0));
-        return types::Function::OK;
-    case VariableNotFound :
-        out.push_back(new types::Double(0.0));
-        return types::Function::OK;
-    default :
-        return types::Function::Error;
+        case VariableFound :
+            out.push_back(new types::Double(1.0));
+            return types::Function::OK;
+        case VariableNotFound :
+            out.push_back(new types::Double(0.0));
+            return types::Function::OK;
+        default :
+            return types::Function::Error;
     }
 }
 
@@ -131,13 +133,13 @@ types::Function::ReturnValue sci_isdef(types::typed_list &in, int _iRetCount, ty
 {
     switch (sci_existsOrIsdef(in, "isdef"))
     {
-    case VariableFound :
-        out.push_back(new types::Bool(true));
-        return types::Function::OK;
-    case VariableNotFound :
-        out.push_back(new types::Bool(false));
-        return types::Function::OK;
-    default :
-        return types::Function::Error;
+        case VariableFound :
+            out.push_back(new types::Bool(true));
+            return types::Function::OK;
+        case VariableNotFound :
+            out.push_back(new types::Bool(false));
+            return types::Function::OK;
+        default :
+            return types::Function::Error;
     }
 }
