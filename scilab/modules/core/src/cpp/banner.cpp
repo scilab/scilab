f00b05a75325ac8181c5040a8cff071b377b0464
Antoine ELIAS <antoine.elias@scilab-enterprises.com>
2013-06-23 09:59:18 +0200

    update banner
    Change-Id: I88ed7311c0bf027cecada930abfa1415532be5d9

--- scilab/modules/core/src/cpp/banner.cpp
+++ scilab/modules/core/src/cpp/banner.cpp
@@ -35,7 +35,6 @@ void banner(void)
     scilabForcedWriteW(L"\n\n");
 
     centerPrint(_W("Scilab Enterprises\n"));
-    centerPrint(_W("Consortium Scilab (DIGITEO)\n"));
     centerPrint(_W("Copyright (c) 2011-2013 (Scilab Enterprises)\n"));
     centerPrint(_W("Copyright (c) 1989-2012 (INRIA)\n"));
     centerPrint(_W("Copyright (c) 1989-2007 (ENPC)\n"));
