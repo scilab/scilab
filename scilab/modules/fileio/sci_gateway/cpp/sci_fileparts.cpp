238a6bf444097a27f51464a00bce807611a4860b
Cedric Delamarre <cedric.delamarre@scilab-enterprises.com>
2013-07-22 15:20:03 +0200

    fileparts function corrected.
    test_run("fileio","fileparts",["no_check_error_output" ]);
    
    Change-Id: I2ba2885280c03591f08f7a551a666d4f0e6ef024

--- scilab/modules/fileio/sci_gateway/cpp/sci_fileparts.cpp
+++ scilab/modules/fileio/sci_gateway/cpp/sci_fileparts.cpp
@@ -38,8 +38,12 @@ enum PartialPart
 /*--------------------------------------------------------------------------*/
 Function::ReturnValue sci_fileparts(typed_list &in, int _iRetCount, typed_list &out)
 {
-    bool bPartialValue  = false;
     PartialPart iPartialPart = AllPart;
+
+    types::String* pStrPath = NULL;
+    types::String* pStrOut  = NULL;
+    types::String* pStrOut2 = NULL;
+    types::String* pStrOut3 = NULL;
 
     if (in.size() < 1 || in.size() > 2)
     {
@@ -53,384 +57,130 @@ Function::ReturnValue sci_fileparts(typed_list &in, int _iRetCount, typed_list &
         return Function::Error;
     }
 
-    if (in.size() == 1 && _iRetCount != 3)
+    if (in.size() == 1 && _iRetCount > 3)
     {
-        Scierror(78, _("%s: Wrong number of output arguments: %d expected.\n"), "fileparts", 3);
+        Scierror(78, _("%s: Wrong number of output arguments: %d to %d expected.\n"), "fileparts", 1, 3);
         return Function::Error;
     }
 
-    if (in[0]->isString() == false && in[0]->getAs<types::String>()->getSize() != 1)
+    if (in[0]->isString() == false)
     {
         Scierror(999, _("%s: Wrong type for input argument #%d: A string expected.\n"), "fileparts", 1);
         return Function::Error;
     }
 
+    pStrPath = in[0]->getAs<types::String>();
+    pStrOut = new types::String(pStrPath->getDims(), pStrPath->getDimsArray());
+
     if (in.size() == 2)
     {
-        if (in[1]->isString() == false && in[1]->getAs<types::String>()->getSize() != 1)
+        if (in[1]->isString() == false)
         {
             Scierror(999, _("%s: Wrong type for input argument #%d: A string expected.\n"), "fileparts", 2);
             return Function::Error;
         }
 
-        wchar_t* pParts = in[1]->getAs<types::String>()->get()[0];
+        if (in[1]->getAs<types::String>()->getSize() != 1)
+        {
+            Scierror(999, _("%s: Wrong size for input argument #%d: A single string expected.\n"), "fileparts", 2);
+            return Function::Error;
+        }
 
+        wchar_t* pParts = in[1]->getAs<types::String>()->get(0);
         if (wcscmp(pParts, FILEPARTS_PATH_SELECTOR) == 0)
         {
-            bPartialValue   = true;
-            iPartialPart    = PathPart;
+            iPartialPart = PathPart;
         }
         else if (wcscmp(pParts, FILEPARTS_FNAME_SELECTOR) == 0)
         {
-            bPartialValue   = true;
-            iPartialPart    = NamePart;
+            iPartialPart = NamePart;
         }
         else if (wcscmp(pParts, FILEPARTS_EXTENSION_SELECTOR) == 0)
         {
-            bPartialValue   = true;
-            iPartialPart    = ExtensionPart;
+            iPartialPart = ExtensionPart;
         }
         else
         {
             Scierror(999, _("%s: Wrong value for input argument #%d.\n"), "fileparts", 2);
             return Function::Error;
         }
-    }
 
-    wchar_t* pPath = in[0]->getAs<types::String>()->get()[0];
-
-    wchar_t* pwstDrive      = new wchar_t[wcslen(pPath) + 1];
-    wchar_t* pwstDirectory  = new wchar_t[wcslen(pPath) + 1];
-    wchar_t* pwstName       = new wchar_t[wcslen(pPath) + 1];
-    wchar_t* pwstExtension  = new wchar_t[wcslen(pPath) + 1];
-
-    splitpathW(pPath, FALSE, pwstDrive, pwstDirectory, pwstName, pwstExtension);
-    wcscat(pwstDrive, pwstDirectory);
-
-    if (in.size() == 2)
-    {
-        String* pOut = NULL;
-        switch (iPartialPart)
+        for (int i = 0; i < pStrPath->getSize(); i++)
         {
-            case PathPart :
+            wchar_t* pPath = pStrPath->get(i);
+
+            wchar_t* pwstDrive      = new wchar_t[wcslen(pPath) + 1];
+            wchar_t* pwstDirectory  = new wchar_t[wcslen(pPath) + 1];
+            wchar_t* pwstName       = new wchar_t[wcslen(pPath) + 1];
+            wchar_t* pwstExtension  = new wchar_t[wcslen(pPath) + 1];
+
+            splitpathW(pPath, FALSE, pwstDrive, pwstDirectory, pwstName, pwstExtension);
+            wcscat(pwstDrive, pwstDirectory);
+
+            switch (iPartialPart)
             {
-                pOut = new String(pwstDrive);
-                break;
+                case PathPart :
+                {
+                    pStrOut->set(i, pwstDrive);
+                    break;
+                }
+                case NamePart :
+                {
+                    pStrOut->set(i, pwstName);
+                    break;
+                }
+                case ExtensionPart :
+                {
+                    pStrOut->set(i, pwstExtension);
+                    break;
+                }
+                default :
+                {
+                    //Never occur
+                }
             }
-            case NamePart :
-            {
-                pOut = new String(pwstName);
-                break;
-            }
-            case ExtensionPart :
-            {
-                pOut = new String(pwstExtension);
-                break;
-            }
-            default :
-            {
-                //Never occur
-            }
+
+            delete[] pwstDirectory;
+            delete[] pwstDrive;
+            delete[] pwstExtension;
+            delete[] pwstName;
         }
 
-
-        out.push_back(pOut);
+        out.push_back(pStrOut);
     }
     else
     {
-        //standard case, 3 outputs
-        String* pOut1 = new String(pwstDrive);
-        out.push_back(pOut1);
+        pStrOut2 = new types::String(pStrPath->getDims(), pStrPath->getDimsArray());
+        pStrOut3 = new types::String(pStrPath->getDims(), pStrPath->getDimsArray());
 
-        String* pOut2 = new String(pwstName);
-        out.push_back(pOut2);
+        for (int i = 0; i < pStrPath->getSize(); i++)
+        {
+            wchar_t* pPath = pStrPath->get(i);
 
-        String* pOut3 = new String(pwstExtension);
-        out.push_back(pOut3);
+            wchar_t* pwstDrive      = new wchar_t[wcslen(pPath) + 1];
+            wchar_t* pwstDirectory  = new wchar_t[wcslen(pPath) + 1];
+            wchar_t* pwstName       = new wchar_t[wcslen(pPath) + 1];
+            wchar_t* pwstExtension  = new wchar_t[wcslen(pPath) + 1];
+
+            splitpathW(pPath, FALSE, pwstDrive, pwstDirectory, pwstName, pwstExtension);
+            wcscat(pwstDrive, pwstDirectory);
+
+            //standard case, 3 outputs
+            pStrOut->set(i, pwstDrive);
+            pStrOut2->set(i, pwstName);
+            pStrOut3->set(i, pwstExtension);
+
+            delete[] pwstDirectory;
+            delete[] pwstDrive;
+            delete[] pwstExtension;
+            delete[] pwstName;
+        }
+
+        out.push_back(pStrOut);
+        out.push_back(pStrOut2);
+        out.push_back(pStrOut3);
     }
 
-    delete[] pwstDirectory;
-    delete[] pwstDrive;
-    delete[] pwstExtension;
-    delete[] pwstName;
-
     return Function::OK;
-    //SciErr sciErr;
-    //int m1 = 0, n1 = 0;
-    //int *piAddressVarOne = NULL;
-    //int iType1	= 0;
-    //wchar_t *pStVarOne = NULL;
-    //int lenStVarOne = 0;
-
-    //int m2 = 0, n2 = 0;
-    //int *piAddressVarTwo = NULL;
-    //int iType2	= 0;
-    //wchar_t *pStVarTwo = NULL;
-    //int lenStVarTwo = 0;
-
-    //wchar_t* drv = NULL;
-    //wchar_t* dir = NULL;
-    //wchar_t* name = NULL;
-    //wchar_t* ext = NULL;
-    //wchar_t* path_out = NULL;
-
-    //CheckLhs(1,3);
-    //CheckRhs(1,2);
-
-    //if ( (Rhs == 2) && (Lhs != 1) )
-    //{
-    //	Scierror(78,_("%s: Wrong number of output arguments: %d expected.\n"), fname, 1);
-    //	return 0;
-    //}
-
-    //sciErr = getVarAddressFromPosition(pvApiCtx, 1, &piAddressVarOne);
-    //if(sciErr.iErr)
-    //{
-    //	printError(&sciErr, 0);
-    //	return 0;
-    //}
-
-    //sciErr = getVarType(pvApiCtx, piAddressVarOne, &iType1);
-    //if(sciErr.iErr)
-    //{
-    //	printError(&sciErr, 0);
-    //	return 0;
-    //}
-
-    //if (iType1  != sci_strings )
-    //{
-    //	Scierror(999,_("%s: Wrong type for input argument #%d: A string expected.\n"),fname,1);
-    //	return 0;
-    //}
-
-    //// get value of lenStVarOne
-    //sciErr = getMatrixOfWideString(pvApiCtx, piAddressVarOne,&m1,&n1,&lenStVarOne,&pStVarOne);
-    //if(sciErr.iErr)
-    //{
-    //	printError(&sciErr, 0);
-    //	return 0;
-    //}
-
-    //if ( (m1 != n1) && (n1 != 1) )
-    //{
-    //	Scierror(999,_("%s: Wrong size for input argument #%d: A string expected.\n"),fname,1);
-    //	return 0;
-    //}
-
-    //pStVarOne = (wchar_t*)MALLOC(sizeof(wchar_t)*(lenStVarOne + 1));
-    //if (pStVarOne == NULL)
-    //{
-    //	Scierror(999,_("%s: Memory allocation error.\n"),fname);
-    //	return 0;
-    //}
-
-    //sciErr = getMatrixOfWideString(pvApiCtx, piAddressVarOne, &m1, &n1, &lenStVarOne, &pStVarOne);
-    //if(sciErr.iErr)
-    //{
-    //	printError(&sciErr, 0);
-    //	if (pStVarOne) {FREE(pStVarOne); pStVarOne = NULL;}
-    //	return 0;
-    //}
-
-    //if (Rhs == 2)
-    //{
-    //	sciErr = getVarAddressFromPosition(pvApiCtx, 2, &piAddressVarTwo);
-    //	if(sciErr.iErr)
-    //	{
-    //		printError(&sciErr, 0);
-    //		if (pStVarOne) {FREE(pStVarOne); pStVarOne = NULL;}
-    //		return 0;
-    //	}
-
-    //	sciErr = getVarType(pvApiCtx, piAddressVarTwo, &iType2);
-    //	if(sciErr.iErr)
-    //	{
-    //		printError(&sciErr, 0);
-    //		if (pStVarOne) {FREE(pStVarOne); pStVarOne = NULL;}
-    //		return 0;
-    //	}
-
-    //	if (iType2  != sci_strings )
-    //	{
-    //		Scierror(999,_("%s: Wrong type for input argument #%d: A string expected.\n"),fname,2);
-    //		if (pStVarOne) {FREE(pStVarOne); pStVarOne = NULL;}
-    //		return 0;
-    //	}
-
-    //	// get value of lenStVarTwo
-    //	sciErr = getMatrixOfWideString(pvApiCtx, piAddressVarTwo, &m2, &n2, &lenStVarTwo, &pStVarTwo);
-    //	if(sciErr.iErr)
-    //	{
-    //		printError(&sciErr, 0);
-    //		if (pStVarOne) {FREE(pStVarOne); pStVarOne = NULL;}
-    //		return 0;
-    //	}
-
-    //	if ( (m2 != n2) && (n2 != 1) )
-    //	{
-    //		Scierror(999,_("%s: Wrong size for input argument #%d: A string expected.\n"),fname,2);
-    //		if (pStVarOne) {FREE(pStVarOne); pStVarOne = NULL;}
-    //		return 0;
-    //	}
-
-    //	pStVarTwo = (wchar_t*)MALLOC(sizeof(wchar_t)*(lenStVarTwo + 1));
-    //	if (pStVarTwo == NULL)
-    //	{
-    //		Scierror(999,_("%s: Memory allocation error.\n"),fname);
-    //		if (pStVarOne) {FREE(pStVarOne); pStVarOne = NULL;}
-    //		return 0;
-    //	}
-    //
-    //	sciErr = getMatrixOfWideString(pvApiCtx, piAddressVarTwo, &m2, &n2, &lenStVarTwo, &pStVarTwo);
-    //	if(sciErr.iErr)
-    //	{
-    //		printError(&sciErr, 0);
-    //		if (pStVarOne) {FREE(pStVarOne); pStVarOne = NULL;}
-    //		if (pStVarTwo) {FREE(pStVarTwo); pStVarTwo = NULL;}
-    //		return 0;
-    //	}
-    //}
-
-    //drv = (wchar_t*)MALLOC(sizeof(wchar_t)*(lenStVarOne + 1));
-    //dir = (wchar_t*)MALLOC(sizeof(wchar_t)*(lenStVarOne + 1));
-    //name = (wchar_t*)MALLOC(sizeof(wchar_t)*(lenStVarOne + 1));
-    //ext = (wchar_t*)MALLOC(sizeof(wchar_t)*(lenStVarOne + 1));
-    //path_out = (wchar_t*)MALLOC(sizeof(wchar_t)*(lenStVarOne + 1));
-
-    //if ( (drv == NULL) || (dir == NULL) || (name == NULL) || (ext == NULL) || (path_out == NULL) )
-    //{
-    //	if (pStVarOne) {FREE(pStVarOne); pStVarOne = NULL;}
-    //	if (pStVarTwo) {FREE(pStVarTwo); pStVarTwo = NULL;}
-    //	if (drv) {FREE(drv); drv = NULL;}
-    //	if (dir) {FREE(dir); dir = NULL;}
-    //	if (name) {FREE(name); name = NULL;}
-    //	if (ext) {FREE(ext); ext = NULL;}
-    //	if (path_out) {FREE(path_out); path_out = NULL;}
-
-    //	Scierror(999,_("%s: Memory allocation error.\n"), fname);
-    //	return 0;
-    //}
-
-    //splitpathW(pStVarOne, FALSE, drv, dir, name, ext);
-
-    //if (pStVarTwo) /* Rhs == 2 */
-    //{
-    //	wchar_t *output_value = NULL;
-    //	int m_out = 0, n_out = 0;
-
-    //	if (wcscmp(pStVarTwo, FILEPARTS_PATH_SELECTOR) == 0)
-    //	{
-    //		output_value = path_out;
-    //		wcscpy(output_value, drv);
-    //		wcscat(output_value, dir);
-    //	}
-    //	else if (wcscmp(pStVarTwo, FILEPARTS_FNAME_SELECTOR) == 0)
-    //	{
-    //		output_value = name;
-    //	}
-    //	else if (wcscmp(pStVarTwo, FILEPARTS_EXTENSION_SELECTOR) == 0)
-    //	{
-    //		output_value = ext;
-    //	}
-    //	else
-    //	{
-    //		if (pStVarOne) {FREE(pStVarOne); pStVarOne = NULL;}
-    //		if (pStVarTwo) {FREE(pStVarTwo); pStVarTwo = NULL;}
-    //		if (drv) {FREE(drv); drv = NULL;}
-    //		if (dir) {FREE(dir); dir = NULL;}
-    //		if (name) {FREE(name); name = NULL;}
-    //		if (ext) {FREE(ext); ext = NULL;}
-    //		if (path_out) {FREE(path_out); path_out = NULL;}
-
-    //		Scierror(999,_("%s: Wrong value for input argument #%d.\n"), fname, 2);
-    //		return 0;
-    //	}
-
-    //	m_out = 1; n_out = 1;
-    //	sciErr = createMatrixOfWideString(pvApiCtx, Rhs + 1, m_out, n_out, &output_value);
-    //	if(sciErr.iErr)
-    //	{
-    //		printError(&sciErr, 0);
-    //		if (pStVarOne) {FREE(pStVarOne); pStVarOne = NULL;}
-    //		if (pStVarTwo) {FREE(pStVarTwo); pStVarTwo = NULL;}
-    //		if (drv) {FREE(drv); drv = NULL;}
-    //		if (dir) {FREE(dir); dir = NULL;}
-    //		if (name) {FREE(name); name = NULL;}
-    //		if (ext) {FREE(ext); ext = NULL;}
-    //		if (path_out) {FREE(path_out); path_out = NULL;}
-    //		return 0;
-    //	}
-
-    //	LhsVar(1) = Rhs + 1;
-    //	C2F(putlhsvar)();
-    //}
-    //else
-    //{
-    //	int m_out = 1, n_out = 1;
-
-    //	wcscpy(path_out, drv);
-    //	wcscat(path_out, dir);
-
-    //	sciErr = createMatrixOfWideString(pvApiCtx, Rhs + 1, m_out, n_out, &path_out);
-    //	if(sciErr.iErr)
-    //	{
-    //		printError(&sciErr, 0);
-    //		if (pStVarOne) {FREE(pStVarOne); pStVarOne = NULL;}
-    //		if (pStVarTwo) {FREE(pStVarTwo); pStVarTwo = NULL;}
-    //		if (drv) {FREE(drv); drv = NULL;}
-    //		if (dir) {FREE(dir); dir = NULL;}
-    //		if (name) {FREE(name); name = NULL;}
-    //		if (ext) {FREE(ext); ext = NULL;}
-    //		if (path_out) {FREE(path_out); path_out = NULL;}
-    //		return 0;
-    //	}
-
-    //	LhsVar(1) = Rhs + 1;
-
-    //	sciErr = createMatrixOfWideString(pvApiCtx, Rhs + 2, m_out, n_out, &name);
-    //	if(sciErr.iErr)
-    //	{
-    //		printError(&sciErr, 0);
-    //		if (pStVarOne) {FREE(pStVarOne); pStVarOne = NULL;}
-    //		if (pStVarTwo) {FREE(pStVarTwo); pStVarTwo = NULL;}
-    //		if (drv) {FREE(drv); drv = NULL;}
-    //		if (dir) {FREE(dir); dir = NULL;}
-    //		if (name) {FREE(name); name = NULL;}
-    //		if (ext) {FREE(ext); ext = NULL;}
-    //		if (path_out) {FREE(path_out); path_out = NULL;}
-    //		return 0;
-    //	}
-
-    //	LhsVar(2) = Rhs + 2;
-
-    //	sciErr = createMatrixOfWideString(pvApiCtx, Rhs + 3, m_out, n_out, &ext);
-    //	if(sciErr.iErr)
-    //	{
-    //		printError(&sciErr, 0);
-    //		if (pStVarOne) {FREE(pStVarOne); pStVarOne = NULL;}
-    //		if (pStVarTwo) {FREE(pStVarTwo); pStVarTwo = NULL;}
-    //		if (drv) {FREE(drv); drv = NULL;}
-    //		if (dir) {FREE(dir); dir = NULL;}
-    //		if (name) {FREE(name); name = NULL;}
-    //		if (ext) {FREE(ext); ext = NULL;}
-    //		if (path_out) {FREE(path_out); path_out = NULL;}
-    //		return 0;
-    //	}
-
-    //	LhsVar(3) = Rhs + 3;
-
-    //	C2F(putlhsvar)();
-    //}
-
-
-    //if (pStVarOne) {FREE(pStVarOne); pStVarOne = NULL;}
-    //if (pStVarTwo) {FREE(pStVarTwo); pStVarTwo = NULL;}
-    //if (drv) {FREE(drv); drv = NULL;}
-    //if (dir) {FREE(dir); dir = NULL;}
-    //if (name) {FREE(name); name = NULL;}
-    //if (ext) {FREE(ext); ext = NULL;}
-    //if (path_out) {FREE(path_out); path_out = NULL;}
-
-    //return 0;
 }
 /*--------------------------------------------------------------------------*/
