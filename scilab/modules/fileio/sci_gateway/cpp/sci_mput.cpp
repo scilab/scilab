370b4a46dc3bdab3aaccd03dbc221844d434bbff
Cedric Delamarre <cedric.delamarre@scilab-enterprises.com>
2013-07-26 16:34:41 +0200

    mput : management of integer in mput (mputi)
    test_run("fileio","mputi",["no_check_error_output" ]);
    
    Change-Id: Ife41e13ffe353d61cbb41d2c9c25d654f07b53a3

--- scilab/modules/fileio/sci_gateway/cpp/sci_mput.cpp
+++ scilab/modules/fileio/sci_gateway/cpp/sci_mput.cpp
@@ -16,6 +16,7 @@
 #include "fileio_gw.hxx"
 #include "string.hxx"
 #include "double.hxx"
+#include "int.hxx"
 #include "function.hxx"
 
 extern "C"
@@ -51,16 +52,6 @@ Function::ReturnValue sci_mput(typed_list &in, int _iRetCount, typed_list &out)
 
     iSize = in[0]->getAs<GenericType>()->getSize();
 
-    if (in[0]->isDouble())
-    {
-        pData = in[0]->getAs<Double>()->get();
-    }
-    else
-    {
-        //ints
-        //???
-    }
-
     if (in.size() >= 2)
     {
         //export format
@@ -91,9 +82,87 @@ Function::ReturnValue sci_mput(typed_list &in, int _iRetCount, typed_list &out)
             return types::Function::Error;
     }
 
+    if (in[0]->isDouble())
+    {
+        pData = in[0]->getAs<Double>()->get();
+    }
+    else
+    {
+        pData = (double*)malloc(iSize * sizeof(double));
+        if (in[0]->isInt8())
+        {
+            char* piData = in[0]->getAs<Int8>()->get();
+            for (int i = 0; i < iSize; i++)
+            {
+                pData[i] = (double)piData[i];
+            }
+        }
+        else if (in[0]->isUInt8())
+        {
+            unsigned char* piData = in[0]->getAs<UInt8>()->get();
+            for (int i = 0; i < iSize; i++)
+            {
+                pData[i] = (double)piData[i];
+            }
+        }
+        else if (in[0]->isInt16())
+        {
+            short* piData = in[0]->getAs<Int16>()->get();
+            for (int i = 0; i < iSize; i++)
+            {
+                pData[i] = (double)piData[i];
+            }
+        }
+        else if (in[0]->isUInt16())
+        {
+            unsigned short* piData = in[0]->getAs<UInt16>()->get();
+            for (int i = 0; i < iSize; i++)
+            {
+                pData[i] = (double)piData[i];
+            }
+        }
+        else if (in[0]->isInt32())
+        {
+            int* piData = in[0]->getAs<Int32>()->get();
+            for (int i = 0; i < iSize; i++)
+            {
+                pData[i] = (double)piData[i];
+            }
+        }
+        else if (in[0]->isUInt32())
+        {
+            unsigned int* piData = in[0]->getAs<UInt32>()->get();
+            for (int i = 0; i < iSize; i++)
+            {
+                pData[i] = (double)piData[i];
+            }
+        }
+        else if (in[0]->isInt64())
+        {
+            long long* piData = in[0]->getAs<Int64>()->get();
+            for (int i = 0; i < iSize; i++)
+            {
+                pData[i] = (double)piData[i];
+            }
+        }
+        else if (in[0]->isUInt64())
+        {
+            unsigned long long* piData = in[0]->getAs<UInt64>()->get();
+            for (int i = 0; i < iSize; i++)
+            {
+                pData[i] = (double)piData[i];
+            }
+        }
+    }
+
     C2F(mput)(&iFile, pData, &iSize, pstType, &iErr);
 
     FREE(pstType);
+    if (in[0]->isDouble() == false)
+    {
+        free(pData);
+    }
+
     return Function::OK;
 }
 /*--------------------------------------------------------------------------*/
