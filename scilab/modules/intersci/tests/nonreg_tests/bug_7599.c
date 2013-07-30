9ddcdef2f0421cea1db7f663cdd7c2e1a0a552cc
Adeline CARNIS <adeline.carnis@scilab-enterprises.com>
2013-07-30 15:13:40 +0200

    Indent files for intersci tests. test_run('intersci', 'bug_4625') test_run('intersci', 'bug_7599')
    Change-Id: I5c285a81fe2bc4cd92c55d2e192bb59c4d9518e2

--- scilab/modules/intersci/tests/nonreg_tests/bug_7599.c
+++ scilab/modules/intersci/tests/nonreg_tests/bug_7599.c
@@ -17,7 +17,7 @@ int intsext1c(char *fname)
     CheckVector(2, m2, n2);
     mn2 = m2 * n2;
     /* cross variable size checking */
-    CheckDimProp(1, 2, m1*n1 != m2 * n2);
+    CheckDimProp(1, 2, m1 * n1 != m2 * n2);
     CreateVar(3, "d", (un = 1, &un), (mn3 = mn1, &mn3), &l3); /* named: c */
     C2F(ext1c)(&mn1, stk(l1), stk(l2), stk(l3));
     LhsVar(1) = 3;
