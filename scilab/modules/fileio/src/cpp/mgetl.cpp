a5103d86a79ca48f00439c3ad84f2118417b1372
Cedric Delamarre <cedric.delamarre@scilab-enterprises.com>
2013-08-02 15:47:25 +0200

    the limit size of a line read removed.
    // /!\ very long test /!\
    test_run("fileio","bug_10300",["no_check_error_output" ]);
    
    a = ones(3, 1000);
    fprintfMat(TMPDIR + '/bug_10300_file.txt',a);
    clear a;
    a = fscanfMat(TMPDIR + '/bug_10300_file.txt'); // should not segfault
    
    Change-Id: I697f9a8bbea8d42f5a37d893e75f71ef95fb2a24

--- scilab/modules/fileio/src/cpp/mgetl.cpp
+++ scilab/modules/fileio/src/cpp/mgetl.cpp
@@ -25,7 +25,13 @@ extern "C"
 #include "MALLOC.h"
 #include "os_wcsdup.h"
 #include "os_strdup.h"
+#include "mtell.h"
+#include "mseek.h"
+#include "sciprint.h"
 }
+
+#include <iostream>
+#include <fstream>
 /*--------------------------------------------------------------------------*/
 #define LINE_MAX 4096
 #define CR L'\r'
@@ -43,7 +49,7 @@ wchar_t **mgetl(int fd, int nbLinesIn, int *nbLinesOut, int *ierr)
 {
     wchar_t **strLines = NULL;
     File* pFile     = NULL;
-
+    int iLineSizeMult = 1;
     *ierr = MGETL_ERROR;
     *nbLinesOut = 0;
 
@@ -54,11 +60,43 @@ wchar_t **mgetl(int fd, int nbLinesIn, int *nbLinesOut, int *ierr)
         nbLinesIn = 1;
     }
 
+    /*try std version*/
+    //{
+    //    int posix_handle = ::_fileno(pFile->getFiledesc());
+
+    //    std::ifstream ifs(::_wfdopen(posix_handle, pFile->getFileMode().c_str()));
+    //    std::list<string> lst;
+    //    std::string str;
+
+    //    while(ifs.eof() == false && lst.size() < nbLinesIn)
+    //    {
+    //        std::getline(ifs, str);
+    //        lst.push_back(str);
+    //    }
+
+    //    sciprint("size : %d\n", lst.size());
+
+    //    *nbLinesOut =  (int)lst.size();
+    //    if(*nbLinesOut == 0)
+    //    {
+    //        return NULL;
+    //    }
+
+    //    strLines = (wchar_t**)MALLOC(sizeof(wchar_t*) * *nbLinesOut);
+    //    for(int i = 0 ; i < *nbLinesOut ; i++)
+    //    {
+    //        strLines[i] = to_wide_string(lst.front().c_str());
+    //        lst.pop_front();
+    //    }
+
+    //    return strLines;
+    //}
+
     if (pFile)
     {
-        wchar_t Line[LINE_MAX * 2];
+        wchar_t* Line = (wchar_t*)MALLOC(LINE_MAX * iLineSizeMult * sizeof(wchar_t));
         int nbLines = 0;
-
+        int iPos = 0;
         if (nbLinesIn < 0)
         {
             strLines = (wchar_t **)MALLOC(sizeof(wchar_t *));
@@ -68,14 +106,24 @@ wchar_t **mgetl(int fd, int nbLinesIn, int *nbLinesOut, int *ierr)
                 *ierr = MGETL_MEMORY_ALLOCATION_ERROR;
                 return NULL;
             }
-
-            while ( getLine ( Line, sizeof(Line), pFile ) != NULL )
+            while ( getLine ( Line, LINE_MAX * iLineSizeMult, pFile ) != NULL )
             {
+                if (wcslen(Line) >= (LINE_MAX * iLineSizeMult) - 1)
+                {
+                    FREE(Line);
+                    iLineSizeMult++;
+                    Line = (wchar_t*)MALLOC(LINE_MAX * iLineSizeMult * sizeof(wchar_t));
+                    mseek(fd, iPos, SEEK_SET);
+
+                    continue;
+                }
+
+                iPos = mtell(fd);
                 /* UTF-16 BOM */
                 if ((nbLines == 0) && (Line[0] == UTF_16BE_BOM))
                 {
                     wchar_t* tmpLine = os_wcsdup(Line);
-                    memset(Line, 0x00, LINE_MAX * 2);
+                    memset(Line, 0x00, LINE_MAX * iLineSizeMult);
                     wcscpy(Line, &tmpLine[1]);
                 }
 
@@ -135,12 +183,24 @@ wchar_t **mgetl(int fd, int nbLinesIn, int *nbLinesOut, int *ierr)
                             header = true;
                         }
 
-                        if ( getLine ( Line, sizeof(Line), pFile) != NULL)
+                        if ( getLine ( Line, LINE_MAX * iLineSizeMult, pFile) != NULL)
                         {
+                            if (wcslen(Line) >= (LINE_MAX * iLineSizeMult) - 1)
+                            {
+                                FREE(Line);
+                                iLineSizeMult++;
+                                Line = (wchar_t*)MALLOC(LINE_MAX * iLineSizeMult * sizeof(wchar_t));
+                                mseek(fd, iPos, SEEK_SET);
+
+                                continue;
+                            }
+
+                            iPos = mtell(fd);
+
                             if (header && (Line[0] == UTF_16BE_BOM))
                             {
                                 wchar_t* tmpLine = os_wcsdup(Line);
-                                memset(Line, 0x00, LINE_MAX * 2);
+                                memset(Line, 0x00, LINE_MAX * iLineSizeMult);
                                 wcscpy(Line, &tmpLine[1]);
                             }
                             nbLines++;
@@ -181,6 +241,7 @@ wchar_t **mgetl(int fd, int nbLinesIn, int *nbLinesOut, int *ierr)
                 }
             }
         }
+        FREE(Line);
     }
     return strLines;
 }
@@ -200,10 +261,10 @@ wchar_t* getLine(wchar_t* _pstLine, int _iLineSize, File* _pFile)
             FREE(pstTemp);
             return NULL;
         }
-        char* pstUtf = convertAnsiToUtf(pstTemp);
-        wchar_t* pstTempWide = to_wide_string(pstUtf);
+        //        char* pstUtf = convertAnsiToUtf(pstTemp);
+        wchar_t* pstTempWide = to_wide_string(pstTemp);
         wcscpy(_pstLine, pstTempWide);
-        FREE(pstUtf);
+        //FREE(pstUtf);
         FREE(pstTemp);
         FREE(pstTempWide);
         return _pstLine;
