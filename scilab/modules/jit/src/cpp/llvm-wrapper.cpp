f2ad5fca9bebbd5d1c8d4e5bba87a2e5524a0290
Sylvestre Ledru <sylvestre.ledru@scilab-enterprises.com>
2013-09-26 14:53:31 +0200

    Improve of the rules + comments + headers
    Change-Id: I6c93af4137ec02e35a874023dc0d0a1e787c746b

--- scilab/modules/jit/src/cpp/llvm-wrapper.cpp
+++ scilab/modules/jit/src/cpp/llvm-wrapper.cpp
@@ -1,7 +1,23 @@
+/*
+ *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ *  Copyright (C) 2013-2013 - S/E - Sylvestre Ledru
+ *  Copyright (C) 2013-2013 - S/E - Antoine Elias
+ *
+ *  This file must be used under the terms of the CeCILL.
+ *  This source file is licensed as described in the file COPYING, which
+ *  you should have received as part of this distribution.  The terms
+ *  are also available at
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
+
 #include "double.hxx"
 #include "types_addition.hxx"
 
-int main()
+/* This is a configuration file which will be transformed by llvm + clang
+for the JIT
+*/
+int foo()
 {
     types::Double* pdbl1 = types::Double::Empty();
     int res = AddDoubleToDouble(pdbl1, pdbl1, &pdbl1);
