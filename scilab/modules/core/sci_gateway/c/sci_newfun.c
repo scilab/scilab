0c7d80e33ee942c5dcd8e5a3c9cbdf7d018d3433
Sylvestre Ledru <sylvestre.ledru@scilab-enterprises.com>
2013-11-26 15:24:27 +0100

    Fix a bad declaration ... C90 sucks.
    Change-Id: I71de94d28918ecf0dec7b000661b34c1fb0a3250

--- scilab/modules/core/sci_gateway/c/sci_newfun.c
+++ scilab/modules/core/sci_gateway/c/sci_newfun.c
@@ -195,11 +195,13 @@ static BOOL Is_a_correct_function_name(char *functionname)
         if ( (strlen(functionname) > 0) && (strlen(functionname) <  nlgh) )
         {
             int i = 0;
+            int len_functionname = (int)strlen(functionname);
+
             if (isdigit(functionname[0]))
             {
                 return FALSE;
             }
-            int len_functionname = (int)strlen(functionname);
+
             for (i = 1; i < len_functionname; i++)
             {
                 /*
