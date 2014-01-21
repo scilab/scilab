8e4f4addafbfbbf9567a3b43a0e4af20c6e04453
Cedric Delamarre <cedric.delamarre@scilab-enterprises.com>
2014-01-21 18:35:39 +0100

    arl2 : the value of input argument 3 checked.
    v=ones(1,20);
    [d,n,e]=arl2(v,poly(1,'z','c'),1)
    [d,n,e]=arl2(v,d,3)
    [d,n,e]=arl2(v,d,8)
    [d,n,e]=arl2(v,d,2)
    
    Change-Id: I9272751cf841f6a90803abb785a8b171cc5a58b4

--- scilab/modules/cacsd/sci_gateway/fortran/sci_f_arl2.f
+++ scilab/modules/cacsd/sci_gateway/fortran/sci_f_arl2.f
@@ -2,11 +2,11 @@ c     --------------------------
 
 c Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
 c Copyright (C) INRIA
-c 
+c
 c This file must be used under the terms of the CeCILL.
 c This source file is licensed as described in the file COPYING, which
 c you should have received as part of this distribution.  The terms
-c are also available at    
+c are also available at
 c http://www.cecill.info/licences/Licence_CeCILL_V2.1-en.txt
 
 c     --------------------------
@@ -25,7 +25,7 @@ c
 c
       iadr(l)=l+l-1
       sadr(l)=(l/2)+1
-      
+
             if(top+lhs-rhs.ge.bot) then
          call error(18)
          return
@@ -117,7 +117,7 @@ c     Final degre parameter
       endif
       itmax=int(stk(l))
 c
-c     Initial point parameter 
+c     Initial point parameter
 c     (for arl2a it only gives le formal variable name)
       top=top-1
       lw=lstk(top+1)
@@ -142,6 +142,12 @@ c     (for arl2a it only gives le formal variable name)
       ld=sadr(ild+10)
       call idegre(stk(ld),istk(ild+9)-2,nd)
       call dscal(nd+1,1.0d+0/stk(ld+nd),stk(ld),1)
+      if(itmax.lt.nd) then
+         buf='arl2: Wrong value for input argument #3: '//
+     &  'More than degree of input argument #2 expected. '
+         call error(999)
+         return
+      endif
 c
 c     fourier coefficients parameters
       if(it1.ne.0) then
@@ -335,8 +341,7 @@ c     errors
 c     Putting in order the stack
       call unsfdcopy(lww-lw0,stk(lw0),1,stk(l0),1)
 c
-      
+
    99 return
       end
 c     ----------------------------
-      
