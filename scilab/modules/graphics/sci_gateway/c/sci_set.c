5da9f5b6dfcb14794ce4c033adb9273fbc1b3839
Antoine ELIAS <antoine.elias@scilab-enterprises.com>
2014-03-04 23:26:12 +0100

    fix display_function_data error
    Change-Id: I1823295e79d21f956c57f46f3a47b3ff741d5379

--- scilab/modules/graphics/sci_gateway/c/sci_set.c
+++ scilab/modules/graphics/sci_gateway/c/sci_set.c
@@ -339,7 +339,9 @@ int sci_set(char *fname, unsigned long fname_len)
             isData = 1;
         }
 
-        if ((strcmp(pstProperty, "user_data") == 0) || (stricmp(pstProperty, "userdata") == 0))
+        if (stricmp(pstProperty, "user_data") == 0 ||
+                stricmp(pstProperty, "userdata") == 0 ||
+                stricmp(pstProperty, "display_function_data") == 0)
         {
             /* in this case set_user_data_property
             * directly uses the  third position in the stack
