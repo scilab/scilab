5e748ca23ac87bbd7b18641b0134342e59c9dce3
Vincent COUVERT <vincent.couvert@scilab-enterprises.com>
2014-03-20 17:48:45 +0100

    Fix Windows compilation after https://codereview.scilab.org/#/c/14072/
    Change-Id: If34e9ae665948e6579956bf288b3c3ee307e29ce

--- scilab/modules/core/src/c/expr.c
+++ scilab/modules/core/src/c/expr.c
@@ -627,12 +627,12 @@ L103:
     C2F(getsym)();
     if (op == equal && C2F(com).sym != equal)
     {
+        int code_message = 7;
         if (C2F(com).sym == great || C2F(com).sym == less)
         {
             SciError(2);
             return 0;
         }
-        int code_message = 7;
         C2F(msgs)(&code_message, &val);
     }
 
