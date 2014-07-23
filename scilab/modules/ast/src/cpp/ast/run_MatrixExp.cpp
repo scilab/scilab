df4f1001fc6d12cf892fd8def97471c07a66cebc
Cedric Delamarre <cedric.delamarre@scilab-enterprises.com>
2014-07-23 11:02:40 +0200

    call overload when concatenate something with int or bool
    [%nan %t]
    [12 int32(14)]
    
    Change-Id: If91c409020b5821b390a4d81affe165417763f40

--- scilab/modules/ast/src/cpp/ast/run_MatrixExp.cpp
+++ scilab/modules/ast/src/cpp/ast/run_MatrixExp.cpp
@@ -81,7 +81,9 @@ void RunVisitorT<T>::visitprivate(const MatrixExp &e)
 
                 GenericType* pGTResult = poRow->getAs<GenericType>();
 
-                if (pGT->isList() || pGTResult->isList())
+                if (pGT->isList() || pGTResult->isList() ||
+                        pGT->isBool() || pGTResult->isBool() ||
+                        pGT->isInt()  || pGTResult->isInt())
                 {
                     poRow = callOverloadMatrixExp(L"c", pGTResult, pGT);
                     continue;
@@ -146,7 +148,9 @@ void RunVisitorT<T>::visitprivate(const MatrixExp &e)
             //check dimension
             GenericType* pGTResult = poResult->getAs<GenericType>();
 
-            if (pGT->isList() || pGTResult->isList())
+            if (pGT->isList() || pGTResult->isList() ||
+                    pGT->isBool() || pGTResult->isBool() ||
+                    pGT->isInt()  || pGTResult->isInt())
             {
                 poResult = callOverloadMatrixExp(L"f", pGTResult, pGT);
                 continue;
