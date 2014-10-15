65a1fd3068c30b6b3272132c0a73087f04a516b1
Clément DAVID <clement.david@scilab-enterprises.com>
2014-10-15 10:35:05 +0200

    Fix some warnings
    Change-Id: I708b1c53956015ab8155c68ae7594f31495c3d0c

--- scilab/modules/completion/src/c/getCommonPart.c
+++ scilab/modules/completion/src/c/getCommonPart.c
@@ -20,7 +20,7 @@
 /*--------------------------------------------------------------------------*/
 static int cmp( const void *a , const void *b)
 {
-    return strcmp(*(const char **)a, *(const char **)b );
+    return strcmp(*(char**)a, *(char**)b );
 }
 /*--------------------------------------------------------------------------*/
 static int cmpPos(char *str1, char *str2)
