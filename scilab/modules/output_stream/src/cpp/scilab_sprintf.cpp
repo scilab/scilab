64ce56bf094378bd7187aedb5d9d5b9d9f452c57
Antoine ELIAS <antoine.elias@scilab-enterprises.com>
2014-12-11 11:55:12 +0100

    improve sprintf on %s and %c
    test_run("output_stream","sprintf",["no_check_error_output" ]);
    test_run("output_stream","msprintf",["no_check_error_output" ]);
    
    Change-Id: I0b28d8e8df38e63131eaab8c082e542ef87355f2

--- scilab/modules/output_stream/src/cpp/scilab_sprintf.cpp
+++ scilab/modules/output_stream/src/cpp/scilab_sprintf.cpp
@@ -13,7 +13,7 @@
  */
 
 #include <stdio.h>
-
+#include <cmath>
 #include "types.hxx"
 #include "double.hxx"
 #include "string.hxx"
@@ -28,6 +28,8 @@ extern "C"
 #include "localization.h"
 #include "charEncoding.h"
 #include "os_wcsdup.h"
+#include "os_wtoi.h"
+#include "os_swprintf.h"
 }
 
 #define NanString L"Nan"
@@ -35,15 +37,16 @@ extern "C"
 #define NegInfString L"-Inf"
 
 static wchar_t* replaceAndCountLines(const wchar_t* _pwstInput, int* _piLines, int* _piNewLine);
-
+static wchar_t* addl(TokenDef* token);
+static void updatel(TokenDef* token);
 wchar_t** scilab_sprintf(const char* _pstName, const wchar_t* _pwstInput, typed_list &in, ArgumentPosition* _pArgs, int _iArgsCount, int* _piOutputRows, int* _piNewLine)
 {
     /* Force Windows display to have two-digit exponent. */
 #ifdef _MSC_VER
     _set_output_format(_TWO_DIGIT_EXPONENT);
 #endif
-    wchar_t** pwstOutput        = NULL;
-    wchar_t* pwstFirstOutput    = NULL;
+    wchar_t** pwstOutput = NULL;
+    wchar_t* pwstFirstOutput = NULL;
     *_piNewLine = 0;
     size_t pos = 0;
 
@@ -62,15 +65,15 @@ wchar_t** scilab_sprintf(const char* _pstName, const wchar_t* _pwstInput, typed_
         //pwstToken[1] : "%s bla2 "
         //pwstToken[2] : "%d bla3"
 
-        size_t iStart   = 0;
-        size_t iEnd     = 0;
-        int iToken      = 0;
-        int iPosArg     = 0;
+        size_t iStart = 0;
+        size_t iEnd = 0;
+        int iToken = 0;
+        int iPosArg = 0;
 
         TokenDef* pToken = new TokenDef[_iArgsCount + 1];
-        wchar_t* pwstStart  = pwstFirstOutput;
+        wchar_t* pwstStart = pwstFirstOutput;
 
-        bool bFinish         = false;
+        bool bFinish = false;
         bool bPercentPercent = false;
 
         while (!bFinish)
@@ -87,7 +90,7 @@ wchar_t** scilab_sprintf(const char* _pstName, const wchar_t* _pwstInput, typed_
                     if (pwstEnd == NULL)
                     {
                         //end of string
-                        iEnd    = wcslen(pwstFirstOutput);
+                        iEnd = wcslen(pwstFirstOutput);
                         bFinish = true;
                     }
                     else
@@ -107,7 +110,7 @@ wchar_t** scilab_sprintf(const char* _pstName, const wchar_t* _pwstInput, typed_
             else
             {
                 //end of string
-                iEnd    = wcslen(pwstFirstOutput);
+                iEnd = wcslen(pwstFirstOutput);
                 bFinish = true;
             }
 
@@ -134,6 +137,7 @@ wchar_t** scilab_sprintf(const char* _pstName, const wchar_t* _pwstInput, typed_
                 }
 
                 //looking for width
+                pToken[iToken].width = -1;
                 if (*(pwstPercent + 1) == L'*')
                 {
                     pwstPercent++;
@@ -141,19 +145,29 @@ wchar_t** scilab_sprintf(const char* _pstName, const wchar_t* _pwstInput, typed_
                 else
                 {
                     //number
-                    while (iswdigit(*(pwstPercent + 1)))
+                    if (iswdigit(*(pwstPercent + 1)))
                     {
-                        pwstPercent++;
+                        pToken[iToken].width = os_wtoi(pwstPercent + 1);
+                        while (iswdigit(*(pwstPercent + 1)))
+                        {
+                            pwstPercent++;
+                        }
                     }
                 }
 
                 //looking for precision
+                pToken[iToken].prec = -1;
                 if (*(pwstPercent + 1) == L'.')
                 {
                     pwstPercent++;
-                    while (iswdigit(*(pwstPercent + 1)))
+
+                    if (iswdigit(*(pwstPercent + 1)))
                     {
-                        pwstPercent++;
+                        pToken[iToken].prec = os_wtoi(pwstPercent + 1);
+                        while (iswdigit(*(pwstPercent + 1)))
+                        {
+                            pwstPercent++;
+                        }
                     }
                 }
 
@@ -171,6 +185,7 @@ wchar_t** scilab_sprintf(const char* _pstName, const wchar_t* _pwstInput, typed_
                 }
 
                 wchar_t wcType = *(pwstPercent + 1);
+                pToken[iToken].typePos = (pwstPercent + 1) - pToken[iToken].pwstToken;
                 switch (wcType)
                 {
                     case L'i' : //integer
@@ -186,15 +201,7 @@ wchar_t** scilab_sprintf(const char* _pstName, const wchar_t* _pwstInput, typed_
                             return NULL;
                         }
 
-                        if (finite(in[iPosArg + 1]->getAs<types::Double>()->get(0)) == false)
-                        {
-                            pToken[iToken].outputType = InternalType::ScilabString;
-                            *(pwstPercent + 1) = L's';
-                        }
-                        else
-                        {
-                            pToken[iToken].outputType = InternalType::ScilabInt32;
-                        }
+                        pToken[iToken].outputType = InternalType::ScilabInt32;
 
                         iPosArg++;
                         break;
@@ -210,15 +217,7 @@ wchar_t** scilab_sprintf(const char* _pstName, const wchar_t* _pwstInput, typed_
                             return NULL;
                         }
 
-                        if (finite(in[iPosArg + 1]->getAs<types::Double>()->get(0)) == false)
-                        {
-                            pToken[iToken].outputType = InternalType::ScilabString;
-                            *(pwstPercent + 1) = L's';
-                        }
-                        else
-                        {
-                            pToken[iToken].outputType = InternalType::ScilabDouble;
-                        }
+                        pToken[iToken].outputType = InternalType::ScilabDouble;
                         iPosArg++;
                         break;
                     case L's' :
@@ -228,6 +227,11 @@ wchar_t** scilab_sprintf(const char* _pstName, const wchar_t* _pwstInput, typed_
                             Scierror(999, _("%s: Wrong number of input arguments: data doesn't fit with format.\n"), _pstName);
                             *_piOutputRows = 0;
                             return NULL;
+                        }
+
+                        if (pToken[iToken].bLengthFlag == false)
+                        {
+                            updatel(&(pToken[iToken]));
                         }
 
                         pToken[iToken].outputType = InternalType::ScilabString;
@@ -281,7 +285,29 @@ wchar_t** scilab_sprintf(const char* _pstName, const wchar_t* _pwstInput, typed_
                 {
                     wchar_t pwstTemp[bsiz];
                     double dblVal = in[_pArgs[iPosArg].iArg]->getAs<Double>()->get(j, _pArgs[iPosArg].iPos);
-                    swprintf(pwstTemp, bsiz, pToken[i].pwstToken, dblVal);
+
+                    if (finite(dblVal))
+                    {
+                        os_swprintf(pwstTemp, bsiz, pToken[i].pwstToken, dblVal);
+                    }
+                    else
+                    {
+                        wchar_t* newToken = addl(&pToken[i]);
+                        if (ISNAN(dblVal))
+                        {
+                            os_swprintf(pwstTemp, bsiz, newToken, NanString);
+                        }
+                        else if (std::signbit(dblVal))
+                        {
+                            os_swprintf(pwstTemp, bsiz, newToken, NegInfString);
+                        }
+                        else
+                        {
+                            os_swprintf(pwstTemp, bsiz, newToken, InfString);
+                        }
+
+                        delete[] newToken;
+                    }
                     iPosArg++;
                     oFirstOutput << pwstTemp;
                 }
@@ -289,7 +315,7 @@ wchar_t** scilab_sprintf(const char* _pstName, const wchar_t* _pwstInput, typed_
                 {
                     wchar_t pwstTemp[bsiz];
                     double dblVal = in[_pArgs[iPosArg].iArg]->getAs<Double>()->get(j, _pArgs[iPosArg].iPos);
-                    swprintf(pwstTemp, bsiz, pToken[i].pwstToken, (int)dblVal);
+                    os_swprintf(pwstTemp, bsiz, pToken[i].pwstToken, (int)dblVal);
                     iPosArg++;
                     oFirstOutput << pwstTemp;
                 }
@@ -320,21 +346,44 @@ wchar_t** scilab_sprintf(const char* _pstName, const wchar_t* _pwstInput, typed_
                     int posC = (int)wcscspn(pToken[i].pwstToken, L"c");
                     int posS = (int)wcscspn(pToken[i].pwstToken, L"s");
 
-                    if (!posS || !posC)
+                    if (posS == 0 || posC == 0)
                     {
                         *_piOutputRows = 0;
                         return NULL;
                     }
 
-                    if (posC < posS)
+                    bool bC = posC < posS;
+                    int len = 1;
+                    if (pToken[i].prec == -1)
                     {
-                        oFirstOutput << pwstStr[0];
+                        if (bC == false)
+                        {
+                            len = (int)wcslen(pwstStr);
+                        }
                     }
-                    else //'s'
+                    else
                     {
-                        oFirstOutput << pwstStr;
+                        if (bC == false)
+                        {
+                            len = std::min(pToken[i].prec, (int)wcslen(pwstStr));
+                        }
                     }
 
+                    len += (int)wcslen(pToken[i].pwstToken);
+                    len = std::max(len, pToken[i].width);
+                    wchar_t* pwstTemp = (wchar_t*)MALLOC((len + 1) * sizeof(wchar_t));
+
+                    if (bC)
+                    {
+                        os_swprintf(pwstTemp, len + 1, pToken[i].pwstToken, pwstStr[0]);
+                    }
+                    else
+                    {
+                        os_swprintf(pwstTemp, len + 1, pToken[i].pwstToken, pwstStr);
+                    }
+
+                    oFirstOutput << pwstTemp;
+                    FREE(pwstTemp);
                     iPosArg++;
                 }
                 else
@@ -346,7 +395,6 @@ wchar_t** scilab_sprintf(const char* _pstName, const wchar_t* _pwstInput, typed_
         }
 
         pwstFirstOutput = os_wcsdup((wchar_t*)oFirstOutput.str().c_str());
-
     }
 
     pwstOutput = (wchar_t**)MALLOC((*_piOutputRows) * sizeof(wchar_t*));
@@ -446,3 +494,25 @@ static wchar_t* replaceAndCountLines(const wchar_t* _pwstInput, int* _piLines, i
     return pwstFirstOutput;
 }
 /*--------------------------------------------------------------------------*/
+wchar_t* addl(TokenDef* token)
+{
+    //replace %s or %c by %ls or %lc to wide char compatibility
+    int iPos = token->typePos;
+    int sizeTotal = (int)wcslen(token->pwstToken);
+    wchar_t* pwstToken = new wchar_t[sizeTotal + 2];
+
+    wcsncpy(pwstToken, token->pwstToken, iPos);
+    pwstToken[iPos] = L'l';
+    pwstToken[iPos + 1] = L's';
+    wcsncpy(&pwstToken[iPos + 2], token->pwstToken + iPos + 1, sizeTotal - (iPos + 1));
+    pwstToken[sizeTotal + 1]  = L'\0';
+
+    return pwstToken;
+}
+/*--------------------------------------------------------------------------*/
+void updatel(TokenDef* token)
+{
+    wchar_t* newToken = addl(token);
+    delete[] token->pwstToken;
+    token->pwstToken = newToken;
+}
