641491fe19feb5fb04f4b11d6489b8128c0fd106
Anais AUBERT <anais.aubert@scilab-enterprises.com>
2015-02-04 14:35:28 +0100

    fix mgetl messages
    test_run("fileio","bug_3427",["no_check_error_output" ]);
    
    Change-Id: Ieed64a1de23397482a0b9237928d94c3156023e1

--- scilab/modules/fileio/sci_gateway/cpp/sci_mgetl.cpp
+++ scilab/modules/fileio/sci_gateway/cpp/sci_mgetl.cpp
@@ -51,13 +51,13 @@ Function::ReturnValue sci_mgetl(typed_list &in, int _iRetCount, typed_list &out)
         //number of lines
         if (in[1]->isDouble() == false)
         {
-            Scierror(999, _("%s: Wrong type for input argument #%d: Integer expected.\n"), "mgetl", 2);
+            Scierror(999, _("%s: Wrong type for input argument #%d: An integer value expected.\n"), "mgetl", 2);
             return Function::Error;
         }
 
         if (in[1]->getAs<Double>()->isScalar() == false)
         {
-            Scierror(999, _("%s: Wrong size for input argument #%d: Integer expected.\n"), "mgetl", 2);
+            Scierror(999, _("%s: Wrong size for input argument #%d: An integer value expected.\n"), "mgetl", 2);
             return Function::Error;
         }
         iLinesExcepted = static_cast<int>(in[1]->getAs<Double>()->get(0));
