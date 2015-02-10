a4b13ecd94d5a479be5c548c912fb0475745174a
Vincent COUVERT <vincent.couvert@scilab-enterprises.com>
2015-02-10 15:57:05 +0100

    Test if file exists before trying to load it (to avoid messages in stderr).
    Update SCI/modules/m2sci/tests/nonreg_tests/bug_683.tst for Scilab 6 (test case for load modification).
    
    To test:
    test_run m2sci bug_683
    exec SCI/modules/development_tools/tests/nonreg_tests/bug_5149.tst
    
    Change-Id: Icdabca942ec031c82cad8b25373dbcf814a6c85c

--- scilab/modules/io/sci_gateway/cpp/sci_load.cpp
+++ scilab/modules/io/sci_gateway/cpp/sci_load.cpp
@@ -1,14 +1,14 @@
 /*
-* Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
-* Copyright (C) 2014 - Scilab Enterprises - Antoine ELIAS
-*
-* This file must be used under the terms of the CeCILL.
-* This source file is licensed as described in the file COPYING, which
-* you should have received as part of this distribution.  The terms
-* are also available at
-* http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
-*
-*/
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2014 - Scilab Enterprises - Antoine ELIAS
+ *
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at
+ * http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *
+ */
 
 #include <string.h>
 #include "api_scilab.hxx"
@@ -20,11 +20,13 @@
 #include "library.hxx"
 #include "loadlib.hxx"
 
+
 extern "C"
 {
 #include "sci_malloc.h"
 #include "expandPathVariable.h"
 #include "h5_fileManagement.h"
+#include "FileExist.h"
 }
 
 using namespace types;
@@ -50,32 +52,41 @@ Function::ReturnValue sci_load(types::typed_list &in, int _iRetCount, types::typ
 
     wchar_t* pwstPathLib = expandPathVariableW(pS->get(0));
     char* pstPath = wide_string_to_UTF8(pwstPathLib);
-
-    if (isHDF5File(pstPath))
+    if (FileExist(pstPath))
     {
-        FREE(pstPath);
-        FREE(pwstPathLib);
+        if (isHDF5File(pstPath))
+        {
+            FREE(pstPath);
+            FREE(pwstPathLib);
 
-        //call overload
-        std::wstring wstFuncName = L"%_sodload";
-        ast::ExecVisitor* exec = new ast::ExecVisitor();
-        Callable::ReturnValue Ret = Callable::Error;
-        Ret = Overload::call(wstFuncName, in, _iRetCount, out, exec);
-        delete exec;
-        return Ret;
+            //call overload
+            std::wstring wstFuncName = L"%_sodload";
+            ast::ExecVisitor* exec = new ast::ExecVisitor();
+            Callable::ReturnValue Ret = Callable::Error;
+            Ret = Overload::call(wstFuncName, in, _iRetCount, out, exec);
+            delete exec;
+            return Ret;
+        }
+        else
+        {
+            Library* lib = loadlib(pS->get(0));
+            FREE(pstPath);
+
+            if (lib == NULL)
+            {
+                Scierror(999, _("%s: Wrong file type \"%ls\".\n"), "load", pwstPathLib);
+                FREE(pwstPathLib);
+                return Function::Error;
+            }
+            FREE(pwstPathLib);
+        }
     }
     else
     {
-        Library* lib = loadlib(pS->get(0));
+        Scierror(999, _("%s: Unable to open file: \"%ls\".\n"), "load", pwstPathLib);
         FREE(pstPath);
-
-        if (lib == NULL)
-        {
-            Scierror(999, _("%s: Wrong file type \"%ls\".\n"), "load", pwstPathLib);
-            FREE(pwstPathLib);
-            return Function::Error;
-        }
         FREE(pwstPathLib);
+        return Function::Error;
     }
 
     return Function::OK;
