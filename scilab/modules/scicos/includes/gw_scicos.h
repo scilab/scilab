aaebd44372fa0c8813ce94d364c56440ffe30504
Paul Bignier <paul.bignier@scilab-enterprises.com>
2015-03-03 14:32:48 +0100

    Xcos gateways: rewrite sci_coserror to C++
    Change-Id: Ie900d5fbd193937baba608f6a6df2500535d4a7c

--- scilab/modules/scicos/includes/gw_scicos.h
+++ scilab/modules/scicos/includes/gw_scicos.h
@@ -41,7 +41,6 @@ SCICOS_IMPEXP int sci_ftree4(char *fname, void* pvApiCtx);
 SCICOS_IMPEXP int sci_diffobjs(char *fname, void* pvApiCtx);
 SCICOS_IMPEXP int sci_set_xproperty(char *fname, void* pvApiCtx);
 SCICOS_IMPEXP int sci_getscicosvars(char *fname, void* pvApiCtx);
-SCICOS_IMPEXP int sci_coserror(char *fname, void* pvApiCtx);
 
 /* it was in a separate gateway */
 SCICOS_IMPEXP int sci_data2sig(char *fname, void* pvApiCtx);
