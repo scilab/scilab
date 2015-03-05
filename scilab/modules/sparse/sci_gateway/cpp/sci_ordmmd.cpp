87db25f1dfdea90ea1377f0c35d85ebb962f247e
Anais AUBERT <anais.aubert@scilab-enterprises.com>
2015-03-05 13:15:44 +0100

    fix bug 10271
    test_run("sparse","bug_10271",["no_check_error_output" ]);
    
    Change-Id: Id725cc19487ff435208103290e9a2f951d12a881

--- scilab/modules/sparse/sci_gateway/cpp/sci_ordmmd.cpp
+++ scilab/modules/sparse/sci_gateway/cpp/sci_ordmmd.cpp
@@ -63,7 +63,6 @@ Function::ReturnValue sci_ordmmd(typed_list &in, int _iRetCount, typed_list &out
     Double* pdbl2 = in[1]->getAs<Double>();
     pdbl2->convertToInteger();
     int* pADJNCY = (int*)pdbl2->get();
-
     //get argument #3
     Double* pdbl3 = in[2]->getAs<Double>();
     if (in[2]->isDouble() == false || pdbl3->getSize() != 1)
@@ -73,6 +72,12 @@ Function::ReturnValue sci_ordmmd(typed_list &in, int _iRetCount, typed_list &out
     }
 
     int NEQNS = (int)pdbl3->get(0);
+    if (NEQNS != pdbl1->getSize())
+    {
+
+        Scierror(999, _(" The provided \"n\" does not correspond to the matrix defined by xadj and iadj\n"));
+        return Function::Error;
+    }
 
     Double* pdbl4 = new Double(NEQNS, 1);
     pdbl4->convertToInteger();
@@ -86,7 +91,6 @@ Function::ReturnValue sci_ordmmd(typed_list &in, int _iRetCount, typed_list &out
     Double* pdbl7 = new Double(4 * NEQNS, 1);
     pdbl7->convertToInteger();
     int iSize = 4 * NEQNS;
-
     int iFlag = 0;
     C2F(ordmmd)(&NEQNS, (int*)pdbl1->get(), (int*)pdbl2->get(), (int*)pdbl5->get(),
                 (int*)pdbl4->get(), &iSize, (int*)pdbl7->get(), (int*)pdbl6->get(), &iFlag);
