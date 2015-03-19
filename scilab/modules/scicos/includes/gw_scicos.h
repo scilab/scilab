73d85d43706ac35ae2e8b8b2fa49f3879209dc43
Paul Bignier <paul.bignier@scilab-enterprises.com>
2015-03-19 11:53:56 +0100

    Xcos gateways: rewrite getscicosvars to C++
    Change-Id: I99a8dc056bdd0075a3d95dcab3972d37ec5a318e

--- scilab/modules/scicos/includes/gw_scicos.h
+++ scilab/modules/scicos/includes/gw_scicos.h
@@ -38,7 +38,6 @@ SCICOS_IMPEXP int sci_ftree3(char *fname, void* pvApiCtx);
 SCICOS_IMPEXP int sci_ftree4(char *fname, void* pvApiCtx);
 
 SCICOS_IMPEXP int sci_diffobjs(char *fname, void* pvApiCtx);
-SCICOS_IMPEXP int sci_getscicosvars(char *fname, void* pvApiCtx);
 
 /* it was in a separate gateway */
 //SCICOS_IMPEXP int sci_model2blk(char *fname, void* pvApiCtx);
