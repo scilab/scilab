6fa24fa8ce00d00305345fcaaae8e3dda7850019
Antoine ELIAS <antoine.elias@scilab-enterprises.com>
2015-03-23 17:58:27 +0100

    fix f2c solution
    Change-Id: Ibeaaab5680658d2b94d80979f5cdd725a618147f

--- scilab/modules/scicos/sci_gateway/fortran/sci_f_tree2.f
+++ scilab/modules/scicos/sci_gateway/fortran/sci_f_tree2.f
@@ -20,34 +20,34 @@ c     See the file ./license.txt
 c 
 	subroutine  intree2(fname)
 c
-      include 'stack.h'
-      logical checklhs,checkrhs,putlhsvar
-      logical createvar,getrhsvar
-      character*(*) fname
-      
-      integer iadr
-      
-      iadr(l)=l+l-1
-
-c	
-	if(.not.checklhs(fname,2,2)) return
-      if(.not.checkrhs(fname,4,4)) return
-      if(.not.getrhsvar(1,'i',nvec,mvec,ipvec)) return
-      if(.not.getrhsvar(2,'i',noin,moin,ipoin)) return
-      if(.not.getrhsvar(3,'i',noinr,moinr,ipoinr)) return
-      if(.not.getrhsvar(4,'i',ndep,mdep,ipdep)) return
-c
-      if(.not.createvar(5,'i',nvec*mvec,1,ipord)) return
-      if(.not.createvar(6,'i',1,1,ipok)) return
-c
-      call ftree2(istk(ipvec),nvec*mvec,istk(ipdep),
-     $     istk(ipoin),istk(ipoinr),istk(ipord),
-     $     nord,istk(ipok))
-      lhsvar(1)=5
-      lhsvar(2)=6
-c      nbrows(5)=nord
-      istk(iadr(iwhere(5))+1)=nord
-      if(.not.putlhsvar())return
+!      include 'stack.h'
+!      logical checklhs,checkrhs,putlhsvar
+!      logical createvar,getrhsvar
+!      character*(*) fname
+!      
+!      integer iadr
+!      
+!      iadr(l)=l+l-1
+!
+!c	
+!	if(.not.checklhs(fname,2,2)) return
+!      if(.not.checkrhs(fname,4,4)) return
+!      if(.not.getrhsvar(1,'i',nvec,mvec,ipvec)) return
+!      if(.not.getrhsvar(2,'i',noin,moin,ipoin)) return
+!      if(.not.getrhsvar(3,'i',noinr,moinr,ipoinr)) return
+!      if(.not.getrhsvar(4,'i',ndep,mdep,ipdep)) return
+!c
+!      if(.not.createvar(5,'i',nvec*mvec,1,ipord)) return
+!      if(.not.createvar(6,'i',1,1,ipok)) return
+!c
+!      call ftree2(istk(ipvec),nvec*mvec,istk(ipdep),
+!     $     istk(ipoin),istk(ipoinr),istk(ipord),
+!     $     nord,istk(ipok))
+!      lhsvar(1)=5
+!      lhsvar(2)=6
+!c      nbrows(5)=nord
+!      istk(iadr(iwhere(5))+1)=nord
+!      if(.not.putlhsvar())return
       end
 c     ********************
 			
--- scilab/modules/scicos/sci_gateway/fortran/sci_f_tree3.f
+++ scilab/modules/scicos/sci_gateway/fortran/sci_f_tree3.f
@@ -21,37 +21,37 @@ c
 	subroutine  intree3(fname)
 c
 c			
-      include 'stack.h'
-      logical checklhs,checkrhs,putlhsvar
-      logical createvar,getrhsvar
-      
-      character*(*) fname
-      integer iadr
-      
-      iadr(l)=l+l-1
-      
-      if(.not.checklhs(fname,2,2)) return
-      if(.not.checkrhs(fname,7,7)) return
-      if(.not.getrhsvar(1,'i',nvec,mvec,ipvec)) return
-      if(.not.getrhsvar(2,'i',ndep,mdep,ipdep)) return
-      if(.not.getrhsvar(3,'i',ntyp,mtyp,iptyp)) return
-      if(.not.getrhsvar(4,'i',nbex,mbex,ipbex)) return
-      if(.not.getrhsvar(5,'i',nbop,mbop,ipbop)) return
-      if(.not.getrhsvar(6,'i',nbln,mbln,ipbln)) return
-      if(.not.getrhsvar(7,'i',nblr,mblr,ipblr)) return
-c
-      if(.not.createvar(8,'i',nvec*mvec,1,ipord)) return
-      if(.not.createvar(9,'i',1,1,ipok)) return
-      if(.not.createvar(10,'i',1,nvec*mvec,ipkk)) return
-c
-      call ftree3(istk(ipvec),nvec*mvec,istk(ipdep),
-     $     istk(iptyp),istk(ipbex),istk(ipbop),istk(ipbln),
-     $     istk(ipblr),istk(ipkk),istk(ipord),nord,
-     $     istk(ipok))
-      lhsvar(1)=8
-      lhsvar(2)=9
-      istk(iadr(iwhere(8))+1)=nord
-      if(.not.putlhsvar())return
+!      include 'stack.h'
+!      logical checklhs,checkrhs,putlhsvar
+!      logical createvar,getrhsvar
+!      
+!      character*(*) fname
+!      integer iadr
+!      
+!      iadr(l)=l+l-1
+!      
+!      if(.not.checklhs(fname,2,2)) return
+!      if(.not.checkrhs(fname,7,7)) return
+!      if(.not.getrhsvar(1,'i',nvec,mvec,ipvec)) return
+!      if(.not.getrhsvar(2,'i',ndep,mdep,ipdep)) return
+!      if(.not.getrhsvar(3,'i',ntyp,mtyp,iptyp)) return
+!      if(.not.getrhsvar(4,'i',nbex,mbex,ipbex)) return
+!      if(.not.getrhsvar(5,'i',nbop,mbop,ipbop)) return
+!      if(.not.getrhsvar(6,'i',nbln,mbln,ipbln)) return
+!      if(.not.getrhsvar(7,'i',nblr,mblr,ipblr)) return
+!c
+!      if(.not.createvar(8,'i',nvec*mvec,1,ipord)) return
+!      if(.not.createvar(9,'i',1,1,ipok)) return
+!      if(.not.createvar(10,'i',1,nvec*mvec,ipkk)) return
+!c
+!      call ftree3(istk(ipvec),nvec*mvec,istk(ipdep),
+!     $     istk(iptyp),istk(ipbex),istk(ipbop),istk(ipbln),
+!     $     istk(ipblr),istk(ipkk),istk(ipord),nord,
+!     $     istk(ipok))
+!      lhsvar(1)=8
+!      lhsvar(2)=9
+!      istk(iadr(iwhere(8))+1)=nord
+!      if(.not.putlhsvar())return
       end
 c     ********************
  
