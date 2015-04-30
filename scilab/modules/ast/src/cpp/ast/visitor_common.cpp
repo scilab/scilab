144ad289e0e37747ace06f6dd2bb732382a4c611
Vincent COUVERT <vincent.couvert@scilab-enterprises.com>
2015-04-30 14:52:02 +0200

    Fix double-free after https://codereview.scilab.org/#/c/16391/ Test: test_run ast insert
    Change-Id: I2ab4929ad666c6e6a7d59e1e7633f874122e116a

--- scilab/modules/ast/src/cpp/ast/visitor_common.cpp
+++ scilab/modules/ast/src/cpp/ast/visitor_common.cpp
@@ -1679,7 +1679,6 @@ InternalType* evaluateFields(const ast::Exp* _pExp, std::list<ExpHistory*>& fiel
             delete *i;
         }
 
-        _pAssignValue->killMe();
         throw error;
     }
 }
