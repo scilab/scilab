61ab4ee87e994e89d714c0253ea43f379cc800de
Marcos CARDINOT <mcardinot@gmail.com>
2015-05-30 16:00:44 -0300

    ZoomOutAction - implements registerKeyAction() (ctrl -)
    Change-Id: I0b83923e7a8d27d365399d7f0929f2c3edcacb18

--- scilab/modules/xcos/src/java/org/scilab/modules/xcos/palette/actions/ZoomOutAction.java
+++ scilab/modules/xcos/src/java/org/scilab/modules/xcos/palette/actions/ZoomOutAction.java
@@ -12,11 +12,19 @@
 
 package org.scilab.modules.xcos.palette.actions;
 
+import static org.scilab.modules.commons.OS.MAC;
+
+import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
 import java.lang.reflect.InvocationTargetException;
 
+import javax.swing.ActionMap;
 import javax.swing.ImageIcon;
+import javax.swing.InputMap;
 import javax.swing.JButton;
+import javax.swing.KeyStroke;
 
+import org.scilab.modules.commons.OS;
 import org.scilab.modules.commons.gui.FindIconHelper;
 import org.scilab.modules.commons.gui.ScilabLAF;
 import org.scilab.modules.graph.utils.ScilabGraphMessages;
@@ -34,11 +42,59 @@ public class ZoomOutAction extends CommonCallBack {
     private static final String LABEL = ScilabGraphMessages.ZOOM_OUT;
     private static final String ICON = FindIconHelper.findIcon("zoom-out");
 
+    private static final int ACCELERATOR_KEY = Toolkit.getDefaultToolkit().getMenuShortcutKeyMask();
+    private static final String ZOOM_OUT = "ZoomOut";
+
     /**
      * Constructor
      */
     public ZoomOutAction() {
         super("");
+    }
+
+    /**
+     * Register the key for the action
+     * @param view PaletteManagerPanel instance
+     */
+    public static void registerKeyAction(PaletteManagerView view) {
+        // Multi-shortcut action
+        ActionMap[] ams = new ActionMap[] {
+                view.getTree().getActionMap(),
+                view.getPanel().getActionMap()
+        };
+        InputMap[] maps = new InputMap[] {
+                view.getTree().getInputMap(),
+                view.getPanel().getInputMap()
+        };
+
+        // register the action to a unique action keyword
+        for (ActionMap am : ams) {
+            am.put(ZOOM_OUT, new ZoomOutAction());
+        }
+
+        // add custom key stroke for this action
+        final KeyStroke[] keystrokes;
+        if (OS.get() == MAC) {
+            // AZERTY for Mac has a non-supported classic layout
+            keystrokes = new KeyStroke[] {
+                KeyStroke.getKeyStroke('=', ACCELERATOR_KEY),
+                KeyStroke.getKeyStroke('=', ACCELERATOR_KEY | KeyEvent.SHIFT_DOWN_MASK)
+            };
+        } else {
+            keystrokes = new KeyStroke[] {
+                KeyStroke.getKeyStroke('-', ACCELERATOR_KEY),
+                KeyStroke.getKeyStroke('-', ACCELERATOR_KEY | KeyEvent.SHIFT_DOWN_MASK),
+                KeyStroke.getKeyStroke('_', ACCELERATOR_KEY),
+                KeyStroke.getKeyStroke('_', ACCELERATOR_KEY | KeyEvent.SHIFT_DOWN_MASK),
+                KeyStroke.getKeyStroke(KeyEvent.VK_SUBTRACT, ACCELERATOR_KEY)
+            };
+        }
+
+        for (InputMap map : maps) {
+            for (KeyStroke k : keystrokes) {
+                map.put(k, ZOOM_OUT);
+            }
+        }
     }
 
     /**
