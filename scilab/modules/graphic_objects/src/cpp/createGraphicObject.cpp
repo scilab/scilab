85770ade73a5e53909225a075d5c2383ec273b85
Antoine ELIAS <antoine.elias@scilab-enterprises.com>
2015-06-04 14:00:32 +0200

    increase user_data reference on gdf copy
    Change-Id: Id2ec16ab46121697543c9b2f18c15d004fd1459a

--- scilab/modules/graphic_objects/src/cpp/createGraphicObject.cpp
+++ scilab/modules/graphic_objects/src/cpp/createGraphicObject.cpp
@@ -19,6 +19,7 @@ extern "C"
 #include "FigureModel.h"
 #include "AxesModel.h"
 #include "CurrentSubwin.h"
+#include "api_scilab.h"
 }
 
 #include "CallGraphicController.hxx"
@@ -94,7 +95,11 @@ int createNewFigureWithAxes()
     if (iUserDataSize != 0)
     {
         int* pUserData = NULL;
+
         getGraphicObjectProperty(getFigureModel(), __GO_USER_DATA__, jni_int_vector, (void**)&pUserData);
+
+        increaseValRef(NULL, (int*)*pUserData);
+
         setGraphicObjectProperty(id, __GO_USER_DATA__, pUserData, jni_int_vector, iUserDataSize);
     }
 
