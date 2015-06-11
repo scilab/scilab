58307a3f16ef1c557f9c0c83ceb14be0c2a5d767
Juergen KOCH <juergen.koch@hs-esslingen.de>
2015-06-11 17:43:42 +0200

    minor changes in Polyline.java
    Change-Id: I36b41977e0743336ffaa6035452e2c996aa62548

--- scilab/modules/graphic_objects/src/java/org/scilab/modules/graphic_objects/polyline/Polyline.java
+++ scilab/modules/graphic_objects/src/java/org/scilab/modules/graphic_objects/polyline/Polyline.java
@@ -245,6 +245,9 @@ public class Polyline extends ClippableContouredObject {
                     case DATATIPS:
                         setDatatips((Integer[]) value);
                         break;
+                    case DATATIPSCOUNT:
+                    	// nothing should be done
+                        break;
                     case TIP_DISPLAY_FNC:
                         setDisplayFunction((String) value);
                         break;
@@ -435,14 +438,6 @@ public class Polyline extends ClippableContouredObject {
      */
     public Integer[] getDatatips() {
         return datatips.toArray(new Integer[datatips.size()]);
-    }
-
-    /**
-     * @param datatips the datatips to set
-     */
-    private UpdateStatus setDatatips(List<Integer> datatips) {
-        this.datatips = datatips;
-        return UpdateStatus.Success;
     }
 
     /**
