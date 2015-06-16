77885efd7bd3622ff86db94d3076148f0dc4a2a5
Vincent COUVERT <vincent.couvert@scilab-enterprises.com>
2015-06-16 15:26:48 +0200

    * Bug #13932 fixed - macrovar was broken in YaSp branch. Tests added in macrovar.tst: test_run functions macrovar
    Change-Id: I6e8e81d87874745d04162a861f8e24f351a697ce

--- scilab/modules/ast/src/cpp/ast/macrovarvisitor.cpp
+++ scilab/modules/ast/src/cpp/ast/macrovarvisitor.cpp
@@ -18,15 +18,41 @@ using namespace ast;
 using namespace symbol;
 namespace ast
 {
+
+void MacrovarVisitor::visit (const MatrixLineExp &e)
+{
+    exps_t columns = e.getColumns();
+    for (exps_t::const_iterator it = columns.begin(), itEnd = columns.end(); it != itEnd ; ++it)
+    {
+        (*it)->accept (*this);
+        add();
+    }
+}
+
 void MacrovarVisitor::visit (const SimpleVar &e)
 {
     m_current = e.getSymbol().getName();
 }
 
+void MacrovarVisitor::visit(const OpExp &e)
+{
+    e.getLeft().accept(*this);
+    add();
+    e.getRight().accept(*this);
+    add();
+}
+
 void MacrovarVisitor::visit(const CallExp &e)
 {
     e.getName().accept (*this);
-    add();
+    if (isAssignExpLeftExp)
+    {
+        add(m_local);
+    }
+    else
+    {
+        add();
+    }
 
     exps_t args = e.getArgs();
     for (auto arg : args)
@@ -36,17 +62,68 @@ void MacrovarVisitor::visit(const CallExp &e)
     }
 }
 
+void MacrovarVisitor::visit (const ForExp  &e)
+{
+    e.getVardec().accept(*this);
+    add(m_local);
+    e.getBody().accept (*this);
+}
+
+void MacrovarVisitor::visit (const FieldExp &e)
+{
+    e.getHead()->accept(*this);
+    add(m_local);
+}
+
 void MacrovarVisitor::visit(const AssignExp &e)
 {
+    isAssignExpLeftExp = true;
     e.getLeftExp().getOriginal()->accept (*this);
-    add();
+    isAssignExpLeftExp = false;
+    if (e.getLeftExp().isSimpleVar())
+    {
+        add(m_local);
+    }
+    else
+    {
+        add();
+    }
+
     e.getRightExp().getOriginal()->accept (*this);
     add();
 }
 
+void MacrovarVisitor::visit (const AssignListExp  &e)
+{
+    isAssignExpLeftExp = true;
+    for (exps_t::const_iterator it = e.getExps().begin (), itEnd = e.getExps().end(); it != itEnd; ++it)
+    {
+        (*it)->accept (*this);
+        if ((*it)->isSimpleVar())
+        {
+            add(m_local);
+        }
+        else
+        {
+            add();
+        }
+    }
+    isAssignExpLeftExp = false;
+}
+
 void MacrovarVisitor::visit (const VarDec  &e)
 {
+    m_current = e.getSymbol().getName();
+    add(m_local);
     e.getInit().getOriginal()->accept(*this);
+    add();
+}
+
+void MacrovarVisitor::visit (const FunctionDec  &e)
+{
+    // Function declared locally but never called
+    // If called then the CallExp visitor does the job
+    m_current = e.getSymbol().getName();
     add(m_local);
 }
 
@@ -93,7 +170,6 @@ void MacrovarVisitor::add(std::list<std::wstring>& lst)
 void MacrovarVisitor::add()
 {
     InternalType* pVar = Context::getInstance()->get(Symbol(m_current));
-
     if (pVar)
     {
         if (pVar->isCallable())
@@ -102,14 +178,9 @@ void MacrovarVisitor::add()
             add(m_called);
             return;
         }
-
-        //symbol already exists and not callable.
-        //it is a external var
-        add(m_external);
-        return;
     }
 
-    add(m_local);
+    add(m_external);
 }
 
 bool MacrovarVisitor::isAlreadyIn(std::list<std::wstring>& lst)
