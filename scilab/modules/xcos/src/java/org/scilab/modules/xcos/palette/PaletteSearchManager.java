d104ee66ee4743f5f1291afa58fdce5a18017ebc
Marcos CARDINOT <mcardinot@gmail.com>
2015-07-16 22:41:00 -0300

    adds a manager class for the palettes search (PaletteSearchManager)
    Change-Id: I8e6f4cc9a0d258699c635d4af85d123fd51a5d39

---
+++ scilab/modules/xcos/src/java/org/scilab/modules/xcos/palette/PaletteSearchManager.java
@@ -0,0 +1,122 @@
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2015 - Marcos CARDINOT
+ *
+ * This file must be used under the terms of the CeCILL.
+ * This source file is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.  The terms
+ * are also available at
+ * http://www.cecill.info/licences/Licence_CeCILL_V2.1-en.txt
+ *
+ */
+
+package org.scilab.modules.xcos.palette;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Hashtable;
+import java.util.List;
+
+import javax.swing.tree.TreeModel;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+import org.scilab.modules.xcos.palette.model.PaletteBlock;
+import org.scilab.modules.xcos.palette.model.PaletteNode;
+import org.scilab.modules.xcos.palette.model.PreLoaded;
+import org.scilab.modules.xcos.palette.view.PaletteManagerView;
+import org.scilab.modules.xcos.palette.view.PaletteSearchView;
+
+/**
+ * Manager class for the palettes search.
+ * @author Marcos Cardinot <mcardinot@gmail.com>
+ */
+public final class PaletteSearchManager {
+
+    private Analyzer analyzer;
+    private IndexWriter writer;
+    private PaletteIndexer paletteIndexer;
+    private PaletteSearcher paletteSearcher;
+    private PaletteSearchView view;
+    private Hashtable<String, List<PaletteBlock>> ht;
+
+    /** Default constructor */
+    public PaletteSearchManager() {
+        try {
+            Directory ramDir = new RAMDirectory();
+            analyzer = new StandardAnalyzer();
+            IndexWriterConfig config = new IndexWriterConfig(analyzer);
+            config.setOpenMode(OpenMode.CREATE);
+            writer = new IndexWriter(ramDir, config);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        view = new PaletteSearchView();
+        paletteIndexer = new PaletteIndexer(this);
+        paletteSearcher = new PaletteSearcher(this);
+        ht = new Hashtable<String, List<PaletteBlock>>();
+    }
+
+    /**
+     * Refreshes the whole Index
+     */
+    public void refreshIndex() {
+        TreeModel model = PaletteManagerView.get().getTree().getModel();
+        if (model != null) {
+            loadHashTable(model, model.getRoot(), "");
+            paletteIndexer.createIndex(ht);
+        }
+    }
+
+    /**
+     * Look for a block into the Index.
+     * @param query Query
+     */
+    public void search(String query) {
+        PaletteManagerView.get().getPanel().setRightComponent(view);
+        paletteSearcher.search(query);
+    }
+
+    /**
+     * Recursive function that loads a hashtable containing all available blocks.
+     * @param model TreeModel
+     * @param obj Object
+     * @param treePath String
+     */
+    private void loadHashTable(TreeModel model, Object obj, String treePath) {
+        int  cc = model.getChildCount(obj);
+        for (int i = 0; i < cc; ++i) {
+            PaletteNode node = (PaletteNode) model.getChild(obj, i);
+            if (node instanceof PreLoaded) {
+                ht.put(treePath + File.separator + node.getName(),
+                       ((PreLoaded) node).getBlock());
+            } else {
+                loadHashTable(model,
+                              node,
+                              treePath + File.separator + node.toString());
+            }
+        }
+    }
+
+    /**
+     * Get analyzer
+     * @return Analyzer
+     */
+    public Analyzer getAnalyzer() {
+        return analyzer;
+    }
+
+    /**
+     * Get IndexWriter
+     * @return IndexWriter
+     */
+    public IndexWriter getIndexWriter() {
+        return writer;
+    }
+}
