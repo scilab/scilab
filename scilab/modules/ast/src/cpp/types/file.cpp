5d0d98840e7a4542e12c6632808ac4ea84d89fde
Charlotte HECQUET <charlotte.hecquet@scilab-enterprises.com>
2015-08-14 16:15:04 +0200

    * Bug #14058 fixed - Scilab crashed with 'file("close", file())' instruction
    Change-Id: I509d448162250dd36a75a048af62a35b473c5b02

--- scilab/modules/ast/src/cpp/types/file.cpp
+++ scilab/modules/ast/src/cpp/types/file.cpp
@@ -27,7 +27,7 @@ File::File()
     m_iSwap = 0;
     m_pstMode = L"";
     m_iFortranMode = -1; // see clunit.f
-    m_iType = 0; // 1 : fortran open   2 : c open
+    m_iType = 0; // 1 : fortran open   2 : c open   3 : std::err std::out std::in
     m_stFilename = L"";
 }
 
@@ -158,6 +158,9 @@ std::wstring File::getFileTypeAsString()
             break;
         case 2 :
             return L"C";
+            break;
+        case 3 :
+            return L"STD";
             break;
         default :
             return L"Error";
