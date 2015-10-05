957ce01c21949a67ddc4bfe17bf5f01d5c6bf53b
Clément DAVID <clement.david@scilab-enterprises.com>
2015-10-05 10:12:39 +0200

    * Apache xmlgraphics stack upgraded to the latest versions :
      - xmlgraphics-commons 2.0.1
      - Batik 1.8
      - FOP 2.0
    
    Change-Id: I29c091f9f779823a5a6a0a45869532bd7ca33359

--- scilab/modules/graph/src/java/org/scilab/modules/graph/utils/ScilabGraphUtils.java
+++ scilab/modules/graph/src/java/org/scilab/modules/graph/utils/ScilabGraphUtils.java
@@ -28,7 +28,7 @@ import org.apache.batik.bridge.DocumentLoader;
 import org.apache.batik.bridge.GVTBuilder;
 import org.apache.batik.bridge.UserAgent;
 import org.apache.batik.bridge.UserAgentAdapter;
-import org.apache.batik.dom.svg.SAXSVGDocumentFactory;
+import org.apache.batik.anim.dom.SAXSVGDocumentFactory;
 import org.apache.batik.gvt.GraphicsNode;
 import org.apache.batik.util.XMLResourceDescriptor;
 import org.scilab.forge.jlatexmath.ParseException;
