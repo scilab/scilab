1d99720d814dc1ab0db9319618bd8f0081757557
Cedric Delamarre <cedric.delamarre@scilab-enterprises.com>
2016-01-11 14:00:40 +0100

    test fixed about lasterror function name.
    Change-Id: Id8eacf7829d104ecb0b5dafbc409652509a36d86

--- scilab/modules/functions/sci_gateway/cpp/sci_exec.cpp
+++ scilab/modules/functions/sci_gateway/cpp/sci_exec.cpp
@@ -336,6 +336,11 @@ types::Function::ReturnValue sci_exec(types::typed_list &in, int _iRetCount, typ
     }
     catch (const ast::InternalError& ie)
     {
+        if (pMacro && ConfigVariable::getLastErrorFunction() == L"")
+        {
+            ConfigVariable::setLastErrorFunction(pMacro->getName());
+        }
+
         if (bErrCatch == false)
         {
             closeFile(file, iID, wstFile, pExp);
