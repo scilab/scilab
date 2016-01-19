f14b4dc3e59087a88b942d54bbdee0e180a6c798
Cedric Delamarre <cedric.delamarre@scilab-enterprises.com>
2016-01-19 18:30:53 +0100

    [typename] input argument removed.
    test_run core typename
    
    Change-Id: Id5517769ac19d0345f0cd3608f476b2c6837d1ed

--- scilab/modules/core/sci_gateway/cpp/sci_typename.cpp
+++ scilab/modules/core/sci_gateway/cpp/sci_typename.cpp
@@ -32,14 +32,14 @@ types::Function::ReturnValue sci_typename(types::typed_list &in, int _iRetCount,
 {
     int iOne = 1;
     const wchar_t* pstShortTypeName[NB_OF_TYPE] = {L"s", L"p", L"b", L"sp", L"spb", L"msp", L"i", L"h", L"c",
-                                            L"m", L"mc", L"f", L"l", L"tl", L"ml", L"ptr", L"ip", L"fptr"
-                                            };
+                                                   L"m", L"mc", L"f", L"l", L"tl", L"ml", L"ptr", L"ip", L"fptr"
+                                                  };
     double pstShortTypeNum[NB_OF_TYPE] = {1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 128, 129, 130};
 
     /* Check the number of input argument */
-    if (in.size() > 2 || in.size() == 1)
+    if (in.size() != 0)
     {
-        Scierror(77, _("%s: Wrong number of input arguments: %d or %d expected.\n"), "typename" , 0, 2);
+        Scierror(77, _("%s: Wrong number of input arguments: %d expected.\n"), "typename" , 0);
         return types::Function::Error;
     }
 
@@ -66,11 +66,6 @@ types::Function::ReturnValue sci_typename(types::typed_list &in, int _iRetCount,
             pStrOut->set(i, pstShortTypeName[i]);
         }
         out.push_back(pStrOut);
-    }
-    else // in.size() == 2
-    {
-        Scierror(999, _("%s: Insert a new type is not managed.\n"), "typename");
-        return types::Function::Error;
     }
 
     return types::Function::OK;
