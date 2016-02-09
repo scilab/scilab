5eaac9c56ffec6134db8b27cc85feb5be17855c6
Clément DAVID <clement.david@scilab-enterprises.com>
2016-02-09 14:50:55 +0100

    Types: remove a warning about an unused internal helper method
    Change-Id: I55ae7c458f7ba90535f6b43187d146946c0a2502

--- scilab/modules/ast/src/cpp/types/arrayof.cpp
+++ scilab/modules/ast/src/cpp/types/arrayof.cpp
@@ -30,6 +30,21 @@ extern "C"
 namespace types
 {
 
+static int get_max_size(int* _piDims, int _iDims)
+{
+    if (_iDims == 0)
+    {
+        return 0;
+    }
+
+    int iMax = 1;
+    for (int i = 0 ; i < _iDims ; i++)
+    {
+        iMax *= _piDims[i];
+    }
+    return iMax;
+}
+
 template <typename T>
 GenericType* ArrayOf<T>::createEmpty()
 {
