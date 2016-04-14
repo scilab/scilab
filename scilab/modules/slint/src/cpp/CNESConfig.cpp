3a5e317e70b738a122d575932b73725104235f1b
Paul Bignier <paul.bignier@scilab-enterprises.com>
2016-04-14 17:01:56 +0200

    Update CeCILL license where it wasn't done
    Change-Id: I4958f959e4745750ed11c3159ceb990be59bfcb2

--- scilab/modules/slint/src/cpp/CNESConfig.cpp
+++ scilab/modules/slint/src/cpp/CNESConfig.cpp
@@ -6,7 +6,7 @@
  *  This source file is licensed as described in the file COPYING, which
  *  you should have received as part of this distribution.  The terms
  *  are also available at
- *  http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+ *  http://www.cecill.info/licences/Licence_CeCILL_V2.1-en.txt
  *
  */
 
@@ -113,7 +113,7 @@ const std::wstring CNESConfig::getId(const ToolConfigurationType & tct, const An
     else
     {
         return scilab::UTF8::toWide(i->second.getStandardRuleId());
-	}*/
+    }*/
     return scilab::UTF8::toWide(art.getId());
 }
 
