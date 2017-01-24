43826f7707de50a6a1feac5145dde5f23d450ec5
Cedric Delamarre <cedric.delamarre@scilab-enterprises.com>
2017-01-24 16:14:46 +0100

    the return of api_scilab.hxx
    Change-Id: I8310feaf782baddfa7cd8b59b4be03a93ac877a9

---
+++ scilab/modules/api_scilab/includes/api_scilab.hxx
@@ -0,0 +1,32 @@
+/*
+* Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+* Copyright (C) 2014 - Scilab Enterprises - Antoine ELIAS
+* Copyright (C) 2017 - Scilab Enterprises - Cedric Delamarre
+*
+ * Copyright (C) 2012 - 2017 - Scilab Enterprises
+ *
+ * This file is hereby licensed under the terms of the GNU GPL v2.0,
+ * pursuant to article 5.3.4 of the CeCILL v.2.1.
+ * This file was originally licensed under the terms of the CeCILL v2.1,
+ * and continues to be available under such terms.
+ * For more information, see the COPYING file which you should have received
+ * along with this program.
+*/
+
+#ifdef _MSC_VER
+#pragma warning( disable : 4275 )
+#endif
+
+#ifndef __API_SCILAB_HXX__
+#define __API_SCILAB_HXX__
+
+#include "function.hxx"
+
+extern "C"
+{
+#include "Scierror.h"
+#include "localization.h"
+}
+#endif /* ! __API_SCILAB_HXX__ */
+
+
