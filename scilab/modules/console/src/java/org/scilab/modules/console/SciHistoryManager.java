scilab/scilab

commit 3d583cb78d948a3533e59ecb453d13312ef2c217

CoverityFix-CID:1350903,1350902 Checking String equality using == or !=
Change-Id: I96703e73655a073609c683c75da53a191c32362f

scilab/modules/console/src/java/org/scilab/modules/console/SciHistoryManager.java
@@ -72,7 +72,7 @@ public class SciHistoryManager implements HistoryManager {
      */
     public String getNextEntry(String beg) {
         /* Ask Scilab history manager for a matching entry */
-        if (HistoryManagement.getSearchedTokenInScilabHistory() != beg) {
+        if (!HistoryManagement.getSearchedTokenInScilabHistory().equals(beg)) {
             //HistoryManagement.resetSearchedTokenInScilabHistory();
             HistoryManagement.setSearchedTokenInScilabHistory(beg);
         }
@@ -87,7 +87,7 @@ public class SciHistoryManager implements HistoryManager {
      */
     public String getPreviousEntry(String beg) {
         /* Ask Scilab history manager for a matching entry */
-        if (HistoryManagement.getSearchedTokenInScilabHistory() != beg) {
+        if (!HistoryManagement.getSearchedTokenInScilabHistory().equals(beg)) {
             //HistoryManagement.resetSearchedTokenInScilabHistory();
             HistoryManagement.setSearchedTokenInScilabHistory(beg);
         }

commit 9591c2d291eb9965279c68842600a1210adbf3c0

Console: fix a regression introduced by 3d583cb7
Change-Id: I48c2bb096aa3885ce9149072896b619180dd106c

scilab/modules/console/src/java/org/scilab/modules/console/SciHistoryManager.java
@@ -72,7 +72,7 @@ public class SciHistoryManager implements HistoryManager {
      */
     public String getNextEntry(String beg) {
         /* Ask Scilab history manager for a matching entry */
-        if (!HistoryManagement.getSearchedTokenInScilabHistory().equals(beg)) {
+        if (beg != null && !beg.equals(HistoryManagement.getSearchedTokenInScilabHistory())) {
             //HistoryManagement.resetSearchedTokenInScilabHistory();
             HistoryManagement.setSearchedTokenInScilabHistory(beg);
         }
@@ -87,7 +87,7 @@ public class SciHistoryManager implements HistoryManager {
      */
     public String getPreviousEntry(String beg) {
         /* Ask Scilab history manager for a matching entry */
-        if (!HistoryManagement.getSearchedTokenInScilabHistory().equals(beg)) {
+        if (beg != null && !beg.equals(HistoryManagement.getSearchedTokenInScilabHistory())) {
             //HistoryManagement.resetSearchedTokenInScilabHistory();
             HistoryManagement.setSearchedTokenInScilabHistory(beg);
         }