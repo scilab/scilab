scilab/scilab

commit 7a41f82deaea2c7b2b752397dabca737cc4961b7

* Bug 6939 fixed: now nearfloat works for hypermatrices
https://bugzilla.scilab.org/show_bug.cgi?id=6939

Gateway has been rewritten in c++

Change-Id: I0893a7406872ce39231aa0f99f4b4423b3d0fae5

scilab/modules/elementary_functions/sci_gateway/cpp/sci_nearfloat.cpp
@@ -1,0 +1,107 @@
+/*
+ * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+ * Copyright (C) 2018 - Stéphane Mottelet
+ *
+ * This file is hereby licensed under the terms of the GNU GPL v2.0,
+ * pursuant to article 5.3.4 of the CeCILL v.2.1.
+ * This file was originally licensed under the terms of the CeCILL v2.1,
+ * and continues to be available under such terms.
+ * For more information, see the COPYING file which you should have received
+ * along with this program.
+ *
+ */
+/*--------------------------------------------------------------------------*/
+
+#include <cfloat> // DBL_MAX
+#include <cmath> // std::nextafter
+#include <complex>
+#include "string.hxx"
+#include "elem_func_gw.hxx"
+#include "function.hxx"
+#include "double.hxx"
+#include "overload.hxx"
+
+extern "C"
+{
+#include "Scierror.h"
+#include "localization.h"
+}
+
+/*--------------------------------------------------------------------------*/
+types::Function::ReturnValue sci_nearfloat(types::typed_list &in, int _iRetCount, types::typed_list &out)
+{
+    double dblMode;
+
+    if (in.size() != 2)
+    {
+        Scierror(77, _("%s: Wrong number of input argument(s): %d expected.\n"), "nearfloat", 2);
+        return types::Function::Error;
+    }
+
+    if (_iRetCount > 1)
+    {
+        Scierror(78, _("%s: Wrong number of output argument(s): %d expected.\n"), "nearfloat", 1);
+        return types::Function::Error;
+    }
+
+    //first arg
+    types::InternalType* pIT = in[0];
+    if (pIT->isString())
+    {
+        types::String* pStr = pIT->getAs<types::String>();
+
+        if (pStr->getSize() > 1)
+        {
+            Scierror(999, _("%s: Wrong size for input argument #%d: A scalar string expected.\n"), "nearfloat", 1);
+            return types::Function::Error;
+        }
+
+        wchar_t* wcsString = pStr->get(0);
+        if (wcscmp(wcsString, L"succ") == 0)
+        {
+            dblMode = DBL_MAX;
+        }
+        else if (wcscmp(wcsString, L"pred") == 0)
+        {
+            dblMode = -DBL_MAX;
+        }
+        else
+        {
+            Scierror(999, _("%s: Wrong value for input argument #%d: Must be in the set {%s}.\n"), "nearfloat", 1, "\"succ\",\"pred\"");
+            return types::Function::Error;
+        }
+    }
+    else
+    {
+        Scierror(999, _("%s: Wrong type for input argument #%d: string expected.\n"), "nearfloat", 1);
+        return types::Function::Error;
+    }
+
+    //second arg
+    pIT = in[1];
+    if (pIT->isDouble() == false)
+    {
+        std::wstring wstFuncName = L"%" + pIT->getShortTypeStr() + L"_nearfloat";
+        return Overload::call(wstFuncName, in, _iRetCount, out);
+    }
+
+
+    types::Double* pDblIn = pIT->getAs<types::Double>();
+    if (pDblIn->isComplex())
+    {
+        Scierror(999, _("%s: Wrong type for input argument #%d : A real matrix expected.\n"), "nearfloat", 2);
+        return types::Function::Error;
+    }
+
+    types::Double* pDblOut = new types::Double(pDblIn->getDims(), pDblIn->getDimsArray());
+    double *pdRealIn = pDblIn->get();
+    double *pdRealOut = pDblOut->get();
+    for (int i = 0; i < pDblIn->getSize(); ++i)
+    {
+        pdRealOut[i] = std::nextafter(pdRealIn[i], dblMode);
+    }
+
+    out.push_back(pDblOut);
+    return types::Function::OK;
+}
+/*--------------------------------------------------------------------------*/

scilab/modules/elementary_functions/Makefile.am
@@ -250,7 +250,6 @@ ELEMENTARIES_FUNCTIONS_FORTRAN_SOURCES = \
     src/fortran/wasin.f \
     src/fortran/wexpm1.f \
     src/fortran/ddpowe.f \
-    src/fortran/nearfloat.f \
     src/fortran/dmprod.f \
     src/fortran/wwpowe.f \
     src/fortran/wdotci.f \
@@ -280,7 +279,6 @@ ELEMENTARIES_FUNCTIONS_FORTRAN_SOURCES = \
 GATEWAY_C_SOURCES = \
     sci_gateway/c/sci_number_properties.c \
     sci_gateway/c/sci_testmatrix.c \
-    sci_gateway/c/sci_nearfloat.c \
     sci_gateway/c/sci_isequalbitwise.c \
     sci_gateway/c/sci_base2dec.c \
     sci_gateway/c/sci_dec2base.c \
@@ -338,7 +336,8 @@ GATEWAY_CXX_SOURCES =  \
     sci_gateway/cpp/sci_sign.cpp \
     sci_gateway/cpp/sci_sqrt.cpp \
     sci_gateway/cpp/sci_issquare.cpp \
-    sci_gateway/cpp/sci_isvector.cpp
+    sci_gateway/cpp/sci_isvector.cpp\
+    sci_gateway/cpp/sci_nearfloat.cpp
 
 EISPACK_SOURCES = src/fortran/eispack/balbak.f
 

scilab/modules/elementary_functions/Makefile.in
@@ -1,7 +1,7 @@
-# Makefile.in generated by automake 1.14.1 from Makefile.am.
+# Makefile.in generated by automake 1.15.1 from Makefile.am.
 # @configure_input@
 
-# Copyright (C) 1994-2013 Free Software Foundation, Inc.
+# Copyright (C) 1994-2017 Free Software Foundation, Inc.
 
 # This Makefile.in is free software; the Free Software Foundation
 # gives unlimited permission to copy and/or distribute it,
@@ -40,7 +40,17 @@
 
 
 VPATH = @srcdir@
-am__is_gnu_make = test -n '$(MAKEFILE_LIST)' && test -n '$(MAKELEVEL)'
+am__is_gnu_make = { \
+  if test -z '$(MAKELEVEL)'; then \
+    false; \
+  elif test -n '$(MAKE_HOST)'; then \
+    true; \
+  elif test -n '$(MAKE_VERSION)' && test -n '$(CURDIR)'; then \
+    true; \
+  else \
+    false; \
+  fi; \
+}
 am__make_running_with_option = \
   case $${target_option-} in \
       ?) ;; \
@@ -105,9 +115,6 @@ build_triplet = @build@
 host_triplet = @host@
 @MAINTAINER_MODE_TRUE@am__append_1 = libscielementary_functions-algo.la
 @MAINTAINER_MODE_FALSE@am__append_2 = libscielementary_functions-algo.la libscielementary_functions.la
-DIST_COMMON = $(top_srcdir)/Makefile.incl.am $(srcdir)/Makefile.in \
-	$(srcdir)/Makefile.am $(top_srcdir)/config/mkinstalldirs \
-	$(top_srcdir)/config/depcomp
 @NEED_JAVA_TRUE@am__append_3 = java
 subdir = modules/elementary_functions
 ACLOCAL_M4 = $(top_srcdir)/aclocal.m4
@@ -131,6 +138,7 @@ am__aclocal_m4_deps = $(top_srcdir)/m4/ax_cxx_compile_stdcxx_11.m4 \
 	$(top_srcdir)/m4/umfpack.m4 $(top_srcdir)/configure.ac
 am__configure_deps = $(am__aclocal_m4_deps) $(CONFIGURE_DEPENDENCIES) \
 	$(ACLOCAL_M4)
+DIST_COMMON = $(srcdir)/Makefile.am $(am__DIST_COMMON)
 mkinstalldirs = $(SHELL) $(top_srcdir)/config/mkinstalldirs
 CONFIG_HEADER = $(top_builddir)/modules/core/includes/machine.h
 CONFIG_CLEAN_FILES =
@@ -312,11 +320,11 @@ am__objects_5 = src/fortran/wasum.lo src/fortran/dexpm1.lo \
 	src/fortran/orthes.lo src/fortran/waxpy.lo src/fortran/coef.lo \
 	src/fortran/dwdiv.lo src/fortran/arcosh.lo \
 	src/fortran/wasin.lo src/fortran/wexpm1.lo \
-	src/fortran/ddpowe.lo src/fortran/nearfloat.lo \
-	src/fortran/dmprod.lo src/fortran/wwpowe.lo \
-	src/fortran/wdotci.lo src/fortran/dmcopy.lo \
-	src/fortran/wexchn.lo src/fortran/isoval.lo \
-	src/fortran/dwrdiv.lo src/fortran/wipow.lo src/fortran/wlog.lo \
+	src/fortran/ddpowe.lo src/fortran/dmprod.lo \
+	src/fortran/wwpowe.lo src/fortran/wdotci.lo \
+	src/fortran/dmcopy.lo src/fortran/wexchn.lo \
+	src/fortran/isoval.lo src/fortran/dwrdiv.lo \
+	src/fortran/wipow.lo src/fortran/wlog.lo \
 	src/fortran/infinity.lo src/fortran/dwpow1.lo \
 	src/fortran/wmul.lo src/fortran/dad.lo src/fortran/dset.lo \
 	src/fortran/dtild.lo src/fortran/i1mach.lo \
@@ -341,12 +349,12 @@ libscielementary_functions_algo_la_OBJECTS =  \
 	$(am_libscielementary_functions_algo_la_OBJECTS)
 @MAINTAINER_MODE_FALSE@am_libscielementary_functions_algo_la_rpath =
 @MAINTAINER_MODE_TRUE@am_libscielementary_functions_algo_la_rpath =
+am__DEPENDENCIES_1 =
 libscielementary_functions_la_DEPENDENCIES =  \
 	libdummy-elementary_functions.la \
-	libscielementary_functions-algo.la
+	libscielementary_functions-algo.la $(am__DEPENDENCIES_1)
 am__objects_7 = sci_gateway/c/libscielementary_functions_la-sci_number_properties.lo \
 	sci_gateway/c/libscielementary_functions_la-sci_testmatrix.lo \
-	sci_gateway/c/libscielementary_functions_la-sci_nearfloat.lo \
 	sci_gateway/c/libscielementary_functions_la-sci_isequalbitwise.lo \
 	sci_gateway/c/libscielementary_functions_la-sci_base2dec.lo \
 	sci_gateway/c/libscielementary_functions_la-sci_dec2base.lo \
@@ -403,7 +411,8 @@ am__objects_8 =  \
 	sci_gateway/cpp/libscielementary_functions_la-sci_sign.lo \
 	sci_gateway/cpp/libscielementary_functions_la-sci_sqrt.lo \
 	sci_gateway/cpp/libscielementary_functions_la-sci_issquare.lo \
-	sci_gateway/cpp/libscielementary_functions_la-sci_isvector.lo
+	sci_gateway/cpp/libscielementary_functions_la-sci_isvector.lo \
+	sci_gateway/cpp/libscielementary_functions_la-sci_nearfloat.lo
 am_libscielementary_functions_la_OBJECTS = $(am__objects_7) \
 	$(am__objects_8)
 libscielementary_functions_la_OBJECTS =  \
@@ -511,6 +520,9 @@ am__define_uniq_tagged_files = \
   done | $(am__uniquify_input)`
 ETAGS = etags
 CTAGS = ctags
+am__DIST_COMMON = $(srcdir)/Makefile.in $(top_srcdir)/Makefile.incl.am \
+	$(top_srcdir)/config/depcomp \
+	$(top_srcdir)/config/mkinstalldirs
 DISTFILES = $(DIST_COMMON) $(DIST_SOURCES) $(TEXINFOS) $(EXTRA_DIST)
 ACLOCAL = @ACLOCAL@
 ALL_LINGUAS = @ALL_LINGUAS@
@@ -1051,7 +1063,6 @@ ELEMENTARIES_FUNCTIONS_FORTRAN_SOURCES = \
     src/fortran/wasin.f \
     src/fortran/wexpm1.f \
     src/fortran/ddpowe.f \
-    src/fortran/nearfloat.f \
     src/fortran/dmprod.f \
     src/fortran/wwpowe.f \
     src/fortran/wdotci.f \
@@ -1081,7 +1092,6 @@ ELEMENTARIES_FUNCTIONS_FORTRAN_SOURCES = \
 GATEWAY_C_SOURCES = \
     sci_gateway/c/sci_number_properties.c \
     sci_gateway/c/sci_testmatrix.c \
-    sci_gateway/c/sci_nearfloat.c \
     sci_gateway/c/sci_isequalbitwise.c \
     sci_gateway/c/sci_base2dec.c \
     sci_gateway/c/sci_dec2base.c \
@@ -1139,7 +1149,8 @@ GATEWAY_CXX_SOURCES = \
     sci_gateway/cpp/sci_sign.cpp \
     sci_gateway/cpp/sci_sqrt.cpp \
     sci_gateway/cpp/sci_issquare.cpp \
-    sci_gateway/cpp/sci_isvector.cpp
+    sci_gateway/cpp/sci_isvector.cpp\
+    sci_gateway/cpp/sci_nearfloat.cpp
 
 EISPACK_SOURCES = src/fortran/eispack/balbak.f
 
@@ -1298,7 +1309,6 @@ $(srcdir)/Makefile.in: @MAINTAINER_MODE_TRUE@ $(srcdir)/Makefile.am $(top_srcdir
 	echo ' cd $(top_srcdir) && $(AUTOMAKE) --foreign modules/elementary_functions/Makefile'; \
 	$(am__cd) $(top_srcdir) && \
 	  $(AUTOMAKE) --foreign modules/elementary_functions/Makefile
-.PRECIOUS: Makefile
 Makefile: $(srcdir)/Makefile.in $(top_builddir)/config.status
 	@case '$?' in \
 	  *config.status*) \
@@ -1307,7 +1317,7 @@ Makefile: $(srcdir)/Makefile.in $(top_builddir)/config.status
 	    echo ' cd $(top_builddir) && $(SHELL) ./config.status $(subdir)/$@ $(am__depfiles_maybe)'; \
 	    cd $(top_builddir) && $(SHELL) ./config.status $(subdir)/$@ $(am__depfiles_maybe);; \
 	esac;
-$(top_srcdir)/Makefile.incl.am:
+$(top_srcdir)/Makefile.incl.am $(am__empty):
 
 $(top_builddir)/config.status: $(top_srcdir)/configure $(CONFIG_STATUS_DEPENDENCIES)
 	cd $(top_builddir) && $(MAKE) $(AM_MAKEFLAGS) am--refresh
@@ -1862,8 +1872,6 @@ src/fortran/wexpm1.lo: src/fortran/$(am__dirstamp) \
 	src/fortran/$(DEPDIR)/$(am__dirstamp)
 src/fortran/ddpowe.lo: src/fortran/$(am__dirstamp) \
 	src/fortran/$(DEPDIR)/$(am__dirstamp)
-src/fortran/nearfloat.lo: src/fortran/$(am__dirstamp) \
-	src/fortran/$(DEPDIR)/$(am__dirstamp)
 src/fortran/dmprod.lo: src/fortran/$(am__dirstamp) \
 	src/fortran/$(DEPDIR)/$(am__dirstamp)
 src/fortran/wwpowe.lo: src/fortran/$(am__dirstamp) \
@@ -1953,9 +1961,6 @@ sci_gateway/c/libscielementary_functions_la-sci_number_properties.lo:  \
 	sci_gateway/c/$(am__dirstamp) \
 	sci_gateway/c/$(DEPDIR)/$(am__dirstamp)
 sci_gateway/c/libscielementary_functions_la-sci_testmatrix.lo:  \
-	sci_gateway/c/$(am__dirstamp) \
-	sci_gateway/c/$(DEPDIR)/$(am__dirstamp)
-sci_gateway/c/libscielementary_functions_la-sci_nearfloat.lo:  \
 	sci_gateway/c/$(am__dirstamp) \
 	sci_gateway/c/$(DEPDIR)/$(am__dirstamp)
 sci_gateway/c/libscielementary_functions_la-sci_isequalbitwise.lo:  \
@@ -2132,6 +2137,9 @@ sci_gateway/cpp/libscielementary_functions_la-sci_issquare.lo:  \
 sci_gateway/cpp/libscielementary_functions_la-sci_isvector.lo:  \
 	sci_gateway/cpp/$(am__dirstamp) \
 	sci_gateway/cpp/$(DEPDIR)/$(am__dirstamp)
+sci_gateway/cpp/libscielementary_functions_la-sci_nearfloat.lo:  \
+	sci_gateway/cpp/$(am__dirstamp) \
+	sci_gateway/cpp/$(DEPDIR)/$(am__dirstamp)
 
 libscielementary_functions.la: $(libscielementary_functions_la_OBJECTS) $(libscielementary_functions_la_DEPENDENCIES) $(EXTRA_libscielementary_functions_la_DEPENDENCIES) 
 	$(AM_V_CXXLD)$(CXXLINK) $(am_libscielementary_functions_la_rpath) $(libscielementary_functions_la_OBJECTS) $(libscielementary_functions_la_LIBADD) $(LIBS)
@@ -2162,7 +2170,6 @@ distclean-compile:
 @AMDEP_TRUE@@am__include@ @am__quote@sci_gateway/c/$(DEPDIR)/libscielementary_functions_la-sci_dec2base.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@sci_gateway/c/$(DEPDIR)/libscielementary_functions_la-sci_isequalbitwise.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@sci_gateway/c/$(DEPDIR)/libscielementary_functions_la-sci_log10.Plo@am__quote@
-@AMDEP_TRUE@@am__include@ @am__quote@sci_gateway/c/$(DEPDIR)/libscielementary_functions_la-sci_nearfloat.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@sci_gateway/c/$(DEPDIR)/libscielementary_functions_la-sci_number_properties.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@sci_gateway/c/$(DEPDIR)/libscielementary_functions_la-sci_testmatrix.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@sci_gateway/cpp/$(DEPDIR)/libscielementary_functions_la-elem_func_gw.Plo@am__quote@
@@ -2200,6 +2207,7 @@ distclean-compile:
 @AMDEP_TRUE@@am__include@ @am__quote@sci_gateway/cpp/$(DEPDIR)/libscielementary_functions_la-sci_log1p.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@sci_gateway/cpp/$(DEPDIR)/libscielementary_functions_la-sci_matrix.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@sci_gateway/cpp/$(DEPDIR)/libscielementary_functions_la-sci_max.Plo@am__quote@
+@AMDEP_TRUE@@am__include@ @am__quote@sci_gateway/cpp/$(DEPDIR)/libscielementary_functions_la-sci_nearfloat.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@sci_gateway/cpp/$(DEPDIR)/libscielementary_functions_la-sci_ones.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@sci_gateway/cpp/$(DEPDIR)/libscielementary_functions_la-sci_prod.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@sci_gateway/cpp/$(DEPDIR)/libscielementary_functions_la-sci_rand.Plo@am__quote@
@@ -2609,13 +2617,6 @@ sci_gateway/c/libscielementary_functions_la-sci_testmatrix.lo: sci_gateway/c/sci
 @AMDEP_TRUE@@am__fastdepCC_FALSE@	$(AM_V_CC)source='sci_gateway/c/sci_testmatrix.c' object='sci_gateway/c/libscielementary_functions_la-sci_testmatrix.lo' libtool=yes @AMDEPBACKSLASH@
 @AMDEP_TRUE@@am__fastdepCC_FALSE@	DEPDIR=$(DEPDIR) $(CCDEPMODE) $(depcomp) @AMDEPBACKSLASH@
 @am__fastdepCC_FALSE@	$(AM_V_CC@am__nodep@)$(LIBTOOL) $(AM_V_lt) --tag=CC $(AM_LIBTOOLFLAGS) $(LIBTOOLFLAGS) --mode=compile $(CC) $(DEFS) $(DEFAULT_INCLUDES) $(INCLUDES) $(libscielementary_functions_la_CPPFLAGS) $(CPPFLAGS) $(AM_CFLAGS) $(CFLAGS) -c -o sci_gateway/c/libscielementary_functions_la-sci_testmatrix.lo `test -f 'sci_gateway/c/sci_testmatrix.c' || echo '$(srcdir)/'`sci_gateway/c/sci_testmatrix.c
-
-sci_gateway/c/libscielementary_functions_la-sci_nearfloat.lo: sci_gateway/c/sci_nearfloat.c
-@am__fastdepCC_TRUE@	$(AM_V_CC)$(LIBTOOL) $(AM_V_lt) --tag=CC $(AM_LIBTOOLFLAGS) $(LIBTOOLFLAGS) --mode=compile $(CC) $(DEFS) $(DEFAULT_INCLUDES) $(INCLUDES) $(libscielementary_functions_la_CPPFLAGS) $(CPPFLAGS) $(AM_CFLAGS) $(CFLAGS) -MT sci_gateway/c/libscielementary_functions_la-sci_nearfloat.lo -MD -MP -MF sci_gateway/c/$(DEPDIR)/libscielementary_functions_la-sci_nearfloat.Tpo -c -o sci_gateway/c/libscielementary_functions_la-sci_nearfloat.lo `test -f 'sci_gateway/c/sci_nearfloat.c' || echo '$(srcdir)/'`sci_gateway/c/sci_nearfloat.c
-@am__fastdepCC_TRUE@	$(AM_V_at)$(am__mv) sci_gateway/c/$(DEPDIR)/libscielementary_functions_la-sci_nearfloat.Tpo sci_gateway/c/$(DEPDIR)/libscielementary_functions_la-sci_nearfloat.Plo
-@AMDEP_TRUE@@am__fastdepCC_FALSE@	$(AM_V_CC)source='sci_gateway/c/sci_nearfloat.c' object='sci_gateway/c/libscielementary_functions_la-sci_nearfloat.lo' libtool=yes @AMDEPBACKSLASH@
-@AMDEP_TRUE@@am__fastdepCC_FALSE@	DEPDIR=$(DEPDIR) $(CCDEPMODE) $(depcomp) @AMDEPBACKSLASH@
-@am__fastdepCC_FALSE@	$(AM_V_CC@am__nodep@)$(LIBTOOL) $(AM_V_lt) --tag=CC $(AM_LIBTOOLFLAGS) $(LIBTOOLFLAGS) --mode=compile $(CC) $(DEFS) $(DEFAULT_INCLUDES) $(INCLUDES) $(libscielementary_functions_la_CPPFLAGS) $(CPPFLAGS) $(AM_CFLAGS) $(CFLAGS) -c -o sci_gateway/c/libscielementary_functions_la-sci_nearfloat.lo `test -f 'sci_gateway/c/sci_nearfloat.c' || echo '$(srcdir)/'`sci_gateway/c/sci_nearfloat.c
 
 sci_gateway/c/libscielementary_functions_la-sci_isequalbitwise.lo: sci_gateway/c/sci_isequalbitwise.c
 @am__fastdepCC_TRUE@	$(AM_V_CC)$(LIBTOOL) $(AM_V_lt) --tag=CC $(AM_LIBTOOLFLAGS) $(LIBTOOLFLAGS) --mode=compile $(CC) $(DEFS) $(DEFAULT_INCLUDES) $(INCLUDES) $(libscielementary_functions_la_CPPFLAGS) $(CPPFLAGS) $(AM_CFLAGS) $(CFLAGS) -MT sci_gateway/c/libscielementary_functions_la-sci_isequalbitwise.lo -MD -MP -MF sci_gateway/c/$(DEPDIR)/libscielementary_functions_la-sci_isequalbitwise.Tpo -c -o sci_gateway/c/libscielementary_functions_la-sci_isequalbitwise.lo `test -f 'sci_gateway/c/sci_isequalbitwise.c' || echo '$(srcdir)/'`sci_gateway/c/sci_isequalbitwise.c
@@ -3103,6 +3104,13 @@ sci_gateway/cpp/libscielementary_functions_la-sci_isvector.lo: sci_gateway/cpp/s
 @AMDEP_TRUE@@am__fastdepCXX_FALSE@	DEPDIR=$(DEPDIR) $(CXXDEPMODE) $(depcomp) @AMDEPBACKSLASH@
 @am__fastdepCXX_FALSE@	$(AM_V_CXX@am__nodep@)$(LIBTOOL) $(AM_V_lt) --tag=CXX $(AM_LIBTOOLFLAGS) $(LIBTOOLFLAGS) --mode=compile $(CXX) $(DEFS) $(DEFAULT_INCLUDES) $(INCLUDES) $(libscielementary_functions_la_CPPFLAGS) $(CPPFLAGS) $(AM_CXXFLAGS) $(CXXFLAGS) -c -o sci_gateway/cpp/libscielementary_functions_la-sci_isvector.lo `test -f 'sci_gateway/cpp/sci_isvector.cpp' || echo '$(srcdir)/'`sci_gateway/cpp/sci_isvector.cpp
 
+sci_gateway/cpp/libscielementary_functions_la-sci_nearfloat.lo: sci_gateway/cpp/sci_nearfloat.cpp
+@am__fastdepCXX_TRUE@	$(AM_V_CXX)$(LIBTOOL) $(AM_V_lt) --tag=CXX $(AM_LIBTOOLFLAGS) $(LIBTOOLFLAGS) --mode=compile $(CXX) $(DEFS) $(DEFAULT_INCLUDES) $(INCLUDES) $(libscielementary_functions_la_CPPFLAGS) $(CPPFLAGS) $(AM_CXXFLAGS) $(CXXFLAGS) -MT sci_gateway/cpp/libscielementary_functions_la-sci_nearfloat.lo -MD -MP -MF sci_gateway/cpp/$(DEPDIR)/libscielementary_functions_la-sci_nearfloat.Tpo -c -o sci_gateway/cpp/libscielementary_functions_la-sci_nearfloat.lo `test -f 'sci_gateway/cpp/sci_nearfloat.cpp' || echo '$(srcdir)/'`sci_gateway/cpp/sci_nearfloat.cpp
+@am__fastdepCXX_TRUE@	$(AM_V_at)$(am__mv) sci_gateway/cpp/$(DEPDIR)/libscielementary_functions_la-sci_nearfloat.Tpo sci_gateway/cpp/$(DEPDIR)/libscielementary_functions_la-sci_nearfloat.Plo
+@AMDEP_TRUE@@am__fastdepCXX_FALSE@	$(AM_V_CXX)source='sci_gateway/cpp/sci_nearfloat.cpp' object='sci_gateway/cpp/libscielementary_functions_la-sci_nearfloat.lo' libtool=yes @AMDEPBACKSLASH@
+@AMDEP_TRUE@@am__fastdepCXX_FALSE@	DEPDIR=$(DEPDIR) $(CXXDEPMODE) $(depcomp) @AMDEPBACKSLASH@
+@am__fastdepCXX_FALSE@	$(AM_V_CXX@am__nodep@)$(LIBTOOL) $(AM_V_lt) --tag=CXX $(AM_LIBTOOLFLAGS) $(LIBTOOLFLAGS) --mode=compile $(CXX) $(DEFS) $(DEFAULT_INCLUDES) $(INCLUDES) $(libscielementary_functions_la_CPPFLAGS) $(CPPFLAGS) $(AM_CXXFLAGS) $(CXXFLAGS) -c -o sci_gateway/cpp/libscielementary_functions_la-sci_nearfloat.lo `test -f 'sci_gateway/cpp/sci_nearfloat.cpp' || echo '$(srcdir)/'`sci_gateway/cpp/sci_nearfloat.cpp
+
 .f.o:
 	$(AM_V_F77)$(F77COMPILE) -c -o $@ $<
 
@@ -3426,6 +3434,8 @@ uninstall-am: uninstall-libscielementary_functions_la_etcDATA \
 	uninstall-libscielementary_functions_la_rootDATA \
 	uninstall-libscielementary_functions_la_sci_gatewayDATA \
 	uninstall-pkglibLTLIBRARIES
+
+.PRECIOUS: Makefile
 
 
 # Following lines are extracted from a processed Makefile.in

scilab/modules/elementary_functions/includes/elem_func_gw.hxx
@@ -39,56 +39,58 @@ public :
     }
 };
 
-CPP_GATEWAY_PROTOTYPE(sci_isequal);
-CPP_GATEWAY_PROTOTYPE(sci_size);
-CPP_GATEWAY_PROTOTYPE(sci_eye);
-CPP_GATEWAY_PROTOTYPE(sci_zeros);
-CPP_GATEWAY_PROTOTYPE(sci_ones);
-CPP_GATEWAY_PROTOTYPE(sci_rand);
-CPP_GATEWAY_PROTOTYPE(sci_prod);
-CPP_GATEWAY_PROTOTYPE(sci_sum);
-CPP_GATEWAY_PROTOTYPE(sci_matrix);
 CPP_GATEWAY_PROTOTYPE(sci_abs);
 CPP_GATEWAY_PROTOTYPE(sci_acos);
+CPP_GATEWAY_PROTOTYPE(sci_acosh);
 CPP_GATEWAY_PROTOTYPE(sci_asin);
+CPP_GATEWAY_PROTOTYPE(sci_asinh);
 CPP_GATEWAY_PROTOTYPE(sci_atan);
+CPP_GATEWAY_PROTOTYPE(sci_atanh);
 CPP_GATEWAY_PROTOTYPE(sci_ceil);
 CPP_GATEWAY_PROTOTYPE(sci_clean);
 CPP_GATEWAY_PROTOTYPE(sci_conj);
 CPP_GATEWAY_PROTOTYPE(sci_cos);
-CPP_GATEWAY_PROTOTYPE(sci_sin);
-CPP_GATEWAY_PROTOTYPE(sci_tan);
+CPP_GATEWAY_PROTOTYPE(sci_cosh);
 CPP_GATEWAY_PROTOTYPE(sci_cumprod);
 CPP_GATEWAY_PROTOTYPE(sci_cumsum);
 CPP_GATEWAY_PROTOTYPE(sci_diag);
 CPP_GATEWAY_PROTOTYPE(sci_dsearch);
 CPP_GATEWAY_PROTOTYPE(sci_exp);
 CPP_GATEWAY_PROTOTYPE(sci_expm);
+CPP_GATEWAY_PROTOTYPE(sci_eye);
 CPP_GATEWAY_PROTOTYPE(sci_floor);
 CPP_GATEWAY_PROTOTYPE(sci_frexp);
 CPP_GATEWAY_PROTOTYPE(sci_gsort);
 CPP_GATEWAY_PROTOTYPE(sci_imag);
 CPP_GATEWAY_PROTOTYPE(sci_imult);
 CPP_GATEWAY_PROTOTYPE(sci_int);
+CPP_GATEWAY_PROTOTYPE(sci_isequal);
 CPP_GATEWAY_PROTOTYPE(sci_isreal);
-CPP_GATEWAY_PROTOTYPE(sci_real);
 CPP_GATEWAY_PROTOTYPE(sci_kron);
 CPP_GATEWAY_PROTOTYPE(sci_log);
 CPP_GATEWAY_PROTOTYPE(sci_log1p);
+CPP_GATEWAY_PROTOTYPE(sci_matrix);
 CPP_GATEWAY_PROTOTYPE(sci_max); // Old name sci_maxi
 CPP_GATEWAY_PROTOTYPE(sci_min); // Old name sci_mini
+CPP_GATEWAY_PROTOTYPE(sci_nearfloat);
+CPP_GATEWAY_PROTOTYPE(sci_ones);
+CPP_GATEWAY_PROTOTYPE(sci_prod);
+CPP_GATEWAY_PROTOTYPE(sci_rand);
 CPP_GATEWAY_PROTOTYPE(sci_rat);
-CPP_GATEWAY_PROTOTYPE(sci_triu);
-CPP_GATEWAY_PROTOTYPE(sci_tril);
+CPP_GATEWAY_PROTOTYPE(sci_real);
 CPP_GATEWAY_PROTOTYPE(sci_round);
 CPP_GATEWAY_PROTOTYPE(sci_sign);
-CPP_GATEWAY_PROTOTYPE(sci_cosh);
+CPP_GATEWAY_PROTOTYPE(sci_sin);
 CPP_GATEWAY_PROTOTYPE(sci_sinh);
-CPP_GATEWAY_PROTOTYPE(sci_tanh);
-CPP_GATEWAY_PROTOTYPE(sci_acosh);
-CPP_GATEWAY_PROTOTYPE(sci_asinh);
-CPP_GATEWAY_PROTOTYPE(sci_atanh);
+CPP_GATEWAY_PROTOTYPE(sci_size);
 CPP_GATEWAY_PROTOTYPE(sci_sqrt);
+CPP_GATEWAY_PROTOTYPE(sci_sum);
+CPP_GATEWAY_PROTOTYPE(sci_tan);
+CPP_GATEWAY_PROTOTYPE(sci_tanh);
+CPP_GATEWAY_PROTOTYPE(sci_tril);
+CPP_GATEWAY_PROTOTYPE(sci_triu);
+CPP_GATEWAY_PROTOTYPE(sci_zeros);
+
 
 bool getDimsFromArguments(types::typed_list& in, const std::string& _pstName, int* _iDims, int** _piDims, bool* _alloc);
 

scilab/modules/elementary_functions/includes/gw_elementary_functions.h
@@ -54,7 +54,7 @@ int sci_isequalbitwise(char *fname, unsigned long fname_len);
 //STACK_GATEWAY_PROTOTYPE(sci_scimatrix);
 //STACK_GATEWAY_PROTOTYPE(sci_maxi);
 //STACK_GATEWAY_PROTOTYPE(sci_mini);
-STACK_GATEWAY_PROTOTYPE(sci_nearfloat);
+//STACK_GATEWAY_PROTOTYPE(sci_nearfloat);
 STACK_GATEWAY_PROTOTYPE(sci_number_properties);
 //STACK_GATEWAY_PROTOTYPE(sci_prod);
 //STACK_GATEWAY_PROTOTYPE(sci_real);

scilab/modules/elementary_functions/sci_gateway/c/sci_nearfloat.c
@@ -1,193 +1,0 @@
-/*
- * Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
- * Copyright (C) 2006 - INRIA - Allan CORNET
- *
- * Copyright (C) 2012 - 2016 - Scilab Enterprises
- *
- * This file is hereby licensed under the terms of the GNU GPL v2.0,
- * pursuant to article 5.3.4 of the CeCILL v.2.1.
- * This file was originally licensed under the terms of the CeCILL v2.1,
- * and continues to be available under such terms.
- * For more information, see the COPYING file which you should have received
- * along with this program.
- *
- */
-/*--------------------------------------------------------------------------*/
-#include <string.h>
-#include "gw_elementary_functions.h"
-#include "basic_functions.h"
-#include "api_scilab.h"
-#include "Scierror.h"
-
-
-int sci_nearfloat(char *fname, void* pvApiCtx)
-{
-    SciErr sciErr;
-    int i;
-
-    int* piAddr1				= NULL;
-    int iRows1					= 0;
-    int iCols1					= 0;
-    int iType1					= 0;
-    int* piLen					= NULL;
-    char **pstData			= NULL;
-
-    int* piAddr2				= NULL;
-    int iRows2					= 0;
-    int iCols2					= 0;
-    int iType2					= 0;
-    double *pdblReal		= NULL;
-
-    double dblMode			= 0;
-
-    double *pdblRealRet	= NULL;
-
-
-    CheckRhs(2, 2);
-    CheckLhs(1, 1);
-
-    sciErr = getVarAddressFromPosition(pvApiCtx, 1, &piAddr1);
-    if (sciErr.iErr)
-    {
-        printError(&sciErr, 0);
-        return 0;
-    }
-
-    sciErr = getVarAddressFromPosition(pvApiCtx, 2, &piAddr2);
-    if (sciErr.iErr)
-    {
-        printError(&sciErr, 0);
-        return 0;
-    }
-
-    sciErr = getVarType(pvApiCtx, piAddr1, &iType1);
-    if (sciErr.iErr)
-    {
-        printError(&sciErr, 0);
-        return 0;
-    }
-
-    sciErr = getVarType(pvApiCtx, piAddr2, &iType2);
-    if (sciErr.iErr)
-    {
-        printError(&sciErr, 0);
-        return 0;
-    }
-
-    if (iType1 != sci_strings)
-    {
-        //Err = 1;
-        SciError(55);
-        return 0;
-    }
-
-    if (iType2 != sci_matrix)
-    {
-        //Err = 2;
-        SciError(53);
-        return 0;
-    }
-
-    sciErr = getMatrixOfString(pvApiCtx, piAddr1, &iRows1, &iCols1, NULL, NULL);
-    if (sciErr.iErr)
-    {
-        printError(&sciErr, 0);
-        return 0;
-    }
-
-    if (iRows1 != 1 || iCols1 != 1)
-    {
-        return 1;
-    }
-
-    piLen = (int*)malloc(sizeof(int) * iRows1 * iCols1);
-    sciErr = getMatrixOfString(pvApiCtx, piAddr1, &iRows1, &iCols1, piLen, NULL);
-    if (sciErr.iErr)
-    {
-        printError(&sciErr, 0);
-        free(piLen);
-        return 0;
-    }
-
-    pstData	= (char**)malloc(sizeof(char*) * iRows1 * iCols1);
-    for (i = 0 ; i < iRows1 * iCols1 ; i++)
-    {
-        pstData[i] = (char*)malloc(sizeof(char) * (piLen[i] + 1));//+1 for null termination
-    }
-
-    sciErr = getMatrixOfString(pvApiCtx, piAddr1, &iRows1, &iCols1, piLen, pstData);
-    if (sciErr.iErr)
-    {
-        printError(&sciErr, 0);
-        free(piLen);
-        for (i = 0 ; i < iRows1 * iCols1 ; i++)
-        {
-            free(pstData[i]);
-        }
-        free(pstData);
-        return 0;
-    }
-
-    sciErr = getMatrixOfDouble(pvApiCtx, piAddr2, &iRows2, &iCols2, &pdblReal);
-    if (sciErr.iErr)
-    {
-        printError(&sciErr, 0);
-        free(piLen);
-        for (i = 0 ; i < iRows1 * iCols1 ; i++)
-        {
-            free(pstData[i]);
-        }
-        free(pstData);
-        return 0;
-    }
-
-    if (strcmp(pstData[0], "succ") == 0)
-    {
-        dblMode = 1.0;
-    }
-    else if (strcmp(pstData[0], "pred") == 0)
-    {
-        dblMode = -1.0;
-    }
-    else
-    {
-        SciError(999);
-        free(piLen);
-        for (i = 0 ; i < iRows1 * iCols1 ; i++)
-        {
-            free(pstData[i]);
-        }
-        free(pstData);
-        return 0;
-    }
-
-    sciErr = allocMatrixOfDouble(pvApiCtx, Rhs + 1, iRows2, iCols2, &pdblRealRet);
-    if (sciErr.iErr)
-    {
-        printError(&sciErr, 0);
-        free(piLen);
-        for (i = 0 ; i < iRows1 * iCols1 ; i++)
-        {
-            free(pstData[i]);
-        }
-        free(pstData);
-        return 0;
-    }
-
-    for (i = 0 ; i < iRows2 * iCols2 ; i++)
-    {
-        pdblRealRet[i] = dblNearFloat(pdblReal[i], dblMode);
-    }
-
-    free(piLen);
-    for (i = 0 ; i < iRows1 * iCols1 ; i++)
-    {
-        free(pstData[i]);
-    }
-    free(pstData);
-
-    LhsVar(1) = Rhs + 1;
-    PutLhsVar();
-    return 0;
-}
-/*--------------------------------------------------------------------------*/

scilab/modules/elementary_functions/sci_gateway/cpp/elem_func_gw/elem_func_gw.vcxproj
@@ -208,7 +208,6 @@ lib /DEF:"$(ProjectDir)elementary_functions_Import.def" /SUBSYSTEM:WINDOWS /MACH
     <ClCompile Include="..\..\c\sci_dec2base.c" />
     <ClCompile Include="..\..\c\sci_isequalbitwise.c" />
     <ClCompile Include="..\..\c\sci_log10.c" />
-    <ClCompile Include="..\..\c\sci_nearfloat.c" />
     <ClCompile Include="..\..\c\sci_number_properties.c" />
     <ClCompile Include="..\..\c\sci_testmatrix.c" />
     <ClCompile Include="..\elem_func_gw.cpp" />
@@ -244,6 +243,7 @@ lib /DEF:"$(ProjectDir)elementary_functions_Import.def" /SUBSYSTEM:WINDOWS /MACH
     <ClCompile Include="..\sci_log1p.cpp" />
     <ClCompile Include="..\sci_matrix.cpp" />
     <ClCompile Include="..\sci_max.cpp" />
+    <ClCompile Include="..\sci_nearfloat.cpp" />
     <ClCompile Include="..\sci_ones.cpp">
       <Optimization Condition="'$(Configuration)|$(Platform)'=='Release|Win32'">MaxSpeed</Optimization>
     </ClCompile>

commit e55a5eb49d271f94c76ac20359b870d44a8d3b4d

[elementary_functions] fix nearfloat regression after 7a41f82d
Change-Id: Iffadd661184a4d26c172dba4210bcecf41e5b9cf

scilab/modules/elementary_functions/sci_gateway/cpp/sci_nearfloat.cpp
@@ -59,11 +59,11 @@ types::Function::ReturnValue sci_nearfloat(types::typed_list &in, int _iRetCount
         wchar_t* wcsString = pStr->get(0);
         if (wcscmp(wcsString, L"succ") == 0)
         {
-            dblMode = DBL_MAX;
+            dblMode = std::numeric_limits<double>::infinity();
         }
         else if (wcscmp(wcsString, L"pred") == 0)
         {
-            dblMode = -DBL_MAX;
+            dblMode = -std::numeric_limits<double>::infinity();
         }
         else
         {