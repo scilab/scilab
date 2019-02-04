scilab/scilab

commit b9e819c80cbf18b026581825e1d721d815a19202

* Bug 15662 fixed: display in mode format(e) repaired
http://bugzilla.scilab.org/show_bug.cgi?id=15662

Change-Id: Ibc5187eab4441bba50f180dbfba9dce16076fb58

scilab/modules/ast/src/cpp/types/tostring_common.cpp
@@ -1,6 +1,7 @@
 /*
 *  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
 *  Copyright (C) 2009 - DIGITEO - Antoine ELIAS
+*  Copyright (C) 2018 - Stéphane MOTTELET
 *
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
  *
@@ -22,8 +23,11 @@
 
 extern "C"
 {
+#include "sci_malloc.h"
 #include "elem_common.h"
 #include "os_string.h"
+#include "dtoa.h"
+#include "charEncoding.h"
 }
 
 #define BLANK_SIZE 1
@@ -216,6 +220,7 @@ void addDoubleValue(std::wostringstream * _postr, double _dblVal, DoubleFormat *
     wchar_t pwstFormat[32] = {0};
     wchar_t pwstOutput[32] = {0};     // > @ format max
     wchar_t pwstSign[32] = {0};
+    char    pstBuf[32] = {0};
 
     if (_pDF == NULL)
     {
@@ -270,82 +275,53 @@ void addDoubleValue(std::wostringstream * _postr, double _dblVal, DoubleFormat *
     else if (_pDF->bExp)
     {
         // Prints the exponent part 1.543D+03 for example
-        double dblAbs = fabs(_dblVal);
-        double dblDec = 0;
-        double dblEnt = 0;
-        double dblTemp = 0;
+        int decpt = 0;
+        int sign = 0;
+        char *rve = nullptr;
+        char *dtoa_str = dtoa(fabs(_dblVal), 2, _pDF->iPrec + 1, &decpt, &sign, &rve);
 
-        // modf returns the fractional par in dblDec
-        // and stores the pointer to the integral part in dblEnt
-        dblDec = std::modf(dblAbs, &dblEnt);
-        if (dblEnt == 0)
+        std::string str(dtoa_str);
+        freedtoa(dtoa_str);
+
+        // in format("e") omiting the decimal point has a sense
+        // only if fabs(_dblVal) is an integer in {1...9}
+        if (_pDF->bPrintPoint || str.length() > 1)
         {
-            // The integral part in 0
-            // the number in between 0 and 1 in absolute value
-            // dblTemp stores the position of the significant digit
-            // floor(log10(0.01)) = -2
-            // floor(log10(0.00123)) = -3
-            // floor(log10(0.0015)) = -3
-            if (dblDec != 0)
-            {
-                dblTemp = std::floor(std::log10(dblDec));
-            }
-            else
-            {
-                dblTemp = 0;
-            }
-        }
-        else
-        {
-            // dblTemp stores the number of digit of the integral part minus one
-            // log10(15) = 1.176
-            // log10(1530) = 3.185
-            dblTemp = std::log10(dblEnt);
+            /* add trailing zeros */
+            str.append(fmax(0, _pDF->iPrec + 1 - str.length()), '0');
+            str.insert(1, ".");
         }
 
-        dblDec = dblAbs / std::pow(10., (double)(int)dblTemp);
-        dblDec = std::modf(dblDec, &dblEnt) * pow(10., _pDF->iPrec);
-
-        if (_pDF->bPrintPoint)
-        {
-            os_swprintf(pwstFormat, 32, L"%ls%%#d.%%0%dlldD%%+.02d", pwstSign, _pDF->iPrec);
-        }
-        else
-        {
-            os_swprintf(pwstFormat, 32, L"%ls%%d%%0%dlldD%%+.02d", pwstSign, _pDF->iPrec);
-        }
-
-        if ((long long int)std::round(dblDec) != (long long int)dblDec)
-        {
-            // casting to long long int to avoid overflow to negative values
-            double d1 = (long long int)std::round(dblDec);
-            d1 = fmod(d1, std::pow(10., _pDF->iPrec));
-            if (d1 < dblDec)
-            {
-                //inc integer part
-                ++dblEnt;
-            }
-
-            dblDec = d1;
-        }
-
-        // long long int to be able to print up to format(25) otherwise you just write overflow
-        // and write a negative number, dblEnt is at most one digit
-        os_swprintf(pwstOutput, 32, pwstFormat, (int)dblEnt, (long long int)dblDec, (int)dblTemp);
+        wchar_t* tmp = to_wide_string(str.data());
+        os_swprintf(pwstOutput, 32, L"%ls%lsD%+.02d", pwstSign, tmp, decpt - 1);
+        FREE(tmp);
     }
     else if ((_pDF->bPrintOne == true) || (isEqual(fabs(_dblVal), 1)) == false)
     {
         //do not print if _bPrintOne == false && _dblVal == 1
-        if (_pDF->bPrintPoint)
+        int decpt = 0;
+        int sign = 0;
+        char *rve = nullptr;
+        char *dtoa_str = dtoa(fabs(_dblVal), 2, std::floor(log10(fabs(_dblVal))) + _pDF->iPrec + 1, &decpt, &sign, &rve);
+
+        std::string str(dtoa_str);
+        freedtoa(dtoa_str);
+
+        /* add leading 0.000..., if applicable */
+        if (decpt <= 0)
         {
-            os_swprintf(pwstFormat, 32, L"%ls%%#-%d.%df", pwstSign, _pDF->iWidth - 1, _pDF->iPrec);
+            str.insert(str.begin(), 1 - decpt, '0');
+            str.insert(1, ".");
         }
-        else
+        else if (_pDF->bPrintPoint || decpt < str.length())
         {
-            os_swprintf(pwstFormat, 32, L"%ls%%-%d.%df", pwstSign, _pDF->iWidth - 2, _pDF->iPrec);  //-2 no point needed
+            str.append(fmax(0, decpt - str.length()), '0');
+            str.insert(decpt, ".");
         }
 
-        os_swprintf(pwstOutput, 32, pwstFormat, fabs(_dblVal));
+        wchar_t* tmp = to_wide_string(str.data());
+        os_swprintf(pwstOutput, 32, L"%ls%ls", pwstSign, tmp);
+        FREE(tmp);
     }
     else if (wcslen(pwstSign) != 0)
     {

scilab/modules/ast/Makefile.am
@@ -27,6 +27,7 @@ src/c/operations/matrix_multiplication.c \
 src/c/operations/matrix_power.c \
 src/c/operations/matrix_transpose.c \
 src/c/operations/operations_tools.c \
+src/c/types/dtoa.c \
 src/cpp/ast/expHistory.cpp \
 src/cpp/ast/macrovarvisitor.cpp \
 src/cpp/ast/printvisitor.cpp \
@@ -464,6 +465,7 @@ includes/types/colon.hxx \
 includes/types/container.hxx \
 includes/types/cpp_gateway_prototype.hxx \
 includes/types/double.hxx \
+includes/types/dtoa.h \
 includes/types/file.hxx \
 includes/types/function.hxx \
 includes/types/gatewaystruct.hxx \

scilab/modules/ast/Makefile.in
@@ -1,7 +1,7 @@
-# Makefile.in generated by automake 1.14.1 from Makefile.am.
+# Makefile.in generated by automake 1.15.1 from Makefile.am.
 # @configure_input@
 
-# Copyright (C) 1994-2013 Free Software Foundation, Inc.
+# Copyright (C) 1994-2017 Free Software Foundation, Inc.
 
 # This Makefile.in is free software; the Free Software Foundation
 # gives unlimited permission to copy and/or distribute it,
@@ -45,7 +45,17 @@
 
 
 VPATH = @srcdir@
-am__is_gnu_make = test -n '$(MAKEFILE_LIST)' && test -n '$(MAKELEVEL)'
+am__is_gnu_make = { \
+  if test -z '$(MAKELEVEL)'; then \
+    false; \
+  elif test -n '$(MAKE_HOST)'; then \
+    true; \
+  elif test -n '$(MAKE_VERSION)' && test -n '$(CURDIR)'; then \
+    true; \
+  else \
+    false; \
+  fi; \
+}
 am__make_running_with_option = \
   case $${target_option-} in \
       ?) ;; \
@@ -109,13 +119,6 @@ POST_UNINSTALL = :
 build_triplet = @build@
 host_triplet = @host@
 @ENABLE_DEBUG_TRUE@am__append_1 = src/cpp/types/inspector.cpp
-DIST_COMMON = $(top_srcdir)/Makefile.incl.am $(srcdir)/Makefile.in \
-	$(srcdir)/Makefile.am $(top_srcdir)/config/mkinstalldirs \
-	$(top_srcdir)/config/depcomp \
-	$(libsciast_call_scilab_la_include_HEADERS) \
-	$(libsciast_gvn_scilab_la_include_HEADERS) \
-	$(libsciast_la_include_HEADERS) \
-	$(libsciast_scilab_la_include_HEADERS)
 @NEED_JAVA_TRUE@am__append_2 = java
 subdir = modules/ast
 ACLOCAL_M4 = $(top_srcdir)/aclocal.m4
@@ -139,6 +142,11 @@ am__aclocal_m4_deps = $(top_srcdir)/m4/ax_cxx_compile_stdcxx_11.m4 \
 	$(top_srcdir)/m4/umfpack.m4 $(top_srcdir)/configure.ac
 am__configure_deps = $(am__aclocal_m4_deps) $(CONFIGURE_DEPENDENCIES) \
 	$(ACLOCAL_M4)
+DIST_COMMON = $(srcdir)/Makefile.am \
+	$(libsciast_call_scilab_la_include_HEADERS) \
+	$(libsciast_gvn_scilab_la_include_HEADERS) \
+	$(libsciast_la_include_HEADERS) \
+	$(libsciast_scilab_la_include_HEADERS) $(am__DIST_COMMON)
 mkinstalldirs = $(SHELL) $(top_srcdir)/config/mkinstalldirs
 CONFIG_HEADER = $(top_builddir)/modules/core/includes/machine.h
 CONFIG_CLEAN_FILES =
@@ -187,11 +195,11 @@ am__libsciast_la_SOURCES_DIST = src/c/operations/doublecomplex.c \
 	src/c/operations/matrix_multiplication.c \
 	src/c/operations/matrix_power.c \
 	src/c/operations/matrix_transpose.c \
-	src/c/operations/operations_tools.c src/cpp/ast/expHistory.cpp \
-	src/cpp/ast/macrovarvisitor.cpp src/cpp/ast/printvisitor.cpp \
-	src/cpp/ast/runvisitor.cpp src/cpp/ast/shortcutvisitor.cpp \
-	src/cpp/ast/treevisitor.cpp src/cpp/ast/visitor_common.cpp \
-	src/cpp/ast/consoledebugger.cpp \
+	src/c/operations/operations_tools.c src/c/types/dtoa.c \
+	src/cpp/ast/expHistory.cpp src/cpp/ast/macrovarvisitor.cpp \
+	src/cpp/ast/printvisitor.cpp src/cpp/ast/runvisitor.cpp \
+	src/cpp/ast/shortcutvisitor.cpp src/cpp/ast/treevisitor.cpp \
+	src/cpp/ast/visitor_common.cpp src/cpp/ast/consoledebugger.cpp \
 	src/cpp/ast/debuggervisitor.cpp src/cpp/ast/debugmanager.cpp \
 	src/cpp/ast/prettyprintvisitor.cpp \
 	src/cpp/operations/interface_addition.cpp \
@@ -384,6 +392,7 @@ am_libsciast_la_OBJECTS =  \
 	src/c/operations/libsciast_la-matrix_power.lo \
 	src/c/operations/libsciast_la-matrix_transpose.lo \
 	src/c/operations/libsciast_la-operations_tools.lo \
+	src/c/types/libsciast_la-dtoa.lo \
 	src/cpp/ast/libsciast_la-expHistory.lo \
 	src/cpp/ast/libsciast_la-macrovarvisitor.lo \
 	src/cpp/ast/libsciast_la-printvisitor.lo \
@@ -698,6 +707,9 @@ am__define_uniq_tagged_files = \
   done | $(am__uniquify_input)`
 ETAGS = etags
 CTAGS = ctags
+am__DIST_COMMON = $(srcdir)/Makefile.in $(top_srcdir)/Makefile.incl.am \
+	$(top_srcdir)/config/depcomp \
+	$(top_srcdir)/config/mkinstalldirs
 DISTFILES = $(DIST_COMMON) $(DIST_SOURCES) $(TEXINFOS) $(EXTRA_DIST)
 ACLOCAL = @ACLOCAL@
 ALL_LINGUAS = @ALL_LINGUAS@
@@ -981,7 +993,6 @@ pdfdir = @pdfdir@
 prefix = @prefix@
 program_transform_name = @program_transform_name@
 psdir = @psdir@
-runstatedir = @runstatedir@
 sbindir = @sbindir@
 sharedstatedir = @sharedstatedir@
 srcdir = @srcdir@
@@ -1001,11 +1012,11 @@ libsciast_la_SOURCES = src/c/operations/doublecomplex.c \
 	src/c/operations/matrix_multiplication.c \
 	src/c/operations/matrix_power.c \
 	src/c/operations/matrix_transpose.c \
-	src/c/operations/operations_tools.c src/cpp/ast/expHistory.cpp \
-	src/cpp/ast/macrovarvisitor.cpp src/cpp/ast/printvisitor.cpp \
-	src/cpp/ast/runvisitor.cpp src/cpp/ast/shortcutvisitor.cpp \
-	src/cpp/ast/treevisitor.cpp src/cpp/ast/visitor_common.cpp \
-	src/cpp/ast/consoledebugger.cpp \
+	src/c/operations/operations_tools.c src/c/types/dtoa.c \
+	src/cpp/ast/expHistory.cpp src/cpp/ast/macrovarvisitor.cpp \
+	src/cpp/ast/printvisitor.cpp src/cpp/ast/runvisitor.cpp \
+	src/cpp/ast/shortcutvisitor.cpp src/cpp/ast/treevisitor.cpp \
+	src/cpp/ast/visitor_common.cpp src/cpp/ast/consoledebugger.cpp \
 	src/cpp/ast/debuggervisitor.cpp src/cpp/ast/debugmanager.cpp \
 	src/cpp/ast/prettyprintvisitor.cpp \
 	src/cpp/operations/interface_addition.cpp \
@@ -1387,6 +1398,7 @@ includes/types/colon.hxx \
 includes/types/container.hxx \
 includes/types/cpp_gateway_prototype.hxx \
 includes/types/double.hxx \
+includes/types/dtoa.h \
 includes/types/file.hxx \
 includes/types/function.hxx \
 includes/types/gatewaystruct.hxx \
@@ -1528,7 +1540,6 @@ $(srcdir)/Makefile.in: @MAINTAINER_MODE_TRUE@ $(srcdir)/Makefile.am $(top_srcdir
 	echo ' cd $(top_srcdir) && $(AUTOMAKE) --foreign modules/ast/Makefile'; \
 	$(am__cd) $(top_srcdir) && \
 	  $(AUTOMAKE) --foreign modules/ast/Makefile
-.PRECIOUS: Makefile
 Makefile: $(srcdir)/Makefile.in $(top_builddir)/config.status
 	@case '$?' in \
 	  *config.status*) \
@@ -1537,7 +1548,7 @@ Makefile: $(srcdir)/Makefile.in $(top_builddir)/config.status
 	    echo ' cd $(top_builddir) && $(SHELL) ./config.status $(subdir)/$@ $(am__depfiles_maybe)'; \
 	    cd $(top_builddir) && $(SHELL) ./config.status $(subdir)/$@ $(am__depfiles_maybe);; \
 	esac;
-$(top_srcdir)/Makefile.incl.am:
+$(top_srcdir)/Makefile.incl.am $(am__empty):
 
 $(top_builddir)/config.status: $(top_srcdir)/configure $(CONFIG_STATUS_DEPENDENCIES)
 	cd $(top_builddir) && $(MAKE) $(AM_MAKEFLAGS) am--refresh
@@ -1618,6 +1629,14 @@ src/c/operations/libsciast_la-matrix_transpose.lo:  \
 src/c/operations/libsciast_la-operations_tools.lo:  \
 	src/c/operations/$(am__dirstamp) \
 	src/c/operations/$(DEPDIR)/$(am__dirstamp)
+src/c/types/$(am__dirstamp):
+	@$(MKDIR_P) src/c/types
+	@: > src/c/types/$(am__dirstamp)
+src/c/types/$(DEPDIR)/$(am__dirstamp):
+	@$(MKDIR_P) src/c/types/$(DEPDIR)
+	@: > src/c/types/$(DEPDIR)/$(am__dirstamp)
+src/c/types/libsciast_la-dtoa.lo: src/c/types/$(am__dirstamp) \
+	src/c/types/$(DEPDIR)/$(am__dirstamp)
 src/cpp/ast/$(am__dirstamp):
 	@$(MKDIR_P) src/cpp/ast
 	@: > src/cpp/ast/$(am__dirstamp)
@@ -2317,6 +2336,8 @@ mostlyclean-compile:
 	-rm -f *.$(OBJEXT)
 	-rm -f src/c/operations/*.$(OBJEXT)
 	-rm -f src/c/operations/*.lo
+	-rm -f src/c/types/*.$(OBJEXT)
+	-rm -f src/c/types/*.lo
 	-rm -f src/cpp/analysis/*.$(OBJEXT)
 	-rm -f src/cpp/analysis/*.lo
 	-rm -f src/cpp/ast/*.$(OBJEXT)
@@ -2345,6 +2366,7 @@ distclean-compile:
 @AMDEP_TRUE@@am__include@ @am__quote@src/c/operations/$(DEPDIR)/libsciast_la-matrix_power.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@src/c/operations/$(DEPDIR)/libsciast_la-matrix_transpose.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@src/c/operations/$(DEPDIR)/libsciast_la-operations_tools.Plo@am__quote@
+@AMDEP_TRUE@@am__include@ @am__quote@src/c/types/$(DEPDIR)/libsciast_la-dtoa.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@src/cpp/analysis/$(DEPDIR)/libsciast_la-AnalysisVisitor.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@src/cpp/analysis/$(DEPDIR)/libsciast_la-ArgnAnalyzer.Plo@am__quote@
 @AMDEP_TRUE@@am__include@ @am__quote@src/cpp/analysis/$(DEPDIR)/libsciast_la-Block.Plo@am__quote@
@@ -2665,6 +2687,13 @@ src/c/operations/libsciast_la-operations_tools.lo: src/c/operations/operations_t
 @AMDEP_TRUE@@am__fastdepCC_FALSE@	$(AM_V_CC)source='src/c/operations/operations_tools.c' object='src/c/operations/libsciast_la-operations_tools.lo' libtool=yes @AMDEPBACKSLASH@
 @AMDEP_TRUE@@am__fastdepCC_FALSE@	DEPDIR=$(DEPDIR) $(CCDEPMODE) $(depcomp) @AMDEPBACKSLASH@
 @am__fastdepCC_FALSE@	$(AM_V_CC@am__nodep@)$(LIBTOOL) $(AM_V_lt) --tag=CC $(AM_LIBTOOLFLAGS) $(LIBTOOLFLAGS) --mode=compile $(CC) $(DEFS) $(DEFAULT_INCLUDES) $(INCLUDES) $(libsciast_la_CPPFLAGS) $(CPPFLAGS) $(AM_CFLAGS) $(CFLAGS) -c -o src/c/operations/libsciast_la-operations_tools.lo `test -f 'src/c/operations/operations_tools.c' || echo '$(srcdir)/'`src/c/operations/operations_tools.c
+
+src/c/types/libsciast_la-dtoa.lo: src/c/types/dtoa.c
+@am__fastdepCC_TRUE@	$(AM_V_CC)$(LIBTOOL) $(AM_V_lt) --tag=CC $(AM_LIBTOOLFLAGS) $(LIBTOOLFLAGS) --mode=compile $(CC) $(DEFS) $(DEFAULT_INCLUDES) $(INCLUDES) $(libsciast_la_CPPFLAGS) $(CPPFLAGS) $(AM_CFLAGS) $(CFLAGS) -MT src/c/types/libsciast_la-dtoa.lo -MD -MP -MF src/c/types/$(DEPDIR)/libsciast_la-dtoa.Tpo -c -o src/c/types/libsciast_la-dtoa.lo `test -f 'src/c/types/dtoa.c' || echo '$(srcdir)/'`src/c/types/dtoa.c
+@am__fastdepCC_TRUE@	$(AM_V_at)$(am__mv) src/c/types/$(DEPDIR)/libsciast_la-dtoa.Tpo src/c/types/$(DEPDIR)/libsciast_la-dtoa.Plo
+@AMDEP_TRUE@@am__fastdepCC_FALSE@	$(AM_V_CC)source='src/c/types/dtoa.c' object='src/c/types/libsciast_la-dtoa.lo' libtool=yes @AMDEPBACKSLASH@
+@AMDEP_TRUE@@am__fastdepCC_FALSE@	DEPDIR=$(DEPDIR) $(CCDEPMODE) $(depcomp) @AMDEPBACKSLASH@
+@am__fastdepCC_FALSE@	$(AM_V_CC@am__nodep@)$(LIBTOOL) $(AM_V_lt) --tag=CC $(AM_LIBTOOLFLAGS) $(LIBTOOLFLAGS) --mode=compile $(CC) $(DEFS) $(DEFAULT_INCLUDES) $(INCLUDES) $(libsciast_la_CPPFLAGS) $(CPPFLAGS) $(AM_CFLAGS) $(CFLAGS) -c -o src/c/types/libsciast_la-dtoa.lo `test -f 'src/c/types/dtoa.c' || echo '$(srcdir)/'`src/c/types/dtoa.c
 
 .cpp.o:
 @am__fastdepCXX_TRUE@	$(AM_V_CXX)depbase=`echo $@ | sed 's|[^/]*$$|$(DEPDIR)/&|;s|\.o$$||'`;\
@@ -4278,6 +4307,7 @@ mostlyclean-libtool:
 clean-libtool:
 	-rm -rf .libs _libs
 	-rm -rf src/c/operations/.libs src/c/operations/_libs
+	-rm -rf src/c/types/.libs src/c/types/_libs
 	-rm -rf src/cpp/analysis/.libs src/cpp/analysis/_libs
 	-rm -rf src/cpp/ast/.libs src/cpp/ast/_libs
 	-rm -rf src/cpp/operations/.libs src/cpp/operations/_libs
@@ -4511,6 +4541,8 @@ distclean-generic:
 	-test . = "$(srcdir)" || test -z "$(CONFIG_CLEAN_VPATH_FILES)" || rm -f $(CONFIG_CLEAN_VPATH_FILES)
 	-rm -f src/c/operations/$(DEPDIR)/$(am__dirstamp)
 	-rm -f src/c/operations/$(am__dirstamp)
+	-rm -f src/c/types/$(DEPDIR)/$(am__dirstamp)
+	-rm -f src/c/types/$(am__dirstamp)
 	-rm -f src/cpp/analysis/$(DEPDIR)/$(am__dirstamp)
 	-rm -f src/cpp/analysis/$(am__dirstamp)
 	-rm -f src/cpp/ast/$(DEPDIR)/$(am__dirstamp)
@@ -4536,7 +4568,7 @@ clean-am: clean-generic clean-libtool clean-local \
 	clean-pkglibLTLIBRARIES mostlyclean-am
 
 distclean: distclean-am
-	-rm -rf src/c/operations/$(DEPDIR) src/cpp/analysis/$(DEPDIR) src/cpp/ast/$(DEPDIR) src/cpp/operations/$(DEPDIR) src/cpp/parse/$(DEPDIR) src/cpp/symbol/$(DEPDIR) src/cpp/system_env/$(DEPDIR) src/cpp/types/$(DEPDIR)
+	-rm -rf src/c/operations/$(DEPDIR) src/c/types/$(DEPDIR) src/cpp/analysis/$(DEPDIR) src/cpp/ast/$(DEPDIR) src/cpp/operations/$(DEPDIR) src/cpp/parse/$(DEPDIR) src/cpp/symbol/$(DEPDIR) src/cpp/system_env/$(DEPDIR) src/cpp/types/$(DEPDIR)
 	-rm -f Makefile
 distclean-am: clean-am distclean-compile distclean-generic \
 	distclean-local distclean-tags
@@ -4587,7 +4619,7 @@ install-ps-am:
 installcheck-am:
 
 maintainer-clean: maintainer-clean-am
-	-rm -rf src/c/operations/$(DEPDIR) src/cpp/analysis/$(DEPDIR) src/cpp/ast/$(DEPDIR) src/cpp/operations/$(DEPDIR) src/cpp/parse/$(DEPDIR) src/cpp/symbol/$(DEPDIR) src/cpp/system_env/$(DEPDIR) src/cpp/types/$(DEPDIR)
+	-rm -rf src/c/operations/$(DEPDIR) src/c/types/$(DEPDIR) src/cpp/analysis/$(DEPDIR) src/cpp/ast/$(DEPDIR) src/cpp/operations/$(DEPDIR) src/cpp/parse/$(DEPDIR) src/cpp/symbol/$(DEPDIR) src/cpp/system_env/$(DEPDIR) src/cpp/types/$(DEPDIR)
 	-rm -f Makefile
 maintainer-clean-am: distclean-am maintainer-clean-generic
 
@@ -4639,6 +4671,8 @@ uninstall-am: uninstall-libsciast_call_scilab_la_includeHEADERS \
 	uninstall-libsciast_la_includeHEADERS \
 	uninstall-libsciast_scilab_la_includeHEADERS \
 	uninstall-pkglibLTLIBRARIES
+
+.PRECIOUS: Makefile
 
 
 @BUILD_PARSER_TRUE@$(FLEX_SCANSCILAB_CXX): $(FLEX_SCANSCILAB_LL)

scilab/modules/ast/ast.vcxproj
@@ -475,6 +475,7 @@ lib /DEF:"$(ProjectDir)functions_manager_import.def" /SUBSYSTEM:WINDOWS /MACHINE
     <ClInclude Include="includes\types\cpp_gateway_prototype.hxx" />
     <ClInclude Include="includes\types\c_gateway_prototype.h" />
     <ClInclude Include="includes\types\double.hxx" />
+    <ClInclude Include="includes\types\dtoa.h" />
     <ClInclude Include="includes\types\file.hxx" />
     <ClInclude Include="includes\types\function.hxx" />
     <ClInclude Include="includes\types\gatewaystruct.hxx" />
@@ -752,6 +753,7 @@ lib /DEF:"$(ProjectDir)functions_manager_import.def" /SUBSYSTEM:WINDOWS /MACHINE
     <ClCompile Include="src\c\operations\matrix_power.c" />
     <ClCompile Include="src\c\operations\matrix_transpose.c" />
     <ClCompile Include="src\c\operations\operations_tools.c" />
+    <ClCompile Include="src\c\types\dtoa.c" />
   </ItemGroup>
   <ItemGroup>
     <ProjectReference Include="..\localization\src\localization.vcxproj">

scilab/modules/ast/includes/types/dtoa.h
@@ -1,0 +1,23 @@
+/*
+*  Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+*  Copyright (C) 2018 - Stéphane Mottelet
+*
+ *
+ * This file is hereby licensed under the terms of the GNU GPL v2.0,
+ * pursuant to article 5.3.4 of the CeCILL v.2.1.
+ * This file was originally licensed under the terms of the CeCILL v2.1,
+ * and continues to be available under such terms.
+ * For more information, see the COPYING file which you should have received
+ * along with this program.
+*
+*/
+
+#ifndef __DTOA_H__
+#define __DTOA_H__
+
+#include "dynlib_ast.h"
+
+EXTERN_AST char *dtoa(double d, int mode, int ndigits, int *decpt, int *sign, char **rve);
+EXTERN_AST void freedtoa(char *s);
+
+#endif // __DTOA_H__

scilab/modules/output_stream/tests/nonreg_tests/bug_15640.dia.ref
@@ -1,0 +1,26 @@
+// =============================================================================
+// Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+// Copyright (C) 2018 - Stéphane MOTTELET
+//
+//  This file is distributed under the same license as the Scilab package.
+// =============================================================================
+//
+// <-- CLI SHELL MODE -->
+//
+// <-- Non-regression test for bug 15640 -->
+//
+// <-- Bugzilla URL -->
+// http://bugzilla.scilab.org/15640
+//
+// <-- Short Description -->
+// display in mode format("e") is corrupted (regression w.r.t. Scilab 5.5.2)
+format("e",25)
+256
+ ans  =
+   2.560000000000000000D+02
+%pi
+ %pi  =
+   3.141592653589793116D+00
+nearfloat("succ",0)
+ ans  =
+   4.94065645841246544D-324

scilab/modules/output_stream/tests/nonreg_tests/bug_15640.tst
@@ -1,0 +1,21 @@
+// =============================================================================
+// Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+// Copyright (C) 2018 - Stéphane MOTTELET
+//
+//  This file is distributed under the same license as the Scilab package.
+// =============================================================================
+//
+// <-- CLI SHELL MODE -->
+//
+// <-- Non-regression test for bug 15640 -->
+//
+// <-- Bugzilla URL -->
+// http://bugzilla.scilab.org/15640
+//
+// <-- Short Description -->
+// display in mode format("e") is corrupted (regression w.r.t. Scilab 5.5.2)
+
+format("e",25)
+256
+%pi
+nearfloat("succ",0)

commit 79f8f21c978c46b47839fd7d3537e71c16fe2ffc

[ast] fix string() and sci2exp() regressions after b9e819c8
Regression detected by Samuel (test_run string bug_15260).
As a side effect, also fixes ambiguous display of floating
point numbers as pointed out by Samuel, again ;-) a long
time ago @ http://bugzilla.scilab.org/show_bug.cgi?id=2745

For example (default format lenght):

--> 1+%eps
 ans  =

   1.0000000

--> 1+%eps/2
 ans  =

   1.

string() behavior and polynom display remains the same as
before, i.e

--> x=1.00000001
 x  =

   1.0000000

--> string(x)
 ans  =

 1

--> x*(1+%s)
 ans  =

   1 +1s

Change-Id: I4dbcad7091c8de1144b84a4c2b4ec57b1e75e045

scilab/modules/ast/src/cpp/types/tostring_common.cpp
@@ -28,6 +28,7 @@ extern "C"
 #include "os_string.h"
 #include "dtoa.h"
 #include "charEncoding.h"
+#include "sciprint.h"
 }
 
 #define BLANK_SIZE 1
@@ -314,15 +315,28 @@ void addDoubleValue(std::wostringstream * _postr, double _dblVal, DoubleFormat *
             str.insert(str.begin(), 1 - iDecpt, '0');
             str.insert(1, ".");
         }
-        else if (_pDF->bPrintPoint || iDecpt < str.length())
+        else if (_pDF->bPrintPoint || iDecpt < (int)str.length())
         {
-            str.append(std::max(0, (int)(iDecpt - str.length())), '0');
+            str.append(std::max(0, (iDecpt - (int)str.length())), '0');
             str.insert(iDecpt, ".");
         }
-        else 
+        else
         {
-            str.append(std::max(0, (int)(iDecpt - str.length())), '0');
+            str.append(std::max(0, (iDecpt - (int)str.length())), '0');
             iWidth--;
+        }
+
+        // append trailing zeros, if applicable
+        if (std::atof(str.data()) != fabs(_dblVal))
+        {
+            if (_pDF->bPrintPoint)
+            {
+                str.append(std::max(0, (ConfigVariable::getFormatSize() - (int)str.length()))-1, '0');
+            }
+            else
+            {
+                iWidth = 1+str.length();
+            }
         }
 
         wchar_t* pwstData = to_wide_string(str.data());