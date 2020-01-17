scilab/scilab

commit 4466c367cdb303bea50e99b66ee62508e09d38b4

* Bug 16263 fixed: insertion in complex polynomial is repaired
https://bugzilla.scilab.org/show_bug.cgi?id=16263

This bug was also breaking addition of complex rationals:

https://bugzilla.scilab.org/show_bug.cgi?id=16262
https://bugzilla.scilab.org/show_bug.cgi?id=16261

Change-Id: Ic1daca304a967158a174573792be6968a44a9fe1

scilab/modules/ast/src/cpp/types/polynom.cpp
@@ -235,12 +235,14 @@ void Polynom::setVariableName(const std::wstring& _szVarName)
 
 bool Polynom::isComplex()
 {
-    if (m_iSize && m_pRealData[0])
-    {
-        return m_pRealData[0]->isComplex();
-    }
-
-    return false;
+        for (int i = 0 ; i < getSize() ; i++)
+        {
+            if  (m_pRealData[i] && m_pRealData[i]->isComplex())
+            {
+                return true;
+            }
+        }
+        return false;
 }
 
 Polynom* Polynom::setComplex(bool _bComplex)

scilab/modules/ast/tests/nonreg_tests/bug_16263.tst
@@ -1,0 +1,21 @@
+// =============================================================================
+// Scilab ( http://www.scilab.org/ ) - This file is part of Scilab
+// Copyright (C) 2019 - Stéphane MOTTELET
+//
+//  This file is distributed under the same license as the Scilab package.
+// =============================================================================
+//
+// <-- CLI SHELL MODE -->
+// <-- NO CHECK REF -->
+//
+// <-- Non-regression test for bug 16263 -->
+//
+// <-- Bugzilla URL -->
+// http://bugzilla.scilab.org/16263
+//
+// <-- Short Description -->
+// Polynomial insertion is broken for complex case
+
+num = [1 1+%i %i+%s];
+num(1) = 1;
+assert_checkfalse(isreal(num))

commit 88e33179961635da584fe7c68cae32f620982f74

fix performance regression after 4466c36
    test_run ast bug_16263
    tic();d(1:50000)=%s;toc()
    r = [0 %i 1/%s] +1  // doesn't crash scilab

Change-Id: Ie5e6f572274a12256d92618936dbf9954536946c

scilab/modules/ast/src/cpp/types/polynom.cpp
@@ -110,6 +110,8 @@ Polynom* Polynom::set(int _iPos, SinglePoly* _pS)
         return pIT;
     }
 
+    bool bComplex = isComplex();
+
     if (m_pRealData[_iPos])
     {
         delete m_pRealData[_iPos];
@@ -117,7 +119,6 @@ Polynom* Polynom::set(int _iPos, SinglePoly* _pS)
 
     m_pRealData[_iPos] = copyValue(_pS);
 
-    bool bComplex = isComplex();
     if (_pS->isComplex() && bComplex == false)
     {
         setComplex(true);
@@ -235,14 +236,12 @@ void Polynom::setVariableName(const std::wstring& _szVarName)
 
 bool Polynom::isComplex()
 {
-        for (int i = 0 ; i < getSize() ; i++)
-        {
-            if  (m_pRealData[i] && m_pRealData[i]->isComplex())
-            {
-                return true;
-            }
-        }
-        return false;
+    if (m_iSize && m_pRealData[0])
+    {
+        return m_pRealData[0]->isComplex();
+    }
+
+    return false;
 }
 
 Polynom* Polynom::setComplex(bool _bComplex)