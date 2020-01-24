scilab/scilab

commit c1dbf52856d2901254e2d05b6f7b8578690fa7d3

[ast] allow _iRetCount == 0 in gateways
http://bugzilla.scilab.org/show_bug.cgi?id=2036

Just an example of what can be done after this patch

* at the macro level (see plot.sci in the patch):

--> plot(1:10,sin(1:10),1:10,cos(1:10))

--> h=plot(1:10,sin(1:10),1:10,cos(1:10))
 h  =

2 by 1 matrix of handles:
=========================
Polyline
Polyline

--> plot(1:10,sin(1:10),1:10,cos(1:10)).foreground
 ans  =

   33.
   2.

zero value of argn(1) is overriden only in relevant cases: e.g. recursive
extraction or use of expression as an argument in another callExp.

* at the gateway level (here old Scilab C API) :

--> plot3d(rand(10,10))

--> h=plot3d(rand(10,10))
 h  =

Handle of type "Plot3d" with properties:
========================================
parent: Axes
children: []
visible = "on"
surface_mode = "on"
foreground = -1
thickness = 1
mark_mode = "off"
mark_style = 0
mark_size_unit = "tabulated"
mark_size = 0
mark_foreground = -1
mark_background = -2
data.x = [1,2,3,4,5,6,7,8,9,10]
data.y = [1,2,3,4,5,6,7,8,9,10]
data.z matrix 10x10
color_mode = 2
color_flag = 0
hiddencolor = 4
clip_state = "clipgrf"
clip_box = []
ambient_color = [0,0,0]
diffuse_color = [1,1,1]
specular_color = [1,1,1]
use_color_material = "on"
material_shininess = 2
user_data = []
tag = ""

Change-Id: I60f075f8e348fea51ca6ddc2c7985324de5d63f5

scilab/modules/ast/src/cpp/types/macro.cpp
@@ -31,7 +31,6 @@ extern "C"
 {
 #include "localization.h"
 #include "Scierror.h"
-#include "sciprint.h"
 #include "sci_malloc.h"
 #include "os_string.h"
 }
@@ -175,6 +174,9 @@ Callable::ReturnValue Macro::call(typed_list &in, optional_list &opt, int _iRetC
 {
     int rhs = (int)in.size();
     bool bVarargout = false;
+
+    int  iRetCount = std::max(0, _iRetCount);
+
     ReturnValue RetVal = Callable::OK;
     symbol::Context *pContext = symbol::Context::getInstance();
 
@@ -280,6 +282,7 @@ Callable::ReturnValue Macro::call(typed_list &in, optional_list &opt, int _iRetC
     // varargout is a list
     // varargout can containt more items than caller need
     // varargout must containt at leat caller needs
+
     if (m_outputArgs->size() >= 1 && m_outputArgs->back()->getSymbol().getName() == L"varargout")
     {
         bVarargout = true;
@@ -304,7 +307,8 @@ Callable::ReturnValue Macro::call(typed_list &in, optional_list &opt, int _iRetC
         m_pDblArgOut = m_pDblArgOut->clone();
         m_pDblArgOut->IncreaseRef();
     }
-    m_pDblArgOut->set(0, _iRetCount);
+
+    m_pDblArgOut->set(0, iRetCount);
 
     pContext->put(m_Nargin, m_pDblArgIn);
     pContext->put(m_Nargout, m_pDblArgOut);
@@ -338,7 +342,7 @@ Callable::ReturnValue Macro::call(typed_list &in, optional_list &opt, int _iRetC
     }
 
     //nb excepted output without varargout
-    int iRet = std::min((int)m_outputArgs->size() - (bVarargout ? 1 : 0), _iRetCount);
+    int iRet = std::min((int)m_outputArgs->size() - (bVarargout ? 1 : 0), std::max(1, iRetCount));
 
     //normal output management
     //for (std::list<symbol::Variable*>::iterator i = m_outputArgs->begin(); i != m_outputArgs->end() && _iRetCount; ++i, --_iRetCount)
@@ -397,7 +401,7 @@ Callable::ReturnValue Macro::call(typed_list &in, optional_list &opt, int _iRetC
         }
 
         List* pVarOut = pOut->getAs<List>();
-        const int size = std::min(pVarOut->getSize(), _iRetCount - (int)out.size());
+        const int size = std::min(pVarOut->getSize(), iRetCount - (int)out.size());
         for (int i = 0 ; i < size ; ++i)
         {
             InternalType* pIT = pVarOut->get(i);

commit 7a99aa2f0c7ab26245dac0e8d9bd9321bb8ffe9a

[ast] fix varargout regression after c1dbf528
Change-Id: Ib94bd0bcd2ed583eb8e4a83026f6b8308bea74ac

scilab/modules/ast/src/cpp/types/macro.cpp
@@ -402,7 +402,7 @@ Callable::ReturnValue Macro::call(typed_list &in, optional_list &opt, int _iRetC
         }
 
         List* pVarOut = pOut->getAs<List>();
-        const int size = std::min(pVarOut->getSize(), iRetCount - (int)out.size());
+        const int size = std::min(pVarOut->getSize(), std::max(1,iRetCount) - (int)out.size());
         for (int i = 0 ; i < size ; ++i)
         {
             InternalType* pIT = pVarOut->get(i);